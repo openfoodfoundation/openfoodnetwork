2e9e441ba1f41f3e67a276eba933cb9456072234
Andrew Spinks <aspinks@dius.com.au>
2012-04-09 10:31:47 +1000

    fix bug with states screwing up when using deface.

--- app/overrides/replace_delivery_addres.rb
+++ app/overrides/replace_delivery_addres.rb
@@ -2,3 +2,4 @@ Deface::Override.new(:virtual_path  => "spree/checkout/_address",
             :replace       => "[data-hook='shipping_fieldset_wrapper']",
             :partial       => "spree/checkout/distributor",
             :name          => "drop_off_point")
+
