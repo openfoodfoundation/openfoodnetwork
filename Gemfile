eb095baf64a5eaa07be03cfa0c6d7f49d4b97fb1
Rohan Mitchell <rohan@rohanmitchell.com>
2012-06-19 07:08:27 +1000

    Installed spork

--- Gemfile
+++ Gemfile
@@ -53,6 +53,7 @@ group :test, :development do
   gem 'machinist'
   gem 'capybara'
   gem 'database_cleaner', '0.7.1', :require => false
+  gem 'spork', '~> 1.0rc'
   gem 'pry'
 end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,57 +1,68 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-require 'rspec/autorun'
-require 'capybara'
-require 'database_cleaner'
+require 'rubygems'
+require 'spork'
+#uncomment the following line to use spork with the debugger
+#require 'spork/ext/ruby-debug'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Spork.prefork do
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'rspec/autorun'
+  require 'capybara'
+  require 'database_cleaner'
 
-require 'active_record/fixtures'
-fixtures_dir = File.expand_path('../../db/default', __FILE__)
-ActiveRecord::Fixtures.create_fixtures(fixtures_dir, ['spree/states', 'spree/countries'])
+  # Requires supporting ruby files with custom matchers and macros, etc,
+  # in spec/support/ and its subdirectories.
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+  require 'active_record/fixtures'
+  fixtures_dir = File.expand_path('../../db/default', __FILE__)
+  ActiveRecord::Fixtures.create_fixtures(fixtures_dir, ['spree/states', 'spree/countries'])
 
 
-RSpec.configure do |config|
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
+  RSpec.configure do |config|
+    # ## Mock Framework
+    #
+    # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+    #
+    # config.mock_with :mocha
+    # config.mock_with :flexmock
+    # config.mock_with :rr
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+    # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+    config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = false
+    # If you're not using ActiveRecord, or you'd prefer not to run each of your
+    # examples within a transaction, remove the following line or assign false
+    # instead of true.
+    config.use_transactional_fixtures = false
 
-  # If true, the base class of anonymous controllers will be inferred
-  # automatically. This will be the default behavior in future versions of
-  # rspec-rails.
-  config.infer_base_class_for_anonymous_controllers = false
+    # If true, the base class of anonymous controllers will be inferred
+    # automatically. This will be the default behavior in future versions of
+    # rspec-rails.
+    config.infer_base_class_for_anonymous_controllers = false
 
-  # ## Filters
-  #
-  config.filter_run_excluding :skip => true
+    # ## Filters
+    #
+    config.filter_run_excluding :skip => true
 
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :truncation, { :except => ['spree_countries', 'spree_states'] }
+    config.before(:suite) do
+      DatabaseCleaner.strategy = :truncation, { :except => ['spree_countries', 'spree_states'] }
+    end
+
+    config.before(:each) do
+      DatabaseCleaner.start
+    end
+
+    config.after(:each) do
+      DatabaseCleaner.clean
+    end
+
+    config.include Spree::UrlHelpers
   end
+end
 
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
-
-  config.include Spree::UrlHelpers
+Spork.each_run do
+  # Dir["#{File.dirname(__FILE__)}/../app/**/*.rb"].each {|f| load f}
+  # Rails.application.reload_routes!
 end
