b89562823bdaa0469b8eae60731e36aa4ab90f47
Rohan Mitchell <rohan@rohanmitchell.com>
2012-06-19 10:38:44 +1000

    Add distributors to admin product forms

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -2,7 +2,7 @@ Spree::Product.class_eval do
   belongs_to :supplier
   has_and_belongs_to_many :distributors
 
-  attr_accessible :supplier_id
+  attr_accessible :supplier_id, :distributor_ids
 
   validates_presence_of :distributors
 end
