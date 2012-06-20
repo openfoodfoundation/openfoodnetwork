b0a8645738d9640873e9b726c429ad6e49833b6f
Rohan Mitchell <rohan@rohanmitchell.com>
2012-06-20 17:44:11 +1000

    Display suppliers and distributors in sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :load_suppliers_and_distributors
+
+  private
+  def load_suppliers_and_distributors
+    @suppliers = Spree::Supplier.all
+    @distributors = Spree::Distributor.all
+  end
+
 end
