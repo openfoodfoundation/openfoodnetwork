d656898ecbd9944f7b645e7d502a90f4c9c01a6a
Rohan Mitchell <rohan@rohanmitchell.com>
2012-06-20 17:43:14 +1000

    Disable filtering distributors using spree filters

--- app/models/spree/taxon_decorator.rb
+++ app/models/spree/taxon_decorator.rb
@@ -2,7 +2,7 @@ Spree::Taxon.class_eval do
   # Indicate which filters should be used for this taxon
   def applicable_filters
     fs = []
-    fs << Spree::ProductFilters.distributor_filter if Spree::ProductFilters.respond_to? :distributor_filter
+    #fs << Spree::ProductFilters.distributor_filter if Spree::ProductFilters.respond_to? :distributor_filter
     fs
   end
 end
