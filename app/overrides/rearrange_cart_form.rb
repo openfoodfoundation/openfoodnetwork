f75b9bb176a25ef7e95ecee55e04edae267d18a1
Rohan Mitchell <rohan@rohanmitchell.com>
2012-06-26 10:58:09 +1000

    Swap order of continue shopping and empty cart buttons

--- app/overrides/rearrange_cart_form.rb
+++ app/overrides/rearrange_cart_form.rb
@@ -3,3 +3,8 @@ Deface::Override.new(:virtual_path  => "spree/orders/edit",
                      :replace       => "[data-hook='inside_cart_form']",
                      :partial       => "spree/orders/inside_cart_form",
                      :name          => "inside_cart_form")
+
+Deface::Override.new(:virtual_path  => "spree/orders/edit",
+                     :replace       => "#empty-cart[data-hook]",
+                     :partial       => "spree/orders/empty_cart_form",
+                     :name          => "empty_cart_form")
