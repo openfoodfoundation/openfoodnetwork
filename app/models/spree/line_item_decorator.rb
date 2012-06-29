411ba107ce8fd118307afc60ea1b587a1276ab85
Rohan Mitchell <rohan@rohanmitchell.com>
2012-06-29 10:47:11 +1000

    Perform shipping calculation in line item model

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,5 +1,7 @@
 Spree::LineItem.class_eval do
   def itemwise_shipping_cost
-    self.product.shipping_cost_for_distributor(self.order.distributor)
+    shipping_method = self.product.shipping_method_for_distributor(self.order.distributor)
+    order = OpenStruct.new :line_items => [self]
+    shipping_method.compute_amount(order)
   end
 end
--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -3,14 +3,17 @@ require 'spec_helper'
 module Spree
   describe LineItem do
     it "computes shipping cost for its product" do
-      distributor = double(:distributor)
-      order = double(:order, :distributor => distributor)
+      # Create a shipping method with flat rate of 10
+      shipping_method = create(:shipping_method)
+      shipping_method.calculator.set_preference :amount, 10
 
       product = double(:product)
-      product.should_receive(:shipping_cost_for_distributor).with(distributor).and_return(10)
+      product.should_receive(:shipping_method_for_distributor).and_return(shipping_method)
 
-      subject.stub(:order).and_return(order)
+      order = double(:order, :distributor => nil)
+
       subject.stub(:product).and_return(product)
+      subject.stub(:order).and_return(order)
 
       subject.itemwise_shipping_cost.should == 10
     end
