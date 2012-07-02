1e471e5e22dc740a7b0875e10e6ef3cd076f9b62
Rohan Mitchell <rohan@rohanmitchell.com>
2012-07-02 16:28:31 +1000

    Name all form-containing partials with _form, tweak admin new product form

--- app/overrides/add_distributor_to_admin_product.rb
+++ app/overrides/add_distributor_to_admin_product.rb
@@ -1,9 +1,9 @@
 Deface::Override.new(:virtual_path  => "spree/admin/products/_form",
-                     :insert_bottom  => "[data-hook='admin_product_form_additional_fields']",
-                     :partial       => "spree/admin/products/distributors",
+                     :insert_bottom => "[data-hook='admin_product_form_additional_fields']",
+                     :partial       => "spree/admin/products/distributors_form",
                      :name          => "distributors")
 
 Deface::Override.new(:virtual_path  => "spree/admin/products/new",
-                     :insert_bottom  => ".left",
-                     :partial       => "spree/admin/products/distributors",
+                     :insert_after  => "[data-hook='new_product_attrs']",
+                     :partial       => "spree/admin/products/distributors_form",
                      :name          => "distributors")
