58e9cd3eeb0d54b98d48259ac7926ffa9d33f1fb
Andrew Spinks <aspinks@dius.com.au>
2012-07-15 21:00:10 +1000

    fix distributor name in report

--- app/controllers/spree/admin/reports_controller_decorator.rb
+++ app/controllers/spree/admin/reports_controller_decorator.rb
@@ -26,12 +26,11 @@ Spree::Admin::ReportsController.class_eval do
     else
       csv_string = CSV.generate do |csv|
         csv << ["Order date", "Order Id", "Name","Email", "SKU", "Item cost", "Quantity", "Cost", "Shipping cost", "Distributor", "Distributor address", "Distributor city", "Distributor postcode"]
-        puts @orders
         @orders.each do |order|
           order.line_items.each do |line_item|
             csv << [order.created_at, order.id, order.bill_address.full_name, order.user.email,
               line_item.product.sku, line_item.product.name, line_item.quantity, line_item.price * line_item.quantity, line_item.itemwise_shipping_cost,
-              order.distributor.pickup_address.full_name, order.distributor.pickup_address.address1, order.distributor.pickup_address.city, order.distributor.pickup_address.zipcode ]
+              order.distributor.name, order.distributor.pickup_address.address1, order.distributor.pickup_address.city, order.distributor.pickup_address.zipcode ]
           end
         end
       end
