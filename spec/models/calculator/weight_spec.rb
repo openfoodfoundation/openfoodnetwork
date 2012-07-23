06c64225916057c6a8542e253c5097b8d14e9648
Rohan Mitchell <rohan@rohanmitchell.com>
2012-07-23 18:49:03 +1000

    Fix by-weight calculator to take line item quantity into account

--- spec/models/calculator/weight_spec.rb
+++ spec/models/calculator/weight_spec.rb
@@ -5,12 +5,12 @@ describe OpenFoodWeb::Calculator::Weight do
     variant_1 = double(:variant, :weight => 10)
     variant_2 = double(:variant, :weight => 20)
 
-    line_item_1 = double(:line_item, :variant => variant_1)
-    line_item_2 = double(:line_item, :variant => variant_2)
+    line_item_1 = double(:line_item, :variant => variant_1, :quantity => 1)
+    line_item_2 = double(:line_item, :variant => variant_2, :quantity => 3)
 
     order = double(:order, :line_items => [line_item_1, line_item_2])
 
     subject.set_preference(:per_kg, 10)
-    subject.compute(order).should == 300
+    subject.compute(order).should == (10*1 + 20*3) * 10
   end
 end
