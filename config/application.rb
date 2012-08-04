b4f867924b746b2e1b541429f01baa9307c15880
Rohan Mitchell <rohan@rohanmitchell.com>
2012-08-04 10:12:06 +1000

    Do not connect to db during asset precompile, fix heroku deploy to cedar

--- config/application.rb
+++ config/application.rb
@@ -65,6 +65,6 @@ module Openfoodweb
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.initialize_on_precompile = true
+    config.assets.initialize_on_precompile = false
   end
 end
