8394c47064cc3dbe6f8a07a36f64499ee40d5f15
Rohan Mitchell <rohan@rohanmitchell.com>
2012-09-13 12:27:20 +1000

    Add admin js includes

--- config/application.rb
+++ config/application.rb
@@ -66,6 +66,6 @@ module Openfoodweb
     config.assets.version = '1.0'
 
     config.assets.initialize_on_precompile = false
-    config.assets.precompile += ['store/all.css', 'store/all.js', 'admin/all.css', 'admin/**/*.js']
+    config.assets.precompile += ['store/all.css', 'store/all.js', 'admin/all.css', 'admin/*.js', 'admin/**/*.js']
   end
 end
