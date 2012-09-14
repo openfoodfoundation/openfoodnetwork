749252206678779da622e21a362b06e4db336bb8
Rohan Mitchell <rohan@rohanmitchell.com>
2012-09-14 15:35:22 +1000

    Improve clarity of various totals (items, shipping, grand total) on checkout page

--- app/helpers/spree/orders_helper_decorator.rb
+++ app/helpers/spree/orders_helper_decorator.rb
@@ -1,7 +1,9 @@
 module Spree
   module OrdersHelper
-    def order_delivery_fee_subtotal(order)
-      number_to_currency order.line_items.map { |li| li.itemwise_shipping_cost }.sum
+    def order_delivery_fee_subtotal(order, options={})
+      options.reverse_merge! :format_as_currency => true
+      amount = order.line_items.map { |li| li.itemwise_shipping_cost }.sum
+      options.delete(:format_as_currency) ? number_to_currency(amount) : amount
     end
   end
 end
