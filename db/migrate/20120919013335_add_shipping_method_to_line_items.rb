5402590d5f7a036ec2fef200e08a78cce68eeeae
Rohan Mitchell <rohan@rohanmitchell.com>
2012-09-19 15:08:05 +1000

    Add debug output on line item migration for lost data

--- db/migrate/20120919013335_add_shipping_method_to_line_items.rb
+++ db/migrate/20120919013335_add_shipping_method_to_line_items.rb
@@ -7,6 +7,7 @@ class AddShippingMethodToLineItems < ActiveRecord::Migration
         shipping_method = li.product.shipping_method_for_distributor(li.order.distributor)
       rescue ArgumentError
         shipping_method = Spree::ShippingMethod.find_by_name 'Producer Delivery'
+        say "Line item #{li.id} does not have a valid shipping method, setting to '#{shipping_method.name}'"
       end
 
       Spree::LineItem.update_all("shipping_method_id = #{shipping_method.id}", "id = #{li.id}")
