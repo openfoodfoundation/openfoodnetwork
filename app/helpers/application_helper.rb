e1d4ee318c9b9399910d252290425cea8aac28ec
Rohan Mitchell <rohan@rohanmitchell.com>
2012-10-11 12:00:30 +1100

    Fix issues with elRTE - sprites and functionality now work for link and image functions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,2 @@
 module ApplicationHelper
-  def cms_elrte_include_tags
-    stylesheet_link_tag(     'comfortable_mexican_sofa/codemirror') +
-      stylesheet_link_tag(   'comfortable_mexican_sofa/elrte') +
-      javascript_include_tag('comfortable_mexican_sofa/application')
-  end
 end
