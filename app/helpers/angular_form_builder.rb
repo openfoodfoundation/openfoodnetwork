52ebe2c495543856b84919160b062d9fe03d711d
Rohan Mitchell <rohan@rohanmitchell.com>
2012-11-22 16:38:25 +1100

    Provide proper names for angular select fields, add angular hidden field

--- app/helpers/angular_form_builder.rb
+++ app/helpers/angular_form_builder.rb
@@ -13,19 +13,35 @@ class AngularFormBuilder < ActionView::Helpers::FormBuilder
     # @fields_for_record_name --> :collection
     # @object.send(@fields_for_record_name).first.class.to_s.underscore --> enterprise_fee
 
-    name = "#{@object_name}[#{@fields_for_record_name}_attributes][{{ $index }}][#{method}]"
-    id = "#{@object_name}_#{@fields_for_record_name}_attributes_{{ $index }}_#{method}"
     value = "{{ #{@object.send(@fields_for_record_name).first.class.to_s.underscore}.#{method} }}"
 
-    @template.text_field_tag name, value, :id => id
+    @template.text_field_tag angular_name(method), value, :id => angular_id(method)
+  end
+
+  def angular_hidden_field(method, options = {})
+    value = "{{ #{@object.send(@fields_for_record_name).first.class.to_s.underscore}.#{method} }}"
+
+    @template.hidden_field_tag angular_name(method), value, :id => angular_id(method)
   end
 
   def angular_select(method, choices, angular_field, options = {})
-    @template.select_tag method, @template.angular_options_for_select(choices, angular_field), options.reverse_merge!({'ng-model' => angular_field})
+    options.reverse_merge!({'id' => angular_id(method)})
+
+    @template.select_tag angular_name(method), @template.angular_options_for_select(choices, angular_field), options
   end
 
   def angular_collection_select(method, collection, value_method, text_method, angular_field, options = {})
-    @template.select_tag method, @template.angular_options_from_collection_for_select(collection, value_method, text_method, angular_field), options.reverse_merge!({'ng-model' => angular_field})
+    options.reverse_merge!({'id' => angular_id(method)})
+
+    @template.select_tag angular_name(method), @template.angular_options_from_collection_for_select(collection, value_method, text_method, angular_field), options
   end
 
+  private
+  def angular_name(method)
+    "#{@object_name}[#{@fields_for_record_name}_attributes][{{ $index }}][#{method}]"
+  end
+
+  def angular_id(method)
+    "#{@object_name}_#{@fields_for_record_name}_attributes_{{ $index }}_#{method}"
+  end
 end
