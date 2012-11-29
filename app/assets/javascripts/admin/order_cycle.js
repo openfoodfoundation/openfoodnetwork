243e1ccd935f1630420f8fc1b6d208b6383afe30
Rohan Mitchell <rohan@rohanmitchell.com>
2012-11-29 11:16:59 +1100

    Add supplier to order cycle

--- app/assets/javascripts/admin/order_cycle.js
+++ app/assets/javascripts/admin/order_cycle.js
@@ -1,7 +1,22 @@
 function AdminOrderCycleCtrl($scope, $http) {
   $http.get('/admin/order_cycles/new.json').success(function(data) {
     $scope.order_cycle = data;
+    $scope.order_cycle.incoming_exchanges = [];
+    $scope.order_cycle.outgoing_exchanges = [];
   });
+
+  $http.get('/admin/enterprises.json').success(function(data) {
+    $scope.enterprises = {};
+
+    for(i in data) {
+      $scope.enterprises[data[i]['id']] = data[i];
+    }
+  });
+
+  $scope.addSupplier = function($event) {
+    $event.preventDefault();
+    $scope.order_cycle.incoming_exchanges.push({'enterprise_id': $scope.new_supplier_id});
+  };
 
   $scope.submit = function() {
     $http.post('/admin/order_cycles', {order_cycle: $scope.order_cycle}).success(function(data) {
