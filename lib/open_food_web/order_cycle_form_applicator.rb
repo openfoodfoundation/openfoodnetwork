07eac918a3817e3b9216b236d4718246a50d8334
Rohan Mitchell <rohan@rohanmitchell.com>
2012-11-30 17:08:57 +1100

    OrderCycleFormApplicator deletes removed exchanges

--- lib/open_food_web/order_cycle_form_applicator.rb
+++ lib/open_food_web/order_cycle_form_applicator.rb
@@ -5,6 +5,8 @@ module OpenFoodWeb
     end
 
     def go!
+      @touched_exchanges = []
+
       @order_cycle.incoming_exchanges.each do |exchange|
         if exchange_exists?(exchange[:enterprise_id], @order_cycle.coordinator_id)
           update_exchange(exchange[:enterprise_id], @order_cycle.coordinator_id)
@@ -12,6 +14,8 @@ module OpenFoodWeb
           add_exchange(exchange[:enterprise_id], @order_cycle.coordinator_id)
         end
       end
+
+      destroy_untouched_exchanges
     end
 
     def exchange_exists?(sender_id, receiver_id)
@@ -19,13 +23,23 @@ module OpenFoodWeb
     end
 
     def add_exchange(sender_id, receiver_id)
-      @order_cycle.exchanges.create! :sender_id => sender_id, :receiver_id => receiver_id
+      exchange = @order_cycle.exchanges.create! :sender_id => sender_id, :receiver_id => receiver_id
+      @touched_exchanges << exchange
     end
 
     def update_exchange(sender_id, receiver_id)
       # NOOP - when we're setting data on the exchange, we can do so here
 
-      #exchange = @order_cycle.exchanges.where(:sender_id => sender_id, :receiver_id => receiver_id).first
+      exchange = @order_cycle.exchanges.where(:sender_id => sender_id, :receiver_id => receiver_id).first
+      @touched_exchanges << exchange
+    end
+
+    def destroy_untouched_exchanges
+      untouched_exchanges.each { |exchange| exchange.destroy }
+    end
+
+    def untouched_exchanges
+      @order_cycle.exchanges - @touched_exchanges
     end
 
   end
--- spec/lib/open_food_web/order_cycle_form_applicator.rb
+++ spec/lib/open_food_web/order_cycle_form_applicator.rb
@@ -6,12 +6,13 @@ module OpenFoodWeb
       coordinator_id = 123
       supplier_id = 456
 
-      oc = double(:order_cycle, :coordinator_id => coordinator_id, :incoming_exchanges => [{:enterprise_id => supplier_id}])
+      oc = double(:order_cycle, :coordinator_id => coordinator_id, :exchanges => [], :incoming_exchanges => [{:enterprise_id => supplier_id}])
 
       applicator = OrderCycleFormApplicator.new(oc)
 
       applicator.should_receive(:exchange_exists?).and_return(false)
       applicator.should_receive(:add_exchange).with(supplier_id, coordinator_id)
+      applicator.should_receive(:destroy_untouched_exchanges)
 
       applicator.go!
     end
@@ -29,10 +30,27 @@ module OpenFoodWeb
 
       applicator.should_receive(:exchange_exists?).and_return(true)
       applicator.should_receive(:update_exchange).with(supplier_id, coordinator_id)
+      applicator.should_receive(:destroy_untouched_exchanges)
 
       applicator.go!
     end
 
-    it "removes exchanges that are no longer present in incoming_exchanges"
+    it "removes exchanges that are no longer present in incoming_exchanges" do
+      coordinator_id = 123
+      supplier_id = 456
+      exchange = double(:exchange, :sender_id => supplier_id, :receiver_id => coordinator_id)
+
+      oc = double(:order_cycle,
+                  :coordinator_id => coordinator_id,
+                  :exchanges => [exchange],
+                  :incoming_exchanges => [])
+
+      applicator = OrderCycleFormApplicator.new(oc)
+
+      applicator.should_receive(:destroy_untouched_exchanges)
+
+      applicator.go!
+      applicator.untouched_exchanges.should == [exchange]
+    end
   end
 end
