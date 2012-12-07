9cf6124df317569ab7aba5638e2aa021eee8e3c2
Rob H <oeoeaio@gmail.com>
2012-12-07 12:24:28 +1100

    Change checkout explanations from CMS pages to CMS snippets

--- app/overrides/add_cms_to_cart.rb
+++ app/overrides/add_cms_to_cart.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path  => "spree/orders/edit",
                      :insert_after  => "h1",
-                     :text          => "<%= cms_page_content(:content, Cms::Page.find_by_full_path('/cart')) %>",
+                     :text          => "<%= cms_snippet_content(Cms::Snippet.find_by_identifier('cart')) %>",
                      :name          => "cms_to_cart")
