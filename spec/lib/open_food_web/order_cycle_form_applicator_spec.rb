e53fff8e3185ff79e940749d413c6ce4db54946a
Rohan Mitchell <rohan@rohanmitchell.com>
2013-01-09 10:18:06 +1100

    Split OrderCycleFormApplicator specs into unit / integration, add integration spec for exchange_exists?

--- spec/lib/open_food_web/order_cycle_form_applicator_spec.rb
+++ spec/lib/open_food_web/order_cycle_form_applicator_spec.rb
@@ -2,67 +2,91 @@ require 'open_food_web/order_cycle_form_applicator'
 
 module OpenFoodWeb
   describe OrderCycleFormApplicator do
-    it "creates new exchanges for incoming_exchanges" do
-      coordinator_id = 123
-      supplier_id = 456
+    context "integration specs" do
+      before(:all) do
+        require 'spec_helper'
+      end
 
-      incoming_exchange = {:enterprise_id => supplier_id, :exchange_variants => {'1' => true, '2' => false, '3' => true}}
+      it "checks whether exchanges exist" do
+        oc = FactoryGirl.create(:simple_order_cycle)
+        exchange = FactoryGirl.create(:exchange, order_cycle: oc)
+        applicator = OrderCycleFormApplicator.new(oc)
 
-      oc = double(:order_cycle, :coordinator_id => coordinator_id, :exchanges => [], :incoming_exchanges => [incoming_exchange])
+        applicator.exchange_exists?(exchange.sender_id, exchange.receiver_id).should be_true
+        applicator.exchange_exists?(exchange.receiver_id, exchange.sender_id).should be_false
+        applicator.exchange_exists?(exchange.sender_id, 999).should be_false
+        applicator.exchange_exists?(999, exchange.receiver_id).should be_false
+        applicator.exchange_exists?(999, 888).should be_false
+      end
 
-      applicator = OrderCycleFormApplicator.new(oc)
+      it "adds exchanges"
 
-      applicator.should_receive(:exchange_variant_ids).with(incoming_exchange).and_return([1, 3])
-      applicator.should_receive(:exchange_exists?).and_return(false)
-      applicator.should_receive(:add_exchange).with(supplier_id, coordinator_id, [1, 3])
-      applicator.should_receive(:destroy_untouched_exchanges)
-
-      applicator.go!
+      it "updates exchanges"
     end
 
-    it "updates existing exchanges for incoming_exchanges" do
-      coordinator_id = 123
-      supplier_id = 456
+    context "unit specs" do
+      it "creates new exchanges for incoming_exchanges" do
+        coordinator_id = 123
+        supplier_id = 456
 
-      incoming_exchange = {:enterprise_id => supplier_id, :exchange_variants => {'1' => true, '2' => false, '3' => true}}
+        incoming_exchange = {:enterprise_id => supplier_id, :exchange_variants => {'1' => true, '2' => false, '3' => true}}
 
-      oc = double(:order_cycle,
-                  :coordinator_id => coordinator_id,
-                  :exchanges => [double(:exchange, :sender_id => supplier_id, :receiver_id => coordinator_id)],
-                  :incoming_exchanges => [incoming_exchange])
+        oc = double(:order_cycle, :coordinator_id => coordinator_id, :exchanges => [], :incoming_exchanges => [incoming_exchange])
 
-      applicator = OrderCycleFormApplicator.new(oc)
+        applicator = OrderCycleFormApplicator.new(oc)
 
-      applicator.should_receive(:exchange_variant_ids).with(incoming_exchange).and_return([1, 3])
-      applicator.should_receive(:exchange_exists?).and_return(true)
-      applicator.should_receive(:update_exchange).with(supplier_id, coordinator_id, [1, 3])
-      applicator.should_receive(:destroy_untouched_exchanges)
+        applicator.should_receive(:exchange_variant_ids).with(incoming_exchange).and_return([1, 3])
+        applicator.should_receive(:exchange_exists?).and_return(false)
+        applicator.should_receive(:add_exchange).with(supplier_id, coordinator_id, [1, 3])
+        applicator.should_receive(:destroy_untouched_exchanges)
 
-      applicator.go!
-    end
+        applicator.go!
+      end
 
-    it "removes exchanges that are no longer present in incoming_exchanges" do
-      coordinator_id = 123
-      supplier_id = 456
-      exchange = double(:exchange, :sender_id => supplier_id, :receiver_id => coordinator_id)
+      it "updates existing exchanges for incoming_exchanges" do
+        coordinator_id = 123
+        supplier_id = 456
 
-      oc = double(:order_cycle,
-                  :coordinator_id => coordinator_id,
-                  :exchanges => [exchange],
-                  :incoming_exchanges => [])
+        incoming_exchange = {:enterprise_id => supplier_id, :exchange_variants => {'1' => true, '2' => false, '3' => true}}
 
-      applicator = OrderCycleFormApplicator.new(oc)
+        oc = double(:order_cycle,
+                    :coordinator_id => coordinator_id,
+                    :exchanges => [double(:exchange, :sender_id => supplier_id, :receiver_id => coordinator_id)],
+                    :incoming_exchanges => [incoming_exchange])
 
-      applicator.should_receive(:destroy_untouched_exchanges)
+        applicator = OrderCycleFormApplicator.new(oc)
 
-      applicator.go!
-      applicator.untouched_exchanges.should == [exchange]
-    end
+        applicator.should_receive(:exchange_variant_ids).with(incoming_exchange).and_return([1, 3])
+        applicator.should_receive(:exchange_exists?).and_return(true)
+        applicator.should_receive(:update_exchange).with(supplier_id, coordinator_id, [1, 3])
+        applicator.should_receive(:destroy_untouched_exchanges)
 
-    it "converts exchange variant ids hash to an array of ids" do
-      applicator = OrderCycleFormApplicator.new(nil)
+        applicator.go!
+      end
 
-      applicator.send(:exchange_variant_ids, {:enterprise_id => 123, :exchange_variants => {'1' => true, '2' => false, '3' => true}}).should == [1, 3]
+      it "removes exchanges that are no longer present in incoming_exchanges" do
+        coordinator_id = 123
+        supplier_id = 456
+        exchange = double(:exchange, :sender_id => supplier_id, :receiver_id => coordinator_id)
+
+        oc = double(:order_cycle,
+                    :coordinator_id => coordinator_id,
+                    :exchanges => [exchange],
+                    :incoming_exchanges => [])
+
+        applicator = OrderCycleFormApplicator.new(oc)
+
+        applicator.should_receive(:destroy_untouched_exchanges)
+
+        applicator.go!
+        applicator.untouched_exchanges.should == [exchange]
+      end
+
+      it "converts exchange variant ids hash to an array of ids" do
+        applicator = OrderCycleFormApplicator.new(nil)
+
+        applicator.send(:exchange_variant_ids, {:enterprise_id => 123, :exchange_variants => {'1' => true, '2' => false, '3' => true}}).should == [1, 3]
+      end
     end
   end
 end
