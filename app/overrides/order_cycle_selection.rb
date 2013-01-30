8b5aea42b825039bdb5de20ca6c02c85c856d73c
Rohan Mitchell <rohan@rohanmitchell.com>
2013-01-31 09:53:06 +1100

    Add deface for order cycle selection

---
+++ app/overrides/order_cycle_selection.rb
@@ -0,0 +1,14 @@
+Deface::Override.new(:virtual_path => "spree/home/index",
+                     :insert_before => "[data-hook='homepage_products']",
+                     :partial      => "order_cycles/selection",
+                     :name         => "order_cycle_selection_home")
+
+Deface::Override.new(:virtual_path => "spree/products/index",
+                     :insert_top   => "[data-hook='homepage_products']",
+                     :partial      => "order_cycles/selection",
+                     :name         => "order_cycle_selection_products")
+
+Deface::Override.new(:virtual_path => "spree/taxons/show",
+                     :insert_top   => "[data-hook='homepage_products']",
+                     :partial      => "order_cycles/selection",
+                     :name         => "order_cycle_selection_taxon")
