1d4fbe865b61326839306417b1499135fb45b166
Rob H <oeoeaio@gmail.com>
2013-02-09 00:40:34 +1100

    Finished revision of defaces

--- app/overrides/order_item_description.rb
+++ app/overrides/order_item_description.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(:virtual_path  => "spree/shared/_order_details",
                      :replace       => "[data-hook='order_item_description']",
                      :partial       => "spree/orders/order_item_description",
-                     :name          => "order_item_description")
+                     :name          => "order_item_description",
+                     :original      => '1729abc5f441607b09cc0d44843a8dfd660ac5e0')
