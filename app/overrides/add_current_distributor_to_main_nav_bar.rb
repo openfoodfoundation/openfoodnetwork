2cdf458d6ae498a7c5f267a86dacbb7f22d928aa
Rob H <oeoeaio@gmail.com>
2013-02-14 15:42:47 +1100

    Move additional of tabs to main nav bar to defaces, rather than overriding view directly

---
+++ app/overrides/add_current_distributor_to_main_nav_bar.rb
@@ -0,0 +1,5 @@
+Deface::Override.new(:virtual_path  => "spree/shared/_main_nav_bar",
+                     :name          => "add_current_distributor_to_main_nav_bar",
+                     :insert_after  => "li#link-to-cart",
+                     :partial       => "spree/shared/current_distributor",
+                     :original      => '0d843946b3a53643c5a7da90a3a21610208db866')
