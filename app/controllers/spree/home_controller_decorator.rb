0874f12797fc6fe2982fbaed7a98dec8cf091e46
Rohan Mitchell <rohan@rohanmitchell.com>
2013-04-05 14:30:11 +1100

    Controllers provide current order cycle for product splitting

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -2,9 +2,10 @@ require 'open_food_web/split_products_by_distribution'
 
 Spree::HomeController.class_eval do
   include EnterprisesHelper
+  include OrderCyclesHelper
   include OpenFoodWeb::SplitProductsByDistribution
 
   respond_override :index => { :html => { :success => lambda {
-        @products, @products_local, @products_remote = split_products_by_distribution @products, current_distributor
+        @products, @products_local, @products_remote = split_products_by_distribution @products, current_distributor, current_order_cycle
       } } }
 end
--- app/controllers/spree/taxons_controller_decorator.rb
+++ app/controllers/spree/taxons_controller_decorator.rb
@@ -2,9 +2,10 @@ require 'open_food_web/split_products_by_distribution'
 
 Spree::TaxonsController.class_eval do
   include EnterprisesHelper
+  include OrderCyclesHelper
   include OpenFoodWeb::SplitProductsByDistribution
 
   respond_override :show => { :html => { :success => lambda {
-        @products, @products_local, @products_remote = split_products_by_distribution @products, current_distributor
+        @products, @products_local, @products_remote = split_products_by_distribution @products, current_distributor, current_order_cycle
       } } }
 end
