f8ebb0c7157bc8157c72921a1ea54b94ab141bbf
Rohan Mitchell <rohan@rohanmitchell.com>
2013-04-05 11:34:03 +1100

    Rename split partial and update overrides

--- app/overrides/split_products_by_distributor_on_products_home.rb
+++ app/overrides/split_products_by_distributor_on_products_home.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/products/index",
                      :replace      => "[data-hook='homepage_products']",
-                     :partial      => "spree/shared/products_by_distributor",
-                     :name         => "split_products_by_distributor_on_products_home",
+                     :partial      => "spree/shared/products_by_distribution",
+                     :name         => "split_products_by_distribution_on_products_home",
                      :original     => '22097416de0a5851d43c572301779de06ed84d17')
--- app/overrides/split_products_by_distributor_on_taxons_show.rb
+++ app/overrides/split_products_by_distributor_on_taxons_show.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/taxons/show",
                      :replace      => "[data-hook='taxon_products']",
-                     :partial      => "spree/shared/products_by_distributor",
-                     :name         => "split_products_by_distributor_on_taxons_show",
+                     :partial      => "spree/shared/products_by_distribution",
+                     :name         => "split_products_by_distribution_on_taxons_show",
                      :original     => '27b6ecd3954022246568b3ddf5e80462aa511ece')
