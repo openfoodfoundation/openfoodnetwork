c5e8ac6fd14760134c61b15338ebb2bb40426ed2
Rohan Mitchell <rohan@rohanmitchell.com>
2013-04-19 09:39:54 +1000

    Newrelic unicorn compatibility

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,2 +1,3 @@
+preload_app true   # https://newrelic.com/docs/ruby/no-data-with-unicorn
 worker_processes 4 # amount of unicorn workers to spin up
 timeout 30         # restarts workers that hang for 30 seconds
