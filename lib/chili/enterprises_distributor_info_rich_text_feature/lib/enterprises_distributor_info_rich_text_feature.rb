49001295111f5a843fd14cb4d605edd0d78ad877
Rohan Mitchell <rohan@rohanmitchell.com>
2013-04-26 12:31:11 +1000

    Enable feature for local organics only

--- lib/chili/enterprises_distributor_info_rich_text_feature/lib/enterprises_distributor_info_rich_text_feature.rb
+++ lib/chili/enterprises_distributor_info_rich_text_feature/lib/enterprises_distributor_info_rich_text_feature.rb
@@ -3,5 +3,5 @@ require "enterprises_distributor_info_rich_text_feature/engine"
 
 module EnterprisesDistributorInfoRichTextFeature
   extend Chili::Base
-  active_if { true } # edit this to activate/deactivate feature at runtime
+  active_if { ENV['OFW_DEPLOYMENT'] == 'local_organics' }
 end
