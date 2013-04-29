a1f189627b9b329b0d09c05a46215ce9ca1e7521
Rohan Mitchell <rohan@rohanmitchell.com>
2013-04-29 16:34:19 +1000

    Only show UserVoice feedback button on Eaterprises deployment

--- app/overrides/add_feedback_script.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path  => "spree/layouts/spree_application",
-                     :insert_bottom => "[data-hook='inside_head']",
-                     :partial       => "layouts/feedback_script",
-                     :name          => "feedback_script")
