f19ef4ed07847d88b3e8c549227b00c17b355274
Rohan Mitchell <rohan@rohanmitchell.com>
2013-05-20 16:05:25 +1000

    Move save button back within form, fix failing test

--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -19,25 +19,6 @@ module Spree
       it "computes shipping cost for its product" do
         line_item.itemwise_shipping_cost.should == 10
       end
-
-      it "updates shipping method when order has not yet been placed" do
-        %w(cart address delivery resumed).each do |state|
-          order.stub(:state).and_return(state)
-          line_item.should_receive(:set_itemwise_shipping_method)
-          line_item.should_receive(:shipping_method_id_changed?).and_return(true)
-          line_item.should_receive(:save!)
-          line_item.itemwise_shipping_cost
-        end
-      end
-
-      it "does not update shipping method when order has been placed" do
-        %w(payment confirm complete cancelled returned awaiting_return).each do |state|
-          order.stub(:state).and_return(state)
-          line_item.should_receive(:set_itemwise_shipping_method).never
-          line_item.should_receive(:save!).never
-          line_item.itemwise_shipping_cost
-        end
-      end
     end
   end
 end
