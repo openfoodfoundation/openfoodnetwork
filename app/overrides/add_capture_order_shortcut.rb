30253ffd074374342ba7fd5ce8d5b52b206623c3
David Cook <david@redcliffs.net>
2013-06-12 15:51:07 +1000

    align actions column left for better usability

--- app/overrides/add_capture_order_shortcut.rb
+++ app/overrides/add_capture_order_shortcut.rb
@@ -20,4 +20,9 @@ Deface::Override.new(:virtual_path    => "spree/admin/orders/index",
                       :set_attributes => "#listing_orders colgroup col:last-child",
                       :attributes     => {:style => "width: 12%"} #was 8%
                       )
-                      
+#And align actions column (not spree standard, but looks better IMO)
+Deface::Override.new(:virtual_path    => "spree/admin/orders/index",
+                      :name           => "add_capture_order_shortcut_align",
+                      :set_attributes => "[data-hook='admin_orders_index_row_actions']",
+                      :attributes     => {:class => "actions", :style => "text-align:left;"} #removes 'align-center' class
+                      )
