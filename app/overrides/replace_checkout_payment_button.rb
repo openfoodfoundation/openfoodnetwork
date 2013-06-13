203232de27144aeb76c2e6827438ba39deb41961
David Cook <david@redcliffs.net>
2013-06-13 12:30:35 +1000

    Disable as this has now been incorporated into move_save_checkout_button

--- app/overrides/replace_checkout_payment_button.rb
+++ app/overrides/replace_checkout_payment_button.rb
@@ -2,4 +2,5 @@ Deface::Override.new(:virtual_path => "spree/checkout/_payment",
                      :replace      => "code[erb-loud]:contains('submit_tag t(:save_and_continue)')",
                      :text         => "<%= submit_tag 'Process My Order', :class => 'continue button primary' %>",
                      :name         => "replace_checkout_payment_button",
-                     :original     => 'ce2043a01931b3bc16b045302ebb0e0bb9150b67')
+                     :original     => 'ce2043a01931b3bc16b045302ebb0e0bb9150b67',
+                     :disabled     => true)
