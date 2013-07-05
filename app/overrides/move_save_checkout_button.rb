8df0f30b4f15108d0416f124378d24873812cf08
David Cook <david@redcliffs.net>
2013-07-05 16:08:26 +1000

    Move override into .deface format, and removed unecessary bits

--- app/overrides/move_save_checkout_button.rb
+++ app/overrides/move_save_checkout_button.rb
@@ -1,14 +1,22 @@
-
+=begin
 # Add a new 'Save and Continue/Process My Order' button under Order Summary on the checkout pages
 Deface::Override.new(:virtual_path  => "spree/checkout/edit",
                      :insert_after  => "[data-hook='checkout_summary_box']",
-                     :text          => '<div id="add_new_save_checkout_button" class="columns omega four" style="margin-top: 30px; padding-top:10px; border-top: 1px solid #d9d9db;">
+                     :text          => '<div id="add_new_save_checkout_button" class="columns omega four" 
+                                            style="display:none; margin-top: 30px; padding-top:10px; border-top: 1px solid #d9d9db;">
                                           <%= submit_tag @order.state == "payment" ? "Process My Order" : t(:save_and_continue), 
                                                           :class => "continue button primary large", 
                                                           :form=> "checkout_form_#{@order.state}" %>
-                                          <script>disableSaveOnClick();</script>
+                                          <script>
+                                            disableSaveOnClick();
+                                            //Show additional button only if form attribute is supported
+                                            if ($("#add_new_save_checkout_button input[type=submit]")[0].form)
+                                              $("#add_new_save_checkout_button").show();
+                                          </script>
                                         </div>',
-                     :name          => "add_new_save_checkout_button")
+                     :name          => "add_new_save_checkout_button",
+                     :disabled      => true)
+ =end
                      
 # Remove the old button from each partial
 =begin
@@ -28,13 +36,15 @@ Deface::Override.new(:virtual_path  => "spree/checkout/_payment",
                      :original      => '312bd1fc045d5bde88f37b41b89ff3ca08beb950')
 =end
 
+#Hide 
+=begin
 Deface::Override.new(:virtual_path  => "spree/checkout/edit",
                      :insert_after  => "#checkout",
                      :text          => '<script type="text/javascript">
                                           if (!$("#add_new_save_checkout_button input[type=submit]")[0].form)
                                           {
-                                            alert("non html5 brwowser doesnt support form attribute");
                                             $("#add_new_save_checkout_button").hide();
                                           }
                                         </script>',
                      :name          => "hide_new_save_checkout_button")
+=end
