0a25dfc679d54df5044ebd839f54bf9e91c34afb
David Cook <david@redcliffs.net>
2013-07-16 17:01:12 +1000

    remove disabled flag

--- app/overrides/replace_checkout_payment_button.rb
+++ app/overrides/replace_checkout_payment_button.rb
@@ -2,5 +2,4 @@ Deface::Override.new(:virtual_path => "spree/checkout/_payment",
                      :replace      => "code[erb-loud]:contains('submit_tag t(:save_and_continue)')",
                      :text         => "<%= submit_tag 'Process My Order', :class => 'continue button primary' %>",
                      :name         => "replace_checkout_payment_button",
-                     :original     => 'ce2043a01931b3bc16b045302ebb0e0bb9150b67',
-                     :disabled     => false)
+                     :original     => 'ce2043a01931b3bc16b045302ebb0e0bb9150b67')
