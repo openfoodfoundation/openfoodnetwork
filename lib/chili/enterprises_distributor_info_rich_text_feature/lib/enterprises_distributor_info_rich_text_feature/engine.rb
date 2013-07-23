b73c584c3b59f31f157b5f008bce31bb3575a07a
Rohan Mitchell <rohan@rohanmitchell.com>
2013-07-23 14:00:29 +1000

    Update order confirmation email in line with changes in spree

--- lib/chili/enterprises_distributor_info_rich_text_feature/lib/enterprises_distributor_info_rich_text_feature/engine.rb
+++ lib/chili/enterprises_distributor_info_rich_text_feature/lib/enterprises_distributor_info_rich_text_feature/engine.rb
@@ -8,12 +8,10 @@ module EnterprisesDistributorInfoRichTextFeature
       if OpenFoodWeb::FeatureToggle.enabled? :enterprises_distributor_info_rich_text
         ::Spree::OrderMailer.class_eval do
           def confirm_email(order, resend = false)
-            @order = order
+            find_order(order)
             subject = (resend ? "[#{t(:resend).upcase}] " : '')
-            subject += "#{Spree::Config[:site_name]} #{t('order_mailer.confirm_email.subject')} ##{order.number}"
-
-            mail(:to => order.email,
-                 :subject => subject,
+            subject += "#{Spree::Config[:site_name]} #{t('order_mailer.confirm_email.subject')} ##{@order.number}"
+            mail(:to => @order.email, :from => from_address, :subject => subject,
                  :template_name => 'confirm_email_with_distributor_info')
           end
         end
