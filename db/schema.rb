ba0b3bad8562f2287df436849ce6f4a6d9fc6841
Rohan Mitchell <rohan@rohanmitchell.com>
2013-08-09 17:35:34 +1000

    Convert product distribution shipping methods to enterprise fees

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130807230834) do
+ActiveRecord::Schema.define(:version => 20130809045637) do
 
   create_table "carts", :force => true do |t|
     t.integer "user_id"
