cace60b18705ec4c6e0351e28e2e3e909b7404a4
Rohan Mitchell <rohan@rohanmitchell.com>
2013-08-30 12:27:13 +1000

    Shipping methods are only shown at checkout if their distributor matches the order

--- app/models/spree/shipping_method_decorator.rb
+++ app/models/spree/shipping_method_decorator.rb
@@ -3,4 +3,11 @@ Spree::ShippingMethod.class_eval do
   attr_accessible :distributor_id
 
   validates_presence_of :distributor_id
+
+  def available_to_order_with_distributor_check?(order, display_on=nil)
+    available_to_order_without_distributor_check?(order, display_on) &&
+      (order.distributor == self.distributor)
+  end
+  alias_method_chain :available_to_order?, :distributor_check
+
 end
--- spec/models/shipping_method_spec.rb
+++ spec/models/shipping_method_spec.rb
@@ -2,12 +2,27 @@ require 'spec_helper'
 
 module Spree
   describe ShippingMethod do
-    it "should be valid when built from factory" do
+    it "is valid when built from factory" do
       build(:shipping_method).should be_valid
     end
 
-    it "should require a distributor" do
+    it "requires a distributor" do
       build(:shipping_method, distributor: nil).should_not be_valid
+    end
+
+    describe "availability" do
+      let(:sm) { build(:shipping_method) }
+
+      it "is available to orders that match its distributor" do
+        o = build(:order, ship_address: build(:address), distributor: sm.distributor)
+        sm.should be_available_to_order o
+      end
+
+      it "is not available to orders that do not match its distributor" do
+        o = build(:order, ship_address: build(:address),
+                  distributor: build(:distributor_enterprise))
+        sm.should_not be_available_to_order o
+      end
     end
   end
 end
