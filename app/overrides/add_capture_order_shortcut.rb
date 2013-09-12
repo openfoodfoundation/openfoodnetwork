a99f23eb0144e718be1f5fc8248939ab6fa6eb86
David Cook <david@redcliffs.net>
2013-09-12 15:16:51 +1000

    Add distributor name to admin orders table

--- app/overrides/add_capture_order_shortcut.rb
+++ app/overrides/add_capture_order_shortcut.rb
@@ -3,19 +3,7 @@ Deface::Override.new(:virtual_path    => "spree/admin/orders/index",
                       :insert_bottom  => "[data-hook='admin_orders_index_row_actions']",
                       :partial        => 'spree/admin/orders/capture'
                       )
-
-#Resize columns to fit new button (note: this may break with a new version of spree)
-Deface::Override.new(:virtual_path    => "spree/admin/orders/index",
-                      :name           => "add_capture_order_shortcut_first_column",
-                      :set_attributes => "#listing_orders colgroup col:first-child",
-                      :attributes     => {:style => "width: 12%"} #was 16%
-                      )
-Deface::Override.new(:virtual_path    => "spree/admin/orders/index",
-                      :name           => "add_capture_order_shortcut_last_column",
-                      :set_attributes => "#listing_orders colgroup col:last-child",
-                      :attributes     => {:style => "width: 12%"} #was 8%
-                      )
-#And align actions column (not spree standard, but looks better IMO)
+# And align actions column (not spree standard, but looks better IMO)
 Deface::Override.new(:virtual_path    => "spree/admin/orders/index",
                       :name           => "add_capture_order_shortcut_align",
                       :set_attributes => "[data-hook='admin_orders_index_row_actions']",
