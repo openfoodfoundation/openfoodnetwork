d1979a70a1a0ca7d691fe55fce1e01dc645a79b2
Rohan Mitchell <rohan@rohanmitchell.com>
2013-09-16 14:26:43 +1000

    Fix migration - dependency on modified model relations

--- db/migrate/20130912021553_create_distributors_payment_methods.rb
+++ db/migrate/20130912021553_create_distributors_payment_methods.rb
@@ -1,4 +1,9 @@
 class CreateDistributorsPaymentMethods < ActiveRecord::Migration
+  class Spree::PaymentMethod < ActiveRecord::Base
+    belongs_to :distributor, class_name: 'Enterprise'
+    has_and_belongs_to_many :distributors, join_table: 'distributors_payment_methods', :class_name => 'Enterprise', association_foreign_key: 'distributor_id'
+  end
+
   def up
     create_table :distributors_payment_methods, :id => false do |t|
       t.references :distributor
