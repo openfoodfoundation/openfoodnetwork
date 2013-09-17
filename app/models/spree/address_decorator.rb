56590e2f4ad8ceda722c92d84d33793224895843
Rohan Mitchell <rohan@rohanmitchell.com>
2013-09-17 18:06:06 +1000

    Instrument saving spree addresses with invalid values in an attempt to track down the cause of some invalid records finding their way into the database

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -1,3 +1,5 @@
+require 'open_food_web/notify_invalid_address_save'
+
 Spree::Address.class_eval do
   has_one :enterprise
 
@@ -7,7 +9,7 @@ Spree::Address.class_eval do
   delegate :name, :to => :state, :prefix => true, :allow_nil => true
 
   def full_address
-    full_address = [address1, address2, zipcode, city, country.name, state.andand.name]
+    full_address = [address1, address2, zipcode, city, country.andand.name, state.andand.name]
     filtered_address = full_address.select{ |field| !field.nil? && field != '' }
     filtered_address.compact.join(', ')
   end
