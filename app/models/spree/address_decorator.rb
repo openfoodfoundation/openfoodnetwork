2f731614215527b3b5ce042e3bd3e366de639d2b
Rohan Mitchell <rohan@rohanmitchell.com>
2013-09-18 07:14:21 +1000

    Move patch to address class - much simpler

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -1,5 +1,3 @@
-require 'open_food_web/notify_invalid_address_save'
-
 Spree::Address.class_eval do
   has_one :enterprise
 
@@ -12,5 +10,31 @@ Spree::Address.class_eval do
     full_address = [address1, address2, zipcode, city, country.andand.name, state.andand.name]
     filtered_address = full_address.select{ |field| !field.nil? && field != '' }
     filtered_address.compact.join(', ')
+  end
+
+
+  private
+
+  # We have a hard-to-track-down bug around invalid addresses with all-nil fields finding
+  # their way into the database. I don't know what the source of them is, so this patch
+  # is designed to track them down.
+  # This is intended to be a temporary investigative measure, and should be removed from the
+  # code base shortly.
+  #
+  #-- Rohan, 17-9-2913
+  def create
+    if self.zipcode.nil?
+      Bugsnag.notify RuntimeError.new('Creating a Spree::Address with nil values')
+    end
+
+    super
+  end
+
+  def update(attribute_names = @attributes.keys)
+    if self.zipcode.nil?
+      Bugsnag.notify RuntimeError.new('Updating a Spree::Address with nil values')
+    end
+
+    super(attribute_names)
   end
 end
--- spec/models/addresses_spec.rb
+++ spec/models/addresses_spec.rb
@@ -28,4 +28,19 @@ describe Spree::Address do
       address.full_address.split(',').length.should eql(4)
     end
   end
+
+  describe "notifying bugsnag when saved with missing data" do
+    it "notifies on create" do
+      Bugsnag.should_receive(:notify)
+      a = Spree::Address.new zipcode: nil
+      a.save validate: false
+    end
+
+    it "notifies on update" do
+      Bugsnag.should_receive(:notify)
+      a = create(:address)
+      a.zipcode = nil
+      a.save validate: false
+    end
+  end
 end
