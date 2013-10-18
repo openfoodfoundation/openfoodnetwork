f7c431be3c1b3ce09cc45d2767f0eb090a8e743c
Rohan Mitchell <rohan@rohanmitchell.com>
2013-10-18 18:21:04 +1100

    Rename database names and user name

--- script/mirror_prod.sh
+++ script/mirror_prod.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-echo "drop database open_food_web_dev" | psql -h localhost -U ofw open_food_web_test
-echo "create database open_food_web_dev" | psql -h localhost -U ofw open_food_web_test
-ssh ofw-prod "pg_dump -h localhost -U openfoodweb openfoodweb_production |gzip" |gunzip |psql -h localhost -U ofw open_food_web_dev
+echo "drop database open_food_network_dev" | psql -h localhost -U ofn open_food_network_test
+echo "create database open_food_network_dev" | psql -h localhost -U ofn open_food_network_test
+ssh ofn-prod "pg_dump -h localhost -U openfoodweb openfoodweb_production |gzip" |gunzip |psql -h localhost -U ofn open_food_network_dev
