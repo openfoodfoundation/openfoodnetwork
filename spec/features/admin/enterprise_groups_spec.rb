be7e76920dc2e879079361e27a7fb606d2718dca
Rohan Mitchell <rohan@rohanmitchell.com>
2013-10-24 14:33:07 +1100

    Create new enterprise group

--- spec/features/admin/enterprise_groups_spec.rb
+++ spec/features/admin/enterprise_groups_spec.rb
@@ -23,4 +23,31 @@ feature %q{
     page.should have_selector 'td', text: e.name
   end
 
+  scenario "creating a new enterprise group" do
+    e1 = create(:enterprise)
+    e2 = create(:enterprise)
+    e3 = create(:enterprise)
+
+    click_link 'Configuration'
+    click_link 'Enterprise Groups'
+    click_link 'New Enterprise Group'
+
+    fill_in 'enterprise_group_name', with: 'EGEGEG'
+    check 'enterprise_group_on_front_page'
+    select e1.name, from: 'enterprise_group_enterprise_ids'
+    select e2.name, from: 'enterprise_group_enterprise_ids'
+    click_button 'Create'
+
+    page.should have_content 'Enterprise group "EGEGEG" has been successfully created!'
+
+    eg = EnterpriseGroup.last
+    eg.name.should == 'EGEGEG'
+    eg.on_front_page.should be_true
+    eg.enterprises.sort.should == [e1, e2].sort
+  end
+
+
+  context "as an enterprise user" do
+    xit "should show me only enterprises I manage when creating a new enterprise group"
+  end
 end
