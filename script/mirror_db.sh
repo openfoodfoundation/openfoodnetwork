e265b406d6c1e77cfb71be61248dadad11e71641
Rohan Mitchell <rohan@rohanmitchell.com>
2013-10-24 13:30:21 +1100

    Generalise script to work with staging servers, too

--- script/mirror_db.sh
+++ script/mirror_db.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# Usage: script/mirror_db.sh [ofn-staging1|ofn-staging2|ofn-prod]
+
 set -e
 
 echo "drop database open_food_network_dev" | psql -h localhost -U ofn open_food_network_test
 echo "create database open_food_network_dev" | psql -h localhost -U ofn open_food_network_test
-ssh ofn-prod "pg_dump -h localhost -U openfoodweb openfoodweb_production |gzip" |gunzip |psql -h localhost -U ofn open_food_network_dev
+ssh $1 "pg_dump -h localhost -U openfoodweb openfoodweb_production |gzip" |gunzip |psql -h localhost -U ofn open_food_network_dev
