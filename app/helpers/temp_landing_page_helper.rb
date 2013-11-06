406f8033feccf9cfba5db5a01f05ef20f14feaf2
Will Marshall <will@willmarshall.me>
2013-11-06 15:36:39 +1100

    Partialising the _distributor render

--- app/helpers/temp_landing_page_helper.rb
+++ app/helpers/temp_landing_page_helper.rb
@@ -1,11 +1,2 @@
 module TempLandingPageHelper
-  def temp_landing_page_distributor_link_class(distributor)
-    cart = current_order(true)
-    @active_distributors ||= Enterprise.distributors_with_active_order_cycles
-
-    klass = "shop-distributor"
-    klass += " empties-cart" unless cart.line_items.empty? || cart.distributor == distributor
-    klass += @active_distributors.include?(distributor) ? ' active' : ' inactive'
-    klass
-  end
 end
