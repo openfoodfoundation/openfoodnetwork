6c5539a1daa9b45d6bb7dfddaf3ff948f3c98bc9
Rohan Mitchell <rohan@rohanmitchell.com>
2014-01-08 17:25:32 +1100

    Update js compressor for staging as well as production

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -45,6 +45,8 @@ Openfoodnetwork::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
+  config.assets.js_compressor = Uglifier.new(mangle: false)
+
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
