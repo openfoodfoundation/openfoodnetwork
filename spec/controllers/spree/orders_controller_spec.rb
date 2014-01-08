4a74c2624c41d6c35648ff715a3ab62806b85a0d
Rohan Mitchell <rohan@rohanmitchell.com>
2014-01-08 14:15:06 +1100

    Remove specs for removed features

--- spec/controllers/spree/orders_controller_spec.rb
+++ spec/controllers/spree/orders_controller_spec.rb
@@ -31,71 +31,6 @@ describe Spree::OrdersController do
     order.distributor.should be_nil
   end
 
-  describe "adding a product to the cart with a distribution combination that can't service the existing cart" do
-    before do
-      @request.env["HTTP_REFERER"] = 'http://test.host/'
-    end
-
-    pending "errors when an invalid distributor is selected" do
-      # Given a product and some distributors
-      d1 = create(:distributor_enterprise)
-      d2 = create(:distributor_enterprise)
-      p = create(:product, :price => 12.34)
-      oc = create(:simple_order_cycle, :distributors => [d1], :variants => [p.master])
-
-      # When I attempt to add the product to the cart with an invalid distributor, it should not be added
-      expect do
-        spree_post :populate, variants: {p.master.id => 1}, distributor_id: d2.id, order_cycle_id: oc.id
-        response.should redirect_to :back
-      end.to change(self, :num_items_in_cart).by(0)
-
-      # And I should see an error
-      flash[:error].should == "That product is not available from the chosen distributor or order cycle."
-    end
-
-    pending "errors when an invalid order cycle is selected" do
-      # Given a product and some order cycles
-      d = create(:distributor_enterprise)
-      p = create(:product, :price => 12.34)
-      oc1 = create(:simple_order_cycle, :distributors => [d], :variants => [p.master])
-      oc2 = create(:simple_order_cycle, :distributors => [d], :variants => [])
-
-      # When I attempt to add the product to my cart with an invalid order cycle, it should not be added
-      expect do
-        spree_post :populate, variants: {p.master.id => 1}, distributor_id: d.id, order_cycle_id: oc2.id
-        response.should redirect_to :back
-      end.to change(self, :num_items_in_cart).by(0)
-
-      # And I should see an error
-      flash[:error].should == "That product is not available from the chosen distributor or order cycle."
-    end
-
-    pending "errors when distribution is valid for the new product but does not cover the cart" do
-      # Given two products with different distributors
-      d1 = create(:distributor_enterprise)
-      d2 = create(:distributor_enterprise)
-      p1 = create(:product, :price => 12.34)
-      p2 = create(:product, :price => 23.45)
-      oc1 = create(:simple_order_cycle, :distributors => [d1], :variants => [p1.master])
-      oc2 = create(:simple_order_cycle, :distributors => [d2], :variants => [p2.master])
-
-      # When I add the first to my cart
-      expect do
-        spree_post :populate, variants: {p1.master.id => 1}, distributor_id: d1.id, order_cycle_id: oc1.id
-        response.should redirect_to spree.cart_path
-      end.to change(self, :num_items_in_cart).by(1)
-
-      # And I attempt to add the second, then the product should not be added to my cart
-      expect do
-        spree_post :populate, variants: {p2.master.id => 1}, distributor_id: d2.id, order_cycle_id: oc2.id
-        response.should redirect_to :back
-      end.to change(self, :num_items_in_cart).by(0)
-
-      # And I should see an error
-      flash[:error].should == "That distributor or order cycle can't supply all the products in your cart. Please choose another."
-    end
-  end
-
   context "adding a group buy product to the cart" do
     it "sets a variant attribute for the max quantity" do
       distributor_product = create(:distributor_enterprise)
@@ -141,27 +76,6 @@ describe Spree::OrdersController do
     end
   end
 
-  context "#populate" do
-    let(:user) { create(:user) }
-    let(:order) { mock_model(Spree::Order, :number => "R123", :reload => nil, :save! => true, :coupon_code => nil, :user => user, :completed? => false, :currency => "USD", :token => 'a1b2c3d4')}
-    let(:populator) { double('OrderPopulator') }
-    before do
-        order.stub(:last_ip_address=)
-        Spree::Order.stub(:find).and_return(order)
-        Spree::OrderPopulator.should_receive(:new).and_return(populator)
-        Spree::Order.stub(:new).and_return(order)
-        if Spree::BaseController.spree_responders[:OrdersController].present?
-          Spree::BaseController.spree_responders[:OrdersController].clear
-        end
-    end
-
-    context "with Variant" do
-      it "should handle multiple variants, each with their own quantity" do
-        populator.should_receive(:populate).with("variants" => { 1 => "10", 3 => "7" }).and_return(true)
-        spree_post :populate, { order_id: order.id, :variants => {1 => 10, 3 => 7} }
-      end
-    end
-  end
 
   private
 
