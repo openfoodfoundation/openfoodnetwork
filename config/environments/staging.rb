e35eccca93fa58eb4520f1e35be212fb49a0e4e4
Rohan Mitchell <rohan@rohanmitchell.com>
2014-01-09 11:18:54 +1100

    Revert 3b3815a 6c5539a 39c2803 - remove non-mangling js compression which breaks build

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -45,9 +45,6 @@ Openfoodnetwork::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
-  require 'uglifier'
-  config.assets.js_compressor = Uglifier.new(mangle: false)
-
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
