031414a41fb8dc55bb84d30b2fc3746fc97b96f3
Rob H <oeoeaio@gmail.com>
2014-01-16 13:18:28 +0800

    Make BPE menu tab selected

--- app/overrides/add_bulk_edit_tab_to_products_admin_sub_menu.rb
+++ app/overrides/add_bulk_edit_tab_to_products_admin_sub_menu.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_product_sub_menu",
                      :name => "add_bulk_edit_tab_to_products_admin_sub_menu",
                      :insert_bottom => "[data-hook='admin_product_sub_tabs']",
-                     :text => "<%= tab :bulk_product_edit, :url => bulk_edit_admin_products_path %>")
+                     :text => "<%= tab :bulk_product_edit, :url => bulk_edit_admin_products_path, :match_path => '/products/bulk_edit' %>")
