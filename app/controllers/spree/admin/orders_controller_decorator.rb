00dfa62f43eed476e3b0ed3d710a5de252c75ec8
Rob H <oeoeaio@gmail.com>
2014-01-17 14:56:26 +0800

    Basic listing of orders on BOM page

--- app/controllers/spree/admin/orders_controller_decorator.rb
+++ app/controllers/spree/admin/orders_controller_decorator.rb
@@ -1,4 +1,6 @@
 Spree::Admin::OrdersController.class_eval do
+  before_filter :load_spree_api_key, :only => :bulk_management
+
   respond_override :index => { :html =>
     { :success => lambda { 
       # Filter orders to only show those distributed by current user (or all for admin user)
@@ -7,4 +9,12 @@ Spree::Admin::OrdersController.class_eval do
         page(params[:page]).
         per(params[:per_page] || Spree::Config[:orders_per_page])
     } } }
+
+
+  private
+
+  def load_spree_api_key
+    current_user.generate_spree_api_key! unless spree_current_user.spree_api_key
+    @spree_api_key = spree_current_user.spree_api_key
+  end
 end
--- spec/features/admin/bulk_order_management_spec.rb
+++ spec/features/admin/bulk_order_management_spec.rb
@@ -28,5 +28,15 @@ feature %q{
       click_link "Bulk Order Management"
       page.should have_selector "h1.page-title", text: "Bulk Order Management"
     end
+
+    it "displays a list of orders" do
+      o1 = FactoryGirl.create(:order, state: 'complete', completed_at: Time.now)
+      o2 = FactoryGirl.create(:order, state: 'complete', completed_at: Time.now)
+
+      visit '/admin/orders/bulk_management'
+
+      page.should have_selector "td", text: o1.id.to_s, :visible => true
+      page.should have_selector "td", text: o2.id.to_s, :visible => true
+    end
   end
 end
