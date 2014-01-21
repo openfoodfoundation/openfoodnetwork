6d2f77b30809da44af4545016f33915090f636ff
Rohan Mitchell <rohan@rohanmitchell.com>
2014-01-22 10:16:45 +1100

    Fix report spec

--- spec/features/admin/reports_spec.rb
+++ spec/features/admin/reports_spec.rb
@@ -144,8 +144,8 @@ feature %q{
 
       table.sort.should == [
         ["Supplier",              "Producer Suburb",                "Product",      "Product Properties",             "Variant Value",  "Price",  "Group Buy Unit Quantity",      "Amount"],
-        [product_1.supplier.name, product_1.supplier.address.city,  "Product Name", product_1.properties.join(", "),  "Size: Test",     "100.0",  product_1.group_buy_unit_size.to_s,  ""],
-        [product_1.supplier.name, product_1.supplier.address.city,  "Product Name", product_1.properties.join(", "),  "Size: S",        "80.0",   product_1.group_buy_unit_size.to_s,  ""],
+        [product_1.supplier.name, product_1.supplier.address.city,  "Product Name", product_1.properties.join(", "),  "Test",     "100.0",  product_1.group_buy_unit_size.to_s,  ""],
+        [product_1.supplier.name, product_1.supplier.address.city,  "Product Name", product_1.properties.join(", "),  "S",        "80.0",   product_1.group_buy_unit_size.to_s,  ""],
         [product_2.supplier.name, product_1.supplier.address.city,  "Product 2",    product_1.properties.join(", "),  "",                "99.0",  product_1.group_buy_unit_size.to_s,  ""]
       ].sort
     end
