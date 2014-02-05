6243642cc9f3c0e9c1a5291ac9be9507f0236e6c
Rob H <oeoeaio@gmail.com>
2014-02-05 23:44:41 +1100

    Require timeago in karma config so that js tests run

--- config/ng-test.conf.js
+++ config/ng-test.conf.js
@@ -8,6 +8,7 @@ module.exports = function(config) {
       'app/assets/javascripts/shared/jquery-1.8.0.js', // TODO: Can we link to Rails' jquery?
       'app/assets/javascripts/shared/angular.js',
       'app/assets/javascripts/shared/angular-*.js',
+      'app/assets/javascripts/shared/jquery.timeago.js',
 
       'app/assets/javascripts/admin/shared_services.js.coffee',
       'app/assets/javascripts/admin/order_cycle.js.erb.coffee',
