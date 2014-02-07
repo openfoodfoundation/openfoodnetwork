63f85fef0efcb1c27b6b69cb6321cc7e22031578
Will Marshall <will@willmarshall.me>
2014-02-07 14:34:49 +1100

    Starting to get in the login stuff

--- spec/features/consumer/shopping/checkout_spec.rb
+++ spec/features/consumer/shopping/checkout_spec.rb
@@ -4,14 +4,14 @@ include AuthenticationWorkflow
 include WebHelper
 
 feature "As a consumer I want to check out my cart", js: true do
+  let(:distributor) { create(:distributor_enterprise) }
+  let(:order_cycle) { create(:order_cycle, distributors: [distributor], coordinator: create(:distributor_enterprise)) }
+
+  before do
+    create_enterprise_group_for distributor
+  end
+
   describe "Attempting to access checkout without meeting the preconditions" do
-    let(:distributor) { create(:distributor_enterprise) }
-    let(:order_cycle) { create(:order_cycle, distributors: [distributor], coordinator: create(:distributor_enterprise)) }
-
-    before do
-      create_enterprise_group_for distributor
-    end
-
     it "redirects to the homepage if no distributor is selected" do
       visit "/shop/checkout"
       current_path.should == root_path
@@ -29,6 +29,43 @@ feature "As a consumer I want to check out my cart", js: true do
       select_order_cycle
       visit "/shop/checkout"
       current_path.should == "/shop/checkout"
+    end
+  end
+
+  describe "Login behaviour" do
+    before do
+      select_distributor
+      select_order_cycle
+    end
+
+    it "renders the login form if user is logged out" do
+      visit "/shop/checkout"
+      within "section[role='main']" do
+        page.should have_content "Login"
+      end
+    end
+
+    it "does not not render the login form if user is logged in" do
+      login_to_consumer_section
+      visit "/shop/checkout"
+      within "section[role='main']" do
+        page.should_not have_content "Login"
+      end
+    end
+
+    it "renders the signup link if user is logged out" do
+      visit "/shop/checkout"
+      within "section[role='main']" do
+        page.should have_content "Sign Up"
+      end
+    end
+
+    it "does not not render the signup form if user is logged in" do
+      login_to_consumer_section
+      visit "/shop/checkout"
+      within "section[role='main']" do
+        page.should_not have_content "Sign Up"
+      end
     end
   end
 end
