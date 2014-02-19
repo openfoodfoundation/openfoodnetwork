3024bbbeb59404195ec8c075721625f8f6c788a5
Rohan Mitchell <rohan@rohanmitchell.com>
2014-02-19 11:56:37 +1100

    Edit unit value and description of variant

--- spec/features/admin/variants_spec.rb
+++ spec/features/admin/variants_spec.rb
@@ -7,28 +7,40 @@ feature %q{
   include AuthenticationWorkflow
   include WebHelper
 
-  describe "units and values" do
-    it "does not show traditional option value fields for unit-related option types" do
-      # Given a product with unit-related option types, with a variant
-      p = create(:simple_product, variant_unit: "weight", variant_unit_scale: "1")
-      v = create(:variant, product: p, unit_value: 1, unit_description: 'foo')
+  scenario "editing unit value and description for a variant" do
+    # Given a product with unit-related option types, with a variant
+    p = create(:simple_product, variant_unit: "weight", variant_unit_scale: "1")
+    v = create(:variant, product: p, unit_value: 1, unit_description: 'foo')
 
-      # And the product has option types for the unit-related and non-unit-related option values
-      p.option_types << v.option_values.first.option_type
+    # And the product has option types for the unit-related and non-unit-related option values
+    p.option_types << v.option_values.first.option_type
 
-      # When I view the variant
-      login_to_admin_section
-      click_link 'Products'
-      within('#sub_nav') { click_link 'Products' }
-      click_link p.name
-      click_link 'Variants'
+    # When I view the variant
+    login_to_admin_section
+    click_link 'Products'
+    within('#sub_nav') { click_link 'Products' }
+    click_link p.name
+    click_link 'Variants'
+    page.find('table.index .icon-edit').click
 
-      page.find('table.index .icon-edit').click
+    # Then I should not see a traditional option value field for the unit-related option value
+    page.all("div[data-hook='presentation'] input").count.should == 1
 
-      # Then I should not see a traditional option value field for the unit-related option value
-      page.all("div[data-hook='presentation'] input").count.should == 1
-    end
+    # And I should see unit value and description fields for the unit-related option value
+    page.should have_field "variant_unit_value", with: "1"
+    page.should have_field "variant_unit_description", with: "foo"
 
-    it "shows unit value and description fields when the variant's product has associated option types set"
+    # When I update the fields and save the variant
+    fill_in "variant_unit_value", with: "123"
+    fill_in "variant_unit_description", with: "bar"
+    click_button 'Update'
+    page.should have_content %Q(Variant "#{p.name}" has been successfully updated!)
+
+    # Then the unit value and description should have been saved
+    v.reload
+    v.unit_value.should == 123
+    v.unit_description.should == 'bar'
   end
+
+  it "should not show unit value or description fields when the product does not have a unit-related option type"
 end
