e7d6e7e5649a70343b61783937ac2e317d9ffd99
Will Marshall <will@willmarshall.me>
2014-04-30 12:15:57 +1000

    Removing the sidebar before filter stuff, since we're not using it anymore

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  before_filter :load_data_for_menu
-  before_filter :load_data_for_sidebar
   before_filter :require_certified_hostname
 
   include EnterprisesHelper
@@ -16,27 +13,6 @@ class ApplicationController < ActionController::Base
   end
 
   private
-  def load_data_for_menu
-    @cms_site = Cms::Site.where(:identifier => 'open-food-network').first
-  end
-
-  # This is getting sloppy, since @all_distributors is also used for order cycle selection,
-  # which is not in the sidebar. I don't like having an application controller method that's
-  # coupled to several parts of the code. We might be able to solve this using cells:
-  # https://github.com/apotonick/cells
-  def load_data_for_sidebar
-    sidebar_distributors_limit = false
-    sidebar_suppliers_limit = false
-
-    @order_cycles = OrderCycle.active
-
-    @sidebar_suppliers = Enterprise.is_primary_producer.with_supplied_active_products_on_hand.limit(sidebar_suppliers_limit)
-    @total_suppliers = Enterprise.is_primary_producer.distinct_count
-
-    @sidebar_distributors = Enterprise.active_distributors.by_name.limit(sidebar_distributors_limit)
-    @all_distributors = Enterprise.active_distributors
-    @total_distributors = Enterprise.is_distributor.distinct_count
-  end
 
   def require_distributor_chosen
     unless current_distributor
