4480c75494e118e319f81ec6672cea0376e87347
Rohan Mitchell <rohan@rohanmitchell.com>
2014-05-01 17:13:07 +1000

    Fix "undefined method 'association_class' for nil:NilClass" error when creating Migs payment gateway

--- app/models/spree/payment_method_decorator.rb
+++ app/models/spree/payment_method_decorator.rb
@@ -1,4 +1,5 @@
 Spree::PaymentMethod.class_eval do
+  # See gateway_decorator.rb when modifying this association
   has_and_belongs_to_many :distributors, join_table: 'distributors_payment_methods', :class_name => 'Enterprise', association_foreign_key: 'distributor_id'
 
   attr_accessible :distributor_ids
