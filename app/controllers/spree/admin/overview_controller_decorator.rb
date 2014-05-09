c98d2e71d8d530a69e31bb0f0f407ce493ddd810
Rob H <oeoeaio@gmail.com>
2014-05-09 17:20:53 +1000

    Add scroll pane to admin dashboard

--- app/controllers/spree/admin/overview_controller_decorator.rb
+++ app/controllers/spree/admin/overview_controller_decorator.rb
@@ -2,7 +2,7 @@ module Spree
   module Admin
     class OverviewController < Spree::Admin::BaseController
       def index
-        @enterprises = Enterprise.managed_by(spree_current_user).order('is_distributor DESC, is_primary_producer ASC, name').limit(4)
+        @enterprises = Enterprise.managed_by(spree_current_user).order('is_distributor DESC, is_primary_producer ASC, name')
         @product_count = Spree::Product.active.managed_by(spree_current_user).count
         @order_cycle_count = OrderCycle.active.managed_by(spree_current_user).count
       end
