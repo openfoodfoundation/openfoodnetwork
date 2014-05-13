a3b5ba2257aa8ddd7b60de8fe7350c9b04ce7f89
Will Marshall <will@willmarshall.me>
2014-05-13 16:38:52 +1000

    Adding a spec for visiblity filtering

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,16 +2,21 @@ require 'spec_helper'
 
 describe HomeController do
   render_views
-  let(:distributor) { create(:distributor_enterprise) }
+  let!(:distributor) { create(:distributor_enterprise) }
+  let!(:invisible_distributor) { create(:distributor_enterprise, visible: false) }
 
   before do
-    controller.stub(:load_data_for_sidebar).and_return nil
     Enterprise.stub(:distributors_with_active_order_cycles).and_return [distributor]
-    Enterprise.stub(:is_distributor).and_return [distributor]
   end
+
   it "sets active distributors" do
     get :index
     assigns[:active_distributors].should == [distributor]
+  end
+
+  it "does not show invisible hubs" do
+    get :index
+    response.body.should_not have_content invisible_distributor.name
   end
   
   # This is done inside the json/hubs RABL template
