93dedb690da18d25042efa7c19322c39d4330340
Rob H <oeoeaio@gmail.com>
2014-05-16 10:03:35 +1000

    Change overview controller to class_eval

--- app/controllers/spree/admin/overview_controller_decorator.rb
+++ app/controllers/spree/admin/overview_controller_decorator.rb
@@ -1,11 +1,7 @@
-module Spree
-  module Admin
-    class OverviewController < Spree::Admin::BaseController
-      def index
-        @enterprises = Enterprise.managed_by(spree_current_user).order('is_distributor DESC, is_primary_producer ASC, name')
-        @product_count = Spree::Product.active.managed_by(spree_current_user).count
-        @order_cycle_count = OrderCycle.active.managed_by(spree_current_user).count
-      end
-    end
+Spree::Admin::OverviewController.class_eval do
+  def index
+    @enterprises = Enterprise.managed_by(spree_current_user).order('is_distributor DESC, is_primary_producer ASC, name')
+    @product_count = Spree::Product.active.managed_by(spree_current_user).count
+    @order_cycle_count = OrderCycle.active.managed_by(spree_current_user).count
   end
 end
