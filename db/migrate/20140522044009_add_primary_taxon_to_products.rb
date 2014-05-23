79393476b040707d52bcd182e26ae5fe9ddc6ce4
Rohan Mitchell <rohan@rohanmitchell.com>
2014-05-24 09:07:56 +1000

    Revert "Adding primary taxon to admin forms"
    This reverts commit 110a6f2d14a8ecb09628abf76f2836a275ff6777.

--- db/migrate/20140522044009_add_primary_taxon_to_products.rb
+++ db/migrate/20140522044009_add_primary_taxon_to_products.rb
@@ -1,7 +1,5 @@
 class AddPrimaryTaxonToProducts < ActiveRecord::Migration
   def change
     add_column :spree_products, :primary_taxon_id, :integer
-    add_index :spree_products, :primary_taxon_id
-    add_foreign_key :spree_products, :spree_taxons, column: :primary_taxon_id
   end
 end
