9b10a2cb9448adff9703dd751d1d62049fd95b01
Rob H <oeoeaio@gmail.com>
2014-05-29 12:13:55 +1000

    Added 'Add Another' link to new product page. Altered redirects

---
+++ spec/controllers/spree/admin/products_controller_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+describe Spree::Admin::ProductsController do
+  context "Creating a new product" do
+    let(:user) do
+      user = create(:user)
+      user.spree_roles << Spree::Role.find_or_create_by_name!('admin')
+      user
+    end
+
+    before do
+      controller.stub spree_current_user: user
+    end
+
+    it "redirects to bulk_edit when the user hits 'create'" do
+      s = create(:supplier_enterprise)
+      spree_post :create, {
+        product: {
+          name: "Product1",
+          supplier_id: s.id,
+          price: 5.0,
+          on_hand: 5,
+          variant_unit: 'weight',
+          variant_unit_scale: 1000,
+          unit_value: 10,
+          unit_description: ""
+        },
+        button: 'create'
+      }
+      response.should redirect_to "/admin/products/bulk_edit"
+    end
+
+    it "redirects to new when the user hits 'add_another'" do
+      s = create(:supplier_enterprise)
+      spree_post :create, {
+        product: {
+          name: "Product1",
+          supplier_id: s.id,
+          price: 5.0,
+          on_hand: 5,
+          variant_unit: 'weight',
+          variant_unit_scale: 1000,
+          unit_value: 10,
+          unit_description: ""
+        },
+        button: 'add_another'
+      }
+      response.should redirect_to "/admin/products/new"
+    end
+  end
+end
