d9f786557270728bb47403401211496fbbdb538c
Rob H <oeoeaio@gmail.com>
2014-05-30 11:31:53 +1000

    Can add an image from redesigned new product page

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -4,7 +4,6 @@ Spree::Product.class_eval do
   # https://github.com/rails/rails/issues/7618
   has_many :option_types, :through => :product_option_types, :dependent => :destroy
 
-
   belongs_to :supplier, :class_name => 'Enterprise'
 
   has_many :product_distributions, :dependent => :destroy
@@ -12,8 +11,9 @@ Spree::Product.class_eval do
 
   accepts_nested_attributes_for :product_distributions, :allow_destroy => true
   delegate_belongs_to :master, :unit_value, :unit_description
+  delegate :images_attributes=, to: :master
 
-  attr_accessible :supplier_id, :distributor_ids, :product_distributions_attributes, :group_buy, :group_buy_unit_size, :variant_unit, :variant_unit_scale, :variant_unit_name, :unit_value, :unit_description, :notes
+  attr_accessible :supplier_id, :distributor_ids, :product_distributions_attributes, :group_buy, :group_buy_unit_size, :variant_unit, :variant_unit_scale, :variant_unit_name, :unit_value, :unit_description, :notes, :images_attributes
 
   validates_presence_of :supplier
 
