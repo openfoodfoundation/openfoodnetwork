98611c3672cfaa27fb0500e18e28f852e246948e
Rohan Mitchell <rohan@rohanmitchell.com>
2014-06-12 15:21:35 +1000

    Fix timing error in enterprise supplied products rabl spec

--- spec/views/admin/enterprises/index.rabl_spec.rb
+++ spec/views/admin/enterprises/index.rabl_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "admin/enterprises/index.rabl" do
   let(:enterprise)       { create(:distributor_enterprise) }
   let!(:product)         { create(:simple_product, supplier: enterprise) }
-  let!(:deleted_product) { create(:simple_product, supplier: enterprise, deleted_at: Time.now) }
+  let!(:deleted_product) { create(:simple_product, supplier: enterprise, deleted_at: 1.day.ago) }
   let(:render)           { Rabl.render([enterprise], 'admin/enterprises/index', view_path: 'app/views', scope: RablHelper::FakeContext.instance) }
 
   describe "supplied products" do
