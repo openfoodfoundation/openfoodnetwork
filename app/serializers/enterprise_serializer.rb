a1789a80585c024862c132e98442e56d1313ae38
Will Marshall <will@willmarshall.me>
2014-06-26 12:17:38 +1000

    Removing old serializers

--- app/serializers/enterprise_serializer.rb
+++
@@ -1,7 +0,0 @@
-class EnterpriseSerializer < ActiveModel::Serializer
-  #attributes :name, :id, :description, :latitude, :longitude, 
-    #:long_description, :website, :instagram, :linkedin, :twitter, 
-    #:facebook, :is_primary_producer, :is_distributor, :phone
-
-  has_many :distributed_taxons, root: :taxons, serializer: Spree::DistributedTaxonSerializer
-end
--- app/serializers/spree/address_serializer.rb
+++
@@ -1,3 +0,0 @@
-class Spree::AddressSerializer < ActiveModel::Serializer
-  attributes :id
-end
