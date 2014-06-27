aa5767cd3022e56875e6ddd329b3ed27c8d644cc
Rohan Mitchell <rohan@rohanmitchell.com>
2014-06-27 11:52:36 +1000

    Remove paypal image button from checkout, fix redirect to paypal

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Openfoodnetwork::Application.routes.draw do
 
   get '/checkout', :to => 'checkout#edit' , :as => :checkout
   put '/checkout', :to => 'checkout#update' , :as => :update_checkout
-  get "/checkout/paypal_payment", to: 'checkout#paypal_payment', as: :paypal_payment
+  get '/checkout/paypal_payment/:order_id', to: 'checkout#paypal_payment', as: :paypal_payment
 
   resources :enterprises do
     collection do
