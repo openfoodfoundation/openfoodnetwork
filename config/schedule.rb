ff507b4b25fcca50961b5d467e86169f1f3fa3da
Rohan Mitchell <rohan@rohanmitchell.com>
2014-07-09 16:10:43 +1000

    Put db backups in their own bucket on S3, put db2fog cron commands in schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -10,3 +10,11 @@ job_type :run_file, "cd :path; :environment_variable=:environment bundle exec sc
 every 1.day, at: '12:05am' do
   run_file "lib/open_food_network/integrity_checker.rb"
 end
+
+every 1.day, at: '2:45am' do
+  rake 'db2fog:clean'
+end
+
+every 4.hours do
+  rake 'db2fog:backup'
+end
