d70ed029775896056b0797948d1f44fb0cc98916
Will Marshall <will@willmarshall.me>
2014-07-31 12:04:12 +1000

    Restoring caching on addresses

--- app/serializers/api/address_serializer.rb
+++ app/serializers/api/address_serializer.rb
@@ -1,6 +1,6 @@
 class Api::AddressSerializer < ActiveModel::Serializer
-  #cached
-  #delegate :cache_key, to: :object
+  cached
+  delegate :cache_key, to: :object
 
   attributes :id, :zipcode, :city, :state_name, :state_id,
     :phone, :firstname, :lastname, :address1, :address2, :city, :country_id,
