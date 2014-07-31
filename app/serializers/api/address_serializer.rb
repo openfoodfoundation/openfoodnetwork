7ca618fb2925ff33fdf1a13dc4fdb776a8023b19
Will Marshall <will@willmarshall.me>
2014-07-31 11:11:34 +1000

    Moving to state_name

--- app/serializers/api/address_serializer.rb
+++ app/serializers/api/address_serializer.rb
@@ -1,10 +1,12 @@
 class Api::AddressSerializer < ActiveModel::Serializer
-  cached
-  delegate :cache_key, to: :object
+  #cached
+  #delegate :cache_key, to: :object
 
-  attributes :id, :zipcode, :city, :state, :state_id 
+  attributes :id, :zipcode, :city, :state_name, :state_id,
+    :phone, :firstname, :lastname, :address1, :address2, :city, :country_id,
+    :zipcode
 
-  def state
+  def state_name
     object.state.andand.abbr
   end
 end
