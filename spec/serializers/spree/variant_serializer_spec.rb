d8c182332837c3b2a3adb186d1ec19a7a207d5f0
Rob H <oeoeaio@gmail.com>
2014-08-08 14:01:43 +1000

    Fix broken specs

--- spec/serializers/spree/variant_serializer_spec.rb
+++ spec/serializers/spree/variant_serializer_spec.rb
@@ -2,6 +2,6 @@ describe Spree::Api::VariantSerializer do
   let(:variant) { create(:variant) }
   it "serializes a variant" do
     serializer = Spree::Api::VariantSerializer.new variant
-    serializer.to_json.should match variant.name
+    serializer.to_json.should match variant.options_text
   end
 end
