98f7e58f9d3d182926c6be3ed3a9f7a98894d156
Rob H <oeoeaio@gmail.com>
2014-09-11 17:58:05 +1000

    Registration spec expects the correct url

--- spec/controllers/registration_controller_spec.rb
+++ spec/controllers/registration_controller_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe RegistrationController do
   it "redirects to authentication page when user not logged in" do
     get :index
-    response.should redirect_to registration_auth_path(anchor: "login?after_login=/register")
+    response.should redirect_to registration_auth_path(anchor: "signup?after_login=/register")
   end
 end
