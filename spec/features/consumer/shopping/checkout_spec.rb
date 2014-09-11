5007fd8ec364474e92503bacb7bfdc4a0677b520
Rob H <oeoeaio@gmail.com>
2014-09-11 22:27:05 +1000

    Shipping method 1 is used, so shipping method amount is zero

--- spec/features/consumer/shopping/checkout_spec.rb
+++ spec/features/consumer/shopping/checkout_spec.rb
@@ -188,7 +188,7 @@ feature "As a consumer I want to check out my cart", js: true do
 
                 # Order should have a payment with the correct amount
                 o = Spree::Order.complete.first
-                o.payments.first.amount.should == 15.79
+                o.payments.first.amount.should == 11.23
               end
 
               it "shows the payment processing failed message when submitted with an invalid credit card" do
