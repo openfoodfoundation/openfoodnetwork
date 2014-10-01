b647f9382fec8e28f9637c2685034d80a192a54e
Rafael Schouten <rafaelschouten@gmail.com>
2014-10-01 17:11:00 +1000

    revert included file

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -11,16 +11,11 @@ require 'spree/product_filters'
 
 Spree.config do |config|
   config.shipping_instructions = true
-  config.checkout_zone = ENV["CHECKOUT_ZONE"]
+  config.checkout_zone = 'Australia'
   config.address_requires_state = true
 
   # 12 should be Australia. Hardcoded for CI (Jenkins), where countries are not pre-loaded.
-  if Rails.env.test? or Rails.env.development?
-    config.default_country_id = 12
-  else
-    country = Spree::Country.find_by_name(ENV["DEFAULT_COUNTRY"])
-    config.default_country_id = country.id if country.present?
-  end
+  config.default_country_id = 12
 
   # -- spree_paypal_express
   # Auto-capture payments. Without this option, payments must be manually captured in the paypal interface.
@@ -28,6 +23,11 @@ Spree.config do |config|
   #config.override_actionmailer_config = false
 end
 
+# TODO Work out why this is necessary
+# Seems like classes within OFN module become 'uninitialized' when server reloads
+# unless the empty module is explicity 'registered' here. Something to do with autoloading?
+module OpenFoodNetwork
+end
 
 # Add calculators category for enterprise fees
 module Spree
@@ -43,7 +43,7 @@ module Spree
 end
 
 # Forcing spree to always allow SSL connections
-# Since we are using config.force_ssl = true 
+# Since we are using config.force_ssl = true
 # Without this we get a redirect loop: see https://groups.google.com/forum/#!topic/spree-user/NwpqGxJ4klk
 SslRequirement.module_eval do
   protected
