e62e87549aa03143abfde4b2c654dd3610419da2
Rafael Schouten <rafaelschouten@gmail.com>
2014-10-02 10:41:43 +1000

    commit this elsewhere

--- config/ng-test.conf.js
+++ config/ng-test.conf.js
@@ -27,16 +27,9 @@ module.exports = function(config) {
       'app/assets/javascripts/admin/util.js.erb'
     ],
 
-    preprocessors: {
-      '**/*.coffee': ['coffee']
-    },
-
     coffeePreprocessor: {
       options: {
         sourceMap: true
-      },
-      transformPath: function(path) {
-        return path.replace(/\.coffee$/, '.js');
       }
     },
 
