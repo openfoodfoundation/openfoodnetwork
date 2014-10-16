7db68795a4c64dffb989ba2b00e9c23c3d0c63ab
Rob Harrington <oeoeaio@gmail.com>
2014-10-16 16:06:54 +1100

    Redirect to admin root after confirmation

---
+++ spec/controllers/devise/confirmation_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Devise::ConfirmationsController do
+  context "after confirmation" do
+    before do
+      e = create(:enterprise)
+      @request.env["devise.mapping"] = Devise.mappings[:enterprise]
+      spree_get :show, confirmation_token: e.confirmation_token
+    end
+
+    it "should redirect to admin root" do
+      expect(response).to redirect_to spree.admin_path
+    end
+  end
+end
