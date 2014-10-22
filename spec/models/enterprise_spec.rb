069b9ce91d89cf489488ca0950b8b69b7df4d656
Rafael Schouten <rafaelschouten@gmail.com>
2014-10-22 17:49:31 +1100

    fix email length validation spec

--- spec/models/enterprise_spec.rb
+++ spec/models/enterprise_spec.rb
@@ -105,7 +105,7 @@ describe Enterprise do
     subject { FactoryGirl.create(:distributor_enterprise, :address => FactoryGirl.create(:address)) }
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
-    it { should validate_length_of(:description, :maximum => 255) }
+    it { should ensure_length_of(:description).is_at_most(255) }
 
     it "requires an owner" do
       expect{
