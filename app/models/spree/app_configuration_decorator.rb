d49598aa976af071c743b30e36ca34537e3412b5
Rob Harrington <oeoeaio@gmail.com>
2014-10-29 15:05:20 +1100

    Adding global config for requiring tax_category

---
+++ app/models/spree/app_configuration_decorator.rb
@@ -0,0 +1,9 @@
+Spree::AppConfiguration.class_eval do
+  # This file decorates the existing preferences file defined by Spree.
+  # It allows us to add our own global configuration variables, which
+  # we can allow to be modified in the UI by adding appropriate form
+  # elements to existing or new configuration pages.
+
+  # Tax Preferences
+  preference :products_require_tax_category, :boolean, default: false
+end
