9b7fd1c16bc45b7efc2b067c4d504eb7120b50f3
Paul Mackay <pauljmackay@gmail.com>
2014-11-09 11:19:26 +0000

    #275: small tweaks.

--- app/mailers/producer_mailer.rb
+++ app/mailers/producer_mailer.rb
@@ -1,12 +1,17 @@
-require 'devise/mailers/helpers'
-class ProducerMailer < ActionMailer::Base Spree::BaseMailer
-  include Devise::Mailers::Helpers
+
+class ProducerMailer < Spree::BaseMailer
 
   def order_cycle_report(producer, order_cycle)
     @producer = producer
     @coordinator = order_cycle.coordinator
     @order_cycle = order_cycle
-    subject = "[Open Food Network] Order cycle report for "
+
+    # TODO: consider what happens if there is more than one distributor
+    first_producer = @order_cycle.distributors[0]
+    @distribution_date = @order_cycle.pickup_time_for first_producer
+    # puts @distribution_date
+
+    subject = "[Open Food Network] Order cycle report for #{@distribution_date}"
     mail(to: @producer.email, from: from_address, subject: subject)
   end
 
