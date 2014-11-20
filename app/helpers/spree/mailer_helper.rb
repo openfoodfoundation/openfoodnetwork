f5a5b93c2238036ef5b287250c802a0efc0860ea
Rob Harrington <oeoeaio@gmail.com>
2014-11-20 15:45:12 +1100

    Can use relative paths for assets in emails because roadie

--- app/helpers/spree/mailer_helper.rb
+++
@@ -1,7 +0,0 @@
-module Spree
-  module MailerHelper
-    def email_asset_url(asset)
-      URI.join(root_url, asset_path(asset)).to_s
-    end
-  end
-end
--- app/mailers/spree/base_mailer_decorator.rb
+++ app/mailers/spree/base_mailer_decorator.rb
@@ -5,4 +5,10 @@ Spree::BaseMailer.class_eval do
   # Define layout
   layout 'mailer'
   helper Spree::MailerHelper
+
+  protected
+  def roadie_options
+    binding.pry
+    super.merge(url_options: {host: URI(spree.root_url).host })
+  end
 end
