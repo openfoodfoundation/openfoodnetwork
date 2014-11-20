b64b48f26ec149125741fc19e5e30c79d8ab79b2
Rob Harrington <oeoeaio@gmail.com>
2014-11-20 20:16:34 +1100

    Removing obsolete MailerHelper reference

--- app/mailers/spree/base_mailer_decorator.rb
+++ app/mailers/spree/base_mailer_decorator.rb
@@ -4,10 +4,10 @@ Spree::BaseMailer.class_eval do
 
   # Define layout
   layout 'mailer'
-  helper Spree::MailerHelper
 
   protected
   def roadie_options
+    # This lets us specify assets using relative paths in email templates
     super.merge(url_options: {host: URI(spree.root_url).host })
   end
 end
