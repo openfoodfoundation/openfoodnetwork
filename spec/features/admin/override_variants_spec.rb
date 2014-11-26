47b479c6c9c36f8d4c8517f3243ef60e32e31cc7
Rohan Mitchell <rohan@rohanmitchell.com>
2014-11-27 10:48:20 +1100

    Show variants in override variants interface, not just products

--- spec/features/admin/override_variants_spec.rb
+++ spec/features/admin/override_variants_spec.rb
@@ -12,18 +12,19 @@ feature %q{
   use_short_wait
 
   let!(:hub) { create(:distributor_enterprise) }
+  let!(:hub2) { create(:distributor_enterprise) }
   let!(:producer) { create(:supplier_enterprise) }
   let!(:er) { create(:enterprise_relationship, parent: producer, child: hub,
                      permissions_list: [:add_to_order_cycle]) }
 
   context "as an enterprise user" do
-    let(:user) { create_enterprise_user enterprises: [hub, producer] }
+    let(:user) { create_enterprise_user enterprises: [hub, hub2, producer] }
     before { quick_login_as user }
 
     describe "selecting a hub" do
       it "displays a list of hub choices" do
         visit '/admin/products/override_variants'
-        page.should have_select2 'hub_id', options: ['', hub.name]
+        page.should have_select2 'hub_id', options: ['', hub.name, hub2.name]
       end
 
       it "displays the hub" do
@@ -36,23 +37,31 @@ feature %q{
     end
 
     context "when a hub is selected" do
-      let!(:product) { create(:simple_product, supplier: producer, price: 1.23, on_hand: 12) }
+      let!(:product) { create(:simple_product, supplier: producer, variant_unit: 'weight', variant_unit_scale: 1) }
+      let!(:variant) { create(:variant, product: product, unit_value: 1, price: 1.23, on_hand: 12) }
       let!(:producer2) { create(:supplier_enterprise) }
-      let!(:product2) { create(:simple_product, supplier: producer2, price: 1.23, on_hand: 12) }
+      let!(:product2) { create(:simple_product, supplier: producer2) }
+      let!(:er) { create(:enterprise_relationship, parent: producer2, child: hub2,
+                         permissions_list: [:add_to_order_cycle]) }
 
       before do
+        # Remove 'S' option value
+        variant.option_values.first.destroy
+
         visit '/admin/products/override_variants'
         select2_select hub.name, from: 'hub_id'
         click_button 'Go'
       end
 
-      it "displays the list of products" do
+      it "displays the list of products with variants" do
         page.should have_table_row ['PRODUCER', 'PRODUCT', 'PRICE', 'ON HAND']
-        page.should have_table_row [producer.name, product.name, '1.23', '12']
+        page.should have_table_row [producer.name, product.name, '', '']
+        page.should have_table_row ['', '1g', '1.23', '12']
       end
 
       it "filters the products to those the hub can add to an order cycle" do
-        page.should_not have_table_row [producer2.name, product2.name, '1.23', '12']
+        page.should_not have_content producer2.name
+        page.should_not have_content product2.name
       end
 
       it "products values are affected by overrides"
