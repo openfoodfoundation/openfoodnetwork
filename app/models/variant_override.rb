45e709b2ccf9928b951f7925bafe20cecaf7c86e
Rohan Mitchell <rohan@rohanmitchell.com>
2014-12-11 10:39:26 +1100

    VariantOverrides require hub and variant

--- app/models/variant_override.rb
+++ app/models/variant_override.rb
@@ -1,6 +1,8 @@
 class VariantOverride < ActiveRecord::Base
-  belongs_to :variant, class_name: 'Spree::Variant'
   belongs_to :hub, class_name: 'Enterprise'
+  belongs_to :variant, class_name: 'Spree::Variant'
+
+  validates_presence_of :hub_id, :variant_id
 
   scope :for_hubs, lambda { |hubs|
     where(hub_id: hubs)
