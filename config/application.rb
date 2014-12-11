e607c9855f861e4fd359c9efb17254d473695496
Rohan Mitchell <rohan@rohanmitchell.com>
2014-12-12 10:59:23 +1100

    Add Pin Payments gateway

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,7 @@ module Openfoodnetwork
     # Register Spree payment methods
     initializer "spree.gateway.payment_methods", :after => "spree.register.payment_methods" do |app|
       app.config.spree.payment_methods << Spree::Gateway::Migs
+      app.config.spree.payment_methods << Spree::Gateway::Pin
     end
 
     # Settings in config/environments/* take precedence over those specified here.
