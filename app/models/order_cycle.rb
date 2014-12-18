4b353fa27b023e824873bbb2d6bd760e939d24ec
Rob Harrington <oeoeaio@gmail.com>
2014-12-18 11:01:08 +1100

    Ammending comments to make slightly more clear

--- app/models/order_cycle.rb
+++ app/models/order_cycle.rb
@@ -113,8 +113,8 @@ class OrderCycle < ActiveRecord::Base
   end
 
   # If a product without variants is added to an order cycle, and then some variants are added
-  # to that product, then the master variant is still part of the order cycle, but customers
-  # should not be able to purchase it.
+  # to that product, but not the order cycle, then the master variant should not available for customers
+  # to purchase.
   # This method filters out such products so that the customer cannot purchase them.
   def valid_products_distributed_by(distributor)
     variants = variants_distributed_by(distributor)
@@ -175,8 +175,8 @@ class OrderCycle < ActiveRecord::Base
   private
 
   # If a product without variants is added to an order cycle, and then some variants are added
-  # to that product, then the master variant is still part of the order cycle, but customers
-  # should not be able to purchase it.
+  # to that product, but not the order cycle, then the master variant should not available for customers
+  # to purchase.
   # This method is used by #valid_products_distributed_by to filter out such products so that
   # the customer cannot purchase them.
   def product_has_only_obsolete_master_in_distribution?(product, distributed_variants)
