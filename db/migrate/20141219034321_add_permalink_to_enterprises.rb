9163b0c1ad3cb815cb07cd37091f4d3f71baa8d0
Rob Harrington <oeoeaio@gmail.com>
2015-01-28 11:37:02 +1100

    Adding missing equals sign to test for equality in permalink migration

--- db/migrate/20141219034321_add_permalink_to_enterprises.rb
+++ db/migrate/20141219034321_add_permalink_to_enterprises.rb
@@ -5,7 +5,7 @@ class AddPermalinkToEnterprises < ActiveRecord::Migration
     Enterprise.all.each do |enterprise|
       counter = 1
       permalink = enterprise.name.parameterize
-      permalink = "my-enterprise-name" if permalink = ""
+      permalink = "my-enterprise-name" if permalink == ""
       while Enterprise.find_by_permalink(permalink) do
         permalink = enterprise.name.parameterize + counter.to_s
         counter += 1
