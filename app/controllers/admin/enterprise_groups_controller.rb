98063dae60e56aeb68b8fededaf4680c91a7da30
Maikel Linke <mkllnk@web.de>
2015-02-19 16:57:19 +1100

    setting default country by id in groups controller

--- app/controllers/admin/enterprise_groups_controller.rb
+++ app/controllers/admin/enterprise_groups_controller.rb
@@ -27,7 +27,7 @@ module Admin
     def build_resource_with_address
       enterprise_group = build_resource_without_address
       enterprise_group.address = Spree::Address.new
-      enterprise_group.address.country = Spree::Country.find_by_name(ENV['DEFAULT_COUNTRY'])
+      enterprise_group.address.country = Spree::Country.find_by_id(Spree::Config[:default_country_id])
       enterprise_group
     end
     alias_method_chain :build_resource, :address
