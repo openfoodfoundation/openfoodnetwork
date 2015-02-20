4554c0555d33e8cc99fc036b9a692e33d74120ee
Rohan Mitchell <rohan@rohanmitchell.com>
2015-02-20 11:31:39 +1100

    Change locking to use postgres syntax, fixes 'must be first statement in transaction' error

--- lib/open_food_network/locking.rb
+++ lib/open_food_network/locking.rb
@@ -2,7 +2,7 @@ module OpenFoodNetwork::Locking
   # http://rhnh.net/2010/06/30/acts-as-list-will-break-in-production
   def with_isolation_level_serializable
     self.transaction do
-      self.connection.execute("SET SESSION TRANSACTION ISOLATION LEVEL SERIALIZABLE")
+      self.connection.execute "SET SESSION CHARACTERISTICS AS TRANSACTION ISOLATION LEVEL SERIALIZABLE"
       yield
     end
   end
