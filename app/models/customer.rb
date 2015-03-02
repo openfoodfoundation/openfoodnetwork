39c5f8febec705d0b162f9f02c0ac45498e6c147
Lynne Davis <lynne@deanforestfoodhub.org.uk>
2015-03-02 12:32:28 +1100

    Updating Customer model with validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,8 @@
 class Customer < ActiveRecord::Base
+
+  belongs_to :enterprise
+
+  validates :code, presence: true, uniqueness: {scope: :enterprise_id}
+  validates :email, presence: true
+  validates :enterprise_id, presence: true
 end
