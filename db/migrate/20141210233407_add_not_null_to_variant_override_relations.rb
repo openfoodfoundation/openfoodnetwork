c6ddf368568af8588e58bf66d441ba437d3a3be2
Rohan Mitchell <rohan@rohanmitchell.com>
2015-03-25 11:35:59 +1100

    Make migration reversible

--- db/migrate/20141210233407_add_not_null_to_variant_override_relations.rb
+++ db/migrate/20141210233407_add_not_null_to_variant_override_relations.rb
@@ -1,6 +1,11 @@
 class AddNotNullToVariantOverrideRelations < ActiveRecord::Migration
-  def change
+  def up
     change_column :variant_overrides, :hub_id, :integer, null: false
     change_column :variant_overrides, :variant_id, :integer, null: false
+  end
+
+  def down
+    change_column :variant_overrides, :hub_id, :integer, null: true
+    change_column :variant_overrides, :variant_id, :integer, null: true
   end
 end
