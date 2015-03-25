63e345f819e992e5d9eed451297822472715d67d
Rob Harrington <oeoeaio@gmail.com>
2015-03-25 11:58:07 +1100

    Exchange serializer cuts down list of variants visible to the current user, based on permissions

--- app/serializers/api/admin/exchange_serializer.rb
+++ app/serializers/api/admin/exchange_serializer.rb
@@ -4,6 +4,14 @@ class Api::Admin::ExchangeSerializer < ActiveModel::Serializer
   has_many :enterprise_fees, serializer: Api::Admin::EnterpriseFeeSerializer
 
   def variants
-    Hash[ object.variants.map { |v| [v.id, true] } ]
+    permitted = Spree::Variant.where("1=0")
+    if object.incoming
+      permitted = OpenFoodNetwork::Permissions.new(options[:current_user]).
+      visible_variants_for_incoming_exchanges_between(object.sender, object.receiver)
+    else
+      permitted = OpenFoodNetwork::Permissions.new(options[:current_user]).
+      visible_variants_for_outgoing_exchanges_between(object.sender, object.receiver, order_cycle: object.order_cycle)
+    end
+    Hash[ object.variants.merge(permitted).map { |v| [v.id, true] } ]
   end
 end
