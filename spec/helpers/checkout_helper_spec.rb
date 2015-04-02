e75c6a8e1d82340756899b159a2db757301782f7
Rohan Mitchell <rohan@rohanmitchell.com>
2015-04-02 11:20:47 +1100

    Add helper to display total tax on an order

--- spec/helpers/checkout_helper_spec.rb
+++ spec/helpers/checkout_helper_spec.rb
@@ -12,4 +12,12 @@ describe CheckoutHelper do
 
     helper.validated_input("test", "foo", type: :email)
   end
+
+  describe "displaying the tax total for an order" do
+    let(:order) { double(:order, total_tax: 123.45, currency: 'AUD') }
+
+    it "retrieves the total tax on the order" do
+      helper.display_checkout_tax_total(order).should == Spree::Money.new(123.45, currency: 'AUD')
+    end
+  end
 end
