048c6a8ee88388d2fb4a5bc29d479523b3eef280
Rohan Mitchell <rohan@rohanmitchell.com>
2015-04-09 10:03:04 +1000

    Include only the bare minimum of helpers into JS template context to avoid intermittent SASS @include issues

--- config/initializers/js_template_helpers.rb
+++ config/initializers/js_template_helpers.rb
@@ -2,7 +2,12 @@
 # https://github.com/pitr/angular-rails-templates/issues/45#issuecomment-43229086
 
 Rails.application.assets.context_class.class_eval do
-  include ApplicationHelper
-  include ActionView::Helpers
-  include Rails.application.routes.url_helpers
+  # include ApplicationHelper
+  # include ActionView::Helpers
+  # include Rails.application.routes.url_helpers
+
+  # Including all of the helpers (above) has caused some intermittent CSS include issues
+  # (not finding mixins from an @include in sass). Therefore, we're only including the
+  # bare minimum here.
+  include ActionView::Helpers::TranslationHelper
 end
