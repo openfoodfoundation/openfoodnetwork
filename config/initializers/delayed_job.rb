998288e21f92e714a947852fe4dae8b76c953293
Rohan Mitchell <rohan@rohanmitchell.com>
2015-04-21 15:37:36 +1000

    Keep failed jobs around for debugging. Limit max runtime to 15 mins (we're only sending emails at present). Notify bugsnag of errors in jobs.

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,1 +1,14 @@
 Delayed::Worker.logger = Logger.new(Rails.root.join('log', 'delayed_job.log'))
+Delayed::Worker.destroy_failed_jobs = false
+Delayed::Worker.max_run_time = 15.minutes
+
+# Notify bugsnag when a job fails
+# Code adapted from http://trevorturk.com/2011/01/25/notify-hoptoad-if-theres-an-exception-in-delayedjob/
+class Delayed::Worker
+  alias_method :original_handle_failed_job, :handle_failed_job
+
+  def handle_failed_job(job, error)
+    Bugsnag.notify(error)
+    original_handle_failed_job(job, error)
+  end
+end
