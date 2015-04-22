6c81109b17945167e171640e2daf88b99b0f0c50
Rohan Mitchell <rohan@rohanmitchell.com>
2015-04-22 14:17:07 +1000

    Local branch isn't checked out, so we need to test remote branch

--- script/includes.sh
+++ script/includes.sh
@@ -1,5 +1,5 @@
 function exit_unless_master_merged {
-    if [[ `git branch -a --merged $BUILDKITE_BRANCH` != *origin/master* ]]; then
+    if [[ `git branch -a --merged origin/$BUILDKITE_BRANCH` != *origin/master* ]]; then
 	echo "This branch does not have the current master merged. Please merge master and push again."
 	exit 1
     fi
