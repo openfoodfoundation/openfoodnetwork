162a5651401d2a8288123e4a807e3d576218017a
Lynne Davis <lynne@deanforestfoodhub.org.uk>
2015-04-24 16:14:24 +0100

    Removing notes to myself from this file

--- app/controllers/spree/api/line_items_controller_decorator.rb
+++ app/controllers/spree/api/line_items_controller_decorator.rb
@@ -6,12 +6,3 @@ Spree::Api::LineItemsController.class_eval do
     order.update_distribution_charge!
   end
 end
-
-
-#when we update a line item the .update_distribution_charge! is called
-# order.should_receive .update_distribution_charge!
-# check fails when absent
-
-# in order model check that .update_distribution_charge! is properly tested.
-# think through use cases - existing completed order
-# currently likely just used to complete orders so add test case that works on a completed order
