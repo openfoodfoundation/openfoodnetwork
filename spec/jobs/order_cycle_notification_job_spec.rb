73376b30e2e57318f08c4d0f3d40ae12efda023b
Rohan Mitchell <rohan@rohanmitchell.com>
2015-04-29 15:56:27 +1000

    Convention

--- spec/jobs/order_cycle_notification_job_spec.rb
+++ spec/jobs/order_cycle_notification_job_spec.rb
@@ -4,7 +4,7 @@ describe OrderCycleNotificationJob do
   let(:order_cycle) { create(:order_cycle) }
 
   it "sends a mail to each supplier" do
-    mail = double()
+    mail = double(:mail)
     allow(mail).to receive(:deliver)
     expect(ProducerMailer).to receive(:order_cycle_report).twice.and_return(mail)
     run_job OrderCycleNotificationJob.new(order_cycle.id)
