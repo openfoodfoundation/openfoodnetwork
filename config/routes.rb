a6cecdcc25af036f5d0645341cddfac67e376054
Rohan Mitchell <rohan@rohanmitchell.com>
2015-05-01 11:59:52 +1000

    Display header for Xero invoices report

--- config/routes.rb
+++ config/routes.rb
@@ -134,6 +134,7 @@ Spree::Core::Engine.routes.prepend do
   match '/admin/orders/bulk_management' => 'admin/orders#bulk_management', :as => "admin_bulk_order_management"
   match '/admin/reports/products_and_inventory' => 'admin/reports#products_and_inventory', :as => "products_and_inventory_admin_reports",  :via  => [:get, :post]
   match '/admin/reports/customers' => 'admin/reports#customers', :as => "customers_admin_reports",  :via  => [:get, :post]
+  match '/admin/reports/xero_invoices' => 'admin/reports#xero_invoices', :as => "xero_invoices_admin_reports",  :via  => [:get, :post]
   match '/admin', :to => 'admin/overview#index', :as => :admin
   match '/admin/payment_methods/show_provider_preferences' => 'admin/payment_methods#show_provider_preferences', :via => :get
 
