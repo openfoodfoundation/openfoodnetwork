50d2ddc05f68c3965793a21c64a30a4ce664c4fd
Rohan Mitchell <rohan@rohanmitchell.com>
2015-05-06 20:00:50 +1000

    Add progress comments

--- script/ci/merge_master_into_branch.sh
+++ script/ci/merge_master_into_branch.sh
@@ -3,8 +3,10 @@
 set -e
 source ./script/ci/includes.sh
 
+echo "--- Checking if master has already been merged"
 succeed_if_master_merged
 
+echo "--- Merging master into this branch"
 git checkout $BUILDKITE_BRANCH
 git merge origin/$BUILDKITE_BRANCH
 git merge origin/master -m "Auto-merge from CI [skip ci]"
