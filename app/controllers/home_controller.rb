1ffba674b331b2480f7ebedd3a6c1d1b507f9463
Rohan Mitchell <rohan@rohanmitchell.com>
2015-05-06 11:31:32 +1000

    Show real stats on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,14 @@
 class HomeController < BaseController
   layout 'darkswarm'
   before_filter :load_active_distributors
-  
+
   def index
+    @num_hubs = Enterprise.is_hub.count
+    @num_producers = Enterprise.is_primary_producer.count
+    @num_users = Spree::User.joins(:orders).count('DISTINCT spree_users.*')
+    @num_orders = Spree::Order.complete.count
   end
 
   def about_us
   end
 end
-
