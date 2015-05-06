f88fdac710e637d18e4c3251c05a3df9767e65ba
Rob Harrington <oeoeaio@gmail.com>
2015-05-06 13:53:59 +1000

    Adding module - doh!

--- app/controllers/api/enterprises_controller.rb
+++ app/controllers/api/enterprises_controller.rb
@@ -13,7 +13,7 @@ module Api
     end
 
     def accessible
-      permitted = Permissions.new(current_api_user).order_cycle_enterprises
+      permitted = OpenFoodNetwork::Permissions.new(current_api_user).order_cycle_enterprises
       @enterprises = permitted.ransack(params[:q]).result
       render params[:template] || :bulk_index
     end
