0737ac8da02f253a1c7a5e766240d90090c9c87b
Rohan Mitchell <rohan@rohanmitchell.com>
2015-05-14 12:11:38 +1000

    Write an rspec table matcher that gives informative error messages

--- spec/support/matchers/table_matchers.rb
+++ spec/support/matchers/table_matchers.rb
@@ -26,3 +26,44 @@ RSpec::Matchers.define :have_table_row do |row|
     node.all('tr').map { |tr| tr.all('th, td').map(&:text) }
   end
 end
+
+
+
+# find("#my-table").should match_table [[...]]
+RSpec::Matchers.define :match_table do |expected_table|
+
+  match_for_should do |node|
+    rows = node.
+      all("tr").
+      map { |r| r.all("th,td").map { |c| c.text.strip } }
+
+    if rows.count != expected_table.count
+      @failure_message = "found table with #{rows.count} rows, expected #{expected_table.count}"
+
+    else
+      rows.each_with_index do |row, i|
+        expected_row = expected_table[i]
+        if row.count != expected_row.count
+          @failure_message = "row #{i} has #{row.count} columns, expected #{expected_row.count}"
+          break
+
+        elsif row != expected_row
+          row.each_with_index do |cell, j|
+            if cell != expected_row[j]
+              @failure_message = "cell [#{i}, #{j}] has content '#{cell}', expected '#{expected_row[j]}'"
+              break
+            end
+          end
+          break if @failure_message
+        end
+      end
+    end
+
+    @failure_message.nil?
+  end
+
+  failure_message_for_should do |text|
+    @failure_message
+  end
+
+end
