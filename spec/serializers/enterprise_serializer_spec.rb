75f1f673ad3d63a6c366947876ce3479b62f050a
Rohan Mitchell <rohan@rohanmitchell.com>
2015-05-27 16:26:31 +1000

    Update spec for EnterpriseSerializer

--- spec/serializers/enterprise_serializer_spec.rb
+++ spec/serializers/enterprise_serializer_spec.rb
@@ -3,19 +3,18 @@
 describe Api::EnterpriseSerializer do
   let(:enterprise) { create(:distributor_enterprise) }
   let(:taxon) { create(:taxon) }
+  let(:data_class) { Struct.new(:earliest_closing_times, :active_distributors,
+                                :distributed_taxons, :supplied_taxons,
+                                :shipping_method_services, :relatives) }
+  let(:data) { data_class.new({}, [], {}, {}, {}, {producers: [], distributors: []}) }
+
   it "serializes an enterprise" do
-    serializer = Api::EnterpriseSerializer.new enterprise
+    serializer = Api::EnterpriseSerializer.new enterprise, data: data
     serializer.to_json.should match enterprise.name
   end
 
-  it "includes distributed taxons" do
-    enterprise.stub(:distributed_taxons).and_return [taxon]
-    serializer = Api::EnterpriseSerializer.new enterprise
-    serializer.to_json.should match taxon.id.to_s
-  end
-
   it "will render urls" do
-    serializer = Api::EnterpriseSerializer.new enterprise
+    serializer = Api::EnterpriseSerializer.new enterprise, data: data
     serializer.to_json.should match "map_005-hub.svg"
   end
 end
