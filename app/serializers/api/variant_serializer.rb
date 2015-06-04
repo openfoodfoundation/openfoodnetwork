ef3155a16a847bfd9f0f35ebda1c2dd2810582c9
Rohan Mitchell <rohan@rohanmitchell.com>
2015-06-04 16:27:32 +1000

    Pull product listing fee calculations out of serializer loop

--- app/serializers/api/variant_serializer.rb
+++ app/serializers/api/variant_serializer.rb
@@ -1,21 +1,20 @@
 class Api::VariantSerializer < ActiveModel::Serializer
-  attributes :id, :is_master, :count_on_hand, :name_to_display, :unit_to_display, 
+  attributes :id, :is_master, :count_on_hand, :name_to_display, :unit_to_display,
     :options_text, :on_demand, :price, :fees, :price_with_fees, :product_name
-
-  def price_with_fees
-    object.price_with_fees(options[:current_distributor], options[:current_order_cycle])
-  end
 
   def price
     object.price
   end
 
   def fees
-    object.fees_by_type_for(options[:current_distributor], options[:current_order_cycle])
+    options[:enterprise_fee_calculator].indexed_fees_by_type_for(object)
+  end
+
+  def price_with_fees
+    object.price + options[:enterprise_fee_calculator].indexed_fees_for(object)
   end
 
   def product_name
     object.product.name
   end
-
 end
