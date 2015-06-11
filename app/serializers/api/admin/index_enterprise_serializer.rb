95073f6fe7c863f10df236759e1fbdb984154042
Rob Harrington <oeoeaio@gmail.com>
2015-06-11 10:42:19 +0800

    Adding actual link to the manage link on enterprises index

--- app/serializers/api/admin/index_enterprise_serializer.rb
+++ app/serializers/api/admin/index_enterprise_serializer.rb
@@ -1,8 +1,12 @@
 class Api::Admin::IndexEnterpriseSerializer < ActiveModel::Serializer
-  attributes :name, :id, :permalink, :is_primary_producer, :sells, :producer_profile_only, :owned
+  attributes :name, :id, :permalink, :is_primary_producer, :sells, :producer_profile_only, :owned, :edit_path
 
   def owned
     return true if options[:spree_current_user].admin?
     object.owner == options[:spree_current_user]
+  end
+
+  def edit_path
+    edit_admin_enterprise_path(object)
   end
 end
