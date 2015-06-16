bf953f898712fb7230b632b1601fdbc88ec9a620
Rohan Mitchell <rohan@rohanmitchell.com>
2015-06-16 18:25:44 +1000

    Undo last change, it broke asset precompilation. Rename style.css to scss for inclusion and force recompilation of all assets.

--- config/application.rb
+++ config/application.rb
@@ -79,7 +79,7 @@ module Openfoodnetwork
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.1'
+    config.assets.version = '1.2'
 
     config.sass.load_paths += [
       "#{Gem.loaded_specs['foundation-rails'].full_gem_path}/vendor/assets/stylesheets/foundation/components",
