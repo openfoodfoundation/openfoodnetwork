083d7327bc85b8263cc9a11acec10ffc0aa0bb7f
Rob Harrington <oeoeaio@gmail.com>
2015-06-25 15:25:15 +0800

    Adding BillItem to database

---
+++ app/models/bill_item.rb
@@ -0,0 +1,15 @@
+class BillItem < ActiveRecord::Base
+  belongs_to :enterprise
+  belongs_to :owner, class_name: 'Spree::User', foreign_key: :owner_id
+
+  def bill
+    # Will make this more sophisicated in the future in that it will use global config variables to calculate
+    return 0 if trial?
+    if ['own', 'any'].include? sells
+      bill = (turnover * 0.02)
+      bill > 50 ? 50 : bill
+    else
+      0
+    end
+  end
+end
