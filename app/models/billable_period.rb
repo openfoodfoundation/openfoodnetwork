f0fa696d2f5a82b634fe608e848c3d478d823af3
Rob Harrington <oeoeaio@gmail.com>
2015-07-01 12:55:48 +0800

    Default scope for BPs ignores deleted records

--- app/models/billable_period.rb
+++ app/models/billable_period.rb
@@ -2,6 +2,8 @@ class BillablePeriod < ActiveRecord::Base
   belongs_to :enterprise
   belongs_to :owner, class_name: 'Spree::User', foreign_key: :owner_id
 
+  default_scope where(deleted_at: nil)
+
   def bill
     # Will make this more sophisicated in the future in that it will use global config variables to calculate
     return 0 if trial?
@@ -11,5 +13,9 @@ class BillablePeriod < ActiveRecord::Base
     else
       0
     end
+  end
+
+  def delete
+    self.update_column(:deleted_at, Time.now)
   end
 end
