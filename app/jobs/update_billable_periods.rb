2048800c4333b40f770850076d45e125bbae4415
Rob Harrington <oeoeaio@gmail.com>
2015-07-03 12:35:58 +0800

    Billable periods updater does not auto-run user invoices updater

--- app/jobs/update_billable_periods.rb
+++ app/jobs/update_billable_periods.rb
@@ -91,8 +91,4 @@ UpdateBillablePeriods = Struct.new("UpdateBillablePeriods") do
 
     obsolete_billable_periods.each(&:delete)
   end
-
-  def success(job)
-    Delayed::Job.enqueue UpdateUserInvoices.new
-  end
 end
