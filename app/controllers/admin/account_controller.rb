128b6853f4799addddeb30fd93dc3b0112ce92aa
Rob Harrington <oeoeaio@gmail.com>
2015-07-07 15:57:31 +0800

    Styling tweaks to the admin account page

--- app/controllers/admin/account_controller.rb
+++ app/controllers/admin/account_controller.rb
@@ -1,7 +1,8 @@
 class Admin::AccountController < Spree::Admin::BaseController
 
   def show
-    @enterprises = spree_current_user.owned_enterprises
+    @invoices = spree_current_user.orders.where(distributor_id: Spree::Config[:accounts_distributor_id])
+    @enterprises = Enterprise.where(id: BillablePeriod.where(owner_id: spree_current_user).map(&:enterprise_id))
     # .group_by('enterprise.id').joins(:billable_periods)
     # .select('SUM(billable_periods.turnover) AS turnover').order('turnover DESC')
   end
