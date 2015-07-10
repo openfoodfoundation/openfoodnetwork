d353906bb849c5d89f971c85687b2bef967df2c4
Rohan Mitchell <rohan@rohanmitchell.com>
2015-07-10 16:59:32 +1000

    Content manage producer signup page

--- app/controllers/admin/contents_controller.rb
+++ app/controllers/admin/contents_controller.rb
@@ -1,8 +1,9 @@
 module Admin
   class ContentsController < Spree::Admin::BaseController
     def edit
-      @preferences_home = [:home_tagline_cta, :home_whats_happening]
-      @preferences_footer = [:footer_facebook_url, :footer_twitter_url, :footer_instagram_url, :footer_linkedin_url, :footer_googleplus_url, :footer_pinterest_url, :footer_email, :footer_links_md, :footer_about_url]
+      @preference_sections = [{name: 'Home page', preferences: [:home_tagline_cta, :home_whats_happening]},
+                              {name: 'Producer signup page', preferences: [:producer_signup_pricing_table_html, :producer_signup_case_studies_html, :producer_signup_detail_html]},
+                              {name: 'Footer', preferences: [:footer_facebook_url, :footer_twitter_url, :footer_instagram_url, :footer_linkedin_url, :footer_googleplus_url, :footer_pinterest_url, :footer_email, :footer_links_md, :footer_about_url]}]
     end
 
     def update
--- app/models/content_configuration.rb
+++ app/models/content_configuration.rb
@@ -1,7 +1,14 @@
 class ContentConfiguration < Spree::Preferences::Configuration
+  # Home page
   preference :home_tagline_cta, :string, default: "Browse Open Food Network Australia"
   preference :home_whats_happening, :string, default: "Thanks for making the Open Food Network possible. Our vision is a better food system, and we're proud of what we're achieving together."
 
+  # Producer sign-up page
+  preference :producer_signup_pricing_table_html, :text, default: "(TODO: Pricing table)"
+  preference :producer_signup_case_studies_html, :text, default: "(TODO: Case studies)"
+  preference :producer_signup_detail_html, :text, default: "(TODO: Detail)"
+
+  # Footer
   preference :footer_facebook_url, :string, default: "https://www.facebook.com/OpenFoodNet"
   preference :footer_twitter_url, :string, default: "https://twitter.com/OpenFoodNet"
   preference :footer_instagram_url, :string, default: ""
