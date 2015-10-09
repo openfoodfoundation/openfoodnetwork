50b3635e7ebcdc80f1a032db4144882937891b6e
Rob Harrington <oeoeaio@gmail.com>
2015-10-09 12:50:34 +1100

    Locking line_items on API update

--- app/controllers/spree/api/line_items_controller_decorator.rb
+++ app/controllers/spree/api/line_items_controller_decorator.rb
@@ -1,8 +1,16 @@
 Spree::Api::LineItemsController.class_eval do
   after_filter :apply_enterprise_fees, only: :update
-
+  around_filter :lock, only: :update
 
   private
+
+  def lock
+    authorize! :read, order
+    @line_item = order.line_items.find(params[:id])
+    @line_item.with_lock do
+      yield
+    end
+  end
 
   def apply_enterprise_fees
     authorize! :read, order
