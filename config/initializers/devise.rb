65eacb7db388dc9a5ed373a98594aa1160834e8c
ludivinecp <coipel.ludivine@gmail.com>
2015-12-18 14:29:54 +0100

    delete APP CODE facebook for generics informations

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -2,6 +2,6 @@ Devise.setup do |config|
   # Add a default scope to devise, to prevent it from checking
   # whether other devise enabled models are signed into a session or not
   config.default_scope = :spree_user
-  config.omniauth :facebook, "929520317127867", "e5ce96dd9a3fca61e3d542cf42c5731f"
+  config.omniauth :facebook, "APP ID", "APP SECRET"
     # callback_url: "CALLBACK_URL"
 end
