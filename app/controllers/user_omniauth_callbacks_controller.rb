63aecfc8641b1d3f373ca09111e82f8795b4c4d3
ludivinecp <coipel.ludivine@gmail.com>
2015-12-18 13:19:59 +0100

    sign in an existing user with facebook

--- app/controllers/user_omniauth_callbacks_controller.rb
+++ app/controllers/user_omniauth_callbacks_controller.rb
@@ -1,14 +1,13 @@
 class UserOmniauthCallbacksController < Devise::OmniauthCallbacksController
     def facebook
     # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = Spree::User.from_omniauth(request.env["omniauth.auth"])
-
-    if @user.persisted?
+    @user = Spree::User.find_from_facebook(request.env["omniauth.auth"])
+    if @user
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to request.referer + "signup"
+      redirect_to request.referer + "#/signup"
     end
   end
 
--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -20,13 +20,8 @@ Spree.user_class.class_eval do
   validate :limit_owned_enterprises
   devise :omniauthable, :omniauth_providers => [:facebook]
   
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-    #   user.name = auth.info.name   # assuming the user model has a name
-    #   user.image = auth.info.image # assuming the user model has an image
-    end
+  def self.find_from_facebook(auth)
+    where(email: auth.info.email).first
   end 
 
   def known_users
