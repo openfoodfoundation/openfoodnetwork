4103ed0ba747c3232206e7bed5ec5716e8d815f8
Rob Harrington <oeoeaio@gmail.com>
2015-12-22 12:28:10 +1100

    Combining variant override controller specs

--- spec/controllers/admin/variant_overrides_controller_spec.rb
+++ spec/controllers/admin/variant_overrides_controller_spec.rb
@@ -16,7 +16,7 @@ describe Admin::VariantOverridesController, type: :controller do
         before do
           user = create(:user)
           user.owned_enterprises << create(:enterprise)
-          controller.stub spree_current_user: user
+          allow(controller).to receive(:spree_current_user) { user }
         end
 
         it "redirects to unauthorized" do
@@ -27,7 +27,7 @@ describe Admin::VariantOverridesController, type: :controller do
 
       context "where I manage the variant override hub" do
         before do
-          controller.stub spree_current_user: hub.owner
+          allow(controller).to receive(:spree_current_user) { hub.owner }
         end
 
         context "but the producer has not granted VO permission" do
@@ -58,6 +58,55 @@ describe Admin::VariantOverridesController, type: :controller do
               spree_put :bulk_update, format: format, variant_overrides: variant_override_params
               expect(VariantOverride.find_by_id(variant_override.id)).to be_nil
             end
+          end
+        end
+      end
+    end
+  end
+
+  describe "bulk_reset" do
+    context "json" do
+      let(:format) { :json }
+
+      let(:hub) { create(:distributor_enterprise) }
+      let(:producer) { create(:supplier_enterprise) }
+      let(:product) { create(:product, supplier: producer) }
+      let(:variant1) { create(:variant, product: product) }
+      let(:variant2) { create(:variant, product: product) }
+      let!(:variant_override1) { create(:variant_override, hub: hub, variant: variant1, count_on_hand: 5, default_stock: 7, resettable: true) }
+      let!(:variant_override2) { create(:variant_override, hub: hub, variant: variant2, count_on_hand: 2, default_stock: 1, resettable: false) }
+
+      before do
+        allow(controller).to receive(:spree_current_user) { hub.owner }
+      end
+
+      context "where the producer has not granted create_variant_overrides permission to the hub" do
+        let(:params) { { format: format, variant_overrides: [ { id: variant_override1.id } ] } }
+
+        it "restricts access" do
+          spree_put :bulk_reset, params
+          expect(response).to redirect_to spree.unauthorized_path
+        end
+      end
+
+      context "where the producer has granted create_variant_overrides permission to the hub" do
+        let!(:er1) { create(:enterprise_relationship, parent: producer, child: hub, permissions_list: [:create_variant_overrides]) }
+
+        context "where reset is enabled" do
+          let(:params) { { format: format, variant_overrides: [ { id: variant_override1.id } ] } }
+
+          it "updates stock to default values" do
+            spree_put :bulk_reset, params
+            expect(variant_override1.reload.count_on_hand).to eq 7
+          end
+        end
+
+        context "where reset is disabled" do
+          let(:params) { { format: format, variant_overrides: [ { id: variant_override2.id } ] } }
+
+          it "doesn't update on_hand" do
+            spree_put :bulk_reset, params
+            expect(variant_override2.reload.count_on_hand).to eq 2
           end
         end
       end
