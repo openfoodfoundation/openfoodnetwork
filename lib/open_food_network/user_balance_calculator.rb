439349246a273965f9a1dbeb27ef38e71817b385
Lynne Davis <lynne@deanforestfoodhub.org.uk>
2016-02-16 17:47:51 +0000

    Bug fixes to Order Cycle Reports

--- lib/open_food_network/user_balance_calculator.rb
+++ lib/open_food_network/user_balance_calculator.rb
@@ -1,19 +1,19 @@
 module OpenFoodNetwork
   class UserBalanceCalculator
-    def initialize(user, distributor)
-      @user = user
+    def initialize(email, distributor)
+      @email = email
       @distributor = distributor
     end
 
     def balance
-      payment_total - order_total
+      payment_total - completed_order_total
     end
 
 
     private
 
-    def order_total
-      orders.sum &:total
+    def completed_order_total
+      orders.complete.sum &:total
     end
 
     def payment_total
@@ -22,11 +22,11 @@ module OpenFoodNetwork
 
 
     def orders
-      Spree::Order.where(distributor_id: @distributor, user_id: @user)
+      Spree::Order.where(distributor_id: @distributor, email: @email)
     end
 
     def payments
-      Spree::Payment.where(order_id: orders)
+      Spree::Payment.where(order_id: orders, state: "completed")
     end
   end
 end
