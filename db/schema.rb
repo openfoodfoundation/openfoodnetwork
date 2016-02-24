51d77d5781ee8458bb69f5cadef92b0b376ee4e6
Rob Harrington <oeoeaio@gmail.com>
2016-02-25 09:58:15 +1100

    Adding migration to explicitly grant VO permission where it is currently implicitly granted via managers/owners
    In preparation for removing implicitly granted permissions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20160218235221) do
+ActiveRecord::Schema.define(:version => 20160224034034) do
 
   create_table "account_invoices", :force => true do |t|
     t.integer  "user_id",    :null => false
