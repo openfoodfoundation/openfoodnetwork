openfoodfoundation/openfoodnetwork

commit a9c37c162e1956028704fbdf74ce1c56c5b3ce7d

Update email footer to use instance email and social media contacts

app/views/shared/mailers/_social_and_contact.html.haml
@@ -7,20 +7,32 @@
           %td
             %h5
               = t :email_social
-            %p
-              %a.soc-btn.fb{:href => "https://www.facebook.com/OpenFoodNet", :target => "_blank"}
-                Facebook
-              %a.soc-btn.tw{:href => "https://twitter.com/OpenFoodNet", :target => "_blank"}
-                Twitter
-              %a.soc-btn.li{:href => "http://www.linkedin.com/groups/Open-Food-Foundation-4743336", :target => "_blank"}
-                LinkedIn
+            %p.social-icons
+              - if ContentConfig.footer_facebook_url.present?
+                %a{href: ContentConfig.footer_facebook_url}
+                  %i.ofn-i_044-facebook
+              - if ContentConfig.footer_twitter_url.present?
+                %a{href: ContentConfig.footer_twitter_url}
+                  %i.ofn-i_041-twitter
+              - if ContentConfig.footer_instagram_url.present?
+                %a{href: ContentConfig.footer_instagram_url}
+                  %i.ofn-i_043-instagram
+              - if ContentConfig.footer_linkedin_url.present?
+                %a{href: ContentConfig.footer_linkedin_url}
+                  %i.ofn-i_042-linkedin
+              - if ContentConfig.footer_googleplus_url.present?
+                %a{href: ContentConfig.footer_googleplus_url}
+                  %i.ofn-i_046-g
+              - if ContentConfig.footer_pinterest_url.present?
+                %a{href: ContentConfig.footer_pinterest_url}
+                  %i.ofn-i_045-pintrest
       %table.column{:align => "left"}
         %tr
           %td
-            %h5
-              = t :email_contact
-            %p
+            - if ContentConfig.footer_email.present?
+              %h5
+                = t :email_contact
               %strong
-                %a{:href => "mailto:hello@openfoodnetwork.org"}
-                  hello@openfoodnetwork.org
+                %a{href: ContentConfig.footer_email.reverse, mailto: true, target: '_blank'}
+                  #{ContentConfig.footer_email}
       %span.clear

config/locales/en-GB.yml
@@ -244,7 +244,7 @@ See the %{link} to find out more about %{sitename}'s features and to start using
   email_payment_method: "Paying via:"
   email_shipping_delivery_details: Delivery details
   email_shipping_delivery_time: "Delivery on:"
-  email_shipping_delivery_address: "Delivery address:"
+  email_shipping_delivery_address: "Delivery address (if applicable):"
   email_shipping_collection_details: Collection details
   email_shipping_collection_time: "Ready for collection:"
   email_shipping_collection_instructions: "Collection instructions:"
@@ -344,19 +344,19 @@ See the %{link} to find out more about %{sitename}'s features and to start using
   login_invalid: "Invalid email or password"
 
   modal_hubs: "Food Hubs"
-  modal_hubs_abstract: Our food hubs are the point of contact between you and the people who make your food! 
-  modal_hubs_content1: You can search for a convenient hub by location or name. Some hubs have multiple points where you can pick-up your purchases, and some will also provide delivery options. Each food hub is a sales point with independent business operations and logistics - so variations between hubs are to be expected. 
+  modal_hubs_abstract: Our food hubs are the point of contact between you and the people who make your food!
+  modal_hubs_content1: You can search for a convenient hub by location or name. Some hubs have multiple points where you can pick-up your purchases, and some will also provide delivery options. Each food hub is a sales point with independent business operations and logistics - so variations between hubs are to be expected.
   modal_hubs_content2: You can only shop at one food hub at a time.
 
   modal_groups: "Groups / Regions"
-  modal_groups_content1: These are the organisations and relationships between hubs which make up the Open Food Network. 
-  modal_groups_content2: Some groups are clustered by location or council, others by non-geographic similarities. 
+  modal_groups_content1: These are the organisations and relationships between hubs which make up the Open Food Network.
+  modal_groups_content2: Some groups are clustered by location or council, others by non-geographic similarities.
 
   modal_how: "How it works"
   modal_how_shop: Shop the Open Food Network
   modal_how_shop_explained: Search for a food hub near you to start shopping! You can expand each food hub to see what kinds of goodies are available, and click through to start shopping. (You can only shop one food hub at a time.)
   modal_how_pickup: Pick-ups, delivery & shipping costs
-  modal_how_pickup_explained: Some food hubs deliver to your door, while others require you to pick-up your purchases. You can see which options are available on the homepage, and select which you'd like at the shopping and check-out pages. Delivery will cost more, and pricing differs from hub-to-hub. Each food hub is a sales point with independent business operations and logisitics - so variations between hubs are to be expected. 
+  modal_how_pickup_explained: Some food hubs deliver to your door, while others require you to pick-up your purchases. You can see which options are available on the homepage, and select which you'd like at the shopping and check-out pages. Delivery will cost more, and pricing differs from hub-to-hub. Each food hub is a sales point with independent business operations and logisitics - so variations between hubs are to be expected.
   modal_how_more: Learn more
   modal_how_more_explained: "If you want to learn more about the Open Food Network, how it works, and get involved, check out:"
 

commit 1770cbb6bf6b0d326fa96bc055cbcf61749a2d19

Display footer_email in confirmation emails correctly
A bug introduced in a9c37c162e1956028704fbdf74ce1c56c5b3ce7d caused the
creation of confirmation emails for shops to fail. The email template
got fixed now and the email address from the database is displayed if
present.

app/views/shared/mailers/_social_and_contact.html.haml
@@ -24,6 +24,5 @@
               %h5
                 = t :email_contact
               %strong
-                %a{href: ContentConfig.footer_email.reverse, mailto: true, target: '_blank'}
-                  #{ContentConfig.footer_email}
+                = mail_to ContentConfig.footer_email
       %span.clear

spec/mailers/order_mailer_spec.rb
@@ -40,5 +40,12 @@ describe Spree::OrderMailer do
       ActionMailer::Base.deliveries.count.should == 1
       ActionMailer::Base.deliveries.first.to.should == [@distributor.email]
     end
+
+    it "sends an email even if a footer_email is given" do
+      # Testing bug introduced by a9c37c162e1956028704fbdf74ce1c56c5b3ce7d
+      ContentConfig.footer_email = "email@example.com"
+      Spree::OrderMailer.confirm_email_for_shop(@order1.id).deliver
+      ActionMailer::Base.deliveries.count.should == 1
+    end
   end
 end