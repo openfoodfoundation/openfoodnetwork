30ed6df38d68931ea8b52fc72afb84cdc4fc551b
Lynne Davis <lynne@deanforestfoodhub.org.uk>
2016-03-17 12:54:03 +0000

    Adding super admin configuration for the minimum billable turnover functionality

--- lib/open_food_network/business_model_configuration_validator.rb
+++ lib/open_food_network/business_model_configuration_validator.rb
@@ -5,13 +5,14 @@ module OpenFoodNetwork
   class BusinessModelConfigurationValidator
     include ActiveModel::Validations
 
-    attr_accessor :shop_trial_length_days, :account_invoices_monthly_fixed, :account_invoices_monthly_rate, :account_invoices_monthly_cap, :account_invoices_tax_rate
+    attr_accessor :shop_trial_length_days, :account_invoices_monthly_fixed, :account_invoices_monthly_rate, :account_invoices_monthly_cap, :account_invoices_tax_rate, :minimum_billable_turnover
 
     validates :shop_trial_length_days, presence: true, numericality: { greater_than_or_equal_to: 0 }
     validates :account_invoices_monthly_fixed, presence: true, numericality: { greater_than_or_equal_to: 0 }
     validates :account_invoices_monthly_rate, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1 }
     validates :account_invoices_monthly_cap, presence: true, numericality: { greater_than_or_equal_to: 0 }
     validates :account_invoices_tax_rate, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1 }
+    validates :minimum_billable_turnover, presence: true, numericality: { greater_than_or_equal_to: -1 }
 
     def initialize(attr, button=nil)
       attr.each { |k,v| instance_variable_set("@#{k}", v) }
