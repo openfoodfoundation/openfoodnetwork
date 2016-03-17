bfaefa4dc941f2465dfbb4a092ef2b132ded53bf
Lynne Davis <lynne@deanforestfoodhub.org.uk>
2016-03-17 11:15:06 +0000

    Updating Bill Calculator to include a minimum billable turnover. Shopfronts are not charged if their tunrover is under the minimum billable.

--- lib/open_food_network/bill_calculator.rb
+++ lib/open_food_network/bill_calculator.rb
@@ -1,6 +1,6 @@
 module OpenFoodNetwork
   class BillCalculator
-    attr_accessor :turnover, :fixed, :rate, :cap, :tax_rate
+    attr_accessor :turnover, :fixed, :rate, :cap, :tax_rate, :min_bill_to
 
     def initialize(opts={})
       @turnover = opts[:turnover] || 0
@@ -8,11 +8,13 @@ module OpenFoodNetwork
       @rate = opts[:rate] || Spree::Config[:account_invoices_monthly_rate]
       @cap = opts[:cap] || Spree::Config[:account_invoices_monthly_cap]
       @tax_rate = opts[:tax_rate] || Spree::Config[:account_invoices_tax_rate]
+      @min_bill_to = opts[:min_bill_to] || Spree::Config[:minimum_billable_turnover]
     end
 
     def bill
       bill = fixed + (turnover * rate)
       bill = cap > 0 ? [bill, cap].min : bill
+      bill = turnover > min_bill_to ? bill : 0
       bill * (1 + tax_rate)
     end
   end
