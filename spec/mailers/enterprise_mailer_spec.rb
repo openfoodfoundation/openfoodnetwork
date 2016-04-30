f3fb158e014cbc6d4ebaf96707e95617a7f7dc6e
Maikel Linke <mkllnk@web.de>
2016-04-30 13:15:46 +1000

    Translate subject of enterprise confirmation email

--- spec/mailers/enterprise_mailer_spec.rb
+++ spec/mailers/enterprise_mailer_spec.rb
@@ -12,7 +12,7 @@ describe EnterpriseMailer do
       EnterpriseMailer.confirmation_instructions(enterprise, 'token').deliver
       ActionMailer::Base.deliveries.count.should == 1
       mail = ActionMailer::Base.deliveries.first
-      expect(mail.subject).to eq "Please confirm your email for #{enterprise.name}"
+      expect(mail.subject).to eq "Please confirm the email address for #{enterprise.name}"
       expect(mail.to).to include enterprise.email
       expect(mail.reply_to).to be_nil
     end
@@ -28,7 +28,7 @@ describe EnterpriseMailer do
       EnterpriseMailer.confirmation_instructions(enterprise, 'token').deliver
       ActionMailer::Base.deliveries.count.should == 1
       mail = ActionMailer::Base.deliveries.first
-      expect(mail.subject).to eq "Please confirm your email for #{enterprise.name}"
+      expect(mail.subject).to eq "Please confirm the email address for #{enterprise.name}"
       expect(mail.to).to include enterprise.unconfirmed_email
     end
   end
