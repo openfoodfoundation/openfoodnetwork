fd1b76a024c32c33a4101368043b2c99c7bbfea5
Nicolas Blanc <blancnic@hotmail.com>
2016-05-05 17:23:28 +0000

    #906 translate titles in enterprise mailer

--- app/mailers/enterprise_mailer.rb
+++ app/mailers/enterprise_mailer.rb
@@ -5,10 +5,11 @@ class EnterpriseMailer < Spree::BaseMailer
   def welcome(enterprise)
     @enterprise = enterprise
     subject = t('enterprise_mailer.welcome.subject',
-                enterprise: @enterprise.name)
-    mail(:to => enterprise.email, 
+                enterprise: @enterprise.name,
+                sitename: Spree::Config[:site_name])
+    mail(:to => enterprise.email,
          :from => from_address,
-         :subject => subject + Spree::Config[:site_name])
+         :subject => subject)
   end
 
   def confirmation_instructions(record, token)
