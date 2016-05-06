c9bfeff0a7b7a72199e0afe145b3265cb7074cbe
Rohan Mitchell <rohan@rohanmitchell.com>
2016-05-06 16:42:28 +1000

    Integrate from Spree fork: Calculators work against LineItems

---
+++ spec/models/spree/calculator/flexi_rate_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe Spree::Calculator::FlexiRate do
+  let(:calculator) { Spree::Calculator::FlexiRate.new }
+  let(:line_item) { mock_model(Spree::LineItem, amount: 10, quantity: 4) }
+
+  describe "computing for a single line item" do
+    it "returns the first item rate" do
+      calculator.stub preferred_first_item: 1.0
+      calculator.compute(line_item).round(2).should == 1.0
+    end
+  end
+
+  it "allows creation of new object with all the attributes" do
+    Spree::Calculator::FlexiRate.new(preferred_first_item: 1, preferred_additional_item: 1, preferred_max_items: 1)
+  end
+end
