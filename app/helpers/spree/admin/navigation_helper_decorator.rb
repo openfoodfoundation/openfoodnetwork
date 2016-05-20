a3b91dabe5fbd2e810ce7f6330c88a057d6eb753
Rohan Mitchell <rohan@rohanmitchell.com>
2016-05-20 16:27:23 +1000

    TEMP: Remove override for no-longer-present method

--- app/helpers/spree/admin/navigation_helper_decorator.rb
+++ app/helpers/spree/admin/navigation_helper_decorator.rb
@@ -4,14 +4,14 @@ module Spree
       # Make it so that the Reports admin tab can be enabled/disabled through the cancan
       # :report resource, since it does not have a corresponding resource class (unlike
       # eg. Spree::Product).
-      def klass_for_with_sym_fallback(name)
-        klass = klass_for_without_sym_fallback(name)
-        klass ||= name.singularize.to_sym
-        klass = :overview if klass == :dashboard
-        klass = Spree::Order if klass == :bulk_order_management
-        klass
-      end
-      alias_method_chain :klass_for, :sym_fallback
+      # def klass_for_with_sym_fallback(name)
+      #   klass = klass_for_without_sym_fallback(name)
+      #   klass ||= name.singularize.to_sym
+      #   klass = :overview if klass == :dashboard
+      #   klass = Spree::Order if klass == :bulk_order_management
+      #   klass
+      # end
+      # alias_method_chain :klass_for, :sym_fallback
     end
   end
 end
