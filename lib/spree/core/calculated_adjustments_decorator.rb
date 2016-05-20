a0b740f52df995f9da8d60df99fcab363e0486e8
Rohan Mitchell <rohan@rohanmitchell.com>
2016-05-20 11:01:54 +1000

    Generalise fix for missing EnterpriseFee::Calculator to any use of calculated_adjustments

---
+++ lib/spree/core/calculated_adjustments_decorator.rb
@@ -0,0 +1,14 @@
+module Spree
+  module Core
+    module CalculatedAdjustments
+      module ClassMethods
+        def calculated_adjustments_with_explicit_class_name
+          calculated_adjustments_without_explicit_class_name
+          # Class name is mis-inferred outside of Spree namespace
+          has_one :calculator, as: :calculable, dependent: :destroy, class_name: 'Spree::Calculator'
+        end
+        alias_method_chain :calculated_adjustments, :explicit_class_name
+      end
+    end
+  end
+end
