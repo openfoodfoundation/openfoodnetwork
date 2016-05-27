6dd05ca4dafa766d655bb049cd7b1f0ad5d24eb0
Rob Harrington <oeoeaio@gmail.com>
2016-05-27 15:04:43 +1000

    Intermittent fail hunting

--- spec/models/spree/taxon_spec.rb
+++ spec/models/spree/taxon_spec.rb
@@ -3,21 +3,20 @@ require 'spec_helper'
 module Spree
   describe Taxon do
     let(:e) { create(:supplier_enterprise) }
-    let(:t0) { p1.taxons.order('id ASC').first }
-    let(:t1) { create(:taxon) }
-    let(:t2) { create(:taxon) }
+    let!(:t1) { create(:taxon) }
+    let!(:t2) { create(:taxon) }
 
     describe "callbacks" do
-      let(:product) { create(:simple_product, taxons: [t1]) }
+      let!(:p2) { create(:simple_product, taxons: [t1]) }
 
       it "refreshes the products cache on save" do
-        expect(OpenFoodNetwork::ProductsCache).to receive(:product_changed).with(product)
+        expect(OpenFoodNetwork::ProductsCache).to receive(:product_changed).with(p2)
         t1.name = 'asdf'
         t1.save
       end
 
       it "refreshes the products cache on destroy" do
-        expect(OpenFoodNetwork::ProductsCache).to receive(:product_changed).with(product)
+        expect(OpenFoodNetwork::ProductsCache).to receive(:product_changed).with(p2)
         t1.destroy
       end
     end
@@ -26,17 +25,17 @@ module Spree
       let!(:p1) { create(:simple_product, supplier: e, taxons: [t1, t2]) }
 
       it "finds taxons" do
-        Taxon.supplied_taxons.should == {e.id => Set.new([t0.id, t1.id, t2.id])}
+        Taxon.supplied_taxons.should == {e.id => Set.new(p1.taxons.map(&:id))}
       end
     end
 
     describe "finding all distributed taxons" do
       let!(:oc) { create(:simple_order_cycle, distributors: [e], variants: [p1.master]) }
-      let(:s) { create(:supplier_enterprise) }
-      let(:p1) { create(:simple_product, supplier: s, taxons: [t1, t2]) }
+      let!(:s) { create(:supplier_enterprise) }
+      let!(:p1) { create(:simple_product, supplier: s, taxons: [t1, t2]) }
 
       it "finds taxons" do
-        Taxon.distributed_taxons.should == {e.id => Set.new([t0.id, t1.id, t2.id])}
+        Taxon.distributed_taxons.should == {e.id => Set.new(p1.taxons.map(&:id))}
       end
     end
   end
