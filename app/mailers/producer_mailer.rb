ed37604bc283ffa1c8000f372c21ca1cb4b953b9
Lynne Davis <lynne@deanforestfoodhub.org.uk>
2016-06-18 09:09:43 +0100

    Update from address in notification emails

--- app/mailers/producer_mailer.rb
+++ app/mailers/producer_mailer.rb
@@ -14,7 +14,7 @@ class ProducerMailer < Spree::BaseMailer
 
     if has_orders? order_cycle, producer
       mail(to: @producer.email,
-           from: from_address,
+           from: @coordinator.email,
            subject: subject,
            reply_to: @coordinator.email,
            cc: @coordinator.email)
