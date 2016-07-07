b637b5b75ab8bd757d0b0fb6f943eb368e358be3
Rohan Mitchell <rohan@rohanmitchell.com>
2016-07-07 12:28:27 +1000

    Remove CMS helper: home_page_cms_content

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,13 +2,6 @@ module ApplicationHelper
   include FoundationRailsHelper::FlashHelper
 
 
-  def home_page_cms_content
-    if controller.controller_name == 'home' && controller.action_name == 'index'
-      cms_page_content(:content, Cms::Page.find_by_full_path('/'))
-    end
-  end
-
-
   def ng_form_for(name, *args, &block)
     options = args.extract_options!
 
