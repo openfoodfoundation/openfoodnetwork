eb8cedc497c938d53d26e2ea6e3eebfbe45569ce
Rohan Mitchell <rohan@rohanmitchell.com>
2016-07-07 12:26:34 +1000

    Remove CMS views and overrides

--- app/overrides/add_cms_admin_tab.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path  => "spree/admin/shared/_configuration_menu",
-                     :name          => "add_cms_admin_to_admin_configurations_menu",
-                     :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
-                     :text          => "<li><%= link_to 'CMS Admin', main_app.cms_admin_path %></li>",
-                     :original      => '29e0ab9c171ffab1988cb439b5d42300b78fe088' )
--- app/overrides/add_cms_tabs_to_main_nav_bar.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path  => "spree/shared/_main_nav_bar",
-                     :name          => "add_cms_tabs_to_main_nav_bar",
-                     :insert_after  => "li#home-link",
-                     :partial       => "spree/shared/cms_tabs",
-                     :original      => '05c6495f8760e58eb68e2cce67433cf7f5299fa4')
