b2e14711acc43ef80928416289da4296a341b319
Rob Harrington <oeoeaio@gmail.com>
2016-08-31 12:43:22 +1000

    Gateway providers inherit from decorated Gateway and PaymentMethod classes in production
    Achieved by requiring payment method and gateway decorators in Spree initializer

--- app/models/spree/gateway/pay_pal_express_decorator.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../gateway_decorator'
-
-module Spree
-  class Gateway::PayPalExpress < Gateway
-    # Something odd is happening with class inheritance here, this class (defined in spree_paypal_express gem)
-    # doesn't seem to pick up attr_accessible from the Gateway class, so we redefine the attrs we need here
-    attr_accessible :tag_list
-  end
-end
