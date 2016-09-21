c37214dbc9337ce484570f942aacfee3d43d80e6
Rob Harrington <oeoeaio@gmail.com>
2016-09-21 18:27:47 +1000

    SPIKE: Basic grid showing data

--- app/serializers/api/admin/reports/order_serializer.rb
+++ app/serializers/api/admin/reports/order_serializer.rb
@@ -1,5 +1,5 @@
 class Api::Admin::Reports::OrderSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :number
 
   # has_one :shop, serializer: Api::Admin::IdSerializer
   # has_one :address, serializer: Api::Admin::IdSerializer
