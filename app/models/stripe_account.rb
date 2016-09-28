7fd8c5956dafc18e833c2f89dadaf9531f460e9c
Steve Pettitt <spettitt@gmail.com>
2016-09-28 17:26:08 +0100

    Check CSRF, allow a Stripe account to be connected to more than one enterprise (but not vice versa)

--- app/models/stripe_account.rb
+++ app/models/stripe_account.rb
@@ -1,5 +1,5 @@
 class StripeAccount < ActiveRecord::Base
   belongs_to :enterprise
   validates_presence_of :stripe_user_id, :stripe_publishable_key
-  validates_uniqueness_of :stripe_user_id, :enterprise_id
+  validates_uniqueness_of :enterprise_id
 end
