69145206a5dcac2f330a5d31075dbacf806771d5
Steve Pettitt <spettitt@gmail.com>
2016-10-01 10:31:49 +0100

    Remove CSRF check - doesn't work properly as implemented, but connect request should probably be changed to POST

--- app/controllers/admin/enterprises_controller.rb
+++ app/controllers/admin/enterprises_controller.rb
@@ -115,31 +115,26 @@ module Admin
     end
 
     def stripe_connect
-      redirect_to authorize_stripe(params[:enterprise_id], csrf: form_authenticity_token)
+      redirect_to authorize_stripe(params[:enterprise_id])
     end
 
     def stripe_connect_callback
       if params["code"]
         state = JSON.parse(params["state"].gsub("=>",":"))
-        # Check csrf
-        if state["csrf"] != form_authenticity_token
-          redirect_to '/unauthorized'
-        else
-          # Get the Enterprise
-          @enterprise = Enterprise.find_by_permalink(state["enterprise_id"])
+        # Get the Enterprise
+        @enterprise = Enterprise.find_by_permalink(state["enterprise_id"])
 
-          # Get the deets from Stripe
-          response_params = get_stripe_token(params["code"]).params
+        # Get the deets from Stripe
+        response_params = get_stripe_token(params["code"]).params
 
-          stripe_account = StripeAccount.new(stripe_user_id: response_params["stripe_user_id"], stripe_publishable_key: response_params["stripe_publishable_key"], enterprise: @enterprise)
-          if stripe_account.save
-            respond_to do |format|
-              format.html { redirect_to main_app.edit_admin_enterprise_path(@enterprise), notice: "Stripe account connected successfully."}
-              format.json { render json: stripe_account }
-            end
-          else
-            render text: "Failed to save Stripe token", status: 500
+        stripe_account = StripeAccount.new(stripe_user_id: response_params["stripe_user_id"], stripe_publishable_key: response_params["stripe_publishable_key"], enterprise: @enterprise)
+        if stripe_account.save
+          respond_to do |format|
+            format.html { redirect_to main_app.edit_admin_enterprise_path(@enterprise), notice: "Stripe account connected successfully."}
+            format.json { render json: stripe_account }
           end
+        else
+          render text: "Failed to save Stripe token", status: 500
         end
       else
         render text: params["error_description"], status: 500
