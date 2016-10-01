b87a6658f9902f932febe4ebe15478fc41f7483b
Steve Pettitt <spettitt@gmail.com>
2016-10-01 11:43:54 +0100

    Worklow to disconnect Stripe from OFN admin interface

--- config/routes.rb
+++ config/routes.rb
@@ -68,7 +68,6 @@ Openfoodnetwork::Application.routes.draw do
       post :search
       get :check_permalink
     end
-    
 
     member do
       get :shop
@@ -98,7 +97,7 @@ Openfoodnetwork::Application.routes.draw do
       end
 
       get "/stripe_connect", to: "enterprises#stripe_connect"
-
+      resources :stripe_accounts
 
       member do
         get :welcome
