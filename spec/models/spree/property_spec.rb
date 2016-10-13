f4f38b41833b66f6a815b6247ba12cea16319f1f
Rohan Mitchell <rohan@rohanmitchell.com>
2016-10-14 09:56:40 +1100

    Re-apply shop property filters - including performance regression
    This reverts commit 7d149ed198a18ce602cf3b35eff93f55645df851.

--- spec/models/spree/property_spec.rb
+++ spec/models/spree/property_spec.rb
@@ -31,42 +31,53 @@ module Spree
         end
       end
 
-      describe ".sold_by" do
+      describe ".currently_sold_by and .ever_sold_by" do
         let!(:shop) { create(:distributor_enterprise) }
         let!(:shop_other) { create(:distributor_enterprise) }
         let!(:product) { create(:simple_product) }
         let!(:product_other_ex) { create(:simple_product) }
         let!(:product_no_oc) { create(:simple_product) }
-        let!(:product_closed_oc) { create(:simple_product) }
         let!(:oc) { create(:simple_order_cycle, distributors: [shop], variants: [product.variants.first]) }
-        let!(:oc_closed) { create(:closed_order_cycle, distributors: [shop], variants: [product_closed_oc.variants.first]) }
         let!(:exchange_other_shop) { create(:exchange, order_cycle: oc, sender: oc.coordinator, receiver: shop_other, variants: [product_other_ex.variants.first]) }
         let(:property) { product.properties.last }
         let(:property_other_ex) { product_other_ex.properties.last }
         let(:property_no_oc) { product_no_oc.properties.last }
-        let(:property_closed_oc) { product_closed_oc.properties.last }
 
         before do
           product.set_property 'Organic', 'NASAA 12345'
           product_other_ex.set_property 'Biodynamic', 'ASDF 12345'
           product_no_oc.set_property 'Shiny', 'Very'
-          product_closed_oc.set_property 'Spiffy', 'Ooh yeah'
         end
 
         it "returns the property" do
-          expect(Property.sold_by(shop)).to eq [property]
+          expect(Property.currently_sold_by(shop)).to eq [property]
+          expect(Property.ever_sold_by(shop)).to eq [property]
         end
 
         it "doesn't return the property from another exchange" do
-          expect(Property.sold_by(shop)).not_to include property_other_ex
+          expect(Property.currently_sold_by(shop)).not_to include property_other_ex
+          expect(Property.ever_sold_by(shop)).not_to include property_other_ex
         end
 
         it "doesn't return the property with no order cycle" do
-          expect(Property.sold_by(shop)).not_to include property_no_oc
+          expect(Property.currently_sold_by(shop)).not_to include property_no_oc
+          expect(Property.ever_sold_by(shop)).not_to include property_no_oc
         end
 
-        it "doesn't return the property from a closed order cycle" do
-          expect(Property.sold_by(shop)).not_to include property_closed_oc
+        describe "closed order cyces" do
+          let!(:product_closed_oc) { create(:simple_product) }
+          let!(:oc_closed) { create(:closed_order_cycle, distributors: [shop], variants: [product_closed_oc.variants.first]) }
+          let(:property_closed_oc) { product_closed_oc.properties.last }
+
+          before { product_closed_oc.set_property 'Spiffy', 'Ooh yeah' }
+
+          it "doesn't return the property for .currently_sold_by" do
+            expect(Property.currently_sold_by(shop)).not_to include property_closed_oc
+          end
+
+          it "returns the property for .ever_sold_by" do
+            expect(Property.ever_sold_by(shop)).to include property_closed_oc
+          end
         end
 
         context "with another product in the order cycle" do
@@ -78,7 +89,8 @@ module Spree
           end
 
           it "doesn't return duplicates" do
-            expect(Property.sold_by(shop).to_a.count).to eq 1
+            expect(Property.currently_sold_by(shop).to_a.count).to eq 1
+            expect(Property.ever_sold_by(shop).to_a.count).to eq 1
           end
         end
       end
