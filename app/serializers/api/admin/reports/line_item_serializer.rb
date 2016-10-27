7e15dcf9ef3d8978176131bcd2ddcc68dd6242cf
Rob Harrington <oeoeaio@gmail.com>
2016-10-27 15:19:44 +1100

    SPIKE: Adding more columns for demonstration

--- app/serializers/api/admin/reports/line_item_serializer.rb
+++ app/serializers/api/admin/reports/line_item_serializer.rb
@@ -2,5 +2,9 @@ class Api::Admin::Reports::LineItemSerializer < ActiveModel::Serializer
   attributes :id, :quantity, :max_quantity, :price, :full_name
 
   has_one :order, serializer: Api::Admin::IdSerializer
-  has_one :product, serializer: Api::Admin::IdSerializer
+  has_one :product, serializer: Api::Admin::IdNameSerializer
+
+  def price
+    object.display_amount_with_adjustments.to_s
+  end
 end
