1a939bd5696180cc8c223f73fadb1c290598b01c
Rob Harrington <oeoeaio@gmail.com>
2016-10-27 11:48:55 +1100

    SPIKE: Back to grouping with ui-grid, using aggregations to diplay relevant info in group headers

--- app/serializers/api/admin/reports/order_serializer.rb
+++ app/serializers/api/admin/reports/order_serializer.rb
@@ -1,5 +1,13 @@
 class Api::Admin::Reports::OrderSerializer < ActiveModel::Serializer
-  attributes :id, :number, :display_total
+  attributes :id, :number, :display_total, :customer, :email
+
+  def customer
+    object.bill_address.full_name
+  end
+
+  def display_total
+    object.display_total.to_s
+  end
 
   # has_one :shop, serializer: Api::Admin::IdSerializer
   # has_one :address, serializer: Api::Admin::IdSerializer
