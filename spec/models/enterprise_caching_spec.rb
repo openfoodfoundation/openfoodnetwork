9b656eaf4f3e951f60057dca5b9ebe21a1e0dccf
Rohan Mitchell <rohan@rohanmitchell.com>
2016-10-28 17:26:35 +1100

    Property / ProducerProperty changes update distributor enterprise cache

--- spec/models/enterprise_caching_spec.rb
+++ spec/models/enterprise_caching_spec.rb
@@ -5,6 +5,7 @@ describe Enterprise do
     describe "is touched when a(n)" do
       let(:enterprise) { create(:distributor_enterprise, updated_at: 1.week.ago) }
       let(:taxon) { create(:taxon) }
+      let(:supplier2) { create(:supplier_enterprise) }
 
       describe "with a supplied product" do
         let(:product) { create(:simple_product, supplier: enterprise) }
@@ -28,16 +29,47 @@ describe Enterprise do
         it "touches enterprise when a producer property on that product changes" do
           expect { producer_property.save! }.to change { enterprise.reload.updated_at }
         end
+
+        it "touches enterprise when the supplier of a product changes" do
+          expect {
+            product.update_attributes!(supplier: supplier2)
+          }.to change { enterprise.updated_at }
+        end
       end
 
       describe "with a distributed product" do
         let(:product) { create(:simple_product) }
         let(:oc) { create(:simple_order_cycle, distributors: [enterprise], variants: [product.variants.first]) }
+        let(:supplier) { product.supplier }
         let!(:classification) { create(:classification, taxon: taxon, product: product) }
+        let(:property) { product.product_properties.last }
+        let(:producer_property) { supplier.producer_properties.last }
 
-        it "touches enterprise when a classification on that product changes" do
-          oc
-          expect { classification.save! }.to change { enterprise.reload.updated_at }
+        before do
+          product.set_property 'Organic', 'NASAA 12345'
+          supplier.set_producer_property 'Biodynamic', 'ASDF 4321'
+        end
+
+        context "with an order cycle" do
+          before { oc }
+
+          it "touches enterprise when a classification on that product changes" do
+            expect { classification.save! }.to change { enterprise.reload.updated_at }
+          end
+
+          it "touches enterprise when a property on that product changes" do
+            expect { property.save! }.to change { enterprise.reload.updated_at }
+          end
+
+          it "touches enterprise when a producer property on that product changes" do
+            expect { producer_property.save! }.to change { enterprise.reload.updated_at }
+          end
+
+          it "touches enterprise when the supplier of a product changes" do
+            expect {
+              product.update_attributes!(supplier: supplier2)
+            }.to change { enterprise.reload.updated_at }
+          end
         end
 
         it "touches enterprise when the product's variant is added to order cycle" do
