7ce5949a246f4a62e5c10e39c61d317d392b9ef0
Bing Xie <bing708@gmail.com>
2016-10-29 13:54:21 +1100

    Tweaks

--- app/controllers/spree/users_controller_decorator.rb
+++ app/controllers/spree/users_controller_decorator.rb
@@ -1,7 +1,7 @@
 Spree::UsersController.class_eval do
   layout 'darkswarm'
 
-  prepend_before_filter :load_object, :only => [:edit_address, :update_address, :show, :edit, :update]
+  prepend_before_filter :load_object, only: [:edit_address, :update_address, :show, :edit, :update]
 
   def edit_address
     country_id = Spree::Config[:default_country_id]
@@ -13,7 +13,7 @@ Spree::UsersController.class_eval do
 
   def update_address
     if @user.update_attributes(params[:user])
-      redirect_to spree.account_url, :notice => t(:account_updated)
+      redirect_to spree.account_url
     else
       render :edit_address
     end
