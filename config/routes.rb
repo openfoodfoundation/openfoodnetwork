48af1956a17337404934103d4ea4da182f497092
Bing Xie <bing708@gmail.com>
2016-10-31 19:08:22 +1100

    Tweaks for new hash syntax

--- config/routes.rb
+++ config/routes.rb
@@ -48,8 +48,8 @@ Openfoodnetwork::Application.routes.draw do
     end
   end
 
-  get '/account/edit_address', :to => 'spree/users#edit_address'
-  put '/account/update_address', :to => 'spree/users#update_address'
+  get '/account/edit_address', to: 'spree/users#edit_address'
+  put '/account/update_address', to: 'spree/users#update_address'
 
   get '/checkout', :to => 'checkout#edit' , :as => :checkout
   put '/checkout', :to => 'checkout#update' , :as => :update_checkout
