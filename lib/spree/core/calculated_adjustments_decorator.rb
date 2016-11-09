3135ef6b7fa9ab38e441b4cfa58872b51da0c53b
Rohan Mitchell <rohan@rohanmitchell.com>
2016-11-09 12:47:57 +1100

    Reinstate explicit class name Spree::Calculator on calculated_adjustments association

--- lib/spree/core/calculated_adjustments_decorator.rb
+++ lib/spree/core/calculated_adjustments_decorator.rb
@@ -1,16 +1,16 @@
-# Maybe we don't need this any more? Commenting out during upgrade process
+module Spree
+  module Core
+    module CalculatedAdjustments
+      class << self
+        def included_with_explicit_class_name(klass)
+          included_without_explicit_class_name(klass)
 
-# module Spree
-#   module Core
-#     module CalculatedAdjustments
-#       module ClassMethods
-#         def calculated_adjustments_with_explicit_class_name
-#           calculated_adjustments_without_explicit_class_name
-#           # Class name is mis-inferred outside of Spree namespace
-#           has_one :calculator, as: :calculable, dependent: :destroy, class_name: 'Spree::Calculator'
-#         end
-#         alias_method_chain :calculated_adjustments, :explicit_class_name
-#       end
-#     end
-#   end
-# end
+          klass.class_eval do
+            has_one :calculator, as: :calculable, dependent: :destroy, class_name: 'Spree::Calculator'
+          end
+        end
+        alias_method_chain :included, :explicit_class_name
+      end
+    end
+  end
+end
