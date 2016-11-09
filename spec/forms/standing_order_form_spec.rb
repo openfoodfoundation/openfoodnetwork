95a7fc8ef536bd1daf4ea7ecbb136bab277bf5a1
Rob Harrington <oeoeaio@gmail.com>
2016-11-10 10:53:00 +1100

    Initialisation and removal of standing order orders respects begins_at and ends_at contraints

--- spec/forms/standing_order_form_spec.rb
+++ spec/forms/standing_order_form_spec.rb
@@ -10,13 +10,15 @@ module OpenFoodNetwork
       let!(:variant2) { create(:variant, product: product2, unit_value: '1000', price: 6.00, option_values: []) }
       let!(:variant3) { create(:variant, product: product2, unit_value: '1000', price: 2.50, option_values: []) }
       let!(:enterprise_fee) { create(:enterprise_fee, amount: 1.75) }
-      let!(:order_cycle1) { create(:simple_order_cycle, coordinator: shop, orders_open_at: 3.weeks.ago, orders_close_at: 2.weeks.ago) }
-      let!(:order_cycle2) { create(:simple_order_cycle, coordinator: shop, orders_open_at: 2.days.from_now, orders_close_at: 9.days.from_now) }
-      let!(:order_cycle3) { create(:simple_order_cycle, coordinator: shop, orders_open_at: 9.days.from_now, orders_close_at: 16.days.from_now) }
+      let!(:order_cycle1) { create(:simple_order_cycle, coordinator: shop, orders_open_at: 9.days.ago, orders_close_at: 2.day.ago) }
+      let!(:order_cycle2) { create(:simple_order_cycle, coordinator: shop, orders_open_at: 2.day.ago, orders_close_at: 5.days.from_now) }
+      let!(:order_cycle3) { create(:simple_order_cycle, coordinator: shop, orders_open_at: 5.days.from_now, orders_close_at: 12.days.from_now) }
+      let!(:order_cycle4) { create(:simple_order_cycle, coordinator: shop, orders_open_at: 12.days.from_now, orders_close_at: 19.days.from_now) }
       let!(:outgoing_exchange1) { order_cycle1.exchanges.create(sender: shop, receiver: shop, variants: [variant1, variant2, variant3], enterprise_fees: [enterprise_fee]) }
-      let!(:outgoing_exchange2) { order_cycle2.exchanges.create(sender: shop, receiver: shop, variants: [variant1], enterprise_fees: [enterprise_fee]) }
-      let!(:outgoing_exchange3) { order_cycle3.exchanges.create(sender: shop, receiver: shop, variants: [variant1, variant2, variant3], enterprise_fees: []) }
-      let!(:schedule) { create(:schedule, order_cycles: [order_cycle1, order_cycle2, order_cycle3]) }
+      let!(:outgoing_exchange2) { order_cycle2.exchanges.create(sender: shop, receiver: shop, variants: [variant1, variant2, variant3], enterprise_fees: [enterprise_fee]) }
+      let!(:outgoing_exchange3) { order_cycle3.exchanges.create(sender: shop, receiver: shop, variants: [variant1, variant3], enterprise_fees: []) }
+      let!(:outgoing_exchange4) { order_cycle4.exchanges.create(sender: shop, receiver: shop, variants: [variant1, variant2, variant3], enterprise_fees: [enterprise_fee]) }
+      let!(:schedule) { create(:schedule, order_cycles: [order_cycle1, order_cycle2, order_cycle3, order_cycle4]) }
       let!(:payment_method) { create(:payment_method, distributors: [shop]) }
       let!(:shipping_method) { create(:shipping_method, distributors: [shop]) }
       let!(:address) { create(:address) }
@@ -30,7 +32,8 @@ module OpenFoodNetwork
         ship_address_attributes: address.clone.attributes,
         payment_method_id: payment_method.id,
         shipping_method_id: shipping_method.id,
-        begins_at: 2.weeks.ago,
+        begins_at: 4.days.ago,
+        ends_at: 14.days.from_now,
         standing_line_items_attributes: [
           {variant_id: variant1.id, quantity: 1},
           {variant_id: variant2.id, quantity: 2},
@@ -43,19 +46,40 @@ module OpenFoodNetwork
       it "creates orders for each order cycle in the schedule" do
         form.save
 
-        expect(standing_order.orders.count).to be 3
+        expect(standing_order.orders.count).to be 2
 
-        # Add line items for variants that aren't yet available from the order cycle
+        # This order cycle has already closed, so no order is initialized
         order1 = standing_order.orders.find_by_order_cycle_id(order_cycle1.id)
-        expect(order1).to be_a Spree::Order
-        expect(order1.line_items.count).to be 3
-        expect(order1.shipments.count).to be 1
-        expect(order1.shipments.first.shipping_method).to eq shipping_method
-        expect(order1.payments.count).to be 1
-        expect(order1.payments.first.payment_method).to eq payment_method
-        expect(order1.payments.first.state).to eq 'checkout'
-        expect(order1.total).to eq 42
-        expect(order1.completed?).to be false
+        expect(order1).to be nil
+
+        # Currently open order cycle, closing after begins_at and before ends_at
+        order2 = standing_order.orders.find_by_order_cycle_id(order_cycle2.id)
+        expect(order2).to be_a Spree::Order
+        expect(order2.line_items.count).to be 3
+        expect(order2.shipments.count).to be 1
+        expect(order2.shipments.first.shipping_method).to eq shipping_method
+        expect(order2.payments.count).to be 1
+        expect(order2.payments.first.payment_method).to eq payment_method
+        expect(order2.payments.first.state).to eq 'checkout'
+        expect(order2.total).to eq 42
+        expect(order2.completed?).to be false
+
+        # Future order cycle, closing after begins_at and before ends_at
+        # Add line items for variants that aren't yet available from the order cycle
+        order3 = standing_order.orders.find_by_order_cycle_id(order_cycle3.id)
+        expect(order3).to be_a Spree::Order
+        expect(order3.line_items.count).to be 3
+        expect(order3.shipments.count).to be 1
+        expect(order3.shipments.first.shipping_method).to eq shipping_method
+        expect(order3.payments.count).to be 1
+        expect(order3.payments.first.payment_method).to eq payment_method
+        expect(order3.payments.first.state).to eq 'checkout'
+        expect(order3.total).to eq 31.50
+        expect(order3.completed?).to be false
+
+        # Future order cycle closing after ends_at
+        order4 = standing_order.orders.find_by_order_cycle_id(order_cycle4.id)
+        expect(order4).to be nil
       end
     end
 
@@ -135,6 +159,23 @@ module OpenFoodNetwork
           expect(payments.count).to be 1
           expect(payments.first.payment_method).to eq changed_payment_method
         end
+      end
+    end
+
+    describe "changing begins_at" do
+      let(:standing_order) { create(:standing_order_with_items, begins_at: Time.zone.now) }
+      let(:params) { { begins_at: 1.year.from_now, ends_at: 2.years.from_now } }
+      let(:form) { StandingOrderForm.new(standing_order, params) }
+
+      before { form.send(:initialise_orders!) }
+
+      it "removes orders outside the newly specified date range" do
+        expect(standing_order.reload.orders.count).to be 1
+        form.save
+        expect(standing_order.reload.orders.count).to be 0
+        form.params = { begins_at: 1.month.ago }
+        form.save
+        expect(standing_order.reload.orders.count).to be 1
       end
     end
 
