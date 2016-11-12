09aa09dd1b028ca148656e70c34483c83b5f2d14
stveep <spettitt@gmail.com>
2016-11-12 12:07:00 +0000

    Undo dodgy redirect changes and set spec to pending

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20160828115018) do
+ActiveRecord::Schema.define(:version => 20160921060442) do
 
   create_table "account_invoices", :force => true do |t|
     t.integer  "user_id",    :null => false
