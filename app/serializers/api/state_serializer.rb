5b9eeacf7027ec5124779a292f3b849538404c5d
Keir Osborn <keir.osborn@gmail.com>
2016-11-16 01:57:23 +0000

    state display internationalization fixes

--- app/serializers/api/state_serializer.rb
+++ app/serializers/api/state_serializer.rb
@@ -1,7 +1,12 @@
 class Api::StateSerializer < ActiveModel::Serializer
-  attributes :id, :name, :abbr
+  attributes :id, :name, :abbr, :label
 
   def abbr
     object.abbr.upcase
   end
+
+  def label 
+	eval I18n.t("registration_detail_state_label")
+  end
+
 end
