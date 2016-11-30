0f78e0309946aa777e99d415c5dea7a778913557
Rob Harrington <oeoeaio@gmail.com>
2016-11-30 16:34:15 +1100

    Standing Order Orders can be cancelled

--- app/models/standing_order_order.rb
+++ app/models/standing_order_order.rb
@@ -1,4 +1,14 @@
 class StandingOrderOrder < ActiveRecord::Base
   belongs_to :order, class_name: 'Spree::Order', dependent: :destroy
   belongs_to :standing_order
+
+  def cancel
+    transaction do
+      if order.order_cycle.orders_close_at > Time.zone.now
+        self.update_column(:cancelled_at, Time.zone.now)
+        order.send('cancel') if order.complete?
+      end
+      self
+    end
+  end
 end
