216355f5ae884ea402d0ff3e4dddb36c74f93c13
Levent Ali <lebreeze@gmail.com>
2016-11-30 18:26:39 +0000

    Refactor set_current_order

--- lib/spree/core/controller_helpers/order_decorator.rb
+++ lib/spree/core/controller_helpers/order_decorator.rb
@@ -18,13 +18,25 @@ Spree::Core::ControllerHelpers::Order.class_eval do
       last_incomplete_order = user.last_incomplete_spree_order
       if session[:order_id].nil? && last_incomplete_order
         session[:order_id] = last_incomplete_order.id
-      elsif current_order && last_incomplete_order && current_order != last_incomplete_order
-        if current_order.distributor == last_incomplete_order.distributor && current_order.order_cycle == last_incomplete_order.order_cycle && current_order.line_items.empty?
+      elsif separate_orders?(last_incomplete_order)
+        if order_should_be_kept?(last_incomplete_order)
           current_order(true).merge!(last_incomplete_order)
         else
           last_incomplete_order.destroy
         end
       end
     end
+  end
+
+  private
+
+  def separate_orders?(order)
+    current_order && order && current_order != order
+  end
+
+  def order_should_be_kept?(order)
+    current_order.distributor == order.distributor &&
+      current_order.order_cycle == order.order_cycle &&
+      current_order.line_items.empty?
   end
 end
