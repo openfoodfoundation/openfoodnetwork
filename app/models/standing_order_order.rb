94b697f0ced486939afaf2fe734d3afb44187d5e
Rob Harrington <oeoeaio@gmail.com>
2016-12-01 11:30:51 +1100

    WIP: Adding basic order panel to standing order index

--- app/models/standing_order_order.rb
+++ app/models/standing_order_order.rb
@@ -2,8 +2,19 @@ class StandingOrderOrder < ActiveRecord::Base
   belongs_to :order, class_name: 'Spree::Order', dependent: :destroy
   belongs_to :standing_order
 
+  delegate :number, :order_cycle_id, :completed_at, :total, to: :order
+
   scope :closed, -> { joins(order: :order_cycle).merge(OrderCycle.closed) }
   scope :not_closed, -> { joins(order: :order_cycle).merge(OrderCycle.not_closed) }
+
+  def status
+    return 'cancelled' if cancelled?
+    order.state
+  end
+
+  def cancelled?
+    cancelled_at.present?
+  end
 
   def cancel
     transaction do
---
+++ app/serializers/api/admin/standing_order_order_serializer.rb
@@ -0,0 +1,15 @@
+class Api::Admin::StandingOrderOrderSerializer < ActiveModel::Serializer
+  attributes :id, :status, :edit_path, :number, :completed_at, :order_cycle_id, :total
+
+  def total
+    object.total.to_money.to_s
+  end
+
+  def completed_at
+    object.completed_at.blank? ? "" : object.completed_at.strftime("%F %T")
+  end
+
+  def edit_path
+    spree.edit_admin_order_path(object.order)
+  end
+end
