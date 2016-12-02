5191877fbe699d17febcb83ad74e73a988ae9be5
Rob Harrington <oeoeaio@gmail.com>
2016-12-02 11:55:44 +1100

    BasicOrderCycleSerializer renders order open and close times

--- app/serializers/api/admin/basic_order_cycle_serializer.rb
+++ app/serializers/api/admin/basic_order_cycle_serializer.rb
@@ -1,7 +1,7 @@
 class Api::Admin::BasicOrderCycleSerializer < ActiveModel::Serializer
   include OrderCyclesHelper
 
-  attributes :id, :name, :status, :first_order, :last_order
+  attributes :id, :name, :status, :orders_open_at, :orders_close_at
 
   has_many :suppliers, serializer: Api::Admin::IdNameSerializer
   has_many :distributors, serializer: Api::Admin::IdNameSerializer
@@ -10,15 +10,11 @@ class Api::Admin::BasicOrderCycleSerializer < ActiveModel::Serializer
     order_cycle_status_class object
   end
 
-  def first_order
+  def orders_open_at
     object.orders_open_at.andand.strftime("%F")
   end
 
-  def last_order
-    if object.orders_close_at.present?
-      (object.orders_close_at + 1.day).strftime("%F")
-    else
-      nil
-    end
+  def orders_close_at
+    object.orders_close_at.andand.strftime("%F")
   end
 end
