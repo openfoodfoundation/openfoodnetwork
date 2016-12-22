0e50009875aae4bb415c81e83370e884ebc860da
Rob Harrington <oeoeaio@gmail.com>
2016-12-22 17:23:10 +1100

    WIP: StandingOrderPlacementJob no longer scoped to an order cycle

--- spec/jobs/standing_order_placement_job_spec.rb
+++ spec/jobs/standing_order_placement_job_spec.rb
@@ -1,6 +1,64 @@
 require 'spec_helper'
 
 describe StandingOrderPlacementJob do
+  let(:job) { StandingOrderPlacementJob.new }
+
+  describe "finding proxy_orders that are ready to be placed" do
+    let(:shop) { create(:distributor_enterprise) }
+    let(:order_cycle1) { create(:simple_order_cycle, coordinator: shop, orders_open_at: 1.minute.ago, orders_close_at: 10.minutes.from_now) }
+    let(:order_cycle2) { create(:simple_order_cycle, coordinator: shop, orders_open_at: 10.minutes.ago, orders_close_at: 1.minute.ago) }
+    let(:schedule) { create(:schedule, order_cycles: [order_cycle1, order_cycle2]) }
+    let(:standing_order) { create(:standing_order, shop: shop, schedule: schedule) }
+    let!(:proxy_order) { create(:proxy_order, standing_order: standing_order, order_cycle: order_cycle1) } # OK
+
+    it "ignores proxy orders where the OC has closed" do
+      expect(job.send(:proxy_orders)).to include proxy_order
+      proxy_order.update_attributes!(order_cycle_id: order_cycle2.id)
+      expect(job.send(:proxy_orders)).to_not include proxy_order
+    end
+
+    it "ignores proxy orders for paused or cancelled standing orders" do
+      expect(job.send(:proxy_orders)).to include proxy_order
+      standing_order.update_attributes!(paused_at: 1.minute.ago)
+      expect(job.send(:proxy_orders)).to_not include proxy_order
+      standing_order.update_attributes!(paused_at: nil)
+      expect(job.send(:proxy_orders)).to include proxy_order
+      standing_order.update_attributes!(canceled_at: 1.minute.ago)
+      expect(job.send(:proxy_orders)).to_not include proxy_order
+    end
+
+    it "ignores proxy orders that have been marked as cancelled or placed" do
+      expect(job.send(:proxy_orders)).to include proxy_order
+      proxy_order.update_attributes!(canceled_at: 5.minutes.ago)
+      expect(job.send(:proxy_orders)).to_not include proxy_order
+      proxy_order.update_attributes!(canceled_at: nil)
+      expect(job.send(:proxy_orders)).to include proxy_order
+      proxy_order.update_attributes!(placed_at: 5.minutes.ago)
+      expect(job.send(:proxy_orders)).to_not include proxy_order
+    end
+  end
+
+  describe "performing the job" do
+    context "when unplaced proxy_orders exist" do
+      let!(:proxy_order) { create(:proxy_order) }
+
+      before do
+        allow(job).to receive(:proxy_orders) { ProxyOrder.where(id: proxy_order.id) }
+        allow(job).to receive(:process)
+      end
+
+      it "marks placeable proxy_orders as processed by setting placed_at" do
+        expect{job.perform}.to change{proxy_order.reload.placed_at}
+        expect(proxy_order.placed_at).to be_within(5.seconds).of Time.now
+      end
+
+      it "processes placeable proxy_orders" do
+        job.perform
+        expect(job).to have_received(:process).with(proxy_order.reload.order)
+      end
+    end
+  end
+
   describe "checking that line items are available to purchase" do
     let(:order_cycle) { create(:simple_order_cycle) }
     let(:shop) { order_cycle.coordinator }
@@ -12,8 +70,6 @@ describe StandingOrderPlacementJob do
     let!(:line_item1) { create(:line_item, order: order, variant: variant1, quantity: 3) }
     let!(:line_item2) { create(:line_item, order: order, variant: variant2, quantity: 3) }
     let!(:line_item3) { create(:line_item, order: order, variant: variant3, quantity: 3) }
-
-    let!(:job) { StandingOrderPlacementJob.new(order_cycle) }
 
     before { Spree::Config.set(:allow_backorders, false) }
 
@@ -68,8 +124,6 @@ describe StandingOrderPlacementJob do
     let!(:order) { proxy_order.initialise_order! }
     let(:changes) { {} }
 
-    let!(:job) { StandingOrderPlacementJob.new(proxy_order.order_cycle) }
-
     before do
       expect_any_instance_of(Spree::Payment).to_not receive(:process!)
       allow(job).to receive(:cap_quantity_and_store_changes) { changes }
@@ -110,8 +164,6 @@ describe StandingOrderPlacementJob do
     let!(:order) { proxy_order.initialise_order! }
     let(:mail_mock) { double(:mailer_mock) }
     let(:changes) { double(:changes) }
-
-    let!(:job) { StandingOrderPlacementJob.new(proxy_order.order_cycle) }
 
     before do
       allow(Spree::OrderMailer).to receive(:standing_order_email) { mail_mock }
