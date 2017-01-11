318183c9e973bd41f5b8bf118e4be09673caf34d
Keir Osborn <keir.osborn@gmail.com>
2017-01-11 01:06:17 +0000

    fix override deface path error

--- app/overrides/spree/admin/general_settings/edit/add_display_preference_to_state.rb
+++ app/overrides/spree/admin/general_settings/edit/add_display_preference_to_state.rb
@@ -1,4 +1,4 @@
   Deface::Override.new(:virtual_path => "spree/admin/general_settings/edit",
 					 :name => "add_display_preferece_to_state",
 					 :insert_before => "[data-hook='buttons']",
-					 :partial => 'spree/admin/states/display_preference_form')
+					 :partial => 'spree/admin/general_settings/state_display_preference_form')
