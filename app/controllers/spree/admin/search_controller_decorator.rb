6ac49a7694e50b3d34e3975dc7f2ff81fc7bbcb2
Rob Harrington <oeoeaio@gmail.com>
2017-01-20 15:52:27 +1100

    Adding route and controller action for searching for customer addresses using AddressFinder

--- app/controllers/spree/admin/search_controller_decorator.rb
+++ app/controllers/spree/admin/search_controller_decorator.rb
@@ -1,3 +1,5 @@
+require 'open_food_network/address_finder'
+
 Spree::Admin::SearchController.class_eval do
   def known_users
     if exact_match = Spree.user_class.find_by_email(params[:q])
@@ -33,4 +35,14 @@ Spree::Admin::SearchController.class_eval do
   end
 
   alias_method_chain :users, :ams
+
+  def customer_addresses
+    customer = Customer.of(spree_current_user.enterprises).find(params[:customer_id])
+    redirect_to :unauthorised unless customer.present?
+
+    finder = OpenFoodNetwork::AddressFinder.new(customer, customer.email)
+    bill_address = Api::AddressSerializer.new(finder.bill_address).serializable_hash
+    ship_address = Api::AddressSerializer.new(finder.ship_address).serializable_hash
+    render json: { bill_address: bill_address, ship_address: ship_address }
+  end
 end
