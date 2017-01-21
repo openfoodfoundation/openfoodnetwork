a039286240526809a4de491f8f5c8ec3bf933c77
stveep <spettitt@gmail.com>
2017-01-21 09:43:34 +0000

    Webhook processing changes: fix expected request data formatting

--- spec/controllers/admin/stripe_account_controller_spec.rb
+++ spec/controllers/admin/stripe_account_controller_spec.rb
@@ -5,19 +5,35 @@ describe Admin::StripeAccountsController, type: :controller do
   it "deletes Stripe accounts in response to a webhook" do
     # https://stripe.com/docs/api#retrieve_event
     allow(controller).to receive(:fetch_event_from_stripe)
-      .and_return({
-      "id" => "evt_18zt9YFBE7f7kItLg9f343bn",
-      "object" => "event",
-      "created" => 1475350088,
-      "data" => {
-          "id" => "webhook_id",
-          "name" => "OFN",
-          "object" => "application"
-      },
-      "type" => "account.application.deauthorized"
-    })
+      .and_return(Stripe::Event.construct_from({"id"=>"evt_wrfwg4323fw",
+                   "object"=>"event",
+                   "api_version"=>nil,
+                   "created"=>1484870684,
+                   "data"=>
+                    {"object"=>
+                      {"id"=>"application_id",
+                       "object"=>"application",
+                       "name"=>"Open Food Network UK"}},
+                   "livemode"=>false,
+                   "pending_webhooks"=>1,
+                   "request"=>nil,
+                   "type"=>"account.application.deauthorized",
+                   "user_id"=>"webhook_id"}))
     account = create(:stripe_account, stripe_user_id: "webhook_id")
-    post 'destroy_from_webhook'
+    post 'destroy_from_webhook', {"id"=>"evt_wrfwg4323fw",
+                                   "object"=>"event",
+                                   "api_version"=>nil,
+                                   "created"=>1484870684,
+                                   "data"=>
+                                    {"object"=>
+                                      {"id"=>"ca_9ByaSyyyXj5O73DWisU0KLluf0870Vro",
+                                       "object"=>"application",
+                                       "name"=>"Open Food Network UK"}},
+                                   "livemode"=>false,
+                                   "pending_webhooks"=>1,
+                                   "request"=>nil,
+                                   "type"=>"account.application.deauthorized",
+                                   "user_id"=>"webhook_id"}
     expect(StripeAccount.all).not_to include account
 
   end
