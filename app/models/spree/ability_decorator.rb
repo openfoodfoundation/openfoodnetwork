eec67925bb0ae4a523776b426e9c1da10390fd1e
Matt-Yorkley <visible_souls@hotmail.com>
2017-02-14 12:37:26 +0000

    Product Import cancan permissions and ui tab

--- app/models/spree/ability_decorator.rb
+++ app/models/spree/ability_decorator.rb
@@ -155,6 +155,8 @@ class AbilityDecorator
     can [:admin, :index, :read, :search], Spree::Taxon
     can [:admin, :index, :read, :create, :edit], Spree::Classification
 
+    can [:admin, :index, :import, :save], ProductImporter
+
     # Reports page
     can [:admin, :index, :customers, :orders_and_distributors, :group_buys, :bulk_coop, :payments, :orders_and_fulfillment, :products_and_inventory, :order_cycle_management, :packing], :report
   end
