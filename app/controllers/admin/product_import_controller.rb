252224f5af22f8bcdc578e4065bf7e64dd29801d
Matt-Yorkley <visible_souls@hotmail.com>
2017-03-03 21:25:54 +0000

    Product Import update

--- app/controllers/admin/product_import_controller.rb
+++ app/controllers/admin/product_import_controller.rb
@@ -2,44 +2,54 @@ require 'roo'
 
 class Admin::ProductImportController < Spree::Admin::BaseController
 
-  before_filter :check_upload, except: :index
+  before_filter :validate_upload_presence, except: :index
 
   def import
     # Save uploaded file to tmp directory
-    @filepath = save_upload(params[:file])
+    @filepath = save_uploaded_file(params[:file])
     @importer = ProductImporter.new(File.new(@filepath), editable_enterprises)
 
-    if @importer.errors.present?
-      flash[:notice] = @importer.errors.full_messages.to_sentence
-    end
+    check_file_errors @importer
+    check_spreadsheet_has_data @importer
+
+    @tax_categories = Spree::TaxCategory.order('is_default DESC, name ASC')
+    @shipping_categories = Spree::ShippingCategory.order('name ASC')
   end
 
   def save
-    file = File.new(params[:filepath])
-    @importer = ProductImporter.new(file, editable_enterprises)
-    @importer.save_all_valid
-
-    if @importer.updated_count && @importer.updated_count > 0
-      File.delete(file)
-      flash[:success] = "#{@importer.updated_count} records updated successfully"
-    else
-      flash[:notice] = @importer.errors.full_messages.to_sentence
-    end
+    @importer = ProductImporter.new(File.new(params[:filepath]), editable_enterprises)
+    @importer.save_all if @importer.has_valid_entries?
   end
 
   private
 
-  def check_upload
+  def validate_upload_presence
     unless params[:file] || (params[:filepath] && File.exist?(params[:filepath]))
-      redirect_to '/admin/product_import', :notice => 'File not found or could not be opened'
+      redirect_to '/admin/product_import', notice: 'File not found or could not be opened'
       return
     end
   end
 
-  def save_upload(upload)
-    filename = Time.now.strftime('%d-%m-%Y-%H-%M-%S')
+  def check_file_errors(importer)
+    if importer.errors.present?
+      redirect_to '/admin/product_import', notice: @importer.errors.full_messages.to_sentence
+      return
+    end
+  end
+
+  def check_spreadsheet_has_data(importer)
+    unless importer.item_count
+      redirect_to '/admin/product_import', notice: 'No data found in spreadsheet'
+      return
+    end
+  end
+
+  def save_uploaded_file(upload)
+    filename = 'import' + Time.now.strftime('%d-%m-%Y-%H-%M-%S')
     extension = '.' + upload.original_filename.split('.').last
-    File.open(Rails.root.join('tmp', filename+extension), 'wb') do |f|
+    directory = 'tmp/product_import'
+    Dir.mkdir(directory) unless File.exists?(directory)
+    File.open(Rails.root.join(directory, filename+extension), 'wb') do |f|
       f.write(upload.read)
       f.path
     end
--- spec/features/admin/product_import_spec.rb
+++ spec/features/admin/product_import_spec.rb
@@ -7,11 +7,14 @@ feature "Product Import", js: true do
 
   let!(:admin) { create(:admin_user) }
   let!(:user) { create_enterprise_user }
-  let!(:enterprise) { create(:enterprise, owner: user, name: "Test Enterprise") }
+  let!(:enterprise) { create(:supplier_enterprise, owner: user, name: "Test Enterprise") }
   let!(:category) { create(:taxon, name: 'Vegetables') }
+  let!(:category2) { create(:taxon, name: 'Cake') }
+  let!(:product) { create(:simple_product, supplier: enterprise, name: 'Hypothetical Cake') }
+  let!(:variant) { create(:variant, product_id: product.id, price: '8.50', count_on_hand: '100', unit_value: '500', display_name: 'Preexisting Banana') }
   let(:permissions) { OpenFoodNetwork::Permissions.new(user) }
 
-  describe "product import" do
+  describe "when importing products from uploaded file" do
     before { quick_login_as_admin }
     after { File.delete('/tmp/test.csv') }
 
@@ -26,14 +29,16 @@ feature "Product Import", js: true do
       visit main_app.admin_product_import_path
 
       expect(page).to have_content "Select a spreadsheet to upload"
-      attach_file('file', '/tmp/test.csv')
-      click_button('Import')
+      attach_file 'file', '/tmp/test.csv'
+      click_button 'Import'
 
-      expect(page).to have_content("Valid entries found: 2")
-      expect(page).to have_content("Invalid entries found: 0")
-      click_button('Save')
+      expect(page).to have_selector '.item-count', text: "2"
+      expect(page).to have_selector '.invalid-count', text: "0"
+      expect(page).to have_selector '.create-count', text: "2"
+      expect(page).to have_selector '.update-count', text: "0"
 
-      expect(page).to have_content("Products created: 2")
+      click_button 'Save'
+      expect(page).to have_content "Products created: 2"
 
       potatoes = Spree::Product.find_by_name('Potatoes')
       potatoes.supplier.should == enterprise
@@ -53,15 +58,16 @@ feature "Product Import", js: true do
 
       expect(page).to have_content "Select a spreadsheet to upload"
       attach_file('file', '/tmp/test.csv')
-      click_button('Import')
+      click_button 'Import'
 
-      expect(page).to have_content("Valid entries found: 1")
-      expect(page).to have_content("Invalid entries found: 1")
-      expect(page).to have_content("errors")
+      expect(page).to have_selector '.item-count', text: "2"
+      expect(page).to have_selector '.invalid-count', text: "1"
+      expect(page).to have_selector '.create-count', text: "1"
+      expect(page).to have_selector '.update-count', text: "0"
 
-      click_button('Save')
-
-      expect(page).to have_content("Products created: 1")
+      expect(page).to have_selector 'input[type=submit][value="Save"]'
+      click_button 'Save'
+      expect(page).to have_content "Products created: 1"
 
       Spree::Product.find_by_name('Bad Potatoes').should == nil
       carrots = Spree::Product.find_by_name('Good Carrots')
@@ -81,14 +87,84 @@ feature "Product Import", js: true do
       visit main_app.admin_product_import_path
 
       expect(page).to have_content "Select a spreadsheet to upload"
-      attach_file('file', '/tmp/test.csv')
-      click_button('Import')
+      attach_file 'file', '/tmp/test.csv'
+      click_button 'Import'
 
-      expect(page).to have_content("Valid entries found: 0")
-      expect(page).to have_content("Invalid entries found: 2")
-      expect(page).to have_content("errors")
+      expect(page).to have_selector '.item-count', text: "2"
+      expect(page).to have_selector '.invalid-count', text: "2"
+      expect(page).to have_selector '.create-count', text: "0"
+      expect(page).to have_selector '.update-count', text: "0"
 
-      expect(page).to_not have_selector('input[type=submit]', text: "Save")
+      expect(page).to_not have_selector 'input[type=submit][value="Save"]'
+    end
+
+    it "can add new variants to existing products and update price and stock level of existing products" do
+      csv_data = CSV.generate do |csv|
+        csv << ["name", "supplier", "category", "on_hand", "price", "unit_value", "variant_unit", "variant_unit_scale", "display_name"]
+        csv << ["Hypothetical Cake", "Test Enterprise", "Cake", "5", "5.50", "500", "weight", "1", "Preexisting Banana"]
+        csv << ["Hypothetical Cake", "Test Enterprise", "Cake", "6", "3.50", "500", "weight", "1", "Emergent Coffee"]
+      end
+      File.write('/tmp/test.csv', csv_data)
+
+      visit main_app.admin_product_import_path
+      attach_file 'file', '/tmp/test.csv'
+      click_button 'Import'
+
+      expect(page).to have_selector '.item-count', text: "2"
+      expect(page).to have_selector '.invalid-count', text: "0"
+      expect(page).to have_selector '.create-count', text: "1"
+      expect(page).to have_selector '.update-count', text: "1"
+
+      click_button 'Save'
+      expect(page).to have_content "Products created: 1"
+      expect(page).to have_content "Products updated: 1"
+
+      added_coffee = Spree::Variant.find_by_display_name('Emergent Coffee')
+      added_coffee.product.name.should == 'Hypothetical Cake'
+      added_coffee.price.should == 3.50
+      added_coffee.on_hand.should == 6
+
+      updated_banana = Spree::Variant.find_by_display_name('Preexisting Banana')
+      updated_banana.product.name.should == 'Hypothetical Cake'
+      updated_banana.price.should == 5.50
+      updated_banana.on_hand.should == 5
+    end
+  end
+
+  describe "when dealing with uploaded files" do
+    before { quick_login_as_admin }
+
+    it "checks filetype on upload" do
+      File.write('/tmp/test.txt', "Wrong filetype!")
+
+      visit main_app.admin_product_import_path
+      attach_file 'file', '/tmp/test.txt'
+      click_button 'Import'
+
+      expect(page).to have_content "Importer could not process file: invalid filetype"
+      expect(page).to_not have_selector 'input[type=submit][value="Save"]'
+      expect(page).to have_content "Select a spreadsheet to upload"
+      File.delete('/tmp/test.txt')
+    end
+
+    it "returns and error if nothing was uploaded" do
+      visit main_app.admin_product_import_path
+      click_button 'Import'
+
+      expect(page).to have_content "File not found or could not be opened"
+    end
+
+    it "handles cases where no meaningful data can be read from the file" do
+      File.write('/tmp/test.csv', "A22££S\\\\\n**VA,,,AF..D")
+
+      visit main_app.admin_product_import_path
+      attach_file 'file', '/tmp/test.csv'
+      click_button 'Import'
+
+      expect(page).to have_selector '.create-count', text: "0"
+      expect(page).to have_selector '.update-count', text: "0"
+      expect(page).to_not have_selector 'input[type=submit][value="Save"]'
+      File.delete('/tmp/test.csv')
     end
   end
 
