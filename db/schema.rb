c39d4377e0e5c560fc5d16532f260b717c97c13b
stveep <spettitt@gmail.com>
2017-03-05 23:55:29 +0000

    New schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20170225203658) do
+ActiveRecord::Schema.define(:version => 20170304151129) do
 
   create_table "account_invoices", :force => true do |t|
     t.integer  "user_id",    :null => false
