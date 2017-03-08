032680eebfee3ee374d51b61ce88b63b0d30fe61
Lynne Davis <lynne@deanforestfoodhub.org.uk>
2017-03-08 10:10:08 +0000

    remove binding.pry

--- config/initializers/user_class_extensions.rb
+++ config/initializers/user_class_extensions.rb
@@ -4,7 +4,6 @@ Spree::Core::Engine.config.to_prepare do
 
       # Override of spree method to ignore orders associated with account_invoices
       def last_incomplete_spree_order_with_ignoring_account_invoices
-        binding.pry
         account_invoice_order_ids = account_invoices.map(&:order_id)
         return last_incomplete_spree_order_without_ignoring_account_invoices if account_invoice_order_ids.empty?
         spree_orders.incomplete.where("id NOT IN (?)", account_invoice_order_ids).order('created_at DESC').first
