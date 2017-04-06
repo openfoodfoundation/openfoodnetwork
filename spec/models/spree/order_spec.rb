e854eb0426b9cc2a1965c52a03f0f6c04ff0ea6f
Pierre de Lacroix <pierre@pdelacroix.com>
2017-04-06 10:57:10 +0200

    add tests
    for methods Spree::Order#tax_adjustment_totals
    and Spree::Adjustment#find_closest_tax_rate_from_included_tax

--- spec/models/spree/order_spec.rb
+++ spec/models/spree/order_spec.rb
@@ -276,6 +276,50 @@ describe Spree::Order do
     end
   end
 
+  describe "getting a hash of all taxes" do
+    let(:zone)            { create(:zone_with_member) }
+    let(:coordinator)     { create(:distributor_enterprise, charges_sales_tax: true) }
+
+    let(:tax_rate10)      { create(:tax_rate, included_in_price: true, calculator: Spree::Calculator::DefaultTax.new, amount: 0.1, zone: zone) }
+    let(:tax_rate15)      { create(:tax_rate, included_in_price: true, calculator: Spree::Calculator::DefaultTax.new, amount: 0.15, zone: zone) }
+    let(:tax_rate20)      { create(:tax_rate, included_in_price: true, calculator: Spree::Calculator::DefaultTax.new, amount: 0.2, zone: zone) }
+    let(:tax_category10)  { create(:tax_category, tax_rates: [tax_rate10]) }
+    let(:tax_category15)  { create(:tax_category, tax_rates: [tax_rate15]) }
+    let(:tax_category20)  { create(:tax_category, tax_rates: [tax_rate20]) }
+
+    let(:variant)         { create(:variant, product: create(:product, tax_category: tax_category10)) }
+    let(:shipping_method) { create(:shipping_method, calculator: Spree::Calculator::FlatRate.new(preferred_amount: 46.0)) }
+    let(:enterprise_fee)  { create(:enterprise_fee, enterprise: coordinator, tax_category: tax_category20, calculator: Spree::Calculator::FlatRate.new(preferred_amount: 48.0)) }
+
+    let(:order_cycle)     { create(:simple_order_cycle, coordinator: coordinator, coordinator_fees: [enterprise_fee], distributors: [coordinator], variants: [variant]) }
+    let!(:order)          { create(:order, shipping_method: shipping_method, bill_address: create(:address), order_cycle: order_cycle, distributor: coordinator) }
+    let!(:line_item)      { create(:line_item, order: order, variant: variant, price: 44.0) }
+
+    before do
+      Spree::Config.shipment_inc_vat = true
+      Spree::Config.shipping_tax_rate = tax_rate15.amount
+      order.create_shipment!
+      Spree::TaxRate.adjust(order)
+      order.reload.update_distribution_charge!
+    end
+
+    it "returns a hash with all 3 taxes" do
+      order.tax_adjustment_totals.size.should == 3
+    end
+
+    it "contains tax on line_item" do
+      order.tax_adjustment_totals[tax_rate10.amount].should == 4.0
+    end
+
+    it "contains tax on shipping_fee" do
+      order.tax_adjustment_totals[tax_rate15.amount].should == 6.0
+    end
+
+    it "contains tax on enterprise_fee" do
+      order.tax_adjustment_totals[tax_rate20.amount].should == 8.0
+    end
+  end
+
   describe "setting the distributor" do
     it "sets the distributor when no order cycle is set" do
       d = create(:distributor_enterprise)
