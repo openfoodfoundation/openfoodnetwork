fdca1f660676fb883472c024c9464be93c055d83
Rob Harrington <oeoeaio@gmail.com>
2017-05-31 14:40:15 +1000

    Adding message about having no saved cards to account/cards UI

--- spec/features/consumer/account/cards_spec.rb
+++ spec/features/consumer/account/cards_spec.rb
@@ -4,10 +4,16 @@ feature "Credit Cards", js: true do
   include AuthenticationWorkflow
   describe "as a logged in user" do
     let(:user) { create(:user) }
-    let!(:card) { create(:credit_card, user_id: user.id) }
+    let!(:card) { create(:credit_card, user_id: user.id, gateway_customer_profile_id: 'cus_AZNMJ') }
 
     before do
       quick_login_as user
+
+      stub_request(:get, "https://api.stripe.com/v1/customers/cus_AZNMJ").
+      to_return(:status => 200, :body => JSON.generate({id: "cus_AZNMJ"}))
+
+      stub_request(:delete, "https://api.stripe.com/v1/customers/cus_AZNMJ").
+      to_return(:status => 200, :body => JSON.generate({deleted: true, id: "cus_AZNMJ"}))
     end
 
     it "lists saved cards, shows interface for adding new cards" do
@@ -22,10 +28,17 @@ feature "Credit Cards", js: true do
         expect(page).to have_content card.last_digits
       end
 
+      # Shows the interface for adding a card
       click_button I18n.t(:add_a_card)
       expect(page).to have_field 'first_name'
       expect(page).to have_field 'card_number'
       expect(page).to have_field 'card_month'
+
+      # Allows deletion of cards
+      click_link I18n.t(:delete)
+
+      expect(page).to have_content I18n.t(:card_has_been_removed, number: "x-#{card.last_digits}")
+      expect(page).to have_content I18n.t(:you_have_no_saved_cards)
     end
   end
 end
