b2bae242d930bba40396df4b83e14a2c897594b5
Duende13 <susana.r.cunado@gmail.com>
2017-06-21 22:03:42 +1000

    Sorting variants by name_to_display and unit_value. Issue 1604.

--- app/serializers/api/variant_serializer.rb
+++ app/serializers/api/variant_serializer.rb
@@ -1,5 +1,5 @@
 class Api::VariantSerializer < ActiveModel::Serializer
-  attributes :id, :is_master, :count_on_hand, :name_to_display, :unit_to_display
+  attributes :id, :is_master, :count_on_hand, :name_to_display, :unit_to_display, :unit_value
   attributes :options_text, :on_demand, :price, :fees, :price_with_fees, :product_name
 
   def price
