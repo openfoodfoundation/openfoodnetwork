9cb40b3037fd2149e90e3dd7ab23f634fe0276f5
Enrico Stano <ocirne.onats@gmail.com>
2017-06-26 13:35:24 +0200

    Restore checkout flow override without :payment state

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -23,6 +23,13 @@ Spree::Order.class_eval do
 
   before_save :update_shipping_fees!, if: :complete?
   before_save :update_payment_fees!, if: :complete?
+  checkout_flow do
+    go_to_state :address
+    go_to_state :delivery
+    go_to_state :confirm, :if => lambda { |order| order.confirmation_required? }
+    go_to_state :complete
+    remove_transition :from => :delivery, :to => :confirm
+  end
 
   # -- Scopes
   scope :managed_by, lambda { |user|
