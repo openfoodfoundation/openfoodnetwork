6689eb94450f37a901aacf48afa26fd5c0a23dc8
Rob Harrington <oeoeaio@gmail.com>
2017-07-07 15:33:40 +1000

    Disable rubocop Style/PredicateName check against override of Spree method

--- app/models/spree/credit_card_decorator.rb
+++ app/models/spree/credit_card_decorator.rb
@@ -7,7 +7,7 @@ Spree::CreditCard.class_eval do
   # Should be able to remove once we reach Spree v2.2.0
   # Commit: https://github.com/spree/spree/commit/5a4d690ebc64b264bf12904a70187e7a8735ef3f
   # See also: https://github.com/spree/spree_gateway/issues/111
-  def has_payment_profile?
+  def has_payment_profile? # rubocop:disable Style/PredicateName
     gateway_customer_profile_id.present? || gateway_payment_profile_id.present?
   end
 end
