8dbb968748c3152c8bca7aa8ab7ecba986b08433
Pierre de Lacroix <pierre@pdelacroix.com>
2017-07-14 12:25:34 +0200

    Create user confirmation email

--- app/mailers/spree/user_mailer_decorator.rb
+++ app/mailers/spree/user_mailer_decorator.rb
@@ -1,7 +1,17 @@
 Spree::UserMailer.class_eval do
+
   def signup_confirmation(user)
     @user = user
     mail(:to => user.email, :from => from_address,
          :subject => t(:welcome_to) + Spree::Config[:site_name])
+  end
+
+  def confirmation_instructions(user, token)
+    @user = user
+    @token = token
+    subject = t('spree.user_mailer.confirmation_instructions.subject')
+    mail(to: user.email,
+         from: from_address,
+         subject: subject)
   end
 end
