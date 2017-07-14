69ea1e6d26b8bfa4c717789a67b6ef22c62d4d6c
Matt-Yorkley <visible_souls@hotmail.com>
2017-07-14 18:23:46 +0100

    Set test users to confirmed by default

--- spec/factories.rb
+++ spec/factories.rb
@@ -430,12 +430,18 @@ FactoryGirl.modify do
   end
 
   factory :user do
+    confirmation_sent_at '1970-01-01 00:00:00'
+    confirmed_at '1970-01-01 00:00:01'
+
     after(:create) do |user|
       user.spree_roles.clear # Remove admin role
     end
   end
 
   factory :admin_user do
+    confirmation_sent_at '1970-01-01 00:00:00'
+    confirmed_at '1970-01-01 00:00:01'
+
     after(:create) do |user|
       user.spree_roles << Spree::Role.find_or_create_by_name!('admin')
     end
