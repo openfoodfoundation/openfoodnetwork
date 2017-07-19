bc62ad5797f939c06d89dc9d233abdf16039d8f9
Enrico Stano <ocirne.onats@gmail.com>
2017-07-19 18:32:23 +0200

    Fix spec

--- spec/helpers/spree/admin/base_helper_spec.rb
+++ spec/helpers/spree/admin/base_helper_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
 describe Spree::BaseHelper, type: :helper do
-  describe "#link_to_remove_fields_without_url" do
+  describe "#link_to_remove_fields" do
     let(:name) { 'Hola' }
     let(:form) { double('form_for', hidden_field: '<input type="hidden" name="_method" value="destroy">') }
     let(:options) { {} }
 
-    subject { helper.link_to_remove_fields_without_url(name, form, options) }
+    subject { helper.link_to_remove_fields(name, form, options) }
 
     it 'returns an `a` tag followed by a hidden `input` tag' do
-      expect(subject).to eq("<a class=\"remove_fields  icon_link with-tip icon-trash\" data-action=\"remove\" title=\"Remove\"><span class='text'>Hola</span></a><input type=\"hidden\" name=\"_method\" value=\"destroy\">")
+      expect(subject).to eq("<a href=\"#\" class=\"remove_fields  icon_link with-tip icon-trash\" data-action=\"remove\" title=\"Remove\"><span class='text'>Hola</span></a>&lt;input type=&quot;hidden&quot; name=&quot;_method&quot; value=&quot;destroy&quot;&gt;")
     end
   end
 end
