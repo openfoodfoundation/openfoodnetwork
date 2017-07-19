3b8cab9392f9f9c3ecf7ac59ce5b06d81f0e3c29
Enrico Stano <ocirne.onats@gmail.com>
2017-07-19 18:27:33 +0200

    Revert "Fix removal of producer properties - remove href attr so Spree doesn't attempt an XHR"
    This reverts commit 8126161df2b96f9cec5c34ffaefa093fe5aed858.

--- app/helpers/spree/admin/base_helper_decorator.rb
+++ app/helpers/spree/admin/base_helper_decorator.rb
@@ -22,17 +22,6 @@ module Spree
 
         link_to_with_icon('icon-trash', name, '#', html_options) + f.hidden_field(:_destroy)
       end
-
-      def link_to_remove_fields_without_url(name, f, options = {})
-        name = '' if options[:no_text]
-        options[:class] = '' unless options[:class]
-        options[:class] += 'no-text with-tip' if options[:no_text]
-
-        html_options = {class: "remove_fields #{options[:class]}", data: {action: 'remove'}, title: t(:remove)}
-        html_options.merge!(options[:html]) if options.key? :html
-
-        link_to_with_icon('icon-trash', name, '#', html_options).gsub('href="#" ', '') + f.hidden_field(:_destroy)
-      end
     end
   end
 end
