98317e96d0e21cfee14fed23186752597849104a
Rob Harrington <oeoeaio@gmail.com>
2017-08-03 16:35:09 +1000

    Remove unused stripe action abilities

--- app/models/spree/ability_decorator.rb
+++ app/models/spree/ability_decorator.rb
@@ -119,9 +119,6 @@ class AbilityDecorator
       column_preference.user == user
     end
 
-    # Not sure how to restrict these methods to a non-resource Controller
-    can [:stripe_connect, :stripe_connect_callback, :stripe_disconnect], :all
-
     can [:status, :destroy], StripeAccount do |stripe_account|
       user.enterprises.include? stripe_account.enterprise
     end
