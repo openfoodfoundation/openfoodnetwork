3131f8270ba9b96ee7a63164ce32dc098b68a90f
Matt-Yorkley <visible_souls@hotmail.com>
2017-08-07 12:41:05 +0100

    WIP uk fixes

--- config/application.rb
+++ config/application.rb
@@ -70,10 +70,9 @@ module Openfoodnetwork
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = ENV["DEFAULT_LOCALE"] || "en"
-    config.i18n.locale = ENV["LOCALE"] || "en"
+    config.i18n.default_locale = ENV["LOCALE"] || ENV["I18N_LOCALE"] || "en"
     config.i18n.available_locales = ENV["AVAILABLE_LOCALES"].andand.split(',').andand.map(&:strip) || [config.i18n.default_locale]
-    I18n.locale = config.i18n.locale
+    I18n.locale = config.i18n.locale = config.i18n.default_locale
 
     # Setting this to true causes a performance regression in Rails 3.2.17
     # When we're on a version with the fix below, we can set it to true
