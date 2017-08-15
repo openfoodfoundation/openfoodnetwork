179cf6e48497ed3a493f61ad5a840c8c9f388722
Matt-Yorkley <visible_souls@hotmail.com>
2017-08-15 12:47:11 +0100

    Tidy up confirmation migrations

--- db/migrate/20170717105219_copy_confirmed_at_from_enterprises_to_spree_users.rb
+++
@@ -1,5 +0,0 @@
-class CopyConfirmedAtFromEnterprisesToSpreeUsers < ActiveRecord::Migration
-  def up
-    execute "UPDATE spree_users SET confirmed_at = enterprises.confirmed_at FROM enterprises WHERE spree_users.email = enterprises.email AND enterprises.confirmed_at IS NOT NULL"
-  end
-end
