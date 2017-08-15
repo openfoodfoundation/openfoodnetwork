582fc2a0519c4ea62f7f0b90a7c9e18161154fe1
Matt-Yorkley <visible_souls@hotmail.com>
2017-08-15 12:47:11 +0100

    Tidy up confirmation migrations

--- db/migrate/20170710145821_add_confirmable_to_user.rb
+++ db/migrate/20170710145821_add_confirmable_to_user.rb
@@ -5,6 +5,9 @@ class AddConfirmableToUser < ActiveRecord::Migration
     add_column :spree_users, :confirmation_sent_at, :datetime
     add_column :spree_users, :unconfirmed_email, :string
     add_index :spree_users, :confirmation_token, :unique => true
+
+    # Set users to confirmed if they previously confirmed their email for an enterprise
+    execute "UPDATE spree_users SET confirmed_at = enterprises.confirmed_at FROM enterprises WHERE spree_users.email = enterprises.email AND enterprises.confirmed_at IS NOT NULL"
   end
 
   def down
