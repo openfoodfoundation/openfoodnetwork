6a4b296c29119875c32bc1091594919a4df0d591
Maikel Linke <mkllnk@web.de>
2017-08-18 15:05:34 +1000

    Simplify conditionals

--- app/controllers/user_confirmations_controller.rb
+++ app/controllers/user_confirmations_controller.rb
@@ -10,10 +10,12 @@ class UserConfirmationsController < DeviseController
   def create
     self.resource = resource_class.send_confirmation_instructions(resource_params)
 
-    if successfully_sent?(resource) && is_navigational_format?
-      set_flash_message(:success, :confirmation_sent)
-    elsif is_navigational_format?
-      set_flash_message(:error, :confirmation_not_sent)
+    if is_navigational_format?
+      if successfully_sent?(resource)
+        set_flash_message(:success, :confirmation_sent)
+      else
+        set_flash_message(:error, :confirmation_not_sent)
+      end
     end
 
     respond_with_navigational(resource){ redirect_to login_path }
@@ -23,10 +25,12 @@ class UserConfirmationsController < DeviseController
   def show
     self.resource = resource_class.confirm_by_token(params[:confirmation_token])
 
-    if resource.errors.empty? && is_navigational_format?
-      set_flash_message(:success, :confirmed)
-    elsif is_navigational_format?
-      set_flash_message(:error, :not_confirmed)
+    if is_navigational_format?
+      if resource.errors.empty?
+        set_flash_message(:success, :confirmed)
+      else
+        set_flash_message(:error, :not_confirmed)
+      end
     end
 
     respond_with_navigational(resource){ redirect_to login_path }
