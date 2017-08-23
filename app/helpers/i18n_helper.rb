39dc765c63bf0e47a3526aa0b189533040d4f6e1
Maikel Linke <mkllnk@web.de>
2017-08-23 18:30:08 +1000

    Add/replace comments and remove useless

--- app/helpers/i18n_helper.rb
+++ app/helpers/i18n_helper.rb
@@ -1,13 +1,12 @@
 module I18nHelper
-  private
-
   def set_locale
-    # update spree_current_user locale if logged in and set cookie locale if params
+    # Save a given locale
     if params[:locale] && Rails.application.config.i18n.available_locales.include?(params[:locale])
       spree_current_user.update_attributes!(locale: params[:locale]) if spree_current_user
       cookies[:locale] = params[:locale]
     end
 
+    # After logging in, check if the user chose a locale before
     if spree_current_user && spree_current_user.locale.nil? && cookies[:locale]
       spree_current_user.update_attributes!(locale: params[:locale])
     end
