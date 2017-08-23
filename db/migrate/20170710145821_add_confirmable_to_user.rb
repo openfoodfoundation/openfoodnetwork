aead867ea0d87d97a784c86592321b4a56014b45
Matt-Yorkley <visible_souls@hotmail.com>
2017-08-23 14:35:56 +0100

    Set all current users to confirmed

--- db/migrate/20170710145821_add_confirmable_to_user.rb
+++ db/migrate/20170710145821_add_confirmable_to_user.rb
@@ -6,8 +6,8 @@ class AddConfirmableToUser < ActiveRecord::Migration
     add_column :spree_users, :unconfirmed_email, :string
     add_index :spree_users, :confirmation_token, :unique => true
 
-    # Set users to confirmed if they previously confirmed their email for an enterprise
-    execute "UPDATE spree_users SET confirmed_at = enterprises.confirmed_at FROM enterprises WHERE spree_users.email = enterprises.email AND enterprises.confirmed_at IS NOT NULL"
+    # Set all current users to confirmed
+    Spree::User.update_all(confirmed_at: Time.zone.now)
   end
 
   def down
