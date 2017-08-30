746c2ffef98a67581ad09d776b1ca9429b50c9c1
Rob Harrington <oeoeaio@gmail.com>
2017-08-30 11:49:16 +1000

    Add 'publishable_key' property to Stripe module (enables stubbing)

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,13 @@
+# Add the :publishable_key property, to allow us to access this
+# property from the object, rather than calling from ENV directly.
+# This is mostly useful for stubbing when testing, but also feels
+# a bit cleaner than accessing keys in different ways.
+module Stripe
+  class << self
+    attr_accessor :publishable_key
+  end
+end
+
 Stripe.api_key = ENV['STRIPE_INSTANCE_SECRET_KEY']
+Stripe.publishable_key = ENV['STRIPE_INSTANCE_PUBLISHABLE_KEY']
 Stripe.client_id = ENV['STRIPE_CLIENT_ID']
