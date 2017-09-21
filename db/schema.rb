074018d2a6c52da8255962fc4d7a49e4b352eab6
Rob Harrington <oeoeaio@gmail.com>
2017-09-21 16:59:28 +1000

    Copy and run adjustment states migration from updated version of spree
    Note: this migration took 28.7571s to run against Aus production data on my local machine

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20170512115519) do
+ActiveRecord::Schema.define(:version => 20170921065259) do
 
   create_table "account_invoices", :force => true do |t|
     t.integer  "user_id",    :null => false
