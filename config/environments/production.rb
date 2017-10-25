fff9b35ca38ffa85fd7ae84196f0dc2f6677c26d
Pau Perez <saulopefa@gmail.com>
2017-10-25 13:13:46 +0200

    Lower log level in prod to :info so we can debug
    Otherwise, there are no log lines for any request, which makes it
    impossible to find out anything about the app in production.
    
    Obviously this increases the size of the log files but this has to be
      dealt with log rotation. The data is our most important asset.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ Openfoodnetwork::Application.configure do
   config.force_ssl = true
 
   # See everything in the log (default is :info)
-  config.log_level = :warn
+  config.log_level = :info
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
