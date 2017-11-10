dfe874cf3fa72789ec126f4de9dea4be6ddb9ef6
Rob Harrington <oeoeaio@gmail.com>
2017-11-10 11:36:19 +1100

    Attempt to add a credit card before processing payment

--- lib/open_food_network/standing_order_payment_updater.rb
+++ lib/open_food_network/standing_order_payment_updater.rb
@@ -1,12 +1,16 @@
 module OpenFoodNetwork
   class StandingOrderPaymentUpdater
-
     def initialize(order)
       @order = order
     end
 
     def update!
-      return unless payment
+      return if payment.blank?
+
+      if card_required? && !card_set?
+        return unless ensure_credit_card
+      end
+
       payment.update_attributes(amount: @order.outstanding_balance)
     end
 
@@ -14,6 +18,23 @@ module OpenFoodNetwork
 
     def payment
       @payment ||= @order.pending_payments.last
+    end
+
+    def card_required?
+      payment.payment_method.is_a? Spree::Gateway::StripeConnect
+    end
+
+    def card_set?
+      payment.source is_a? Spree::CreditCard
+    end
+
+    def ensure_credit_card
+      return false unless saved_credit_card.present?
+      payment.update_attributes(source: saved_credit_card)
+    end
+
+    def saved_credit_card
+      @order.standing_order.credit_card
     end
   end
 end
