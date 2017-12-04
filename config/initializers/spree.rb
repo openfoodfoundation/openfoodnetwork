openfoodfoundation/openfoodnetwork

commit a87c89c83db9e54c58ea43e5b273d2906669c624

Require authentication for API endpoints
Authentication and authorization layers (cancan) should not be mixed
https://github.com/openfoodfoundation/spree/commit/c594d6111b0c7e4fafe03b548748049bf5a7b28a

config/initializers/spree.rb
@@ -13,6 +13,8 @@ require 'spree/core/calculated_adjustments_decorator'
 require "#{Rails.root}/app/models/spree/payment_method_decorator"
 require "#{Rails.root}/app/models/spree/gateway_decorator"
 
+Spree::Api::Config[:requires_authentication] = true
+
 Spree.config do |config|
   config.shipping_instructions = true
   config.address_requires_state = true

commit f8b3e49817e188b0af87f0eac2ed65574cd095cc

Disable api auth as there is no Spree api key set
Although Spree::Api::Config[:requires_authentication] is set to false by
default for some unknown reason if not done explicitly Spree still
returns it as false.

This amends the change done in a87c89c83db9e54c58ea43e5b273d2906669c624,
which introduced the bug. As there is no Spree api key set the auth
fails when getting taxons.

config/initializers/spree.rb
@@ -12,7 +12,7 @@ require 'spree/core/calculated_adjustments_decorator'
 require "#{Rails.root}/app/models/spree/payment_method_decorator"
 require "#{Rails.root}/app/models/spree/gateway_decorator"
 
-Spree::Api::Config[:requires_authentication] = true
+Spree::Api::Config[:requires_authentication] = false
 
 Spree.config do |config|
   config.shipping_instructions = true

spec/controllers/spree/api/products_controller_spec.rb
@@ -15,8 +15,7 @@ module Spree
     let(:attributes) { [:id, :name, :supplier, :price, :on_hand, :available_on, :permalink_live] }
 
     before do
-      stub_authentication!
-      Spree.user_class.stub :find_by_spree_api_key => current_api_user
+      allow(controller).to receive(:spree_current_user) { current_api_user }
     end
 
     context "as a normal user" do
@@ -109,14 +108,11 @@ module Spree
     end
 
     describe '#clone' do
-      before do
-        spree_post :clone, product_id: product1.id, format: :json
-      end
-
       context 'as a normal user' do
         sign_in_as_user!
 
         it 'denies access' do
+          spree_post :clone, product_id: product1.id, format: :json
           assert_unauthorized!
         end
       end
@@ -125,10 +121,12 @@ module Spree
         sign_in_as_enterprise_user! [:supplier]
 
         it 'responds with a successful response' do
+          spree_post :clone, product_id: product1.id, format: :json
           expect(response.status).to eq(201)
         end
 
         it 'clones the product' do
+          spree_post :clone, product_id: product1.id, format: :json
           expect(json_response['name']).to eq("COPY OF #{product1.name}")
         end
       end
@@ -137,10 +135,12 @@ module Spree
         sign_in_as_admin!
 
         it 'responds with a successful response' do
+          spree_post :clone, product_id: product1.id, format: :json
           expect(response.status).to eq(201)
         end
 
         it 'clones the product' do
+          spree_post :clone, product_id: product1.id, format: :json
           expect(json_response['name']).to eq("COPY OF #{product1.name}")
         end
       end