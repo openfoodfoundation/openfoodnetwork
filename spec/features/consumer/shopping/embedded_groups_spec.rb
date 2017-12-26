5c17e4c94f15f5c45f52ea24523043e8807fa683
Keir Osborn <keir.osborn@gmail.com>
2017-12-26 21:49:41 +0000

    remove contact details section from embedded groups

--- spec/features/consumer/shopping/embedded_groups_spec.rb
+++ spec/features/consumer/shopping/embedded_groups_spec.rb
@@ -8,7 +8,6 @@ feature "Using embedded shopfront functionality", js: true do
 	let(:enterprise) { create(:distributor_enterprise) }
   	let!(:group) { create(:enterprise_group, enterprises: [enterprise], permalink: 'group1', on_front_page: true) }
 
-
   	before do
   	  Spree::Config[:enable_embedded_shopfronts] = true
       Spree::Config[:embedded_shopfronts_whitelist] = 'localhost'
@@ -28,6 +27,18 @@ feature "Using embedded shopfront functionality", js: true do
 	  	end
 	  end	  
 	end
+
+	it "doesn't display contact details when embedded" do
+	  expect(page).to have_selector 'iframe#group_test_iframe'
+
+	  within_frame 'group_test_iframe' do
+	    within 'div#group-page' do
+          expect(page).to have_no_selector 'div.contact-container'
+          expect(page).to have_no_content '#{group.address.address1}'
+	  	end
+	  end
+	end
+
   
   end
 
