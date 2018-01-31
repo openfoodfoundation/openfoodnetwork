c79641c77c3dedcbc79feb3b0c12973f614060da
Pau Perez <saulopefa@gmail.com>
2018-01-31 15:23:31 +0100

    Fix typos in documentation

--- app/jobs/order_cycle_notification_job.rb
+++ app/jobs/order_cycle_notification_job.rb
@@ -1,4 +1,4 @@
-# Deliers an email with a report of the order cycle to each of its suppliers
+# Delivers an email with a report of the order cycle to each of its suppliers
 OrderCycleNotificationJob = Struct.new(:order_cycle_id) do
   def perform
     order_cycle = OrderCycle.find(order_cycle_id)
