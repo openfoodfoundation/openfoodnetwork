ba66c0e862b20c9c1adf7d37400b52753b3531e5
Rob Harrington <oeoeaio@gmail.com>
2018-02-02 17:22:02 +1100

    Replace references to 'standing order' with 'subscription' (schedule)

--- config/schedule.rb
+++ config/schedule.rb
@@ -28,8 +28,8 @@ end
 
 every 5.minutes do
   enqueue_job 'HeartbeatJob', priority: 0
-  enqueue_job 'StandingOrderPlacementJob', priority: 0
-  enqueue_job 'StandingOrderConfirmJob', priority: 0
+  enqueue_job 'SubscriptionPlacementJob', priority: 0
+  enqueue_job 'SubscriptionConfirmJob', priority: 0
 end
 
 every 1.day, at: '1:00am' do
