49c86c86fef0e9e971f65cf8da84e205318aead6
yasirazgar <yasirazgar@gmail.com>
2018-03-04 22:19:59 +0530

    add creator association to distributor shipping and payment models

--- app/models/distributor_shipping_method.rb
+++ app/models/distributor_shipping_method.rb
@@ -2,4 +2,5 @@ class DistributorShippingMethod < ActiveRecord::Base
   self.table_name = "distributors_shipping_methods" 
   belongs_to :shipping_method, class_name: Spree::ShippingMethod
   belongs_to :distributor, class_name: Enterprise, touch: true
+  belongs_to :creator, class_name: Spree::User
 end
