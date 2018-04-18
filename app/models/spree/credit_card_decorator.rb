d146d3714f070c403dd19ba23fa26b5f38ad9394
Rob Harrington <oeoeaio@gmail.com>
2018-04-18 17:00:47 +1000

    Add callbacks to ensure a user always has a default credit card
    (if any exist)

--- app/models/spree/credit_card_decorator.rb
+++ app/models/spree/credit_card_decorator.rb
@@ -5,7 +5,7 @@ Spree::CreditCard.class_eval do
   attr_accessible :cc_type, :last_digits
 
   # For holding customer preference in memory
-  attr_accessible :save_requested_by_customer
+  attr_accessible :save_requested_by_customer, :is_default
   attr_writer :save_requested_by_customer
 
   # Should be able to remove once we reach Spree v2.2.0
@@ -13,6 +13,9 @@ Spree::CreditCard.class_eval do
   belongs_to :payment_method
 
   belongs_to :user
+
+  after_create :ensure_default
+  after_save :ensure_default, if: :is_default_changed?
 
   # Allows us to use a gateway_payment_profile_id to store Stripe Tokens
   # Should be able to remove once we reach Spree v2.2.0
@@ -24,5 +27,18 @@ Spree::CreditCard.class_eval do
 
   def save_requested_by_customer?
     !!@save_requested_by_customer
+  end
+
+  private
+
+  def default_missing?
+    user.credit_cards.where(is_default: true).none?
+  end
+
+  def ensure_default
+    return unless user
+    return unless is_default? || default_missing?
+    user.credit_cards.update_all(['is_default=(id=?)', id])
+    self.is_default = true
   end
 end
