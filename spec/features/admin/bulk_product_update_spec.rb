c6a383aafd041d39d09284bd24ae29715b70403d
Rob Harrington <oeoeaio@gmail.com>
2018-05-31 16:55:50 +1000

    Update bulk_product_update_spec to work with Selenium

--- spec/features/admin/bulk_product_update_spec.rb
+++ spec/features/admin/bulk_product_update_spec.rb
@@ -74,7 +74,7 @@ feature %q{
 
       visit spree.admin_products_path
       expect(page).to have_selector "a.view-variants", count: 1
-      find("a.view-variants").trigger('click')
+      find("a.view-variants").click
 
       expect(page).to have_no_selector "span[name='on_hand']", text: "On demand", visible: true
       expect(page).to     have_field "variant_on_hand", with: "4"
@@ -111,7 +111,7 @@ feature %q{
 
       visit spree.admin_products_path
       expect(page).to have_selector "a.view-variants", count: 2
-      all("a.view-variants").each { |e| e.trigger('click') }
+      all("a.view-variants").each { |e| e.click }
 
       expect(page).to have_field "product_name", with: v1.product.name
       expect(page).to have_field "product_name", with: v2.product.name
@@ -126,7 +126,7 @@ feature %q{
 
       visit spree.admin_products_path
       expect(page).to have_selector "a.view-variants", count: 1
-      all("a.view-variants").each { |e| e.trigger('click') }
+      all("a.view-variants").each { |e| e.click }
 
       expect(page).to have_selector "span[name='on_hand']", text: p1.variants.sum{ |v| v.on_hand }.to_s
       expect(page).to have_field "variant_on_hand", with: "15"
@@ -141,7 +141,7 @@ feature %q{
 
       visit spree.admin_products_path
       expect(page).to have_selector "a.view-variants", count: 1
-      all("a.view-variants").each { |e| e.trigger('click') }
+      all("a.view-variants").each { |e| e.click }
 
       expect(page).to have_field "price", with: "2.0", visible: false
       expect(page).to have_field "variant_price", with: "12.75"
@@ -155,7 +155,7 @@ feature %q{
 
       visit spree.admin_products_path
       expect(page).to have_selector "a.view-variants", count: 1
-      all("a.view-variants").each { |e| e.trigger('click') }
+      all("a.view-variants").each { |e| e.click }
 
       expect(page).to have_field "variant_unit_value_with_description", with: "1.2 (small bag)"
       expect(page).to have_field "variant_unit_value_with_description", with: "4.8 (large bag)"
@@ -197,18 +197,20 @@ feature %q{
     visit spree.admin_products_path
 
     # I should see an add variant button
-    page.find('a.view-variants').trigger('click')
+    page.find('a.view-variants').click
 
     # When I add three variants
-    page.find('a.add-variant', visible: true).trigger('click')
-    page.find('a.add-variant', visible: true).trigger('click')
+    page.find('a.add-variant', visible: true).click
+    page.find('a.add-variant', visible: true).click
 
     # They should be added, and should not see edit buttons for new variants
     expect(page).to have_selector "tr.variant", count: 3
     expect(page).to have_selector "a.edit-variant", count: 1
 
     # When I remove two, they should be removed
-    page.all('a.delete-variant', visible: true).first.click
+    accept_alert do
+      page.all('a.delete-variant', visible: true).first.click
+    end
     expect(page).to have_selector "tr.variant", count: 2
     page.all('a.delete-variant', visible: true).first.click
     expect(page).to have_selector "tr.variant", count: 1
@@ -310,7 +312,7 @@ feature %q{
     quick_login_as_admin
     visit spree.admin_products_path
     expect(page).to have_selector "a.view-variants", count: 1
-    find("a.view-variants").trigger('click')
+    find("a.view-variants").click
 
     toggle_columns "SKU"
 
@@ -346,7 +348,7 @@ feature %q{
     quick_login_as_admin
     visit spree.admin_products_path
     expect(page).to have_selector "a.view-variants", count: 1
-    find("a.view-variants").trigger('click')
+    find("a.view-variants").click
 
     expect(page).to have_field "variant_price", with: "3.0"
 
@@ -458,7 +460,9 @@ feature %q{
         expect(page).to have_selector "a.delete-product", :count => 2
 
         within "tr#p_#{p1.id}" do
-          find("a.delete-product").click
+          accept_alert do
+            find("a.delete-product").click
+          end
         end
 
         expect(page).to have_selector "a.delete-product", :count => 1
@@ -470,19 +474,21 @@ feature %q{
 
       it "shows a delete button for variants, which deletes the appropriate variant when clicked" do
         expect(page).to have_selector "a.view-variants"
-        all("a.view-variants").each { |e| e.trigger('click') }
+        all("a.view-variants").each { |e| e.click }
 
         expect(page).to have_selector "a.delete-variant", :count => 3
 
         within "tr#v_#{v3.id}" do
-          find("a.delete-variant").click
+          accept_alert do
+            find("a.delete-variant").click
+          end
         end
 
         expect(page).to have_selector "a.delete-variant", :count => 2
 
         visit spree.admin_products_path
         expect(page).to have_selector "a.view-variants"
-        all("a.view-variants").select { |e| e.visible? }.each { |e| e.trigger('click') }
+        all("a.view-variants").select { |e| e.visible? }.each { |e| e.click }
 
         expect(page).to have_selector "a.delete-variant", :count => 2
       end
@@ -511,7 +517,7 @@ feature %q{
 
       it "shows an edit button for variants, which takes the user to the standard edit page for that variant" do
         expect(page).to have_selector "a.view-variants"
-        all("a.view-variants").each { |e| e.trigger('click') }
+        all("a.view-variants").each { |e| e.click }
 
         expect(page).to have_selector "a.edit-variant", :count => 2
 
@@ -701,7 +707,7 @@ feature %q{
         fill_in "available_on", with: (3.days.ago.beginning_of_day).strftime("%F %T")
         select "Weight (kg)", from: "variant_unit_with_scale"
 
-        find("a.view-variants").trigger('click')
+        find("a.view-variants").click
       end
 
       within "#v_#{v.id}" do
@@ -741,7 +747,7 @@ feature %q{
         expect(page).to have_css "img[src='/assets/noimage/mini.png']"
 
         # Click image
-        page.find("a.image-modal").trigger('click')
+        page.find("a.image-modal").click
       end
 
       # Shows upload modal
@@ -766,7 +772,7 @@ feature %q{
         @new_thumb_src = page.find("a.image-modal img")['src']
         expect(@old_thumb_src) != @new_thumb_src
 
-        page.find("a.image-modal").trigger('click')
+        page.find("a.image-modal").click
       end
 
       expect(page).to have_selector "div.reveal-modal.product-image-upload"
