a62d1a6878df79ff6da6e648bea5d341b4e0dce9
Rob Harrington <oeoeaio@gmail.com>
2018-05-31 21:23:49 +1000

    TEEMp

--- spec/support/request/checkout_workflow.rb
+++ spec/support/request/checkout_workflow.rb
@@ -4,15 +4,15 @@ module CheckoutWorkflow
   end
 
   def checkout_as_guest
-    find("button", text: "Checkout as guest").trigger "click"
+    find("button", text: "Checkout as guest").click
   end
 
   def place_order
-    find("button", text: "Place order now").trigger "click"
+    find("button", text: "Place order now").click
   end
 
   def toggle_accordion(id)
-    find("##{id} dd a").trigger "click"
+    find("##{id} dd a").click
   end
 
   def toggle_details
