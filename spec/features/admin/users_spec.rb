6a58deb436b853e5b731c67c76e71877556f5fad
Maikel Linke <maikel@email.org.au>
2018-06-07 15:01:41 +1000

    Add message about unconfirmed email to user form

--- spec/features/admin/users_spec.rb
+++ spec/features/admin/users_spec.rb
@@ -7,6 +7,11 @@ feature "Managing users" do
     before { quick_login_as_admin }
 
     describe "creating a user" do
+      it "shows no confirmation message to start with" do
+        visit spree.new_admin_user_path
+        expect(page).to have_no_text "Email confirmation is pending"
+      end
+
       it "works" do
         visit spree.new_admin_user_path
         fill_in "Email", with: "user1@example.org"
@@ -16,6 +21,7 @@ feature "Managing users" do
           click_button "Create"
         end.to change { Spree::User.count }.by 1
         expect(page).to have_text "Created Successfully"
+        expect(page).to have_text "Email confirmation is pending"
       end
     end
   end
