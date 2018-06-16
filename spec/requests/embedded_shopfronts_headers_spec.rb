ff0e0d9f3d6461caf8f749942f1e6de607779619
Matt-Yorkley <visible_souls@hotmail.com>
2018-06-16 02:49:22 +0100

    Move logic from ApplicationController to service and improve clarity

--- spec/requests/embedded_shopfronts_headers_spec.rb
+++ spec/requests/embedded_shopfronts_headers_spec.rb
@@ -48,7 +48,7 @@ describe "setting response headers for embedded shopfronts", type: :request do
       end
 
       it "allows iframes on certain pages when enabled in configuration" do
-        get shops_path
+        get enterprise_shop_path(enterprise) + '?embedded_shopfront=true'
 
         expect(response.status).to be 200
         expect(response.headers['X-Frame-Options']).to be_nil
@@ -69,7 +69,7 @@ describe "setting response headers for embedded shopfronts", type: :request do
       end
 
       it "matches the URL structure in the header" do
-        get shops_path
+        get enterprise_shop_path(enterprise) + '?embedded_shopfront=true'
 
         expect(response.status).to be 200
         expect(response.headers['X-Frame-Options']).to be_nil
