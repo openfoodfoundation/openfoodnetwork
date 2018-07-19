962c54b606e509e0002f96fbedf23ad0f92e2ebd
Maikel Linke <maikel@email.org.au>
2018-07-19 10:45:33 +1000

    Remove unnecessary expectation
    The `iframe` is rendered in pure HTML and should always be there.

--- spec/support/embedded_pages_helper.rb
+++ spec/support/embedded_pages_helper.rb
@@ -1,8 +1,6 @@
 module OpenFoodNetwork
   module EmbeddedPagesHelper
     def on_embedded_page
-      expect(page).to have_selector "iframe"
-
       within_frame :frame do
         yield
       end
