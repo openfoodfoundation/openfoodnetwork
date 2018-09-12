cc43d789e20fb8ca03e1fb3bb0a59570b2b35b67
Hugo Daniel <hugodaniel.icam@gmail.com>
2018-09-12 14:51:53 +0200

    Refacto edit method for content controller and enable custom user guide link

---
+++ app/models/preference_sections/footer_and_external_links_section.rb
@@ -0,0 +1,23 @@
+module PreferenceSections
+  class FooterAndExternalLinksSection
+    def name
+      I18n.t('admin.contents.edit.footer_and_external_links')
+    end
+
+    def preferences
+      [
+        :footer_logo,
+        :footer_facebook_url,
+        :footer_twitter_url,
+        :footer_instagram_url,
+        :footer_linkedin_url,
+        :footer_googleplus_url,
+        :footer_pinterest_url,
+        :footer_email,
+        :community_forum_url,
+        :footer_links_md,
+        :footer_about_url
+      ]
+    end
+  end
+end
