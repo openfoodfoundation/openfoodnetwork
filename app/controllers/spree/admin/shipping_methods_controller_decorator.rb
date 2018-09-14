238d2f4febf22b5736b19d65b10866a792550d15
luisramos0 <luisramos0@gmail.com>
2018-09-14 22:20:21 +0100

    Removed unnecessary code copied and replaced with call to identical super class

--- app/controllers/spree/admin/shipping_methods_controller_decorator.rb
+++ app/controllers/spree/admin/shipping_methods_controller_decorator.rb
@@ -5,22 +5,10 @@ module Spree
       before_filter :load_hubs, only: [:new, :edit, :create, :update]
 
       # Sort shipping methods by distributor name
-      # ! Code copied from Spree::Admin::ResourceController with two added lines
       def collection
-        return parent.send(controller_name) if parent_data.present?
+        collection = super
+        collection = collection.managed_by(spree_current_user).by_name
 
-        collection = if model_class.respond_to?(:accessible_by) &&
-                         !current_ability.has_block?(params[:action], model_class)
-
-                       model_class.accessible_by(current_ability, action)
-
-                     else
-                       model_class.scoped
-                     end
-
-        collection = collection.managed_by(spree_current_user).by_name # This line added
-
-        # This block added
         if params.key? :enterprise_id
           distributor = Enterprise.find params[:enterprise_id]
           collection = collection.for_distributor(distributor)
