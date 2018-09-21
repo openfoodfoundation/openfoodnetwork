75202a1492a2f39507c05aed8772e0ff05c6f429
Kristina Lim <kristinalim.ph@gmail.com>
2018-09-21 17:52:09 +0800

    Add basic form object for enterprise fee summary

---
+++ lib/order_management/reports/enterprise_fee_summary/parameters.rb
@@ -0,0 +1,46 @@
+module OrderManagement
+  module Reports
+    module EnterpriseFeeSummary
+      class Parameters
+        @i18n_scope = "order_management.reports.enterprise_fee_summary"
+
+        DATE_END_BEFORE_START_ERROR = I18n.t("date_end_before_start_error", scope: @i18n_scope)
+
+        extend ActiveModel::Naming
+        include ActiveModel::Validations
+
+        attr_accessor :start_at, :end_at, :distributor_ids, :producer_ids, :order_cycle_ids,
+                      :enterprise_fee_ids, :shipping_method_ids, :payment_method_ids
+
+        validates :start_at, :end_at, date_time_string: true
+        validates :distributor_ids, :producer_ids, integer_array: true
+        validates :order_cycle_ids, integer_array: true
+        validates :enterprise_fee_ids, integer_array: true
+        validates :shipping_method_ids, :payment_method_ids, integer_array: true
+
+        validate :require_valid_datetime_range
+
+        def initialize(attributes = {})
+          self.distributor_ids = []
+          self.producer_ids = []
+          self.order_cycle_ids = []
+          self.enterprise_fee_ids = []
+          self.shipping_method_ids = []
+          self.payment_method_ids = []
+
+          attributes.each do |key, value|
+            public_send("#{key}=", value)
+          end
+        end
+
+        protected
+
+        def require_valid_datetime_range
+          return if start_at.blank? || end_at.blank?
+
+          errors.add(:end_at, DATE_END_BEFORE_START_ERROR) unless start_at < end_at
+        end
+      end
+    end
+  end
+end
