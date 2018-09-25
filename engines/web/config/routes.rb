a23b1b980da5cf2eedd1b3fcab270537f972b63a
luisramos0 <luisramos0@gmail.com>
2018-09-25 12:34:38 +0100

    Make Web engine an isolated namespace engine mounted on / (without /web prefix) This approach is better to separate concerns, see “Isolated Engine” here: https://api.rubyonrails.org/v3.2/classes/Rails/Engine.html

--- engines/web/config/routes.rb
+++ engines/web/config/routes.rb
@@ -1,9 +1,7 @@
 Web::Engine.routes.draw do
-  namespace :web do
-    namespace :api do
-      scope '/cookies' do
-        resource :consent, only: [:show, :create, :destroy], controller: "cookies_consent"
-      end
+  namespace :api do
+    scope '/cookies' do
+      resource :consent, only: [:show, :create, :destroy], controller: "cookies_consent"
     end
   end
 end
