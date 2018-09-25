921105301ce51aaab18a0693a9087468d33b57db
luisramos0 <luisramos0@gmail.com>
2018-09-25 14:31:43 +0100

    Move angular-templates route and controller into Web engine The route and controller were in the main app and the views in the engine, with this commit they stay all inside the engine This is done to keep it simple and remove the unnecessary dependency between main app and engine If we use this mechanism in the future for other things in the main app or other engines, we can find a way to extract/abstract this

--- engines/web/config/routes.rb
+++ engines/web/config/routes.rb
@@ -4,4 +4,6 @@ Web::Engine.routes.draw do
       resource :consent, only: [:show, :create, :destroy], controller: "cookies_consent"
     end
   end
+
+  get "/angular-templates/:id", to: "angular_templates#show", constraints: { name: %r{[\/\w\.]+} }
 end
