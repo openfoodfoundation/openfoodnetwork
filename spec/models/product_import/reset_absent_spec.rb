718f529ede96511a78932ca0a2ab5be3c0c356e9
Pau Perez <saulopefa@gmail.com>
2018-09-25 21:31:34 +0200

    Use nested module in test to improve readability

--- spec/models/product_import/reset_absent_spec.rb
+++ spec/models/product_import/reset_absent_spec.rb
@@ -1,43 +1,103 @@
 require 'spec_helper'
 
-describe ProductImport::ResetAbsent do
-  let(:entry_processor) { instance_double(ProductImport::EntryProcessor) }
+module ProductImport
+  describe ResetAbsent do
+    let(:entry_processor) { instance_double(EntryProcessor) }
 
-  let(:reset_absent) do
-    described_class.new(entry_processor, settings, strategy)
-  end
+    let(:reset_absent) do
+      described_class.new(entry_processor, settings, strategy)
+    end
 
-  describe '#call' do
-    context 'when there are no enterprises_to_reset' do
-      let(:settings) do
-        instance_double(
-          ProductImport::Settings,
-          enterprises_to_reset: [],
-          updated_ids: []
-        )
+    describe '#call' do
+      context 'when there are no enterprises_to_reset' do
+        let(:settings) do
+          instance_double(
+            Settings,
+            enterprises_to_reset: [],
+            updated_ids: []
+          )
+        end
+
+        let(:strategy) do
+          instance_double(InventoryReset, supplier_ids: [])
+        end
+
+        it 'returns nil' do
+          expect(reset_absent.call).to be_nil
+        end
       end
 
-      let(:strategy) do
-        instance_double(ProductImport::InventoryReset, supplier_ids: [])
+      context 'when there are updated_ids and enterprises_to_reset' do
+        let(:enterprise) { instance_double(Enterprise, id: 1) }
+
+        let(:settings) do
+          instance_double(
+            Settings,
+            updated_ids: [0],
+            enterprises_to_reset: [enterprise.id.to_s]
+          )
+        end
+
+        let(:strategy) { instance_double(ProductsReset) }
+
+        before do
+          allow(entry_processor)
+            .to receive(:permission_by_id?).with(enterprise.id.to_s) { true }
+
+          allow(strategy).to receive(:<<).with(enterprise.id)
+          allow(strategy).to receive(:supplier_ids) { [enterprise.id] }
+          allow(strategy).to receive(:reset).with([0], [enterprise.id]) { 2 }
+        end
+
+        it 'returns the number of products reset' do
+          expect(reset_absent.call).to eq(2)
+        end
+
+        it 'resets the products of the specified suppliers' do
+          expect(strategy).to receive(:reset).with([0], [enterprise.id]) { 2 }
+          reset_absent.call
+        end
       end
 
-      it 'returns nil' do
-        expect(reset_absent.call).to be_nil
+      context 'when the enterprise has no permission' do
+        let(:enterprise) { instance_double(Enterprise, id: 1) }
+
+        let(:settings) do
+          instance_double(
+            Settings,
+            updated_ids: [0],
+            enterprises_to_reset: [enterprise.id.to_s]
+          )
+        end
+
+        let(:strategy) { instance_double(InventoryReset) }
+
+        before do
+          allow(entry_processor)
+            .to receive(:permission_by_id?).with(enterprise.id.to_s) { false }
+
+          allow(strategy).to receive(:supplier_ids) { [] }
+        end
+
+        it 'does not reset stock' do
+          expect(strategy).not_to receive(:reset)
+          reset_absent.call
+        end
       end
     end
 
-    context 'when there are updated_ids and enterprises_to_reset' do
+    describe '#products_reset_count' do
       let(:enterprise) { instance_double(Enterprise, id: 1) }
 
       let(:settings) do
         instance_double(
-          ProductImport::Settings,
+          Settings,
           updated_ids: [0],
           enterprises_to_reset: [enterprise.id.to_s]
         )
       end
 
-      let(:strategy) { instance_double(ProductImport::ProductsReset) }
+      let(:strategy) { instance_double(InventoryReset) }
 
       before do
         allow(entry_processor)
@@ -45,71 +105,13 @@ describe ProductImport::ResetAbsent do
 
         allow(strategy).to receive(:<<).with(enterprise.id)
         allow(strategy).to receive(:supplier_ids) { [enterprise.id] }
-        allow(strategy).to receive(:reset).with([0], [enterprise.id]) { 2 }
+        allow(strategy).to receive(:reset).with([0], [enterprise.id]) { 1 }
       end
 
-      it 'returns the number of products reset' do
-        expect(reset_absent.call).to eq(2)
-      end
-
-      it 'resets the products of the specified suppliers' do
-        expect(strategy).to receive(:reset).with([0], [enterprise.id]) { 2 }
+      it 'returns the number of reset variant overrides' do
         reset_absent.call
+        expect(reset_absent.products_reset_count).to eq(1)
       end
-    end
-
-    context 'when the enterprise has no permission' do
-      let(:enterprise) { instance_double(Enterprise, id: 1) }
-
-      let(:settings) do
-        instance_double(
-          ProductImport::Settings,
-          updated_ids: [0],
-          enterprises_to_reset: [enterprise.id.to_s]
-        )
-      end
-
-      let(:strategy) { instance_double(ProductImport::InventoryReset) }
-
-      before do
-        allow(entry_processor)
-          .to receive(:permission_by_id?).with(enterprise.id.to_s) { false }
-
-        allow(strategy).to receive(:supplier_ids) { [] }
-      end
-
-      it 'does not reset stock' do
-        expect(strategy).not_to receive(:reset)
-        reset_absent.call
-      end
-    end
-  end
-
-  describe '#products_reset_count' do
-    let(:enterprise) { instance_double(Enterprise, id: 1) }
-
-    let(:settings) do
-      instance_double(
-        ProductImport::Settings,
-        updated_ids: [0],
-        enterprises_to_reset: [enterprise.id.to_s]
-      )
-    end
-
-    let(:strategy) { instance_double(ProductImport::InventoryReset) }
-
-    before do
-      allow(entry_processor)
-        .to receive(:permission_by_id?).with(enterprise.id.to_s) { true }
-
-      allow(strategy).to receive(:<<).with(enterprise.id)
-      allow(strategy).to receive(:supplier_ids) { [enterprise.id] }
-      allow(strategy).to receive(:reset).with([0], [enterprise.id]) { 1 }
-    end
-
-    it 'returns the number of reset variant overrides' do
-      reset_absent.call
-      expect(reset_absent.products_reset_count).to eq(1)
     end
   end
 end
