cb77e8731afdcdea201ae6fcb6882fdc40958c83
Kristina Lim <kristinalim.ph@gmail.com>
2018-09-26 14:00:53 +0800

    Add data classes for Enterprise Fee Summary report

---
+++ lib/open_food_network/reports/report_data/base.rb
@@ -0,0 +1,13 @@
+module OpenFoodNetwork
+  module Reports
+    module ReportData
+      class Base
+        def initialize(attributes = {})
+          attributes.each do |key, value|
+            public_send("#{key}=", value)
+          end
+        end
+      end
+    end
+  end
+end
