06b28c1ab46ef94f3b07e6eca12111696ca336ba
Aditya Sridhar <aditya@justworks.com>
2018-09-30 15:50:33 -0400

    Better docs for Stripe.publishable_key and Stripe.endpoint_secret.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -4,6 +4,8 @@
 # a bit cleaner than accessing keys in different ways.
 module Stripe
   class << self
+  	# Returns the value of Stripe.publishable_key and Stripe.endpoint_secret.
+  	# Attribute values can also be set by doing Stripe.publishable_key = <your_new_value>
     attr_accessor :publishable_key, :endpoint_secret
   end
 end
