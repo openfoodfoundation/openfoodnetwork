c194eb52d0e1a1ee280b4ce10bcc21af2849f68a
Kristina Lim <kristinalim.ph@gmail.com>
2018-10-01 13:58:51 +0800

    Specify sort order of enterprise fee totals data

--- lib/order_management/reports/enterprise_fee_summary/report_data/enterprise_fee_type_total.rb
+++ lib/order_management/reports/enterprise_fee_summary/report_data/enterprise_fee_type_total.rb
@@ -7,6 +7,23 @@ module OrderManagement
         class EnterpriseFeeTypeTotal < OpenFoodNetwork::Reports::ReportData::Base
           attr_accessor :fee_type, :enterprise_name, :fee_name, :customer_name, :fee_placement,
                         :fee_calculated_on_transfer_through_name, :tax_category_name, :total_amount
+
+          def <=>(other)
+            self.class.sortable_data(self) <=> self.class.sortable_data(other)
+          end
+
+          def self.sortable_data(instance)
+            [
+              instance.fee_type,
+              instance.enterprise_name,
+              instance.fee_name,
+              instance.customer_name,
+              instance.fee_placement,
+              instance.fee_calculated_on_transfer_through_name,
+              instance.tax_category_name,
+              instance.total_amount
+            ]
+          end
         end
       end
     end
