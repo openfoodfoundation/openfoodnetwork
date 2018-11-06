71702af3c35cb6b2006c9adcf1058274c5980f86
Pau Perez <saulopefa@gmail.com>
2018-11-06 13:58:32 +0000

    Have a single attributes list in serializer

--- app/serializers/api/admin/product_serializer.rb
+++ app/serializers/api/admin/product_serializer.rb
@@ -1,7 +1,7 @@
 class Api::Admin::ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :sku, :variant_unit, :variant_unit_scale, :variant_unit_name, :inherits_properties
-
-  attributes :on_hand, :price, :available_on, :permalink_live, :tax_category_id, :import_date, :image_url, :thumb_url
+  attributes :id, :name, :sku, :variant_unit, :variant_unit_scale, :variant_unit_name,
+             :inherits_properties, :on_hand, :price, :available_on, :permalink_live,
+             :tax_category_id, :import_date, :image_url, :thumb_url
 
   has_one :supplier, key: :producer_id, embed: :id
   has_one :primary_taxon, key: :category_id, embed: :id
