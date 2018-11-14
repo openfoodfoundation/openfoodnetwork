b7d2b54a6053050feee0cc2f23945e8849de38c8
Kristina Lim <kristinalim.ph@gmail.com>
2018-11-14 16:57:49 +0800

    Add skeleton for OrderManagement engine

---
+++ engines/order_management/spec/spec_helper.rb
@@ -0,0 +1,9 @@
+ENV["RAILS_ENV"] = "test"
+
+require "../../spec/spec_helper"
+
+# Require factories in Spree and main application.
+require 'spree/testing_support/factories'
+require '../../spec/factories'
+
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
