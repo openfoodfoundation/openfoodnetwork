1363a2a17dc6152175d6849a4172768b260de7ed
Pau Perez <saulopefa@gmail.com>
2018-11-26 16:01:03 +0100

    Improve readability of spec

--- spec/lib/open_food_network/property_merge_spec.rb
+++ spec/lib/open_food_network/property_merge_spec.rb
@@ -2,23 +2,31 @@ require 'spec_helper'
 
 module OpenFoodNetwork
   describe PropertyMerge do
-    let(:p1a) { create(:property, presentation: 'One') }
-    let(:p1b) { create(:property, presentation: 'One') }
-    let(:p2)  { create(:property, presentation: 'Two') }
+    let(:property) { create(:property, presentation: 'One') }
+    let(:duplicate_property) { create(:property, presentation: 'One') }
+    let(:different_property) { create(:property, presentation: 'Two') }
 
     describe "merging Spree::Properties" do
       it "merges properties" do
-        expect(PropertyMerge.merge([p1a], [p1b, p2])).to eq [p1a, p2]
+        merge = PropertyMerge.merge(
+          [property],
+          [duplicate_property, different_property]
+        )
+        expect(merge).to eq [property, different_property]
       end
     end
 
     describe "merging ProducerProperties and Spree::ProductProperties" do
-      let(:pp1a) { create(:product_property,  property: p1a) }
-      let(:pp1b) { create(:producer_property, property: p1b) }
-      let(:pp2)  { create(:producer_property, property: p2) }
+      let(:product_property) { create(:product_property,  property: property) }
+      let(:duplicate_product_property) { create(:producer_property, property: duplicate_property) }
+      let(:producer_property) { create(:producer_property, property: different_property) }
 
       it "merges properties" do
-        expect(PropertyMerge.merge([pp1a], [pp1b, pp2])).to eq [pp1a, pp2]
+        merge = PropertyMerge.merge(
+          [product_property],
+          [duplicate_product_property, producer_property]
+        )
+        expect(merge).to eq [product_property, producer_property]
       end
     end
   end
