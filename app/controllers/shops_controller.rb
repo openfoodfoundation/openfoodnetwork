4608c434b4384213d92ca5eafea17d5395d61d82
Pau Perez <saulopefa@gmail.com>
2018-11-26 18:17:58 +0100

    Eager load producer properties to avoid N+1
    This totally removes the following N+1 query from the GET /shops request
    
    ```sql
    SELECT "spree_properties".* FROM "spree_properties"
      INNER JOIN "producer_properties"
        ON "spree_properties"."id" = "producer_properties"."property_id"
      WHERE "producer_properties"."producer_id" = 17
      ORDER BY producer_properties.position;
    ```
    
    The producer properties of the corresponding enterprises are now loaded
    in a single query fired from the controller.

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -4,6 +4,10 @@ class ShopsController < BaseController
   before_filter :enable_embedded_shopfront
 
   def index
-    @enterprises = Enterprise.activated.includes(address: :state).all
+    @enterprises = Enterprise
+      .activated
+      .includes(address: :state)
+      .includes(:properties)
+      .all
   end
 end
