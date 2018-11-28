96202ab00dc25c5f0ef86ed34990e4c28232a10c
Pau Perez <saulopefa@gmail.com>
2018-11-28 14:17:16 +0100

    Fix Rubocop violations

--- app/serializers/api/uncached_enterprise_serializer.rb
+++ app/serializers/api/uncached_enterprise_serializer.rb
@@ -1,13 +1,15 @@
-class Api::UncachedEnterpriseSerializer < ActiveModel::Serializer
-  include SerializerHelper
+module Api
+  class UncachedEnterpriseSerializer < ActiveModel::Serializer
+    include SerializerHelper
 
-  attributes :orders_close_at, :active
+    attributes :orders_close_at, :active
 
-  def orders_close_at
-    options[:data].earliest_closing_times[object.id]
-  end
+    def orders_close_at
+      options[:data].earliest_closing_times[object.id]
+    end
 
-  def active
-    options[:data].active_distributors.andand.include? object
+    def active
+      options[:data].active_distributors.andand.include? object
+    end
   end
 end
