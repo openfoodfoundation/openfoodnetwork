285346fcdac71c32566449a3ab1008af02670ce9
Maikel Linke <maikel@email.org.au>
2018-11-29 10:25:44 +1100

    Update schema for latest migration
    I feel embarrased that this is the second follow up of my last
    migration: https://github.com/openfoodfoundation/openfoodnetwork/pull/3126
    
    The last migration didn't change any database structure, but the schema
    still needs the latest migration version. Otherwise it will display
    pending migrations when setting up the database.
    
    This commit allows to run `bundle exec rake db:reset` in development
    without the following message:
    
      Run `rake db:migrate` to update your database then try again.
      You have 1 pending migrations:
        20181123012635 AssociateCustomersToUsers
    
    The next pull request with a migration would have solved this problem as
    well.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20181106162211) do
+ActiveRecord::Schema.define(:version => 20181123012635) do
 
   create_table "account_invoices", :force => true do |t|
     t.integer  "user_id",    :null => false
