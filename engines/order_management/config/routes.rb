91d2d2be6914a8e0827828c2ba288a8270bc0d73
Kristina Lim <kristinalim.ph@gmail.com>
2018-12-14 06:36:16 +1100

    Move routes for enterprise fee summary to engine

--- engines/order_management/config/routes.rb
+++ engines/order_management/config/routes.rb
@@ -1,2 +1,7 @@
-OrderManagement::Engine.routes.draw do
+Spree::Core::Engine.routes.prepend do
+  namespace :admin do
+    namespace :reports do
+      resource :enterprise_fee_summary, only: [:new, :create]
+    end
+  end
 end
