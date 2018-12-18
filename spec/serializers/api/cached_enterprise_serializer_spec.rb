8fef69387ebc18f687053ebf8c52efa7689a7021
Pau Perez <saulopefa@gmail.com>
2018-12-18 13:36:32 +0100

    Simplify test setup using factory transient attrs

--- spec/serializers/api/cached_enterprise_serializer_spec.rb
+++ spec/serializers/api/cached_enterprise_serializer_spec.rb
@@ -27,28 +27,26 @@ describe Api::CachedEnterpriseSerializer do
   describe '#distributed_properties' do
     let(:cached_enterprise_serializer) { described_class.new(shop, options) }
 
+    let(:shop) { create(:distributor_enterprise) }
+
+    let(:options) { { data: enterprise_injection_data } }
     let(:enterprise_injection_data) do
       instance_double(OpenFoodNetwork::EnterpriseInjectionData, active_distributors: [])
     end
-    let(:options) { { data: enterprise_injection_data } }
-    let(:shop) { create(:distributor_enterprise, name: 'Distributor') }
 
     let(:property) { create(:property, presentation: 'One') }
 
     before do
-      producer = create(:supplier_enterprise, name: 'Supplier')
+      producer = create(:supplier_enterprise)
       product = create(:product, supplier: producer, properties: [property])
 
-      order_cycle = build(:simple_order_cycle, coordinator: shop)
-
-      incoming_exchange = build(:exchange, sender: producer, receiver: shop, incoming: true)
-      outgoing_exchange = build(:exchange, sender: shop, receiver: shop, incoming: false)
-      outgoing_exchange.variants << product.variants.first
-
-      order_cycle.exchanges << incoming_exchange
-      order_cycle.exchanges << outgoing_exchange
-
-      order_cycle.save
+      create(
+        :simple_order_cycle,
+        coordinator: shop,
+        suppliers: [producer],
+        distributors: [shop],
+        variants: [product.variants]
+      )
     end
 
     it 'does not duplicate properties' do
