7155f28acf7f30c78c7c90b992158c33e2ea026c
luisramos0 <luisramos0@gmail.com>
2018-12-21 14:58:03 +0000

    Remove shipping_method_id from RestartCheckout, destroying shipments is now enough as order.shipping_method_id is no longer used and will be dropped. See #2683 for more details

--- spec/services/restart_checkout_spec.rb
+++ spec/services/restart_checkout_spec.rb
@@ -17,7 +17,6 @@ describe RestartCheckout do
       let!(:payment_failed) { create(:payment, order: order, state: 'failed') }
 
       before do
-        order.shipping_method_id = shipment_pending.shipping_method_id
         order.update_attribute(:state, "payment")
       end
 
@@ -28,7 +27,6 @@ describe RestartCheckout do
         RestartCheckout.new(order).call
 
         expect(order.state).to eq 'cart'
-        expect(order.shipping_method_id).to eq nil
         expect(order.shipments.count).to eq 0
         expect(order.adjustments.shipping.count).to eq 0
         expect(order.payments.count).to eq 1
