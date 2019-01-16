705bf86baa7e3a12838945186a0d9535f28a5d84
Kristina Lim <kristinalim.ph@gmail.com>
2019-01-16 08:44:28 +0800

    Add feature flag for enterprise fee summary

--- spec/models/feature_flags_spec.rb
+++ spec/models/feature_flags_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
 describe FeatureFlags do
-  describe '.product_import_enabled?' do
-    let(:user) { build_stubbed(:user) }
-    let(:feature_flags) { described_class.new(user) }
+  let(:user) { build_stubbed(:user) }
+  let(:feature_flags) { described_class.new(user) }
 
+  describe '.product_import_enabled?' do
     context 'when the user is superadmin' do
       before do
         allow(user).to receive(:has_spree_role?).with('admin') { true }
@@ -22,6 +22,24 @@ describe FeatureFlags do
 
       it 'returns false' do
         expect(feature_flags.product_import_enabled?).to eq(false)
+      end
+    end
+  end
+
+  describe ".enterprise_fee_summary_enabled?" do
+    context "when the user is superadmin" do
+      let!(:user) { create(:admin_user) }
+
+      it "returns true" do
+        expect(feature_flags).to be_enterprise_fee_summary_enabled
+      end
+    end
+
+    context "when the user is not superadmin" do
+      let!(:user) { create(:user) }
+
+      it "returns false" do
+        expect(feature_flags).not_to be_enterprise_fee_summary_enabled
       end
     end
   end
