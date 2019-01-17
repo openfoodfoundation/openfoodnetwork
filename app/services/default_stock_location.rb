f896e78e6f85d6f25755a444a4f4fed203ac8132
luisramos0 <luisramos0@gmail.com>
2019-01-17 14:34:15 +0000

    Set StockLocation.backorderable_default to false in DefaultStockLocation and in StockLocation factory. This makes the default value of variant.on_demand false in all environments and in tests.
    Additionally, adapt VariantStock.on_demand test and product factory to this change (setting on_hand value in product factory so it's not out of stock by default).

--- app/services/default_stock_location.rb
+++ app/services/default_stock_location.rb
@@ -6,7 +6,7 @@ class DefaultStockLocation
   def self.create!
     country = Spree::Country.find_by_iso(ENV['DEFAULT_COUNTRY_CODE'])
     state = country.states.first
-    Spree::StockLocation.create!(name: NAME, country_id: country.id, state_id: state.id)
+    Spree::StockLocation.create!(name: NAME, country_id: country.id, state_id: state.id, backorderable_default: false)
   end
 
   def self.destroy_all
