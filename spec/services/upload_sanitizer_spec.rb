9705c249ac9da3aeb9f5bd86aeaf3a98a7ea4f2d
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2019-02-15 14:59:15 +0000

    Sanitize no-parsable BOM characters inserted by Excel

---
+++ spec/services/upload_sanitizer_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe UploadSanitizer do
+  describe "#call" do
+    let(:upload) do
+      File.open("/tmp/unsanitized.csv", 'wb:ascii-8bit') do |f|
+        f << "\xEF\xBB\xBF"
+        f << "Test"
+      end
+    end
+    let(:service) { UploadSanitizer.new(File.open(upload).read) }
+
+    it "sanitizes the uploaded file" do
+      sanitized_upload = service.call
+
+      expect(sanitized_upload.encoding.name).to eq "UTF-8"
+      expect(sanitized_upload.to_s).to eq "Test"
+    end
+  end
+end
