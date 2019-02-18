8ee14ccb4b1b7cb2f4ff102264930c26926418e8
luisramos0 <luisramos0@gmail.com>
2019-02-18 15:13:51 +0000

    Make variants_stock_levels return variants on_demand values

--- spec/services/variants_stock_levels_spec.rb
+++ spec/services/variants_stock_levels_spec.rb
@@ -17,34 +17,34 @@ describe VariantsStockLevels do
     order.reload
   end
 
-  it "returns a hash with variant id, quantity, max_quantity and stock on hand" do
+  it "returns a hash with variant id, quantity, max_quantity, on hand and on demand" do
     expect(variant_stock_levels.call(order, [variant_in_the_order.id])).to eq(
-      variant_in_the_order.id => { quantity: 2, max_quantity: 3, on_hand: 4 }
+      variant_in_the_order.id => { quantity: 2, max_quantity: 3, on_hand: 4, on_demand: false }
     )
   end
 
   it "includes all line items, even when the variant_id is not specified" do
     expect(variant_stock_levels.call(order, [])).to eq(
-      variant_in_the_order.id => { quantity: 2, max_quantity: 3, on_hand: 4 }
+      variant_in_the_order.id => { quantity: 2, max_quantity: 3, on_hand: 4, on_demand: false }
     )
   end
 
   it "includes an empty quantity entry for variants that aren't in the order" do
     variant_ids = [variant_in_the_order.id, variant_not_in_the_order.id]
     expect(variant_stock_levels.call(order, variant_ids)).to eq(
-      variant_in_the_order.id => { quantity: 2, max_quantity: 3, on_hand: 4 },
-      variant_not_in_the_order.id => { quantity: 0, max_quantity: 0, on_hand: 2 }
+      variant_in_the_order.id => { quantity: 2, max_quantity: 3, on_hand: 4, on_demand: false },
+      variant_not_in_the_order.id => { quantity: 0, max_quantity: 0, on_hand: 2, on_demand: false }
     )
   end
 
-  describe "encoding Infinity" do
+  describe "when variant is on_demand" do
     let!(:variant_in_the_order) { create(:variant, on_demand: true) }
 
     before { variant_in_the_order.on_hand = 0 }
 
-    it "encodes Infinity as a large, finite integer" do
+    it "includes the actual on_hand value and on_demand: true" do
       expect(variant_stock_levels.call(order, [variant_in_the_order.id])).to eq(
-        variant_in_the_order.id => { quantity: 2, max_quantity: 3, on_hand: 2147483647 }
+        variant_in_the_order.id => { quantity: 2, max_quantity: 3, on_hand: 0, on_demand: true }
       )
     end
   end
