a728da4f624d22064246d5e154f395619729a2da
Pau Perez <saulopefa@gmail.com>
2019-02-26 12:09:26 +0100

    Cover product and producer props. in ctrl. spec

--- spec/controllers/shops_controller_spec.rb
+++ spec/controllers/shops_controller_spec.rb
@@ -16,10 +16,49 @@ describe ShopsController, type: :controller do
     expect(response.body).to have_content(invisible_distributor.name)
   end
 
+  it 'renders distributed product properties' do
+    product_property = create(:property, presentation: 'eggs')
+    product = create(:product, properties: [product_property])
+    producer = create(:supplier_enterprise)
+
+    create(
+      :simple_order_cycle,
+      coordinator: distributor,
+      suppliers: [producer],
+      distributors: [distributor],
+      variants: [product.variants]
+    )
+
+    get :index
+
+    expect(response.body)
+      .to match(/"distributed_properties":\[{"id":\d+,"name":"eggs","presentation":"eggs"}\]/)
+  end
+
+  it 'renders distributed producer properties' do
+    producer_property = create(:property, presentation: 'certified')
+    producer = create(:supplier_enterprise, properties: [producer_property])
+    product = create(:product)
+
+    create(
+      :simple_order_cycle,
+      coordinator: distributor,
+      suppliers: [producer],
+      distributors: [distributor],
+      variants: [product.variants]
+    )
+
+    get :index
+
+    expect(response.body)
+      .to match(/"distributed_properties":\[{"id":\d+,"name":"certified","presentation":"certified"}\]/)
+  end
+
   it 'renders distributed properties' do
     duplicate_property = create(:property, presentation: 'dairy')
     producer = create(:supplier_enterprise, properties: [duplicate_property])
     property = create(:property, presentation: 'dairy')
+
     product = create(:product, properties: [property])
     producer.supplied_products << product
 
