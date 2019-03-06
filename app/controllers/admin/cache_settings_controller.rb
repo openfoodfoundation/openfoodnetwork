bbfd35c861b1fc4280237ce39f3e8fbf0a8b700a
Pau Perez <saulopefa@gmail.com>
2019-03-06 10:45:45 +0100

    Fix CacheSettingsController violations

--- app/controllers/admin/cache_settings_controller.rb
+++ app/controllers/admin/cache_settings_controller.rb
@@ -1,18 +1,27 @@
 require 'open_food_network/products_cache_integrity_checker'
 
-class Admin::CacheSettingsController < Spree::Admin::BaseController
-  def edit
-    @results = Exchange.cachable.map do |exchange|
-      checker = OpenFoodNetwork::ProductsCacheIntegrityChecker.new(exchange.receiver, exchange.order_cycle)
+module Admin
+  class CacheSettingsController < Spree::Admin::BaseController
+    def edit
+      @results = Exchange.cachable.map do |exchange|
+        checker = OpenFoodNetwork::ProductsCacheIntegrityChecker
+          .new(exchange.receiver, exchange.order_cycle)
 
-      {distributor: exchange.receiver, order_cycle: exchange.order_cycle, status: checker.ok?, diff: checker.diff}
+        {
+          distributor: exchange.receiver,
+          order_cycle: exchange.order_cycle,
+          status: checker.ok?,
+          diff: checker.diff
+        }
+      end
     end
-  end
 
-  def update
-    Spree::Config.set(params[:preferences])
-    respond_to do |format|
-      format.html { redirect_to main_app.edit_admin_cache_settings_path }
+    def update
+      Spree::Config.set(params[:preferences])
+
+      respond_to do |format|
+        format.html { redirect_to main_app.edit_admin_cache_settings_path }
+      end
     end
   end
 end
