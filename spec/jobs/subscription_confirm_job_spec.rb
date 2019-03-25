2aad5baac5c3b64a41634b48a2e60be3eec2feb6
Kristina Lim <kristinalim.ph@gmail.com>
2019-03-25 22:22:11 +0800

    Make code style changes for SubscriptionConfirmJob specs

--- spec/jobs/subscription_confirm_job_spec.rb
+++ spec/jobs/subscription_confirm_job_spec.rb
@@ -11,7 +11,10 @@ describe SubscriptionConfirmJob do
     let(:order_cycle2) { create(:simple_order_cycle, coordinator: shop, orders_close_at: 61.minutes.ago, updated_at: 1.day.ago) }
     let(:schedule) { create(:schedule, order_cycles: [order_cycle1, order_cycle2]) }
     let(:subscription) { create(:subscription, with_items: true, shop: shop, schedule: schedule) }
-    let!(:proxy_order) { create(:proxy_order, subscription: subscription, order_cycle: order_cycle1, placed_at: 5.minutes.ago) }
+    let!(:proxy_order) do
+      create(:proxy_order, subscription: subscription, order_cycle: order_cycle1,
+                           placed_at: 5.minutes.ago)
+    end
     let!(:order) { proxy_order.initialise_order! }
     let(:proxy_orders) { job.send(:proxy_orders) }
 
