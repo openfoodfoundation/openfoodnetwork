3035d14369e382c6aa67e0e3cb3ea993c377bbfb
Kristina Lim <kristinalim.ph@gmail.com>
2019-03-31 00:39:49 +0800

    Use bang version of create! in DB migration script

--- db/migrate/20180426145653_add_shipping_rates_to_shipments.spree.rb
+++ db/migrate/20180426145653_add_shipping_rates_to_shipments.spree.rb
@@ -2,9 +2,9 @@
 class AddShippingRatesToShipments < ActiveRecord::Migration
   def up
     Spree::Shipment.all.each do |shipment|
-      shipment.shipping_rates.create(:shipping_method_id => shipment.shipping_method_id,
-                                     :cost => shipment.cost,
-                                     :selected => true)
+      shipment.shipping_rates.create!(:shipping_method_id => shipment.shipping_method_id,
+                                      :cost => shipment.cost,
+                                      :selected => true)
     end
 
     remove_column :spree_shipments, :shipping_method_id
