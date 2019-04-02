2a6236823f263dba65c359147290918be3a2440b
Maikel Linke <maikel@email.org.au>
2019-04-02 17:25:33 +1100

    Move adjustment filtering from view to helper

--- app/helpers/admin/orders_helper.rb
+++ app/helpers/admin/orders_helper.rb
@@ -1,7 +1,14 @@
 module Admin
   module OrdersHelper
     def order_adjustments(order)
-      order.adjustments.eligible
+      order.adjustments.eligible.select do |adjustment|
+        type = adjustment.originator_type
+
+        is_shipping_method_fee = (type == 'Spree::ShippingMethod')
+        is_zero_tax_rate = (type == 'Spree::TaxRate' && adjustment.amount.zero?)
+
+        !is_shipping_method_fee && !is_zero_tax_rate
+      end
     end
   end
 end
