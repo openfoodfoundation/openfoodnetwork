d31b5efac6b454b9a0e043d0441d0cec1da58808
Maikel Linke <maikel@email.org.au>
2019-04-03 11:15:54 +1100

    Simplify orders helper

--- app/helpers/admin/orders_helper.rb
+++ app/helpers/admin/orders_helper.rb
@@ -4,16 +4,9 @@ module Admin
     #
     # We exclude shipping method adjustments because they are displayed in a
     # separate table together with the order line items.
-    #
-    # We also exclude tax rate adjustment with zero value.
     def order_adjustments_for_display(order)
       order.adjustments.eligible.select do |adjustment|
-        type = adjustment.originator_type
-
-        is_shipping_method_adjustment = (type == 'Spree::ShippingMethod')
-        is_zero_tax_rate_adjustment = (type == 'Spree::TaxRate' && adjustment.amount.zero?)
-
-        !is_shipping_method_adjustment && !is_zero_tax_rate_adjustment
+        adjustment.originator_type != "Spree::ShippingMethod"
       end
     end
   end
