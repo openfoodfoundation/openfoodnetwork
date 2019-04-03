7035d572678ccd2efe6ae74e2db2cfcf56f6ee1c
Maikel Linke <maikel@email.org.au>
2019-04-03 11:07:22 +1100

    Better helper naming and spec cover

--- app/helpers/admin/orders_helper.rb
+++ app/helpers/admin/orders_helper.rb
@@ -1,13 +1,19 @@
 module Admin
   module OrdersHelper
-    def order_adjustments(order)
+    # Adjustments to display under "Order adjustments".
+    #
+    # We exclude shipping method adjustments because they are displayed in a
+    # separate table together with the order line items.
+    #
+    # We also exclude tax rate adjustment with zero value.
+    def order_adjustments_for_display(order)
       order.adjustments.eligible.select do |adjustment|
         type = adjustment.originator_type
 
-        is_shipping_method_fee = (type == 'Spree::ShippingMethod')
-        is_zero_tax_rate = (type == 'Spree::TaxRate' && adjustment.amount.zero?)
+        is_shipping_method_adjustment = (type == 'Spree::ShippingMethod')
+        is_zero_tax_rate_adjustment = (type == 'Spree::TaxRate' && adjustment.amount.zero?)
 
-        !is_shipping_method_fee && !is_zero_tax_rate
+        !is_shipping_method_adjustment && !is_zero_tax_rate_adjustment
       end
     end
   end
