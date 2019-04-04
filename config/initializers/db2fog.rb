9b45269cbb63d1b4c96c99513ecf58d42d18d8cd
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2019-04-04 13:06:05 +0100

    Fix name of Spree::Config preference

--- config/initializers/db2fog.rb
+++ config/initializers/db2fog.rb
@@ -2,6 +2,6 @@
 DB2Fog.config = {
     :aws_access_key_id     => Spree::Config[:s3_access_key],
     :aws_secret_access_key => Spree::Config[:s3_secret],
-    :directory             => Spree::Config[:s3_backups_bucket],
+    :directory             => Spree::Config[:s3_bucket],
     :provider              => 'AWS'
 }
