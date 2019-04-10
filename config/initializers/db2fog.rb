37cbfcb6925fd0287d830a4dbed65ca943222c2b
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2019-04-10 17:17:17 +0100

    Use ENV var for backups bucket

--- config/initializers/db2fog.rb
+++ config/initializers/db2fog.rb
@@ -2,6 +2,6 @@
 DB2Fog.config = {
     :aws_access_key_id     => Spree::Config[:s3_access_key],
     :aws_secret_access_key => Spree::Config[:s3_secret],
-    :directory             => Spree::Config[:s3_bucket],
+    :directory             => ENV['S3_BACKUPS_BUCKET'],
     :provider              => 'AWS'
 }
