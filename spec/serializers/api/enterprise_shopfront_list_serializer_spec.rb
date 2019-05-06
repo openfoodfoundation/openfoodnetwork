fc8b220b4cc46cdfbd9db9dfe17ddb3bd6ea0d45
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2019-05-06 22:42:51 +0100

    Add specs for endpoint and serializers

---
+++ spec/serializers/api/enterprise_shopfront_list_serializer_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe Api::EnterpriseShopfrontListSerializer do
+  let(:enterprise) { create(:distributor_enterprise) }
+  let(:serializer) {
+    Api::EnterpriseShopfrontListSerializer.new enterprise
+  }
+
+  it "serializes enterprise attributes" do
+    expect(serializer.to_json).to match enterprise.name
+  end
+
+  it "serializes shopfront path" do
+    expect(serializer.to_json).to match enterprise_shop_path(enterprise)
+  end
+
+  it "serializes icons" do
+    expect(serializer.to_json).to match "map_005-hub.svg"
+  end
+end
