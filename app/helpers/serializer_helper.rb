d9bafd56daaca7e98d030ae5205d15be7e8bcf55
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2019-05-07 12:23:52 +0100

    Improve queries for serializers

--- app/helpers/serializer_helper.rb
+++ app/helpers/serializer_helper.rb
@@ -3,4 +3,13 @@ module SerializerHelper
     return [] if ids.blank?
     ids.map { |id| {id: id} }
   end
+
+  # Returns an array of the fields a serializer needs from it's object
+  # so we can #select only what the serializer will actually use
+  def required_attributes(model, serializer)
+    model_attributes = model.attribute_names
+    serializer_attributes = serializer._attributes.keys.map(&:to_s)
+
+    (serializer_attributes & model_attributes).map { |attr| "#{model.table_name}.#{attr}" }
+  end
 end
