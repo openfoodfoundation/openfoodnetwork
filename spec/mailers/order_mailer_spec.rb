e7c6892ab804b62a19f2d84dbd8302fb2cc51ae5
luisramos0 <luisramos0@gmail.com>
2019-05-09 23:47:32 +0100

    Run transpec for spec/mailers

--- spec/mailers/order_mailer_spec.rb
+++ spec/mailers/order_mailer_spec.rb
@@ -27,28 +27,28 @@ describe Spree::OrderMailer do
     describe "for customers" do
       it "should send an email to the customer when given an order" do
         Spree::OrderMailer.confirm_email_for_customer(@order1.id).deliver
-        ActionMailer::Base.deliveries.count.should == 1
-        ActionMailer::Base.deliveries.first.to.should == [@order1.email]
+        expect(ActionMailer::Base.deliveries.count).to eq(1)
+        expect(ActionMailer::Base.deliveries.first.to).to eq([@order1.email])
       end
 
       it "sets a reply-to of the enterprise email" do
         Spree::OrderMailer.confirm_email_for_customer(@order1.id).deliver
-        ActionMailer::Base.deliveries.first.reply_to.should == [@distributor.contact.email]
+        expect(ActionMailer::Base.deliveries.first.reply_to).to eq([@distributor.contact.email])
       end
     end
 
     describe "for shops" do
       it "sends an email to the shop owner when given an order" do
         Spree::OrderMailer.confirm_email_for_shop(@order1.id).deliver
-        ActionMailer::Base.deliveries.count.should == 1
-        ActionMailer::Base.deliveries.first.to.should == [@distributor.contact.email]
+        expect(ActionMailer::Base.deliveries.count).to eq(1)
+        expect(ActionMailer::Base.deliveries.first.to).to eq([@distributor.contact.email])
       end
 
       it "sends an email even if a footer_email is given" do
         # Testing bug introduced by a9c37c162e1956028704fbdf74ce1c56c5b3ce7d
         ContentConfig.footer_email = "email@example.com"
         Spree::OrderMailer.confirm_email_for_shop(@order1.id).deliver
-        ActionMailer::Base.deliveries.count.should == 1
+        expect(ActionMailer::Base.deliveries.count).to eq(1)
       end
     end
   end
