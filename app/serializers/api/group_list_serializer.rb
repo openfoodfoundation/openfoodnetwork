e5ff3cc1f4c25a06e82b720bccbaaf1f858c5355
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2019-05-09 10:25:26 +0100

    Fix groups index page

---
+++ app/serializers/api/group_list_serializer.rb
@@ -0,0 +1,16 @@
+module Api
+  class GroupListSerializer < ActiveModel::Serializer
+    attributes :id, :name, :permalink, :email, :website, :facebook, :instagram,
+               :linkedin, :twitter, :enterprises, :state, :address_id
+
+    def state
+      object.address.state.abbr
+    end
+
+    def enterprises
+      ActiveModel::ArraySerializer.new(
+        object.enterprises, each_serializer: Api::EnterpriseThinSerializer
+      )
+    end
+  end
+end
---
+++ spec/serializers/api/group_list_serializer_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe Api::GroupListSerializer do
+  let!(:group) { create(:enterprise_group) }
+  let!(:producer) { create(:supplier_enterprise) }
+
+  let(:serializer) { Api::GroupListSerializer.new group }
+
+  before do
+    group.enterprises << producer
+  end
+
+  it "serializes group attributes" do
+    expect(serializer.serializable_hash[:name]).to match group.name
+  end
+
+  it "serializes abbreviated state" do
+    expect(serializer.serializable_hash[:state]).to eq group.address.state.abbr
+  end
+
+  it "serialises an array of enterprises" do
+    expect(serializer.serializable_hash[:enterprises]).to be_a ActiveModel::ArraySerializer
+    expect(serializer.serializable_hash[:enterprises].to_json).to match producer.name
+  end
+end
