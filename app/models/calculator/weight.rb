45511495329ec9356b01eb6cdf894734c42d6c78
luisramos0 <luisramos0@gmail.com>
2019-05-31 19:41:33 +0100

    Improve code of weight calculator

--- app/models/calculator/weight.rb
+++ app/models/calculator/weight.rb
@@ -13,14 +13,17 @@ module Calculator
 
     def compute(object)
       line_items = line_items_for object
-      total_weight = line_items.sum do |line_item|
+      total_weight(line_items) * preferred_per_kg
+    end
+
+    def total_weight(line_items)
+      line_items.sum do |line_item|
         if line_item.final_weight_volume.present?
           line_item.final_weight_volume / 1000
         else
-          ((line_item.variant.andand.weight || 0) * line_item.quantity)
+          (line_item.variant.andand.weight || 0) * line_item.quantity
         end
       end
-      total_weight * preferred_per_kg
     end
   end
 end
