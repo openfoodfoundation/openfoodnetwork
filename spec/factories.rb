bd493c392a2704698d62acc7de2a49727d20a5b1
luisramos0 <luisramos0@gmail.com>
2019-06-14 12:25:10 +0100

    Extract shipping_methods factories to separate file

--- spec/factories.rb
+++ spec/factories.rb
@@ -99,43 +99,6 @@ FactoryBot.define do
     enterprise_role 'distributor'
   end
 
-  factory :shipping_method_with, parent: :shipping_method do
-    trait :delivery do
-      require_ship_address { true }
-    end
-
-    trait :pickup do
-      require_ship_address { false }
-    end
-
-    trait :flat_rate do
-      calculator { Spree::Calculator::FlatRate.new(preferred_amount: 50.0) }
-    end
-
-    trait :expensive_name do
-      name { "Shipping" }
-      description { "Expensive" }
-      calculator { Spree::Calculator::FlatRate.new(preferred_amount: 100.55) }
-    end
-
-    trait :distributor do
-      transient do
-        distributor { create :enterprise }
-      end
-      distributors { [distributor] }
-    end
-
-    trait :shipping_fee do
-      transient do
-        shipping_fee 3
-      end
-
-      calculator { build(:calculator_per_item, preferred_amount: shipping_fee) }
-      require_ship_address { false }
-      distributors { [create(:distributor_enterprise_with_tax)] }
-    end
-  end
-
   factory :shipment_with, class: Spree::Shipment do
     tracking 'U10000'
     number '100'
@@ -321,11 +284,6 @@ FactoryBot.modify do
       variant.on_hand = evaluator.on_hand
       variant.save
     end
-  end
-
-  factory :shipping_method, parent: :base_shipping_method do
-    distributors { [Enterprise.is_distributor.first || FactoryBot.create(:distributor_enterprise)] }
-    display_on ''
   end
 
   factory :address do
