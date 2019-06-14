91fcb7c7c40b2a5312545181bc6e35b858385556
luisramos0 <luisramos0@gmail.com>
2019-06-14 12:12:45 +0100

    Extract enterprises factories to separate file

--- spec/factories.rb
+++ spec/factories.rb
@@ -65,36 +65,6 @@ FactoryBot.define do
     visible true
   end
 
-  factory :enterprise, :class => Enterprise do
-    owner { FactoryBot.create :user }
-    sequence(:name) { |n| "Enterprise #{n}" }
-    sells 'any'
-    description 'enterprise'
-    long_description '<p>Hello, world!</p><p>This is a paragraph.</p>'
-    address { FactoryBot.create(:address) }
-  end
-
-  factory :supplier_enterprise, :parent => :enterprise do
-    is_primary_producer true
-    sells "none"
-  end
-
-  factory :distributor_enterprise, :parent => :enterprise do
-    is_primary_producer false
-    sells "any"
-
-    transient do
-      with_payment_and_shipping false
-    end
-
-    after(:create) do |enterprise, proxy|
-      if proxy.with_payment_and_shipping
-        create(:payment_method,  distributors: [enterprise])
-        create(:shipping_method, distributors: [enterprise])
-      end
-    end
-  end
-
   factory :enterprise_relationship do
   end
 
@@ -256,11 +226,6 @@ FactoryBot.define do
         end
       end
     end
-  end
-
-  factory :distributor_enterprise_with_tax, parent: :distributor_enterprise do
-    charges_sales_tax { true }
-    allow_order_changes { true }
   end
 
   factory :completed_order_with_fees, parent: :order_with_distributor do
