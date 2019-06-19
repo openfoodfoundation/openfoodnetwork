67b5f08b0753424c2708c0173f45150dba1655d0
luisramos0 <luisramos0@gmail.com>
2019-06-19 15:33:32 +0100

    Refactor OrderHelper.order_links: extract links object to separate methods

--- app/helpers/spree/admin/orders_helper_decorator.rb
+++ app/helpers/spree/admin/orders_helper_decorator.rb
@@ -2,26 +2,93 @@ module Spree
   module Admin
     module OrdersHelper
       def order_links(order)
+        @order ||= order
         links = []
-        links << { name: t(:edit_order), url: edit_admin_order_path(order), icon: 'icon-edit' } unless action_name == "edit"
+        links << edit_order_link unless action_name == "edit"
         if @order.complete?
-          links << { name: t(:resend_confirmation), url: resend_admin_order_path(order), icon: 'icon-email', method: 'post', confirm: t(:confirm_resend_order_confirmation) }
+          links << resend_confirmation_link
           if @order.distributor.can_invoice?
-            links << { name: t(:send_invoice), url: invoice_admin_order_path(order), icon: 'icon-email', confirm: t(:confirm_send_invoice) }
+            links << send_invoice_link_with_url
           else
-            links << { name: t(:send_invoice), url: "#", icon: 'icon-email', confirm: t(:must_have_valid_business_number, enterprise_name: order.distributor.name) }
+            links << send_invoice_link_without_url
           end
-          links << { name: t(:print_invoice), url: print_admin_order_path(order), icon: 'icon-print', target: "_blank" }
+          links << print_invoice_link
           if Spree::Config.enable_receipt_printing?
-            links << { name: t(:print_ticket), url: print_ticket_admin_order_path(order), icon: 'icon-print', target: "_blank" }
-            links << { name: t(:select_ticket_printer), url: "#{print_ticket_admin_order_path(order)}#select-printer", icon: 'icon-print', target: "_blank" }
+            links << print_ticket_link
+            links << select_ticket_printer_link
           end
         end
         if @order.ready_to_ship?
-          links << { name: t(:ship_order), url: fire_admin_order_path(@order, e: 'ship'), method: 'put', icon: 'icon-truck', confirm: t(:are_you_sure) }
+          links << ship_order_link
         end
-        links << { name: t(:cancel_order), url: fire_admin_order_path(@order.number, e: 'cancel'), icon: 'icon-trash', confirm: t(:are_you_sure) } if order.can_cancel?
+        links << cancel_order_link if @order.can_cancel?
         links
+      end
+
+      private
+
+      def edit_order_link
+        { name: t(:edit_order),
+          url: edit_admin_order_path(@order),
+          icon: 'icon-edit' }
+      end
+
+      def resend_confirmation_link
+        { name: t(:resend_confirmation),
+          url: resend_admin_order_path(@order),
+          icon: 'icon-email',
+          method: 'post',
+          confirm: t(:confirm_resend_order_confirmation) }
+      end
+
+      def send_invoice_link_with_url
+        { name: t(:send_invoice),
+          url: invoice_admin_order_path(@order),
+          icon: 'icon-email',
+          confirm: t(:confirm_send_invoice) }
+      end
+
+      def send_invoice_link_without_url
+        { name: t(:send_invoice),
+          url: "#",
+          icon: 'icon-email',
+          confirm: t(:must_have_valid_business_number, enterprise_name: @order.distributor.name) }
+      end
+
+      def print_invoice_link
+        { name: t(:print_invoice),
+          url: print_admin_order_path(@order),
+          icon: 'icon-print',
+          target: "_blank" }
+      end
+
+      def print_ticket_link
+        { name: t(:print_ticket),
+          url: print_ticket_admin_order_path(@order),
+          icon: 'icon-print',
+          target: "_blank" }
+      end
+
+      def select_ticket_printer_link
+        { name: t(:select_ticket_printer),
+          url: "#{print_ticket_admin_order_path(@order)}#select-printer",
+          icon: 'icon-print',
+          target: "_blank" }
+      end
+
+      def ship_order_link
+        { name: t(:ship_order),
+          url: fire_admin_order_path(@order, e: 'ship'),
+          method: 'put',
+          icon: 'icon-truck',
+          confirm: t(:are_you_sure) }
+      end
+
+      def cancel_order_link
+        { name: t(:cancel_order),
+          url: fire_admin_order_path(@order.number, e: 'cancel'),
+          icon: 'icon-trash',
+          confirm: t(:are_you_sure) }
       end
     end
   end
