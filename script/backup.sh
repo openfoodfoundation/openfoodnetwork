503658f93031026416e9e9bfd4f1673972d64b83
Maikel Linke <maikel@email.org.au>
2019-07-03 14:46:56 +1000

    Remove obsolete backup script
    This script doesn't work anymore. It was written for old an Australian
    production server. We have automatic backups now. And if we wanted to
    take a backup manually, we should probably give it a meaningful name,
    not using a script.

--- script/backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Usage: script/backup.sh [ofn-staging1|ofn-staging2|ofn-prod]
-
-set -e
-
-mkdir -p db/backup
-ssh $1 "pg_dump -h localhost -U openfoodweb openfoodweb_production |gzip" > db/backup/$1-`date +%Y%m%d`.sql.gz
