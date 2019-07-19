314ed50e0f9ede36277136d8d3be2fb7ef1c13d5
luisramos0 <luisramos0@gmail.com>
2019-07-19 21:07:28 +0100

    Fix a rubocop issue in spree/api/products_controller

--- app/controllers/spree/api/products_controller.rb
+++ app/controllers/spree/api/products_controller.rb
@@ -63,7 +63,9 @@ module Spree
         authorize! :read, Spree::Product
 
         @products = product_scope.
-        ransack(params[:q]).result.managed_by(current_api_user).page(params[:page]).per(params[:per_page])
+          ransack(params[:q]).result.
+          managed_by(current_api_user).
+          page(params[:page]).per(params[:per_page])
         respond_with(@products, default_template: :index)
       end
 
