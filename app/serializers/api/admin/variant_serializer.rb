7e6259da31297ac29035cf45b8015714ae1a780d
luisramos0 <luisramos0@gmail.com>
2019-07-23 12:31:07 +0100

    Replace admin/variants/search rabl template with existing admin variant serializer with a few added attributes
    This commit fixes a bug as the image of the variatn was not being rendered, we need to fetch the image of the product, not the image of the variant.

--- app/serializers/api/admin/variant_serializer.rb
+++ app/serializers/api/admin/variant_serializer.rb
@@ -1,7 +1,15 @@
 class Api::Admin::VariantSerializer < ActiveModel::Serializer
   attributes :id, :options_text, :unit_value, :unit_description, :unit_to_display, :on_demand, :display_as, :display_name, :name_to_display, :sku
-  attributes :on_hand, :price, :import_date
+  attributes :on_hand, :price, :import_date, :name, :producer_name, :image
   has_many :variant_overrides
+
+  def name
+    if object.full_name.present?
+      "#{object.name} - #{object.full_name}"
+    else
+      object.name
+    end
+  end
 
   def on_hand
     return 0 if object.on_hand.nil?
@@ -11,5 +19,13 @@ class Api::Admin::VariantSerializer < ActiveModel::Serializer
   def price
     # Decimals are passed to json as strings, we need to run parseFloat.toFixed(2) on the client side.
     object.price.nil? ? 0.to_f : object.price
+  end
+
+  def producer_name
+    object.product.supplier.name
+  end
+
+  def image
+    object.product.images.first.mini_url
   end
 end
