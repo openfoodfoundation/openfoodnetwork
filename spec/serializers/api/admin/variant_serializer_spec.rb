7a8b5e89be149a40fa037059ff48a9a6ea9ecc7a
luisramos0 <luisramos0@gmail.com>
2019-07-23 22:11:00 +0100

    Add specs to cover the most important new fields in api/admin/variant_serializer

--- spec/serializers/api/admin/variant_serializer_spec.rb
+++ spec/serializers/api/admin/variant_serializer_spec.rb
@@ -1,7 +1,30 @@
+require 'spec_helper'
+
 describe Api::Admin::VariantSerializer do
+
   let(:variant) { create(:variant) }
-  it "serializes a variant" do
+
+  it "serializes the variant name" do
     serializer = Api::Admin::VariantSerializer.new variant
+
+    expect(serializer.to_json).to match variant.name
+  end
+
+  it "serializes the variant options" do
+    serializer = Api::Admin::VariantSerializer.new variant
+
     expect(serializer.to_json).to match variant.options_text
+  end
+
+  it "serializes the variant full name" do
+    serializer = Api::Admin::VariantSerializer.new variant
+
+    expect(serializer.to_json).to match variant.full_name
+  end
+
+  it "serializes the variant stock location id" do
+    serializer = Api::Admin::VariantSerializer.new variant
+
+    expect(serializer.to_json).to match variant.stock_items.first.stock_location.id.to_s
   end
 end
