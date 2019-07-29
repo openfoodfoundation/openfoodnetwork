8868b7eb1245ad295dcb625ec6e02a5eb5c3aec5
luisramos0 <luisramos0@gmail.com>
2019-07-29 19:05:39 +0100

    Fix rubocop issue: use nested class/module definition

--- app/serializers/api/order_cycle_serializer.rb
+++ app/serializers/api/order_cycle_serializer.rb
@@ -1,11 +1,13 @@
-class Api::OrderCycleSerializer < ActiveModel::Serializer
-  attributes :order_cycle_id, :orders_close_at
+module Api
+  class OrderCycleSerializer < ActiveModel::Serializer
+    attributes :order_cycle_id, :orders_close_at
 
-  def order_cycle_id
-    object.id
-  end
+    def order_cycle_id
+      object.id
+    end
 
-  def orders_close_at
-    object.orders_close_at.to_s
+    def orders_close_at
+      object.orders_close_at.to_s
+    end
   end
 end
