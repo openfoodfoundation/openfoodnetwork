55080eff513d5333a6d83d08b9aec37e4b71e27c
Pau Perez <saulopefa@gmail.com>
2019-08-09 15:08:52 +0200

    Refactor SQL query in ActiveRecord methods

--- app/services/order_cycle_distributed_products.rb
+++ app/services/order_cycle_distributed_products.rb
@@ -36,37 +36,14 @@ class OrderCycleDistributedProducts
 
   # TODO: filter by products supplied by the OC suppliers so we don't go through the whole products table.
   def products_with_obsolete_master
-    query = <<-SQL
-SELECT "spree_products".*
-  FROM "spree_products"
-  LEFT
-  JOIN "spree_variants"
-    ON "spree_variants"."product_id"    = "spree_products"."id"
-   AND "spree_variants"."deleted_at" IS NULL
-  LEFT
-  JOIN "exchange_variants"
-    ON "exchange_variants"."variant_id" = "spree_variants"."id"
-  LEFT
-  JOIN "exchanges"
-    ON "exchanges"."id"                 = "exchange_variants"."exchange_id"
-  LEFT OUTER
-  JOIN (
-    SELECT *
-      from inventory_items
- WHERE enterprise_id                    = ?) AS o_inventory_items
-    ON o_inventory_items.variant_id     = spree_variants.id
-   AND ("spree_products".deleted_at IS NULL)
-   AND ("spree_variants".deleted_at IS NULL)
-   AND (o_inventory_items.id IS NULL
-    OR o_inventory_items.visible        = ('t'))
- GROUP BY "spree_products"."id"
-HAVING COUNT(*) > 1
-   AND bool_or(is_master = true AND exchanges.id IS NOT NULL)
-   AND COUNT(exchanges.id) = 1
-    SQL
-
-    Spree::Product.find_by_sql(
-      [query, distributor.id]
+    Spree::Product
+      .joins('LEFT JOIN "spree_variants" ON "spree_variants"."product_id" = "spree_products"."id" AND "spree_variants"."deleted_at" IS NULL')
+      .joins('LEFT JOIN "exchange_variants" ON "exchange_variants"."variant_id" = "spree_variants"."id"')
+      .joins('LEFT JOIN "exchanges" ON "exchanges"."id" = "exchange_variants"."exchange_id"')
+      .merge(distributor.inventory_variants)
+      .group('"spree_products"."id"')
+      .having(
+        'COUNT(*) > 1 AND bool_or(is_master = true AND exchanges.id IS NOT NULL) AND COUNT(exchanges.id) = 1'
     )
   end
 end
