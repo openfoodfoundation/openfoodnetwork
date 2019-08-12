353c6af3077214f70be70b35438a734e3b4adddc
Pau Perez <saulopefa@gmail.com>
2019-08-12 17:19:13 +0200

    DRY and make complex AR query a bit more readable

--- app/services/order_cycle_distributed_products.rb
+++ app/services/order_cycle_distributed_products.rb
@@ -19,22 +19,8 @@ class OrderCycleDistributedProducts
       .unscoped
       .select('DISTINCT(spree_products.*)')
       .joins(variants_including_master: :exchanges)
-      .joins(<<-SQL)
-        LEFT OUTER JOIN (
-          SELECT * FROM inventory_items WHERE enterprise_id = #{distributor.id}
-        ) AS o_inventory_items
-        ON o_inventory_items.variant_id = variants_including_masters_spree_products.id
-      SQL
-      .where("o_inventory_items.id IS NULL OR o_inventory_items.visible = true")
-      .merge(Exchange.in_order_cycle(order_cycle.id))
-      .merge(Exchange.outgoing)
-      .merge(Exchange.to_enterprise(distributor))
-      .joins(<<-SQL.strip_heredoc)
-        LEFT JOIN (
-          #{products_with_obsolete_master.to_sql}
-        ) AS products_with_obsolete_master
-        ON variants_including_masters_spree_products.product_id = products_with_obsolete_master.id
-      SQL
+      .merge(variants_distributed_by_distributor)
+      .joins(left_join_products_with_obsolete_master)
       .where('products_with_obsolete_master.id IS NULL')
       .where('spree_products.deleted_at IS NULL')
       .where('variants_including_masters_spree_products.deleted_at IS NULL')
@@ -43,6 +29,33 @@ class OrderCycleDistributedProducts
   private
 
   attr_reader :order_cycle, :distributor
+
+  def variants_distributed_by_distributor
+    distributor_inventory_variants
+      .merge(Exchange.in_order_cycle(order_cycle.id))
+      .merge(Exchange.outgoing)
+      .merge(Exchange.to_enterprise(distributor))
+  end
+
+  def distributor_inventory_variants
+    Spree::Product
+      .joins(<<-SQL)
+        LEFT OUTER JOIN (
+          SELECT * FROM inventory_items WHERE enterprise_id = #{distributor.id}
+        ) AS o_inventory_items
+        ON o_inventory_items.variant_id = variants_including_masters_spree_products.id
+      SQL
+      .where("o_inventory_items.id IS NULL OR o_inventory_items.visible = true")
+  end
+
+  def left_join_products_with_obsolete_master
+    <<-SQL.strip_heredoc
+        LEFT JOIN (#{products_with_obsolete_master.to_sql})
+        AS products_with_obsolete_master
+        ON variants_including_masters_spree_products.product_id
+          = products_with_obsolete_master.id
+    SQL
+  end
 
   # TODO: filter by products supplied by the OC suppliers so we don't go through the whole products table.
   def products_with_obsolete_master
