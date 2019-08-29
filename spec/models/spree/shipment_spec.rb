59504e20051a6119daa708fb9a057b518a4bc76b
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2019-08-29 20:26:23 +0100

    Add failing test for shipping backordered items

--- spec/models/spree/shipment_spec.rb
+++ spec/models/spree/shipment_spec.rb
@@ -29,5 +29,23 @@ describe Spree::Shipment do
         expect(variants.sort_by(&:id)).to eq([deleted_variant, other_variant].sort_by(&:id))
       end
     end
+
+    context "when the order contains items with negative stock" do
+      let!(:on_demand_variant) { create(:variant, on_demand: true, on_hand: 1) }
+      let!(:pending_order) { create(:completed_order_with_totals) }
+      let!(:backordered_line_item) {
+        create(:line_item, order: pending_order, variant: on_demand_variant, quantity: 99)
+      }
+
+      before do
+        pending_order.payments << create(:payment, amount: pending_order.total, state: 'completed')
+        pending_order.update!
+      end
+
+      xit "can still be shipped" do
+        expect(pending_order.can_ship?).to be true
+        expect(pending_order.ready_to_ship?).to be true
+      end
+    end
   end
 end
