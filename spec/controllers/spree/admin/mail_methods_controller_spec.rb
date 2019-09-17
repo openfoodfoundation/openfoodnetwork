6677543de0dba2aca0a8b435867af604e96efd68
luisramos0 <luisramos0@gmail.com>
2019-09-17 17:55:00 +0100

    bring 2 specs from spree_backend to cover image_settings page and mail_methods page

---
+++ spec/controllers/spree/admin/mail_methods_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe Spree::Admin::MailMethodsController do
+  stub_authorization!
+
+  context "#update" do
+    it "should reinitialize the mail settings" do
+      Spree::Core::MailSettings.should_receive(:init)
+      spree_put :update, { :enable_mail_delivery => "1", :mails_from => "spree@example.com" }
+    end
+  end
+
+  it "can trigger testmail" do
+    request.env["HTTP_REFERER"] = "/"
+    user = double('User', email: 'user@spree.com', spree_api_key: 'fake')
+    controller.stub(:try_spree_current_user => user)
+    Spree::Config[:enable_mail_delivery] = "1"
+    ActionMailer::Base.perform_deliveries = true
+
+    expect {
+      spree_post :testmail
+    }.to change { ActionMailer::Base.deliveries.size }.by(1)
+  end
+end
