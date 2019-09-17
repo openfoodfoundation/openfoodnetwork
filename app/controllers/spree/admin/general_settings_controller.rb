2377b833ee29218f97f89c470d439cef1c79f52d
luisramos0 <luisramos0@gmail.com>
2019-09-17 16:02:37 +0100

    Bring general settings and image settings controllers that are overrides in ofn to ofn so we can merge them with their decorators in a second step

---
+++ app/controllers/spree/admin/general_settings_controller.rb
@@ -0,0 +1,34 @@
+module Spree
+  module Admin
+    class GeneralSettingsController < Spree::Admin::BaseController
+
+      def edit
+        @preferences_general = [:site_name, :default_seo_title, :default_meta_keywords,
+                        :default_meta_description, :site_url]
+        @preferences_security = [:allow_ssl_in_production,
+                        :allow_ssl_in_staging, :allow_ssl_in_development_and_test,
+                        :check_for_spree_alerts]
+        @preferences_currency = [:display_currency, :hide_cents]
+      end
+
+      def update
+        params.each do |name, value|
+          next unless Spree::Config.has_preference? name
+          Spree::Config[name] = value
+        end
+        flash[:success] = Spree.t(:successfully_updated, :resource => Spree.t(:general_settings))
+
+        redirect_to edit_admin_general_settings_path
+      end
+
+      def dismiss_alert
+        if request.xhr? and params[:alert_id]
+          dismissed = Spree::Config[:dismissed_spree_alerts] || ''
+          Spree::Config.set :dismissed_spree_alerts => dismissed.split(',').push(params[:alert_id]).join(',')
+          filter_dismissed_alerts
+          render :nothing => true
+        end
+      end
+    end
+  end
+end
