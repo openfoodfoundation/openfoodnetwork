713769b4975023705a17951f236091c85e06848b
luisramos0 <luisramos0@gmail.com>
2019-09-17 18:18:26 +0100

    Fix rubocop issues in mail_methods_controller_spec

--- spec/controllers/spree/admin/mail_methods_controller_spec.rb
+++ spec/controllers/spree/admin/mail_methods_controller_spec.rb
@@ -1,19 +1,25 @@
 require 'spec_helper'
 
 describe Spree::Admin::MailMethodsController do
-  stub_authorization!
+  include AuthenticationWorkflow
+
+  before { login_as_admin }
 
   context "#update" do
     it "should reinitialize the mail settings" do
-      Spree::Core::MailSettings.should_receive(:init)
-      spree_put :update, { :enable_mail_delivery => "1", :mails_from => "spree@example.com" }
+      expect(Spree::Core::MailSettings).to receive(:init)
+      spree_put :update, enable_mail_delivery: "1", mails_from: "spree@example.com"
     end
   end
 
   it "can trigger testmail" do
     request.env["HTTP_REFERER"] = "/"
-    user = double('User', email: 'user@spree.com', spree_api_key: 'fake')
-    controller.stub(:try_spree_current_user => user)
+    user = double('User', email: 'user@spree.com',
+                          spree_api_key: 'fake',
+                          id: nil,
+                          owned_groups: nil)
+    allow(user).to receive_messages(enterprises: [create(:enterprise)], has_spree_role?: true)
+    allow(controller).to receive_messages(try_spree_current_user: user)
     Spree::Config[:enable_mail_delivery] = "1"
     ActionMailer::Base.perform_deliveries = true
 
