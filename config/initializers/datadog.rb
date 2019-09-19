e6387197ec49f158ab5382bf53ebb0099520743d
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2019-09-19 16:55:29 +0100

    Add memcached APM

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -2,5 +2,6 @@ if ENV['DATADOG_RAILS_APM']
   Datadog.configure do |c|
     c.use :rails, service_name: 'rails'
     c.use :delayed_job, service_name: 'delayed_job'
+    c.use :dalli, service_name: 'memcached'
   end
 end
