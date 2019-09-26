bef4741e318e1e58e9366c05d313060ed844e195
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2019-09-26 22:38:20 +0100

    Enable analytics in all APM services

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,7 +1,7 @@
 if ENV['DATADOG_RAILS_APM']
   Datadog.configure do |c|
-    c.use :rails, service_name: 'rails'
-    c.use :delayed_job, service_name: 'delayed_job'
-    c.use :dalli, service_name: 'memcached'
+    c.use :rails, service_name: 'rails', analytics_enabled: true
+    c.use :delayed_job, service_name: 'delayed_job', analytics_enabled: true
+    c.use :dalli, service_name: 'memcached', analytics_enabled: true
   end
 end
