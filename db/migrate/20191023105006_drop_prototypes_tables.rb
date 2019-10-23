a6cb5903d696b3df79a59f770eaa0efa6a899e0a
luisramos0 <luisramos0@gmail.com>
2019-10-23 12:16:34 +0100

    Delete prototypes tables and all references in pages

---
+++ db/migrate/20191023105006_drop_prototypes_tables.rb
@@ -0,0 +1,25 @@
+class DropPrototypesTables < ActiveRecord::Migration
+  def up
+    drop_table :spree_option_types_prototypes
+    drop_table :spree_properties_prototypes
+    drop_table :spree_prototypes
+  end
+
+  def down
+    create_table :spree_prototypes do |t|
+      t.string   :name
+      t.datetime :created_at, null: false
+      t.datetime :updated_at, null: false
+    end
+
+    create_table :spree_option_types_prototypes, id: false do |t|
+      t.integer :prototype_id
+      t.integer :option_type_id
+    end
+
+    create_table :spree_properties_prototypes, id: false do |t|
+      t.integer :prototype_id
+      t.integer :property_id
+    end
+  end
+end
