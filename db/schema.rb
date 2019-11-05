9e1b2eb4ca3fef94439da1d6bfaccf0a4f11876e
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2019-11-05 22:58:14 +0000

    Fix migration mismatch
    It looks like this was probably changed whilst resolving a merge conflict somewhere. The number doesn't match the last migration file, and it's breaking the ofn-install CI build (as well as migrations on fresh servers).

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20191023105006) do
+ActiveRecord::Schema.define(:version => 20191023172424) do
 
   create_table "adjustment_metadata", :force => true do |t|
     t.integer "adjustment_id"
