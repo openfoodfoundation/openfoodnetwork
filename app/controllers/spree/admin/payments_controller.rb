61ce849546865e9e1f76e746b658411cf074a0a1
luisramos0 <luisramos0@gmail.com>
2019-11-11 16:36:07 +0000

    Merge spree/admin/payments_controller with its decorator and fix a few rubocop issues

--- app/controllers/spree/admin/payments_controller.rb
+++ app/controllers/spree/admin/payments_controller.rb
@@ -1,10 +1,12 @@
 module Spree
   module Admin
     class PaymentsController < Spree::Admin::BaseController
-      before_filter :load_order, :only => [:create, :new, :index, :fire]
-      before_filter :load_payment, :except => [:create, :new, :index]
+      before_filter :load_order, only: [:create, :new, :index, :fire]
+      before_filter :load_payment, except: [:create, :new, :index]
       before_filter :load_data
       before_filter :can_transition_to_payment
+
+      append_before_filter :filter_payment_methods
 
       respond_to :html
 
@@ -19,7 +21,10 @@ module Spree
 
       def create
         @payment = @order.payments.build(object_params)
-        if @payment.payment_method.is_a?(Spree::Gateway) && @payment.payment_method.payment_profiles_supported? && params[:card].present? and params[:card] != 'new'
+        if @payment.payment_method.is_a?(Spree::Gateway) &&
+           @payment.payment_method.payment_profiles_supported? &&
+           params[:card].present? &&
+           (params[:card] != 'new')
           @payment.source = CreditCard.find_by_id(params[:card])
         end
 
@@ -33,42 +38,50 @@ module Spree
             @payment.process!
             flash[:success] = flash_message_for(@payment, :successfully_created)
 
-             redirect_to admin_order_payments_path(@order)
+            redirect_to admin_order_payments_path(@order)
           else
-            #This is the first payment (admin created order)
-            until @order.completed?
-              @order.next!
-            end
+            AdvanceOrderService.new(@order).call!
+
             flash[:success] = Spree.t(:new_order_completed)
             redirect_to edit_admin_order_url(@order)
           end
-
         rescue Spree::Core::GatewayError => e
-          flash[:error] = "#{e.message}"
+          flash[:error] = e.message.to_s
           redirect_to new_admin_order_payment_path(@order)
         end
       end
 
+      # When a user fires an event, take them back to where they came from
+      # (we can't use respond_override because Spree no longer uses respond_with)
       def fire
-        return unless event = params[:e] and @payment.payment_source
+        event = params[:e]
+        return unless event && @payment.payment_source
 
         # Because we have a transition method also called void, we do this to avoid conflicts.
         event = "void_transaction" if event == "void"
-        if @payment.send("#{event}!")
-          flash[:success] = Spree.t(:payment_updated)
+        if @payment.public_send("#{event}!")
+          flash[:success] = t(:payment_updated)
         else
-          flash[:error] = Spree.t(:cannot_perform_operation)
+          flash[:error] = t(:cannot_perform_operation)
         end
-      rescue Spree::Core::GatewayError => ge
-        flash[:error] = "#{ge.message}"
+      rescue Spree::Core::GatewayError => e
+        flash[:error] = e.message
       ensure
-        redirect_to admin_order_payments_path(@order)
+        redirect_to request.referer
       end
 
       private
 
+      # Only show payments for the order's distributor
+      def filter_payment_methods
+        @payment_methods = @payment_methods.select{ |pm| pm.has_distributor? @order.distributor }
+        @payment_method ||= @payment_methods.first
+      end
+
       def object_params
-        if params[:payment] and params[:payment_source] and source_params = params.delete(:payment_source)[params[:payment][:payment_method_id]]
+        if params[:payment] &&
+           params[:payment_source] &&
+           source_params = params.delete(:payment_source)[params[:payment][:payment_method_id]]
           params[:payment][:source_attributes] = source_params
         end
         params[:payment]
@@ -77,11 +90,11 @@ module Spree
       def load_data
         @amount = params[:amount] || load_order.total
         @payment_methods = PaymentMethod.available(:back_end)
-        if @payment and @payment.payment_method
-          @payment_method = @payment.payment_method
-        else
-          @payment_method = @payment_methods.first
-        end
+        @payment_method = if @payment && @payment.payment_method
+                            @payment.payment_method
+                          else
+                            @payment_methods.first
+                          end
         @previous_cards = @order.credit_cards.with_payment_profile
       end
 
@@ -92,10 +105,10 @@ module Spree
       #
       # Otherwise redirect user to that step
       def can_transition_to_payment
-        unless @order.payment? || @order.complete?
-          flash[:notice] = Spree.t(:fill_in_customer_info)
-          redirect_to edit_admin_order_customer_url(@order)
-        end
+        return if @order.payment? || @order.complete?
+
+        flash[:notice] = Spree.t(:fill_in_customer_info)
+        redirect_to edit_admin_order_customer_url(@order)
       end
 
       def load_order
