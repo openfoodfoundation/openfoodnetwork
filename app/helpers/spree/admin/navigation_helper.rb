1b18595d9400d842bc5388f232c3d7aefdc8032f
luisramos0 <luisramos0@gmail.com>
2019-12-14 21:14:26 +0000

    Merge navigator_helper with decorator

--- app/helpers/spree/admin/navigation_helper.rb
+++ app/helpers/spree/admin/navigation_helper.rb
@@ -42,6 +42,17 @@ module Spree
         content_tag('li', link, :class => css_classes.join(' '))
       end
 
+      def tab_with_cancan_check(*args)
+        options = { label: args.first.to_s }
+        if args.last.is_a?(Hash)
+          options = options.merge(args.last)
+        end
+        return '' if (klass = klass_for(options[:label])) && cannot?(:admin, klass)
+
+        tab_without_cancan_check(*args)
+      end
+      alias_method_chain :tab, :cancan_check
+
       # finds class for a given symbol / string
       #
       # Example :
@@ -53,10 +64,24 @@ module Spree
       def klass_for(name)
         model_name = name.to_s
 
-        ["Spree::#{model_name.classify}", model_name.classify, model_name.gsub('_', '/').classify].find do |t|
-          t.safe_constantize
-        end.try(:safe_constantize)
+        ["Spree::#{model_name.classify}", model_name.classify, model_name.tr('_', '/').classify].find(&:safe_constantize).try(:safe_constantize)
       end
+
+      # Make it so that the Reports admin tab can be enabled/disabled through the cancan
+      # :report resource, since it does not have a corresponding resource class (unlike
+      # eg. Spree::Product).
+      def klass_for_with_sym_fallback(name)
+        klass = klass_for_without_sym_fallback(name)
+        klass ||= name.singularize.to_sym
+        klass = :overview if klass == :dashboard
+        klass = Spree::Order if klass == :bulk_order_management
+        klass = EnterpriseGroup if klass == :group
+        klass = VariantOverride if klass == :Inventory
+        klass = ProductImport::ProductImporter if klass == :import
+        klass = Spree::Admin::ReportsController if klass == :report
+        klass
+      end
+      alias_method_chain :klass_for, :sym_fallback      
 
       def link_to_clone(resource, options={})
         options[:data] = {:action => 'clone'}
