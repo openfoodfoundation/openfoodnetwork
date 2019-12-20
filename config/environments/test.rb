08ae9f3f55e48e0df712363bf523244b16588516
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2019-12-20 13:12:22 +0100

    Add missing eager_load setting in test.rb

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,6 +7,8 @@ Openfoodnetwork::Application.configure do
   # and recreated between test runs.  Don't rely on the data there!
   config.cache_classes = false
 
+  config.eager_load = false
+
   # Configure static asset server for tests with Cache-Control for performance
   config.serve_static_assets = true
   config.static_cache_control = "public, max-age=3600"
