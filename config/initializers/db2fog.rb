a2eff3025d2abcd206c8f5b99b7753a664e4b2ec
Maikel Linke <maikel@email.org.au>
2019-12-20 15:42:02 +1100

    Load DB2Fog configuration from file if it exists
    This enables us to define a custom configuration file in ofn-install
    (not yet implemented yet).

--- config/initializers/db2fog.rb
+++ config/initializers/db2fog.rb
@@ -1,11 +1,16 @@
 require_relative 'spree'
 
-# See: https://github.com/yob/db2fog
-DB2Fog.config = {
-    :aws_access_key_id     => Spree::Config[:s3_access_key],
-    :aws_secret_access_key => Spree::Config[:s3_secret],
-    :directory             => ENV['S3_BACKUPS_BUCKET'],
-    :provider              => 'AWS'
-}
+# Use a config file provided by ofn-install if available.
+if File.exists?(Rails.root.join("config", "db2fog.yml"))
+  DB2Fog.config = YAML.load_file(Rails.root.join("config", "db2fog.yml"))
+else
+  # See: https://github.com/yob/db2fog
+  DB2Fog.config = {
+      :aws_access_key_id     => Spree::Config[:s3_access_key],
+      :aws_secret_access_key => Spree::Config[:s3_secret],
+      :directory             => ENV['S3_BACKUPS_BUCKET'],
+      :provider              => 'AWS'
+  }
 
-DB2Fog.config[:region] = ENV['S3_REGION'] if ENV['S3_REGION']
+  DB2Fog.config[:region] = ENV['S3_REGION'] if ENV['S3_REGION']
+end
