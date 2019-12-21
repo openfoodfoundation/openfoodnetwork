2063473846be1d5e544ec0ce99baf1ce3ba0865a
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2019-12-21 01:42:04 +0100

    Move filter parameters to initializer for Rails 4

--- config/application.rb
+++ config/application.rb
@@ -137,9 +137,6 @@ module Openfoodnetwork
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
-    # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
-
     # Enable the asset pipeline
     config.assets.enabled = true
 
