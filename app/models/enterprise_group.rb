a02c58e23101ffc7d0aec7792e387be9b04edcda
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2019-12-21 19:51:52 +0100

    Add join_table to enterprise groups relation
    has_and_belongs_to_many relationships now require a join_table

--- app/models/enterprise_group.rb
+++ app/models/enterprise_group.rb
@@ -5,7 +5,7 @@ class EnterpriseGroup < ActiveRecord::Base
   include PermalinkGenerator
   acts_as_list
 
-  has_and_belongs_to_many :enterprises
+  has_and_belongs_to_many :enterprises, join_table: 'enterprise_groups_enterprises'
   belongs_to :owner, class_name: 'Spree::User', foreign_key: :owner_id, inverse_of: :owned_groups
   belongs_to :address, class_name: 'Spree::Address'
   accepts_nested_attributes_for :address
