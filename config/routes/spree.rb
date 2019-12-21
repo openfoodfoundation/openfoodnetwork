fb2db90bc75c1d6217ff9c1d1332a5caa93ef80a
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2019-12-21 16:08:30 +0100

    Temporarily fix root_path missing error in many controllers

--- config/routes/spree.rb
+++ config/routes/spree.rb
@@ -1,5 +1,7 @@
 # Overriding Devise routes to use our own controller
 Spree::Core::Engine.routes.draw do
+  root to: 'home#index'
+
   devise_for :spree_user,
              :class_name => 'Spree::User',
              :controllers => { :sessions => 'spree/user_sessions',
