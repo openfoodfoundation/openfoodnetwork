d9378207c866d857c21473de62b3d85f14320eaf
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2019-12-22 23:58:45 +0100

    Fix reports method #url_for_report

--- app/controllers/spree/admin/reports_controller_decorator.rb
+++ app/controllers/spree/admin/reports_controller_decorator.rb
@@ -285,9 +285,9 @@ Spree::Admin::ReportsController.class_eval do
   end
 
   def url_for_report(report)
-    public_send("admin_reports_#{report}_url".to_sym)
+    spree.public_send("admin_reports_#{report}_url".to_sym)
   rescue NoMethodError
-    url_for([:new, :admin, :reports, report.to_s.singularize])
+    url_for([:admin, :reports, report.to_s])
   end
 
   def timestamp
