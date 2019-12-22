196b10ec3a1b2e818503173667bd0a117ba190d6
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2019-12-22 19:35:44 +0100

    Remove Spree::Admin::NavigationHelper method overrides now that Spree has implemented the logic we wanted

--- app/helpers/spree/admin/navigation_helper_decorator.rb
+++ app/helpers/spree/admin/navigation_helper_decorator.rb
@@ -1,13 +1,6 @@
 module Spree
   module Admin
     module NavigationHelper
-      # TEMP: import method until it is re-introduced into Spree.
-      def klass_for(name)
-        model_name = name.to_s
-
-        ["Spree::#{model_name.classify}", model_name.classify, model_name.tr('_', '/').classify].find(&:safe_constantize).try(:safe_constantize)
-      end
-
       # Make it so that the Reports admin tab can be enabled/disabled through the cancan
       # :report resource, since it does not have a corresponding resource class (unlike
       # eg. Spree::Product).
@@ -23,18 +16,6 @@ module Spree
         klass
       end
       alias_method_chain :klass_for, :sym_fallback
-
-      # TEMP: override method until it is fixed in Spree.
-      def tab_with_cancan_check(*args)
-        options = { label: args.first.to_s }
-        if args.last.is_a?(Hash)
-          options = options.merge(args.last)
-        end
-        return '' if (klass = klass_for(options[:label])) && cannot?(:admin, klass)
-
-        tab_without_cancan_check(*args)
-      end
-      alias_method_chain :tab, :cancan_check
     end
   end
 end
