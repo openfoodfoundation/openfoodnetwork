e06449aee3946c2d2a42c086b040870f40f45e50
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2020-01-07 12:29:42 +0100

    Allow specifying an alternate region for db2fog backups

--- config/initializers/db2fog.rb
+++ config/initializers/db2fog.rb
@@ -8,4 +8,8 @@ DB2Fog.config = {
     :provider              => 'AWS'
 }
 
-DB2Fog.config[:region] = ENV['S3_REGION'] if ENV['S3_REGION']
+region = ENV['S3_BACKUPS_REGION'] || ENV['S3_REGION']
+
+# If no region is defined we leave this config key undefined (instead of nil),
+# so that db2fog correctly applies it's default
+DB2Fog.config[:region] = region if region
