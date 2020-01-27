1d76cc17a8ac985fc884350ef53023b9999f9bf5
luisramos0 <luisramos0@gmail.com>
2020-01-27 16:43:31 +0000

    Adapt CreditCard cloner to clone not 'cards of the platform account to payment_methods of the connected accounts' but instead 'cards or payment_methods of the platform account to payment_methods of the connected accounts'
    This process mimicks the existing process of generating a token on the connected account from a card on the platform account. In the Payment Intents API we need to create a payment method in the connected account, a token is not enough

--- spec/lib/stripe/credit_card_cloner_spec.rb
+++ spec/lib/stripe/credit_card_cloner_spec.rb
@@ -5,7 +5,7 @@ require 'stripe/credit_card_cloner'
 
 module Stripe
   describe CreditCardCloner do
-    describe "#clone!" do
+    describe "#clone" do
       let(:cloner) { Stripe::CreditCardCloner.new }
 
       let(:customer_id) { "cus_A123" }
@@ -14,11 +14,16 @@ module Stripe
       let(:new_payment_method_id) { "pm_4567" }
       let(:stripe_account_id) { "acct_456" }
       let(:customer_response_mock) { { status: 200, body: customer_response_body } }
+      let(:payment_method_response_mock) { { status: 200, body: payment_method_response_body } }
 
       let(:credit_card) { create(:credit_card, user: create(:user)) }
 
       before do
         allow(Stripe).to receive(:api_key) { "sk_test_12345" }
+
+        stub_request(:post, "https://api.stripe.com/v1/payment_methods")
+          .with(basic_auth: ["sk_test_12345", ""])
+          .to_return(payment_method_response_mock)
 
         stub_request(:post, "https://api.stripe.com/v1/customers")
           .with(basic_auth: ["sk_test_12345", ""], body: { email: credit_card.user.email })
@@ -26,12 +31,15 @@ module Stripe
       end
 
       context "when called with a credit_card with valid id (card_*)" do
+        let(:payment_method_response_body) {
+          JSON.generate(id: new_payment_method_id, default_card: card_id)
+        }
         let(:customer_response_body) {
           JSON.generate(id: customer_id, default_card: card_id)
         }
 
         it "clones the card successefully" do
-          cloner.clone!(credit_card, stripe_account_id)
+          cloner.clone(credit_card, stripe_account_id)
 
           expect(credit_card.gateway_customer_profile_id).to eq new_customer_id
           expect(credit_card.gateway_payment_profile_id).to eq new_payment_method_id
