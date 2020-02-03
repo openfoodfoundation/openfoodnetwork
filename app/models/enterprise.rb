285c78a5e492d1a83f37485d374a7c39f75248df
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2020-02-04 00:57:50 +0100

    Remove use of #pluck and ensure subquery does not include all columns

--- app/models/enterprise.rb
+++ app/models/enterprise.rb
@@ -116,7 +116,10 @@ class Enterprise < ActiveRecord::Base
   scope :not_ready_for_checkout, lambda {
     # When ready_for_checkout is empty, return all rows when there are no enterprises ready for
     # checkout.
-    ready_enterprises = Enterprise.ready_for_checkout.pluck(:id)
+    ready_enterprises = Enterprise.ready_for_checkout.
+      except(:select).
+      select('DISTINCT enterprises.id')
+
     if ready_enterprises.present?
       where("enterprises.id NOT IN (?)", ready_enterprises)
     else
