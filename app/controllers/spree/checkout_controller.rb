858d2cc6c29570229866296ebab017bdd4b208dc
luisramos0 <luisramos0@gmail.com>
2020-02-07 11:47:01 +0000

    Add doc to Spree::CheckoutController to make it more obvious why this controller exists

--- app/controllers/spree/checkout_controller.rb
+++ app/controllers/spree/checkout_controller.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+# This controller (and respective route in the Spree engine)
+#   is only needed for the spree_paypal_express gem that redirects here explicitly.
+#
+# According to the rails docs it would be possible to redirect
+#   to CheckoutController directly in the routes
+#   with a slash like "to: '/checkout#edit'", but it does not work in this case.
 module Spree
   class CheckoutController < Spree::StoreController
     def edit
