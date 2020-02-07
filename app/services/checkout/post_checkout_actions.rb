cb25c270e800ca2567d4f3ad9528c2969f6758f9
luisramos0 <luisramos0@gmail.com>
2020-02-07 17:04:47 +0000

    Extract PostCheckoutActions from checkout controller

---
+++ app/services/checkout/post_checkout_actions.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+# Executes actions after checkout
+module Checkout
+  class PostCheckoutActions
+    def initialize(order)
+      @order = order
+    end
+
+    def success(controller, params, current_user)
+      save_order_addresses_as_user_default(params, current_user)
+      ResetOrderService.new(controller, @order).call
+    end
+
+    def failure
+      @order.updater.shipping_address_from_distributor
+      RestartCheckout.new(@order).call
+    end
+
+    private
+
+    def save_order_addresses_as_user_default(params, current_user)
+      return unless params[:order]
+
+      user_default_address_setter = UserDefaultAddressSetter.new(@order, current_user)
+      user_default_address_setter.set_default_bill_address if params[:order][:default_bill_address]
+      user_default_address_setter.set_default_ship_address if params[:order][:default_ship_address]
+    end
+  end
+end
