e36b0249b9032c776368a571eee27985f7fd0d22
Luis Ramos <luisramos0@gmail.com>
2020-02-11 17:27:02 +0000

    Use nested module names to fix rubocpo issue

--- engines/order_management/app/services/order_management/subscriptions/payment_setup.rb
+++ engines/order_management/app/services/order_management/subscriptions/payment_setup.rb
@@ -1,67 +1,69 @@
 # frozen_string_literal: true
 
-module OrderManagement::Subscriptions
-  class PaymentSetup
-    def initialize(order)
-      @order = order
-    end
+module OrderManagement
+  module Subscriptions
+    class PaymentSetup
+      def initialize(order)
+        @order = order
+      end
 
-    def call!
-      create_payment
-      ensure_payment_source
-      return if order.errors.any?
+      def call!
+        create_payment
+        ensure_payment_source
+        return if order.errors.any?
 
-      payment.update_attributes(amount: order.outstanding_balance)
-    end
+        payment.update_attributes(amount: order.outstanding_balance)
+      end
 
-    private
+      private
 
-    attr_reader :order
+      attr_reader :order
 
-    def payment
-      @payment ||= order.pending_payments.last
-    end
+      def payment
+        @payment ||= order.pending_payments.last
+      end
 
-    def create_payment
-      return if payment.present?
+      def create_payment
+        return if payment.present?
 
-      @payment = order.payments.create(
-        payment_method_id: order.subscription.payment_method_id,
-        amount: order.outstanding_balance
-      )
-    end
+        @payment = order.payments.create(
+          payment_method_id: order.subscription.payment_method_id,
+          amount: order.outstanding_balance
+        )
+      end
 
-    def card_required?
-      [Spree::Gateway::StripeConnect,
-       Spree::Gateway::StripeSCA].include? payment.payment_method.class
-    end
+      def card_required?
+        [Spree::Gateway::StripeConnect,
+         Spree::Gateway::StripeSCA].include? payment.payment_method.class
+      end
 
-    def card_set?
-      payment.source is_a? Spree::CreditCard
-    end
+      def card_set?
+        payment.source is_a? Spree::CreditCard
+      end
 
-    def ensure_payment_source
-      return unless card_required? && !card_set?
+      def ensure_payment_source
+        return unless card_required? && !card_set?
 
-      ensure_credit_card || order.errors.add(:base, :no_card)
-    end
+        ensure_credit_card || order.errors.add(:base, :no_card)
+      end
 
-    def ensure_credit_card
-      return false if saved_credit_card.blank? || !allow_charges?
+      def ensure_credit_card
+        return false if saved_credit_card.blank? || !allow_charges?
 
-      payment.update_attributes(source: saved_credit_card)
-    end
+        payment.update_attributes(source: saved_credit_card)
+      end
 
-    def allow_charges?
-      order.customer.allow_charges?
-    end
+      def allow_charges?
+        order.customer.allow_charges?
+      end
 
-    def saved_credit_card
-      order.user.default_card
-    end
+      def saved_credit_card
+        order.user.default_card
+      end
 
-    def errors_present?
-      order.errors.any?
+      def errors_present?
+        order.errors.any?
+      end
     end
   end
 end
