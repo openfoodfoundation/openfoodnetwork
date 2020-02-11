15231a9128c8ad4322b9eed271c3940b1da9b5a1
Luis Ramos <luisramos0@gmail.com>
2020-02-11 12:23:05 +0000

    Make SubsConfirmJob more readable

--- app/jobs/subscription_confirm_job.rb
+++ app/jobs/subscription_confirm_job.rb
@@ -1,16 +1,9 @@
 require 'open_food_network/subscription_summarizer'
 
+# Confirms orders of unconfirmed proxy orders in recently closed Order Cycles
 class SubscriptionConfirmJob
   def perform
-    ids = proxy_orders.pluck(:id)
-    proxy_orders.update_all(confirmed_at: Time.zone.now)
-    ProxyOrder.where(id: ids).each do |proxy_order|
-      Rails.logger.info "Confirming Order for Proxy Order #{proxy_order.id}"
-      @order = proxy_order.order
-      process!
-    end
-
-    send_confirmation_summary_emails
+    confirm_proxy_orders!
   end
 
   private
@@ -22,7 +15,23 @@ class SubscriptionConfirmJob
     @summarizer ||= OpenFoodNetwork::SubscriptionSummarizer.new
   end
 
-  def proxy_orders
+  def confirm_proxy_orders!
+    # Fetch all unconfirmed proxy orders
+    unconfirmed_proxy_orders_ids = unconfirmed_proxy_orders.pluck(:id)
+
+    # Mark these proxy orders as confirmed
+    unconfirmed_proxy_orders.update_all(confirmed_at: Time.zone.now)
+
+    # Confirm these proxy orders
+    ProxyOrder.where(id: unconfirmed_proxy_orders_ids).each do |proxy_order|
+      Rails.logger.info "Confirming Order for Proxy Order #{proxy_order.id}"
+      confirm_order!(proxy_order.order)
+    end
+
+    send_confirmation_summary_emails
+  end
+
+  def unconfirmed_proxy_orders
     ProxyOrder.not_canceled.where('confirmed_at IS NULL AND placed_at IS NOT NULL')
       .joins(:order_cycle).merge(recently_closed_order_cycles)
       .joins(:order).merge(Spree::Order.complete.not_state('canceled'))
@@ -32,30 +41,32 @@ class SubscriptionConfirmJob
     OrderCycle.closed.where('order_cycles.orders_close_at BETWEEN (?) AND (?) OR order_cycles.updated_at BETWEEN (?) AND (?)', 1.hour.ago, Time.zone.now, 1.hour.ago, Time.zone.now)
   end
 
-  def process!
-    record_order(@order)
-    setup_payment! if @order.payment_required?
-    return send_failed_payment_email if @order.errors.present?
+  # It sets up payments, processes payments and sends confirmation emails
+  def confirm_order!(order)
+    record_order(order)
 
-    @order.process_payments! if @order.payment_required?
-    return send_failed_payment_email if @order.errors.present?
+    setup_payment!(order) if order.payment_required?
+    return send_failed_payment_email(order) if order.errors.present?
 
-    send_confirm_email
+    order.process_payments! if order.payment_required?
+    return send_failed_payment_email(order) if order.errors.present?
+
+    send_confirmation_email(order)
   end
 
-  def setup_payment!
-    Subscriptions::PaymentSetup.new(@order).call!
+  def setup_payment!(order)
+    Subscriptions::PaymentSetup.new(order).call!
   end
 
-  def send_confirm_email
-    @order.update!
-    record_success(@order)
-    SubscriptionMailer.confirmation_email(@order).deliver
+  def send_confirmation_email(order)
+    order.update!
+    record_success(order)
+    SubscriptionMailer.confirmation_email(order).deliver
   end
 
-  def send_failed_payment_email
-    @order.update!
-    record_and_log_error(:failed_payment, @order)
-    SubscriptionMailer.failed_payment_email(@order).deliver
+  def send_failed_payment_email(order)
+    order.update!
+    record_and_log_error(:failed_payment, order)
+    SubscriptionMailer.failed_payment_email(order).deliver
   end
 end
