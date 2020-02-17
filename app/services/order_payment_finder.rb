3e0a5bac6a7e44e1dee4837b2c086fddde105f0a
Maikel Linke <maikel@email.org.au>
2020-02-18 10:40:58 +1100

    Move helper to service for re-use

--- app/services/order_payment_finder.rb
+++ app/services/order_payment_finder.rb
@@ -1,5 +1,7 @@
-module OrdersHelper
-  def order_paid_via(order)
+# frozen_string_literal: true
+
+module OrderPaymentFinder
+  def self.last_payment_method(order)
     # `sort_by` avoids additional database queries when payments are loaded
     # already. There is usually only one payment and this shouldn't cause
     # any overhead compared to `order(:updated_at)`.
