431076fc6d8d8b6482490d3ab4a7a5815cd151cf
Luis Ramos <luisramos0@gmail.com>
2020-02-22 19:03:48 +0000

    Add strong parameters permits to admin users_controller

--- app/controllers/spree/admin/users_controller.rb
+++ app/controllers/spree/admin/users_controller.rb
@@ -22,7 +22,7 @@ module Spree
           roles = params[:user].delete("spree_role_ids")
         end
 
-        @user = Spree::User.new(params[:user])
+        @user = Spree::User.new(user_params)
         if @user.save
 
           if roles
@@ -41,7 +41,7 @@ module Spree
           roles = params[:user].delete("spree_role_ids")
         end
 
-        if @user.update_attributes(params[:user])
+        if @user.update_attributes(user_params)
           if roles
             @user.spree_roles = roles.reject(&:blank?).collect{ |r| Spree::Role.find(r) }
           end
@@ -135,6 +135,10 @@ module Spree
 
       def new_email_unconfirmed?
         params[:user][:email] != @user.email
+      end
+
+      def user_params
+        params.require(:user).permit(:email, :enterprise_limit, :password, :password_confirmation)
       end
     end
   end
