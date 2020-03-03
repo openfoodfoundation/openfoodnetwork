a508c5570048419f7f6ec7795baaf66b497fced3
Luis Ramos <luisramos0@gmail.com>
2020-03-03 09:27:04 +0000

    Bring TaxRate.match to OFN
    The version of this method in spree 2.1 will break our build
    
    This way we simply bypass this fix in spree: https://github.com/spree/spree/pull/3669
    
    We can get back to this in the future if we ever experience the mentioned bug

--- app/models/spree/tax_rate_decorator.rb
+++ app/models/spree/tax_rate_decorator.rb
@@ -1,6 +1,13 @@
 module Spree
   TaxRate.class_eval do
     class << self
+      def match(order)
+        return [] unless order.tax_zone
+        all.select do |rate|
+          rate.zone == order.tax_zone || rate.zone.contains?(order.tax_zone) || rate.zone.default_tax
+        end
+      end
+
       def match_with_sales_tax_registration(order)
         return [] if order.distributor && !order.distributor.charges_sales_tax
 
