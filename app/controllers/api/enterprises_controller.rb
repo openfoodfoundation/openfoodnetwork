56c29ab472752140301523dd3f26be514bf54f11
Luis Ramos <luisramos0@gmail.com>
2020-03-07 18:12:05 +0000

    Make EnterprisesController inherit from Api::BaseController to fix authentication and fix problem with saving params[:user_ids]

--- app/controllers/api/enterprises_controller.rb
+++ app/controllers/api/enterprises_controller.rb
@@ -1,5 +1,5 @@
 module Api
-  class EnterprisesController < BaseController
+  class EnterprisesController < Api::BaseController
     before_filter :override_owner, only: [:create, :update]
     before_filter :check_type, only: :update
     before_filter :override_sells, only: [:create, :update]
@@ -10,8 +10,12 @@ module Api
     def create
       authorize! :create, Enterprise
 
+      # params[:user_ids] breaks the enterprise creation
+      # We remove them from params and save them after creating the enterprise
+      user_ids = params[:enterprise].delete(:user_ids)
       @enterprise = Enterprise.new(params[:enterprise])
       if @enterprise.save
+        save_enterprise_users(user_ids)
         render text: @enterprise.id, status: :created
       else
         invalid_resource!(@enterprise)
@@ -71,6 +75,14 @@ module Api
 
     def override_visible
       params[:enterprise][:visible] = false
+    end
+
+    def save_enterprise_users(user_ids)
+      user_ids.each do |user_id|
+        next if @enterprise.user_ids.include? user_id.to_i
+
+        @enterprise.users << Spree::User.find(user_id)
+      end
     end
   end
 end
