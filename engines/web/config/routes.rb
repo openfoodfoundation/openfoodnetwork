0b05312f1940055a3881079a05bc0999c7205389
Luis Ramos <luisramos0@gmail.com>
2020-03-13 10:37:59 +0000

    Move cookies spec to web engine and adapt routes to the fact they are now normal main apps routes

--- engines/web/config/routes.rb
+++ engines/web/config/routes.rb
@@ -1,9 +1,7 @@
 Openfoodnetwork::Application.routes.prepend do
-  namespace :api do
-    scope '/cookies' do
-      resource :consent, only: [:show, :create, :destroy], controller: "cookies_consent"
-    end
+  scope '/api/cookies' do
+    resource :consent, only: [:show, :create, :destroy], controller: "web/api/cookies_consent"
   end
 
-  get "/angular-templates/:id", to: "angular_templates#show", constraints: { name: %r{[\/\w\.]+} }
+  get "/angular-templates/:id", to: "web/angular_templates#show", constraints: { name: %r{[\/\w\.]+} }
 end
