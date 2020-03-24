ffc258787ae2429ac7a09616baf9f8817da2fd7c
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2020-03-24 12:20:13 +0100

    Refactor user_balance_calculator_spec for clarity

--- spec/lib/open_food_network/user_balance_calculator_spec.rb
+++ spec/lib/open_food_network/user_balance_calculator_spec.rb
@@ -7,132 +7,114 @@ module OpenFoodNetwork
       let!(:user1) { create(:user) }
       let!(:hub1) { create(:distributor_enterprise) }
 
-      let!(:o1) {
+      let!(:order1) {
         create(:order_with_totals_and_distribution,
                user: user1, distributor: hub1,
                completed_at: 1.day.ago)
-      } # total=13 (10 + 3 shipping fee)
-      let!(:o2) {
+      }
+      let!(:order2) {
         create(:order_with_totals_and_distribution,
                user: user1, distributor: hub1,
                completed_at: 1.day.ago)
-      } # total=13 (10 + 3 shipping fee)
-      let!(:p1) {
-        create(:payment, order: o1, amount: 15.00,
-                         state: "completed")
       }
-      let!(:p2) {
-        create(:payment, order: o2, amount: 2.00,
-                         state: "completed")
-      }
+      let!(:payment1) { create(:payment, order: order1, amount: 15.00, state: "completed") }
+      let!(:payment2) { create(:payment, order: order2, amount: 2.00, state: "completed") }
+
+      let(:initial_balance) { payment1.amount + payment2.amount - order1.total - order2.total }
 
       it "finds the correct balance for this email and enterprise" do
-        expect(UserBalanceCalculator.new(o1.email, hub1).balance).to eq(-9) # = 15 + 2 - 13 - 13
+        expect(UserBalanceCalculator.new(user1.email, hub1).balance).to eq(initial_balance)
       end
 
       context "with another hub" do
         let!(:hub2) { create(:distributor_enterprise) }
-        let!(:o3) {
+        let!(:order3) {
           create(:order_with_totals_and_distribution,
                  user: user1, distributor: hub2,
                  completed_at: 1.day.ago)
-        } # total=13 (10 + 3 shipping fee)
-        let!(:p3) {
-          create(:payment, order: o3, amount: 15.00,
-                           state: "completed")
         }
+        let!(:payment3) { create(:payment, order: order3, amount: 55.00, state: "completed") }
 
         it "does not find the balance for other enterprises" do
-          expect(UserBalanceCalculator.new(o3.email, hub2).balance).to eq(2) # = 15 - 13
+          expected_balance = payment3.amount - order3.total
+          expect(UserBalanceCalculator.new(user1.email, hub2).balance).to eq(expected_balance)
         end
       end
 
       context "with another user" do
         let!(:user2) { create(:user) }
-        let!(:o4) {
+        let!(:order4) {
           create(:order_with_totals_and_distribution,
                  user: user2, distributor: hub1,
                  completed_at: 1.day.ago)
-        } # total=13 (10 + 3 shipping fee)
-        let!(:p3) {
-          create(:payment, order: o4, amount: 20.00,
-                           state: "completed")
         }
+        let!(:payment4) { create(:payment, order: order4, amount: 11.00, state: "completed") }
 
         it "does not find the balance for other users" do
-          expect(UserBalanceCalculator.new(o4.email, hub1).balance).to eq(7) # = 20 - 13
+          expected_balance = payment4.amount - order4.total
+          expect(UserBalanceCalculator.new(user2.email, hub1).balance).to eq(expected_balance)
         end
       end
 
       context "with canceled orders" do
-        let!(:o4) {
+        let!(:cancelled_order) {
           create(:order_with_totals_and_distribution,
                  user: user1, distributor: hub1,
                  completed_at: 1.day.ago, state: "canceled")
-        } # total=13 (10 + 3 shipping fee)
-        let!(:p4) {
-          create(:payment, order: o4, amount: 20.00,
-                           state: "completed")
+        }
+        let!(:cancelled_order_payment) {
+          create(:payment, order: cancelled_order, amount: 25.00, state: "completed")
         }
 
         it "does not include canceled orders in the balance" do
-          expect(UserBalanceCalculator.new(o4.email, hub1).balance).to eq(11) # = 15 + 2 + 20 - 13 - 13
+          expected_balance = initial_balance + cancelled_order_payment.amount
+          expect(UserBalanceCalculator.new(user1.email, hub1).balance).to eq(expected_balance)
         end
       end
 
       context "with void payments" do
-        let!(:o4) {
+        let!(:order5) {
           create(:order_with_totals_and_distribution,
                  user: user1, distributor: hub1,
                  completed_at: 1.day.ago)
-        } # total=13 (10 + 3 shipping fee)
-        let!(:p4) {
-          create(:payment, order: o4, amount: 20.00,
-                           state: "void")
         }
+        let!(:payment5) { create(:payment, order: order5, amount: 200.00, state: "void") }
 
         it "does not include void in the balance" do
-          expect(UserBalanceCalculator.new(o4.email, hub1).balance).to eq(-22) # = 15 + 2 - 13 - 13 - 10
+          expected_balance = initial_balance - order5.total
+          expect(UserBalanceCalculator.new(user1.email, hub1).balance).to eq(expected_balance)
         end
       end
 
       context "with invalid payments" do
-        let!(:o4) {
+        let!(:order6) {
           create(:order_with_totals_and_distribution,
                  user: user1, distributor: hub1,
                  completed_at: 1.day.ago)
-        } # total=13 (10 + 3 shipping fee)
-        let!(:p4) {
-          create(:payment, order: o4, amount: 20.00,
-                           state: "invalid")
         }
+        let!(:payment6) { create(:payment, order: order6, amount: 20.00, state: "invalid") }
 
         it "does not include invalid payments in the balance" do
-          expect(UserBalanceCalculator.new(o4.email, hub1).balance).to eq(-22) # = 15 + 2 - 13 - 13 - 10
+          expected_balance = initial_balance - order6.total
+          expect(UserBalanceCalculator.new(user1.email, hub1).balance).to eq(expected_balance)
         end
       end
 
       context "with multiple payments on single order" do
-        let!(:o4) {
+        let!(:order7) {
           create(:order_with_totals_and_distribution,
                  user: user1, distributor: hub1,
                  completed_at: 1.day.ago)
-        } # total=13 (10 + 3 shipping fee)
-        let!(:p4) {
-          create(:payment, order: o4, amount: 4.00,
-                           state: "completed")
         }
-        let!(:p5) {
-          create(:payment, order: o4, amount: 5.00,
-                           state: "completed")
-        }
-        let!(:p6) {
-          create(:payment, order: o4, amount: 6.00,
-                           state: "completed")
-        }
+        let!(:payment7) { create(:payment, order: order7, amount: 4.00, state: "completed") }
+        let!(:payment8) { create(:payment, order: order7, amount: 5.00, state: "completed") }
+        let!(:payment9) { create(:payment, order: order7, amount: 6.00, state: "completed") }
 
         it "includes orders with multiple payments in the balance" do
-          expect(UserBalanceCalculator.new(o4.email, hub1).balance).to eq(-7) # = 15 + 2 + 4 + 5 + 6 - 13 - 13 - 10
+          expected_balance = initial_balance + payment7.amount + payment8.amount \
+            + payment9.amount - order7.total
+
+          expect(UserBalanceCalculator.new(user1.email, hub1).balance).to eq(expected_balance)
         end
       end
     end
