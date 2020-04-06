c189742a21566c65ef21f26956ca4656e53332e3
Luis Ramos <luisramos0@gmail.com>
2020-04-06 12:22:33 +0100

    Merge per item calculator with its decorator

--- app/models/spree/calculator/per_item.rb
+++ app/models/spree/calculator/per_item.rb
@@ -1,20 +1,30 @@
 require_dependency 'spree/calculator'
+require 'spree/localized_number'
 
 module Spree
   class Calculator::PerItem < Calculator
+    extend Spree::LocalizedNumber
+
     preference :amount, :decimal, default: 0
     preference :currency, :string, default: Spree::Config[:currency]
+    localize_number :preferred_amount
 
     def self.description
-      Spree.t(:flat_rate_per_item)
+      I18n.t(:flat_rate_per_item)
     end
 
-    def compute(object=nil)
+    def compute(object = nil)
       return 0 if object.nil?
-      self.preferred_amount * object.line_items.reduce(0) do |sum, value|
-        value_to_add = value.quantity
+
+      number_of_line_items = line_items_for(object).reduce(0) do |sum, line_item|
+        value_to_add = if matching_products.blank? || matching_products.include?(line_item.product)
+                         line_item.quantity
+                       else
+                         0
+                       end
         sum + value_to_add
       end
+      preferred_amount * number_of_line_items
     end
   end
 end
