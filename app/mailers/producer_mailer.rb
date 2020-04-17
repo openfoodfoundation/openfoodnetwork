06ead827d8a41b6f0f0ac0491b405484ddfd09d8
Pau Perez <saulopefa@gmail.com>
2020-04-17 17:14:51 +0200

    Split long method

--- app/mailers/producer_mailer.rb
+++ app/mailers/producer_mailer.rb
@@ -1,22 +1,16 @@
+# frozen_string_literal: true
+
 class ProducerMailer < Spree::BaseMailer
   include I18nHelper
 
   def order_cycle_report(producer, order_cycle)
-    unscoping do
-      @producer = producer
-      @coordinator = order_cycle.coordinator
-      @order_cycle = order_cycle
+    @producer = producer
+    @order_cycle = order_cycle
 
-      line_items = line_items_from(@order_cycle, @producer)
-      @grouped_line_items = line_items.group_by(&:product_and_full_name)
-      @receival_instructions = @order_cycle.receival_instructions_for @producer
-      @total = total_from_line_items(line_items)
-      @tax_total = tax_total_from_line_items(line_items)
+    with_unscoped_products_and_variants do
+      load_data
 
-      I18n.with_locale valid_locale(@producer.owner) do
-        order_cycle_subject = I18n.t('producer_mailer.order_cycle.subject', producer: producer.name)
-        subject = "[#{Spree::Config.site_name}] #{order_cycle_subject}"
-
+      I18n.with_locale(owner_locale) do
         return unless orders?(order_cycle, producer)
 
         mail(
@@ -31,6 +25,26 @@ class ProducerMailer < Spree::BaseMailer
   end
 
   private
+
+  def owner_locale
+    valid_locale(@producer.owner)
+  end
+
+  def load_data
+    @coordinator = @order_cycle.coordinator
+
+    line_items = line_items_from(@order_cycle, @producer)
+
+    @grouped_line_items = line_items.group_by(&:product_and_full_name)
+    @receival_instructions = @order_cycle.receival_instructions_for(@producer)
+    @total = total_from_line_items(line_items)
+    @tax_total = tax_total_from_line_items(line_items)
+  end
+
+  def subject
+    order_cycle_subject = I18n.t('producer_mailer.order_cycle.subject', producer: @producer.name)
+    "[#{Spree::Config.site_name}] #{order_cycle_subject}"
+  end
 
   def orders?(order_cycle, producer)
     line_items_from(order_cycle, producer).any?
@@ -56,7 +70,7 @@ class ProducerMailer < Spree::BaseMailer
   # This hack makes ActiveRecord skip the default_scope (deleted_at IS NULL)
   # when eager loading associations. Further details:
   # https://github.com/rails/rails/issues/11036
-  def unscoping
+  def with_unscoped_products_and_variants
     variant_default_scopes = Spree::Variant.default_scopes
     product_default_scopes = Spree::Product.default_scopes
 
