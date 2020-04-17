04c962432a7664a716907a55a9cd15afd115c6a8
Pau Perez <saulopefa@gmail.com>
2020-04-17 12:15:37 +0200

    Skip deleted default_scope in OC notification
    This fixes the RuntimeError we get when accessing deleted variants due
    to the variant being nil.
    
    https://github.com/rails/rails/issues/11036 is still an open Rails bug,
    as some people mention in https://github.com/rails/rails/pull/21550.
    
    The issue is that `includes` doesn't respect `unscoped`. I found
    a potential solution for the entire app in
    https://github.com/rails/rails/issues/11036#issuecomment-302133116 but
    our friend @markets has a gem,
    https://github.com/markets/unscoped_associations, that solves that too.

--- app/mailers/producer_mailer.rb
+++ app/mailers/producer_mailer.rb
@@ -2,28 +2,31 @@ class ProducerMailer < Spree::BaseMailer
   include I18nHelper
 
   def order_cycle_report(producer, order_cycle)
-    @producer = producer
-    @coordinator = order_cycle.coordinator
-    @order_cycle = order_cycle
-    line_items = line_items_from(@order_cycle, @producer)
-    @grouped_line_items = line_items.group_by(&:product_and_full_name)
-    @receival_instructions = @order_cycle.receival_instructions_for @producer
-    @total = total_from_line_items(line_items)
-    @tax_total = tax_total_from_line_items(line_items)
+    unscoping do
+      @producer = producer
+      @coordinator = order_cycle.coordinator
+      @order_cycle = order_cycle
 
-    I18n.with_locale valid_locale(@producer.owner) do
-      order_cycle_subject = I18n.t('producer_mailer.order_cycle.subject', producer: producer.name)
-      subject = "[#{Spree::Config.site_name}] #{order_cycle_subject}"
+      line_items = line_items_from(@order_cycle, @producer)
+      @grouped_line_items = line_items.group_by(&:product_and_full_name)
+      @receival_instructions = @order_cycle.receival_instructions_for @producer
+      @total = total_from_line_items(line_items)
+      @tax_total = tax_total_from_line_items(line_items)
 
-      return unless has_orders?(order_cycle, producer)
+      I18n.with_locale valid_locale(@producer.owner) do
+        order_cycle_subject = I18n.t('producer_mailer.order_cycle.subject', producer: producer.name)
+        subject = "[#{Spree::Config.site_name}] #{order_cycle_subject}"
 
-      mail(
-        to: @producer.contact.email,
-        from: from_address,
-        subject: subject,
-        reply_to: @coordinator.contact.email,
-        cc: @coordinator.contact.email
-      )
+        return unless has_orders?(order_cycle, producer)
+
+        mail(
+          to: @producer.contact.email,
+          from: from_address,
+          subject: subject,
+          reply_to: @coordinator.contact.email,
+          cc: @coordinator.contact.email
+        )
+      end
     end
   end
 
@@ -35,7 +38,7 @@ class ProducerMailer < Spree::BaseMailer
 
   def line_items_from(order_cycle, producer)
     Spree::LineItem.
-      includes(variant: { option_values: :option_type }).
+      includes(variant: [:product, { option_values: :option_type }]).
       from_order_cycle(order_cycle).
       sorted_by_name_and_unit_value.
       merge(Spree::Product.in_supplier(producer)).
@@ -48,5 +51,23 @@ class ProducerMailer < Spree::BaseMailer
 
   def tax_total_from_line_items(line_items)
     Spree::Money.new line_items.sum(&:included_tax)
+  end
+
+  # This hack makes ActiveRecord skip the default_scope (deleted_at IS NULL)
+  # when eager loading associations. Further details:
+  # https://github.com/rails/rails/issues/11036
+  def unscoping
+    variant_default_scopes = Spree::Variant.default_scopes
+    product_default_scopes = Spree::Product.default_scopes
+
+    Spree::Variant.default_scopes = []
+    Spree::Product.default_scopes = []
+
+    return_value = yield
+
+    Spree::Variant.default_scopes = variant_default_scopes
+    Spree::Product.default_scopes = product_default_scopes
+
+    return_value
   end
 end
