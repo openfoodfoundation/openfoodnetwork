8af40f4675fb67c731fb93c72229e6aa251be742
Maikel Linke <maikel@email.org.au>
2020-04-17 12:47:49 +1000

    Format and indent migration message
    It's much clearer to read this way:
    
    ```
    ==  OldMigrationsRemoved: migrating ===========================================
    rake aborted!
    StandardError: An error has occurred, this and all later migrations canceled:
    
          You haven't updated your dev environment in a long time!
          Legacy migration files before 2019 have now been removed.
          Run `rails db:schema:load` before running `rails db:migrate`.
    
    ```

--- db/migrate/20181128054803_old_migrations_removed.rb
+++ db/migrate/20181128054803_old_migrations_removed.rb
@@ -1,8 +1,11 @@
 class OldMigrationsRemoved < ActiveRecord::Migration
   def up
-    message = "You haven't updated your dev environment in a long time! " \
-              "Legacy migration files before 2019 have now been removed. " \
-              "Run `rails db:schema:load` before running `rails db:migrate`."
-    raise StandardError, message
+    raise StandardError, <<-MESSAGE
+
+      You haven't updated your dev environment in a long time!
+      Legacy migration files before 2019 have now been removed.
+      Run `rails db:schema:load` before running `rails db:migrate`.
+
+    MESSAGE
   end
 end
