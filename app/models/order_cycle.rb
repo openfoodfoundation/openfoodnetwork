c5f5380783a12471d630bf2bfa1956b7ddfaf9d3
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2020-04-29 17:19:16 +0200

    Simplify relation used in #exchanges_supplying

--- app/models/order_cycle.rb
+++ app/models/order_cycle.rb
@@ -240,8 +240,8 @@ class OrderCycle < ActiveRecord::Base
   end
 
   def exchanges_supplying(order)
-    variants_relation = Spree::Variant.joins(:line_items).merge(Spree::LineItem.in_orders(order))
-    exchanges.supplying_to(order.distributor).with_any_variant(variants_relation)
+    variant_ids_relation = Spree::LineItem.in_orders(order).select(:variant_id)
+    exchanges.supplying_to(order.distributor).with_any_variant(variant_ids_relation)
   end
 
   def coordinated_by?(user)
