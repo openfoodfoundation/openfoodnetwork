deafe3235455024d51184a7c39e811a474894d47
Cillian O'Ruanaidh <contact@cilliano.com>
2020-05-08 15:40:04 +0100

    Add support for using Open Street Map on the Map and Group pages.
    The map is displayed using https://leafletjs.com/
    
    To enable Open Street Map go to the Admin -> Configuration -> Content section and click 'Open Street Map Enabled'.
    
    The 'Open Street Map Provider Name' setting can be used to configure different tile providers thanks to the Leaflet-providers extension (https://github.com/leaflet-extras/leaflet-providers)
    
    Some tile providers require an API key, this can provided in JSON format e.g. '{ apiKey: 123 }' in the 'Open Street Map Provider Options' setting.
    
    Each tile provider has their own usage policy so this should be checked before enabling Open Street Map.
    
    The search field for the Open Street Map works differently than searching on Google Maps. It matches producers by their name or address because it was easier to implement instead of matching place names all over the world.

---
+++ app/models/preference_sections/map_section.rb
@@ -0,0 +1,15 @@
+module PreferenceSections
+  class MapSection
+    def name
+      I18n.t('admin.contents.edit.map')
+    end
+
+    def preferences
+      [
+        :open_street_map_enabled,
+        :open_street_map_provider_name,
+        :open_street_map_provider_options
+      ]
+    end
+  end
+end
