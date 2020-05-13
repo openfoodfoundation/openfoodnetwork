84d973d383d816b2760a4c0c4bf926875a341cae
Kristina Lim <kristinalim.ph@gmail.com>
2020-05-14 04:19:24 +0800

    Specify RSpec.describe in StockItem spec file

--- spec/models/spree/stock_item_spec.rb
+++ spec/models/spree/stock_item_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe Spree::StockItem do
+RSpec.describe Spree::StockItem do
   let(:stock_location) { create(:stock_location_with_items) }
 
   subject { stock_location.stock_items.order(:id).first }
