e77d9aba7b452441ce8e785c65dbf5e03f08d5ab
Luis Ramos <luisramos0@gmail.com>
2020-05-13 12:12:32 +0100

    Add missing strong params to less important controllers not tested in specs

--- app/controllers/spree/admin/countries_controller.rb
+++ app/controllers/spree/admin/countries_controller.rb
@@ -1,6 +1,11 @@
 module Spree
   module Admin
     class CountriesController < ResourceController
+      def permitted_resource_params
+        params.require(:country).
+          permit(:name, :iso_name, :states_required)
+      end
+
       def collection
         super.order(:name)
       end
