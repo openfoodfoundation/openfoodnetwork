754b657f81b378306269c900fcfe7efb036b57e5
Luis Ramos <luisramos0@gmail.com>
2020-05-18 15:13:03 +0100

    Move controller methods to protected to keeep consitency and isolation

--- app/controllers/spree/admin/countries_controller.rb
+++ app/controllers/spree/admin/countries_controller.rb
@@ -1,6 +1,9 @@
 module Spree
   module Admin
     class CountriesController < ResourceController
+
+      protected
+
       def permitted_resource_params
         params.require(:country).
           permit(:name, :iso_name, :states_required)
--- app/controllers/spree/admin/shipping_categories_controller.rb
+++ app/controllers/spree/admin/shipping_categories_controller.rb
@@ -1,6 +1,9 @@
 module Spree
   module Admin
     class ShippingCategoriesController < ResourceController
+
+      protected
+
       def permitted_resource_params
         params.require(:shipping_category).
           permit(:name, :temperature_controlled)
