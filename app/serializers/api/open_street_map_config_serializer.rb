94b85fb9e38bd37895d655a27848b77d8f55ea2f
Cillian O'Ruanaidh <contact@cilliano.com>
2020-05-28 20:52:47 +0100

    Fix up Code Climate issues with Open Street Map code.

--- app/serializers/api/open_street_map_config_serializer.rb
+++ app/serializers/api/open_street_map_config_serializer.rb
@@ -1,15 +1,21 @@
-class Api::OpenStreetMapConfigSerializer < ActiveModel::Serializer
-  attributes :open_street_map_enabled, :open_street_map_provider_name, :open_street_map_provider_options
+# frozen_string_literal: true
 
-  def open_street_map_enabled
-    ContentConfig.open_street_map_enabled
-  end
+module Api
+  class OpenStreetMapConfigSerializer < ActiveModel::Serializer
+    attributes :open_street_map_enabled,
+               :open_street_map_provider_name,
+               :open_street_map_provider_options
 
-  def open_street_map_provider_name
-    ContentConfig.open_street_map_provider_name
-  end
+    def open_street_map_enabled
+      ContentConfig.open_street_map_enabled
+    end
 
-  def open_street_map_provider_options
-    ContentConfig.open_street_map_provider_options.to_json
+    def open_street_map_provider_name
+      ContentConfig.open_street_map_provider_name
+    end
+
+    def open_street_map_provider_options
+      ContentConfig.open_street_map_provider_options.to_json
+    end
   end
 end
