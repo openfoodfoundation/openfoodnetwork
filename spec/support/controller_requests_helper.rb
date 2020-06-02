53a80de503436c1dbce458f01063160d0e23d803
Luis Ramos <luisramos0@gmail.com>
2020-06-02 17:32:09 +0100

    Improve method names and set use_route with reverse_merge so it can be overridden

--- spec/support/controller_requests_helper.rb
+++ spec/support/controller_requests_helper.rb
@@ -4,53 +4,50 @@ require 'active_support/all'
 
 module ControllerRequestsHelper
   def api_get(action, params = {}, session = nil, flash = nil)
-    api_process(action, params, session, flash, "GET")
+    process_json_action(action, params, session, flash, "GET")
   end
 
   def api_post(action, params = {}, session = nil, flash = nil)
-    api_process(action, params, session, flash, "POST")
+    process_json_action(action, params, session, flash, "POST")
   end
 
   def api_put(action, params = {}, session = nil, flash = nil)
-    api_process(action, params, session, flash, "PUT")
+    process_json_action(action, params, session, flash, "PUT")
   end
 
   def api_delete(action, params = {}, session = nil, flash = nil)
-    api_process(action, params, session, flash, "DELETE")
+    process_json_action(action, params, session, flash, "DELETE")
   end
 
   def spree_get(action, params = {}, session = nil, flash = nil)
-    process_spree_action(action, params, session, flash, "GET")
+    process_action_with_route(action, params, session, flash, "GET")
   end
 
-  # Executes a request simulating POST HTTP method and set/volley the response
   def spree_post(action, params = {}, session = nil, flash = nil)
-    process_spree_action(action, params, session, flash, "POST")
+    process_action_with_route(action, params, session, flash, "POST")
   end
 
-  # Executes a request simulating PUT HTTP method and set/volley the response
   def spree_put(action, params = {}, session = nil, flash = nil)
-    process_spree_action(action, params, session, flash, "PUT")
+    process_action_with_route(action, params, session, flash, "PUT")
   end
 
-  # Executes a request simulating DELETE HTTP method and set/volley the response
   def spree_delete(action, params = {}, session = nil, flash = nil)
-    process_spree_action(action, params, session, flash, "DELETE")
+    process_action_with_route(action, params, session, flash, "DELETE")
   end
 
   private
 
-  def api_process(action, params = {}, session = nil, flash = nil, method = "get")
-    process_spree_action(action,
+  def process_json_action(action, params = {}, session = nil, flash = nil, method = "get")
+    process_action_with_route(action,
                          params.reverse_merge!(format: :json),
                          session,
                          flash,
                          method)
   end
 
-  def process_spree_action(action, params = {}, session = nil, flash = nil, method = "GET")
+  def process_action_with_route(action, params = {}, session = nil, flash = nil, method = "GET")
     process(action,
-            params.merge!(use_route: :main_app),
+            params.reverse_merge!(use_route: :main_app),
             session,
             flash,
             method)
