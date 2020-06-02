2f76e0b15baf240de6c050a177acc61d330dbb31
Luis Ramos <luisramos0@gmail.com>
2020-06-02 13:54:33 +0100

    Bring methods from Spree::TestingSupport::ControllerRequests to our ControllerRequestsHelper so we can merge them later

--- spec/support/controller_requests_helper.rb
+++ spec/support/controller_requests_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'active_support/all'
 
 module ControllerRequestsHelper
@@ -17,6 +19,27 @@ module ControllerRequestsHelper
     api_process(action, params, session, flash, "DELETE")
   end
 
+  def spree_get(action, parameters = nil, session = nil, flash = nil)
+    process_spree_action(action, parameters, session, flash, "GET")
+  end
+
+  # Executes a request simulating POST HTTP method and set/volley the response
+  def spree_post(action, parameters = nil, session = nil, flash = nil)
+    process_spree_action(action, parameters, session, flash, "POST")
+  end
+
+  # Executes a request simulating PUT HTTP method and set/volley the response
+  def spree_put(action, parameters = nil, session = nil, flash = nil)
+    process_spree_action(action, parameters, session, flash, "PUT")
+  end
+
+  # Executes a request simulating DELETE HTTP method and set/volley the response
+  def spree_delete(action, parameters = nil, session = nil, flash = nil)
+    process_spree_action(action, parameters, session, flash, "DELETE")
+  end
+
+  private
+
   def api_process(action, params = {}, session = nil, flash = nil, method = "get")
     process(action,
             params.
@@ -24,5 +47,10 @@ module ControllerRequestsHelper
             session,
             flash,
             method)
+  end
+
+  def process_spree_action(action, parameters = nil, session = nil, flash = nil, method = "GET")
+    parameters ||= {}
+    process(action, parameters.merge!(use_route: :spree), session, flash, method)
   end
 end
