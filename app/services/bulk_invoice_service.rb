6056c1699f58a0a3bb75e8e7accbfe3d75e8f65b
François Turbelin <pacodelaluna@gmail.com>
2020-06-09 10:10:34 +0200

    Use batches in order to fetch orders

--- app/services/bulk_invoice_service.rb
+++ app/services/bulk_invoice_service.rb
@@ -8,7 +8,7 @@ class BulkInvoiceService
   def start_pdf_job(order_ids)
     pdf = CombinePDF.new
 
-    orders_from(order_ids).each do |order|
+    orders_from(order_ids).find_each do |order|
       invoice = renderer.render_to_string(order)
 
       pdf << CombinePDF.parse(invoice)
