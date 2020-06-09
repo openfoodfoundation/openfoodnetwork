f05b0d1a5736ad531c76f05594a97a87d21fe511
Luis Ramos <luisramos0@gmail.com>
2020-06-09 12:02:35 +0100

    Add missing fields to ship method controller permitted params list

--- spec/controllers/spree/admin/shipping_methods_controller_spec.rb
+++ spec/controllers/spree/admin/shipping_methods_controller_spec.rb
@@ -3,6 +3,24 @@ require 'spec_helper'
 describe Spree::Admin::ShippingMethodsController, type: :controller do
   include AuthenticationWorkflow
 
+  describe "#update" do
+    describe "calculator details" do
+      let(:shipping_method) { create(:shipping_method_with, :flat_rate) }
+
+      it "updates flat rate calculator preferred_amount" do
+        login_as_admin
+        spree_post :update, id: shipping_method.id,
+                            shipping_method: {
+                              calculator_attributes: {
+                                id: shipping_method.calculator.id,
+                                preferred_amount: 123
+                              }
+                            }
+        expect(shipping_method.reload.calculator.preferred_amount).to eq 123
+      end
+    end
+  end
+
   describe "#delete" do
     describe "shipping method not referenced by order" do
       let(:shipping_method) { create(:shipping_method) }
