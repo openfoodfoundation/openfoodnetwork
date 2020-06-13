fdf0d923667395a7ccb70b67d7c764bc63ab7524
Cillian O'Ruanaidh <contact@cilliano.com>
2020-06-13 15:52:42 +0100

    Move feature spec for bulk coop report to the order management engine.

---
+++ engines/order_management/spec/features/order_management/reports/bulk_coop_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+feature "bulk coop" do
+  include AuthenticationWorkflow
+  include WebHelper
+
+  scenario "bulk co-op report" do
+    quick_login_as_admin
+    visit spree.admin_reports_path
+    click_link 'Bulk Co-Op'
+    click_button 'Generate Report'
+
+    expect(page).to have_content 'Supplier'
+  end
+end
+
+
