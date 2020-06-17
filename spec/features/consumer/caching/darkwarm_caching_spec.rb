640c9e6d2bfcf3db91cf85b21461ce86f26cc82e
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2020-06-17 12:36:42 +0200

    Re-enable flaky spec

--- spec/features/consumer/caching/darkwarm_caching_spec.rb
+++ spec/features/consumer/caching/darkwarm_caching_spec.rb
@@ -29,7 +29,7 @@ feature "Darkswarm data caching", js: true, caching: true do
       visit shops_path
     end
 
-    xit "invalidates caches for taxons and properties" do
+    it "invalidates caches for taxons and properties" do
       visit shops_path
 
       taxon_timestamp1 = CacheService.latest_timestamp_by_class(Spree::Taxon)
