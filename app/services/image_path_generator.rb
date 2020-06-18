130f639b61db51ccf74ea205ae58ae80201d8f55
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2020-06-18 17:14:50 +0200

    Rename service and use #call

--- app/services/image_path_generator.rb
+++ app/services/image_path_generator.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-class ImagePathService
+class ImagePathGenerator
   # Since Rails 4 has adjusted the way assets paths are handled, we have to access certain
   # asset-based helpers like this when outside of a view or controller context.
   # See: https://stackoverflow.com/a/16609815
-  def self.image_path(path)
+  def self.call(path)
     ActionController::Base.helpers.image_path(path)
   end
 end
