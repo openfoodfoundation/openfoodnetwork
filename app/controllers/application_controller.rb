730959f85fa832dfc0908de73de815db6ed9bb10
Luis Ramos <luisramos0@gmail.com>
2020-06-22 16:19:37 +0100

    Fix rubocop issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -134,7 +134,8 @@ class ApplicationController < ActionController::Base
     nil
   end
 
-  def set_cache_headers # https://jacopretorius.net/2014/01/force-page-to-reload-on-browser-back-in-rails.html
+  # See https://jacopretorius.net/2014/01/force-page-to-reload-on-browser-back-in-rails.html
+  def set_cache_headers
     response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
     response.headers["Pragma"] = "no-cache"
     response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
