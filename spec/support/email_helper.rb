c0f9f8c8bfa87e63633c10db231eed717349064e
Luis Ramos <luisramos0@gmail.com>
2020-06-24 21:09:06 +0100

    Remove comment refering to old spree upgrade

--- spec/support/email_helper.rb
+++ spec/support/email_helper.rb
@@ -2,8 +2,6 @@ module OpenFoodNetwork
   module EmailHelper
     # Some specs trigger actions that send emails, for example creating an order.
     # But sending emails doesn't work out-of-the-box. This code sets it up.
-    # It's here in a single place to allow an easy upgrade to Spree 2 which
-    # needs a different implementation of this method.
     def setup_email
       Spree::Config[:mails_from] = "test@ofn.example.org"
     end
