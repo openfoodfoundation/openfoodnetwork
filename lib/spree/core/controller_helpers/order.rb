d5744572f7a02a93966b8e0c2ec1c6e677e297a3
Luis Ramos <luisramos0@gmail.com>
2020-06-27 12:39:09 +0100

    Fix easy rubocop issues

--- lib/spree/core/controller_helpers/order.rb
+++ lib/spree/core/controller_helpers/order.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Spree
   module Core
     module ControllerHelpers
@@ -10,43 +12,50 @@ module Spree
           end
         end
 
-        # The current incomplete order from the session for use in cart and during checkout
+        # The current incomplete session order used in cart and checkout
         def current_order(create_order_if_necessary = false)
           return @current_order if @current_order
+
           if session[:order_id]
-            current_order = Spree::Order.includes(:adjustments).find_by(id: session[:order_id], currency: current_currency)
+            current_order = Spree::Order.includes(:adjustments)
+              .find_by(id: session[:order_id], currency: current_currency)
             @current_order = current_order unless current_order.try(:completed?)
           end
-          if create_order_if_necessary and (@current_order.nil? or @current_order.completed?)
+
+          if create_order_if_necessary && (@current_order.nil? || @current_order.completed?)
             @current_order = Spree::Order.new(currency: current_currency)
             @current_order.user ||= try_spree_current_user
-            # See issue #3346 for reasons why this line is here
+            # See https://github.com/spree/spree/issues/3346 for reasons why this line is here
             @current_order.created_by ||= try_spree_current_user
             @current_order.save!
 
-            # make sure the user has permission to access the order (if they are a guest)
+            # Verify that the user has access to the order (if they are a guest)
             if try_spree_current_user.nil?
               session[:access_token] = @current_order.token
             end
           end
-          if @current_order
-            @current_order.last_ip_address = ip_address
-            session[:order_id] = @current_order.id
-            return @current_order
-          end
+
+          return unless @current_order
+
+          @current_order.last_ip_address = ip_address
+          session[:order_id] = @current_order.id
+          @current_order
         end
 
         def associate_user
           @order ||= current_order
           if try_spree_current_user && @order
-            @order.associate_user!(try_spree_current_user) if @order.user.blank? || @order.email.blank?
+            if @order.user.blank? || @order.email.blank?
+              @order.associate_user!(try_spree_current_user)
+            end
           end
 
           # This will trigger any "first order" promotions to be triggered
           # Assuming of course that this session variable was set correctly in
           # the authentication provider's registrations controller
           if session[:spree_user_signup] && @order
-            fire_event('spree.user.signup', user: try_spree_current_user, order: @order)
+            fire_event('spree.user.signup', user: try_spree_current_user,
+                                            order: @order)
             session[:spree_user_signup] = nil
           end
 
@@ -54,13 +63,15 @@ module Spree
         end
 
         def set_current_order
-          if user = try_spree_current_user
-            last_incomplete_order = user.last_incomplete_spree_order
-            if session[:order_id].nil? && last_incomplete_order
-              session[:order_id] = last_incomplete_order.id
-            elsif current_order(true) && last_incomplete_order && current_order != last_incomplete_order
-              current_order.merge!(last_incomplete_order)
-            end
+          return unless (user = try_spree_current_user)
+
+          last_incomplete_order = user.last_incomplete_spree_order
+          if session[:order_id].nil? && last_incomplete_order
+            session[:order_id] = last_incomplete_order.id
+          elsif current_order(true) &&
+                last_incomplete_order &&
+                current_order != last_incomplete_order
+            current_order.merge!(last_incomplete_order)
           end
         end
 
