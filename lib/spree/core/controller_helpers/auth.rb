3599cb2047c892f3a9d5e164e3c70ad7f544816b
Luis Ramos <luisramos0@gmail.com>
2020-06-27 19:11:35 +0100

    Make unauthorized in ControllerHelpers::Auth the same as in Spree::Admin::BaseController
    It adapts the method in ControllerHelpers::Auth to also use the after_login mechanism.
    
    Ideally we would remove one of the two after_login mechanisms after_login and spree_user_return_to but they might still be in use.

--- lib/spree/core/controller_helpers/auth.rb
+++ lib/spree/core/controller_helpers/auth.rb
@@ -29,7 +29,7 @@ module Spree
             redirect_to '/unauthorized'
           else
             store_location
-            redirect_to(respond_to?(:spree_login_path) ? spree_login_path : main_app.root_path)
+            redirect_to main_app.root_path(anchor: "login?after_login=#{request.env['PATH_INFO']}")
           end
         end
 
