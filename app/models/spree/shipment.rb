7a03f57da0fed481070ceb5db8896a89e589048a
Luis Ramos <luisramos0@gmail.com>
2020-07-01 17:48:08 +0100

    Merge shipment decorator with class brought from spree_core

--- app/models/spree/shipment.rb
+++ app/models/spree/shipment.rb
@@ -159,11 +159,16 @@ module Spree
     end
 
     def manifest
-      inventory_units.joins(:variant).includes(:variant).group_by(&:variant).map do |variant, units|
+      inventory_units.group_by(&:variant).map do |variant, units|
         states = {}
         units.group_by(&:state).each { |state, iu| states[state] = iu.count }
+        scoper.scope(variant)
         OpenStruct.new(variant: variant, quantity: units.length, states: states)
       end
+    end
+
+    def scoper
+      @scoper ||= OpenFoodNetwork::ScopeVariantToHub.new(order.distributor)
     end
 
     def line_items
@@ -295,10 +300,28 @@ module Spree
                                           "open")
         reload # ensure adjustment is present on later saves
       end
+
+      update_adjustment_included_tax if adjustment
+    end
+
+    def update_adjustment_included_tax
+      if Config.shipment_inc_vat && (order.distributor.nil? || order.distributor.charges_sales_tax)
+        adjustment.set_included_tax! Config.shipping_tax_rate
+      else
+        adjustment.set_included_tax! 0
+      end
     end
 
     def update_order
       order.update!
+    end
+
+    # NOTE: This is an override of spree's method, needed to allow orders
+    # without line items (ie. user invoices) to not have inventory units
+    def require_inventory
+      return false unless line_items.count > 0 # This line altered
+
+      order.completed? && !order.canceled?
     end
   end
 end
