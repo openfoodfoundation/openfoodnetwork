4711a7469ac3a1930cbe8a357318c04443aa94cc
Luis Ramos <luisramos0@gmail.com>
2020-07-01 16:37:23 +0100

    Adapt coordinator spec to ofn and remove spec that is not applicable (multi stock locations)

--- spec/models/spree/stock/coordinator_spec.rb
+++ spec/models/spree/stock/coordinator_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 module Spree
   module Stock
     describe Coordinator do
-      let!(:order) { create(:order_with_line_items) }
+      let!(:order) { create(:order_with_line_items, distributor: create(:distributor_enterprise)) }
 
       subject { Coordinator.new(order) }
 
@@ -15,20 +15,6 @@ module Spree
           subject.should_receive(:prioritize_packages).ordered
           subject.should_receive(:estimate_packages).ordered
           subject.packages
-        end
-      end
-
-      context "build packages" do
-        it "builds a package for every stock location" do
-          subject.packages.count == StockLocation.count
-        end
-
-        context "missing stock items in stock location" do
-          let!(:another_location) { create(:stock_location, propagate_all_variants: false) }
-
-          it "builds packages only for valid stock locations" do
-            subject.build_packages.count.should == (StockLocation.count - 1)
-          end
         end
       end
     end
