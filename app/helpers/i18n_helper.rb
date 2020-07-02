8dfaea629ba2f04be40e46c76c52a6633dc45249
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2020-07-02 12:54:11 +0200

    Refactor current_user_locale to a new method

--- app/helpers/i18n_helper.rb
+++ app/helpers/i18n_helper.rb
@@ -1,17 +1,17 @@
 module I18nHelper
   def set_locale
-    # Save a given locale
+    # Save a given locale from params
     if params[:locale] && available_locale?(params[:locale])
       spree_current_user&.update!(locale: params[:locale])
       cookies[:locale] = params[:locale]
     end
 
     # After logging in, check if the user chose a locale before
-    if spree_current_user && spree_current_user.locale.nil? && cookies[:locale]
-      spree_current_user.update!(locale: params[:locale])
+    if current_user_locale.nil? && cookies[:locale]
+      spree_current_user&.update!(locale: params[:locale])
     end
 
-    I18n.locale = spree_current_user.andand.locale || cookies[:locale] || I18n.default_locale
+    I18n.locale = current_user_locale || cookies[:locale] || I18n.default_locale
   end
 
   def valid_locale(user)
@@ -28,5 +28,9 @@ module I18nHelper
 
   def available_locale?(locale)
     Rails.application.config.i18n.available_locales.include?(locale)
+  end
+
+  def current_user_locale
+    spree_current_user.andand.locale
   end
 end
