bebffcc90f85b0e2ac5f5a29d7096fa7d96e1060
Luis Ramos <luisramos0@gmail.com>
2020-07-06 12:51:10 +0100

    Set new S3 and attachments configs from ofn-install

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -33,7 +33,15 @@ Spree.config do |config|
   config.s3_access_key = ENV['S3_ACCESS_KEY'] if ENV['S3_ACCESS_KEY']
   config.s3_secret = ENV['S3_SECRET'] if ENV['S3_SECRET']
   config.use_s3 = true if ENV['S3_BUCKET']
+  config.s3_headers = ENV['S3_HEADERS'] if ENV['S3_HEADERS']
   config.s3_protocol = ENV.fetch('S3_PROTOCOL', 'https')
+
+  # Attachments settings
+  config.attachment_default_url = ENV['ATTACHMENT_DEFAULT_URL'] if ENV['ATTACHMENT_DEFAULT_URL']
+  config.attachment_path = ENV['ATTACHMENT_PATH'] if ENV['ATTACHMENT_PATH']
+  config.attachment_url = ENV['ATTACHMENT_URL'] if ENV['ATTACHMENT_URL']
+  config.attachment_styles = ENV['ATTACHMENT_STYLES'] if ENV['ATTACHMENT_STYLES']
+  config.attachment_default_style = ENV['ATTACHMENT_DEFAULT_STYLE'] if ENV['ATTACHMENT_DEFAULT_STYLE']
 end
 
 # Spree 2.0 recommends explicitly setting this here when using spree_auth_devise
