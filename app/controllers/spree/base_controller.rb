ab67a4f80c8bc4ebae1325d5e9181201f44c063f
Luis Ramos <luisramos0@gmail.com>
2020-07-06 14:45:04 +0100

    Bring base controller from spree

---
+++ app/controllers/spree/base_controller.rb
@@ -0,0 +1,16 @@
+require 'cancan'
+require_dependency 'spree/core/controller_helpers/strong_parameters'
+
+class Spree::BaseController < ApplicationController
+  include Spree::Core::ControllerHelpers::Auth
+  include Spree::Core::ControllerHelpers::RespondWith
+  include Spree::Core::ControllerHelpers::SSL
+  include Spree::Core::ControllerHelpers::Common
+  include Spree::Core::ControllerHelpers::Search
+  include Spree::Core::ControllerHelpers::StrongParameters
+  include Spree::Core::ControllerHelpers::Search
+
+  respond_to :html
+end
+
+require 'spree/i18n/initializer'
