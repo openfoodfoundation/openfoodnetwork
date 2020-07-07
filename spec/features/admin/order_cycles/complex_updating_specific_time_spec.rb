c8254b833886f84deed2c3532cd6a65cc8fa3e9d
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2020-07-07 10:38:06 +0200

    Adjust exchange_row assertion to use slightly more specific criteria

--- spec/features/admin/order_cycles/complex_updating_specific_time_spec.rb
+++ spec/features/admin/order_cycles/complex_updating_specific_time_spec.rb
@@ -109,8 +109,10 @@ feature '
     exchange_rows = page.all("table.exchanges tbody")
     exchange_rows.each do |exchange_row|
       exchange_row.find("td.products").click
-      # Wait for the products panel to be visible.
-      expect(exchange_row).to have_selector "tr", count: 2
+      within(exchange_row) do
+        # Wait for the products panel to be visible.
+        expect(page).to have_selector ".exchange-distributed-products"
+      end
     end
 
     uncheck "order_cycle_outgoing_exchange_2_variants_#{v1.id}"
