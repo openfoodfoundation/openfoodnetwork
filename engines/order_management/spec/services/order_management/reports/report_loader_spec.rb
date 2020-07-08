6cee8a8f3f6f70471a60fba5b6517b84954b0d93
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2020-07-08 22:26:15 +0200

    Fix spec namespacing and indentation

--- engines/order_management/spec/services/order_management/reports/report_loader_spec.rb
+++ engines/order_management/spec/services/order_management/reports/report_loader_spec.rb
@@ -12,104 +12,100 @@ module OrderManagement
   end
 end
 
-module OrderManagement
-  module Reports
-    describe ReportLoader do
-      let(:service) { OrderManagement::Reports::ReportLoader.new(*arguments) }
-      let(:report_base_class) { OrderManagement::Reports::Bananas::Base }
-      let(:report_subtypes) { ["green", "yellow"] }
+describe OrderManagement::Reports::ReportLoader do
+  let(:service) { OrderManagement::Reports::ReportLoader.new(*arguments) }
+  let(:report_base_class) { OrderManagement::Reports::Bananas::Base }
+  let(:report_subtypes) { ["green", "yellow"] }
 
-      before do
-        allow(report_base_class).to receive(:report_subtypes).and_return(report_subtypes)
+  before do
+    allow(report_base_class).to receive(:report_subtypes).and_return(report_subtypes)
+  end
+
+  describe "#report_class" do
+    describe "given report type and subtype" do
+      let(:arguments) { ["bananas", "yellow"] }
+
+      it "returns a report class when given type and subtype" do
+        expect(service.report_class).to eq OrderManagement::Reports::Bananas::Yellow
       end
+    end
 
-      describe "#report_class" do
-        describe "given report type and subtype" do
-          let(:arguments) { ["bananas", "yellow"] }
+    describe "given report type only" do
+      context "when the report has multiple subtypes" do
+        let(:arguments) { ["bananas"] }
 
-          it "returns a report class when given type and subtype" do
-            expect(service.report_class).to eq OrderManagement::Reports::Bananas::Yellow
-          end
-        end
-
-        describe "given report type only" do
-          context "when the report has multiple subtypes" do
-            let(:arguments) { ["bananas"] }
-
-            it "returns first listed report type" do
-              expect(service.report_class).to eq OrderManagement::Reports::Bananas::Green
-            end
-          end
-
-          context "when the report has no subtypes" do
-            let(:arguments) { ["bananas"] }
-            let(:report_subtypes) { [] }
-
-            it "returns base class" do
-              expect(service.report_class).to eq OrderManagement::Reports::Bananas::Base
-            end
-          end
-
-          context "given a report type that does not exist" do
-            let(:arguments) { ["apples"] }
-            let(:report_subtypes) { [] }
-
-            it "raises and error" do
-              expect{ service.report_class }.to raise_error(
-                OrderManagement::Errors::ReportNotFound
-              )
-            end
-          end
+        it "returns first listed report type" do
+          expect(service.report_class).to eq OrderManagement::Reports::Bananas::Green
         end
       end
 
-      describe "#default_report_subtype" do
-        context "when the report has multiple subtypes" do
-          let(:arguments) { ["bananas"] }
+      context "when the report has no subtypes" do
+        let(:arguments) { ["bananas"] }
+        let(:report_subtypes) { [] }
 
-          it "returns the first report type" do
-            expect(service.default_report_subtype).to eq report_base_class.report_subtypes.first
-          end
-        end
-
-        context "when the report has no subtypes" do
-          let(:arguments) { ["bananas"] }
-          let(:report_subtypes) { [] }
-
-          it "returns base" do
-            expect(service.default_report_subtype).to eq "base"
-          end
-        end
-
-        context "given a report type that does not exist" do
-          let(:arguments) { ["apples"] }
-          let(:report_subtypes) { [] }
-
-          it "raises and error" do
-            expect{ service.report_class }.to raise_error(
-              OrderManagement::Errors::ReportNotFound
-            )
-          end
+        it "returns base class" do
+          expect(service.report_class).to eq OrderManagement::Reports::Bananas::Base
         end
       end
 
-      describe "#report_subtypes" do
-        context "when the report has multiple subtypes" do
-          let(:arguments) { ["bananas"] }
+      context "given a report type that does not exist" do
+        let(:arguments) { ["apples"] }
+        let(:report_subtypes) { [] }
 
-          it "returns a list of report subtypes for a given report" do
-            expect(service.report_subtypes).to eq report_subtypes
-          end
+        it "raises and error" do
+          expect{ service.report_class }.to raise_error(
+            OrderManagement::Errors::ReportNotFound
+          )
         end
+      end
+    end
+  end
 
-        context "when the report has no subtypes" do
-          let(:arguments) { ["bananas"] }
-          let(:report_subtypes) { [] }
+  describe "#default_report_subtype" do
+    context "when the report has multiple subtypes" do
+      let(:arguments) { ["bananas"] }
 
-          it "returns an empty array" do
-            expect(service.report_subtypes).to eq []
-          end
-        end
+      it "returns the first report type" do
+        expect(service.default_report_subtype).to eq report_base_class.report_subtypes.first
+      end
+    end
+
+    context "when the report has no subtypes" do
+      let(:arguments) { ["bananas"] }
+      let(:report_subtypes) { [] }
+
+      it "returns base" do
+        expect(service.default_report_subtype).to eq "base"
+      end
+    end
+
+    context "given a report type that does not exist" do
+      let(:arguments) { ["apples"] }
+      let(:report_subtypes) { [] }
+
+      it "raises and error" do
+        expect{ service.report_class }.to raise_error(
+          OrderManagement::Errors::ReportNotFound
+        )
+      end
+    end
+  end
+
+  describe "#report_subtypes" do
+    context "when the report has multiple subtypes" do
+      let(:arguments) { ["bananas"] }
+
+      it "returns a list of report subtypes for a given report" do
+        expect(service.report_subtypes).to eq report_subtypes
+      end
+    end
+
+    context "when the report has no subtypes" do
+      let(:arguments) { ["bananas"] }
+      let(:report_subtypes) { [] }
+
+      it "returns an empty array" do
+        expect(service.report_subtypes).to eq []
       end
     end
   end
