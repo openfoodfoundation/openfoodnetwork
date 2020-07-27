3217b3ba8660b5d8773a701622513e93c44277da
Luis Ramos <luisramos0@gmail.com>
2020-07-27 12:28:53 +0100

    Delete dead commented code

--- spec/support/request/authentication_workflow.rb
+++ spec/support/request/authentication_workflow.rb
@@ -40,10 +40,6 @@ module AuthenticationWorkflow
   def login_to_admin_as(user)
     quick_login_as user
     visit spree.admin_dashboard_path
-    # visit spree.admin_dashboard_path
-    # fill_in 'spree_user_email', :with => user.email
-    # fill_in 'spree_user_password', :with => user.password
-    # click_button 'Login'
   end
 
   def fill_in_and_submit_login_form(user)
