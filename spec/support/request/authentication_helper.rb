61c14cb61d09c01b811c66580c9e9918bbdf8130
Pau Perez <saulopefa@gmail.com>
2020-08-04 08:48:08 +0200

    Remove unneeded RSpec config block
    Spec files individually include the module and we specify the type of
    spec in each RSpec's describe so none of this settings are needed. They
    are just Spree's legacy I bet.

--- spec/support/request/authentication_helper.rb
+++ spec/support/request/authentication_helper.rb
@@ -32,18 +32,3 @@ module AuthenticationHelper
     expect(page).to_not have_selector ".menu #login-link"
   end
 end
-
-RSpec.configure do |config|
-  config.extend AuthenticationHelper, type: :feature
-
-  # rspec-rails 3 will no longer automatically infer an example group's spec type
-  # from the file location. You can explicitly opt-in to the feature using this
-  # config option.
-  # To explicitly tag specs without using automatic inference, set the `:type`
-  # metadata manually:
-  #
-  #     describe ThingsController, :type => :controller do
-  #       # Equivalent to being in spec/controllers
-  #     end
-  config.infer_spec_type_from_file_location!
-end
