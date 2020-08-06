b36d0bc4f39e06666c941b50a462b7617294fecf
Luis Ramos <luisramos0@gmail.com>
2020-08-06 11:30:49 +0100

    Fix easy rubocop isssues

--- app/models/spree/image.rb
+++ app/models/spree/image.rb
@@ -6,7 +6,8 @@ module Spree
     validate :no_attachment_errors
 
     has_attached_file :attachment,
-                      styles: { mini: '48x48>', small: '100x100>', product: '240x240>', large: '600x600>' },
+                      styles: { mini: '48x48>', small: '100x100>',
+                                product: '240x240>', large: '600x600>' },
                       default_style: :product,
                       url: '/spree/products/:id/:style/:basename.:extension',
                       path: ':rails_root/public/spree/products/:id/:style/:basename.:extension',
@@ -19,11 +20,14 @@ module Spree
     include Spree::Core::S3Support
     supports_s3 :attachment
 
-    Spree::Image.attachment_definitions[:attachment][:styles] = ActiveSupport::JSON.decode(Spree::Config[:attachment_styles]).symbolize_keys!
+    Spree::Image.attachment_definitions[:attachment][:styles] =
+      ActiveSupport::JSON.decode(Spree::Config[:attachment_styles]).symbolize_keys!
     Spree::Image.attachment_definitions[:attachment][:path] = Spree::Config[:attachment_path]
     Spree::Image.attachment_definitions[:attachment][:url] = Spree::Config[:attachment_url]
-    Spree::Image.attachment_definitions[:attachment][:default_url] = Spree::Config[:attachment_default_url]
-    Spree::Image.attachment_definitions[:attachment][:default_style] = Spree::Config[:attachment_default_style]
+    Spree::Image.attachment_definitions[:attachment][:default_url] =
+      Spree::Config[:attachment_default_url]
+    Spree::Image.attachment_definitions[:attachment][:default_style] =
+      Spree::Config[:attachment_default_style]
 
     # used by admin products autocomplete
     def mini_url
@@ -41,12 +45,10 @@ module Spree
 
     # if there are errors from the plugin, then add a more meaningful message
     def no_attachment_errors
-      unless attachment.errors.empty?
-        # uncomment this to get rid of the less-than-useful interrim messages
-        # errors.clear
-        errors.add :attachment, "Paperclip returned errors for file '#{attachment_file_name}' - check ImageMagick installation or image source file."
-        false
-      end
+      return if attachment.errors.empty?
+
+      errors.add :attachment, "Paperclip returned errors for file '#{attachment_file_name}' - check ImageMagick installation or image source file."
+      false
     end
 
     # Spree stores attachent definitions in JSON. This converts the style name and format to
