0b053c18af41ecffc8f498059136e421d37c1857
Luis Ramos <luisramos0@gmail.com>
2020-08-06 10:17:50 +0100

    Merge decoorator with original file from spree

--- app/models/spree/stock_location.rb
+++ app/models/spree/stock_location.rb
@@ -49,26 +49,11 @@ module Spree
     end
 
     def move(variant, quantity, originator = nil)
-      stock_item_or_create(variant).stock_movements.create!(quantity: quantity,
-                                                            originator: originator)
+      variant.move(quantity, originator)
     end
 
     def fill_status(variant, quantity)
-      if item = stock_item(variant)
-
-        if item.count_on_hand >= quantity
-          on_hand = quantity
-          backordered = 0
-        else
-          on_hand = item.count_on_hand
-          on_hand = 0 if on_hand < 0
-          backordered = item.backorderable? ? (quantity - on_hand) : 0
-        end
-
-        [on_hand, backordered]
-      else
-        [0, 0]
-      end
+      variant.fill_status(quantity)
     end
 
     private
