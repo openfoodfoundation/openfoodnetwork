0c7a0e3e96fe91233b3da50c923572cf05d8335f
Luis Ramos <luisramos0@gmail.com>
2020-08-07 10:06:04 +0100

    Bring modesl from spree_core

--- spec/models/spree/shipping_method_spec.rb
+++ spec/models/spree/shipping_method_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
+class DummyShippingCalculator < Spree::ShippingCalculator
+end
+
 module Spree
   describe ShippingMethod do
     it "is valid when built from factory" do
@@ -117,6 +120,49 @@ module Spree
 
       it "is touched when applied to a distributor" do
         expect{ add_distributor }.to change { shipping_method.reload.updated_at }
+      end
+    end
+
+    context 'calculators' do
+      let(:shipping_method){ create(:shipping_method) }
+
+      it "Should reject calculators that don't inherit from Spree::ShippingCalculator" do
+        Spree::ShippingMethod.stub_chain(:spree_calculators, :shipping_methods).and_return([
+          Spree::Calculator::Shipping::FlatPercentItemTotal,
+          Spree::Calculator::Shipping::PriceSack,
+          Spree::Calculator::DefaultTax,
+          DummyShippingCalculator # included as regression test for https://github.com/spree/spree/issues/3109
+        ])
+
+        Spree::ShippingMethod.calculators.should == [Spree::Calculator::Shipping::FlatPercentItemTotal, Spree::Calculator::Shipping::PriceSack, DummyShippingCalculator ]
+        Spree::ShippingMethod.calculators.should_not == [Spree::Calculator::DefaultTax]
+      end
+    end
+
+    context "validations" do
+      before { subject.valid? }
+
+      it "validates presence of name" do
+        subject.should have(1).error_on(:name)
+      end
+
+      context "shipping category" do
+        it "validates presence of at least one" do
+          subject.should have(1).error_on(:base)
+        end
+
+        context "one associated" do
+          before { subject.shipping_categories.push create(:shipping_category) }
+          it { subject.should have(0).error_on(:base) }
+        end
+      end
+    end
+
+    context 'factory' do
+      let(:shipping_method){ create :shipping_method }
+
+      it "should set calculable correctly" do
+        shipping_method.calculator.calculable.should == shipping_method
       end
     end
   end
