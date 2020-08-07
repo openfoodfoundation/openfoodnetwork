114c947b68e08e8f564ed166001476e6c48e92dd
Luis Ramos <luisramos0@gmail.com>
2020-08-07 20:14:03 +0100

    Merge decorators with original spree files

--- app/models/spree/product_option_type.rb
+++ app/models/spree/product_option_type.rb
@@ -1,7 +1,16 @@
 module Spree
   class ProductOptionType < ActiveRecord::Base
+    after_destroy :remove_option_values
+
     belongs_to :product, class_name: 'Spree::Product'
     belongs_to :option_type, class_name: 'Spree::OptionType'
     acts_as_list scope: :product
+
+    def remove_option_values
+      product.variants_including_master.each do |variant|
+        option_values = variant.option_values.where(option_type_id: option_type)
+        variant.option_values.destroy(*option_values)
+      end
+    end
   end
 end
