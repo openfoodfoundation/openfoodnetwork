0ea21028d44e2126953ab8c6c09282a7da64999c
Luis Ramos <luisramos0@gmail.com>
2020-08-07 19:51:10 +0100

    Bring models from spree_core: Spree::Product and Spree::Variant!
    EPIC COMMIT ALERT :-)

---
+++ app/models/spree/product_property.rb
@@ -0,0 +1,25 @@
+module Spree
+  class ProductProperty < ActiveRecord::Base
+    belongs_to :product,  class_name: 'Spree::Product'
+    belongs_to :property, class_name: 'Spree::Property'
+
+    validates :property, presence: true
+    validates :value, length: { maximum: 255 }
+
+    default_scope -> { order("#{self.table_name}.position") }
+
+    # virtual attributes for use with AJAX completion stuff
+    def property_name
+      property.name if property
+    end
+
+    def property_name=(name)
+      unless name.blank?
+        unless property = Property.find_by(name: name)
+          property = Property.create(name: name, presentation: name)
+        end
+        self.property = property
+      end
+    end
+  end
+end
