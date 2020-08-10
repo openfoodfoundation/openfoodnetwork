625dd278569c5b67cf5c7d85c8f10c234b9cb1f3
Eduardo <jeduardo824@gmail.com>
2020-08-09 21:33:25 -0300

    fix method to use map..sum and calculate total fees correctly

--- app/models/calculator/default_tax.rb
+++ app/models/calculator/default_tax.rb
@@ -49,11 +49,11 @@ module Calculator
     # Finds relevant fees for each line_item,
     #   calculates the tax on them, and returns the total tax
     def per_item_fees_total(order, calculator)
-      order.line_items.sum do |line_item|
+      order.line_items.map do |line_item|
         calculator.per_item_enterprise_fee_applicators_for(line_item.variant)
           .select { |applicator| applicable_rate?(applicator, line_item) }
           .sum { |applicator| applicator.enterprise_fee.compute_amount(line_item) }
-      end
+      end.sum
     end
 
     def applicable_rate?(applicator, line_item)
