737fc699edfa6afe1d12292e1deb6079a45b2b21
Luis Ramos <luisramos0@gmail.com>
2020-08-19 04:33:30 +0100

    Fix rubocop issues

--- app/models/spree/role.rb
+++ app/models/spree/role.rb
@@ -2,6 +2,7 @@
 
 module Spree
   class Role < ActiveRecord::Base
-    has_and_belongs_to_many :users, join_table: 'spree_roles_users', class_name: Spree.user_class.to_s
+    has_and_belongs_to_many :users, join_table: 'spree_roles_users',
+                                    class_name: Spree.user_class.to_s
   end
 end
