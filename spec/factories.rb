6d9250c014dd0b8f28afb8f7c49a3e7e2e7ef1c1
Luis Ramos <luisramos0@gmail.com>
2020-08-23 14:15:20 +0100

    Bring tax category factory from spree_core and fix a problem with the stock location of the product factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,7 @@
 require 'ffaker'
 
 FactoryBot.define do
+  sequence(:random_string)      { Faker::Lorem.sentence }
   sequence(:random_description) { Faker::Lorem.paragraphs(1 + Kernel.rand(5)).join("\n") }
   sequence(:random_email)       { Faker::Internet.email }
 
--- spec/factories/product_factory.rb
+++ spec/factories/product_factory.rb
@@ -21,7 +21,7 @@ FactoryBot.define do
     shipping_category { DefaultShippingCategory.find_or_create }
 
     # ensure stock item will be created for this products master
-    before(:create) { create(:stock_location) if Spree::StockLocation.count == 0 }
+    before(:create) { DefaultStockLocation.find_or_create }
 
     factory :product do
       transient do
