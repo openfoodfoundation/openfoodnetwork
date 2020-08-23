e2a865476da2ea03cc45b57593c27c68bbfbeccf
Luis Ramos <luisramos0@gmail.com>
2020-08-23 14:01:59 +0100

    Bring adjustment factory from spree_core

---
+++ spec/factories/adjustment_factory.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :adjustment, class: Spree::Adjustment do
+    association(:adjustable, factory: :order)
+    amount 100.0
+    label 'Shipping'
+    association(:source, factory: :shipment)
+    eligible true
+  end
+end
