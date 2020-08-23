cd22361d131235b887c9c94f01ae57ccf5d38e32
Luis Ramos <luisramos0@gmail.com>
2020-08-23 15:28:42 +0100

    Fix shipping category factory

--- spec/factories/shipping_category_factory.rb
+++ spec/factories/shipping_category_factory.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
   factory :shipping_category, class: Spree::ShippingCategory do   
     initialize_with { DefaultShippingCategory.find_or_create }
-    transient { name 'Default' }
-    sequence(:name) { |n| "ShippingCategory ##{n}" }
   end
 end
