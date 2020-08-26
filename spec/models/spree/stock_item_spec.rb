f9aac02e1323e0693adb18bdf64569c35eb69398
Luis Ramos <luisramos0@gmail.com>
2020-08-26 22:16:02 +0100

    Run rubocop -a

--- spec/models/spree/stock_item_spec.rb
+++ spec/models/spree/stock_item_spec.rb
@@ -9,8 +9,8 @@ RSpec.describe Spree::StockItem do
     product_1 = create(:product)
     product_2 = create(:product)
 
-    stock_location.stock_items.where(:variant_id => product_1.master.id).first.adjust_count_on_hand(10)
-    stock_location.stock_items.where(:variant_id => product_2.master.id).first.adjust_count_on_hand(20)
+    stock_location.stock_items.where(variant_id: product_1.master.id).first.adjust_count_on_hand(10)
+    stock_location.stock_items.where(variant_id: product_2.master.id).first.adjust_count_on_hand(20)
   end
 
   subject { stock_location.stock_items.order(:id).first }
