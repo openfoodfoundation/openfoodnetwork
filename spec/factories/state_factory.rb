37bfdf69a9361c3967082406fd1afd3a3db2cbc6
Pau Perez <saulopefa@gmail.com>
2020-08-28 12:44:30 +0200

    Bring required state factory
    It was removed from master in
    https://github.com/openfoodfoundation/openfoodnetwork/commit/2abbfad18cc73c80408a1d57fe78e8c60d340664
    but it's still required by the zone_specs and the address factory.

---
+++ spec/factories/state_factory.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :state, class: Spree::State do
+    name 'Alabama'
+    abbr 'AL'
+    country do |country|
+      if usa = Spree::Country.find_by_numcode(840)
+        country = usa
+      else
+        country.association(:country)
+      end
+    end
+  end
+end
