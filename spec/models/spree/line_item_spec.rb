8edd6a0af0308549dbef564f8f5b6a2e180d5ea2
Luis Ramos <luisramos0@gmail.com>
2020-09-03 23:08:57 +0100

    Use ofn's calculator in all tests and add missing translation

--- spec/models/spree/line_item_spec.rb
+++ spec/models/spree/line_item_spec.rb
@@ -172,7 +172,7 @@ module Spree
       end
 
       describe "finding line items with and without tax" do
-        let(:tax_rate) { create(:tax_rate, calculator: Calculator::DefaultTax.new) }
+        let(:tax_rate) { create(:tax_rate, calculator: ::Calculator::DefaultTax.new) }
         let!(:adjustment1) { create(:adjustment, originator: tax_rate, label: "TR", amount: 123, included_tax: 10.00) }
 
         before do
@@ -448,7 +448,7 @@ module Spree
     describe "tax" do
       let(:li_no_tax)   { create(:line_item) }
       let(:li_tax)      { create(:line_item) }
-      let(:tax_rate)    { create(:tax_rate, calculator: Calculator::DefaultTax.new) }
+      let(:tax_rate)    { create(:tax_rate, calculator: ::Calculator::DefaultTax.new) }
       let!(:adjustment) { create(:adjustment, adjustable: li_tax, originator: tax_rate, label: "TR", amount: 123, included_tax: 10.00) }
 
       context "checking if a line item has tax included" do
