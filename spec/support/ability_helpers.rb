405b31772613f1bd7c4ba1506245d596d5216fc3
Luis Ramos <luisramos0@gmail.com>
2020-09-04 10:28:28 +0100

    Merge ability and ability decorator

--- spec/support/ability_helpers.rb
+++ spec/support/ability_helpers.rb
@@ -2,106 +2,106 @@
 
 shared_examples_for 'access granted' do
   it 'should allow read' do
-    expect(ability).to be_able_to(:read, resource, token) if token
-    expect(ability).to be_able_to(:read, resource) unless token
+    expect(subject).to be_able_to(:read, resource, token) if token
+    expect(subject).to be_able_to(:read, resource) unless token
   end
 
   it 'should allow create' do
-    expect(ability).to be_able_to(:create, resource, token) if token
-    expect(ability).to be_able_to(:create, resource) unless token
+    expect(subject).to be_able_to(:create, resource, token) if token
+    expect(subject).to be_able_to(:create, resource) unless token
   end
 
   it 'should allow update' do
-    expect(ability).to be_able_to(:update, resource, token) if token
-    expect(ability).to be_able_to(:update, resource) unless token
+    expect(subject).to be_able_to(:update, resource, token) if token
+    expect(subject).to be_able_to(:update, resource) unless token
   end
 end
 
 shared_examples_for 'access denied' do
   it 'should not allow read' do
-    expect(ability).to_not be_able_to(:read, resource)
+    expect(subject).to_not be_able_to(:read, resource)
   end
 
   it 'should not allow create' do
-    expect(ability).to_not be_able_to(:create, resource)
+    expect(subject).to_not be_able_to(:create, resource)
   end
 
   it 'should not allow update' do
-    expect(ability).to_not be_able_to(:update, resource)
+    expect(subject).to_not be_able_to(:update, resource)
   end
 end
 
 shared_examples_for 'admin granted' do
   it 'should allow admin' do
-    expect(ability).to be_able_to(:admin, resource, token) if token
-    expect(ability).to be_able_to(:admin, resource) unless token
+    expect(subject).to be_able_to(:admin, resource, token) if token
+    expect(subject).to be_able_to(:admin, resource) unless token
   end
 end
 
 shared_examples_for 'admin denied' do
   it 'should not allow admin' do
-    expect(ability).to_not be_able_to(:admin, resource)
+    expect(subject).to_not be_able_to(:admin, resource)
   end
 end
 
 shared_examples_for 'index allowed' do
   it 'should allow index' do
-    expect(ability).to be_able_to(:index, resource)
+    expect(subject).to be_able_to(:index, resource)
   end
 end
 
 shared_examples_for 'no index allowed' do
   it 'should not allow index' do
-    expect(ability).to_not be_able_to(:index, resource)
+    expect(subject).to_not be_able_to(:index, resource)
   end
 end
 
 shared_examples_for 'create only' do
   it 'should allow create' do
-    expect(ability).to be_able_to(:create, resource)
+    expect(subject).to be_able_to(:create, resource)
   end
 
   it 'should not allow read' do
-    expect(ability).to_not be_able_to(:read, resource)
+    expect(subject).to_not be_able_to(:read, resource)
   end
 
   it 'should not allow update' do
-    expect(ability).to_not be_able_to(:update, resource)
+    expect(subject).to_not be_able_to(:update, resource)
   end
 
   it 'should not allow index' do
-    expect(ability).to_not be_able_to(:index, resource)
+    expect(subject).to_not be_able_to(:index, resource)
   end
 end
 
 shared_examples_for 'read only' do
   it 'should not allow create' do
-    expect(ability).to_not be_able_to(:create, resource)
+    expect(subject).to_not be_able_to(:create, resource)
   end
 
   it 'should not allow update' do
-    expect(ability).to_not be_able_to(:update, resource)
+    expect(subject).to_not be_able_to(:update, resource)
   end
 
   it 'should allow index' do
-    expect(ability).to be_able_to(:index, resource)
+    expect(subject).to be_able_to(:index, resource)
   end
 end
 
 shared_examples_for 'update only' do
   it 'should not allow create' do
-    expect(ability).to_not be_able_to(:create, resource)
+    expect(subject).to_not be_able_to(:create, resource)
   end
 
   it 'should not allow read' do
-    expect(ability).to_not be_able_to(:read, resource)
+    expect(subject).to_not be_able_to(:read, resource)
   end
 
   it 'should allow update' do
-    expect(ability).to be_able_to(:update, resource)
+    expect(subject).to be_able_to(:update, resource)
   end
 
   it 'should not allow index' do
-    expect(ability).to_not be_able_to(:index, resource)
+    expect(subject).to_not be_able_to(:index, resource)
   end
 end
