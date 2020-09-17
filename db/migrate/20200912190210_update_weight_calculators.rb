6466829bdc2eccb326d0bc6d3d9d7970d4dc0521
Andy Brett <andy@andybrett.com>
2020-09-17 07:17:25 -0700

    fix typo in weight calculator migration

--- db/migrate/20200912190210_update_weight_calculators.rb
+++ db/migrate/20200912190210_update_weight_calculators.rb
@@ -1,6 +1,6 @@
 class UpdateWeightCalculators < ActiveRecord::Migration
   def change
-    Calculator::Weight.each { |calculator|
+    Calculator::Weight.all.each { |calculator|
       calculator.preferred_unit_from_list = 'kg'
       calculator.preferred_per_unit = calculator.preferred_per_kg
       calculator.preferences.delete(:preferred_per_kg)
