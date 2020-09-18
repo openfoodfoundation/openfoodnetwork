dc7b9ed8fa60607015ca0b79f660e8981788e366
Andy Brett <andy@andybrett.com>
2020-09-18 09:55:29 -0700

    use sql query to update weight calculator prefs

--- db/migrate/20200912190210_update_weight_calculators.rb
+++ db/migrate/20200912190210_update_weight_calculators.rb
@@ -1,10 +1,12 @@
 class UpdateWeightCalculators < ActiveRecord::Migration
   def change
+    Spree::Calculator.connection.execute(
+      "UPDATE spree_preferences SET key = replace( key, 'per_kg', 'per_unit') WHERE key like '/calculator/weight/per_kg/%'"
+    )
+
     Calculator::Weight.all.each { |calculator|
       calculator.preferred_unit_from_list = 'kg'
-      calculator.preferred_per_unit = calculator.preferred_per_kg
-      calculator.preferences.delete(:preferred_per_kg)
-      Rails.cache.delete(calculator.preference_cache_key("preferred_per_kg"))
+      Rails.cache.delete("/calculator/weight/preferred_per_kg/" + calculator.id)
       calculator.save
     }
   end
