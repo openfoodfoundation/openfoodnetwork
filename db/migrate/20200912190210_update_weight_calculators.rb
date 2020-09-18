843f43eb787ce5b936a6ee4527f4b8bb15cfda51
Andy Brett <andy@andybrett.com>
2020-09-17 17:30:43 -0700

    delete cached per_kg preference for all weight calculators

--- db/migrate/20200912190210_update_weight_calculators.rb
+++ db/migrate/20200912190210_update_weight_calculators.rb
@@ -4,6 +4,7 @@ class UpdateWeightCalculators < ActiveRecord::Migration
       calculator.preferred_unit_from_list = 'kg'
       calculator.preferred_per_unit = calculator.preferred_per_kg
       calculator.preferences.delete(:preferred_per_kg)
+      Rails.cache.delete(calculator.preference_cache_key("preferred_per_kg"))
       calculator.save
     }
   end
