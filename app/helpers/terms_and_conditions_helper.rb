053a1aadc1a37e6b7597fe76c8484d094849a04b
Luis Ramos <luisramos0@gmail.com>
2020-09-22 18:39:54 +0100

    Rename methods to follwo ruby convention and improve readability

--- app/helpers/terms_and_conditions_helper.rb
+++ app/helpers/terms_and_conditions_helper.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 module TermsAndConditionsHelper
-  def terms_and_conditions_activated
+  def terms_and_conditions_activated?
     current_order.distributor.terms_and_conditions.file?
   end
 
-  def terms_and_conditions_already_accepted
+  def terms_and_conditions_already_accepted?
     customer_terms_and_conditions_accepted_at = spree_current_user.
       customer_of(current_order.distributor)&.terms_and_conditions_accepted_at
 
