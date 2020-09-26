20b37e3c35b13be6493c1308559df9cca6f081d0
Luis Ramos <luisramos0@gmail.com>
2020-09-26 08:41:47 +0100

    Move Spree::ProductDuplicator to Spree::Core namespace

--- app/models/spree/product.rb
+++ app/models/spree/product.rb
@@ -263,7 +263,7 @@ module Spree
     # for adding products which are closely related to existing ones
     # define "duplicate_extra" for site-specific actions, eg for additional fields
     def duplicate
-      duplicator = ProductDuplicator.new(self)
+      duplicator = Spree::Core::ProductDuplicator.new(self)
       duplicator.duplicate
     end
 
