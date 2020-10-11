fbadf2504336fa83f5cd72ada45bb8d0cc9e5942
Rajan Joshi <rajan.joshi@onthebeach.co.uk>
2020-10-11 17:01:29 +0100

    Add coverage threshold for simplecov.

---
+++ spec/coverage_helper.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'simplecov'
+
+SimpleCov.minimum_coverage 94
+SimpleCov.start 'rails' do
+  add_filter '/bin/'
+  add_filter '/config/'
+  add_filter '/jobs/application_job.rb'
+  add_filter '/schemas/'
+  add_filter '/lib/generators'
+  add_filter '/spec/'
+  add_filter '/vendor/'
+end
