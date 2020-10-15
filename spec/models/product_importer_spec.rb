623fdd3626f4bff4e07fd024b1a88189e2b5fc70
Luis Ramos <luisramos0@gmail.com>
2020-10-16 00:26:12 +0100

    Fix prod importer spec in rails 4.1

--- spec/models/product_importer_spec.rb
+++ spec/models/product_importer_spec.rb
@@ -484,7 +484,7 @@ describe ProductImport::ProductImporter do
       expect(importer.inventory_updated_count).to eq 0
       expect(importer.updated_ids.count).to eq 2
 
-      products = Spree::Product.find_all_by_name('Bag of Oats')
+      products = Spree::Product.where(name: 'Bag of Oats')
 
       expect(products.count).to eq 1
       expect(products.first.variants.count).to eq 5
