10ed1f04c10715f2ab48ec394076c02f9cab54ab
Luis Ramos <luisramos0@gmail.com>
2020-10-20 11:22:08 +0100

    Revert default styles to what they were before, we can do this in a separate PR

--- app/models/spree/image.rb
+++ app/models/spree/image.rb
@@ -12,8 +12,8 @@ module Spree
     #                                      and Product image edit page
     # - large: used in the FrontOffice: product modal
     has_attached_file :attachment,
-                      styles: { mini: ["48x48#", :jpg], small: ["227x227#", :jpg],
-                                product: ["240x240>", :jpg], large: ["600x600>", :jpg] },
+                      styles: { mini: '48x48>', small: '100x100>',
+                                product: '240x240>', large: '600x600>' },
                       default_style: :product,
                       url: '/spree/products/:id/:style/:basename.:extension',
                       path: ':rails_root/public/spree/products/:id/:style/:basename.:extension',
