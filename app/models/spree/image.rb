92d6168a14d9bfa1c271be75a2be81a2785a05dc
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2020-10-21 13:42:07 +0100

    Don't restrict image filetypes to JPG

--- app/models/spree/image.rb
+++ app/models/spree/image.rb
@@ -6,8 +6,8 @@ module Spree
     validate :no_attachment_errors
 
     has_attached_file :attachment,
-                      styles: { mini: ["48x48#", :jpg], small: ["227x227#", :jpg],
-                                product: ["240x240>", :jpg], large: ["600x600>", :jpg] },
+                      styles: { mini: "48x48#", small: "227x227#",
+                                product: "240x240>", large: "600x600>" },
                       default_style: :product,
                       url: '/spree/products/:id/:style/:basename.:extension',
                       path: ':rails_root/public/spree/products/:id/:style/:basename.:extension',
