7d051782b97faa4655073f54795e98ccee33058b
Luis Ramos <luisramos0@gmail.com>
2020-10-29 15:56:41 +0000

    Fix bug in terms_and_conditions_helper related to guest checkout where current_user is nil and T&Cs must be shown all the time

--- app/helpers/terms_and_conditions_helper.rb
+++ app/helpers/terms_and_conditions_helper.rb
@@ -6,7 +6,7 @@ module TermsAndConditionsHelper
   end
 
   def terms_and_conditions_already_accepted?
-    customer_terms_and_conditions_accepted_at = spree_current_user.
+    customer_terms_and_conditions_accepted_at = spree_current_user&.
       customer_of(current_order.distributor)&.terms_and_conditions_accepted_at
 
     customer_terms_and_conditions_accepted_at.present? &&
