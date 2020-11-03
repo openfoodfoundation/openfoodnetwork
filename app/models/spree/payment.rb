c0ddeceb1e4bbfcecc7b5b70dea0eb8b15b03a66
Luis Ramos <luisramos0@gmail.com>
2020-11-03 14:36:20 +0000

    Remove Pin Payments

--- app/models/spree/payment.rb
+++ app/models/spree/payment.rb
@@ -103,19 +103,12 @@ module Spree
       source.user_id = order.user_id if order
     end
 
-    # Pin payments lacks void and credit methods, but it does have refund
-    # Here we swap credit out for refund and remove void as a possible action
     def actions
       return [] unless payment_source&.respond_to?(:actions)
 
       actions = payment_source.actions.select do |action|
         !payment_source.respond_to?("can_#{action}?") ||
           payment_source.__send__("can_#{action}?", self)
-      end
-
-      if payment_method.is_a? Gateway::Pin
-        actions << 'refund' if actions.include? 'credit'
-        actions.reject! { |a| ['credit', 'void'].include? a }
       end
 
       actions
