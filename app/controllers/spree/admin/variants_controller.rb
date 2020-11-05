de19efca64246ad77794a1abef77ec72faf26115
Luis Ramos <luisramos0@gmail.com>
2020-11-05 12:11:31 +0000

    Fix rubocop issue and add missing exceptions to manual_todo to get a green rubocop run

--- app/controllers/spree/admin/variants_controller.rb
+++ app/controllers/spree/admin/variants_controller.rb
@@ -23,7 +23,9 @@ module Spree
           flash[:success] = flash_message_for(@object, :successfully_updated)
           redirect_to spree.admin_product_variants_url(params[:product_id], @url_filters)
         else
-          redirect_to spree.edit_admin_product_variant_url(params[:product_id], @object, @url_filters)
+          redirect_to spree.edit_admin_product_variant_url(params[:product_id],
+                                                           @object,
+                                                           @url_filters)
         end
       end
 
