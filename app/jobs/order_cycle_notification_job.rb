aaf7a90f8b735e1b5d0853c29f3ac7d18ca8992d
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2020-11-06 02:08:37 +0000

    Convert OrderCycleNotificationJob to ActiveJob

--- app/jobs/order_cycle_notification_job.rb
+++ app/jobs/order_cycle_notification_job.rb
@@ -1,6 +1,6 @@
 # Delivers an email with a report of the order cycle to each of its suppliers
-OrderCycleNotificationJob = Struct.new(:order_cycle_id) do
-  def perform
+class OrderCycleNotificationJob < ActiveJob::Base
+  def perform(order_cycle_id)
     order_cycle = OrderCycle.find(order_cycle_id)
     order_cycle.suppliers.each do |supplier|
       ProducerMailer.order_cycle_report(supplier, order_cycle).deliver
