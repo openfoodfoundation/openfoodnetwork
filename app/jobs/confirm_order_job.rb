79228a329f3725a461f9231b6b5fc78c61cd22a8
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2020-11-06 02:05:10 +0000

    Convert ConfirmOrderJob to ActiveJob

--- app/jobs/confirm_order_job.rb
+++ app/jobs/confirm_order_job.rb
@@ -1,5 +1,5 @@
-ConfirmOrderJob = Struct.new(:order_id) do
-  def perform
+class ConfirmOrderJob < ActiveJob::Base
+  def perform(order_id)
     Spree::OrderMailer.confirm_email_for_customer(order_id).deliver
     Spree::OrderMailer.confirm_email_for_shop(order_id).deliver
   end
