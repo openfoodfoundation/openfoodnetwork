1cad631bdb802e6c5110c04c520dcb3c0a357e72
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2020-11-10 23:36:54 +0000

    Replace secret_token and update comments
    `secret_token` is apparently still called directly in other places...

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,15 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Openfoodnetwork::Application.config.secret_key_base = ENV["SECRET_TOKEN"]
+
+secret_key = if Rails.env.development? or Rails.env.test?
+  ('x' * 30) # Meets basic minimum of 30 chars.
+else
+  ENV["SECRET_TOKEN"]
+end
+
+# Rails 4+ key for signing and encrypting cookies.
+Openfoodnetwork::Application.config.secret_key_base = secret_key
+
+# Legacy secret_token variable. This is still used directly for encryption.
+Openfoodnetwork::Application.config.secret_token = secret_key
