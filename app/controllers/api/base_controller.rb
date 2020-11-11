1766f33d46bc50852785a825c75cabe467199705
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2020-11-11 23:01:25 +0000

    Use ActionController::Metal and include StrongParameters

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -3,7 +3,8 @@ require_dependency 'spree/api/controller_setup'
 require "spree/core/controller_helpers/ssl"
 
 module Api
-  class BaseController < ActionController::Base
+  class BaseController < ActionController::Metal
+    include ActionController::StrongParameters
     include Spree::Api::ControllerSetup
     include Spree::Core::ControllerHelpers::SSL
     include ::ActionController::Head
