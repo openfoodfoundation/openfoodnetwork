fea7576ac19201e3fed0e09746ef25598e512a77
Andy Brett <andy@andybrett.com>
2020-11-19 11:45:08 -0800

    update CreditCardCloner to find existing clone

--- app/services/recurring_payments.rb
+++ app/services/recurring_payments.rb
@@ -3,15 +3,13 @@
 class RecurringPayments
   def self.setup_for(customer)
     return unless card = customer.user.default_card
+    return unless stripe_account = customer.enterprise.stripe_account&.stripe_user_id
 
-    stripe_account = customer.enterprise.stripe_account&.stripe_user_id
-
-    customer_id, payment_method_id = Stripe::CreditCardCloner.new.clone(card,
-                                                                        stripe_account)
-    setup_intent = Stripe::SetupIntent.create({
-      payment_method: payment_method_id, customer: customer_id }, {
-        stripe_account: stripe_account
-      }
+    customer_id, payment_method_id =
+      Stripe::CreditCardCloner.new.find_or_clone(card, stripe_account)
+    setup_intent = Stripe::SetupIntent.create(
+      { payment_method: payment_method_id, customer: customer_id },
+      { stripe_account: stripe_account }
     )
     setup_intent.client_secret
   end
