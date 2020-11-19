277d7f44b7ed2a32e1389ceee4301581f30d8ff0
Andy Brett <andy@andybrett.com>
2020-11-19 13:49:47 -0800

    refactor api customers controller; resolve merge conflict

--- app/controllers/api/customers_controller.rb
+++ app/controllers/api/customers_controller.rb
@@ -14,24 +14,24 @@ module Api
       client_secret = RecurringPayments.setup_for(@customer) if params[:customer][:allow_charges]
 
       if @customer.update(customer_params)
-        add_recurring_payment_info(@customer, client_secret)
+        add_recurring_payment_info(client_secret)
         render json: @customer, serializer: CustomerSerializer, status: :ok
       else
         invalid_resource!(@customer)
       end
     end
 
-    def customer_params
-      params.require(:customer).permit(:code, :email, :enterprise_id, :allow_charges)
-    end
-
     private
 
-    def add_recurring_payment_info(customer, client_secret)
+    def add_recurring_payment_info(client_secret)
       return unless client_secret
 
-      customer.gateway_recurring_payment_client_secret = client_secret
-      customer.gateway_shop_id = customer.enterprise.stripe_account&.stripe_user_id
+      @customer.gateway_recurring_payment_client_secret = client_secret
+      @customer.gateway_shop_id = @customer.enterprise.stripe_account&.stripe_user_id
+    end
+
+    def customer_params
+      params.require(:customer).permit(:code, :email, :enterprise_id, :allow_charges)
     end
   end
 end
