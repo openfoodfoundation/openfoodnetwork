ae1ccf4f0d09057251c62939893002762bf9864a
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2020-12-01 17:10:15 +0000

    Clean up promotions-related order reloading

--- app/models/spree/order_contents.rb
+++ app/models/spree/order_contents.rb
@@ -27,10 +27,7 @@ module Spree
     def update_cart(params)
       if order.update_attributes(params)
         order.line_items = order.line_items.select { |li| li.quantity > 0 }
-        # Update totals, then check if the order is eligible for any cart promotions.
-        # If we do not update first, then the item total will be wrong and ItemTotal
-        # promotion rules would not be triggered.
-        reload_totals
+
         order.ensure_updated_shipments
         reload_totals
         true
