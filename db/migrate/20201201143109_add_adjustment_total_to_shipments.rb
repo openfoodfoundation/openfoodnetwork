a8a30cac5bb4b1c2d7110b49b7a731f6aeb37339
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2020-12-01 14:31:54 +0000

    core/db/migrate/20130815024413_add_adjustment_total_to_shipments.rb

---
+++ db/migrate/20201201143109_add_adjustment_total_to_shipments.rb
@@ -0,0 +1,6 @@
+class AddAdjustmentTotalToShipments < ActiveRecord::Migration
+  def change
+    add_column :spree_shipments, :adjustment_total, :decimal,
+               precision: 10, scale: 2, default: 0.0
+  end
+end
