84d8a7f3de6f30e7a1500e2f4d6ab668166edff5
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2020-12-01 11:55:38 +0000

    core/app/models/spree/calculator/flat_percent_item_total.rb

--- app/models/calculator/flat_percent_item_total.rb
+++ app/models/calculator/flat_percent_item_total.rb
@@ -15,9 +15,8 @@ module Calculator
       Spree.t(:flat_percent)
     end
 
-    def compute(object)
-      item_total = line_items_for(object).map(&:amount).sum
-      value = item_total * BigDecimal(preferred_flat_percent.to_s) / 100.0
+    def compute(line_item)
+      value = line_item.amount * BigDecimal(self.preferred_flat_percent.to_s) / 100.0
       (value * 100).round.to_f / 100
     end
   end
