8963a787d6d837114ab039eb5cac107a5b442938
Andy Brett <andy@andybrett.com>
2020-11-30 18:39:25 -0800

    only validate unit_value presence if "items" is not chosen

--- app/models/spree/product.rb
+++ app/models/spree/product.rb
@@ -95,8 +95,7 @@ module Spree
     validates :tax_category_id, presence: true, if: "Spree::Config.products_require_tax_category"
 
     validates :variant_unit, presence: true
-    validates :unit_value, presence: true
-    validates :unit_value, numericality: true
+    validates :unit_value, presence: { if: ->(p) { %w(weight volume).include? p.variant_unit } }
     validates :variant_unit_scale,
               presence: { if: ->(p) { %w(weight volume).include? p.variant_unit } }
     validates :variant_unit_name,
