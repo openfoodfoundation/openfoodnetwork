cd278a71c6d3f0e44240bb14e08b6240f11706c1
Andy Brett <andy@andybrett.com>
2020-12-02 13:38:44 -0800

    don't clone without a connected account passed

--- app/services/recurring_payments.rb
+++ app/services/recurring_payments.rb
@@ -3,8 +3,7 @@
 class RecurringPayments
   def self.setup_for(customer)
     return unless card = customer.user.default_card
-
-    stripe_account = customer.enterprise.stripe_account&.stripe_user_id
+    return unless stripe_account = customer.enterprise.stripe_account&.stripe_user_id
 
     customer_id, payment_method_id =
       Stripe::CreditCardCloner.new.find_or_clone(card, stripe_account)
