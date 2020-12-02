fda0a98f42524642661bc4c44b8845b2c0c06ff7
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2020-12-02 15:56:03 +0000

    Fix namespacing in recently imported  Calculator::PercentOnLineItem

--- app/models/calculator/percent_on_line_item.rb
+++ app/models/calculator/percent_on_line_item.rb
@@ -1,17 +1,15 @@
 # frozen_string_literal: true
 
-module Spree
-  class Calculator
-    class PercentOnLineItem < Calculator
-      preference :percent, :decimal, default: 0
+module Calculator
+  class PercentOnLineItem < Spree::Calculator
+    preference :percent, :decimal, default: 0
 
-      def self.description
-        Spree.t(:percent_per_item)
-      end
+    def self.description
+      Spree.t(:percent_per_item)
+    end
 
-      def compute(object)
-        ((object.price * object.quantity) * preferred_percent) / 100
-      end
+    def compute(object)
+      ((object.price * object.quantity) * preferred_percent) / 100
     end
   end
 end
