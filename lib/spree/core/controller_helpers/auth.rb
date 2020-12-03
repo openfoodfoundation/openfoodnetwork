81b03906f9c0b910b47755006d490ae11f330fdd
Arun Kumar Mohan <arunmohandm@gmail.com>
2020-12-03 18:50:10 -0500

    Remove /signup routes

--- lib/spree/core/controller_helpers/auth.rb
+++ lib/spree/core/controller_helpers/auth.rb
@@ -35,7 +35,7 @@ module Spree
 
         def store_location
           # disallow return to login, logout, signup pages
-          authentication_routes = [:spree_signup_path, :spree_login_path, :spree_logout_path]
+          authentication_routes = [:spree_login_path, :spree_logout_path]
           disallowed_urls = []
           authentication_routes.each do |route|
             if respond_to?(route)
