18e220c0721c9c8b238a491a8963ccadf76518b0
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2020-12-04 18:55:09 +0000

    Fix missing attributes in AdjustmentSerializer

--- app/serializers/api/adjustment_serializer.rb
+++ app/serializers/api/adjustment_serializer.rb
@@ -2,7 +2,6 @@ module Api
   class AdjustmentSerializer < ActiveModel::Serializer
     attributes :id, :amount, :label, :eligible,
                :source_type, :source_id,
-               :adjustable_type, :adjustable_id,
-               :originator_type, :originator_id
+               :adjustable_type, :adjustable_id
   end
 end
