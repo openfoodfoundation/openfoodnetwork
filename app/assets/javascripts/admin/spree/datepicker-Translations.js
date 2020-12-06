00bd41fb92c3bfacd8839e0a88cd94eeaf2ea353
nikhilt99 <hilnik9@gmail.com>
2020-12-06 15:57:56 -0500

    fixed undefined datePicker error

--- app/assets/javascripts/admin/spree/datepicker-Translations.js
+++ app/assets/javascripts/admin/spree/datepicker-Translations.js
@@ -1,26 +1,12 @@
-/* Inicialización en español para la extensión 'UI date picker' para jQuery. */
-/* Traducido por Vester (xvester@gmail.com). */
-( function( factory ) {
-	if ( typeof define === "function" && define.amd ) {
-
-		// AMD. Register as an anonymous module.
-		define( [ "../widgets/datepicker" ], factory );
-	} else {
-
-		// Browser globals
-		factory( jQuery.datepicker );
-	}
-}( function( datepicker ) {
-
-datepicker.regional.es = {
+$.datepicker.regional.es = {
 	closeText: "Cerrar",
 	prevText: "&#x3C;Ant",
 	nextText: "Sig&#x3E;",
 	currentText: "Hoy",
 	monthNames: [ "enero","febrero","marzo","abril","mayo","junio",
-	"julio","agosto","septiembre","octubre","noviembre","diciembre" ],
+	  "julio","agosto","septiembre","octubre","noviembre","diciembre" ],
 	monthNamesShort: [ "ene","feb","mar","abr","may","jun",
-	"jul","ago","sep","oct","nov","dic" ],
+	  "jul","ago","sep","oct","nov","dic" ],
 	dayNames: [ "domingo","lunes","martes","miércoles","jueves","viernes","sábado" ],
 	dayNamesShort: [ "dom","lun","mar","mié","jue","vie","sáb" ],
 	dayNamesMin: [ "D","L","M","X","J","V","S" ],
@@ -29,28 +15,9 @@ datepicker.regional.es = {
 	firstDay: 1,
 	isRTL: false,
 	showMonthAfterYear: false,
-	yearSuffix: "" };
-datepicker.setDefaults( datepicker.regional.es );
-
-return datepicker.regional.es;
-
-} ) );
-
-/* English/UK initialisation for the jQuery UI date picker plugin. */
-/* Written by Stuart. */
-( function( factory ) {
-	if ( typeof define === "function" && define.amd ) {
-
-		// AMD. Register as an anonymous module.
-		define( [ "../widgets/datepicker" ], factory );
-	} else {
-
-		// Browser globals
-		factory( jQuery.datepicker );
-	}
-}( function( datepicker ) {
-
-datepicker.regional.en = {
+  	yearSuffix: "" 
+};
+$.datepicker.regional.en = {
 	closeText: "Done",
 	prevText: "Prev",
 	nextText: "Next",
@@ -67,9 +34,5 @@ datepicker.regional.en = {
 	firstDay: 1,
 	isRTL: false,
 	showMonthAfterYear: false,
-	yearSuffix: "" };
-datepicker.setDefaults( datepicker.regional.en);
-
-return datepicker.regional.en;
-
-} ) );
+	yearSuffix: "" 
+};
