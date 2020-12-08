0d84481179ea1c4f38126e585190d9a26fde96c3
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2020-12-08 16:55:21 +0000

    Pull in additional test coverage for DefaultTaxRate

--- spec/models/calculator/default_tax_spec.rb
+++ spec/models/calculator/default_tax_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe Calculator::DefaultTax do
+  let!(:country) { create(:country) }
+  let!(:zone) { create(:zone, name: "Country Zone", default_tax: true, zone_members: []) }
   let!(:tax_category) { create(:tax_category, tax_rates: []) }
   let!(:rate) {
     build_stubbed(:tax_rate, tax_category: tax_category, amount: 0.05, included_in_price: included_in_price)
@@ -12,6 +14,67 @@ describe Calculator::DefaultTax do
   let!(:shipment) { create(:shipment, cost: 15) }
 
   context "#compute" do
+    context "when given an order" do
+      let!(:line_item_1) { line_item }
+      let!(:line_item_2) { create(:line_item, price: 10, quantity: 3, tax_category: tax_category) }
+
+      before do
+        allow(order).to receive(:line_items) { [line_item_1, line_item_2] }
+      end
+
+      context "when no line items match the tax category" do
+        before do
+          line_item_1.tax_category = nil
+          line_item_2.tax_category = nil
+        end
+
+        it "should be 0" do
+          expect(calculator.compute(order)).to eq 0
+        end
+      end
+
+      context "when one item matches the tax category" do
+        before do
+          line_item_1.tax_category = tax_category
+          line_item_2.tax_category = nil
+        end
+
+        it "should be equal to the item total * rate" do
+          expect(calculator.compute(order)).to eq 1.5
+        end
+
+        context "correctly rounds to within two decimal places" do
+          before do
+            line_item_1.price = 10.333
+            line_item_1.quantity = 1
+          end
+
+          specify do
+            # Amount is 0.51665, which will be rounded to...
+            expect(calculator.compute(order)).to eq 0.52
+          end
+        end
+      end
+
+      context "when more than one item matches the tax category" do
+        it "should be equal to the sum of the item totals * rate" do
+          expect(calculator.compute(order)).to eq 3
+        end
+      end
+
+      context "when tax is included in price" do
+        let(:included_in_price) { true }
+
+        it "will return the deducted amount from the totals" do
+          # total price including 5% tax = $60
+          # ex tax = $57.14
+          # 57.14 + %5 = 59.997 (or "close enough" to $60)
+          # 60 - 57.14 = $2.86
+          expect(calculator.compute(order).to_f).to eql 2.86
+        end
+      end
+    end
+
     context "when tax is included in price" do
       let(:included_in_price) { true }
 
