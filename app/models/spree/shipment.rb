4f81df1bcd2de70d291a260ed685aceb5624e9eb
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2020-12-08 14:31:55 +0000

    Remove Shipment#persist_cost (now dead code)
    https://github.com/spree/spree/commit/378b9cd3deb692195f428092de3e3f4fcb62712e

--- app/models/spree/shipment.rb
+++ app/models/spree/shipment.rb
@@ -178,11 +178,6 @@ module Spree
       !shipped?
     end
 
-    def persist_cost
-      self.cost = selected_shipping_rate.cost
-      update_amounts
-    end
-
     def update_amounts
       self.update_columns(
         cost: selected_shipping_rate.cost,
