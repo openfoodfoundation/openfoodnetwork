2a0f132b8b641ab1d18632e485f717b24cc435bd
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2020-12-08 18:32:22 +0000

    Improve ShippingMethod -> Shipment association
    https://github.com/spree/spree/commit/5a54a0b6b0abc9ea3b928a243c2daace9a36c1b1

--- app/models/spree/shipping_method.rb
+++ app/models/spree/shipping_method.rb
@@ -10,7 +10,7 @@ module Spree
 
     default_scope -> { where(deleted_at: nil) }
 
-    has_many :shipments
+    has_many :shipments, through: :shipping_rates
     has_many :shipping_method_categories
     has_many :shipping_categories, through: :shipping_method_categories
     has_many :shipping_rates, inverse_of: :shipping_method
