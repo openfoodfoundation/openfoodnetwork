b8b6c989dd5ebd25daf2c02afedf6f20e7406fc1
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2020-12-13 10:41:11 +0000

    Update adjustment fields migration
    https://github.com/spree/spree/commit/2949ac7437de0157fcbdebe28bf9616951b7ca33
    
    and
    
    https://github.com/spree/spree/commit/0e77c15257183cd31b7837d9a17717b8cdb5ba4d

--- db/migrate/20201201142556_rename_adjustment_fields.rb
+++ db/migrate/20201201142556_rename_adjustment_fields.rb
@@ -4,5 +4,11 @@ class RenameAdjustmentFields < ActiveRecord::Migration
     remove_column :spree_adjustments, :originator_type
 
     add_column :spree_adjustments, :order_id, :integer
+
+    # This enables the Spree::Order#all_adjustments association to work correctly
+    Spree::Adjustment.reset_column_information
+    Spree::Adjustment.where(adjustable_type: "Spree::Order").find_each do |adjustment|
+      adjustment.update_column(:order_id, adjustment.adjustable_id)
+    end
   end
 end
