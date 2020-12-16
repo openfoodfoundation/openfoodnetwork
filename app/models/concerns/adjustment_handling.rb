b4e6a9a1e2d834ca389b075c2feee62d603a1aa1
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2020-12-16 12:53:05 +0000

    Refactor AdjustmentHandling concern

--- app/models/concerns/adjustment_handling.rb
+++ app/models/concerns/adjustment_handling.rb
@@ -5,19 +5,13 @@ module AdjustmentHandling
   #   (which is any class that has_many :adjustments) and sets amount based on the
   #   calculator as applied to the given calculable (Order, LineItems[], Shipment, etc.)
   # By default the adjustment will not be considered mandatory
-  def create_adjustment(label, order, target, calculable, adjustable, mandatory = false, state = "closed")
-    # Adjustment calculations done on Spree::Shipment objects MUST
-    # be done on their to_package'd variants instead
-    # It's only the package that contains the correct information.
-    # See https://github.com/spree/spree_active_shipping/pull/96 et. al
-    old_calculable = calculable
-    calculable = calculable.to_package if calculable.is_a?(Spree::Shipment)
-    amount = compute_amount(calculable)
+  def create_adjustment(label, order, source, adjustable, mandatory = false, state = "closed")
+    amount = compute_amount(adjustable, source)
     return if amount.zero? && !mandatory
 
-    target.adjustments.create(
+    self.adjustments.create(
       amount: amount,
-      source: self,
+      source: source,
       adjustable: adjustable,
       order: order,
       label: label,
@@ -27,7 +21,12 @@ module AdjustmentHandling
   end
 
   # Calculate the amount to be used when creating an adjustment
-  def compute_amount(calculable)
-    calculator.compute(calculable)
+  def compute_amount(calculable, source = self)
+    # Adjustment calculations done on Spree::Shipment objects MUST
+    # be done on their to_package'd variants instead
+    # It's only the package that contains the correct information.
+    # See https://github.com/spree/spree_active_shipping/pull/96 et. al
+    calculable.to_package if calculable.is_a?(Spree::Shipment)
+    source.calculator.compute(calculable)
   end
 end
