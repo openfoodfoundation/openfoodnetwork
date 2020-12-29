42468e2ef33f72e39e9189f5e411534980de916e
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2020-12-29 11:48:54 +0000

    Apply Spree::Admin::PaymentsController decorator

--- app/controllers/spree/admin/payments_controller.rb
+++ app/controllers/spree/admin/payments_controller.rb
@@ -67,6 +67,25 @@ module Spree
         redirect_to request.referer
       end
 
+      def paypal_refund
+        if request.get?
+          if @payment.source.state == 'refunded'
+            flash[:error] = Spree.t(:already_refunded, scope: 'paypal')
+            redirect_to admin_order_payment_path(@order, @payment)
+          end
+        elsif request.post?
+          response = @payment.payment_method.refund(@payment, params[:refund_amount])
+          if response.success?
+            flash[:success] = Spree.t(:refund_successful, scope: 'paypal')
+            redirect_to admin_order_payments_path(@order)
+          else
+            flash.now[:error] = Spree.t(:refund_unsuccessful, scope: 'paypal') +
+                                " (#{response.errors.first.long_message})"
+            render
+          end
+        end
+      end
+
       private
 
       def load_payment_source
