72f295a782696acb01a798028e7341557cdb7e58
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2020-12-31 19:06:03 +0000

    Ensure order totals are updated in cart spec
    `order.update!` would normally be called when the cart is updated.

--- spec/features/consumer/shopping/cart_spec.rb
+++ spec/features/consumer/shopping/cart_spec.rb
@@ -133,6 +133,7 @@ feature "full-page cart", js: true do
           cart_service = CartService.new(order)
           cart_service.populate(variants: { product_with_fee.variants.first.id => 3, product_with_tax.variants.first.id => 3 })
           order.update_distribution_charge!
+          order.update!
 
           visit main_app.cart_path
         end
