e5be249f2cc0777140d0c9a0000460d49a349e86
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-01-06 15:39:38 +0000

    Update comment on #expire_current_order

--- app/controllers/spree/paypal_controller.rb
+++ app/controllers/spree/paypal_controller.rb
@@ -92,9 +92,7 @@ module Spree
       redirect_to main_app.checkout_path
     end
 
-    # Clears the cached order. Required for #current_order to return a new order
-    # to serve as cart. See https://github.com/spree/spree/blob/1-3-stable/core/lib/spree/core/controller_helpers/order.rb#L14
-    # for details.
+    # Clears the cached order. Required for #current_order to return a new order to serve as cart.
     def expire_current_order
       session[:order_id] = nil
       @current_order = nil
