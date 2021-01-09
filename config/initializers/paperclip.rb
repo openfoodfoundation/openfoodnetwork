0ce61a10d68d92bdc7937ceb30a1568da9ec5fd2
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-01-09 20:31:43 +0000

    Patch paperclip bug

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -12,3 +12,13 @@ url_adapters = [
 Paperclip.io_adapters.registered_handlers.delete_if do |_proc, adapter_class|
   url_adapters.include? adapter_class.to_s
 end
+
+# Patches an error for missing method #silence_stream with Rails 5.0
+# Can be removed after Paperclip is upgraded to 3.5+
+module Paperclip
+  class GeometryDetector
+    def silence_stream(_stream, &block)
+      yield
+    end
+  end
+end
