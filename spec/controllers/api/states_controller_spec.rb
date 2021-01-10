fd1b14ca4a584c8025e454834b15f39dfb7fe3bd
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-01-10 18:56:43 +0000

    Fix string/integer issues in states controller spec

--- spec/controllers/api/states_controller_spec.rb
+++ spec/controllers/api/states_controller_spec.rb
@@ -33,14 +33,14 @@ module Api
       end
 
       it "paginates when page parameter is passed through" do
-        expect(@scope).to receive(:page).with(1).and_return(@scope)
+        expect(@scope).to receive(:page).with("1").and_return(@scope)
         expect(@scope).to receive(:per).with(nil)
         api_get :index, page: 1
       end
 
       it "paginates when per_page parameter is passed through" do
         expect(@scope).to receive(:page).with(nil).and_return(@scope)
-        expect(@scope).to receive(:per).with(25)
+        expect(@scope).to receive(:per).with("25")
         api_get :index, per_page: 25
       end
     end
