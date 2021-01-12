5ea2e7434866f3891a9f9ff5616e76605c43378f
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-01-12 18:06:21 +0000

    Update LineItem spec

--- spec/models/spree/line_item_spec.rb
+++ spec/models/spree/line_item_spec.rb
@@ -460,9 +460,7 @@ module Spree
         li = LineItem.new
 
         allow(li).to receive(:price) { 55.55 }
-        allow(li).to receive_message_chain(:order, :adjustments, :loaded?)
-        allow(li).to receive_message_chain(:order, :adjustments, :select)
-        allow(li).to receive_message_chain(:order, :adjustments, :where, :to_a, :sum) { 11.11 }
+        allow(li).to receive_message_chain(:all_adjustments, :only_additional, :to_a, :sum) { 11.11 }
         allow(li).to receive(:quantity) { 2 }
         expect(li.price_with_adjustments).to eq(61.11)
       end
