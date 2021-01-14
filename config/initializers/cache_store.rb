02a1116fffb1a7513e39e56ad5ad5adae40e4d5a
Luis Ramos <luisramos0@gmail.com>
2021-01-14 23:45:05 +0000

    Remove cache intrumentation
    It doesnt work in rails 5 and apparently it was introduced to debug already deleted products cache in staging
    
    See this commit that introduced it https://github.com/openfoodfoundation/openfoodnetwork/commit/10a79d5a65f8d00808a5465d805b2c51900411a5

--- config/initializers/cache_store.rb
+++ config/initializers/cache_store.rb
@@ -1,7 +1,4 @@
 unless Rails.env.production?
-  # Enable cache instrumentation, which is disabled by default
-  ActiveSupport::Cache::Store.instrument = true
-
   # Log message in the same default logger
   ActiveSupport::Cache::Store.logger = Rails.logger
 end
