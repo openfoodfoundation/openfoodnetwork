d18e79ab19a4a40e8c1e54f52ff0ecfdbb2838da
Pau Perez <saulopefa@gmail.com>
2021-01-14 10:47:25 +0100

    Move query object to app/queries/ and doc it

--- app/queries/customers_with_balance.rb
+++ app/queries/customers_with_balance.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# Fetches the customers of the specified enterprise including the aggregated balance across the
+# customer's orders. That is, we get the total balance for each customer with this enterprise.
 class CustomersWithBalance
   def initialize(enterprise)
     @enterprise = enterprise
