209e2f20c5bbc5ec71d8ca297da3e3249cd45331
Luis Ramos <luisramos0@gmail.com>
2021-01-15 00:23:37 +0000

    Add rails 5 version for js template helpers

--- config/initializers/js_template_helpers.rb
+++ config/initializers/js_template_helpers.rb
@@ -1,6 +1,14 @@
 # Make helpers (#t in particular) available to javascript templates
 # https://github.com/pitr/angular-rails-templates/issues/45#issuecomment-43229086
 
-Rails.application.assets.context_class.class_eval do
-  include ActionView::Helpers::TranslationHelper
+if ENV['DEPENDENCIES_NEXT']
+  Rails.application.config.assets.configure do |env|
+    env.context_class.class_eval do
+      include ActionView::Helpers::TranslationHelper
+    end
+  end
+else
+  Rails.application.assets.context_class.class_eval do
+    include ActionView::Helpers::TranslationHelper
+  end
 end
