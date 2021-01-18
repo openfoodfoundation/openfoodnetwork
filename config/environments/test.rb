006abee23dd8a0d393c41225a91119391f6c3454
Pau Perez <saulopefa@gmail.com>
2021-01-18 11:59:57 +0100

    Update old setting to serve static assets
    Fixes the following deprecation warning:
    
    ```
    DEPRECATION WARNING: The configuration option `config.serve_static_assets` has been renamed to `config.serve_static_files` to clarify its role (it merely enables serving everything in the `public` folder and is unrelated to the asset pipeline). The `serve_
    static_assets` alias will be removed in Rails 5.0. Please migrate your configuration files accordingly. (called from block in <top (required)> at /usr/src/app/config/environments/test.rb:13)
    ```

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -10,7 +10,7 @@ Openfoodnetwork::Application.configure do
   config.eager_load = false
 
   # Configure static asset server for tests with Cache-Control for performance
-  config.serve_static_assets = true
+  config.serve_static_files = true
   config.static_cache_control = "public, max-age=3600"
 
   # Separate cache stores when running in parallel
