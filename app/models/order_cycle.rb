ecc42800c8dd89f771c9d54d6ae30f05ac8d7083
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-01-24 19:52:12 +0000

    Update deprecated use of #uniq in OrderCyle scopes

--- app/models/order_cycle.rb
+++ app/models/order_cycle.rb
@@ -13,8 +13,8 @@ class OrderCycle < ActiveRecord::Base
   has_many :cached_incoming_exchanges, -> { where incoming: true }, class_name: "Exchange"
   has_many :cached_outgoing_exchanges, -> { where incoming: false }, class_name: "Exchange"
 
-  has_many :suppliers, -> { uniq }, source: :sender, through: :cached_incoming_exchanges
-  has_many :distributors, -> { uniq }, source: :receiver, through: :cached_outgoing_exchanges
+  has_many :suppliers, -> { distinct }, source: :sender, through: :cached_incoming_exchanges
+  has_many :distributors, -> { distinct }, source: :receiver, through: :cached_outgoing_exchanges
 
   has_many :schedules, through: :order_cycle_schedules
   has_many :order_cycle_schedules
