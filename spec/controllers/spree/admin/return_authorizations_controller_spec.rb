815fc97cdf73e1bb77f443ae4cc4f2becbaf1902
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-01-24 13:09:15 +0000

    Fix test controller issue

--- spec/controllers/spree/admin/return_authorizations_controller_spec.rb
+++ spec/controllers/spree/admin/return_authorizations_controller_spec.rb
@@ -33,6 +33,9 @@ module Spree
         expect(return_authorization.amount.to_s).to eq "20.2"
         expect(return_authorization.reason.to_s).to eq "broken"
 
+        # Reset the test controller between requests
+        reset_controller_environment
+
         # Update return authorization
         spree_put :update, id: return_authorization.id,
                            return_authorization: { amount: "10.2", reason: "half broken" }
