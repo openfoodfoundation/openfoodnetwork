5677c86f9bdc4713da5b72fda70327a2aa8df050
Pau Perez <saulopefa@gmail.com>
2021-01-25 13:33:11 +0100

    Remove enable_mail_delivery preference from DB
    It's no longer used.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210115143738) do
+ActiveRecord::Schema.define(version: 20210125123000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
