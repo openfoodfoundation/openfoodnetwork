d642984261c1a85f68c80fc698666ef28c6f534d
Andy Brett <andy@andybrett.com>
2021-02-04 11:24:35 -0800

    add db constraint; check for existing nil/nan in migration

--- db/schema.rb
+++ db/schema.rb
@@ -1061,7 +1061,7 @@ ActiveRecord::Schema.define(version: 20210202052337) do
 
   create_table "spree_variants", force: :cascade do |t|
     t.string   "sku",              limit: 255,                         default: "",    null: false
-    t.decimal  "weight",                       precision: 8, scale: 2
+    t.decimal  "weight",                       precision: 8, scale: 2,                 null: false
     t.decimal  "height",                       precision: 8, scale: 2
     t.decimal  "width",                        precision: 8, scale: 2
     t.decimal  "depth",                        precision: 8, scale: 2
