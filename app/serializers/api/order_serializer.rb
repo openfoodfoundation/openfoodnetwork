8c7bf863f89db50678f49a5c274fc7ab42f04e4e
Andy Brett <andy@andybrett.com>
2021-02-05 10:45:10 -0800

    return pending payments in order serializer

--- app/serializers/api/order_serializer.rb
+++ app/serializers/api/order_serializer.rb
@@ -16,7 +16,7 @@ module Api
     end
 
     def payments
-      object.payments.joins(:payment_method).valid
+      object.payments.joins(:payment_method).where('state IN (?)', %w(completed pending))
     end
 
     def shop_id
