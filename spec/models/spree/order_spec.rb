3045fed67eadf37d14a2a597abf85580b6fa39b3
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-02-10 20:01:53 +0000

    Update Order shipment adjustments fetching

--- spec/models/spree/order_spec.rb
+++ spec/models/spree/order_spec.rb
@@ -1088,7 +1088,8 @@ describe Spree::Order do
       Spree::Config.shipping_tax_rate = 0.25
 
       # Sanity check the fees
-      expect(order.adjustments.length).to eq 2
+      expect(order.adjustments.length).to eq 1
+      expect(order.shipment_adjustments.length).to eq 1
       expect(item_num).to eq 2
       expect(order.adjustment_total).to eq expected_fees
       expect(order.shipment.adjustment.included_tax).to eq 1.2
@@ -1105,7 +1106,7 @@ describe Spree::Order do
 
       context "when finalized fee adjustments exist on the order" do
         let(:payment_fee_adjustment) { order.adjustments.payment_fee.first }
-        let(:shipping_fee_adjustment) { order.adjustments.shipping.first }
+        let(:shipping_fee_adjustment) { order.shipment_adjustments.first }
 
         before do
           payment_fee_adjustment.finalize!
