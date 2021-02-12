c851705d5a6160f9c77249d5a747d41587fc8151
Andy Brett <andy@andybrett.com>
2021-02-12 13:36:17 -0800

    reload order's new state after processing payment

--- app/controllers/spree/orders_controller.rb
+++ app/controllers/spree/orders_controller.rb
@@ -27,6 +27,7 @@ module Spree
     def show
       @order = Spree::Order.find_by!(number: params[:id])
       ProcessPaymentIntent.new(params["payment_intent"], @order).call!
+      @order.reload
     end
 
     def empty
