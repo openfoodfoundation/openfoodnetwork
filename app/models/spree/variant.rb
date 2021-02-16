42a5d7cdc7e479873c32d2ef906675938a13eab4
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-02-16 20:29:33 +0000

    Delete dead code Variant#set_option_value

--- app/models/spree/variant.rb
+++ app/models/spree/variant.rb
@@ -174,39 +174,6 @@ module Spree
       deleted_at
     end
 
-    def set_option_value(opt_name, opt_value)
-      # no option values on master
-      return if is_master
-
-      option_type = Spree::OptionType.where(name: opt_name).first_or_initialize do |o|
-        o.presentation = opt_name
-        o.save!
-      end
-
-      current_value = option_values.detect { |o| o.option_type.name == opt_name }
-
-      if current_value.nil?
-        # then we have to check to make sure that the product has the option type
-        unless product.option_types.include? option_type
-          product.option_types << option_type
-          product.save
-        end
-      else
-        return if current_value.name == opt_value
-
-        option_values.delete(current_value)
-      end
-
-      option_value = Spree::OptionValue.where(option_type_id: option_type.id,
-                                              name: opt_value).first_or_initialize do |o|
-        o.presentation = opt_value
-        o.save!
-      end
-
-      option_values << option_value
-      save
-    end
-
     def option_value(opt_name)
       option_values.detect { |o| o.option_type.name == opt_name }.try(:presentation)
     end
