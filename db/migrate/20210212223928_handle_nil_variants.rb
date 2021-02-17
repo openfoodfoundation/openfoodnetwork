125918db1edbf866c09f88002d279656a6d30cd1
Andy Brett <andy@andybrett.com>
2021-02-16 22:25:21 -0800

    add defensive check for validations

--- db/migrate/20210212223928_handle_nil_variants.rb
+++ db/migrate/20210212223928_handle_nil_variants.rb
@@ -2,12 +2,32 @@ class HandleNilVariants < ActiveRecord::Migration
    def up
      Spree::Variant.where(product_id: nil).destroy_all
      Spree::Variant.where(unit_value: [nil, Float::NAN]).find_each do |variant|
-       variant.unit_value = 1
-       variant.save
+       if variant.price.nil? && variant.product.master == variant
+         variant.update_column(:unit_value, 1)
+       elsif !variant.valid?
+         variant.update_column(:unit_value, 1)
+       else
+         variant.unit_value = 1
+         begin
+           variant.save
+         rescue
+           variant.update_column(:unit_value, 0)
+         end
+       end
      end
      Spree::Variant.where(weight: [nil, Float::NAN]).find_each do |variant|
-       variant.weight = 0
-       variant.save
+       if variant.price.nil? && variant.product.master == variant
+         variant.update_column(:weight, 0)
+       elsif !variant.valid?
+         variant.update_column(:weight, 0)
+       else
+         variant.weight = 0
+         begin
+           variant.save
+         rescue
+           variant.update_column(:weight, 0)
+         end
+       end
      end
      change_column_null :spree_variants, :unit_value, false, 1
      change_column_null :spree_variants, :weight, false, 0.0
