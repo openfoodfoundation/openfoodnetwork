5b940496204ab962ecd4148e7d32ce3bb3503ce3
Maikel Linke <maikel@email.org.au>
2021-02-19 11:34:28 +1100

    Whitelist params for several settings for Rails 5
    Rails 5 is a bit stricter and Spree's Config#set method doesn't work
    with a Parameters object.

--- app/controllers/admin/invoice_settings_controller.rb
+++ app/controllers/admin/invoice_settings_controller.rb
@@ -1,13 +1,23 @@
 module Admin
   class InvoiceSettingsController < Spree::Admin::BaseController
     def update
-      Spree::Config.set(params[:preferences])
+      Spree::Config.set(preferences_params.to_h)
 
       respond_to do |format|
         format.html {
           redirect_to main_app.edit_admin_invoice_settings_path
         }
       end
+    end
+
+    private
+
+    def preferences_params
+      params.require(:preferences).permit(
+        :enable_invoices?,
+        :invoice_style2?,
+        :enable_receipt_printing?,
+      )
     end
   end
 end
