0b245ad7b19dd3cd02e882e4f5c637f18e39f7e8
Andy Brett <andy@andybrett.com>
2021-02-22 09:18:06 -0800

    make user attr private

--- app/queries/payments_requiring_action.rb
+++ app/queries/payments_requiring_action.rb
@@ -5,10 +5,12 @@ class PaymentsRequiringAction
     @user = user
   end
 
-  attr_reader :user
-
   def query
     Spree::Payment.joins(order: [:user]).where.not(cvv_response_message: nil).
       where("spree_users.id = ?", user.id)
   end
+
+  private
+
+  attr_reader :user
 end
