5d1d72b36bff53524326d90dfed7b0a46e3b8a92
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-02-27 12:40:01 +0000

    Update Admin::OrdersHelper#order_adjustments_for_display

--- spec/helpers/admin/orders_helper_spec.rb
+++ spec/helpers/admin/orders_helper_spec.rb
@@ -7,13 +7,14 @@ describe Admin::OrdersHelper, type: :helper do
     let(:order) { create(:order) }
 
     it "selects eligible adjustments" do
-      adjustment = create(:adjustment, order: order, adjustable: order, amount: 1)
+      adjustment = create(:adjustment, order: order, adjustable: order, amount: 1, source: nil)
 
       expect(helper.order_adjustments_for_display(order)).to eq [adjustment]
     end
 
     it "filters shipping method adjustments" do
-      create(:adjustment, order: order, adjustable: order, amount: 1, originator_type: "Spree::ShippingMethod")
+      create(:adjustment, order: order, adjustable: order, amount: 1,
+                          originator_type: "Spree::ShippingMethod")
 
       expect(helper.order_adjustments_for_display(order)).to eq []
     end
