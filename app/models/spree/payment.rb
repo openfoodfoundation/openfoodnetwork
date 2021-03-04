6bb85e9acde5107543ebb9f4f7c954624c4081b9
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-03-04 18:29:20 +0000

    Fix payment and shipment states issue
    For some reason the order objects were stale here when calling order.update! from either a payment or shipment callback, which was overwriting those states as nil on the order.

--- app/models/spree/payment.rb
+++ app/models/spree/payment.rb
@@ -199,8 +199,7 @@ module Spree
     end
 
     def update_order
-      order.payments.reload
-      order.update!
+      order.reload.update!
     end
 
     # Necessary because some payment gateways will refuse payments with
