6a039e5faba784f08f698f17e965b5f7115c1a22
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-03-07 14:10:38 +0000

    Move taxing of enterprise fees to TaxRate

--- app/models/spree/order.rb
+++ app/models/spree/order.rb
@@ -393,6 +393,7 @@ module Spree
     def create_tax_charge!
       Spree::TaxRate.adjust(self, line_items)
       Spree::TaxRate.adjust(self, shipments) if shipments.any?
+      Spree::TaxRate.adjust(self, all_adjustments.enterprise_fee)
     end
 
     def outstanding_balance
