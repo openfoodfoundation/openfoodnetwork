c8f6e1d8e79fb1d549b8b7ee11cc5c881786d00b
Pau Perez <saulopefa@gmail.com>
2021-03-08 19:45:50 +0100

    Extract OrderBalance value object in old branch

---
+++ app/models/order_balance.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class OrderBalance
+  def initialize(order)
+    @order = order
+  end
+
+  def state
+    outstanding_balance.negative? ? I18n.t(:credit_owed) : I18n.t(:balance_due)
+  end
+
+  def amount
+    Spree::Money.new(outstanding_balance, currency: order.currency)
+  end
+
+  private
+
+  attr_reader :order
+
+  def outstanding_balance
+    order.outstanding_balance
+  end
+end
