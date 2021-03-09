ffb4a178d49861277482858ddf6e88d3c74c6be2
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-03-09 17:16:34 +0000

    Ensure taxes are added in Cart test setup

--- spec/features/consumer/shopping/cart_spec.rb
+++ spec/features/consumer/shopping/cart_spec.rb
@@ -150,6 +150,8 @@ feature "full-page cart", js: true do
       before do
         add_enterprise_fee enterprise_fee
         add_product_to_cart order, product_with_tax
+        order.create_tax_charge!
+        order.update!
         visit main_app.cart_path
       end
 
