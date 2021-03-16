5bbe7688272b5db3570b155ebc0f856ae08e4a25
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-03-16 18:02:52 +0000

    Update order total in test setup

--- spec/models/spree/order_spec.rb
+++ spec/models/spree/order_spec.rb
@@ -692,7 +692,7 @@ describe Spree::Order do
         amount: 123,
         included_tax: 2
       )
-      order.reload
+      order.update!
     end
 
     it "returns a sum of all tax on the order" do
