ec4bae399564a60bb4181b288734b354f39e5ba3
Pau Perez <saulopefa@gmail.com>
2021-03-22 18:57:10 +0100

    Reproduce regression in a test

--- spec/controllers/spree/admin/reports_controller_spec.rb
+++ spec/controllers/spree/admin/reports_controller_spec.rb
@@ -291,6 +291,30 @@ describe Spree::Admin::ReportsController, type: :controller do
     end
   end
 
+
+  context 'Order Cycle Management' do
+    let!(:present_objects) { [orderA1, orderA2, orderB1, orderB2] }
+
+    context 'when the customer_balance feature is enabled' do
+      before do
+        allow(OpenFoodNetwork::FeatureToggle)
+          .to receive(:enabled?).with(:customer_balance, kind_of(Spree::User)) { true }
+
+        controller_login_as_enterprise_user [coordinator1]
+      end
+
+      it 'renders the delivery report' do
+        spree_post :order_cycle_management, {
+          q: { completed_at_lt: 1.day.ago },
+          shipping_method_in: [ "123" ], # We just need to search for shipping methods
+          report_type: "delivery",
+        }
+
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
   context "Admin" do
     before { controller_login_as_admin }
 
