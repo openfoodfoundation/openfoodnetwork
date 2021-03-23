b3d10cfb6011874f2f0c42232dc68f2e44a35793
Luis Ramos <luisramos0@gmail.com>
2021-03-23 21:20:12 +0000

    Fix validation of inventory item

--- app/models/inventory_item.rb
+++ app/models/inventory_item.rb
@@ -3,8 +3,8 @@ class InventoryItem < ActiveRecord::Base
   belongs_to :variant, class_name: "Spree::Variant"
 
   validates :variant_id, uniqueness: { scope: :enterprise_id }
-  validates :enterprise_id, presence: true
-  validates :variant_id, presence: true
+  validates :enterprise, presence: true
+  validates :variant, presence: true
   validates :visible, inclusion: { in: [true, false], message: I18n.t(:inventory_item_visibility_error) }
 
   scope :visible, -> { where(visible: true) }
