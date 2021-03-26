9c2010a43ebe482ff0749caead7415fe06f20d45
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-03-26 19:22:36 +0000

    Update SubscriptionLineItem#variant

--- app/models/subscription_line_item.rb
+++ app/models/subscription_line_item.rb
@@ -1,6 +1,6 @@
 class SubscriptionLineItem < ActiveRecord::Base
   belongs_to :subscription, inverse_of: :subscription_line_items
-  belongs_to :variant, class_name: 'Spree::Variant'
+  belongs_to :variant, -> { with_deleted }, class_name: 'Spree::Variant'
 
   validates :subscription, presence: true
   validates :variant, presence: true
@@ -10,11 +10,6 @@ class SubscriptionLineItem < ActiveRecord::Base
 
   def total_estimate
     (price_estimate || 0) * (quantity || 0)
-  end
-
-  # Ensure SubscriptionLineItem always has access to soft-deleted Variant attribute
-  def variant
-    Spree::Variant.unscoped { super }
   end
 
   # Used to calculators to estimate fees
