fa37be6072888689dda8c74c1653b0b4869d2529
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-03-27 13:24:39 +0000

    Update params syntax in engine controller specs

--- engines/order_management/spec/controllers/order_management/reports/enterprise_fee_summaries_controller_spec.rb
+++ engines/order_management/spec/controllers/order_management/reports/enterprise_fee_summaries_controller_spec.rb
@@ -25,7 +25,9 @@ describe OrderManagement::Reports::EnterpriseFeeSummariesController, type: :cont
   describe "#create" do
     context "when the parameters are valid" do
       it "sends the generated report in the correct format" do
-        post :create, report: { start_at: "2018-10-09 07:30:00" }, report_format: "csv"
+        post :create, params: {
+          report: { start_at: "2018-10-09 07:30:00" }, report_format: "csv"
+        }
 
         expect(response).to be_success
         expect(response.body).not_to be_blank
@@ -35,7 +37,9 @@ describe OrderManagement::Reports::EnterpriseFeeSummariesController, type: :cont
 
     context "when the parameters are invalid" do
       it "renders the report form with an error" do
-        post :create, report: { start_at: "invalid date" }, report_format: "csv"
+        post :create, params: {
+          report: { start_at: "invalid date" }, report_format: "csv"
+        }
 
         expect(flash[:error]).to eq(I18n.t("invalid_filter_parameters", scope: i18n_scope))
         expect(response).to render_template(new_template_path)
@@ -49,7 +53,9 @@ describe OrderManagement::Reports::EnterpriseFeeSummariesController, type: :cont
       let(:current_user) { distributor.owner }
 
       it "renders the report form with an error" do
-        post :create, report: { distributor_ids: [other_distributor.id] }, report_format: "csv"
+        post :create, params: {
+          report: { distributor_ids: [other_distributor.id] }, report_format: "csv"
+        }
 
         expect(flash[:error]).to eq(report_klass::Authorizer.parameter_not_allowed_error_message)
         expect(response).to render_template(new_template_path)
@@ -66,7 +72,7 @@ describe OrderManagement::Reports::EnterpriseFeeSummariesController, type: :cont
       let(:current_user) { distributor.owner }
 
       it "applies permissions to report" do
-        post :create, report: {}, report_format: "csv"
+        post :create, params: { report: {}, report_format: "csv" }
 
         expect(assigns(:permissions).allowed_order_cycles.to_a).to eq([order_cycle])
       end
