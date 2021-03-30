901c1589eea64f29b69abc7b7024db60946b2fc5
Luis Ramos <luisramos0@gmail.com>
2021-03-30 22:55:39 +0100

    Remove decorator, it's not needed now in v1.119 of active merchant

--- app/models/spree/gateway/stripe_sca.rb
+++ app/models/spree/gateway/stripe_sca.rb
@@ -4,7 +4,8 @@ require 'stripe/profile_storer'
 require 'stripe/credit_card_cloner'
 require 'stripe/authorize_response_patcher'
 require 'stripe/payment_intent_validator'
-require 'active_merchant/billing/gateways/stripe_payment_intents'
+require 'active_merchant/billing/gateways/stripe_payment_intents_decorator'
+require 'active_merchant/billing/gateways/stripe'
 
 module Spree
   class Gateway
