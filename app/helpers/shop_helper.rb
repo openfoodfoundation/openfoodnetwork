026c0640c45155db70552b92e5c3ac5512553d9c
Luis Ramos <luisramos0@gmail.com>
2021-04-07 23:11:14 +0100

    Remove dead code

--- app/helpers/shop_helper.rb
+++ app/helpers/shop_helper.rb
@@ -1,13 +1,4 @@
 module ShopHelper
-  def order_cycles_name_and_pickup_times(order_cycles)
-    order_cycles.map do |oc|
-      [
-        pickup_time(oc),
-        oc.id
-      ]
-    end
-  end
-
   def oc_select_options
     @order_cycles.map { |oc| { time: pickup_time(oc), id: oc.id } }
   end
