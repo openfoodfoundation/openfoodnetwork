4043e19910939ce83dd09737c5ad17c718fecb3f
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-04-07 18:10:07 +0100

    Add deleted_at to spree_return_authorizations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210329123820) do
+ActiveRecord::Schema.define(version: 20210407170804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -754,6 +754,7 @@ ActiveRecord::Schema.define(version: 20210329123820) do
     t.datetime "created_at",                                                             null: false
     t.datetime "updated_at",                                                             null: false
     t.integer  "stock_location_id"
+    t.datetime "deleted_at"
   end
 
   create_table "spree_roles", force: :cascade do |t|
