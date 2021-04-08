796068439dee9576daefdc7a0ce0e8571d1b060f
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2021-04-08 14:57:36 +0200

    Enable running Flipper migration
    The initializer ran code the needs the database in the migrated state.
    The decision is to not initialize anything relative to feature. This has to be done within the FlipperUI by the instance managers.

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -9,8 +9,3 @@ end
 Rails.configuration.middleware.use Flipper::Middleware::Memoizer, preload_all: true
 
 Flipper.register(:admins) { |actor| actor.respond_to?(:admin?) && actor.admin? }
-
-if !Flipper[:unit_price].exist?
-  # Unit price default setup, could be overided by admin in the flipper-ui interface
-  Flipper.enable_group :unit_price, :admins
-end
