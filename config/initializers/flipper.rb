1c53bbec57d8cef89fb75769677039918a7fcbfe
jibees <jb.bellet@gmail.com>
2021-04-19 17:03:49 +0200

    Add banner to Flipper UI to warn the admin users
    Add a link (escaped...) to the wiki page
    
    Co-authored-by: Pau Pérez Fabregat <saulopefa@gmail.com>

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -6,6 +6,14 @@ Flipper.configure do |config|
     Flipper.new(Flipper::Adapters::ActiveRecord.new)
   end
 end
+
+if Rails.env.production?
+  Flipper::UI.configure do |config|
+    config.banner_text = '⚠️ Production environment: be aware that the changes have an impact on the application. Please, read the how-to before: https://github.com/openfoodfoundation/openfoodnetwork/wiki/Feature-toggle-with-Flipper'
+    config.banner_class = 'danger'
+  end
+end
+
 Rails.configuration.middleware.use Flipper::Middleware::Memoizer, preload_all: true
 
 Flipper.register(:admins) { |actor| actor.respond_to?(:admin?) && actor.admin? }
