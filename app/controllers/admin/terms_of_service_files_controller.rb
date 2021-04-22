897af75c3866c084e800ef0d2183aa6494ffb76c
Maikel Linke <maikel@email.org.au>
2021-04-22 13:41:42 +1000

    Add upload of Terms of Service files
    Admins can upload files and view the most recent (current) one.

--- app/controllers/admin/terms_of_service_files_controller.rb
+++ app/controllers/admin/terms_of_service_files_controller.rb
@@ -3,6 +3,25 @@
 module Admin
   class TermsOfServiceFilesController < Spree::Admin::BaseController
     def show
+      @current_file = TermsOfServiceFile.current
+      @new_file = TermsOfServiceFile.new
+    end
+
+    def create
+      TermsOfServiceFile.create!(file_params)
+      redirect_to main_app.admin_terms_of_service_files_path
+    end
+
+    private
+
+    # Needed by Spree::Admin::BaseController#authorize_admin or it
+    # tries to find a Spree model.
+    def model_class
+      TermsOfServiceFile
+    end
+
+    def file_params
+      params.require(:terms_of_service_file).permit(:attachment)
     end
   end
 end
