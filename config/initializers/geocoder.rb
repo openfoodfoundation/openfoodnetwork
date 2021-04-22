42d002cee8f0dfc1f9e0e88528f33e9c3acec4aa
Cillian O'Ruanaidh <contact@cilliano.com>
2021-04-22 22:10:36 +0100

    Increase the default geocoder timeout to prevent geocoding timeouts in development
    The timeout default was 3 before, this changes it to 6. I presume these timeouts could happen in production too.

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -2,7 +2,7 @@
 # The key is stored in config/application.yml.
 
 Geocoder.configure(
-  timeout: ENV.fetch('GEOCODER_TIMEOUT', 3).to_i,
+  timeout: ENV.fetch('GEOCODER_TIMEOUT', 6).to_i,
   lookup: ENV.fetch('GEOCODER_SERVICE', :google).to_sym,
   use_https: true,
   api_key: ENV.fetch('GEOCODER_API_KEY', ENV["GOOGLE_MAPS_API_KEY"])
