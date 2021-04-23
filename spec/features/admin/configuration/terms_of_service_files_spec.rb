d8098da657c47d8c6bde220faf766edfe9ed7007
Maikel Linke <maikel@email.org.au>
2021-04-23 11:56:40 +1000

    Remove old UI to edit Terms of service
    It's now completely replaced by file uploads on a separate page.

--- spec/features/admin/configuration/terms_of_service_files_spec.rb
+++ spec/features/admin/configuration/terms_of_service_files_spec.rb
@@ -13,7 +13,10 @@ describe "Terms of Service files" do
     it "can be reached via Configuration" do
       visit spree.edit_admin_general_settings_path
       click_link "Terms of Service"
+
       expect(page).to have_content "No terms of services have been uploaded yet."
+      expect(page).to have_content "your old Terms of service"
+      expect(page).to have_link "Terms of service", href: "/Terms-of-service.pdf"
     end
 
     it "can be uploaded" do
