56c4278a6a898ddea39d161728920b49a7176dc8
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2021-04-30 16:39:24 +0200

    Add tailwind
     - Configure postcss-loader to run with webpack

---
+++ app/assets/javascripts/css/tailwind.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./app/views/**/*.haml',],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
