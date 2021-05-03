6d698d99af9cb6eef72bc41fd1f0b41d1f6b0e97
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2021-05-03 17:28:50 +0200

    Add tailwind
     - Configure postcss-loader to run with webpack
     - Store generated CSS files into `public/packs/css`
     - Add class only used into './app/views/**/*.haml' files.

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./app/**/*.html.haml"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
