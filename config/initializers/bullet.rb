c14ccdc21c53f8fe5547004a959813dc8ac49dda
Viktor Smari <viktorsmari@gmail.com>
2021-05-09 16:49:00 +0200

    Add bullet footer
    Left corner of the page will show the bullet warnings

--- config/initializers/bullet.rb
+++ config/initializers/bullet.rb
@@ -3,6 +3,7 @@
 if defined?(Bullet) && ENV.fetch("ENABLE_BULLET", false)
   Rails.application.config.after_initialize do
     Bullet.enable = true
+    Bullet.add_footer = true
     Bullet.bullet_logger = true
     Bullet.rails_logger = true
   end
