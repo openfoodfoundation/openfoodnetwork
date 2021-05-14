471a7903f652ef52d8a0cfd2b05245356b9359dc
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-05-14 15:23:28 +0100

    Update PaymentIntentValidator spec

--- spec/lib/stripe/payment_intent_validator_spec.rb
+++ spec/lib/stripe/payment_intent_validator_spec.rb
@@ -27,20 +27,8 @@ module Stripe
         it "returns payment intent id and does not raise" do
           expect {
             result = validator.call(payment_intent_id, stripe_account_id)
-            expect(result).to eq payment_intent_id
+            expect(result).to eq payment_intent_response_body
           }.to_not raise_error Stripe::StripeError
-        end
-      end
-
-      context "when payment intent status is not requires status" do
-        let(:payment_intent_response_body) {
-          JSON.generate(id: payment_intent_id, status: "failed")
-        }
-
-        it "raises Stripe error with an invalid_payment_state message" do
-          expect {
-            validator.call(payment_intent_id, stripe_account_id)
-          }.to raise_error Stripe::StripeError, "Invalid payment state: failed"
         end
       end
 
