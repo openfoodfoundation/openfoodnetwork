e90e0f5cb068ccb628e9244782885a9d5ec41265
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-05-25 20:03:43 +0100

    Remove Dalli gem and references
    This gem is a Memcached client that's no longer needed

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -2,7 +2,6 @@ if ENV['DATADOG_RAILS_APM']
   Datadog.configure do |c|
     c.use :rails, service_name: 'rails'
     c.use :delayed_job, service_name: 'delayed_job'
-    c.use :dalli, service_name: 'memcached'
 
     c.analytics_enabled = true
     c.runtime_metrics_enabled = true
