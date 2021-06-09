083f512aae9cbb2cb541f1c085a6bbcc420c1318
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-06-09 18:19:58 +0100

    Refactor Sidekiq settings and set expiry in development
    This means old cache entries will be automatically removed instead of perpetually filling up the local Redis instance in dev.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,15 @@
-redis_jobs_url = ENV.fetch("OFN_REDIS_JOBS_URL", "redis://localhost:6381/0")
+# Redis connection configuration for Sidekiq
+
+redis_connection_settings = {
+  url: ENV.fetch("OFN_REDIS_JOBS_URL", "redis://localhost:6381/0"),
+  network_timeout: 5,
+  expires_in: Rails.env.development? ? 90.minutes : nil
+}
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: redis_jobs_url, network_timeout: 5 }
+  config.redis = redis_connection_settings
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: redis_jobs_url, network_timeout: 5 }
+  config.redis = redis_connection_settings
 end
