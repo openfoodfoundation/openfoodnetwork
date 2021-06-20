3128232d7ebe7ed152eb06e3d529cb5a45db5ad1
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-06-20 09:08:05 +0100

    Create customers controller
    And add new customers routes.
    
    Disable them in production for now.

---
+++ app/controllers/api/v1/customers_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class CustomersController < Api::V1::BaseController
+      def index; end
+
+      def show; end
+
+      def update; end
+
+      def destroy; end
+    end
+  end
+end
