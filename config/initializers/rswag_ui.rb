ae2726fc6bfd82f8b0e46e2cdb01739a9a54f8e9
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-06-21 09:21:41 +0100

    Generate rswag initializers
    Done via rswag rake tasks and tidied up a bit.

---
+++ config/initializers/rswag_ui.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+Rswag::Ui.configure do |config|
+  # List the Swagger endpoints that you want to be documented through the
+  # swagger-ui. The first parameter is the path (absolute or relative to the UI
+  # host) to the corresponding endpoint and the second is a title that will be
+  # displayed in the document selector.
+  # NOTE: If you're using rspec-api to expose Swagger files
+  # (under swagger_root) as JSON or YAML endpoints, then the list below should
+  # correspond to the relative paths for those endpoints.
+
+  config.swagger_endpoint '/api-docs/v1/swagger.yaml', 'API V1 Docs'
+
+  # Add Basic Auth in case your API is private
+  # config.basic_auth_enabled = true
+  # config.basic_auth_credentials 'username', 'password'
+end
