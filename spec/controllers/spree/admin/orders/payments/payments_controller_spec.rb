51d93d1a0e7a7343dd691a3295d7d2933fcc513b
Pau Perez <saulopefa@gmail.com>
2021-07-01 13:24:57 +0200

    Replace custom redirect matcher with built-in one

--- spec/controllers/spree/admin/orders/payments/payments_controller_spec.rb
+++ spec/controllers/spree/admin/orders/payments/payments_controller_spec.rb
@@ -158,18 +158,13 @@ describe Spree::Admin::PaymentsController, type: :controller do
       end
 
       def redirects_to_list_of_payments_with_success_flash
-        expect_redirect_to spree.admin_order_payments_url(order)
+        expect(response).to redirect_to spree.admin_order_payments_url(order)
         expect(flash[:success]).to eq "Payment has been successfully created!"
       end
 
       def redirects_to_new_payment_page_with_flash_error(flash_error)
-        expect_redirect_to spree.new_admin_order_payment_url(order)
+        expect(response).to redirect_to spree.new_admin_order_payment_url(order)
         expect(flash[:error]).to eq flash_error
-      end
-
-      def expect_redirect_to(path)
-        expect(response.status).to eq 302
-        expect(response.location).to eq path
       end
     end
   end
