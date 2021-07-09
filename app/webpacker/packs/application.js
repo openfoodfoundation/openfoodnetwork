8c47a8d5fd2c63ad3222c40f6e75d54cc1739bc4
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2021-07-09 15:12:42 +0200

    Compile with webpack tailwind

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -1,9 +1,12 @@
 /* eslint no-console:0 */
 
 // StimulusJS
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
+import { Application } from "stimulus";
+import { definitionsFromContext } from "stimulus/webpack-helpers";
 
-const application = Application.start()
-const context = require.context("controllers", true, /.js$/)
-application.load(definitionsFromContext(context))
+const application = Application.start();
+const context = require.context("controllers", true, /.js$/);
+application.load(definitionsFromContext(context));
+
+// Add tailwind CSS
+import "./application.scss";
