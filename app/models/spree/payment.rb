b4a9a6fea5585021f2ba945d717230055f0e66c5
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-07-09 13:28:19 +0100

    Add :incomplete scope in Spree::Payment

--- app/models/spree/payment.rb
+++ app/models/spree/payment.rb
@@ -46,6 +46,7 @@ module Spree
     scope :from_credit_card, -> { where(source_type: 'Spree::CreditCard') }
     scope :with_state, ->(s) { where(state: s.to_s) }
     scope :completed, -> { with_state('completed') }
+    scope :incomplete, -> { where(state: %w(checkout pending requires_authorization)) }
     scope :pending, -> { with_state('pending') }
     scope :failed, -> { with_state('failed') }
     scope :valid, -> { where('state NOT IN (?)', %w(failed invalid)) }
