af1e67e24a0fb46c9845f9e768e5e095ff3b0f92
Andy Brett <andy@andybrett.com>
2021-07-13 10:05:22 -0700

    add omniauth methods and fields on User

--- app/models/spree/user.rb
+++ app/models/spree/user.rb
@@ -53,7 +53,10 @@ module Spree
     end
 
     def self.from_omniauth(auth)
-      ##
+      where(auth.slice(:provider, :uid)).first_or_create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+      end
     end
 
     # Whether a user has a role or not.
