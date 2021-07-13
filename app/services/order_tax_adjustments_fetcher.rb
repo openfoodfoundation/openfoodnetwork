cab36f375bab24bd67416d06b34462160f9dad54
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-07-13 13:09:11 +0200

    Update app/services/order_tax_adjustments_fetcher.rb
    Co-authored-by: Maikel <maikel@email.org.au>

--- app/services/order_tax_adjustments_fetcher.rb
+++ app/services/order_tax_adjustments_fetcher.rb
@@ -10,8 +10,7 @@ class OrderTaxAdjustmentsFetcher
   def totals
     order.all_adjustments.tax.each_with_object({}) do |adjustment, hash|
       tax_rate = adjustment.originator
-      tax_amounts = { tax_rate => adjustment.amount }
-      hash.update(tax_amounts) { |_tax_rate, amount1, amount2| amount1 + amount2 }
+      hash[tax_rate] = hash[tax_rate].to_f + adjustment.amount
     end
   end
 
