58baa6bd0a55252b1b015a6bc09a661a2de7ffa6
Andy Brett <andy@andybrett.com>
2021-07-16 12:02:47 -0700

    add error handling to OIDC sign in

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,4 +3,10 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
     sign_in_and_redirect @user
   end
+
+  def failure
+    error_message = request.env["omniauth.error"].to_s
+    flash[:error] = t("devise.oidc.failure", error: error_message)
+    super
+  end
 end
