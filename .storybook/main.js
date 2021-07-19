2200e582b883a5152e4363bad38688748b8df766
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2021-07-19 17:21:50 +0200

    Add webpack configuration to storybook
    Documentation: https://storybook.js.org/docs/riot/configure/webpack

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,13 @@
+const railsWebpackEnv = require("../config/webpack/development");
+
 module.exports = {
-  stories: ['../spec/components/stories/**/*.stories.json'],
-  addons: [
-    '@storybook/addon-docs',
-    '@storybook/addon-controls',
-  ],
+  stories: ["../spec/components/stories/**/*.stories.json"],
+  addons: ["@storybook/addon-docs", "@storybook/addon-controls"],
+  webpackFinal: async (config) => {
+    config.module.rules = [
+      ...config.module.rules,
+      ...railsWebpackEnv.module.rules,
+    ];
+    return config;
+  },
 };
