ac64908c2bd87f2e663e8e22c3ff48f8c5a1c7f3
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-07-22 10:18:18 +0100

    Tidy up compiling helper

--- spec/system/support/precompile_assets.rb
+++ spec/system/support/precompile_assets.rb
@@ -11,13 +11,11 @@ RSpec.configure do |config|
     # We can use webpack-dev-server for tests, too!
     # Useful if you working on a frontend code fixes and want to verify them via system tests.
     if Webpacker.dev_server.running?
-      $stdout.puts "\n⚙️  Webpack dev server is running! Skip assets compilation.\n"
       next
     else
-      $stdout.puts "\n🐢  Precompiling assets.\n"
+      $stdout.puts "\n Precompiling assets.\n"
 
-      # The code to run webpacker:compile Rake task
-      # ...
+      Webpacker.compile
     end
   end
 end
