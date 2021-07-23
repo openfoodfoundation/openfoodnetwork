cdf25ee580fffc58494d4a2d8bffe4e1b04548ec
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-07-23 23:26:39 +0100

    Fix some syntax issues

--- app/assets/javascripts/admin/spree/calculator.js
+++ app/assets/javascripts/admin/spree/calculator.js
@@ -1,16 +1,17 @@
 $(function() {
-  var calculator_select = $('select#calc_type')
-  var original_calc_type = calculator_select.attr('value');
+  var calculator_select = $('select#calc_type');
+  var original_calc_type = calculator_select.val();
   $('.calculator-settings-warning').hide();
-  calculator_select.change(function() {
-    if (calculator_select.attr('value') == original_calc_type) {
+
+  calculator_select.on("change", function() {
+    if (calculator_select.val() === original_calc_type) {
       $('div.calculator-settings').show();
       $('.calculator-settings-warning').hide();
       $('.calculator-settings').find('input,textarea').prop("disabled", false);
     } else {
       $('div.calculator-settings').hide();
       $('.calculator-settings-warning').show();
-      $('.calculator-settings').find('input,texttarea').prop("disabled", true);
+      $('.calculator-settings').find('input,textarea').prop("disabled", true);
     }
   });
 })
