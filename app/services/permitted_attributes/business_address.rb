26c7cb2bd383835bbb8cbd785eab704fe2367790
Nihal Mohammed <mnihal64@outlook.com>
2021-07-26 19:00:07 +0530

    Fix business_address attribute permissions

--- app/services/permitted_attributes/business_address.rb
+++ app/services/permitted_attributes/business_address.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 module PermittedAttributes
-    class BusinessAddress
-      def self.attributes
-        [
-          :business_name, :address1, :address2,
-          :city, :country_id, :state_id, :zipcode,
-          :phone
-        ]
-      end
+  class BusinessAddress
+    def self.attributes
+      [
+        :company, :address1, :address2,
+        :city, :country_id, :state_id, :zipcode,
+        :phone
+      ]
     end
   end
+end
   
