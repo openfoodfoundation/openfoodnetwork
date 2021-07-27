83f6132b6abff51a537d248d780bd4f32413d618
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-07-27 14:46:39 +0100

    Fix shipping tax display in regular invoice template

---
+++ app/helpers/tax_helper.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module TaxHelper
+  def display_taxes(taxable, display_zero: true)
+    if !taxable.included_tax_total.zero?
+      amount = Spree::Money.new(taxable.included_tax_total, currency: taxable.currency)
+      I18n.t(:tax_amount_included, amount: amount)
+    elsif !taxable.additional_tax_total.zero?
+      Spree::Money.new(taxable.additional_tax_total, currency: taxable.currency)
+    else
+      Spree::Money.new(0.00, currency: taxable.currency) if display_zero
+    end
+  end
+
+  def display_total_with_tax(taxable)
+    total = taxable.amount + taxable.additional_tax_total
+    Spree::Money.new(total, currency: taxable.currency)
+  end
+end
