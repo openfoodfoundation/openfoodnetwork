48c1312ceada4aa007202f43c2f181a2cbe12be5
Nihal Mohammed <mnihal64@outlook.com>
2021-08-05 14:49:24 +0530

    Remove expectation to generate clean name for StripeConnect from payment_method_spec

--- spec/models/spree/payment_method_spec.rb
+++ spec/models/spree/payment_method_spec.rb
@@ -60,7 +60,6 @@ module Spree
     it "generates a clean name for known Payment Method types" do
       expect(Spree::PaymentMethod::Check.clean_name).to eq(I18n.t("spree.admin.payment_methods.providers.check"))
       expect(Spree::Gateway::PayPalExpress.clean_name).to eq(I18n.t("spree.admin.payment_methods.providers.paypalexpress"))
-      expect(Spree::Gateway::StripeConnect.clean_name).to eq(I18n.t("spree.admin.payment_methods.providers.stripeconnect"))
       expect(Spree::Gateway::StripeSCA.clean_name).to eq(I18n.t("spree.admin.payment_methods.providers.stripesca"))
       expect(Spree::Gateway::BogusSimple.clean_name).to eq(I18n.t("spree.admin.payment_methods.providers.bogussimple"))
       expect(Spree::Gateway::Bogus.clean_name).to eq(I18n.t("spree.admin.payment_methods.providers.bogus"))
