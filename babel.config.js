ec4d5bd9a2d390ecee2810df037251d1aa2645bf
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2021-08-24 14:58:49 +0200

    Remove babel warning

--- babel.config.js
+++ babel.config.js
@@ -64,7 +64,8 @@ module.exports = function(api) {
         {
           async: false
         }
-      ]
+      ],
+      ["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
     ].filter(Boolean)
   }
 }
