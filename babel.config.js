600e6bbe956fdee77f2638a4c9a295738806bc3d
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2021-08-26 15:23:25 +0200

    Remove warning

--- babel.config.js
+++ babel.config.js
@@ -42,6 +42,7 @@ module.exports = function(api) {
       isTestEnv && 'babel-plugin-dynamic-import-node',
       '@babel/plugin-transform-destructuring',
       ["@babel/plugin-proposal-private-methods", { loose: true }],
+      ["@babel/plugin-proposal-private-property-in-object", { "loose": true }],
       [
         '@babel/plugin-proposal-class-properties',
         {
