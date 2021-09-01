dc8939c35f70a193641c50f65aee3ea1e7fca039
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-09-01 23:21:52 +0100

    Strip subdomains from session cookie host

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,9 +3,15 @@
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
+
+domain = if Rails.env.staging? || Rails.env.production?
+           ".#{ENV['SITE_URL'].gsub(/^(www\.)|^(app\.)|^(staging\.)|^(stg\.)/, '')}"
+         else
+           :all
+         end
+
 Openfoodnetwork::Application.config.session_store(
   :active_record_store,
   key: "_ofn_session_id",
-  domain: :all,
-  tld_length: 2
+  domain: domain
 )
