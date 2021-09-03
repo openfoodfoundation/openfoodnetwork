6c77f83eef6e3b42e770565aa88950d84f8e9d56
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-09-03 16:56:30 +0100

    Update postcss config for purging

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+const environment = {
   plugins: [
     require('postcss-import'),
     require('tailwindcss'),
@@ -12,3 +12,18 @@ module.exports = {
     })
   ]
 }
+
+if (process.env.RAILS_ENV === "production" || process.env.RAILS_ENV === "staging") {
+  environment.plugins.push(
+      require('@fullhuman/postcss-purgecss')({
+        content: [
+          './app/views/**/*.html.erb',
+          './app/views/**/*.html.haml',
+          './app/assets/javascript/templates/**/*.html.haml',
+        ],
+        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+      })
+  )
+}
+
+module.exports = environment
