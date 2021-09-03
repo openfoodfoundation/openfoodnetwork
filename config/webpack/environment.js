a2e222ab0e47b7966f2b94bdba1a943d4f36c4ab
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2021-09-03 16:00:17 +0200

    Configure webpacker to load scss files

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,26 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require("@rails/webpacker");
 
-module.exports = environment
+// Get the actual sass-loader config
+const sassLoader = environment.loaders.get("sass");
+const sassLoaderConfig = sassLoader.use.find(function (element) {
+  return element.loader == "sass-loader";
+});
+
+// Use Dart-implementation of Sass (default is node-sass)
+const options = sassLoaderConfig.options;
+options.implementation = require("sass");
+
+function hotfixPostcssLoaderConfig(subloader) {
+  const subloaderName = subloader.loader;
+  if (subloaderName === "postcss-loader") {
+    subloader.options.postcssOptions = subloader.options.config;
+    delete subloader.options.config;
+  }
+}
+
+environment.loaders.keys().forEach((loaderName) => {
+  const loader = environment.loaders.get(loaderName);
+  loader.use.forEach(hotfixPostcssLoaderConfig);
+});
+
+module.exports = environment;
