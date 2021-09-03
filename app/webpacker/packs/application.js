be5a55c7a8c0d46897dff6f121a7adeb3d86ff2c
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2021-09-03 16:00:34 +0200

    Add tailwind and import as a scss file in webpack

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -3,6 +3,7 @@
 // StimulusJS
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
+import "./application.scss"
 
 const application = Application.start()
 const context = require.context("controllers", true, /.js$/)
