cf989483ceb1e82f3a3bcc476be12a6851e6a325
Nihal <mnihal64@outlook.com>
2021-09-17 18:28:12 +0530

    Fix SQL syntax

--- db/migrate/20210914084059_migrate_stripe_connect_enterprise_to_stripe_sca.rb
+++ db/migrate/20210914084059_migrate_stripe_connect_enterprise_to_stripe_sca.rb
@@ -1,6 +1,6 @@
 class MigrateStripeConnectEnterpriseToStripeSca < ActiveRecord::Migration[6.1]
   def up
-    execute "UPDATE spree_preferences SET key = replace( key, 'stripe_connect', 'stripe_sca') WHERE key like '/spree/gateway/stripe_connect/%'"
+    execute "UPDATE spree_preferences SET key = replace( key, 'stripe_connect', 'stripe_sca') WHERE key LIKE '/spree/gateway/stripe_connect/'"
   end
 end
 
