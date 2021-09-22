786b198f4d41fd97e940d6a215fd789f4460c018
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2021-09-22 10:31:53 +0200

    Create an updateinput controller
     - that update the targets input value to the value stored in data attribute
     - Add tests

---
+++ spec/javascripts/stimulus/update_controller_test.js
@@ -0,0 +1,30 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { Application } from "stimulus";
+import updateinput_controller from "../../../app/webpacker/controllers/updateinput_controller";
+
+describe("updateInput controller", () => {
+  describe("#update", () => {
+    beforeEach(() => {
+      document.body.innerHTML = `<form data-controller="updateinput">
+        <input id="input" type="hidden" value="false" data-updateinput-target="input" />
+        <div id="submit" data-action="click->updateinput#update" data-updateinput-value="true" />
+      </form>`;
+
+      const application = Application.start();
+      application.register("updateinput", updateinput_controller);
+    });
+
+    it("update the input value", () => {
+      const submit = document.getElementById("submit");
+      const input = document.getElementById("input");
+      expect(input.value).toBe("false");
+
+      submit.click();
+
+      expect(input.value).toBe("true");
+    });
+  });
+});
