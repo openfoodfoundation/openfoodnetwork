cb2c6835c4cec404d9bf82f2b37fe3d01a653f34
Maikel Linke <maikel@email.org.au>
2021-09-27 12:38:31 +1000

    Standardise default country and state
    The OFN software was first written in Melbourne and a lot of spec code
    relies on the state Victoria being in the database. When we imported
    Spree code, it came with its default state Alabame in the USA. Having
    two different defaults is confusing and caused specs to fail in rare
    circumstances. Let's standardise it here which will also open the path
    to removing the seeding of test data and the exclusion of states and
    countries in the database cleaner.

--- spec/factories/state_factory.rb
+++ spec/factories/state_factory.rb
@@ -2,14 +2,8 @@
 
 FactoryBot.define do
   factory :state, class: Spree::State do
-    name { 'Alabama' }
-    abbr { 'AL' }
-    country do |country|
-      if usa = Spree::Country.find_by(numcode: 840)
-        country = usa
-      else
-        country.association(:country)
-      end
-    end
+    name { "Victoria" }
+    abbr { "Vic" }
+    country
   end
 end
