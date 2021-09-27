567a3609ab86ecea60d3f73624d8889190282c6f
Nihal <mnihal64@outlook.com>
2021-09-27 15:10:20 +0530

    Migrate StripeConnect account owner to StripeSCA

---
+++ db/migrate/20210927092807_migrate_stripe_account_owner_to_stripe_sca.rb
@@ -0,0 +1,17 @@
+class MigrateStripeAccountOwnerToStripeSca < ActiveRecord::Migration[6.1]
+  def up
+    replace_preferences_key("/spree/gateway/strip_connect", "/spree/gateway/stripe_sca")
+  end
+
+  private
+
+  def replace_preferences_key(from_pattern, to_pattern)
+    updated_pref_key = "replace( pref.key, '" + from_pattern + "', '" + to_pattern + "')"
+    Spree::Preference.connection.execute(
+      "UPDATE spree_preferences pref SET key = " + updated_pref_key + "
+        WHERE pref.key like '" + from_pattern + "%'
+          AND NOT EXISTS (SELECT 1 FROM spree_preferences existing_pref
+                           WHERE existing_pref.key = " + updated_pref_key + ")"
+    )
+  end
+end
