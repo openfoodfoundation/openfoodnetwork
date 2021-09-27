ad1dc54699f2a072f283e0e8e823afadead2db34
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-09-27 14:54:15 +0100

    Recompile in development when views change on disk

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,26 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+process.env.NODE_ENV = process.env.NODE_ENV || "development"
 
-const environment = require('./environment')
+const environment = require("./environment")
+
+// Trigger recompile in dev when views change.
+const chokidar = require("chokidar")
+if (process.env.RAILS_ENV == "development") {
+  environment.config.merge({
+    devServer: {
+      before: (app, server) => {
+        chokidar.watch(
+          [
+            "app/views/**/*.html.erb",
+            "app/views/**/*.html.haml",
+            "app/components/**/*.html.haml",
+            "app/assets/javascript/templates/**/*.html.haml",
+            "engines/**/*.html.haml",
+          ],
+          { awaitWriteFinish: true }
+        ).on("change", () => server.sockWrite(server.sockets, "content-changed"))
+      }
+    }
+  })  
+}
 
 module.exports = environment.toWebpackConfig()
