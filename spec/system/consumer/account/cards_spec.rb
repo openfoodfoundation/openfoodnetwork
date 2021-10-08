97a79a77e100ef89439bfeb42a86932d83e4aa1e
filipefurtad0 <filipefurtado@gmail.com>
2021-10-08 14:29:41 +0100

    Updates/moves consumer/account/cards_spec into system

--- spec/system/consumer/account/cards_spec.rb
+++ spec/system/consumer/account/cards_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
+require 'system_helper'
 
 describe "Credit Cards", js: true do
   include AuthenticationHelper
@@ -64,7 +64,6 @@ describe "Credit Cards", js: true do
       # Allows switching of default card
       within(".card#card#{non_default_card.id}") do
         find_field('default_card').click
-        accept_alert
         expect(find_field('default_card')).to be_checked
       end
 
