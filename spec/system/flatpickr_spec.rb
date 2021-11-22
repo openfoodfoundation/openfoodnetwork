21b7e22121b4f7ff9fe311bf514be61d2b4c5bca
Thierry Jet <thierry.jet@kombicode.com>
2021-11-22 14:11:18 +0100

    Update flatpickr test in orders context with date range field

--- spec/system/flatpickr_spec.rb
+++ spec/system/flatpickr_spec.rb
@@ -9,7 +9,7 @@ describe "Test Flatpickr", js: true do
   context "orders" do
     it "opens the datepicker and closes it using the 'CLOSE' button" do
       login_as_admin_and_visit 'admin/orders'
-      open_datepicker('#q_completed_at_gteq')       
+      open_datepicker('#q_ranged_from_to')       
       # Looks for the close button and click it
       within(".flatpickr-calendar.open") do
         expect(page).to have_selector '.shortcut-buttons-flatpickr-buttons'
@@ -21,14 +21,14 @@ describe "Test Flatpickr", js: true do
     
     it "opens the datepicker and sets date to today" do
       login_as_admin_and_visit 'admin/orders'
-      open_datepicker('#q_completed_at_gteq')
+      open_datepicker('#q_ranged_from_to')
       choose_today_from_datepicker
-      check_fielddate('#q_completed_at_gteq', Date.today())       
+      check_fielddate('#q_ranged_from_to', Date.today())       
     end
     
     it "opens the datepicker and closes it by clicking outside" do
       login_as_admin_and_visit 'admin/orders'
-      open_datepicker('#q_completed_at_gteq')
+      open_datepicker('#q_ranged_from_to')
       find("#admin-menu").click       
       # Should no more have opened flatpickr
       expect(page).not_to have_selector '.flatpickr-calendar.open'
