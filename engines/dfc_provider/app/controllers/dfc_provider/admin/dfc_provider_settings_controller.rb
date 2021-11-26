f6bfa7ff19efacac58366060beec2e7683ba5c6b
François Turbelin <pacodelaluna@gmail.com>
2021-11-26 18:20:18 +0100

    link provider account to existing user

---
+++ engines/dfc_provider/app/controllers/dfc_provider/admin/dfc_provider_settings_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module DfcProvider
+  module Admin
+    class DfcProviderSettingsController < Spree::Admin::BaseController
+      def show; end
+    end
+  end
+end
--- engines/dfc_provider/config/routes.rb
+++ engines/dfc_provider/config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 DfcProvider::Engine.routes.draw do
+  namespace :admin do
+    resource :dfc_provider_settings, only: %i[show]
+  end
+
   namespace :api do
     scope :dfc_provider, as: :dfc_provider, path: '/dfc_provider' do
       resources :enterprises, only: [:show] do
