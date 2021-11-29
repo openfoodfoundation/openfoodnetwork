887879f410f8fe459e421bc57310fa8cf7a24226
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-11-29 09:44:10 +0000

    Create job for handling of closing order cycles

---
+++ app/jobs/order_cycle_closing_job.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class OrderCycleClosingJob < ActiveJob::Base
+  def perform
+    return if recently_closed_order_cycles.empty?
+
+    send_notifications
+    mark_as_processed
+  end
+
+  private
+
+  def recently_closed_order_cycles
+    @recently_closed_order_cycles ||= OrderCycle.closed.unprocessed.
+      where(
+        'order_cycles.orders_close_at BETWEEN (?) AND (?)', 1.hour.ago, Time.zone.now
+      ).select(:id, :automatic_notifications).to_a
+  end
+
+  def send_notifications
+    recently_closed_order_cycles.each do |oc|
+      OrderCycleNotificationJob.perform_later(oc.id) if oc.automatic_notifications?
+    end
+  end
+
+  def mark_as_processed
+    OrderCycle.where(id: recently_closed_order_cycles).update_all(
+      processed_at: Time.zone.now,
+      updated_at: Time.zone.now
+    )
+  end
+end
