8d20174a0fbaf80c69cc4ad1fd3bec2933b7f798
filipefurtad0 <filipefurtado@gmail.com>
2021-12-03 11:06:04 +0000

    Deletes doubled unused pick_date method

--- spec/support/features/datepicker_helper.rb
+++ spec/support/features/datepicker_helper.rb
@@ -48,12 +48,6 @@ module Features
       month.to_s + " " + year.to_s
     end
 
-    def pick_date(calendar_selector, date_selector)
-      find(calendar_selector).click
-      select_datetime_from_datepicker date_selector
-      find("body").send_keys(:escape)
-    end
-
     def pick_datetime(calendar_selector, datetime_selector)
       find(calendar_selector).click
       select_datetime_from_datepicker datetime_selector
