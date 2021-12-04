6d1c94ae4f0ae5f2891c9c40462097b22c9f0ba9
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-12-04 11:49:56 +0000

    Delete routes to non-existant content controller

--- config/routes/spree.rb
+++ config/routes/spree.rb
@@ -169,8 +169,6 @@ Spree::Core::Engine.routes.draw do
   resources :products
 
   # Used by spree_paypal_express
-  get '/content/cvv', :to => 'content#cvv', :as => :cvv
-  get '/content/*path', :to => 'content#show', :as => :content
   get '/paypal', :to => "paypal#express", :as => :paypal_express
   get '/paypal/confirm', :to => "paypal#confirm", :as => :confirm_paypal
   get '/paypal/cancel', :to => "paypal#cancel", :as => :cancel_paypal
