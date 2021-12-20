af66fd069692c4b9fa1e27ab26e775937000780f
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2021-12-20 13:22:21 +0000

    Tidy up initializers

--- config/initializers/rswag_ui.rb
+++ config/initializers/rswag_ui.rb
@@ -1,4 +1,4 @@
-Rswag::Ui.configure do |c|
+Rswag::Ui.configure do |config|
 
   # List the Swagger endpoints that you want to be documented through the swagger-ui
   # The first parameter is the path (absolute or relative to the UI host) to the corresponding
@@ -6,9 +6,9 @@ Rswag::Ui.configure do |c|
   # NOTE: If you're using rspec-api to expose Swagger files (under swagger_root) as JSON or YAML endpoints,
   # then the list below should correspond to the relative paths for those endpoints
 
-  c.swagger_endpoint '/api-docs/v1/swagger.yaml', 'API V1 Docs'
+  config.swagger_endpoint '/api-docs/v1/swagger.yaml', 'API V1 Docs'
 
   # Add Basic Auth in case your API is private
-  # c.basic_auth_enabled = true
-  # c.basic_auth_credentials 'username', 'password'
+  # config.basic_auth_enabled = true
+  # config.basic_auth_credentials 'username', 'password'
 end
