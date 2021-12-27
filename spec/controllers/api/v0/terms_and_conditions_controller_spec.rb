eaterprises/openfoodweb

Refactor use of image files in specs

spec/controllers/api/v0/terms_and_conditions_controller_spec.rb
@@ -5,14 +5,15 @@ require "spec_helper"
 module Api
   describe V0::TermsAndConditionsController, type: :controller do
     include AuthenticationHelper
+    include FileHelper
 
     let(:enterprise_owner) { create(:user) }
     let(:enterprise) { create(:enterprise, owner: enterprise_owner ) }
     let(:enterprise_manager) { create(:user, enterprises: [enterprise]) }
 
     describe "removing terms and conditions file" do
-      fake_terms_file_path = File.open(Rails.root.join("app", "webpacker", "images", "logo-black.png"))
-      let(:terms_and_conditions_file) { Rack::Test::UploadedFile.new(fake_terms_file_path, "application/pdf") }
+      let(:fake_terms_file) { black_logo_file }
+      let(:terms_and_conditions_file) { Rack::Test::UploadedFile.new(fake_terms_file, "application/pdf") }
       let(:enterprise) { create(:enterprise, owner: enterprise_owner) }
 
       before do

openfoodfoundation/openfoodnetwork

spec/controllers/api/v0/terms_and_conditions_controller_spec.rb
@@ -5,13 +5,14 @@ require "spec_helper"
 module Api
   describe V0::TermsAndConditionsController, type: :controller do
     include AuthenticationHelper
+    include FileHelper
 
     let(:enterprise_owner) { create(:user) }
     let(:enterprise) { create(:enterprise, owner: enterprise_owner ) }
     let(:enterprise_manager) { create(:user, enterprises: [enterprise]) }
 
     describe "removing terms and conditions file" do
-      fake_terms_file_path = File.open(Rails.root.join("app", "webpacker", "images", "logo-black.png"))
+      let(:fake_terms_file_path) { black_logo_file }
       let(:terms_and_conditions_file) {
         Rack::Test::UploadedFile.new(fake_terms_file_path, "application/pdf")
       }