22a48de033b0ec0340d393d41b27893eb56709be
Cillian O'Ruanaidh <contact@cilliano.com>
2022-01-14 12:33:30 +0000

    Remove unused code for currency select menu which no longer exists
    This removes inline JavaScript for #8699. This code was for the old currency select menu which was removed in 4839b7bd631d462001c8cf4b612f244dfadacaac

--- app/helpers/spree/admin/general_settings_helper.rb
+++ app/helpers/spree/admin/general_settings_helper.rb
@@ -3,14 +3,6 @@
 module Spree
   module Admin
     module GeneralSettingsHelper
-      def currency_options
-        currencies = ::Money::Currency.table.map do |_code, details|
-          iso = details[:iso_code]
-          [iso, "#{details[:name]} (#{iso})"]
-        end
-        options_from_collection_for_select(currencies, :first, :last, Spree::Config[:currency])
-      end
-
       def all_units
         ["g", "oz", "lb", "kg", "T", "mL", "L", "kL"]
       end
