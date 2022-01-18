8ec7d3b651f2f1ea46dfe7437a8e3b7cab1ec0c1
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2022-01-18 10:59:53 +0000

    Fix use of deprecated keyword args syntax in reports

--- engines/order_management/app/services/order_management/reports/enterprise_fee_summary/data_representations/with_i18n.rb
+++ engines/order_management/app/services/order_management/reports/enterprise_fee_summary/data_representations/with_i18n.rb
@@ -8,7 +8,7 @@ module OrderManagement
           private
 
           def i18n_translate(translation_key, options = {})
-            I18n.t("order_management.reports.enterprise_fee_summary.#{translation_key}", options)
+            I18n.t("order_management.reports.enterprise_fee_summary.#{translation_key}", **options)
           end
         end
       end
