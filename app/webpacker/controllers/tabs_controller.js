5eb88e020cbd0b2cc680cf6a4d01395254c7a28e
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2022-01-31 16:35:41 +0000

    Extract/clarify methods in tabs_controller

--- app/webpacker/controllers/tabs_controller.js
+++ app/webpacker/controllers/tabs_controller.js
@@ -3,19 +3,28 @@ import { Controller } from "stimulus";
 export default class extends Controller {
   static targets = ["tab", "content"]
 
-  connect() {
-    this.setCurrentTab()
-  }
-
   select(event) {
     this.setCurrentTab(this.tabTargets.indexOf(event.currentTarget))
   }
 
+  // private
+
+  connect() {
+    this.setCurrentTab()
+  }
+
   setCurrentTab(tabIndex = 0) {
+    this.showSelectedContent(tabIndex)
+    this.setButtonActiveClass(tabIndex)
+  }
+
+  showSelectedContent(tabIndex) {
     this.contentTargets.forEach((element, index) => {
       element.hidden = index !== tabIndex
     })
+  }
 
+  setButtonActiveClass(tabIndex) {
     this.tabTargets.forEach((element, index) => {
       if(index === tabIndex) {
         element.classList.add("active")
