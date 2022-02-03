2e6d8c12161d918940d02c8800b0ac41757b2a47
Maikel Linke <maikel@email.org.au>
2022-02-03 11:26:34 +1100

    Provide aws-sdk with working URI.encode method
    The S3 class is calling URI.encode which is removed in Ruby 3. By
    providing a URI module within the S3 class makes the S3 code call that
    module instead.

--- spec/models/spree/image_spec.rb
+++ spec/models/spree/image_spec.rb
@@ -19,7 +19,7 @@ module Spree
         attachment = image.attachment
         expect(attachment.exists?).to eq true
         expect(attachment.file?).to eq true
-        expect(attachment.url).to match /logo-black\.png\?[0-9]+$/
+        expect(attachment.url).to match %r"^/spree/products/[0-9]+/product/logo-black\.png\?[0-9]+$"
       end
     end
 
@@ -41,24 +41,22 @@ module Spree
           path: "/:id/:style/:basename.:extension",
         }
       }
-      around do |example|
-        original_config = Spree::Image.attachment_definitions
-        test_config = original_config.dup
-        test_config[:attachment].merge!(s3_config)
-        Spree::Image.attachment_definitions = test_config
 
-        example.run
-
-        Spree::Image.attachment_definitions = original_config
+      before do
+        attachment_definition = Spree::Image.attachment_definitions[:attachment]
+        allow(Spree::Image).to receive(:attachment_definitions).and_return(
+          attachment: attachment_definition.merge(s3_config)
+        )
       end
 
       it "saves a new image when none is present" do
-        pending "patch for aws-sdk to encode URIs"
+        upload_pattern = %r"^https://ofn.s3.amazonaws.com/[0-9]+/(original|mini|small|product|large)/logo-black.png$"
+        download_pattern = %r"^https://ofn.s3.amazonaws.com/[0-9]+/product/logo-black.png$"
+        public_url_pattern = %r"^https://ofn.s3.us-east-1.amazonaws.com/[0-9]+/product/logo-black.png\?[0-9]+$"
 
-        stub_request(:put, %r"https://ofn.s3.amazonaws.com/[0-9]+/(original|mini|small|product|large)/logo-black.png").
-          to_return(status: 200, body: "", headers: {})
-        stub_request(:head, %r"https://ofn.s3.amazonaws.com/[0-9]+/product/logo-black.png").
-          to_return(status: 200, body: "", headers: {})
+        stub_request(:put, upload_pattern).to_return(status: 200, body: "", headers: {})
+        stub_request(:head, download_pattern).to_return(status: 200, body: "", headers: {})
+
         image = Spree::Image.create!(
           attachment: file,
           viewable: product.master,
@@ -67,7 +65,7 @@ module Spree
         attachment = image.attachment
         expect(attachment.exists?).to eq true
         expect(attachment.file?).to eq true
-        expect(attachment.url).to match /logo-black\.png\?[0-9]+$/
+        expect(attachment.url).to match public_url_pattern
       end
     end
   end
