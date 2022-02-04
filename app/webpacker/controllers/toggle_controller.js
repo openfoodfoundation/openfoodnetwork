d03b52a163302a32b6417947c9b49131dd681b84
Cillian O'Ruanaidh <contact@cilliano.com>
2022-02-04 11:47:38 +0000

    Revert "Replace inline JS for toggling order cycle advanced settings with StimulusJs controller"
    This reverts commit 67ae2e72f31f50ebf94e48d904a72e5b746e88d9.

--- app/webpacker/controllers/toggle_controller.js
+++ app/webpacker/controllers/toggle_controller.js
@@ -4,22 +4,9 @@ export default class extends Controller {
   static targets = ["content"];
 
   toggle(event) {
-    event.stopImmediatePropagation()
     const input = event.currentTarget;
-    const chevron = input.querySelector(".icon-chevron-down, .icon-chevron-up")
-    const toggleViaSingleElement = !input.dataset.toggleShow;
-
-    if(chevron) {
-      chevron.classList.toggle("icon-chevron-down");
-      chevron.classList.toggle("icon-chevron-up");
-    }
-
     this.contentTargets.forEach((t) => {
-      if(toggleViaSingleElement) {
-        t.style.display = t.style.display === "none" ? "block" : "none";
-      } else {
-        t.style.display = input.dataset.toggleShow === "true" ? "block" : "none";
-      }
+      t.style.display = input.dataset.toggleShow === "true" ? "block" : "none";
     });
   }
 }
