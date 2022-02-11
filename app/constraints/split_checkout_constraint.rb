f60068c0cecd1cd4c34ef5b814373e28e1bbb631
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2022-02-11 11:16:41 +0100

    Use the proxy OpenFoodNetwork::FeatureToggle
    that actually call Flipper.enabled?

--- app/constraints/split_checkout_constraint.rb
+++ app/constraints/split_checkout_constraint.rb
@@ -2,7 +2,7 @@
 
 class SplitCheckoutConstraint
   def matches?(request)
-    Flipper.enabled? :split_checkout, current_user(request)
+    OpenFoodNetwork::FeatureToggle.enabled? :split_checkout, current_user(request)
   end
 
   def current_user(request)
