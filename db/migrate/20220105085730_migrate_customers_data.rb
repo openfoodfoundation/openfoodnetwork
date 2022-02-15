4c50ca69ea9c2c33ede9b15795a843d052f8bf6e
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2022-02-15 15:42:29 +0000

    Improve data migration logic and add test coverage

--- db/migrate/20220105085730_migrate_customers_data.rb
+++ db/migrate/20220105085730_migrate_customers_data.rb
@@ -11,14 +11,47 @@ class MigrateCustomersData < ActiveRecord::Migration[6.1]
   end
 
   def migrate_customer_name_data!
-    Customer.where(first_name: "", last_name: "").where.not(name: [nil, ""]).find_each do |customer|
-      name_words = customer.name.split(' ')
-      next if name_words.empty?
+    customers_with_bill_addresses.find_each do |customer|
+      if bill_address_name_matches?(customer)
+        apply_name_from_bill_address!(customer)
+        next
+      end
 
-      customer.update(
-        first_name: name_words.first,
-        last_name: name_words[1..].join(' ')
-      )
+      split_customer_name!(customer)
     end
+
+    customers_without_bill_addresses.find_each do |customer|
+      split_customer_name!(customer)
+    end
+  end
+
+  def customers_with_bill_addresses
+    Customer.joins(:bill_address).where(first_name: "", last_name: "").where.not(name: [nil, ""])
+  end
+
+  def customers_without_bill_addresses
+    Customer.where(bill_address_id: nil, first_name: "", last_name: "").where.not(name: [nil, ""])
+  end
+
+  def bill_address_name_matches?(customer)
+    customer.name.delete(" ") == (customer.bill_address.firstname + customer.bill_address.lastname).delete(" ")
+  end
+
+  def split_customer_name!(customer)
+    return if (name_parts = customer.name.split(' ')).empty?
+
+    customer.update_columns(
+      first_name: name_parts.first,
+      last_name: name_parts[1..].join(' '),
+      updated_at: Time.zone.now
+    )
+  end
+
+  def apply_name_from_bill_address!(customer)
+    customer.update_columns(
+      first_name: customer.bill_address.firstname,
+      last_name: customer.bill_address.lastname,
+      updated_at: Time.zone.now
+    )
   end
 end
