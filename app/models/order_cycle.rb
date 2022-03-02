771f7b0393b74d85aae6a590d1967d6f7bf16894
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2022-03-02 11:00:04 +0100

    Delete coordinators_fees linked to an order_cycle when it is destroyed

--- app/models/order_cycle.rb
+++ app/models/order_cycle.rb
@@ -10,7 +10,8 @@ class OrderCycle < ApplicationRecord
   belongs_to :coordinator, class_name: 'Enterprise'
 
   has_many :coordinator_fee_refs, class_name: 'CoordinatorFee'
-  has_many :coordinator_fees, through: :coordinator_fee_refs, source: :enterprise_fee
+  has_many :coordinator_fees, through: :coordinator_fee_refs, source: :enterprise_fee,
+                              dependent: :destroy
 
   has_many :exchanges, dependent: :destroy
 
