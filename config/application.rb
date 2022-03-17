95cb6e93e72d0cd8c7531ec87e78e92e9f8ece39
Maikel Linke <maikel@email.org.au>
2022-03-17 11:20:28 +1100

    Configure Active Storage
    We are re-using the same config used for Paperclip except for disk
    storage. Active Storage uses directory sharding on the local disk which
    means that we can't create blob entries that point to the existing
    Paperclip files. We will just copy them to the standard `storage/`
    directory.

--- config/application.rb
+++ config/application.rb
@@ -238,5 +238,7 @@ module Openfoodnetwork
     Rails.application.routes.default_url_options[:host] = ENV["SITE_URL"]
 
     Rails.autoloaders.main.ignore(Rails.root.join('app/webpacker'))
+
+    config.active_storage.service = ENV["S3_BUCKET"].present? ? :amazon : :local
   end
 end
