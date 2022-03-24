22d13621f4c40acc1afb3e1d6a5c63b1c87629fc
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2022-03-24 09:44:49 +0100

    Only send close reflex if element is actually visible

--- app/webpacker/controllers/selector_controller.js
+++ app/webpacker/controllers/selector_controller.js
@@ -14,8 +14,15 @@ export default class extends ApplicationController {
     window.removeEventListener("click", this.closeOnClickOutside);
   }
 
+  afterReflex() {
+    this.computeItemsHeight();
+  }
+
   closeOnClickOutside = (event) => {
-    if (!this.element.contains(event.target)) {
+    if (
+      !this.element.contains(event.target) &&
+      this.isVisible(this.element.querySelector(".selector-wrapper"))
+    ) {
       this.stimulate(`${this.reflex}#close`, this.element);
     }
   };
@@ -24,5 +31,10 @@ export default class extends ApplicationController {
     const items = this.element.querySelector(".selector-items");
     const rect = items.getBoundingClientRect();
     items.style.maxHeight = `calc(100vh - ${rect.height}px)`;
+  };
+
+  isVisible = (element) => {
+    const style = window.getComputedStyle(element);
+    return style.display !== "none" && style.visibility !== "hidden";
   };
 }
