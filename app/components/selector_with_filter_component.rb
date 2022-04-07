3cf01623da4f1f9e6e4bc4271725e055b29e2a0b
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2022-04-07 15:39:47 +0200

    Handle filtering on js controller for SelectorWithFilter

--- app/components/selector_with_filter_component.rb
+++ app/components/selector_with_filter_component.rb
@@ -4,23 +4,8 @@ class SelectorWithFilterComponent < SelectorComponent
   def initialize(title:, selected:, items:, data: {},
                  selected_items_i18n_key: 'components.selector_with_filter.selected_items')
     super(title: title, selected: selected, items: items, data: data)
-    @query = ""
     @selected_items = items.select { |item| @selected.include?(item[:value]) }
     @selected_items_i18n_key = selected_items_i18n_key
-
-    filter_items
-  end
-
-  def search
-    @query = element.value
-    filter_items
-  end
-
-  def filter_items
-    @filtered_items = if @query.empty?
-                        @items
-                      else
-                        @items.select { |item| item[:label].downcase.include?(@query.downcase) }
-                      end
+    @items = items
   end
 end
