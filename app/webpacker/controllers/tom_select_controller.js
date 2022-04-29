d7df714280a8437d727b07164865c67db2383c18
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2022-04-29 10:36:53 +0200

    Formatting file

--- app/webpacker/controllers/tom_select_controller.js
+++ app/webpacker/controllers/tom_select_controller.js
@@ -1,22 +1,23 @@
-import { Controller } from "stimulus"
-import TomSelect from "tom-select"
+import { Controller } from "stimulus";
+import TomSelect from "tom-select";
 
 export default class extends Controller {
-  static values = { options: Object }
+  static values = { options: Object };
   static defaults = {
     maxItems: 1,
     maxOptions: null,
     plugins: ["dropdown_input"],
-    allowEmptyOption: true
-  }
+    allowEmptyOption: true,
+  };
 
   connect() {
-    this.control = new TomSelect(
-      this.element, { ...this.constructor.defaults, ...this.optionsValue }
-    )
+    this.control = new TomSelect(this.element, {
+      ...this.constructor.defaults,
+      ...this.optionsValue,
+    });
   }
 
   disconnect() {
-    if (this.control) this.control.destroy()
+    if (this.control) this.control.destroy();
   }
 }
