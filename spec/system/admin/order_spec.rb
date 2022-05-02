9c231a831d9a3777f7b409866d5dfe2c6a230e21
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2022-05-02 13:37:12 +0200

    Replace Angular directive and use stimulus controller
     + Update specs as well

--- spec/system/admin/order_spec.rb
+++ spec/system/admin/order_spec.rb
@@ -392,7 +392,7 @@ describe '
     expect(page).to have_selector '#select-customer'
 
     # And I select that customer's email address and save the order
-    select2_select customer.email, from: 'customer_search_override', search: true
+    tomselect_search_and_select customer.email, from: 'customer_search_override'
     click_button 'Update'
     expect(page).to have_selector "h1.js-admin-page-title", text: "Customer Details"
 
@@ -400,6 +400,10 @@ describe '
     order = Spree::Order.last
     expect(order.ship_address.lastname).to eq customer.ship_address.lastname
     expect(order.bill_address.lastname).to eq customer.bill_address.lastname
+    expect(order.ship_address.zipcode).to eq customer.ship_address.zipcode
+    expect(order.bill_address.zipcode).to eq customer.bill_address.zipcode
+    expect(order.ship_address.city).to eq customer.ship_address.city
+    expect(order.bill_address.city).to eq customer.bill_address.city
   end
 
   context "as an enterprise manager" do
