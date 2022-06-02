076efd653d04cc6701b6c0da326eb0242a6de5d4
Maikel Linke <maikel@email.org.au>
2022-06-02 12:13:34 +1000

    Correct checksum of big files stored on AWS S3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_10_162955) do
+ActiveRecord::Schema.define(version: 2022_06_02_013938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
