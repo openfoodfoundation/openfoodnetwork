77e384ab7160d361c305674af5fa680510161e31
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2022-06-28 16:58:16 +0200

    Replace button by a drop-down
    + update specs as well

--- spec/system/admin/orders_spec.rb
+++ spec/system/admin/orders_spec.rb
@@ -113,12 +113,14 @@ describe '
         page.find("#listing_orders thead th:first-child input[type=checkbox]").click
         expect(page.find("#listing_orders tbody tr td:first-child input[type=checkbox]")).to be_checked
         # enables print invoices button
-        expect(page).to have_button('Print Invoices', disabled: false)
+        page.find("span.icon-reorder", text: "ACTIONS").click
+        expect(page).to have_content "Print Invoices"
         # unselect all orders
         page.find("#listing_orders thead th:first-child input[type=checkbox]").click
         expect(page.find("#listing_orders tbody tr td:first-child input[type=checkbox]")).to_not be_checked
         # disables print invoices button
-        expect(page).to have_button('Print Invoices', disabled: true)
+        page.find("span.icon-reorder", text: "ACTIONS").click
+        expect(page).to_not have_content "Print Invoices"
       end
     end
 
