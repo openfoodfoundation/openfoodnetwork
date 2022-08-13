d69f312a0ff862bd5c67c87a49b7737ccab8e742
binarygit <inscription4@outlook.com>
2022-08-13 14:27:48 +0545

    Convert note save to stimulus

--- app/webpacker/controllers/order_info_controller.js
+++ app/webpacker/controllers/order_info_controller.js
@@ -1,6 +1,7 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
+  static values = { orderNumber: String };
   static targets = ["editor", "editField", "resourceDisplayer"];
 
   showEditor() {
@@ -13,5 +14,32 @@ export default class extends Controller {
   hideEditor() {
     this.editorTarget.classList.add("hidden");
     this.resourceDisplayerTarget.classList.remove("hidden");
+  }
+
+  save() {
+    let data = this.editFieldTarget.value;
+    this.makeApiCall(this.params(data));
+  }
+
+  makeApiCall(params) {
+    fetch(this.url, {
+      method: "PUT",
+      body: JSON.stringify(params),
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+    }).then(function (response) {
+      if (response.ok) {
+        window.location.reload();
+      } else {
+        console.error(response);
+      }
+    });
+  }
+
+  params(data) {
+    return { note: data };
+  }
+
+  get url() {
+    return Spree.url(Spree.routes.orders_api + "/" + this.orderNumberValue);
   }
 }
