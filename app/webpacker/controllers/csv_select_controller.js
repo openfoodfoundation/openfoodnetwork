2a9db0eea4fa61c2f79c7ab9c1b8e6eec7757d3c
Lucas da Costa <aintluks@gmail.com>
2022-08-16 16:32:19 -0300

    Disable header row bulk_coop report

---
+++ app/webpacker/controllers/csv_select_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from "stimulus";
+
+export default class extends Controller {
+  static targets = ["reportType", "checkbox", "label"]
+
+  handleSelectChange() {
+    this.reportTypeTarget.value == "csv" ? this.disableField() : this.enableField()
+  }
+
+  disableField() {
+    this.checkboxTarget.checked = false;
+    this.checkboxTarget.disabled = true;
+    this.labelTarget.classList.add("disabled");
+  }
+
+  enableField() {
+    this.checkboxTarget.checked = true;
+    this.checkboxTarget.disabled = false;
+    this.labelTarget.classList.remove("disabled");
+  }
+}
