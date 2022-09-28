312f2544dddb44c8d01f0a84e133a0db2c8cfba6
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2022-09-28 15:29:12 +0200

    Be sure that option key is a Symbol (it could be a String)

--- app/components/multiple_checked_select_component.rb
+++ app/components/multiple_checked_select_component.rb
@@ -4,7 +4,7 @@ class MultipleCheckedSelectComponent < ViewComponent::Base
   def initialize(name:, options:, selected:, filter_placeholder: "Filter options")
     @name = name
     @filter_placeholder = filter_placeholder
-    @options = options
+    @options = options.map { |option| [option[0], option[1].to_sym] }
     @selected = selected.map(&:to_sym)
   end
 end
