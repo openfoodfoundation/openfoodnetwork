7d15baef9d503aac47086e7f5f80090d130939ab
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2022-10-04 12:32:09 +0200

    Do not use a variable `filter_placeholder` but translate it in the view

--- app/components/multiple_checked_select_component.rb
+++ app/components/multiple_checked_select_component.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 class MultipleCheckedSelectComponent < ViewComponent::Base
-  def initialize(name:, options:, selected:, filter_placeholder: "Filter options")
+  def initialize(name:, options:, selected:)
     @name = name
-    @filter_placeholder = filter_placeholder
     @options = options.map { |option| [option[0], option[1].to_sym] }
     @selected = selected.map(&:to_sym)
   end
