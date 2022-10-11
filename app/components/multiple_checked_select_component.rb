8a6874326a58f1af862fdcd0ff760a263bab78ac
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2022-10-11 10:04:37 +0200

    selected could be nil if no columns are selected

--- app/components/multiple_checked_select_component.rb
+++ app/components/multiple_checked_select_component.rb
@@ -4,6 +4,6 @@ class MultipleCheckedSelectComponent < ViewComponent::Base
   def initialize(name:, options:, selected:)
     @name = name
     @options = options.map { |option| [option[0], option[1].to_sym] }
-    @selected = selected.map(&:to_sym)
+    @selected = selected.nil? ? [] : selected.map(&:to_sym)
   end
 end
