c3805b8187180e31715d1ffd5bd739085c986aec
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2022-10-17 12:00:05 +0200

    Create ModalComponent and make HelpModalComponent inherit from it

--- app/components/help_modal_component.rb
+++ app/components/help_modal_component.rb
@@ -1,26 +1,7 @@
 # frozen_string_literal: true
 
-class HelpModalComponent < ViewComponent::Base
+class HelpModalComponent < ModalComponent
   def initialize(id:, close_button: true)
-    @id = id
-    @close_button = close_button
-  end
-
-  private
-
-  def close_button_class
-    if namespace == "admin"
-      "red"
-    else
-      "primary"
-    end
-  end
-
-  def close_button?
-    !!@close_button
-  end
-
-  def namespace
-    helpers.controller_path.split("/").first
+    super(id: id, close_button: close_button)
   end
 end
