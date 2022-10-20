edc667a3362d5c322e59a072dd3d543c5900cb47
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2022-10-20 14:46:07 +0200

    By default, some columns are hidden.

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -47,7 +47,7 @@ module Admin
       # Initialize data
       params[:display_summary_row] = true if request.get?
       @params_fields_to_show = if request.get?
-                                 @report.columns.keys
+                                 @report.columns.keys - @report.fields_to_hide
                                else
                                  params[:fields_to_show]
                                end
