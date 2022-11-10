029c0afaa9e92a63e1f2780bb3addfe380636ce6
Maikel Linke <maikel@email.org.au>
2022-11-10 15:58:49 +1100

    Verify token signature for authenticated login
    We were just logging in any user without password or other verification
    before. Now we verify the Keycloak signature and know that the person is
    indeed logged in.

--- engines/dfc_provider/spec/support/authorization_helper.rb
+++ engines/dfc_provider/spec/support/authorization_helper.rb
@@ -2,7 +2,15 @@
 
 module AuthorizationHelper
   def authorise(email)
-    token = JWT.encode({ email: email }, nil)
+    token = allow_token_for(email: email)
     request.headers["Authorization"] = "JWT #{token}"
+  end
+
+  def allow_token_for(payload)
+    private_key = OpenSSL::PKey::RSA.generate 2048
+    allow(DfcProvider::AuthorizationControl).to receive(:public_key).
+      and_return(private_key.public_key)
+
+    JWT.encode(payload, private_key, "RS256")
   end
 end
