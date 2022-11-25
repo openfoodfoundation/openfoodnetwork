61e7b594371757a666773eff9d8057e1b5c53b30
David Cook <david@redcliffs.net>
2022-11-25 16:03:56 +1100

    Refactor spec
    Ready to expand in the next commit.

--- engines/dfc_provider/spec/services/authorization_control_spec.rb
+++ engines/dfc_provider/spec/services/authorization_control_spec.rb
@@ -10,23 +10,21 @@ describe DfcProvider::AuthorizationControl do
   describe "with OIDC token" do
     it "finds a user" do
       token = allow_token_for(email: user.email)
-      auth = described_class.new(
-        double(:request,
-               headers: { "Authorization" => "Bearer #{token}" })
-      )
 
-      expect(auth.user).to eq user
+      expect(auth(token).user).to eq user
     end
 
     it "ignores expired signatures" do
       token = allow_token_for(exp: Time.now.to_i, email: user.email)
 
-      auth = described_class.new(
-        double(:request,
-               headers: { "Authorization" => "Bearer #{token}" })
-      )
-
-      expect(auth.user).to eq nil
+      expect(auth(token).user).to eq nil
     end
+  end
+
+  def auth(token)
+    described_class.new(
+      double(:request,
+             headers: { "Authorization" => "Bearer #{token}" })
+    )
   end
 end
