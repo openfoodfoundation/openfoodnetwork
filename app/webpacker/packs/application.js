500c4e8a2f5fd268619e60fb47581cb6185fb2f1
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2022-12-07 15:38:20 +0100

    Already handled by `app/webpacker/controllers/index.js`
    Controller were instanced twice.

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -1,13 +1,4 @@
 /* eslint no-console:0 */
-
-// StimulusJS
-import { Application } from "stimulus";
-import { definitionsFromContext } from "stimulus/webpack-helpers";
-
-const application = Application.start();
-const context = require.context("controllers", true, /.js$/);
-application.load(definitionsFromContext(context));
-
 import CableReady from "cable_ready";
 import mrujs from "mrujs";
 import { CableCar } from "mrujs/plugins";
