8227a61fc8763601792452906df203d322d13c85
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2022-12-12 11:36:42 +0100

    `MultipleCheckedSelectComponent` can receive a `form_attributes` params
    Prepare to post the whole form

--- app/components/multiple_checked_select_component.rb
+++ app/components/multiple_checked_select_component.rb
@@ -3,10 +3,11 @@
 class MultipleCheckedSelectComponent < ViewComponent::Base
   renders_one :bottom_content
 
-  def initialize(name:, options:, selected:)
+  def initialize(name:, options:, selected:, form_attributes: {})
     @name = name
     @options = options.map { |option| [option[0], identifier(option[1])] }
     @selected = selected.nil? ? [] : selected.map{ |s| identifier(s) }
+    @form_attributes = form_attributes
   end
 
   def identifier(option)
