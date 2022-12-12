6c9e895eb3871cd8bbb1a9553fe9e41d992cd55c
aaronzh <aaronzh@umich.edu>
2022-12-12 12:38:24 -0500

    Fix missing payment_states translation in payment report
    Co-authored-by: Aaron Zhang <aaronzh@umich.edu>
    Co-authored-by: Crystal Sun <crysun@umich.edu>

--- lib/reporting/reports/payments/payments_by_payment_type.rb
+++ lib/reporting/reports/payments/payments_by_payment_type.rb
@@ -15,7 +15,7 @@ module Reporting
 
         def columns
           {
-            payment_state: proc { |payments| payments.first.order.payment_state },
+            payment_state: proc { |payments| I18n.t("js.admin.orders.payment_states.#{payments.first.order.payment_state}") },
             distributor: proc { |payments| payments.first.order.distributor.name },
             payment_type: proc { |payments| payments.first.payment_method.name },
             total_price: proc { |payments| payments.sum(&:amount) }
