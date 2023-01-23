a91ece8f3bac363c049ff0c600edd107373af32f
Maikel Linke <maikel@email.org.au>
2023-01-23 16:40:36 +1100

    Replace outdated datadog config syntax

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,10 +1,9 @@
 if ENV['DATADOG_RAILS_APM']
   Datadog.configure do |c|
+    c.tracing.instrument :rack, request_queuing: true
     c.tracing.instrument :rails, service_name: 'rails'
 
     c.tracing.analytics.enabled = true
     c.runtime_metrics.enabled = true
-
-    c[:rack].request_queuing = true
   end
 end
