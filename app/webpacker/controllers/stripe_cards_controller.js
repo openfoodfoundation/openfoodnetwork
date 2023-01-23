251ab2ac9aa962944961a7e96a51b3b7a02efa1a
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2023-01-23 17:45:26 +0100

    Payment method id attribute don't need to start with `paymentmethod`

--- app/webpacker/controllers/stripe_cards_controller.js
+++ app/webpacker/controllers/stripe_cards_controller.js
@@ -8,7 +8,7 @@ export default class extends Controller {
   connect() {
     this.initSelectedCard();
     document.addEventListener("stripecards:initSelectedCard", (e) => {
-      if (e.detail == `paymentmethod${this.element.dataset.paymentid}`) {
+      if (e.detail == this.element.dataset.paymentid) {
         this.initSelectedCard();
       }
     });
