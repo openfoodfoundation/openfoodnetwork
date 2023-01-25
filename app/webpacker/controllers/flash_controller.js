2bba38134ebb7db39ada495d15fa2698289c45bc
Maikel Linke <maikel@email.org.au>
2023-01-25 15:33:17 +1100

    Show new flash messages until discarded by user
    We currently have two mechanisms to display flash messages. The old one
    through AngularJS and the new one with StimulusReflex.
    
    The AngularJS directive showed flashes for 10 seconds. The
    StimulusReflex controller showed them only for 3 seconds. But any time
    based disappearance of error messages is problematic. There's important
    information in there and some error messages can be long. It's also
    possible that a request takes a while, the user leaves the computer and
    comes back later. If we hide the flash automatically then the user may
    have no idea what went wrong. They may even think that everything is
    fine and their order went through.
    
    I removed the time-based removal of flash messages from the new
    StimulusReflex controller to address this problem. But I didn't touch
    the AngularJS directive because it will be removed anyway. There may
    also be many more messages that could be annoying if they didn't
    disappear, for example a simple "login successful".
    
    I personally think that flash messages that are not important to keep,
    don't need to be shown in the first place. The best UX makes the success
    obvious on the page. And success should be assumed.

--- app/webpacker/controllers/flash_controller.js
+++ app/webpacker/controllers/flash_controller.js
@@ -5,15 +5,6 @@ document.addEventListener("turbolinks:before-cache", () =>
 );
 
 export default class extends Controller {
-  connect() {
-    setTimeout(this.fadeout.bind(this), 3000);
-  }
-
-  fadeout() {
-    this.element.classList.add("animate-hide-500");
-    setTimeout(this.close.bind(this), 500);
-  }
-
   close() {
     this.element.remove();
   }
