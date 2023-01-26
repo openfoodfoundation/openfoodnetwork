dd0957fa720f7d05ee259974defaab5dc4b1aa1c
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2023-01-26 18:09:53 +0100

    Remove test no related to consumer spec
    Furthermore, this test is more about testing the text editor (itself) which is an external library

--- spec/system/consumer/shopping/products_spec.rb
+++ spec/system/consumer/shopping/products_spec.rb
@@ -56,24 +56,7 @@ describe "As a consumer I want to view products", js: true do
           expect(html).to include('<p><b>Formatted</b> product description.</p> Link to an <a href="http://google.fr" target="_blank">external site</a>')
         end
 
-        # -- edit product via admin interface
-        login_as_admin_and_visit spree.edit_admin_product_path(product)
-        expect(page.find("div[id^='taTextElement']")['innerHTML']).to include('<a href="http://google.fr" target="_blank">external site</a>')
 
-        fill_in 'product_name', with: "#{product.name}_update"
-        click_button 'Update'
-
-        # -- check back consumer product view
-        visit shop_path
-        expect(page).to have_content("#{product.name}_update")
-        click_link("#{product.name}_update")
-
-        expect(page).to have_selector '.reveal-modal'
-        modal_should_be_open_for product
-
-        within(".reveal-modal") do
-          expect(html).to include('<p><b>Formatted</b> product description.</p> Link to an <a href="http://google.fr" target="_blank">external site</a>')
-        end
       end
 
       it "does not show unsecure HTML" do
