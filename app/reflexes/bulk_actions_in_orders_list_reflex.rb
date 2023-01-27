5ca5c32da491ec1d025c65e64a85b40aa1515f38
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2023-01-27 15:54:14 +0100

    Factorize what could be into two methods: `success` and `orders`

--- app/reflexes/bulk_actions_in_orders_list_reflex.rb
+++ app/reflexes/bulk_actions_in_orders_list_reflex.rb
@@ -2,22 +2,30 @@
 
 class BulkActionsInOrdersListReflex < ApplicationReflex
   def resend_confirmation_email(order_ids)
-    Spree::Order.where(id: order_ids).find_each do |o|
+    orders(order_ids).find_each do |o|
       Spree::OrderMailer.confirm_email_for_customer(o.id, true).deliver_later if can? :resend, o
     end
 
-    flash[:success] = I18n.t("admin.resend_confirmation_emails_feedback", count: order_ids.count)
+    success("admin.resend_confirmation_emails_feedback", order_ids.count)
+  end
+
+  def send_invoice(order_ids)
+    orders(order_ids).find_each do |o|
+      Spree::OrderMailer.invoice_email(o.id).deliver_later unless o.distributor.can_invoice?
+    end
+
+    success("admin.send_invoice_feedback", order_ids.count)
+  end
+
+  private
+
+  def success(i18n_key, count)
+    flash[:success] = I18n.t(i18n_key, count: count)
     cable_ready.dispatch_event(name: "modal:close")
     morph "#flashes", render(partial: "shared/flashes", locals: { flashes: flash })
   end
 
-  def send_invoice(order_ids)
-    Spree::Order.where(id: order_ids).find_each do |o|
-      Spree::OrderMailer.invoice_email(o.id).deliver_later unless o.distributor.can_invoice?
-    end
-
-    flash[:success] = I18n.t("admin.send_invoice_feedback", count: order_ids.count)
-    cable_ready.dispatch_event(name: "modal:close")
-    morph "#flashes", render(partial: "shared/flashes", locals: { flashes: flash })
+  def orders(order_ids)
+    Spree::Order.where(id: order_ids)
   end
 end
