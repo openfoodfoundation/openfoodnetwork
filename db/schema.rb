8c458c2d488a761add7106673e613f477794b0fa
Maikel Linke <maikel@email.org.au>
2023-01-30 14:32:31 +1100

    Remove outdated SSL setting from database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_17_043628) do
+ActiveRecord::Schema.define(version: 2023_01_30_032659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
