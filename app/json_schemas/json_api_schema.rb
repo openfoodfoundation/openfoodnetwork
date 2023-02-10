3989843a21297454418c0f23e4712ed29ac18eac
Mikael Norlén <mickenorlen@gmail.com>
2023-02-10 12:01:56 +0100

    Add support for extra fields in JsonApiSchemas
    As we are now supporting optional extra fields,
    We need to be able to construct schemas where
    extra fields are optionally included.

--- app/json_schemas/json_api_schema.rb
+++ app/json_schemas/json_api_schema.rb
@@ -76,8 +76,10 @@ class JsonApiSchema
 
     private
 
-    def data_properties(require_all: false)
-      required = require_all ? all_attributes : required_attributes
+    def data_properties(require_all: false, extra_fields: nil)
+      extra_fields_result = get_extra_fields(extra_fields)
+      attributes = get_attributes(extra_fields_result)
+      required = get_required(require_all, extra_fields, extra_fields_result)
 
       {
         id: { type: :string, example: "1" },
@@ -97,6 +99,59 @@ class JsonApiSchema
           end
         }
       }
+    end
+
+    # Example
+    # extra_fields: :my_method
+    # => extra_fields_result = my_method
+    # => attributes = attributes.merge(extra_fields_result)
+    #
+    # extra_fields: {name: :my_method, required: true, opts: {method_opt: true}}
+    # => extra_fields_result = my_method({method_opt: true})
+    # => attributes = attributes.merge(extra_fields_result)
+    # => required += extra_fields_result.keys
+    #
+    # extra_fields: [:my_method, :another_method]
+    # => extra_fields_result = my_method.merge(another_method)
+    # => attributes = attribtues.merge(extra_fields_result)
+    #
+    # To test use eg::
+    # => MySchema.collection(..., extra_fields: ...)
+    #     .dig(:properties, :data, :items, :properties, :attributes)
+    def get_extra_fields(extra_fields)
+      case extra_fields
+      when Symbol
+        public_send(extra_fields)
+      when Hash
+        extra_fields[:opts] &&
+          public_send(extra_fields[:name], extra_fields[:opts]) || public_send(extra_fields[:name])
+      when Array
+        obj = {}
+
+        extra_fields.each do |w|
+          obj.merge!(get_extra_fields(w))
+        end
+
+        obj
+      end
+    end
+
+    def get_required(require_all, extra_fields, extra_fields_result)
+      required = require_all ? all_attributes : required_attributes
+
+      if extra_fields.is_a?(Hash) && extra_fields[:required] == true && extra_fields_result.present?
+        required += extra_fields_result.keys
+      end
+
+      required
+    end
+
+    def get_attributes(extra_fields_result)
+      if [extra_fields_result, attributes].all?{ |obj| obj.respond_to?(:merge) }
+        attributes.merge(extra_fields_result)
+      else
+        attributes
+      end
     end
 
     def relationship_schema(name)
