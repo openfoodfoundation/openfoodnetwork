b7a3f7263bb56f409887ddea275ce8fa726000a8
David Cook <david@redcliffs.net>
2023-02-17 10:17:48 +1100

    Add script to install current node version
    The API is conveniently exactly the same as rbenv.

---
+++ script/nodenv-install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Install our selected Node version defined in the .node-version file.
+#
+# If our node-build version is outdated and it can't build the version we want
+# then we try upgrading node-build and installing again.
+
+if nodenv install --skip-existing; then
+  echo "Correct Node version is installed."
+else
+  echo "Upgrading node-build:"
+  git -C "$(nodenv root)"/plugins/node-build pull
+
+  nodenv install
+fi
