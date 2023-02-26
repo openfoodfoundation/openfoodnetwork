c3281e90c9c7c515227e2b725d1e6d05dbb63c00
vviekk <karampuri.vivek73@gmail.com>
2023-02-26 12:22:06 +0530

    checking string

--- spec/system/admin/product_import_spec.rb
+++ spec/system/admin/product_import_spec.rb
@@ -582,7 +582,11 @@ describe "Product Import", js: true do
       attach_file 'file', '/tmp/test.csv'
       click_button 'Upload'
 
-      proceed_to_validation
+      expect(page).to have_selector 'a.button.proceed'
+      within("#content") { click_link 'Import' }
+      expect(page).to have_content 'Import validation overview'
+      expect(page).to have_selector 'form.product-import'
+      
 
       expect(page).to have_selector '.item-count', text: "4"
       expect(page).to have_selector '.inv-create-count', text: '2'
@@ -616,7 +620,10 @@ describe "Product Import", js: true do
       attach_file 'file', '/tmp/test.csv'
       click_button 'Upload'
 
-      proceed_to_validation
+      expect(page).to have_selector 'a.button.proceed'
+      within("#content") { click_link 'Import' }
+      expect(page).to have_content 'Import validation overview'
+      expect(page).to have_selector 'form.product-import'
 
       expect(page).to have_selector '.item-count', text: "4"
       expect(page).to have_selector '.inv-create-count', text: '2'
@@ -657,6 +664,7 @@ describe "Product Import", js: true do
       find('div.header-description', text: 'Items contain errors').click
       expect(page).to have_content "line 2: Beans - Count_on_hand must be blank if on demand"
       expect(page).to have_content "line 3: Sprouts - Count_on_hand must be blank if on demand"
+      expect(page).to have_content "line 4: Cabbage - Count_on_hand must be blank if on demand"
       expect(page).to have_content "Imported file contains invalid entries"
       expect(page).to have_no_selector 'input[type=submit][value="Save"]'
     end
