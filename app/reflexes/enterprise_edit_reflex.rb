5cbec5521c5340c4178156632ec8d14932721838
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2023-02-28 17:59:31 +0100

    Simplify ability
    Co-Authored-By: David Cook <david@redcliffs.net>

--- app/reflexes/enterprise_edit_reflex.rb
+++ app/reflexes/enterprise_edit_reflex.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: false
 
 class EnterpriseEditReflex < ApplicationReflex
-  delegate :current_user, to: :connection
-
   def remove_terms_and_conditions
     @enterprise = Enterprise.find(element.dataset['enterprise-id'])
-    ability = Spree::Ability.new(current_user).can? :remove_terms_and_conditions, @enterprise
-    throw :forbidden unless ability
+    throw :forbidden unless can?(:remove_terms_and_conditions, @enterprise)
+
     @enterprise.terms_and_conditions.purge_later
   end
 end
