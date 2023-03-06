094fc039e9f29f5df893dab3f053a3cce46aac8a
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2023-03-07 10:01:35 +1100

    Update tabs_and_panels to display tab and panel based on the url anchor
    For Vouchers, this means the voucher tab and panel are displayed when
    you come back to entreprise edit screen from the new vourcher page

--- app/webpacker/controllers/tabs_and_panels_controller.js
+++ app/webpacker/controllers/tabs_and_panels_controller.js
@@ -12,12 +12,33 @@ export default class extends Controller {
 
     // only display the default panel
     this.defaultTarget.style.display = "block";
+
+    // Display panel specified in url anchor
+    const anchors = window.location.toString().split("#");
+    const anchor = anchors.length > 1 ? anchors.pop() : "";
+
+    if (anchor != "") {
+      this.updateActivePanel(anchor);
+
+      // tab
+      const tab_id = anchor.split("_panel").shift();
+      this.updateActiveTab(tab_id);
+    }
   }
 
   changeActivePanel(event) {
+    this.updateActivePanel(`${event.currentTarget.id}_panel`);
+  }
+
+  updateActivePanel(panel_id) {
     const newActivePanel = this.panelTargets.find(
-      (panel) => panel.id == `${event.currentTarget.id}_panel`
+      (panel) => panel.id == panel_id
     );
+
+    if (newActivePanel === undefined) {
+      // No panel found
+      return;
+    }
 
     this.currentActivePanel.style.display = "none";
     newActivePanel.style.display = "block";
@@ -26,6 +47,18 @@ export default class extends Controller {
   changeActiveTab(event) {
     this.currentActiveTab.classList.remove(`${this.classNameValue}`);
     event.currentTarget.classList.add(`${this.classNameValue}`);
+  }
+
+  updateActiveTab(tab_id) {
+    const newActiveTab = this.tabTargets.find((tab) => tab.id == tab_id);
+
+    if (newActiveTab === undefined) {
+      // No tab found
+      return;
+    }
+
+    this.currentActiveTab.classList.remove(`${this.classNameValue}`);
+    newActiveTab.classList.add(`${this.classNameValue}`);
   }
 
   get currentActiveTab() {
