04748b6e0e7c5cd20274405e880cbd4e7e4daebf
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2023-03-07 10:22:41 +1100

    Move voucher value to the model
    Use Spree::Money to display amount with currency

--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -4,4 +4,12 @@ class Voucher < ApplicationRecord
   belongs_to :enterprise
 
   validates :code, presence: true, uniqueness: { scope: :enterprise_id }
+
+  def value
+    10
+  end
+
+  def display_value
+    Spree::Money.new(value)
+  end
 end
