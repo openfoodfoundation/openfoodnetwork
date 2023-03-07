f1a5c46685b2e9c8e8f41cf2df90486cbd7194fd
Maikel Linke <maikel@email.org.au>
2023-03-07 13:02:28 +1100

    Remove unnecessary Flipper memoizer config
    Auto-config, yay.

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -8,6 +8,4 @@ if Rails.env.production?
   end
 end
 
-Rails.configuration.middleware.use Flipper::Middleware::Memoizer, preload_all: true
-
 Flipper.register(:admins) { |actor| actor.respond_to?(:admin?) && actor.admin? }
