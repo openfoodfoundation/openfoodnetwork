9b680e1a926e1e20a385e0612d9c11f8c86ce5db
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2023-03-14 14:55:07 +1100

    Add CalculatedAdjustments to Voucher
    Vouchers are only flat rate of 10 for now, so we add a FlatRate
    calculator in a before_validation callback

--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: false
 
 class Voucher < ApplicationRecord
+  include CalculatedAdjustments
+
   belongs_to :enterprise
 
+  has_many :adjustments, as: :originator, class_name: 'Spree::Adjustment'
+
   validates :code, presence: true, uniqueness: { scope: :enterprise_id }
+
+  before_validation :add_calculator
 
   def value
     10
@@ -11,5 +17,12 @@ class Voucher < ApplicationRecord
 
   def display_value
     Spree::Money.new(value)
+  end
+
+  private
+
+  # For now voucher are only flat rate of 10
+  def add_calculator
+    self.calculator = Calculator::FlatRate.new(preferred_amount: -value)
   end
 end
--- spec/models/voucher_spec.rb
+++ spec/models/voucher_spec.rb
@@ -3,16 +3,29 @@
 require 'spec_helper'
 
 describe Voucher do
+  let(:enterprise) { build(:enterprise) }
+
   describe 'associations' do
     it { is_expected.to belong_to(:enterprise) }
+    it { is_expected.to have_many(:adjustments) }
   end
 
   describe 'validations' do
     subject { Voucher.new(code: 'new_code', enterprise: enterprise) }
 
-    let(:enterprise) { build(:enterprise) }
-
     it { is_expected.to validate_presence_of(:code) }
     it { is_expected.to validate_uniqueness_of(:code).scoped_to(:enterprise_id) }
+  end
+
+  describe 'after_save' do
+    subject { Voucher.create(code: 'new_code', enterprise: enterprise) }
+
+    it 'adds a FlateRate calculator' do
+      expect(subject.calculator.instance_of?(Calculator::FlatRate)).to be(true)
+    end
+
+    it 'has a preferred_amount of -10' do
+      expect(subject.calculator.preferred_amount.to_f).to eq(-10)
+    end
   end
 end
