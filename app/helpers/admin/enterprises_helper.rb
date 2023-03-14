3452864503731f9e471322af6976becc2dd78afb
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2023-03-14 18:15:53 +0100

    Add "white label" panel in enterprise settings page

--- app/helpers/admin/enterprises_helper.rb
+++ app/helpers/admin/enterprises_helper.rb
@@ -49,7 +49,8 @@ module Admin
         { name: 'inventory_settings', icon_class: "icon-list-ol", show: is_shop },
         { name: 'tag_rules', icon_class: "icon-random", show: is_shop },
         { name: 'shop_preferences', icon_class: "icon-shopping-cart", show: is_shop },
-        { name: 'users', icon_class: "icon-user", show: true }
+        { name: 'users', icon_class: "icon-user", show: true },
+        { name: 'white_label', icon_class: "icon-leaf", show: true }
       ]
     end
     # rubocop:enable Metrics/MethodLength
