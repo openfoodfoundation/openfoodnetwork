9a1b1498bf343a52282ed6d4d885a9406fd00b91
Maikel Linke <maikel@email.org.au>
2023-03-22 14:35:17 +1100

    Add all feature toggles with descriptions at boot
    So you can easily inspect and activate new features without trying to
    use them first. It brings more visibility and will enable us to easily
    remove retired features as well.

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -1,5 +1,6 @@
 require "flipper"
 require "flipper/adapters/active_record"
+require "open_food_network/feature_toggle"
 
 if Rails.env.production?
   Flipper::UI.configure do |config|
@@ -9,3 +10,17 @@ if Rails.env.production?
 end
 
 Flipper.register(:admins) { |actor| actor.respond_to?(:admin?) && actor.admin? }
+
+Flipper::UI.configure do |config|
+  config.descriptions_source = ->(_keys) do
+    # return has to be hash of {String key => String description}
+    OpenFoodNetwork::FeatureToggle::CURRENT_FEATURES
+  end
+
+  # Defaults to false. Set to true to show feature descriptions on the list
+  # page as well as the view page.
+  # config.show_feature_description_in_list = true
+end
+
+# Add known feature toggles. This may fail if the database isn't setup yet.
+OpenFoodNetwork::FeatureToggle.setup! rescue ActiveRecord::StatementInvalid
