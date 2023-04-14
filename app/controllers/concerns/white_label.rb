9447ff8471c8a069bd0c5a47b30fb88447f48bef
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2023-04-14 09:51:46 +0200

    Do no filter on `white_label` feature to know if hide_ofn_navigation is true
    The `white_label` feature is used to *configure* a specific distributor. Once it has been configured and activated, to disabled it, this should be done via panel, not through `white_label` feature.

--- app/controllers/concerns/white_label.rb
+++ app/controllers/concerns/white_label.rb
@@ -5,8 +5,6 @@ module WhiteLabel
   include EnterprisesHelper
 
   def hide_ofn_navigation(distributor = current_distributor)
-    return false unless OpenFoodNetwork::FeatureToggle.enabled?(:white_label)
-
     # if the distributor has the hide_ofn_navigation preference set to true
     # then we should hide the OFN navigation
     @hide_ofn_navigation = distributor.hide_ofn_navigation
