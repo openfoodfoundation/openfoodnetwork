3a81912c7cf95da817f9e574cc305f5d098aee0d
Maikel Linke <maikel@email.org.au>
2023-04-20 11:38:24 +1000

    Notify only about slow reports
    It would be annoying to get an email for reports which display on the
    screen immediately.

--- app/jobs/report_job.rb
+++ app/jobs/report_job.rb
@@ -2,11 +2,18 @@
 
 # Renders a report and stores it in a given blob.
 class ReportJob < ApplicationJob
+  NOTIFICATION_TIME = 5.seconds
+
   def perform(report_class, user, params, format, blob)
+    start_time = Time.zone.now
+
     report = report_class.new(user, params, render: true)
     result = report.render_as(format)
     blob.store(result)
-    email_result
+
+    execution_time = Time.zone.now - start_time
+
+    email_result if execution_time > NOTIFICATION_TIME
   end
 
   def email_result
