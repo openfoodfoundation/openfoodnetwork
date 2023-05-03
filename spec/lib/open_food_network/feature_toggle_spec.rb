5f1f7179747bd4a3db93626a82706be362c84932
Maikel Linke <maikel@email.org.au>
2023-05-03 14:57:05 +1000

    Spec FeatureToggle.setup!

--- spec/lib/open_food_network/feature_toggle_spec.rb
+++ spec/lib/open_food_network/feature_toggle_spec.rb
@@ -15,4 +15,21 @@ describe OpenFoodNetwork::FeatureToggle do
       expect(feature_toggle.enabled?(:foo)).to be true
     end
   end
+
+  describe ".setup!" do
+    it "created all current features at boot time" do
+      expect(Flipper.features.count)
+        .to eq feature_toggle::CURRENT_FEATURES.count
+    end
+
+    it "adds any missing features" do
+      pending "We don't have features to test with at the moment." if Flipper.features.empty?
+
+      feature = Flipper.features.first
+      feature.remove
+
+      expect { feature_toggle.setup! }
+        .to change { Flipper.features }.by([feature])
+    end
+  end
 end
