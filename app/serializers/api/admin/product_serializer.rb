1995a4484875b8035cce74c746cc9d3ce09d1413
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2023-05-06 13:30:25 +0100

    Improve eager-loading on admin products page adn resolve issue with serializer
    For some reason eager-loaded data isn't passed to nested serializers when using associations. Defining the association as a method and passing in some optional paramaters resolves the issue.
    
    This reduces queries run by the controller action by around 75%, as well as the response times.

--- app/serializers/api/admin/product_serializer.rb
+++ app/serializers/api/admin/product_serializer.rb
@@ -5,12 +5,26 @@ module Api
     class ProductSerializer < ActiveModel::Serializer
       attributes :id, :name, :sku, :variant_unit, :variant_unit_scale, :variant_unit_name,
                  :inherits_properties, :on_hand, :price, :available_on, :permalink_live,
-                 :tax_category_id, :import_date, :image_url, :thumb_url
+                 :tax_category_id, :import_date, :image_url, :thumb_url, :variants, :master
 
       has_one :supplier, key: :producer_id, embed: :id
       has_one :primary_taxon, key: :category_id, embed: :id
-      has_many :variants, key: :variants, serializer: Api::Admin::VariantSerializer
-      has_one :master, serializer: Api::Admin::VariantSerializer
+
+      def variants
+        ActiveModel::ArraySerializer.new(
+          object.variants,
+          each_serializer: Api::Admin::VariantSerializer,
+          image: thumb_url,
+          stock_location: Spree::StockLocation.first
+        )
+      end
+
+      def master
+        Api::Admin::VariantSerializer.new(
+          object.master,
+          image: thumb_url
+        )
+      end
 
       def image_url
         object.images.first&.url(:product) || "/noimage/product.png"
