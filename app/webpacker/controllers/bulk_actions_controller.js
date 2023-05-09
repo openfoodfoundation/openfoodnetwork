bee46720f44774121f305665ec6f7e72f5216ea4
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2023-05-09 23:11:42 +0100

    Combine bulk action controllers and pass reflex as an argument

--- app/webpacker/controllers/bulk_actions_controller.js
+++ app/webpacker/controllers/bulk_actions_controller.js
@@ -1,20 +1,35 @@
 import ApplicationController from "./application_controller";
 
 export default class extends ApplicationController {
+  static targets = ["extraParams"]
+  static values = { reflex: String }
+
   connect() {
     super.connect();
   }
 
-  // abstract
-  confirm(action) {
-    this.stimulate(action, this.getSelectedIds());
+  perform() {
+    let params = { bulk_ids: this.getSelectedIds() };
+
+    if (this.hasExtraParamsTarget) {
+      Object.assign(params, this.extraFormData())
+    }
+
+    this.stimulate(this.reflexValue, params);
   }
 
   // private
+
   getSelectedIds() {
     const checkboxes = document.querySelectorAll(
       "table input[name='bulk_ids[]']:checked"
     );
     return Array.from(checkboxes).map((checkbox) => checkbox.value);
+  }
+
+  extraFormData() {
+    if (this.extraParamsTarget.constructor.name !== "HTMLFormElement") { return {} }
+
+    return Object.fromEntries(new FormData(this.extraParamsTarget).entries())
   }
 }
