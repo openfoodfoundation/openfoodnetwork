dde4ea93346a7a586351a040d8e538cc8e526c80
Maikel Linke <maikel@email.org.au>
2023-05-10 16:01:59 +1000

    Convert CatalogItems controller spec to request spec

--- engines/dfc_provider/spec/spec_helper.rb
+++ engines/dfc_provider/spec/spec_helper.rb
@@ -7,6 +7,7 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].sort.each { |f| require f }
 RSpec.configure do |config|
   config.include AuthorizationHelper, type: :request
   config.include DfcProvider::Engine.routes.url_helpers, type: :request
+  config.include Warden::Test::Helpers, type: :request
 
   config.around(:each) do |example|
     # The DFC Connector fetches the context when loaded.
