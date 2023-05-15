101b9025b4dee3b5436a9afcb849371a97edf312
Maikel Linke <maikel@email.org.au>
2023-05-15 15:14:33 +1000

    Spec user email validation
    The pending spec addresses:
    
     * https://github.com/openfoodfoundation/openfoodnetwork/issues/10808

--- spec/models/spree/user_spec.rb
+++ spec/models/spree/user_spec.rb
@@ -80,6 +80,34 @@ describe Spree::User do
     end
   end
 
+  describe "validations" do
+    subject(:user) { build(:user) }
+
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+
+    it "detects emails without @" do
+      user.email = "examplemail.com"
+      expect(user).to_not be_valid
+      expect(user.errors.messages[:email]).to include "is invalid"
+    end
+
+    pending "detects backslashes at the end" do
+      user.email = "example@gmail.com\\\\"
+      expect(user).to_not be_valid
+    end
+
+    it "is okay with numbers before the @" do
+      user.email = "example.42@posteo.de"
+      expect(user).to be_valid
+    end
+
+    pending "detects typos in the domain" do
+      user.email = "example@ho020tmail.com"
+      expect(user).to_not be_valid
+    end
+  end
+
   context "#create" do
     it "should send a confirmation email" do
       expect do
