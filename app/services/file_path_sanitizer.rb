360a2414028bd5157078b3cf14b6ef39e3aad2b4
David Cook <david@redcliffs.net>
2023-05-18 10:07:52 +1000

    Style fix

--- app/services/file_path_sanitizer.rb
+++ app/services/file_path_sanitizer.rb
@@ -3,7 +3,7 @@
 class FilePathSanitizer
   def sanitize(file_path, on_error: nil)
     pathname = Pathname.new(file_path)
-    return pathname.realpath if  pathname.file?
+    return pathname.realpath if pathname.file?
 
     on_error&.call
     false
