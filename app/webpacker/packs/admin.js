f961548da2b425a2a5269a1f0eb59921632607ef
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2023-05-24 17:54:58 +0200

    Deactivate file upload for trix editor
    Source: https://benborgers.com/posts/trix-disable-files

--- app/webpacker/packs/admin.js
+++ app/webpacker/packs/admin.js
@@ -18,3 +18,7 @@ import debounced from "debounced";
 debounced.initialize({ input: { wait: 300 } });
 
 import Trix from "trix";
+
+document.addEventListener("trix-file-accept", (event) => {
+  event.preventDefault();
+});
