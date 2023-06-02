1cd38c957df846dfbb774c3897c7b80d0078ac79
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2023-06-02 20:31:49 +0100

    Introduce "zero priced orders" in admin order payments UI and helper

--- app/helpers/spree/payment_methods_helper.rb
+++ app/helpers/spree/payment_methods_helper.rb
@@ -4,12 +4,13 @@ module Spree
   module PaymentMethodsHelper
     def payment_method(payment)
       # hack to allow us to retrieve the name of a "deleted" payment method
-      id = payment.payment_method_id
+      return unless (id = payment.payment_method_id)
+
       Spree::PaymentMethod.find_with_destroyed(id)
     end
 
     def payment_method_name(payment)
-      payment_method(payment).name
+      payment_method(payment)&.name
     end
   end
 end
