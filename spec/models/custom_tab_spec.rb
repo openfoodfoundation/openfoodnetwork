5938577bd1e4d1b307bfbbb0dd2c718facd45214
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2023-06-05 15:43:11 +0200

    Update custom_tab_spec.rb

--- spec/models/custom_tab_spec.rb
+++ spec/models/custom_tab_spec.rb
@@ -6,4 +6,8 @@ describe CustomTab do
   describe 'associations' do
     it { is_expected.to belong_to(:enterprise).required }
   end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:title) }
+  end
 end
