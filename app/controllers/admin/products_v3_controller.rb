5212a8e713c809ede93fd1c5fc540c191c265c7c
David Cook <david@redcliffs.net>
2023-06-06 15:45:28 +1000

    Add products data table

--- app/controllers/admin/products_v3_controller.rb
+++ app/controllers/admin/products_v3_controller.rb
@@ -6,18 +6,35 @@ module Admin
 
     def index; end
 
+    # copied from ProductsTableComponent
     def fetch_products
-      # product_query = OpenFoodNetwork::Permissions.new(spree_current_user).editable_products.merge(product_scope)
-      product_query = Spree::Product.limit(10)
-      @products = product_query
+      product_query = OpenFoodNetwork::Permissions.new(spree_current_user)
+        .editable_products.merge(product_scope)
+      @products = product_query.order(:name).limit(50)
     end
 
     def product_scope
-      scope = if spree_current_user.has_spree_role?("admin") || spree_current_user.enterprises.present?
+      scope = if spree_current_user.has_spree_role?("admin") ||
+                 spree_current_user.enterprises.present?
                 Spree::Product
               else
                 Spree::Product.active
               end
+
+      scope.includes(product_query_includes)
+    end
+
+    def product_query_includes
+      # TODO: add other fielsd used in columns? (eg supplier: [:name])
+      [
+        master: [:images],
+        variants: [
+          :default_price,
+          :stock_locations,
+          :stock_items,
+          :variant_overrides
+        ]
+      ]
     end
   end
 end
