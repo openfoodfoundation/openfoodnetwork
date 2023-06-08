7ce3c3f447d87ddd193a555b1c072a4c388c8bc4
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2023-06-08 14:24:20 +0200

    Overide a `text-transform: uppercase` to `capitalize`
    Inherit comes from `app/webpacker/css/admin/components/buttons.scss`

--- spec/system/admin/orders_spec.rb
+++ spec/system/admin/orders_spec.rb
@@ -683,7 +683,7 @@ distributors: [distributor4, distributor5]) }
 
             # mouse-hovers and finds tooltip
             find(".icon-road").hover
-            expect(page).to have_content "SHIP"
+            expect(page).to have_content "Ship"
           end
 
           within "tr#order_#{order.id}" do
@@ -692,7 +692,7 @@ distributors: [distributor4, distributor5]) }
 
             # mouse-hovers and finds tooltip
             find(".icon-capture").hover
-            expect(page).to have_content "CAPTURE"
+            expect(page).to have_content "Capture"
           end
         end
 
@@ -703,7 +703,7 @@ distributors: [distributor4, distributor5]) }
 
             # mouse-hovers and finds tooltip
             find(".icon-edit").hover
-            expect(page).to have_content "EDIT"
+            expect(page).to have_content "Edit"
           end
         end
       end
