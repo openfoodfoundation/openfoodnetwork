9c4f77123578f345311271ac7190f3863159cccf
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2023-06-09 11:44:45 +1000

    VoucherAdjustmentsService.calculate fix tax related specs
    It seems like :order_with_taxes is returning different order.total than
    before ?? Fix test calculation

--- spec/services/voucher_adjustments_service_spec.rb
+++ spec/services/voucher_adjustments_service_spec.rb
@@ -24,7 +24,7 @@ describe VoucherAdjustmentsService do
       end
     end
 
-    context 'with price included in order price' do
+    context 'with tax included in order price' do
       subject { order.voucher_adjustments.first }
 
       let(:order) do
@@ -53,10 +53,10 @@ describe VoucherAdjustmentsService do
 
       it 'updates the adjustment included_tax' do
         # voucher_rate = amount / order.total
-        # -10 / 150 = -0.066666667
+        # -10 / 160 = -0.0625
         # included_tax = voucher_rate * order.included_tax_total
-        # -0.66666666 * 10 = -0.67
-        expect(subject.included_tax.to_f).to eq(-0.67)
+        # -0.625 * 10 = -0.63
+        expect(subject.included_tax.to_f).to eq(-0.63)
       end
 
       it 'moves the adjustment state to closed' do
@@ -64,7 +64,7 @@ describe VoucherAdjustmentsService do
       end
     end
 
-    context 'with price not included in order price' do
+    context 'with tax not included in order price' do
       let(:order) do
         create(
           :order_with_taxes,
@@ -89,22 +89,22 @@ describe VoucherAdjustmentsService do
         VoucherAdjustmentsService.calculate(order)
       end
 
-      it 'includes amount withou tax' do
+      it 'includes amount without tax' do
         adjustment = order.voucher_adjustments.first
         # voucher_rate = amount / order.total
-        # -10 / 161 = -0.062111801
+        # -10 / 171 = -0.058479532
         # amount = voucher_rate * (order.total - order.additional_tax_total)
-        # -0.062111801 * (161 -11) = -9.32
-        expect(adjustment.amount.to_f).to eq(-9.32)
+        # -0.058479532 * (171 -11) = -9.36
+        expect(adjustment.amount.to_f).to eq(-9.36)
       end
 
       it 'creates a tax adjustment' do
         # voucher_rate = amount / order.total
-        # -10 / 161 = -0.062111801
+        # -10 / 171 = -0.058479532
         # amount = voucher_rate * order.additional_tax_total
-        # -0.0585 * 11 = -0.68
+        # -0.058479532 * 11 = -0.64
         tax_adjustment = order.voucher_adjustments.second
-        expect(tax_adjustment.amount.to_f).to eq(-0.68)
+        expect(tax_adjustment.amount.to_f).to eq(-0.64)
         expect(tax_adjustment.label).to match("Tax")
       end
 
