4b003aa08bec3c36306f68fe11a0f8dd17d0d01e
Mohamed ABDELLANI <abdellani@gmail.com>
2023-06-20 10:42:20 +0100

    replace status:status with cancelled:boolean on invoice model

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -3,18 +3,18 @@
 require 'spec_helper'
 
 RSpec.describe Invoice, type: :model do
+  let(:distributor) { create(:distributor_enterprise) }
+  let(:order) { create(:order, :with_line_item, :completed, distributor:) }
   describe 'presenter' do
     it 'should return an instance of Invoice::DataPresenter' do
-      invoice = create(:invoice)
+      invoice = create(:invoice, order:)
       expect(invoice.presenter).to be_a(Invoice::DataPresenter)
     end
   end
 
   describe 'serialize_order' do
-    let!(:distributor) { create(:distributor_enterprise) }
-    let!(:order) { create(:order, :with_line_item, :completed, distributor: distributor) }
     it 'serializes the order' do
-      invoice = create(:invoice, order: order)
+      invoice = create(:invoice, order:)
       expect(invoice.data).to eq(Invoice::OrderSerializer.new(order).serializable_hash)
     end
   end
