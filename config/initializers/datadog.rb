14b2e0f9628c72a5ca18f4c7cae5e4bd8e93debe
Maikel Linke <maikel@email.org.au>
2023-06-21 11:44:21 +1000

    Remove datadog gem, too expensive

--- config/initializers/datadog.rb
+++
@@ -1,9 +0,0 @@
-if ENV['DATADOG_RAILS_APM']
-  Datadog.configure do |c|
-    c.tracing.instrument :rack, request_queuing: true
-    c.tracing.instrument :rails, service_name: 'rails'
-
-    c.tracing.analytics.enabled = true
-    c.runtime_metrics.enabled = true
-  end
-end
