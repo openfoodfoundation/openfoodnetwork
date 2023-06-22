82fc6a2a9d3441826996f071805bb1185e9a3b0c
Maikel Linke <maikel@email.org.au>
2023-06-22 16:27:04 +1000

    Prepare spec to authenticate with other means
    I want to add OFN API key support.

--- engines/dfc_provider/spec/services/authorization_control_spec.rb
+++ engines/dfc_provider/spec/services/authorization_control_spec.rb
@@ -12,7 +12,7 @@ describe AuthorizationControl do
       create(:oidc_user) # another user
       token = allow_token_for(email: user.email)
 
-      expect(auth(token).user).to eq user
+      expect(auth(oidc_token: token).user).to eq user
     end
 
     it "ignores blank email" do
@@ -20,27 +20,27 @@ describe AuthorizationControl do
       create(:user, uid: "")
       token = allow_token_for(email: nil)
 
-      expect(auth(token).user).to eq nil
+      expect(auth(oidc_token: token).user).to eq nil
     end
 
     it "ignores non-existent user" do
       user
       token = allow_token_for(email: generate(:random_email))
 
-      expect(auth(token).user).to eq nil
+      expect(auth(oidc_token: token).user).to eq nil
     end
 
     it "ignores expired signatures" do
       token = allow_token_for(exp: Time.now.to_i, email: user.email)
 
-      expect(auth(token).user).to eq nil
+      expect(auth(oidc_token: token).user).to eq nil
     end
   end
 
-  def auth(token)
+  def auth(oidc_token:)
     described_class.new(
       double(:request,
-             headers: { "Authorization" => "Bearer #{token}" },
+             headers: { "Authorization" => "Bearer #{oidc_token}" },
              env: { 'warden' => nil })
     )
   end
