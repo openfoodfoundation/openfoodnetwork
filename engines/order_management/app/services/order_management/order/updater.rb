366cca7984a086b992dc59a08a50877d2ede1bc7
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2023-06-26 14:43:25 +1000

    Prevent voucher adjustment from bein updated when update is called

--- engines/order_management/app/services/order_management/order/updater.rb
+++ engines/order_management/app/services/order_management/order/updater.rb
@@ -137,7 +137,10 @@ module OrderManagement
       end
 
       def update_all_adjustments
-        order.all_adjustments.reload.each(&:update_adjustment!)
+        # Voucher are modelled as a Spree::Adjustment but  they don't behave like all the other
+        # adjustments, so we don't want voucher adjustment to be updated here.
+        # Calculation are handled by VoucherAdjustmentsService.calculate
+        order.all_adjustments.non_voucher.reload.each(&:update_adjustment!)
       end
 
       def before_save_hook
