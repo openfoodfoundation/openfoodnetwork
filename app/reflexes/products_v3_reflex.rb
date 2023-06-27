5aebbe410285475f271c8b01fa67a52604760255
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2023-06-27 11:44:36 +0200

    use `beforeFetch` and `afterFetch` lifecycle methods
    + Move outside `Admin` module the reflex
    
    Therefore, this reflex should be _equivalent_ to its javascript controller: `ProductsV3` (relation is made through names)
    
    Remove unwanted line
    
    Actually call StimulusJS controller instead of calling the reflex itself
    
    In order to have this "showLoading", "hideLoading" behavior.
    
    It seems to be possible to directly use the Reflex itself (use `data-reflex` instead of `data-action`) but I can't make it work: the `stimulus-controller:after` event is never broadcasted/catched (but `stimulus-controller:before` yes...)
    
    Documentation:
    https://docs.stimulusreflex.com/guide/reflexes.html#understanding-stimulusreflex-controllers
    https://docs.stimulusreflex.com/guide/lifecycle.html#generic-life-cycle-methods
    
    Maybe @dacook if you want to have a look...

---
+++ app/reflexes/products_v3_reflex.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+class ProductsV3Reflex < ApplicationReflex
+  include Pagy::Backend
+
+  def fetch
+    fetch_products(element.dataset.page || 1, element.dataset.per_page || 15)
+
+    cable_ready.replace(
+      selector: "#products-content",
+      html: render(partial: "admin/products_v3/content",
+                   locals: { products: @products, pagy: @pagy })
+    ).broadcast
+
+    morph :nothing
+  end
+
+  private
+
+  # copied from ProductsTableComponent
+  def fetch_products(page, per_page)
+    product_query = OpenFoodNetwork::Permissions.new(current_user)
+      .editable_products.merge(product_scope)
+    @pagy, @products = pagy(product_query.order(:name), items: per_page, page:)
+  end
+
+  def product_scope
+    scope = if current_user.has_spree_role?("admin") || current_user.enterprises.present?
+              Spree::Product
+            else
+              Spree::Product.active
+            end
+
+    scope.includes(product_query_includes)
+  end
+
+  # Optimise by pre-loading required columns
+  def product_query_includes
+    # TODO: add other fields used in columns? (eg supplier: [:name])
+    [
+      # variants: [
+      #   :default_price,
+      #   :stock_locations,
+      #   :stock_items,
+      #   :variant_overrides
+      # ]
+    ]
+  end
+end
