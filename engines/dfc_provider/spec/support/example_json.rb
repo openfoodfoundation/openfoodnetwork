7b9e4f6914dbfe31c1a964468f30625376eb2682
Maikel Linke <maikel@email.org.au>
2023-07-03 12:41:27 +1000

    Move test files to own directory

--- engines/dfc_provider/spec/support/example_json.rb
+++ engines/dfc_provider/spec/support/example_json.rb
@@ -2,7 +2,7 @@
 
 module ExampleJson
   def self.read(name)
-    pathname = DfcProvider::Engine.root.join("spec/support/#{name}.json")
+    pathname = DfcProvider::Engine.root.join("spec/fixtures/files/#{name}.json")
     JSON.parse(pathname.read)
   end
 end
