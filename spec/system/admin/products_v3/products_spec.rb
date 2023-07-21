ff95228f0d45b920755d84817e6ecc27e1fdef27
David Cook <david@redcliffs.net>
2023-07-21 13:34:32 +1000

    Optimise spec: only create objects when needed
    Moving the 'clear filters' and 'no results' tests up into the first context.

--- spec/system/admin/products_v3/products_spec.rb
+++ spec/system/admin/products_v3/products_spec.rb
@@ -9,17 +9,6 @@ describe 'As an admin, I can see the new product page' do
 
   # create lot of products
   let!(:products) { create_list(:simple_product, 70) }
-  # create a product with a name that can be searched
-  let!(:product_by_name) { create(:simple_product, name: "searchable product") }
-  # create a product with a supplier that can be searched
-  let!(:product_by_supplier) {
-    create(:simple_product,
-           supplier: create(:enterprise, name: "Producer 1", is_primary_producer: true))
-  }
-  # create a product with a category that can be searched
-  let!(:product_by_category) {
-    create(:simple_product, taxons: [create(:taxon, name: "Category 1")])
-  }
 
   before do
     # activate feature toggle admin_style_v3 to use new admin interface
@@ -61,7 +50,10 @@ describe 'As an admin, I can see the new product page' do
       visit "/admin/products_v3"
     end
 
-    context "search by search term" do
+    context "product has searchable term" do
+      # create a product with a name that can be searched
+      let!(:product_by_name) { create(:simple_product, name: "searchable product") }
+
       it "can search for a product" do
         search_for "searchable product"
 
@@ -82,34 +74,7 @@ describe 'As an admin, I can see the new product page' do
         expect_page_to_be 1
         expect_products_count_to_be 1
       end
-    end
 
-    context "search by producer" do
-      it "can search for a product" do
-        search_by_producer "Producer 1"
-
-        expect(page).to have_select "producer_id", selected: "Producer 1"
-        expect_page_to_be 1
-        expect_products_count_to_be 1
-      end
-    end
-
-    context "search by category" do
-      it "has a category select" do
-        expect(page).to have_selector "select#category_id"
-      end
-
-      it "can search for a product" do
-        search_by_category "Category 1"
-
-        expect(page).to have_select "category_id", selected: "Category 1"
-        expect_page_to_be 1
-        expect_products_count_to_be 1
-        expect(page).to have_selector "table.products tbody tr td", text: product_by_category.name
-      end
-    end
-
-    context "clear filters" do
       it "can clear filters" do
         search_for "searchable product"
         expect(page).to have_field "search_term", with: "searchable product"
@@ -122,13 +87,41 @@ describe 'As an admin, I can see the new product page' do
         expect_page_to_be 1
         expect_products_count_to_be 15
       end
-    end
 
-    context "no results" do
       it "shows a message when there are no results" do
         search_for "no results"
         expect(page).to have_content "No products found for your search criteria"
         expect(page).to have_link "Clear search"
+      end
+    end
+
+    context "product has producer" do
+      # create a product with a supplier that can be searched
+      let!(:producer) { create(:supplier_enterprise, name: "Producer 1") }
+      let!(:product_by_supplier) { create(:simple_product, supplier: producer) }
+
+      it "can search for a product" do
+        search_by_producer "Producer 1"
+
+        expect(page).to have_select "producer_id", selected: "Producer 1"
+        expect_page_to_be 1
+        expect_products_count_to_be 1
+      end
+    end
+
+    context "product has category" do
+      # create a product with a category that can be searched
+      let!(:product_by_category) {
+        create(:simple_product, primary_taxon: create(:taxon, name: "Category 1"))
+      }
+
+      it "can search for a product" do
+        search_by_category "Category 1"
+
+        expect(page).to have_select "category_id", selected: "Category 1"
+        expect_page_to_be 1
+        expect_products_count_to_be 1
+        expect(page).to have_selector "table.products tbody tr td", text: product_by_category.name
       end
     end
   end
