0f086df12bd3ff15127822128dce7f9699e50536
David Cook <david@redcliffs.net>
2023-07-26 12:06:18 +1000

    Setup StimulusReflex testing [add gem]
    Surprisingly, the StimulusReflex framework [doesn't have many resources for testing](https://docs.stimulusreflex.com/appendices/testing.html), but thankfully someone's made a gem.

---
+++ spec/reflex_helper.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+require "base_spec_helper"
+require "stimulus_reflex_testing/rspec"
---
+++ spec/reflexes/products_reflex_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require "reflex_helper"
+
+describe ProductsReflex, type: :reflex do
+
+end
