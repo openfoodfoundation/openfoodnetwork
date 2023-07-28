252f8094638c13564f145a230fe86f4f93f74113
Maikel Linke <maikel@email.org.au>
2023-07-28 16:19:42 +1000

    Require CustomTab.belongs_to by default

--- app/models/custom_tab.rb
+++ app/models/custom_tab.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: false
 
 class CustomTab < ApplicationRecord
-  belongs_to :enterprise, optional: false
+  self.belongs_to_required_by_default = true
+
+  belongs_to :enterprise
 
   validates :title, presence: true, length: { maximum: 20 }
 end
