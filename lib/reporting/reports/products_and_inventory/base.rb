087ff0f803b5e738f59ac1f29d01dbc228c02a47
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2023-08-08 12:34:08 +0100

    Update taxon querying in reports

--- lib/reporting/reports/products_and_inventory/base.rb
+++ lib/reporting/reports/products_and_inventory/base.rb
@@ -17,7 +17,7 @@ module Reporting
             producer_suburb: proc { |variant| variant.product.supplier.address.city },
             product: proc { |variant| variant.product.name },
             product_properties: proc { |v| v.product.properties.map(&:name).join(", ") },
-            taxons: proc { |variant| variant.product.primary_taxon.name },
+            taxons: proc { |variant| variant.primary_taxon.name },
             variant_value: proc { |variant| variant.full_name },
             price: proc { |variant| variant.price },
             group_buy_unit_quantity: proc { |variant| variant.product.group_buy_unit_size },
