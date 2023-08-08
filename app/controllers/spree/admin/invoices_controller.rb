70b61eb481a02a746d4c88c65e0c3fc9ed594617
Mohamed ABDELLANI <abdellani@gmail.com>
2023-08-08 08:41:33 +0100

    remove bulk invoice service

--- app/controllers/spree/admin/invoices_controller.rb
+++ app/controllers/spree/admin/invoices_controller.rb
@@ -12,7 +12,7 @@ module Spree
 
       def show
         invoice_id = params[:id]
-        invoice_pdf = BulkInvoiceService.new.filepath(invoice_id)
+        invoice_pdf = filepath(invoice_id)
 
         send_file(invoice_pdf, type: 'application/pdf', disposition: :inline)
       end
@@ -22,6 +22,12 @@ module Spree
         authorize! :invoice, @order
         OrderInvoiceGenerator.new(@order).generate_or_update_latest_invoice
         redirect_back(fallback_location: spree.admin_dashboard_path)
+      end
+
+      private
+
+      def filepath(invoice_id)
+        "tmp/invoices/#{invoice_id}.pdf"
       end
     end
   end
