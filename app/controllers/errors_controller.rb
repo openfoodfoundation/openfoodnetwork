6ee6ad2b8646c66ccd1211c1c55152cb4cbbcd43
David Cook <david@redcliffs.net>
2023-08-11 14:14:32 +1000

    [fixup] Remove unnecessary template name
    This was left over after trying different things to get this working.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -9,7 +9,7 @@ class ErrorsController < ApplicationController
 
       event.add_metadata(:request, request.env)
     end
-    render "not_found", status: :not_found, formats: :html
+    render status: :not_found, formats: :html
   end
 
   def internal_server_error
