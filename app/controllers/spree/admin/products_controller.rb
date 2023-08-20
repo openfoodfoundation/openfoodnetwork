384e7e6056139481ca5dbf5542f7f97fa1c934e3
kefayatullahkhadem <137641088+kefayatullahkhadem@users.noreply.github.com>
2023-08-20 10:04:01 +0430

    i change the footer links
    i added four pagse in the f.....

--- app/controllers/spree/admin/products_controller.rb
+++ app/controllers/spree/admin/products_controller.rb
@@ -21,6 +21,7 @@ module Spree
         @show_latest_import = params[:latest_import] || false
       end
 
+      #i want to edit this files
       def show
         session[:return_to] ||= request.referer
         redirect_to( action: :edit )
