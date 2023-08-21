244b9abd21be8c7071e4850042b391ca85215331
Maikel Linke <maikel@email.org.au>
2023-08-21 16:19:45 +1000

    Spec with given data included in examples
    And use in-memory data where possible to speed up specs.

--- engines/dfc_provider/spec/services/enterprise_builder_spec.rb
+++ engines/dfc_provider/spec/services/enterprise_builder_spec.rb
@@ -4,31 +4,41 @@ require DfcProvider::Engine.root.join("spec/spec_helper")
 
 describe EnterpriseBuilder do
   subject(:builder) { described_class }
-  let(:enterprise) { variant.product.supplier }
-  let(:variant) { create(:product, name: "Apple").variants.first }
+  let(:enterprise) {
+    build(
+      :enterprise,
+      id: 10_000, name: "Fabi's Farm",
+      description: "The place where stuff grows", abn: "123 456 789 0",
+    )
+  }
+  let(:variant) {
+    create(:product, supplier: enterprise, name: "Apple").variants.first
+  }
 
   describe ".enterprise" do
     let(:result) { builder.enterprise(enterprise) }
 
     it "assigns a semantic id" do
       expect(result.semanticId).to eq(
-        "http://test.host/api/dfc-v1.7/enterprises/#{enterprise.id}"
+        "http://test.host/api/dfc-v1.7/enterprises/10000"
       )
     end
 
     it "assigns a name" do
-      expect(result.name).to eq(enterprise.name)
+      expect(result.name).to eq "Fabi's Farm"
     end
 
     it "assigns a description" do
-      expect(result.description).to eq(enterprise.description)
+      expect(result.description).to eq "The place where stuff grows"
     end
 
     it "assigns a VAT Number (ABN in australia)" do
-      expect(result.vatNumber).to eq(enterprise.abn)
+      expect(result.vatNumber).to eq "123 456 789 0"
     end
 
     it "assignes products" do
+      expect(variant).to be_persisted
+
       expect(result.suppliedProducts.count).to eq 1
       expect(result.suppliedProducts[0].name).to eq "Apple"
     end
