5d05c3c0f1d79e1e5ec86a07803ac8a93fbf41db
Maikel Linke <maikel@email.org.au>
2023-08-21 15:57:03 +1000

    DRY DFC enterprise spec

--- engines/dfc_provider/spec/services/enterprise_builder_spec.rb
+++ engines/dfc_provider/spec/services/enterprise_builder_spec.rb
@@ -8,35 +8,27 @@ describe EnterpriseBuilder do
   let(:variant) { create(:product, name: "Apple").variants.first }
 
   describe ".enterprise" do
-    it "assigns a semantic id" do
-      result = builder.enterprise(enterprise)
+    let(:result) { builder.enterprise(enterprise) }
 
+    it "assigns a semantic id" do
       expect(result.semanticId).to eq(
         "http://test.host/api/dfc-v1.7/enterprises/#{enterprise.id}"
       )
     end
 
     it "assigns a name" do
-      result = builder.enterprise(enterprise)
-
       expect(result.name).to eq(enterprise.name)
     end
 
     it "assigns a description" do
-      result = builder.enterprise(enterprise)
-
       expect(result.description).to eq(enterprise.description)
     end
 
     it "assigns a VAT Number (ABN in australia)" do
-      result = builder.enterprise(enterprise)
-
       expect(result.vatNumber).to eq(enterprise.abn)
     end
 
     it "assignes products" do
-      result = builder.enterprise(enterprise)
-
       expect(result.suppliedProducts.count).to eq 1
       expect(result.suppliedProducts[0].name).to eq "Apple"
     end
