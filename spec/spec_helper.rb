c117fcd3811dd267312117e16de91611f84b1741
filipefurtad0 <filipefurtado@gmail.com>
2023-08-30 22:38:11 +0100

    Enables database cleaner for spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,4 +12,18 @@ RSpec.configure do |config|
 
   # Fix encoding issue in Rails 5.0; allows passing empty arrays or hashes as params.
   config.before(:each, type: :controller) { @request.env["CONTENT_TYPE"] = 'application/json' }
+
+  # Setting this to true keeps the database clean by rolling back any changes.
+  config.use_transactional_fixtures = true
+
+  # Some tests don't work within a transaction. Then we use DatabaseCleaner.
+  config.before(:each, concurrency: true) do
+    config.use_transactional_fixtures = false
+    DatabaseCleaner.strategy = :deletion, { except: ['spree_countries', 'spree_states'] }
+    DatabaseCleaner.start
+  end
+  config.append_after(:each, concurrency: true) do
+    DatabaseCleaner.clean
+    config.use_transactional_fixtures = true
+  end
 end
