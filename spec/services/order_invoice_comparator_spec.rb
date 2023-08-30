38eb84b1d86fe11c94f283ae944efe1ba00e7d4d
filipefurtad0 <filipefurtado@gmail.com>
2023-08-30 12:52:15 +0100

    Covers order total change

--- spec/services/order_invoice_comparator_spec.rb
+++ spec/services/order_invoice_comparator_spec.rb
@@ -28,6 +28,12 @@ describe OrderInvoiceComparator do
         expect(subject).to be true
       end
 
+      it "returns true if a relevant attribute changes" do
+        Spree::Order.where(id: order.id).update_all(total: order.total + 10)
+        order.reload
+        expect(subject).to be true
+      end
+
       it "returns false if a non-relevant attribute changes" do
         order.update!(note: "THIS IS A NEW NOTE")
         expect(subject).to be false
