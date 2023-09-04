38869cba2f8fdd2b12f616c29e979d5e3915236c
Maikel Linke <maikel@email.org.au>
2023-09-04 17:07:07 +1000

    Require associations on join model
    It's actually required in the database already. This just simplifies the
    code.

--- app/models/order_cycle_schedule.rb
+++ app/models/order_cycle_schedule.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class OrderCycleSchedule < ApplicationRecord
-  self.belongs_to_required_by_default = false
-
   belongs_to :schedule
   belongs_to :order_cycle
 end
