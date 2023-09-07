95d39877525388993cda9e662342dfafb7e23541
Maikel Linke <maikel@email.org.au>
2023-09-07 13:28:12 +1000

    List enterprise groups on DFC API

--- engines/dfc_provider/app/controllers/dfc_provider/enterprise_groups_controller.rb
+++ engines/dfc_provider/app/controllers/dfc_provider/enterprise_groups_controller.rb
@@ -3,6 +3,16 @@
 # OFN EnterpriseGroup is represented as DFC Enterprise.
 module DfcProvider
   class EnterpriseGroupsController < DfcProvider::ApplicationController
+    def index
+      person = PersonBuilder.person(current_user)
+      groups = current_user.owned_groups
+      enterprises = groups.map do |group|
+        EnterpriseBuilder.enterprise_group(group)
+      end
+      person.affiliatedOrganizations = enterprises
+      render json: DfcIo.export(person, *enterprises)
+    end
+
     def show
       group = EnterpriseGroup.find(params[:id])
       enterprise = EnterpriseBuilder.enterprise_group(group)
--- engines/dfc_provider/spec/requests/enterprise_groups_spec.rb
+++ engines/dfc_provider/spec/requests/enterprise_groups_spec.rb
@@ -3,16 +3,42 @@
 require_relative "../swagger_helper"
 
 describe "EnterpriseGroups", type: :request, swagger_doc: "dfc.yaml", rswag_autodoc: true do
-  let(:user) { create(:oidc_user) }
+  let(:user) { create(:oidc_user, id: 12_345) }
   let(:group) {
     create(
       :enterprise_group,
-      id: 60_000, name: "Sustainable Farmers", enterprises: [enterprise],
+      id: 60_000, owner: user, name: "Sustainable Farmers",
+      enterprises: [enterprise],
     )
   }
   let(:enterprise) { create(:enterprise, id: 10_000) }
 
   before { login_as user }
+
+  path "/api/dfc/enterprise_groups" do
+    get "List groups" do
+      produces "application/json"
+
+      response "200", "successful" do
+        let!(:groups) { [group] }
+
+        run_test! do
+          graph = json_response["@graph"]
+
+          expect(graph[0]["@type"]).to eq "dfc-b:Person"
+          expect(graph[0]).to include(
+            "dfc-b:affiliates" => "http://test.host/api/dfc/enterprise_groups/60000",
+          )
+
+          expect(graph[1]["@type"]).to eq "dfc-b:Enterprise"
+          expect(graph[1]).to include(
+            "dfc-b:hasName" => "Sustainable Farmers",
+            "dfc-b:affiliatedBy" => "http://test.host/api/dfc/enterprises/10000",
+          )
+        end
+      end
+    end
+  end
 
   path "/api/dfc/enterprise_groups/{id}" do
     get "Show groups" do
