b41006d870d352a8c6f8539068e4924acf4a47b6
kefayatullahkhadem <137641088+kefayatullahkhadem@users.noreply.github.com>
2023-09-13 03:35:58 +0430

    Update contact_mailer.rb

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,13 @@
 class ContactMailer < ApplicationMailer
-    def contact_email(name, email, message, subject)
-      @name = name
-      @message = message
-  
-      mail(from: email, to: 'fruits@labelleorange.es', subject: subject)
-    end
+  def contact_email(name, email, message, subject)
+    @name = name
+    @message = message
+
+    mail(
+      to: 'fruits@labelleorange.es',
+      from: email,  
+      subject: subject
+    )    
   end
+end
   
