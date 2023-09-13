3991715a13fc45801f47492540d6d22f8455cf24
kefayatullahkhadem <137641088+kefayatullahkhadem@users.noreply.github.com>
2023-09-13 14:57:08 +0430

    Update contact_mailer.rb

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,10 @@
 class ContactMailer < ApplicationMailer
-    def contact_email(name, email, message, subject)
-      @name = name
-      @message = message
-  
-      mail(to: email, subject: subject)
+  def contact_email(name, email, message, subject)
+    @name = name
+    @message = message
+
+    mail(to: 'contact.afg.updates@gmail.com', subject: subject, from: email) do |format|
+      format.text { render plain: message }
     end
   end
-  
+end
