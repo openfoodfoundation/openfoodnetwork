9ca69feb79b8614669fc06d1912f86ce0aa7e40d
kefayatullahkhadem <137641088+kefayatullahkhadem@users.noreply.github.com>
2023-09-14 23:17:34 +0430

    Update contact_mailer.rb

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,9 +2,9 @@ class ContactMailer < ApplicationMailer
   def contact_email(name, email, message, subject)
     @name = name
     @message = message
-    @user_email = email # Include the user's email
+    @user_email = email 
 
-    mail(from: email, to: 'contact.afg.updates@gmail.com', subject: subject) do |format|
+    mail(to: 'contact.afg.updates@gmail.com', subject: subject) do |format|
       format.text { render plain: "User Email: #{@user_email}\n\n#{@message}" }
     end
   end
