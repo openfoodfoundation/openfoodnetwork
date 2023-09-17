2705f27509818a50785c290a8ffcccf3e32cb02e
kefayatullahkhadem <137641088+kefayatullahkhadem@users.noreply.github.com>
2023-09-17 19:04:34 +0430

    new

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,7 @@ class HomeController < BaseController
   layout 'darkswarm'
 
   def index
-    if ContentConfig.home_show_stats
-      load_home_stats
-    end
+    load_home_stats if ContentConfig.home_show_stats
   end
 
   def sell; end
@@ -19,18 +17,29 @@ class HomeController < BaseController
 
   # Cache the value of the query count
   def cached_count(statistic, query)
-    CacheService.home_stats(statistic) do
-      query.count
-    end
+    CacheService.home_stats(statistic) { query.count }
   end
 
   # Load statistics for the home page
   def load_home_stats
-    @num_distributors = cached_count('distributors', Enterprise.is_distributor.activated.visible)
-    @num_producers = cached_count('producers', Enterprise.is_primary_producer.activated.visible)
-    @num_orders = cached_count('orders', Spree::Order.complete)
+    @num_distributors = cached_count(
+      'distributors',
+      Enterprise.is_distributor.activated.visible
+    )
+
+    @num_producers = cached_count(
+      'producers',
+      Enterprise.is_primary_producer.activated.visible
+    )
+
+    @num_orders = cached_count(
+      'orders',
+      Spree::Order.complete
+    )
+
     @num_users = cached_count(
-      'users', Spree::Order.complete.select('DISTINCT spree_orders.user_id')
+      'users',
+      Spree::Order.complete.select('DISTINCT spree_orders.user_id')
     )
   end
 end
