963b0365f44cf5e6ba5b407525dda295725d5e90
David Cook <david@redcliffs.net>
2023-09-27 12:15:00 +1000

    Show error message summary at top of form

--- app/reflexes/products_reflex.rb
+++ app/reflexes/products_reflex.rb
@@ -42,7 +42,7 @@ class ProductsReflex < ApplicationReflex
       # morph_admin_flashes  # ERROR: selector morph type has already been set
     elsif product_set.invalid_count.positive?
       # @error_msg = with_locale{ I18n.t('.products_have_error', count: product_set.invalid.count) }
-      @error_msg = "#{product_set.invalid_count} products have errors."
+      @error_counts = { saved: product_set.saved_count, invalid: product_set.invalid_count }
     end
 
     render_products_form
@@ -87,10 +87,12 @@ class ProductsReflex < ApplicationReflex
   end
 
   def render_products_form
+    locals = { products: @products }
+    locals[:error_counts] = @error_counts if @error_counts.present?
+
     cable_ready.replace(
       selector: "#products-form",
-      html: render(partial: "admin/products_v3/table",
-                   locals: { products: @products, error_msg: @error_msg })
+      html: render(partial: "admin/products_v3/table", locals:)
     ).broadcast
     morph :nothing
 
