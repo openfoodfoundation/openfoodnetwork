2915048afaff53884d2245c72c5fbec2e2a863da
David Cook <david@redcliffs.net>
2023-10-05 11:02:56 +1100

    Always set locale for reflexes
    I considered using I18nHelper like we do in controllers, but that is for choosing a locale, which we don't need to do for reflexes.

--- app/reflexes/application_reflex.rb
+++ app/reflexes/application_reflex.rb
@@ -5,20 +5,15 @@ class ApplicationReflex < StimulusReflex::Reflex
   #
   # Learn more at: https://docs.stimulusreflex.com/rtfm/reflex-classes
   #
-  # If your ActionCable connection is: `identified_by :current_user`
-  #   delegate :current_user, to: :connection
-  #
-  # If you need to localize your Reflexes, you can set the I18n locale here:
-  #
-  #   before_reflex do
-  #     I18n.locale = :fr
-  #   end
-  #
   # For code examples, considerations and caveats, see:
   # https://docs.stimulusreflex.com/rtfm/patterns#internationalization
   include CanCan::ControllerAdditions
 
   delegate :current_user, to: :connection
+
+  before_reflex do
+    I18n.locale = current_user.locale
+  end
 
   private
 
