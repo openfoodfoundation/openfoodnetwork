30537f7743a038c2bd88ce6aec354884dd3167e7
Mohamed ABDELLANI <abdellani@gmail.com>
2023-10-18 10:30:01 +0100

    add "Weight/VOL." column to the invoice

--- app/serializers/invoice/line_item_serializer.rb
+++ app/serializers/invoice/line_item_serializer.rb
@@ -3,7 +3,7 @@
 class Invoice
   class LineItemSerializer < ActiveModel::Serializer
     attributes :id, :added_tax, :currency, :included_tax, :price_with_adjustments, :quantity,
-               :variant_id, :unit_price_price_and_unit
+               :variant_id, :unit_price_price_and_unit, :unit_presentation
     has_one :variant, serializer: Invoice::VariantSerializer
     has_many :tax_rates, serializer: Invoice::TaxRateSerializer
   end
