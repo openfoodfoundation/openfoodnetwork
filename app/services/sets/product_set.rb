a9b5909a3522dded5de6b2573dcbe3a4478fc516
David Cook <david@redcliffs.net>
2023-10-20 17:17:31 +1100

    [wip] validate all records before saving
    once concept proven, add specs too.

--- app/services/sets/product_set.rb
+++ app/services/sets/product_set.rb
@@ -55,14 +55,21 @@ module Sets
     end
 
     def update_product(product, attributes)
-      return false unless update_product_only_attributes(product, attributes)
+      return false unless assign_product_attributes(product, attributes) &&
+        assign_variant_attributes(product, attributes[:variants_attributes])
+
+      # save products
+      # save variatnts
+      # and then set variant on_hand/on_demand values
+      return false unless product.save && save_variants(product, attributes[:variants_attributes])
 
       ExchangeVariantDeleter.new.delete(product) if product.saved_change_to_supplier_id?
 
-      update_product_variants(product, attributes)
+      # update_product_variants(product, attributes)
     end
 
-    def update_product_only_attributes(product, attributes)
+    ## 1. Assign and validate
+    def assign_product_attributes(product, attributes)
       variant_related_attrs = [:id, :variants_attributes]
       product_related_attrs = attributes.except(*variant_related_attrs)
       return true if product_related_attrs.blank?
@@ -71,7 +78,28 @@ module Sets
 
       validate_presence_of_unit_value_in_product(product)
 
-      product.errors.empty? && product.save
+      product.errors.empty? && product.valid?
+    end
+
+    def assign_variant_attributes(product, variants_attributes)
+      variants_attributes&.each do |attributes|
+        new_or_assign_variant(product, attributes)
+      end
+      product.errors.empty?
+    end
+
+    def new_or_assign_variant(product, variant_attributes)
+      variant = find_model(product.variants, variant_attributes[:id])
+
+      if variant.present?
+        variant.assign_attributes(variant_attributes.except(:id))
+      else
+        # 'You need to save the variant to create a stock item before you can set stock levels.'
+        # we need to keep a refernence to newly created variants, so we know which ones to .save later.
+        variant = product.variants.new(variant_attributes.except(:on_hand, :on_demand))
+      end
+
+      product.errors.merge!(variant.errors) unless variant.valid?
     end
 
     def validate_presence_of_unit_value_in_product(product)
@@ -87,47 +115,26 @@ module Sets
       product.errors.add(:unit_value, "can't be blank")
     end
 
-    def update_product_variants(product, attributes)
-      return true unless attributes[:variants_attributes]
 
-      update_variants_attributes(product, attributes[:variants_attributes])
-    end
+    ## 2. Save records
+    def save_variants(product, variants_attributes)
+      #todo
+        variant = find_model(product.variants, attributes[:id])
 
-    def update_variants_attributes(product, variants_attributes)
-      variants_attributes.each do |attributes|
-        create_or_update_variant(product, attributes)
-      end
-      product.errors.empty?
-    end
+      variants = existing_Variants + new_Variants
 
-    def create_or_update_variant(product, variant_attributes)
-      variant = find_model(product.variants, variant_attributes[:id])
-      if variant.present?
-        variant.update(variant_attributes.except(:id))
-      else
-        create_variant(product, variant_attributes)
+      variants&.all? do |attributes|
+        variant.save
+        set_stock_levels(variant, attributes)
       end
     end
 
-    def create_variant(product, variant_attributes)
-      return if variant_attributes.blank?
-
-      # 'You need to save the variant to create a stock item before you can set stock levels.'
-      on_hand = variant_attributes.delete(:on_hand)
-      on_demand = variant_attributes.delete(:on_demand)
-
-      variant = product.variants.create(variant_attributes)
-
-      if variant.errors.present?
-        product.errors.merge!(variant.errors)
-        return false
-      end
-
+    def set_stock_levels(variant, attributes)
       begin
-        variant.on_demand = on_demand if on_demand.present?
-        variant.on_hand = on_hand.to_i if on_hand.present?
+        variant.on_demand = on_demand if attributes[:on_demand].present?
+        variant.on_hand = on_hand.to_i if attributes[:on_hand].present?
       rescue StandardError => e
-        notify_bugsnag(e, product, variant, variant_attributes)
+        notify_bugsnag(e, product, variant, attributes)
         raise e
       end
     end
