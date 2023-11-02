37feefce37f91a28d400dd5161c3346bdea21042
Maikel Linke <maikel@email.org.au>
2023-11-02 15:07:01 +1100

    Avoid precompiling assets unnecessary
    We had two calls to precompile assets, one for all specs and then a
    second one just for system specs. The one for system specs is better
    because it checks for the webpack dev server before compiling assets. So
    let's use just that one.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,12 +4,6 @@ require 'base_spec_helper'
 require 'database_cleaner'
 
 RSpec.configure do |config|
-  # Precompile Webpacker assets (once) when starting the suite. The default setup can result
-  # in the assets getting compiled many times throughout the build, slowing it down.
-  config.before :suite do
-    Webpacker.compile
-  end
-
   # Fix encoding issue in Rails 5.0; allows passing empty arrays or hashes as params.
   config.before(:each, type: :controller) { @request.env["CONTENT_TYPE"] = 'application/json' }
 end
