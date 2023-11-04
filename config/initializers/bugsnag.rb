eb82accb7911f440ce980d7b865db9474f4273e8
Yasir Azgar <yasirazgar@Yasirs-MacBook-Pro.local>
2023-11-04 19:35:53 +0530

    revert unwanted change

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -2,10 +2,5 @@ Bugsnag.configure do |config|
   config.api_key = ENV['BUGSNAG_API_KEY']
   config.release_stage = ENV['RAILS_ENV']
 
-  release_stages_to_notify = if ENV['RAILS_ENV'].in? %w(test development)
-                                nil
-                             else
-                               Array[ENV['RAILS_ENV']]
-                             end
-  config.notify_release_stages = release_stages_to_notify
+  config.notify_release_stages = %w(production staging)
 end
