1d88510b7b9352673fe4766ad89ceb3bcd4c10a7
Cillian O'Ruanaidh <contact@cilliano.com>
2023-11-10 15:51:21 +0000

    Only display Supplier column in Order Cycle Report email if there is more than one supplier

--- spec/mailers/producer_mailer_spec.rb
+++ spec/mailers/producer_mailer_spec.rb
@@ -185,6 +185,31 @@ describe ProducerMailer, type: :mailer do
     end
   end
 
+  context "products from multiple suppliers" do
+    before do
+      order_cycle.exchanges.create! sender: s1, receiver: d2, incoming: true,
+                                    receival_instructions: 'Community hall'
+      order_cycle.exchanges.create! sender: d2, receiver: d2, incoming: false,
+                                    pickup_time: 'Mon, 22nd Dec'
+      order = create(:order, distributor: d2, order_cycle:, state: 'complete')
+      order.line_items << create(:line_item, quantity: 3, variant: p1.variants.first)
+      order.finalize!
+      order.save
+    end
+
+    it "displays a supplier column" do
+      expect(body_as_html(mail).find(".order-summary"))
+        .to have_selector("th", text: "Supplier")
+    end
+  end
+
+  context "products from only one supplier" do
+    it "doesn't display a supplier column" do
+      expect(body_as_html(mail).find(".order-summary"))
+        .to have_no_selector("th", text: "Supplier")
+    end
+  end
+
   private
 
   def body_lines_including(mail, str)
