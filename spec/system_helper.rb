94fb1397f0a37cc5fb5936d644783fc96b877002
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2023-11-10 17:33:20 +1100

    Set up data to hide ToS banner
    The banner can in some case overlap element we are trying to interact
    with. Add a fake ToS file and make sure user have accepted the ToS, so
    that the banner is not shown

--- spec/system_helper.rb
+++ spec/system_helper.rb
@@ -2,5 +2,12 @@
 
 require "base_spec_helper"
 
+RSpec.configure do |config|
+  # Set up a fake ToS file
+  config.before(:each, type: :system) do
+    allow(TermsOfServiceFile).to receive(:updated_at).and_return(2.hours.ago)
+  end
+end
+
 # system/support/ files contain system tests configurations and helpers
 Dir[File.join(__dir__, "system/support/**/*.rb")].sort.each { |file| require file }
