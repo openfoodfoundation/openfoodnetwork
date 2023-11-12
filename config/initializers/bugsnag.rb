5eed5007866bae7ddff4f344b9f9fdb64121b605
Yasir Azgar <yasirazgar@Yasirs-MacBook-Pro.local>
2023-11-12 17:21:02 +0530

    supress bugsnag warn in dev

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,6 +1,9 @@
 Bugsnag.configure do |config|
   config.api_key = ENV['BUGSNAG_API_KEY']
   config.release_stage = ENV['RAILS_ENV']
-
+  if Rails.env.development?
+    config.logger = Logger.new(STDOUT) # In Rails apps, create a new logger to avoid changing the Rails log level
+    config.logger.level = Logger::ERROR
+  end
   config.notify_release_stages = %w(production staging)
 end
