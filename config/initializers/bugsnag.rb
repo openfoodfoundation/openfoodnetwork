b119a97eafd9e1414cf0b8ec7fead27b6b1158f0
yasir azgar <yasirazgar@gmail.com>
2023-11-12 21:12:26 -0800

    Update config/initializers/bugsnag.rb
    Co-authored-by: Maikel <maikel@email.org.au>

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,6 +1,6 @@
 Bugsnag.configure do |config|
   config.api_key = ENV['BUGSNAG_API_KEY']
-  config.release_stage = ENV['RAILS_ENV']
+  config.release_stage = Rails.env
   if Rails.env.development?
     config.logger = Logger.new(STDOUT) # In Rails apps, create a new logger to avoid changing the Rails log level
     config.logger.level = Logger::ERROR
