703ef8a203422df688ec1cf08442867e83409afb
yasir azgar <yasirazgar@gmail.com>
2023-11-12 21:12:32 -0800

    Update config/initializers/bugsnag.rb
    Co-authored-by: Maikel <maikel@email.org.au>

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,8 +1,9 @@
 Bugsnag.configure do |config|
   config.api_key = ENV['BUGSNAG_API_KEY']
   config.release_stage = Rails.env
+  # Avoid missing API key warning without changing the Rails log level.
   if Rails.env.development?
-    config.logger = Logger.new(STDOUT) # In Rails apps, create a new logger to avoid changing the Rails log level
+    config.logger = Logger.new(STDOUT)
     config.logger.level = Logger::ERROR
   end
   config.notify_release_stages = %w(production staging)
