d2b210c818942f72d11a16c8a27b498fddd3b756
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2023-11-17 14:16:33 +1100

    Update migration to set terms_of_service_accepted_at to time of deployment

--- db/migrate/20231103061213_add_terms_of_service_accepted_at_to_spree_users.rb
+++ db/migrate/20231103061213_add_terms_of_service_accepted_at_to_spree_users.rb
@@ -1,5 +1,13 @@
 class AddTermsOfServiceAcceptedAtToSpreeUsers < ActiveRecord::Migration[7.0]
-  def change
+  def up
     add_column :spree_users, :terms_of_service_accepted_at, :datetime
+
+    if Spree::Config.enterprises_require_tos == true
+      Spree::User.update_all(terms_of_service_accepted_at: Time.zone.now)
+    end
+  end
+
+  def down
+   remove_column :spree_users, :terms_of_service_accepted_at
   end
 end
