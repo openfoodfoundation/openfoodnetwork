3586497c562dcf7ccffeeaa95ad61f78d11cca97
Maikel Linke <maikel@email.org.au>
2023-11-23 16:58:20 +1100

    Precompile assets only when needed

--- spec/support/precompile_assets.rb
+++ spec/support/precompile_assets.rb
@@ -6,7 +6,13 @@ RSpec.configure do |config|
     # Useful if you working on a frontend code fixes and want to verify them via system tests.
     next if Webpacker.dev_server.running?
 
-    $stdout.puts "\n Precompiling assets.\n"
-    Webpacker.compile
+    specs_needing_assets = %i[controller feature mailer request system view]
+    examples = RSpec.world.filtered_examples.values.flatten
+    types = examples.map(&:metadata).pluck(:type).uniq
+
+    if types.intersect?(specs_needing_assets)
+      $stdout.puts "\n Precompiling assets.\n"
+      Webpacker.compile
+    end
   end
 end
