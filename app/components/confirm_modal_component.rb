5d490ded1581f1bb687fd1067eb7e61b7db92902
Ahmed Ejaz <chahmedejaz9050@gmail.com>
2023-11-25 15:00:48 +0500

    #11068, update ConfirmModalComponent to take more options

--- app/components/confirm_modal_component.rb
+++ app/components/confirm_modal_component.rb
@@ -1,14 +1,26 @@
 # frozen_string_literal: true
 
 class ConfirmModalComponent < ModalComponent
-  def initialize(id:, confirm_actions: nil, reflex: nil, controller: nil, message: nil,
-                 confirm_reflexes: nil)
+  def initialize(
+    id:,
+    reflex: nil,
+    controller: nil,
+    message: nil,
+    confirm_actions: nil,
+    confirm_reflexes: nil,
+    confirm_button_color: :primary,
+    confirm_button_text: I18n.t('js.admin.modals.confirm'),
+    cancel_button_text: I18n.t('js.admin.modals.cancel')
+  )
     super(id:, close_button: true)
     @confirm_actions = confirm_actions
     @reflex = reflex
     @confirm_reflexes = confirm_reflexes
     @controller = controller
     @message = message
+    @confirm_button_color = confirm_button_color
+    @confirm_button_text = confirm_button_text
+    @cancel_button_text = cancel_button_text
   end
 
   private
