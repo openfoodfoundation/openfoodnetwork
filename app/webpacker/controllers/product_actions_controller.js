2f0965aa6201149569311d45f175971ba1c87069
Ahmed Ejaz <chahmedejaz9050@gmail.com>
2023-11-25 19:38:43 +0500

    #11068, add product_actions_controller

---
+++ app/webpacker/controllers/product_actions_controller.js
@@ -0,0 +1,9 @@
+import ApplicationController from "./application_controller";
+
+export default class extends ApplicationController {
+  static values = {currentId: Number, currentVariantId: Number};
+
+  delete() {
+
+  }
+}
