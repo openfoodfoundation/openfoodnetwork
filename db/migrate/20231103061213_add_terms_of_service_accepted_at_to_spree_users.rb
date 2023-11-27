17fa6b616816c6cd4197202a757033dc12cfc1e7
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2023-11-27 11:58:40 +1100

    Per review, revert change

--- db/migrate/20231103061213_add_terms_of_service_accepted_at_to_spree_users.rb
+++ db/migrate/20231103061213_add_terms_of_service_accepted_at_to_spree_users.rb
@@ -3,18 +3,10 @@ class AddTermsOfServiceAcceptedAtToSpreeUsers < ActiveRecord::Migration[7.0]
     add_column :spree_users, :terms_of_service_accepted_at, :datetime
 
     if Spree::Config.enterprises_require_tos == true
-      # Update only user who are owners of at least 1 enterprise
-      ActiveRecord::Base.sanitize_sql(["
-         UPDATE spree_users
-         SET terms_of_service_accepted_at = :time
-         WHERE spree_users.id IN(
-           SELECT id FROM spree_users WHERE EXISTS (
-             SELECT 1 FROM enterprises WHERE spree_users.id = enterprises.owner_id
-           )
-        )".squish,
-        time: Time.zone.now
-      ])
-      ActiveRecord::Base.connection.execute(sql)
+      # There isn't really a way to know which user have access to admin pages, so we update
+      # everyone. It's technically wrong to say shoppers have accepted ToS, but they will be
+      # required to accept the terms if they sign up for an enterprise.
+      Spree::User.update_all(terms_of_service_accepted_at: Time.zone.now)
     end
   end
 
