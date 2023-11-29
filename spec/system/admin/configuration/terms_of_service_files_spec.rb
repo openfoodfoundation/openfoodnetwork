d3f2c72730359b1e6d18a143844b1e770cf9b1de
Maikel Linke <maikel@email.org.au>
2023-11-29 11:20:16 +1100

    DRY Terms of Service spec to save time
    The test conditions were already checked in the normal file upload.
    
    Run time reduced from 7.2s to 5.8s.

--- spec/system/admin/configuration/terms_of_service_files_spec.rb
+++ spec/system/admin/configuration/terms_of_service_files_spec.rb
@@ -10,25 +10,17 @@ describe "Terms of Service files" do
 
     before { login_as_admin }
 
-    it "can be reached via Configuration" do
+    it "can be uploaded" do
       visit spree.edit_admin_general_settings_path
       click_link "Terms of Service"
 
       expect(page).to have_content "No terms of services have been uploaded yet."
-    end
 
-    it "can be uploaded" do
-      visit admin_terms_of_service_files_path
       attach_file "Attachment", Rails.root.join(test_file_path)
       click_button "Create Terms of service file"
 
       expect(page).to have_link "Terms of Service"
       expect(page).to have_link "Delete file"
-    end
-
-    it "provides Rails' standard action for a new file" do
-      visit new_admin_terms_of_service_files_path
-      expect(page).to have_button "Create Terms of service file"
     end
 
     it "can delete the current file" do
