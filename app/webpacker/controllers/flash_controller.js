7d299affd3bd9515fba9c2cf8ba96e3b38a2bb57
David Cook <david@redcliffs.net>
2023-11-30 15:03:08 +1100

    Move hiding logic to stimulus controller
    This ensures morphed flashes hide like other flashes (eg in bulk order actions). I wanted to write a spec to prove it, but Capybara doesn't support mocking setTimeout and I didn't want to use sleep.
    
    I've made it optional because this controller is shared with the shop frontend ([supposedly](https://github.com/openfoodfoundation/openfoodnetwork/commit/5ef34347a39a05c1dd2495109142b53246e135b4), although angular seems to override it).

--- app/webpacker/controllers/flash_controller.js
+++ app/webpacker/controllers/flash_controller.js
@@ -5,7 +5,24 @@ document.addEventListener("turbolinks:before-cache", () =>
 );
 
 export default class extends Controller {
+  static values = {
+    autoClose: Boolean,
+  };
+
+  connect() {
+    if (this.autoCloseValue) {
+      setTimeout(this.close.bind(this), 5000);
+    }
+  }
+
   close() {
+    // Fade out
+    this.element.classList.remove("animate-show");
+    this.element.classList.add("animate-hide-500");
+    setTimeout(this.remove.bind(this), 500);
+  }
+
+  remove() {
     this.element.remove();
   }
 }
