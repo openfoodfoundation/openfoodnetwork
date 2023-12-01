32a4088386ebe54b7beec2e09c9372bc5ca12b7d
David Cook <david@redcliffs.net>
2023-12-01 15:25:50 +1100

    Revert "#11067, remove messages to match with old UI UX"
    Now we support flash messages, we can show it!
    
    This reverts commit d8904099dd85bcf358c2e7182349c81325faa755.

--- app/controllers/spree/admin/products_controller.rb
+++ app/controllers/spree/admin/products_controller.rb
@@ -82,7 +82,12 @@ module Spree
 
       def clone
         @new = @product.duplicate
-        @new.save
+
+        flash[:success] = if @new.save
+                            Spree.t('notice_messages.product_cloned')
+                          else
+                            Spree.t('notice_messages.product_not_cloned')
+                          end
 
         redirect_to spree.admin_products_url
       end
