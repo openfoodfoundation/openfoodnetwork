a1d3b20e5bef4fbeed6373ddd0da94ac169bd1b5
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2023-12-01 13:53:46 +1100

    Remove the fact ToS file
    Now that we check if there is a ToS file before displaying the banner
    it's not needed anymore

--- spec/system_helper.rb
+++ spec/system_helper.rb
@@ -2,12 +2,5 @@
 
 require "base_spec_helper"
 
-RSpec.configure do |config|
-  # Set up a fake ToS file
-  config.before(:each, type: :system) do
-    allow(TermsOfServiceFile).to receive(:updated_at).and_return(2.hours.ago)
-  end
-end
-
 # system/support/ files contain system tests configurations and helpers
 Dir[File.join(__dir__, "system/support/**/*.rb")].sort.each { |file| require file }
