0efd78b979b06575d3b3111b0a0f07e805f3dab3
Ahmed Ejaz <chahmedejaz9050@gmail.com>
2023-12-03 23:01:56 +0500

    #11068: update confirm_modal_component to accept controller values

--- app/components/confirm_modal_component.rb
+++ app/components/confirm_modal_component.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ConfirmModalComponent < ModalComponent
+  # @param controller_data_values [Array(Hash)] format: {<value-name-1>: value1, <value-name-2>: value2}
   def initialize(
     id:,
     reflex: nil,
@@ -10,7 +11,8 @@ class ConfirmModalComponent < ModalComponent
     confirm_reflexes: nil,
     confirm_button_color: :primary,
     confirm_button_text: I18n.t('js.admin.modals.confirm'),
-    cancel_button_text: I18n.t('js.admin.modals.cancel')
+    cancel_button_text: I18n.t('js.admin.modals.cancel'),
+    controller_data_values: {}
   )
     super(id:, close_button: true)
     @confirm_actions = confirm_actions
@@ -21,11 +23,25 @@ class ConfirmModalComponent < ModalComponent
     @confirm_button_color = confirm_button_color
     @confirm_button_text = confirm_button_text
     @cancel_button_text = cancel_button_text
+    @controller_data_values = transform_values_for_controller(controller_data_values)
   end
 
   private
 
   def close_button_class
     "secondary"
+  end
+
+  def modal_attrs
+    @modal_attrs ||= {
+      id: @id,
+      "data-controller": "modal #{@controller}",
+      "data-action": "keyup@document->modal#closeIfEscapeKey",
+      "data-#{@controller}-reflex-value": @reflex
+    }.merge(@controller_data_values)
+  end
+
+  def transform_values_for_controller(values)
+    values.transform_keys { |value_name| "data-#{@controller}-#{value_name}-value" }
   end
 end
