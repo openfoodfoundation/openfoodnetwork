a2cb1f4c37a0d62995ac2527e73149e1c404ef02
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2023-12-14 15:29:12 +1100

    Update swagger documentation

--- engines/dfc_provider/spec/requests/enterprises_spec.rb
+++ engines/dfc_provider/spec/requests/enterprises_spec.rb
@@ -29,6 +29,7 @@ describe "Enterprises", type: :request, swagger_doc: "dfc.yaml", rswag_autodoc: 
       :product_with_image,
       id: 90_000, supplier: enterprise, name: "Apple", description: "Round",
       variants: [variant],
+      primary_taxon: build(:taxon, name: "Non local vegetable", dfc_name: "non_local_vegetable"),
     )
   }
   let(:variant) { build(:base_variant, id: 10_001, unit_value: 1, sku: "APP") }
