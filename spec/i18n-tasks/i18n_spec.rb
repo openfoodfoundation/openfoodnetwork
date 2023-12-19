5c0c845b4995f4ee4670c8abdfc7a1899f5c604f
filipefurtad0 <filipefurtado@gmail.com>
2023-12-19 17:26:29 +0000

    Checks only en locale
    Removes pending from passing example

--- spec/i18n-tasks/i18n_spec.rb
+++ spec/i18n-tasks/i18n_spec.rb
@@ -8,28 +8,30 @@ RSpec.describe I18n do
   let(:unused_keys) { i18n.unused_keys }
   let(:inconsistent_interpolations) { i18n.inconsistent_interpolations }
 
-  before do
-    pending "fixing i18n issues"
-  end
+  context "pending" do
+    before do
+      pending "fixing i18n issues"
+    end
 
-  it 'does not have missing keys' do
-    expect(missing_keys).to be_empty,
-                            "Missing #{missing_keys.leaves.count} i18n keys, run  \
-                            `i18n-tasks missing' to show them"
-  end
+    it 'does not have missing keys' do
+      expect(missing_keys).to be_empty,
+                              "Missing #{missing_keys.leaves.count} i18n keys, run  \
+                              `i18n-tasks missing' to show them"
+    end
 
-  it 'does not have unused keys' do
-    expect(unused_keys).to be_empty,
-                           "#{unused_keys.leaves.count} unused i18n keys, run `i18n-tasks unused' \
-                           to show them"
-  end
+    it 'does not have unused keys' do
+      expect(unused_keys).to be_empty,
+                             "#{unused_keys.leaves.count} unused i18n keys, run `i18n-tasks unused' \
+                             to show them"
+    end
 
-  it 'files are normalized' do
-    non_normalized = i18n.non_normalized_paths
-    error_message = "The following files need to be normalized:\n" \
-                    "#{non_normalized.map { |path| "  #{path}" }.join("\n")}\n" \
-                    "Please run `i18n-tasks normalize' to fix"
-    expect(non_normalized).to be_empty, error_message
+    it 'files are normalized' do
+      non_normalized = i18n.non_normalized_paths
+      error_message = "The following files need to be normalized:\n" \
+                      "#{non_normalized.map { |path| "  #{path}" }.join("\n")}\n" \
+                      "Please run `i18n-tasks normalize' to fix"
+      expect(non_normalized).to be_empty, error_message
+    end
   end
 
   it 'does not have inconsistent interpolations' do
