408b62dc93e79838f6fc5beadfd16629f22978c2
Mohamed ABDELLANI <abdellani@gmail.com>
2023-12-22 10:39:15 +0100

    update load defaults to 7.1

--- config/application.rb
+++ config/application.rb
@@ -223,7 +223,7 @@ module Openfoodnetwork
     # Apply framework defaults. New recommended defaults are successively added with each Rails version and
     # include the defaults from previous versions. For more info see:
     # https://guides.rubyonrails.org/configuring.html#results-of-config-load-defaults
-    config.load_defaults 6.1
+    config.load_defaults 7.1
     config.action_view.form_with_generates_remote_forms = false
     config.active_record.cache_versioning = false
     config.active_record.has_many_inversing = false
