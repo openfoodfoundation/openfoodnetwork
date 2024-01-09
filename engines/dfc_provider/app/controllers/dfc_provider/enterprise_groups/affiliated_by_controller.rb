b90349e4c23aa765a1b9276a44f76d2d95c5ffb2
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2024-01-09 14:44:59 +1100

    Add endpoint to add enterprise to a group
    plus documentaion

---
+++ engines/dfc_provider/app/controllers/dfc_provider/enterprise_groups/affiliated_by_controller.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module DfcProvider
+  module EnterpriseGroups
+    class AffiliatedByController < DfcProvider::ApplicationController
+      def create
+        group = EnterpriseGroup.find(params[:enterprise_group_id])
+
+        authorize! :update, group
+
+        enterprise_uri = RDF::URI.new(params[:@id])
+
+        return head :bad_request unless enterprise_uri.valid?
+
+        enterprise_id = ofn_id_from_uri(enterprise_uri)
+        enterprise = Enterprise.find(enterprise_id)
+
+        group.enterprises << enterprise
+
+        return head :unprocessable_entity unless group.save
+
+        head :created
+      end
+
+      private
+
+      def ofn_id_from_uri(uri)
+        # enterprise uri follow this format http://test.host/api/dfc/enterprises/{ofn_enterprise_id}
+        uri.path.split("/").last
+      end
+    end
+  end
+end
