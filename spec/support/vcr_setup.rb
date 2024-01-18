a2436e9a1a172047977b1bb32c33cb5fa14534d3
David Cook <david@redcliffs.net>
2024-01-18 12:14:07 +1100

    Rename hidden keys
    If they follow a rule, it reduces indirection and makes it easier to manage, I reckon.

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -7,8 +7,8 @@ VCR.configure do |config|
   config.hook_into :webmock
   config.ignore_localhost = true
   config.configure_rspec_metadata!
-  config.filter_sensitive_data('<HIDDEN_KEY>') { ENV.fetch('STRIPE_SECRET_TEST_API_KEY', nil) }
-  config.filter_sensitive_data('<HIDDEN_CUSTOMER>') { ENV.fetch('STRIPE_CUSTOMER', nil) }
-  config.filter_sensitive_data('<HIDDEN_ACCOUNT>') { ENV.fetch('STRIPE_ACCOUNT', nil) }
+  config.filter_sensitive_data('<HIDDEN-STRIPE_SECRET_TEST_API_KEY>') { ENV.fetch('STRIPE_SECRET_TEST_API_KEY', nil) }
+  config.filter_sensitive_data('<HIDDEN-STRIPE_CUSTOMER>') { ENV.fetch('STRIPE_CUSTOMER', nil) }
+  config.filter_sensitive_data('<HIDDEN-STRIPE_ACCOUNT>') { ENV.fetch('STRIPE_ACCOUNT', nil) }
   config.ignore_hosts('localhost', '127.0.0.1', '0.0.0.0', 'api.knapsackpro.com')
 end
