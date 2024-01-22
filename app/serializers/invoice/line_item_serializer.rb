ba841d9fbc949f8d34772d92e6838491ed8742f1
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2024-01-22 12:57:43 +1100

    Add enterprise_fee_tax to Invoice::LineItemSerializer

--- app/serializers/invoice/line_item_serializer.rb
+++ app/serializers/invoice/line_item_serializer.rb
@@ -3,8 +3,12 @@
 class Invoice
   class LineItemSerializer < ActiveModel::Serializer
     attributes :id, :added_tax, :currency, :included_tax, :price_with_adjustments, :quantity,
-               :variant_id, :unit_price_price_and_unit, :unit_presentation
+               :variant_id, :unit_price_price_and_unit, :unit_presentation, :enterprise_fee_tax
     has_one :variant, serializer: Invoice::VariantSerializer
     has_many :tax_rates, serializer: Invoice::TaxRateSerializer
+
+    def enterprise_fee_tax
+      EnterpriseFeeAdjustments.new(object.enterprise_fee_adjustments).total_tax
+    end
   end
 end
