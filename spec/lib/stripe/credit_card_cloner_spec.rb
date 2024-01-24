3e734f1e6b942ba8e3a8476d6df5d2115182dec4
filipefurtad0 <filipefurtado@gmail.com>
2024-01-24 13:21:11 +0000

    Attaches customer to payment method and clones payment method
    Improves naming of actors; improves assertions

--- spec/lib/stripe/credit_card_cloner_spec.rb
+++ spec/lib/stripe/credit_card_cloner_spec.rb
@@ -5,7 +5,7 @@ require 'stripe/credit_card_cloner'
 
 module Stripe
   describe CreditCardCloner do
-    let!(:user) { create(:user, email: "jumping.jane@example.com") }
+    let!(:user) { create(:user, email: "apple.customer@example.com") }
     let!(:enterprise) { create(:enterprise) }
 
     let(:secret) { ENV.fetch('STRIPE_SECRET_TEST_API_KEY', nil) }
@@ -15,11 +15,11 @@ module Stripe
 
       before { Stripe.api_key = secret }
 
-      let!(:customer_id) { ENV.fetch('STRIPE_CUSTOMER', nil) }
+      let(:customer_id) { ENV.fetch('STRIPE_CUSTOMER', nil) }
 
-      let!(:stripe_account_id) { ENV.fetch('STRIPE_ACCOUNT', nil) }
+      let(:stripe_account_id) { ENV.fetch('STRIPE_ACCOUNT', nil) }
 
-      let!(:stripe_account) {
+      let(:stripe_account) {
         create(:stripe_account, enterprise:, stripe_user_id: stripe_account_id)
       }
 
@@ -39,42 +39,50 @@ module Stripe
         )
       }
 
-      let!(:connected_account) do
+      let(:connected_account) do
         Stripe::Account.create({
                                  type: 'standard',
                                  country: 'AU',
-                                 email: 'jumping.jack@example.com'
+                                 email: 'apple.producer@example.com'
                                })
       end
 
-      let!(:cloner) { Stripe::CreditCardCloner.new(credit_card, connected_account.id) }
+      let(:cloner) { Stripe::CreditCardCloner.new(credit_card, connected_account.id) }
 
       context "when called with a card without a customer (one time usage card)" do
-        let!(:payment_method_id) { pm_card.id }
+        let(:payment_method_id) { pm_card.id }
 
         it "clones the payment method only" do
           customer_id, new_payment_method_id = cloner.find_or_clone
 
+          expect(payment_method_id).to match(/pm_/)
+          expect(new_payment_method_id).to match(/pm_/)
           expect(payment_method_id).not_to eq new_payment_method_id
           expect(customer_id).to eq nil
         end
       end
 
-      xcontext "when called with a valid customer and payment_method" do
+      context "when called with a valid customer and payment_method" do
+        let(:payment_method_id) { pm_card.id }
+
         before do
-          stub_request(:post, "https://api.stripe.com/v1/payment_methods")
-            .with(body: { customer: customer_id, payment_method: payment_method_id },
-                  headers: { 'Stripe-Account' => stripe_account_id })
-            .to_return(payment_method_response_mock)
+          Stripe::PaymentMethod.attach(
+            payment_method_id,
+            { customer: customer_id },
+          )
 
           credit_card.update_attribute :gateway_customer_profile_id, customer_id
         end
 
         it "clones both the payment method and the customer" do
-          customer_id, payment_method_id = cloner.find_or_clone
+          new_customer_id, new_payment_method_id = cloner.find_or_clone
 
-          expect(payment_method_id).to eq new_payment_method_id
-          expect(customer_id).to eq new_customer_id
+          expect(payment_method_id).to match(/pm_/)
+          expect(new_payment_method_id).to match(/pm_/)
+          expect(payment_method_id).not_to eq new_payment_method_id
+          expect(customer_id).to match(/cus_/)
+          expect(new_customer_id).to match(/cus_/)
+          expect(customer_id).not_to eq new_customer_id
         end
       end
     end
