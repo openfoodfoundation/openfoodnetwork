581627cd3fef57ff3a87d5aec913ac5ee2658018
drummer83 <mail@konradsachse.de>
2024-01-30 22:07:53 +0100

    Add translations to shipment mailer to send the mail in the customer's language

--- app/mailers/spree/shipment_mailer.rb
+++ app/mailers/spree/shipment_mailer.rb
@@ -2,6 +2,7 @@
 
 module Spree
   class ShipmentMailer < ApplicationMailer
+    include I18nHelper
     helper MailerHelper
 
     def shipped_email(shipment, delivery:)
@@ -9,12 +10,14 @@ module Spree
       @order = @shipment.order
       @hide_ofn_navigation = @shipment.order.distributor.hide_ofn_navigation
       @delivery = delivery
-      subject = t(base_subject,
-                  number: @order.number,
-                  distributor: @order.distributor.name)
-      mail(to: @shipment.order.email,
-           subject:,
-           reply_to: @order.distributor.contact.email)
+      I18n.with_locale valid_locale(@order.user) do
+        subject = t(base_subject,
+                    number: @order.number,
+                    distributor: @order.distributor.name)
+        mail(to: @shipment.order.email,
+             subject:,
+             reply_to: @order.distributor.contact.email)
+      end
     end
 
     private
