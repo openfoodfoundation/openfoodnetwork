57f55d7585d200c541ef61dc09099259ebebd7f8
filipefurtad0 <filipefurtado@gmail.com>
2024-01-31 18:57:56 +0000

    Replaces stubs for existing customer test case

--- spec/lib/stripe/credit_card_remover_spec.rb
+++ spec/lib/stripe/credit_card_remover_spec.rb
@@ -4,20 +4,73 @@ require 'spec_helper'
 require 'stripe/credit_card_remover'
 
 describe Stripe::CreditCardRemover do
-  let(:credit_card) { double('credit_card', gateway_customer_profile_id: 1) }
+  let!(:secret) { ENV.fetch('STRIPE_SECRET_TEST_API_KEY', nil) }
+
+  let(:credit_card) { create(:credit_card, gateway_payment_profile_id: pm_card.id, user:) }
+
+  let!(:user) { create(:user, email: "apple.customer@example.com") }
+  let!(:enterprise) { create(:enterprise) }
 
   describe "#remove", :vcr, :stripe_version do
+    before { Stripe.api_key = secret }
+
+    let(:stripe_account_id) { ENV.fetch('STRIPE_ACCOUNT', nil) }
+
+    let(:stripe_account) {
+      create(:stripe_account, enterprise:, stripe_user_id: stripe_account_id)
+    }
+
+    let(:credit_card) { create(:credit_card, gateway_payment_profile_id: pm_card.id, user:) }
+
+    let(:pm_card) {
+      Stripe::PaymentMethod.create(
+        {
+          type: 'card',
+          card: {
+            number: '4242424242424242',
+            exp_month: 8,
+            exp_year: Time.zone.now.year.next,
+            cvc: '314',
+          },
+        },
+      )
+    }
+
+    let(:connected_account) do
+      Stripe::Account.create({
+                               type: 'standard',
+                               country: 'AU',
+                               email: 'apple.producer@example.com'
+                             })
+    end
+
+    let(:cloner) { Stripe::CreditCardCloner.new(credit_card, connected_account.id) }
+
     context 'Stripe customer exists' do
+      let(:payment_method_id) { pm_card.id }
+      let(:customer_id) { customer.id }
+
+      let(:customer) do
+        Stripe::Customer.create({
+                                  name: 'Apple Customer',
+                                  email: 'applecustomer@example.com',
+                                })
+      end
+
+      before do
+        Stripe::PaymentMethod.attach(
+          payment_method_id,
+          { customer: customer_id },
+        )
+
+        credit_card.update_attribute :gateway_customer_profile_id, customer_id
+      end
+
       context 'and is not deleted' do
         it 'deletes the credit card clone and the customer' do
-          customer = double('customer', deleted?: false)
-          allow(Stripe::Customer).to receive(:retrieve).and_return(customer)
+          response = Stripe::CreditCardRemover.new(credit_card).call
 
-          expect_any_instance_of(Stripe::CreditCardCloneDestroyer).to receive(:destroy_clones).with(
-            credit_card
-          )
-          expect(customer).to receive(:delete)
-          Stripe::CreditCardRemover.new(credit_card).call
+          expect(response.deleted).to eq(true)
         end
       end
 
