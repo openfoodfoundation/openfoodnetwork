0fa272a1256ddeee98ba0916a2eccb21a03acce0
filipefurtad0 <filipefurtado@gmail.com>
2024-02-08 17:56:59 +0000

    Changes helper for dropdown selection

--- spec/system/admin/products_v3/products_spec.rb
+++ spec/system/admin/products_v3/products_spec.rb
@@ -1597,7 +1597,7 @@ RSpec.describe 'As an enterprise user, I can manage my products', feature: :admi
       find("a", text: "New Product").click
       expect(page).to have_content "New Product"
       fill_in 'product_name', with: 'Big Bag Of Apples'
-      select_tom_select supplier.name, from: 'product_supplier_field'
+      tomselect_select supplier.name, from: 'product[supplier_id]'
       select_tom_select 'Weight (g)', from: 'product_variant_unit_field'
       fill_in 'product_unit_value', with: '100'
       fill_in 'product_price', with: '10.00'
