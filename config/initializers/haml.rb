7a767ab037830fa37ad52ba9c146cbe601dc9454
Maikel Linke <maikel@email.org.au>
2024-02-08 17:02:30 +1100

    Make haml aware of custom boolean attributes

---
+++ config/initializers/haml.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+# Haml 6 treats only a small set of standard boolean attributes as such.
+# Other attributes are added in full. But AngularJS distinguishes between
+#   <div ng-cloak>
+# and
+#   <div ng-cloak="true">
+#
+# The latter raises errors.
+#
+# Adding to these attributes is officially supported:
+# - https://github.com/haml/haml/releases/tag/v6.2.2
+#
+Haml::BOOLEAN_ATTRIBUTES.push(
+  *%w[
+    mailto
+    new-tag-rule-dialog
+    ng-cloak
+    ng-transclude
+    offcanvas
+    ofn-disable-enter
+    question-mark-with-tooltip-animation
+    scroll-after-load
+    textangular-links-target-blank
+  ]
+)
