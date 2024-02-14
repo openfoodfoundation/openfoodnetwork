cffe573203ef7009d0b4a02b2499078e0626dacc
isidzukuri <axesigon@gmail.com>
2024-02-14 18:41:28 +0200

    improve random name generation in factories related to tax_rate_factory

--- spec/factories/tax_category_factory.rb
+++ spec/factories/tax_category_factory.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :tax_category, class: Spree::TaxCategory do
-    name { "TaxCategory - #{rand(999_999)}" }
+    sequence(:name) { |n| "TaxCategory - #{n}" }
     description { generate(:random_string) }
   end
 end
