78bfd47ad38aa1cae206b9c4f30d24d7be2c962c
Maikel Linke <maikel@email.org.au>
2024-02-15 16:36:01 +1100

    Add rake task to reset queued jobs
    Please note that the spec is using Redis as Sidekiq does. This revealed
    that Sidekiq wasn't set up for jobs in the test environment properly.

---
+++ spec/lib/tasks/reset_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'rake'
+
+describe "reset.rake" do
+  before(:all) do
+    Rake.application.rake_require("tasks/reset")
+    Rake::Task.define_task(:environment)
+  end
+
+  it "clears job queues" do
+    job_class = Class.new do
+      include Sidekiq::Job
+    end
+    job_class.perform_async
+
+    queue = Sidekiq::Queue.all.first # rubocop:disable Rails/RedundantActiveRecordAllMethod
+
+    expect {
+      Rake.application.invoke_task "ofn:reset_sidekiq"
+    }.to change {
+      queue.count
+    }.to(0)
+  end
+end
