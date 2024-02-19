56fe7f5e2187ef6a3ef38bd0cbc63af8df5d5499
Mohamed ABDELLANI <abdellani@gmail.com>
2024-02-19 12:22:57 +0100

    fix tax rates listing in invoices

--- app/models/invoice/data_presenter/line_item.rb
+++ app/models/invoice/data_presenter/line_item.rb
@@ -3,7 +3,7 @@
 class Invoice
   class DataPresenter
     class LineItem < Invoice::DataPresenter::Base
-      attributes :added_tax, :currency, :included_tax, :price_with_adjustments, :quantity,
+      attributes :id, :added_tax, :currency, :included_tax, :price_with_adjustments, :quantity,
                  :variant_id, :unit_price_price_and_unit, :unit_presentation,
                  :enterprise_fee_additional_tax, :enterprise_fee_included_tax
       attributes_with_presenter :variant
@@ -34,10 +34,6 @@ class Invoice
       def single_display_amount_with_adjustments
         fee_tax = enterprise_fee_included_tax || 0.0
         Spree::Money.new(price_with_adjustments - ((included_tax + fee_tax) / quantity), currency:)
-      end
-
-      def display_line_item_tax_rates
-        tax_rates.map { |tr| number_to_percentage(tr.amount * 100, precision: 1) }.join(", ")
       end
     end
   end
