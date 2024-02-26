b69fc99b08d75c7cce5e040930ee6d2b93df6767
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2024-02-26 20:53:27 +1100

    Move supplier to variant part 2
    Variant model and specs

--- spec/factories/variant_factory.rb
+++ spec/factories/variant_factory.rb
@@ -12,7 +12,9 @@ FactoryBot.define do
     depth  { generate(:random_float) }
 
     primary_taxon { Spree::Taxon.first || FactoryBot.create(:taxon) }
-    product { |p| p.association(:product) }
+    supplier { Enterprise.is_primary_producer.first || FactoryBot.create(:supplier_enterprise) }
+
+    product { |p| p.association(:base_product) }
 
     # ensure stock item will be created for this variant
     before(:create) { create(:stock_location) if Spree::StockLocation.count.zero? }
@@ -23,6 +25,7 @@ FactoryBot.define do
         on_hand { 5 }
       end
 
+      product { |p| p.association(:product) }
       unit_value { 1 }
       unit_description { '' }
 
