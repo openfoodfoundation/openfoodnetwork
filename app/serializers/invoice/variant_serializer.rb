80112709f33dec7ac02c2918a90fc625f07e372b
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2024-02-28 10:25:51 +1100

    Fix invoice and api serializer

--- app/serializers/invoice/variant_serializer.rb
+++ app/serializers/invoice/variant_serializer.rb
@@ -4,5 +4,6 @@ class Invoice
   class VariantSerializer < ActiveModel::Serializer
     attributes :id, :display_name, :options_text
     has_one :product, serializer: Invoice::ProductSerializer
+    has_one :supplier, serializer: Invoice::EnterpriseSerializer
   end
 end
