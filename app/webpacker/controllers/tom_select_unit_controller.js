2d967d8763c2c4b25aeb95ab9734ebe466066ab4
David Cook <david@redcliffs.net>
2024-03-04 16:55:43 +1100

    more try..

--- app/webpacker/controllers/tom_select_unit_controller.js
+++ app/webpacker/controllers/tom_select_unit_controller.js
@@ -8,6 +8,16 @@ export default class extends TomSelectController {
     const nameInputTarget = this.element.parentNode.querySelector('[data-tom-select-unit-target=nameInput]');
     const createElementWithText = this.createElementWithText;
 
+    nameInputTarget.addEventListener("keydown", function(e){ console.log('keydown'); e.stopPropagation(); });
+    // Prevent bubbling up to tomselect, and focus. But tomselect captures it still :(
+    nameInputTarget.addEventListener("click", function(e){
+      console.log('click'); e.stopPropagation();
+      // e.preventDefault();
+      // nameInputTarget.focus();
+    });
+    // But tomselect seems to  captures it still, befure this is called:
+    nameInputTarget.addEventListener("focus", function(e){ console.log('focus'); e.stopPropagation(); });
+
     options = {
       closeAfterSelect: false,
       searchField: [], //disable type to search. todo: copy for a no-input controller
@@ -24,11 +34,24 @@ export default class extends TomSelectController {
         },
       },
       onChange: function(value) {
+        console.log('onChange', value);
         if (value == "items") {
-          console.log('onChange', value);
           nameInputTarget.focus(); //pls focus the input, I want to type into it!
           return false;
+        } else {
+          this.close();
         }
+      },
+      onDropdownClose: function(dropdown) {
+        console.log("onDropdownClose", dropdown, this.getValue());
+        // nameInputTarget.blur();
+        const value = this.getValue()
+        // if(value == "items") {
+        // if (value == "boxes") { //todo: if value is not a pre-defined value
+
+        //   this.setValue("items", true); // poopoo, silent mode still resets the input
+        //   nameInputTarget.value = value; //todo: only if custom item value
+        // }
       },
       plugins: [],
       ...options,
