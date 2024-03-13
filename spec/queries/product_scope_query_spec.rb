e1c3f0a31c62d224ebc93efe88a02ab710e0f942
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2024-03-13 16:18:18 +1100

    Fix product scope query
    Plus a small name refactor

--- spec/queries/product_scope_query_spec.rb
+++ spec/queries/product_scope_query_spec.rb
@@ -6,14 +6,14 @@ RSpec.describe ProductScopeQuery do
   let!(:taxon) { create(:taxon) }
   let(:supplier) { create(:supplier_enterprise) }
   let(:supplier2) { create(:supplier_enterprise) }
-  let!(:product) { create(:product, supplier:, primary_taxon: taxon) }
-  let!(:product2) { create(:product, supplier: supplier2, primary_taxon: taxon) }
+  let!(:product) { create(:product, supplier_id: supplier.id, primary_taxon: taxon) }
+  let!(:product2) { create(:product, supplier_id: supplier2.id, primary_taxon: taxon) }
   let!(:current_api_user) { supplier_enterprise_user(supplier) }
 
   before { current_api_user.enterprise_roles.create(enterprise: supplier2) }
 
   describe '#bulk_products' do
-    let!(:product3) { create(:product, supplier: supplier2) }
+    let!(:product3) { create(:product, supplier_id: supplier2.id) }
 
     it "returns a list of products" do
       expect(ProductScopeQuery.new(current_api_user, {}).bulk_products)
@@ -22,7 +22,7 @@ RSpec.describe ProductScopeQuery do
 
     it "filters results by supplier" do
       subject = ProductScopeQuery
-        .new(current_api_user, { q: { supplier_id_eq: supplier.id } }).bulk_products
+        .new(current_api_user, { q: { variants_supplier_id_eq: supplier.id } }).bulk_products
 
       expect(subject).to include(product)
       expect(subject).not_to include(product2, product3)
@@ -79,6 +79,32 @@ RSpec.describe ProductScopeQuery do
       it 'finds product to be cloned' do
         expect(query.find_product_to_be_cloned).to eq(product2)
       end
+    end
+  end
+
+  describe "#products_for_producers" do
+    subject(:query) { ProductScopeQuery.new(current_api_user, { page: 1, per_page: 20 }) }
+
+    let(:hub) { create(:distributor_enterprise) }
+    let(:producer) { create(:supplier_enterprise) }
+    let!(:product3) { create(:product, supplier_id: producer.id) }
+    let!(:product4) { create(:product, supplier_id: producer.id) }
+    let!(:product5) { create(:product, supplier_id: supplier.id) }
+    let!(:er) {
+      create(:enterprise_relationship, parent: producer, child: hub,
+                                       permissions_list: [:create_variant_overrides])
+    }
+
+    before do
+      current_api_user.enterprise_roles.create(enterprise: hub)
+    end
+
+    it "finds products by producer" do
+      # Add variants so we can check if we are not returning duplicate products
+      create(:variant, product: product3, supplier: producer)
+      create(:variant, product: product3, supplier: producer)
+
+      expect(query.products_for_producers).to eq([product3, product4])
     end
   end
 
