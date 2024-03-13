656361c82d23111e39746d93f78497b6e33cc465
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2024-03-13 16:45:07 +1100

    Fix API v0 packing report

--- lib/reporting/queries/joins.rb
+++ lib/reporting/queries/joins.rb
@@ -19,8 +19,8 @@ module Reporting
         reflect query.join(association(Spree::Variant, :product))
       end
 
-      def joins_product_supplier
-        reflect query.join(association(Spree::Product, :supplier, supplier_alias))
+      def joins_variant_supplier
+        reflect query.join(association(Spree::Variant, :supplier, supplier_alias))
       end
 
       def joins_variant_shipping_category
