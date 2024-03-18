c57d44f35c01ce1c4b2ae4320f729341cc73ba07
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2024-03-18 13:33:40 +1100

    Fix bulk coop report

--- spec/lib/reports/bulk_coop_report_spec.rb
+++ spec/lib/reports/bulk_coop_report_spec.rb
@@ -122,7 +122,7 @@ module Reporting
                                ship_address: create(:address))
               end
               let(:li2) do
-                build(:line_item_with_shipment, product: create(:simple_product, supplier: s1))
+                build(:line_item_with_shipment, variant: create(:variant, supplier: s1))
               end
 
               before do
@@ -144,7 +144,7 @@ module Reporting
                                ship_address: create(:address))
               end
               let(:li2) do
-                build(:line_item_with_shipment, product: create(:simple_product, supplier: s1))
+                build(:line_item_with_shipment, variant: create(:variant, supplier: s1))
               end
 
               before do
