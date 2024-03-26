b18fe8ce35b1378f6c20a467ca51bbfa248aff12
Ana Nunes da Silva <ananunesdasilva@gmail.com>
2024-03-26 10:59:35 +0000

    Fix offense constant definition in block in models/spree/preferences/configuration_spec.rb

--- spec/models/spree/preferences/configuration_spec.rb
+++ spec/models/spree/preferences/configuration_spec.rb
@@ -3,25 +3,24 @@
 require 'spec_helper'
 
 describe Spree::Preferences::Configuration do
-  before :all do
-    class AppConfig < Spree::Preferences::Configuration
+  let(:config) do
+    Class.new(Spree::Preferences::Configuration) do
       preference :color, :string, default: :blue
-    end
-    @config = AppConfig.new
+    end.new
   end
 
   it "has named methods to access preferences" do
-    @config.color = 'orange'
-    expect(@config.color).to eq 'orange'
+    config.color = 'orange'
+    expect(config.color).to eq 'orange'
   end
 
   it "uses [ ] to access preferences" do
-    @config[:color] = 'red'
-    expect(@config[:color]).to eq 'red'
+    config[:color] = 'red'
+    expect(config[:color]).to eq 'red'
   end
 
   it "uses set/get to access preferences" do
-    @config.set :color, 'green'
-    expect(@config.get(:color)).to eq 'green'
+    config.set :color, 'green'
+    expect(config.get(:color)).to eq 'green'
   end
 end
