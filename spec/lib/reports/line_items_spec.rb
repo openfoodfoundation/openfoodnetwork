760e167f898fde2c2a7b69e0b4ca4250fdd70104
Ana Nunes da Silva <ananunesdasilva@gmail.com>
2024-03-26 10:54:16 +0000

    Fix offense constant definition in block in reports/line_items_spec.rb

--- spec/lib/reports/line_items_spec.rb
+++ spec/lib/reports/line_items_spec.rb
@@ -2,35 +2,35 @@
 
 require 'spec_helper'
 
+# This object lets us add some test coverage despite the very deep coupling between the class
+# under test and the various objects it depends on. Other more common moking strategies where very
+# hard.
+class FakeOrderPermissions
+  def initialize(line_items, orders_relation)
+    @relations = Spree::LineItem.where(id: line_items.map(&:id))
+    @orders_relation = orders_relation
+  end
+
+  def visible_line_items
+    relations
+  end
+
+  def editable_line_items
+    line_item = FactoryBot.create(:line_item)
+    Spree::LineItem.where(id: line_item.id)
+  end
+
+  def visible_orders
+    orders_relation
+  end
+
+  private
+
+  attr_reader :relations, :orders_relation
+end
+
 describe Reporting::LineItems do
   subject(:reports_line_items) { described_class.new(order_permissions, params) }
-
-  # This object lets us add some test coverage despite the very deep coupling between the class
-  # under test and the various objects it depends on. Other more common moking strategies where very
-  # hard.
-  class FakeOrderPermissions
-    def initialize(line_items, orders_relation)
-      @relations = Spree::LineItem.where(id: line_items.map(&:id))
-      @orders_relation = orders_relation
-    end
-
-    def visible_line_items
-      relations
-    end
-
-    def editable_line_items
-      line_item = FactoryBot.create(:line_item)
-      Spree::LineItem.where(id: line_item.id)
-    end
-
-    def visible_orders
-      orders_relation
-    end
-
-    private
-
-    attr_reader :relations, :orders_relation
-  end
 
   describe '#list' do
     let!(:order) do
