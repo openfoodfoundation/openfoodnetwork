4140257fa1e3ffa1b63efa53ae694276ecf9c81a
Anthony Musyoki <445103+anthonyms@users.noreply.github.com>
2024-03-26 09:54:51 +0300

    Fix Rubocop: Do not delete dependent adjustments
    TaxRate acts_as_paranoid iand is thus not hard_deleted

--- app/models/spree/tax_rate.rb
+++ app/models/spree/tax_rate.rb
@@ -21,7 +21,7 @@ module Spree
 
     belongs_to :zone, class_name: "Spree::Zone", inverse_of: :tax_rates
     belongs_to :tax_category, class_name: "Spree::TaxCategory", inverse_of: :tax_rates
-    has_many :adjustments, as: :originator
+    has_many :adjustments, as: :originator, dependent: nil
 
     validates :amount, presence: true, numericality: true
     validates :tax_category, presence: true
