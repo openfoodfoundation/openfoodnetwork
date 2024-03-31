4ba6afa6652cf8fc4b3f8070bc2159299f9b8055
Ahmed Ejaz <chahmedejaz9050@gmail.com>
2024-04-01 02:19:14 +0500

    12314 - fix Rails/HelperInstanceVariable error - InjectionHelper - OrdersHelper

--- app/helpers/spree/orders_helper.rb
+++ app/helpers/spree/orders_helper.rb
@@ -17,17 +17,18 @@ module Spree
 
     def changeable_orders
       # Only returns open order for the current user + shop + oc combo
-      return @changeable_orders unless @changeable_orders.nil?
-      return @changeable_orders = [] unless spree_current_user &&
-                                            current_distributor && current_order_cycle
-      return @changeable_orders = [] unless current_distributor.allow_order_changes?
+      @changeable_orders ||= if spree_current_user &&
+                                current_distributor&.allow_order_changes? && current_order_cycle
 
-      @changeable_orders = Spree::Order.complete.where(
-        state: 'complete',
-        user_id: spree_current_user.id,
-        distributor_id: current_distributor.id,
-        order_cycle_id: current_order_cycle.id
-      )
+                               Spree::Order.complete.where(
+                                 state: 'complete',
+                                 user_id: spree_current_user.id,
+                                 distributor_id: current_distributor.id,
+                                 order_cycle_id: current_order_cycle.id
+                               )
+                             else
+                               []
+                             end
     end
 
     def changeable_orders_link_path
