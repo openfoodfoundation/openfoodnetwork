d16308011554c9da955bdd0f638b88a0d3f85277
Kelly <kellyzhangg12@gmail.com>
2024-04-16 14:52:17 -0400

    added test for if user isn't logged in for payments_controller

---
+++ spec/controllers/payments_controller_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe PaymentsController, type: :controller do
+    
+    describe "testing redirect_to_authorize" do
+        context "when user isn't logged in" do
+            it "redirects to the login page and set error flash msg" do
+                get :redirect_to_authorize, params: { id: payment.id }
+                expect(response).to redirect_to(root_path(anchor: "/login", after_login: request.original_fullpath))
+                expect(flash[:error]).to eq I18n.t("spree.orders.edit.login_to_view_order")
+            end
+        end
+    
+    context "when user is logged in" do
+    
+    end
+
+end
