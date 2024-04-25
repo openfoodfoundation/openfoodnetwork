5ab56e421775164cd2dffef78c353b8af0f162c2
labelleorange <fruits@labelleorange.es>
2024-04-25 15:28:02 +0500

    corrected the use of aws cli into aws-sdk

--- app/services/database_backup_on_s3_service.rb
+++ app/services/database_backup_on_s3_service.rb
@@ -1,21 +1,49 @@
 # frozen_string_literal: true
 
+require 'aws-sdk-s3'
+
 class DatabaseBackupOnS3Service
-  def call
-    bucket_name = ENV.fetch('DB_BUCKET_NAME', 'database-backups')
-    object_key = "#{Time.now.to_i}_open_food_network_dev.sql"
+  def self.perform_backup
+    new.perform
+  end
 
-    pg_dump_command = "PGPASSWORD=#{ENV.fetch('OFN_DB_PASSWORD', 'f00d')} pg_dump -Fc -Z9 -U #{ENV.fetch('OFN_DB_USERNAME', 'ofn')} -h #{ENV.fetch('OFN_DB_HOST', 'localhost')} #{ENV.fetch('OFN_DB_NAME', 'openfoodnetwork')} > #{object_key}"
-    system(pg_dump_command)
+  def perform
+    setup_environment
+    create_database_dump
+    upload_to_s3
+  ensure
+    cleanup
+  end
 
-    # Upload the backup file to S3
-    aws_copy_command = "aws --profile eu2 --region default --endpoint-url https://eu2.contabostorage.com s3 cp #{object_key} s3://#{bucket_name}"
-    system(aws_copy_command)
+  private
 
-    Rails.logger.debug { "Backup successfully uploaded to S3 bucket: #{bucket_name}/#{object_key}" }
+  def setup_environment
+    @bucket_name = ENV.fetch('DB_BUCKET_NAME', 'database-backups')
+    @object_key = "#{Time.now.to_i}_open_food_network_backup.sql"
+    @db_dump_path = Rails.root.join(@object_key)
+  end
 
-    # Delete the local backup file after uploading to S3
-    File.delete(object_key)
-    Rails.logger.debug "Local backup file deleted."
+  def create_database_dump
+    pg_dump_command = "PGPASSWORD=#{ENV.fetch('OFN_DB_PASSWORD', 'f00d')} pg_dump -Fc -Z9 -U #{ENV.fetch('OFN_DB_USERNAME', 'ofn')} -h #{ENV.fetch('OFN_DB_HOST', 'localhost')} #{ENV.fetch('OFN_DB_NAME', 'openfoodnetwork')} > #{@object_key}"
+    system(pg_dump_command) or raise "Database dump failed"
+  end
+
+  def upload_to_s3
+    s3 = Aws::S3::Resource.new(
+      profile: 'eu2',
+      endpoint: 'https://eu2.contabostorage.com',
+      region: 'eu-west-2',  # Adjust the region if necessary
+      force_path_style: true
+    )
+    obj = s3.bucket(@bucket_name).object(@object_key)
+    obj.upload_file(@db_dump_path.to_s)
+    Rails.logger.info "Backup successfully uploaded to S3 bucket: #{@bucket_name}/#{@object_key}"
+  end
+
+  def cleanup
+    File.delete(@db_dump_path) if File.exist?(@db_dump_path)
+    Rails.logger.info "Local backup file deleted."
+  rescue => e
+    Rails.logger.error "Failed to delete local backup file: #{e.message}"
   end
 end
