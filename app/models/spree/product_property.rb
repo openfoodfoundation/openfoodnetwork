521e24d7a62569958f9b2c57c50722829119a174
cyrillefr <cyrille.vasseur@gmail.com>
2024-04-30 09:58:24 +0200

    Requested changes on Fix RedundantPresenceValidationOnBelongs II
     - product id required
     - DB + AR model

--- app/models/spree/product_property.rb
+++ app/models/spree/product_property.rb
@@ -2,7 +2,7 @@
 
 module Spree
   class ProductProperty < ApplicationRecord
-    belongs_to :product, class_name: "Spree::Product", touch: true, optional: true
+    belongs_to :product, class_name: "Spree::Product", touch: true
     belongs_to :property, class_name: 'Spree::Property'
 
     validates :value, length: { maximum: 255 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_24_121221) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_30_075133) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
@@ -674,7 +674,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_24_121221) do
 
   create_table "spree_product_properties", id: :serial, force: :cascade do |t|
     t.string "value", limit: 255
-    t.integer "product_id"
+    t.integer "product_id", null: false
     t.integer "property_id", null: false
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
