bf56a8d33027f84c2ba4f792a1c16a925a1b428e
David Cook <david@redcliffs.net>
2024-05-08 09:35:15 +1000

    Check if assets need compiling before all examples in top-level group

--- spec/support/precompile_assets.rb
+++ spec/support/precompile_assets.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.configure do |config|
-  config.before(:each) do
+  config.before(:all) do
     # We can use webpack-dev-server for tests, too!
     # Useful if you working on a frontend code fixes and want to verify them via system tests.
     next if Webpacker.dev_server.running?
