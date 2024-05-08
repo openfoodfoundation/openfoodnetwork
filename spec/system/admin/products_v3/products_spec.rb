b5fe19cb7d897f31844c0e08eac05a1b82916927
Ahmed Ejaz <chahmedejaz9050@gmail.com>
2024-05-08 13:47:47 +0500

    11058: add specs

--- spec/system/admin/products_v3/products_spec.rb
+++ spec/system/admin/products_v3/products_spec.rb
@@ -26,18 +26,41 @@ describe 'As an enterprise user, I can manage my products', feature: :admin_styl
   describe "sorting" do
     let!(:product_b) { create(:simple_product, name: "Bananas") }
     let!(:product_a) { create(:simple_product, name: "Apples") }
+    let(:products_table) { "table.products" }
 
     before do
       visit admin_products_url
     end
 
-    it "Should sort products alphabetically by default" do
-      within "table.products" do
+    it "Should sort products alphabetically by default in ascending order" do
+      within products_table do
         # Gather input values, because page.content doesn't include them.
         input_content = page.find_all('input[type=text]').map(&:value).join
 
         # Products are in correct order.
         expect(input_content).to match /Apples.*Bananas/
+      end
+    end
+
+    context "when clicked on 'Name' column header" do
+      it "Should sort products alphabetically in descending/ascending order" do
+        within products_table do
+          name_header = page.find('th > a[data-column="name"]')
+
+          # Sort in descending order
+          name_header.click
+          expect(page).to have_content("Name ▼") # this indicates the re-sorted content has loaded
+
+          input_content = page.find_all('input[type=text]').map(&:value).join
+          expect(input_content).to match /Bananas.*Apples/
+
+          # Sort in ascending order
+          name_header.click
+          expect(page).to have_content("Name ▲") # this indicates the re-sorted content has loaded
+
+          input_content = page.find_all('input[type=text]').map(&:value).join
+          expect(input_content).to match /Apples.*Bananas/
+        end
       end
     end
   end
