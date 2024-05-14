343232fa6a43609a9f9ded1f910623e5539e52f8
labelleorange <fruits@labelleorange.es>
2024-05-14 13:03:01 +0500

    Grouped shops implementation

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -4,6 +4,27 @@ class ShopsController < BaseController
   layout 'darkswarm'
 
   def index
-    @enterprises = ShopsListService.new.open_shops
+    user_enterprises = spree_current_user.enterprises
+
+    @enterprises = Set.new
+
+    user_enterprises.each do |enterprise|
+      # Find all groups of the current enterprise
+      groups = enterprise.groups
+
+      # For each group associated with the current enterprise
+      groups.each do |group|
+        # Fetch all enterprises in that group with public visibility
+        enterprises = group.enterprises.where(visible: 'public')
+
+        # If the user's enterprise is private, include only public enterprises
+        if enterprise.visible == 'private'
+          @enterprises.merge(enterprises)
+        else
+          @enterprises.merge(group.enterprises)
+        end
+      end
+    end
+    @enterprises.merge(ShopsListService.new.open_shops)
   end
 end
