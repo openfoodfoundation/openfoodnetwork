1c6d10d4e78dabf28d1dfa2288d70af95da09a2f
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2024-05-22 20:31:54 +1000

    Remove supplier_id from simple product serializer

--- app/serializers/api/admin/product_simple_serializer.rb
+++ app/serializers/api/admin/product_simple_serializer.rb
@@ -3,13 +3,9 @@
 module Api
   module Admin
     class ProductSimpleSerializer < ActiveModel::Serializer
-      attributes :id, :name, :producer_id
+      attributes :id, :name
 
       has_many :variants, key: :variants, serializer: Api::Admin::VariantSimpleSerializer
-
-      def producer_id
-        object.supplier_id
-      end
     end
   end
 end
