22bd25c5873f2f29740380645ec23e9d9a34d4da
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2024-05-22 22:07:22 +1000

    Fix enterprise touching when associated record updated

--- app/models/spree/property.rb
+++ app/models/spree/property.rb
@@ -6,12 +6,20 @@ module Spree
     has_many :products, through: :product_properties
     has_many :producer_properties, dependent: :destroy
 
+    after_touch :touch_producer_properties
+
     validates :name, :presentation, presence: true
 
     scope :sorted, -> { order(:name) }
 
     def property
       self
+    end
+
+    private
+
+    def touch_producer_properties
+      producer_properties.each(&:touch)
     end
   end
 end
