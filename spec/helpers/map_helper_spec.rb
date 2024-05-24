850385a8d377b63ea9b7cf981c9d69003c9249e9
Cillian O'Ruanaidh <contact@cilliano.com>
2024-05-24 11:14:43 +0100

    Add tests to make sure the appropriate map is loaded e.g. Open Street Map or Google

---
+++ spec/helpers/map_helper_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: false
+
+require 'spec_helper'
+
+RSpec.describe MapHelper, type: :helper do
+  describe "#using_google_maps?" do
+    it "returns true when a GOOGLE_MAPS_API_KEY is present" do
+      stub_environment_variable("GOOGLE_MAPS_API_KEY", "ABC")
+
+      expect(helper.using_google_maps?).to eq true
+    end
+
+    it "returns false if Open Street Map is enabled, even if a GOOGLE_MAPS_API_KEY is present" do
+      stub_environment_variable("GOOGLE_MAPS_API_KEY", "ABC")
+      ContentConfig.open_street_map_enabled = true
+
+      expect(helper.using_google_maps?).to eq false
+    end
+  end
+
+  private
+
+  def stub_environment_variable(key, value)
+    allow(ENV).to receive(:[]).and_call_original # Allow non-stubbed calls to ENV to proceed
+    allow(ENV).to receive(:[]).with(key).and_return(value)
+  end
+end
