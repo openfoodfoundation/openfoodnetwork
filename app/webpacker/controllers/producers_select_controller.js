dab49f3e894f95295177693aa3da6628b6205943
cyrillefr <cyrille.vasseur@gmail.com>
2024-05-28 13:48:28 +0200

    Requested changes on Producers selects are to be populated on focus
    - comments
    - id of template as a parameter
    - little clean-up

--- app/webpacker/controllers/producers_select_controller.js
+++ app/webpacker/controllers/producers_select_controller.js
@@ -3,18 +3,20 @@ import TomSelect from "./tom_select_controller";
 const minimumOptionsForSearchField = 11;
 
 export default class extends TomSelect {
-  static values = { options: Object, placeholder: String };
+  static values = { templateId: String, placeholder: String };
 
-  connect(options = {}) {
-    const template = this.template();
+  connect() {
+    const template = this.template(this.templateIdValue );
     let additional_options = {};
     const lines_of_option = template.content.children.length; 
 
+    // Disable search input ("dropdown_input" plugin) from tom-select
     if (lines_of_option < minimumOptionsForSearchField) {
       additional_options.plugins = [];  
     }
 
     super.connect(additional_options);
+    // focus event available on tom-select , not on html select
     this.control.on('focus', this.fetchProducers.bind(this));
   }
 
@@ -25,13 +27,13 @@ export default class extends TomSelect {
   fetchProducers() {
     if ((Object.keys(this.control.options).length) > 1) return false;
 
-    const template = this.template();
+    const template = this.template(this.templateIdValue );
     Array.from(template.content.children).forEach((option) => {
       this.control.addOption({value: option.value, text: option.text});
     }); 
   }
   
-  template() {
-    return document.querySelector("#producer_options");
+  template(id) {
+    return document.querySelector(`#${id}`);
   }
 }
