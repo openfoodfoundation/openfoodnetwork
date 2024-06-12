d5456a85b76761506fb1ee466a5673a8539fc31c
David Cook <david@redcliffs.net>
2024-06-12 16:58:01 +1000

    Reset cell colspans
    This might be a little simpler if we move the 'new variant' button to col 0, and assume colspan cells always span the whole table.

--- app/webpacker/controllers/column_preferences_controller.js
+++ app/webpacker/controllers/column_preferences_controller.js
@@ -4,8 +4,15 @@ import { Controller } from "stimulus";
 //
 export default class ColumnPreferencesController extends Controller {
   connect() {
-    this.checkboxes = this.element.querySelectorAll("input[type=checkbox]");
+    this.table = document.querySelector('table[data-column-preferences-target="table"]');
+    this.cols = Array.from(this.table.querySelectorAll('col'));
+    this.colSpanCells = this.table.querySelectorAll('th[colspan],td[colspan]');
+    // Initialise data-default-col-span
+    this.colSpanCells.forEach((cell)=> {
+      cell.dataset.defaultColSpan ||= cell.colSpan;
+    });
 
+    this.checkboxes = this.element.querySelectorAll("input[type=checkbox]");
     for (const element of this.checkboxes) {
       // On initial load
       this.#showHideColumn(element);
@@ -20,7 +27,7 @@ export default class ColumnPreferencesController extends Controller {
     const element = e.target || e;
     const name = element.dataset.columnName;
     const selector = `col[data-column-preferences-name="${name}"]`;
-    const column = document.querySelector(selector);
+    const column = this.table.querySelector(selector);
     const index = this.#getIndex(column);
 
     if (column == null) {
@@ -32,7 +39,7 @@ export default class ColumnPreferencesController extends Controller {
     this.#showHideElement(column, element.checked);
 
     // Hide each cell in column (ignore rows with colspan)
-    const rows = column.closest("table").querySelectorAll("tr:not(:has(td[colspan]))");
+    const rows = this.table.querySelectorAll("tr:not(:has(td[colspan]))");
     rows.forEach((row) => {
       // Ignore cell if spanning multiple columns
       const cell = row.children[index];
@@ -40,6 +47,13 @@ export default class ColumnPreferencesController extends Controller {
 
       this.#showHideElement(cell, element.checked);
     });
+
+    // Reset cell colspans
+    const hiddenColCount = this.cols.filter((col)=> col.style.display == 'none').length;
+    for(const cell of this.colSpanCells) {
+      const span = parseInt(cell.dataset.defaultColSpan, 10) - hiddenColCount;
+      cell.colSpan = span;
+    };
   }
 
   #getIndex(column) {
