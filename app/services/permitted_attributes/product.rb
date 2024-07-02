76b21ff3f86b67785ffef3dc7aecaca5fe55ec64
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2024-07-02 15:53:10 +1000

    Fix API v0 variants controller spec

--- app/services/permitted_attributes/product.rb
+++ app/services/permitted_attributes/product.rb
@@ -5,6 +5,8 @@ module PermittedAttributes
     def self.attributes
       [
         :id, :name, :description, :price,
+        :variant_unit, :variant_unit_scale, :variant_unit_with_scale, :unit_value,
+        :unit_description, :variant_unit_name,
         :display_as, :sku, :group_buy, :group_buy_unit_size,
         :taxon_ids, :primary_taxon_id, :tax_category_id, :supplier_id,
         :meta_keywords, :notes, :inherits_properties, :shipping_category_id,
