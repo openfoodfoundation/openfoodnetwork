35e124fdfb558123457ef67ada9f19e87cb96a8d
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2024-07-10 16:37:47 +1000

    Update jest configuration to include webpacker dir
    This allows the test environment to correctly resolve import of
    services in controller ie: `import OptionValueNamer from
    "js/services/option_value_namer";`
    
    The added benefit is we can now import package to be tested directly
    without having to specify the whole relative path ie in test file you
    can do : `import variant_controller from "controllers/variant_controller";`

--- jest.config.js
+++ jest.config.js
@@ -66,9 +66,7 @@ module.exports = {
   // maxWorkers: "50%",
 
   // An array of directory names to be searched recursively up from the requiring module's location
-  // moduleDirectories: [
-  //   "node_modules"
-  // ],
+  moduleDirectories: ["node_modules", "app/webpacker"],
 
   // An array of file extensions your modules use
   // moduleFileExtensions: [
@@ -157,9 +155,7 @@ module.exports = {
   // ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
-  "testRegex": [
-    "spec/javascripts/.*_test\\.js"
-  ],
+  testRegex: ["spec/javascripts/.*_test\\.js"],
 
   // This option allows the use of a custom results processor
   // testResultsProcessor: undefined,
@@ -177,9 +173,7 @@ module.exports = {
   // transform: undefined,
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  "transformIgnorePatterns": [
-    "/node_modules/(?!(stimulus)/)"
-  ]
+  transformIgnorePatterns: ["/node_modules/(?!(stimulus)/)"],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
