09869714739ec364e4f76f497fc03f42886834cc
Ahmed Ejaz <chahmedejaz9050@gmail.com>
2024-07-12 17:00:42 +0500

    12570 - fix variant's display_as field being empty issue - New variant unit_value is empty, so +VariantUnits::OptionValueNamer.new(variant).name+ returns "" - Now we are making sure that new variant unit_value won't be empty

--- app/helpers/admin/products_helper.rb
+++ app/helpers/admin/products_helper.rb
@@ -9,5 +9,23 @@ module Admin
         new_admin_product_image_path(product.id)
       end
     end
+
+    def prepare_new_variant(product)
+      product.variants.build do |variant|
+        variant.unit_value = 1.0 * product.variant_unit_scale
+        variant.unit_presentation = VariantUnits::OptionValueNamer.new(variant).name
+      end
+    end
+
+    def unit_value_with_description(variant)
+      scaled_unit_value = variant.unit_value / (variant.product.variant_unit_scale || 1)
+      precised_unit_value = number_with_precision(
+        scaled_unit_value,
+        precision: nil,
+        strip_insignificant_zeros: true
+      )
+
+      [precised_unit_value, variant.unit_description].compact_blank.join(" ")
+    end
   end
 end
