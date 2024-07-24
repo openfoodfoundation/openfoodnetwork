27916dd0a6962832a1b394d1061cc737bd699e6f
wandji20 <wbertrand10@gmail.com>
2024-07-24 20:06:25 +0100

    Revert front end validation and implement backend validation for changes in datetime order cycle values [OFN-11613]

--- spec/system/admin/order_cycles/edit_spec.rb
+++ spec/system/admin/order_cycles/edit_spec.rb
@@ -10,29 +10,19 @@ RSpec.describe '
   include AuthenticationHelper
   include WebHelper
 
-  let(:oc0) {
-    create(:simple_order_cycle, name: 'oc0',
-                                orders_open_at: nil, orders_close_at: nil)
-  }
-  let(:oc1) { create(:order_cycle, name: 'oc1') }
+  context 'when cycle has attached order(s)' do
+    let(:order) { create(:order_without_full_payment) }
 
-  context 'when cycle has attached schedule(s)' do
-    it "properly toggles order cycle save bar buttons to show warning modal" do
-      create(:schedule, name: 'Schedule1', order_cycles: [oc0])
-
+    it "show warning modal when datetime field values change" do
       # When I go to the admin order cycle edit page
       login_as_admin
-      visit edit_admin_order_cycle_path(oc0)
-
-      expect(page).to have_selector("#linked-schedule-warning-modal")
-      expect(page).not_to have_selector("#modal-actions")
-      expect(page).to have_selector("#form-actions")
+      visit edit_admin_order_cycle_path(order.order_cycle)
 
       # change non-date range field
-      fill_in 'order_cycle_name', with: "OC0 name updated"
+      fill_in 'order_cycle_name', with: "Order cycle name updated"
       expect(page).to have_content('You have unsaved changes')
       click_button('Save')
-      expect(page).not_to have_selector('#linked-schedule-warning-modal .reveal-modal.in')
+      expect(page).not_to have_content "Orders are linked to this order cycle"
       expect(page).to have_content('Your order cycle has been updated.')
 
       # change date range field value
@@ -40,33 +30,32 @@ RSpec.describe '
       find('#order_cycle_orders_close_at').click
       select_datetime_from_datepicker Time.zone.at(time)
 
-      # Enable savebar save buttons to open warning modal
       expect(page.find('#order_cycle_orders_close_at').value).to eq time.strftime('%Y-%m-%d %H:%M')
-      expect(page).not_to have_selector("#form-actions")
-      expect(page).to have_selector("#modal-actions")
       expect(page).to have_content('You have unsaved changes')
-      expect(page).not_to have_selector('#linked-schedule-warning-modal .reveal-modal.in')
 
       # click save to open warning modal
       click_button('Save')
-      expect(page).to have_selector('#linked-schedule-warning-modal .reveal-modal.in')
+      expect(page).to have_content('You have unsaved changes')
+      expect(page).to have_content "Orders are linked to this order cycle."
 
       # confirm to close modal and update order cycle changed fields
       click_button('Proceed anyway')
-      expect(page).not_to have_selector('#linked-schedule-warning-modal .reveal-modal.in')
+      expect(page).not_to have_content "Orders are linked to this cycle"
+      expect(page).to have_content('Your order cycle has been updated.')
       expect(page.find('#order_cycle_orders_close_at').value).to eq time.strftime('%Y-%m-%d %H:%M')
     end
   end
 
   context 'when cycle does not have attached schedule' do
+    let(:order_cycle) {
+      create(:simple_order_cycle, name: 'My Order cycle',
+                                  orders_open_at: nil, orders_close_at: nil)
+    }
+
     it "does not render warning modal" do
       # When I go to the admin order cycle edit page
       login_as_admin
-      visit edit_admin_order_cycle_path(oc1)
-
-      expect(page).not_to have_selector("#linked-schedule-warning-modal")
-      expect(page).not_to have_selector("#modal-actions")
-      expect(page).to have_selector("#form-actions")
+      visit edit_admin_order_cycle_path(order_cycle)
 
       # change non-date range field value
       fill_in 'order_cycle_name', with: "OC1 name updated"
@@ -74,17 +63,17 @@ RSpec.describe '
 
       # click save
       click_button('Save')
-      expect(page).not_to have_selector('#linked-schedule-warning-modal .reveal-modal.in')
+      expect(page.find('#order_cycle_name').value).to eq 'OC1 name updated'
       expect(page).to have_content('Your order cycle has been updated.')
 
-      # change date range field value
+      # Now change date range field value
       time = DateTime.current
       find('#order_cycle_orders_close_at').click
       select_datetime_from_datepicker Time.zone.at(time)
       expect(page).to have_content('You have unsaved changes')
 
       click_button('Save')
-      expect(page).not_to have_selector("#modal-actions")
+      expect(page.find('#order_cycle_orders_close_at').value).to eq time.strftime('%Y-%m-%d %H:%M')
       expect(page).to have_content('Your order cycle has been updated.')
     end
   end
