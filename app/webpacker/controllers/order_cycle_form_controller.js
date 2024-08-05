f322d53a60c1bdd72f378876aa00d24f1e6b80df
wandji20 <wbertrand10@gmail.com>
2024-08-06 00:42:33 +0100

    Refactor and update order cycle form controller [OFN-11613]

--- app/webpacker/controllers/order_cycle_form_controller.js
+++ app/webpacker/controllers/order_cycle_form_controller.js
@@ -1,19 +1,20 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ['element']
+  static targets = ['statusMessage']
   connect() {
+    console.log(this.statusMessageTarget)
     this.observer = new MutationObserver(this.updateCallback);
     this.observer.observe(
-      this.elementTarget,
+      this.statusMessageTarget,
       { attributes: true, attributeOldValue: true, attributeFilter: ['data-type'] }
     );
   }
 
   // Callback to trigger warning modal
   updateCallback(mutationsList) {
-    const newDataType =  $('#status-message').attr('data-type');
-    const actionName =  $('#status-message').attr('data-action-name');
+    const newDataType = document.getElementById('status-message').getAttribute('data-type');
+    const actionName = document.getElementById('status-message').getAttribute('data-action-name');
     if(!actionName) return;
 
     for(let mutation of mutationsList) {
@@ -21,10 +22,18 @@ export default class extends Controller {
         // Only trigger warning modal when notice display (notice) is preceeded by progress display (progress)
         if(mutation.oldValue === 'progress' && newDataType === 'notice') {
           // Hide all confirmation buttons in warning modal
-          $('#linked-order-warning-modal .modal-actions button.secondary').css({ display: 'none' })
+          document.querySelectorAll(
+            '#linked-order-warning-modal .modal-actions button.secondary'
+          ).forEach((node) => {
+            node.style.display = 'none';
+          });
           // Show the appropriate confirmation button, open warning modal, and return
-          $(`#linked-order-warning-modal button[data-trigger-action=${actionName}]`).css({ display: 'block' });
-          $('.warning-modal button.modal-target-trigger').trigger('click');
+          document.querySelectorAll(
+            `#linked-order-warning-modal button[data-trigger-action=${actionName}]`
+          ).forEach((node) => {
+            node.style.display = 'block';
+          })
+          document.querySelector('.warning-modal button.modal-target-trigger').click();
         }
       }
     }
