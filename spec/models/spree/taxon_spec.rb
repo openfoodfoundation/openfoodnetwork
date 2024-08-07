c39dee3617d4b48460efe93abccc1e3c220199a8
wandji20 <wbertrand10@gmail.com>
2024-08-07 19:35:24 +0100

    Remove awesome nested set gem and dependencies [OFN-11636]

--- spec/models/spree/taxon_spec.rb
+++ spec/models/spree/taxon_spec.rb
@@ -63,41 +63,6 @@ module Spree
         taxon.set_permalink
         expect(taxon.permalink).to eq 'ni-hao'
       end
-
-      context "with parent taxon" do
-        before do
-          allow(taxon).to receive_messages parent_id: 123
-          allow(taxon).to receive_messages parent: build_stubbed(:taxon, permalink: "brands")
-        end
-
-        it "should set permalink correctly when taxon has parent" do
-          taxon.set_permalink
-          expect(taxon.permalink).to eq "brands/ruby-on-rails"
-        end
-
-        it "should set permalink correctly with existing permalink present" do
-          taxon.permalink = "b/rubyonrails"
-          taxon.set_permalink
-          expect(taxon.permalink).to eq "brands/rubyonrails"
-        end
-
-        it "should support Chinese characters" do
-          taxon.name = "我"
-          taxon.set_permalink
-          expect(taxon.permalink).to eq "brands/wo"
-        end
-      end
-    end
-
-    # Regression test for Spree #2620
-    context "creating a child node using first_or_create" do
-      let(:taxonomy) { create(:taxonomy) }
-
-      it "does not error out" do
-        expect {
-          taxonomy.root.children.where(name: "Some name").first_or_create
-        }.not_to raise_error
-      end
     end
   end
 end
