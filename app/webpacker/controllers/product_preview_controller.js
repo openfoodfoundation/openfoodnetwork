27dd5def575325743838cd42d0cdc2eb9c5bb611
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2024-08-28 11:47:35 +1000

    Open modal  before rendering the received html
    This way we don't see a blank modal waiting for the content to load

--- app/webpacker/controllers/product_preview_controller.js
+++ app/webpacker/controllers/product_preview_controller.js
@@ -1,7 +1,19 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  open() {
+  connect() {
+    // open the modal before rendering the html to avoid opening a blank modal
+    // TODO other option is having a controller in the stream html that would dispatch the open element on connect
+    window.addEventListener("turbo:before-stream-render", this.#open.bind(this));
+  }
+
+  disconnect() {
+    window.removeEventListener("turbo:before-stream-render", this.#open.bind(this));
+  }
+
+  // private
+
+  #open() {
     // dispatch "product-preview:open" event to trigger modal->open action
     // see views/admin/product_v3/index.html.haml
     this.dispatch("open");
