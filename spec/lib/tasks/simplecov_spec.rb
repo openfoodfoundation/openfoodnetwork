a696c66857d530e22226645d08a8f6535a50462f
Maikel Linke <maikel@email.org.au>
2024-08-28 11:27:42 +1000

    Clean up tmp dir after test and avoid collisions
    Best viewed ignoring whitespace changes.

--- spec/lib/tasks/simplecov_spec.rb
+++ spec/lib/tasks/simplecov_spec.rb
@@ -4,8 +4,6 @@ require 'spec_helper'
 require 'rake'
 
 RSpec.describe "simplecov.rake" do
-  let(:output_dir) { "tmp/test_#{Process.pid}_coverage" }
-
   before(:all) do
     Rake.application.rake_require("tasks/simplecov")
   end
@@ -15,15 +13,21 @@ RSpec.describe "simplecov.rake" do
       let(:input_dir) { Rails.root.join("spec/fixtures/simplecov") }
 
       it "creates a new combined report" do
-        expect {
-          Rake.application.invoke_task "simplecov:collate_results[#{input_dir},#{output_dir}]"
-        }.to change { Dir.exist? output_dir }.
-          from(false).
-          to(true).
+        Dir.mktmpdir do |tmp_dir|
+          output_dir = File.join(tmp_dir, "output")
 
-          and change { File.exist? File.join(output_dir, "index.html") }.
-          from(false).
-          to(true)
+          expect {
+            Rake.application.invoke_task(
+              "simplecov:collate_results[#{input_dir},#{output_dir}]"
+            )
+          }.to change { Dir.exist?(output_dir) }.
+            from(false).
+            to(true).
+
+            and change { File.exist?(File.join(output_dir, "index.html")) }.
+            from(false).
+            to(true)
+        end
       end
     end
   end
