3e7f61c4d11553534754a76debc97ac6b586fb56
Maikel Linke <maikel@email.org.au>
2024-09-05 16:26:10 +1000

    Add new DFC vocabulary
    So that we can use order states programmatically.

--- engines/dfc_provider/app/services/dfc_loader.rb
+++ engines/dfc_provider/app/services/dfc_loader.rb
@@ -2,15 +2,24 @@
 
 class DfcLoader
   def self.connector
-    @connector ||= load_vocabularies
+    unless @connector
+      @connector = DataFoodConsortium::Connector::Connector.instance
+      load_vocabularies
+    end
+
+    @connector
+  end
+
+  def self.vocabulary(name)
+    @vocabs ||= {}
+    @vocabs[name] ||= connector.__send__(:loadThesaurus, read_file(name))
   end
 
   def self.load_vocabularies
-    connector = DataFoodConsortium::Connector::Connector.instance
     connector.loadMeasures(read_file("measures"))
     connector.loadFacets(read_file("facets"))
     connector.loadProductTypes(read_file("productTypes"))
-    connector
+    vocabulary("vocabulary") # order states etc
   end
 
   def self.read_file(name)
