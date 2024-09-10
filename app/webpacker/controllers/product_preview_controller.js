379e5acfe5e7a81abac22dd1b5c1aadd3d42b858
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2024-09-10 10:23:22 +1000

    Fix product preview modal opening
    The previous solution failed to take into account that it would have been
    trigger on any turbo steam rendering action, not just the product preview
    one. Now the open event is dispatched when the product preview
    controller is connected, which happens when the modal html is rendered.

--- app/webpacker/controllers/product_preview_controller.js
+++ app/webpacker/controllers/product_preview_controller.js
@@ -2,13 +2,8 @@ import { Controller } from "stimulus";
 
 export default class extends Controller {
   connect() {
-    // open the modal before rendering the html to avoid opening a blank modal
-    // TODO other option is having a controller in the stream html that would dispatch the open element on connect
-    window.addEventListener("turbo:before-stream-render", this.#open.bind(this));
-  }
-
-  disconnect() {
-    window.removeEventListener("turbo:before-stream-render", this.#open.bind(this));
+    // open the modal when html is rendering to avoid opening a blank modal
+    this.#open()
   }
 
   // private
