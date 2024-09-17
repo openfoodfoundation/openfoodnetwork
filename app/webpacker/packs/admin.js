c2e0c94f2e57418d33a72bd597957ac790819848
wandji20 <wbertrand10@gmail.com>
2024-09-17 11:56:07 +0100

    Remove unused debounced plugin

--- app/webpacker/packs/admin.js
+++ app/webpacker/packs/admin.js
@@ -9,9 +9,6 @@ import "../js/moment";
 import bigDecimal from "js-big-decimal";
 window.bigDecimal = bigDecimal;
 
-import debounced from "debounced";
-debounced.initialize(['input'], { wait: 300 });
-
 import Trix from "trix";
 
 document.addEventListener("trix-file-accept", (event) => {
