a8db288425cb426798bacf9f6084d3cf77e5c38b
wandji20 <wbertrand10@gmail.com>
2024-09-17 01:56:44 +0100

    Improve debounced initialised events

--- app/webpacker/packs/admin.js
+++ app/webpacker/packs/admin.js
@@ -10,7 +10,7 @@ import bigDecimal from "js-big-decimal";
 window.bigDecimal = bigDecimal;
 
 import debounced from "debounced";
-debounced.initialize({ input: { wait: 300 } });
+debounced.initialize(['input'], { wait: 300 });
 
 import Trix from "trix";
 
