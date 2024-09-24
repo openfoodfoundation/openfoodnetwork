0824430da5e31b9f2b21af862c51640da5d328d5
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2024-09-24 10:43:55 +1000

    Add Vine connected app
    The connection/disconnection logic is yet to be implemented

---
+++ app/models/connected_apps/vine.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# An enterprise can opt-in to use VINE API to manage vouchers
+#
+module ConnectedApps
+  class Vine < ConnectedApp
+    def connect(api_key: )
+      # TODO
+    end
+
+    def disconnect
+      # TODO
+    end
+  end
+end
