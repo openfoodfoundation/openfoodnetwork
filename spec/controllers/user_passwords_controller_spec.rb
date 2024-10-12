1af298555b2ad3f3ee9c92b9cfa0c36de774636a
wandji20 <wbertrand10@gmail.com>
2024-10-13 00:59:16 +0100

    Handle user password responses with turbo_stream

--- spec/controllers/user_passwords_controller_spec.rb
+++ spec/controllers/user_passwords_controller_spec.rb
@@ -14,20 +14,20 @@ RSpec.describe UserPasswordsController, type: :controller do
 
   describe "create" do
     it "returns 404 if user is not found" do
-      spree_post :create, spree_user: { email: "xxxxxxxxxx@example.com" }
+      spree_post :create, spree_user: { email: "xxxxxxxxxx@example.com" }, format: :turbo_stream
       expect(response.status).to eq 404
       expect(response.body).to match 'Email address not found'
     end
 
     it "returns 422 if user is registered but not confirmed" do
-      spree_post :create, spree_user: { email: unconfirmed_user.email }
+      spree_post :create, spree_user: { email: unconfirmed_user.email }, format: :turbo_stream
       expect(response.status).to eq 422
       expect(response.body).to match "You must confirm your email \
 address before you can reset your password."
     end
 
     it "returns 200 when password reset was successful" do
-      spree_post :create, spree_user: { email: user.email }
+      spree_post :create, spree_user: { email: user.email }, format: :turbo_stream
       expect(response.status).to eq 200
       expect(response.body).to match "An email with instructions on resetting \
 your password has been sent!"
