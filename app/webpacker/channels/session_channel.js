085cd8b56bb7511fbd37e40c790522fc20cc003e
wandji20 <wbertrand10@gmail.com>
2024-10-15 03:13:47 +0100

    Replace bulk invoice CableReady broadcast with ActionCable

--- app/webpacker/channels/session_channel.js
+++ app/webpacker/channels/session_channel.js
@@ -1,8 +1,9 @@
 import consumer from './consumer'
-import CableReady from 'cable_ready'
 
 consumer.subscriptions.create("SessionChannel", {
   received(data) {
-    if (data.cableReady) CableReady.perform(data.operations)
+    if (!data.selector) return;
+
+    document.querySelector(data.selector).innerHTML = data.html;
   }
 });
