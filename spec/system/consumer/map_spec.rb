ea1002f0681798c5ccc743919f86b87487b56f37
Ryan Murphy <murjax@gmail.com>
2024-11-06 08:39:35 -0500

    8230 - Map spec update

--- spec/system/consumer/map_spec.rb
+++ spec/system/consumer/map_spec.rb
@@ -19,9 +19,10 @@ RSpec.describe 'Map' do
   context 'map cannot load' do
     it 'shows alert' do
       message = accept_alert { visit '/map' }
-      expect(message).to eq("Unable to load map.
-      Please check your browser settings
-      and allow 3rd party cookies for this website.".squish)
+      expect(message).to eq(
+        "Unable to load map. Please check your browser " \
+        "settings and allow 3rd party cookies for this website."
+      )
     end
   end
 end
