a3f4f0dcf2a064d802b2a9cd0af7f08b2189a946
bouaik <bouaik.lhou@gmail.com>
2024-11-09 14:33:58 +0100

    fix incorrect rounding of final weight volume

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_23_054951) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_01_231144) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
@@ -553,7 +553,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_23_054951) do
     t.integer "max_quantity"
     t.string "currency", limit: 255
     t.decimal "distribution_fee", precision: 10, scale: 2
-    t.decimal "final_weight_volume", precision: 10, scale: 2
+    t.decimal "final_weight_volume", precision: 10, scale: 3
     t.integer "tax_category_id"
     t.decimal "weight", precision: 8, scale: 2
     t.decimal "height", precision: 8, scale: 2
