45a92338e30b611cf6abd8fe8dc3cfb97621b0b4
Maikel Linke <maikel@email.org.au>
2024-11-14 16:08:55 +1100

    Add simpler Bugsnag wrapper

---
+++ app/services/alert.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# A handy wrapper around error reporting libraries like Bugsnag.
+#
+# Bugsnag's API is great for general purpose but overly complex for our use.
+# It also changes over time and we often make mistakes using it. So this class
+# aims at:
+#
+# * Abstracting from Bugsnag, open for other services.
+# * Simpler interface to reduce user error.
+# * Central place to update Bugsnag API usage when it changes.
+#
+class Alert
+  # Alert Bugsnag with additional metadata to appear in tabs.
+  #
+  #   Alert.raise(
+  #     "Invalid order during checkout",
+  #     {
+  #       order: { number: "ABC123", state: "awaiting_return" },
+  #       env: { referer: "example.com" }
+  #     }
+  #   )
+  def self.raise(error, metadata, &block)
+    Bugsnag.notify(error) do |payload|
+      metadata.each do |name, data|
+        payload.add_metadata(name, data)
+      end
+      block.call(payload)
+    end
+  end
+end
