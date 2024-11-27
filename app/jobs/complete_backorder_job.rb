543e622dc89a2576575b702e914a160af3542ac4
Maikel Linke <maikel@email.org.au>
2024-11-27 15:04:26 +1100

    Re-use BackorderUpdater to complete backorder

--- app/jobs/complete_backorder_job.rb
+++ app/jobs/complete_backorder_job.rb
@@ -24,8 +24,7 @@ class CompleteBackorderJob < ApplicationJob
 
     urls = FdcUrlBuilder.new(order.lines[0].offer.offeredItem.semanticId)
 
-    variants = order_cycle.variants_distributed_by(distributor)
-    adjust_quantities(order_cycle, user, order, urls, variants)
+    BackorderUpdater.new.update(order, user, distributor, order_cycle)
 
     FdcBackorderer.new(user, urls).complete_order(order)
 
@@ -35,56 +34,5 @@ class CompleteBackorderJob < ApplicationJob
     BackorderMailer.backorder_incomplete(user, distributor, order_cycle, order_id).deliver_later
 
     raise
-  end
-
-  # Check if we have enough stock to reduce the backorder.
-  #
-  # Our local stock can increase when users cancel their orders.
-  # But stock levels could also have been adjusted manually. So we review all
-  # quantities before finalising the order.
-  def adjust_quantities(order_cycle, user, order, urls, variants)
-    broker = FdcOfferBroker.new(user, urls)
-
-    order.lines.each do |line|
-      line.quantity = line.quantity.to_i
-      wholesale_product_id = line.offer.offeredItem.semanticId
-      transformation = broker.wholesale_to_retail(wholesale_product_id)
-      linked_variant = variants.linked_to(transformation.retail_product_id)
-
-      # Assumption: If a transformation is present then we only sell the retail
-      # variant. If that can't be found, it was deleted and we'll ignore that
-      # for now.
-      next if linked_variant.nil?
-
-      # Find all line items for this order cycle
-      # Update quantity accordingly
-      if linked_variant.on_demand
-        release_superfluous_stock(line, linked_variant, transformation)
-      else
-        aggregate_final_quantities(order_cycle, line, linked_variant, transformation)
-      end
-    end
-
-    # Clean up empty lines:
-    order.lines.reject! { |line| line.quantity.zero? }
-  end
-
-  def release_superfluous_stock(line, linked_variant, transformation)
-    # Note that a division of integers dismisses the remainder, like `floor`:
-    wholesale_items_contained_in_stock = linked_variant.on_hand / transformation.factor
-
-    # But maybe we didn't actually order that much:
-    deductable_quantity = [line.quantity, wholesale_items_contained_in_stock].min
-    line.quantity -= deductable_quantity
-
-    retail_stock_changes = deductable_quantity * transformation.factor
-    linked_variant.on_hand -= retail_stock_changes
-  end
-
-  def aggregate_final_quantities(order_cycle, line, variant, transformation)
-    orders = order_cycle.orders.invoiceable
-    quantity = Spree::LineItem.where(order: orders, variant:).sum(:quantity)
-    wholesale_quantity = (quantity.to_f / transformation.factor).ceil
-    line.quantity = wholesale_quantity
   end
 end
