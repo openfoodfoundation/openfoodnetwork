60b62d41d6acbc584fe77ec8ab77341cbfb01513
Maikel Linke <maikel@email.org.au>
2024-12-17 13:47:32 +1100

    Remove old, replaced backorder lookup

--- spec/services/fdc_backorderer_spec.rb
+++ spec/services/fdc_backorderer_spec.rb
@@ -23,8 +23,6 @@ RSpec.describe FdcBackorderer do
 
   it "creates, finds and updates orders", vcr: true do
     # This test case contains a full order life cycle.
-    # It assumes that there's no open order yet to start with.
-    # After closing the order at the end, the test can be repeated live again.
 
     # Build a new order when no open one is found:
     order.order_cycle = create(:order_cycle, distributors: [order.distributor])
@@ -44,13 +42,7 @@ RSpec.describe FdcBackorderer do
     # That process seems to be async.
     sleep 10 if VCR.current_cassette.recording?
 
-    # Now we can find the open order:
-    found_backorder = subject.find_or_build_order(order)
-    expect(found_backorder.semanticId).to eq placed_order.semanticId
-    expect(found_backorder.lines.count).to eq 1
-    expect(found_backorder.lines[0].quantity.to_i).to eq 3
-
-    # Without a stored semantic link, it can't look it up directly though:
+    # Without a stored semantic link, it can't look it up:
     found_backorder = subject.lookup_open_order(order)
     expect(found_backorder).to eq nil
 
@@ -58,6 +50,8 @@ RSpec.describe FdcBackorderer do
     order.exchange.semantic_links.create!(semantic_id: placed_order.semanticId)
     found_backorder = subject.lookup_open_order(order)
     expect(found_backorder.semanticId).to eq placed_order.semanticId
+    expect(found_backorder.lines.count).to eq 1
+    expect(found_backorder.lines[0].quantity.to_i).to eq 3
 
     # And close the order again:
     subject.complete_order(placed_order)
@@ -82,28 +76,24 @@ RSpec.describe FdcBackorderer do
     it "add quantity to an existing line item", vcr: true do
       catalog = FdcOfferBroker.load_catalog(order.distributor.owner, urls.catalog_url)
       backorder = subject.find_or_build_order(order)
-      existing_line = backorder.lines[0]
 
-      # The FDC API returns different ids for the same offer.
-      # In order to test that we can still match it, we are retrieving
-      # the catalog offer here which is different to the offer on the
-      # existing order line.
-      ordered_product = existing_line.offer.offeredItem
-      catalog_product = catalog.find do |i|
-        i.semanticId == ordered_product.semanticId
-      end
-      catalog_offer = FdcOfferBroker.new(nil, nil).offer_of(catalog_product)
+      expect(backorder.lines.count).to eq 0
 
-      # The API response is missing this connection:
-      catalog_offer.offeredItem = catalog_product
+      # Add new item to the new order:
+      catalog = FdcOfferBroker.load_catalog(order.distributor.owner, urls.catalog_url)
+      product = catalog.find { |i| i.semanticType == "dfc-b:SuppliedProduct" }
+      offer = FdcOfferBroker.new(nil, nil).offer_of(product)
+      line = subject.find_or_build_order_line(backorder, offer)
 
-      # Just confirm that we got good test data from the API:
-      expect(backorder.semanticId).to match %r{^https.*/[0-9]+$}
       expect(backorder.lines.count).to eq 1
+      expect(backorder.lines[0]).to eq line
 
-      found_line = subject.find_or_build_order_line(backorder, catalog_offer)
+      expect {
+        subject.find_or_build_order_line(backorder, offer)
+      }.not_to change { backorder.lines.count }
 
-      expect(found_line).to eq existing_line
+      found_line = subject.find_or_build_order_line(backorder, offer)
+      expect(found_line).to eq line
     end
   end
 
