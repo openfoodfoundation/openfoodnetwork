698d1daa5732bc3a8cc3715ef2865538ff95f8aa
Maikel Linke <maikel@email.org.au>
2025-01-07 12:45:40 +1100

    Add admin attribute to users

--- db/schema.rb
+++ db/schema.rb
@@ -945,6 +945,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_13_055412) do
     t.string "provider"
     t.string "uid"
     t.datetime "terms_of_service_accepted_at"
+    t.boolean "admin", default: false, null: false
     t.index ["confirmation_token"], name: "index_spree_users_on_confirmation_token", unique: true
     t.index ["email"], name: "email_idx_unique", unique: true
     t.index ["persistence_token"], name: "index_users_on_persistence_token"
