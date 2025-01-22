c27aa00bed57a6a1ce28698578e04a9f77a0372c
Maikel Linke <maikel@email.org.au>
2025-01-22 11:25:58 +1100

    Make old migration independent of app code

--- db/migrate/20190313142051_set_default_stock_location_on_shipments.rb
+++ db/migrate/20190313142051_set_default_stock_location_on_shipments.rb
@@ -1,7 +1,10 @@
 class SetDefaultStockLocationOnShipments < ActiveRecord::Migration[4.2]
+  class SpreeStockLocation < ActiveRecord::Base
+  end
+
   def up
     if Spree::Shipment.where('stock_location_id IS NULL').count > 0
-      location = DefaultStockLocation.find_or_create
+      location = SpreeStockLocation.find_or_create_by(name: "default")
       Spree::Shipment.where('stock_location_id IS NULL').update_all(stock_location_id: location.id)
     end
   end
