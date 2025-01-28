1a393592b4c1fc85352fbfb64f3466352023539a
Maikel Linke <maikel@email.org.au>
2025-01-28 15:47:32 +1100

    Reduce database writes seeding zones

--- db/default/zones.rb
+++ db/default/zones.rb
@@ -1,24 +1,25 @@
 # frozen_string_literal: true
 
 unless Spree::Zone.find_by(name: "EU_VAT")
-  eu_vat = Spree::Zone.create(name: "EU_VAT",
-                              description: "Countries that make up the EU VAT zone.")
+  eu_vat = Spree::Zone.new(
+    name: "EU_VAT", description: "Countries that make up the EU VAT zone."
+  )
 
   ["Poland", "Finland", "Portugal", "Romania", "Germany", "France",
    "Slovakia", "Hungary", "Slovenia", "Ireland", "Austria", "Spain",
    "Italy", "Belgium", "Sweden", "Latvia", "Bulgaria", "United Kingdom",
    "Lithuania", "Cyprus", "Luxembourg", "Malta", "Denmark", "Netherlands",
    "Estonia"].each do |name|
-    eu_vat.zone_members << Spree::ZoneMember.create(zoneable: Spree::Country.find_by!(name:))
+    eu_vat.zone_members.new(zoneable: Spree::Country.find_by!(name:))
   end
   eu_vat.save!
 end
 
 unless Spree::Zone.find_by(name: "North America")
-  north_america = Spree::Zone.create(name: "North America", description: "USA + Canada")
+  north_america = Spree::Zone.new(name: "North America", description: "USA + Canada")
 
   ["United States", "Canada"].each do |name|
-    north_america.zone_members << Spree::ZoneMember.create(zoneable: Spree::Country.find_by!(name:))
+    north_america.zone_members.new(zoneable: Spree::Country.find_by!(name:))
   end
   north_america.save!
 end
