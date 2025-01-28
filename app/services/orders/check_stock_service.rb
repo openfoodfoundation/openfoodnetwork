7d62a7f5ddd9b6401d0763a43cbf846c143da6ae
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2025-01-29 10:55:34 +1100

    Add CheckStockService#update_line_items
    Update line item if any related variant has run out of stock or has had
    stock reduced

--- app/services/orders/check_stock_service.rb
+++ app/services/orders/check_stock_service.rb
@@ -4,12 +4,24 @@ module Orders
   class CheckStockService
     attr_reader :order
 
-    def initialize(order: )
+    def initialize(order:)
       @order = order
     end
 
     def sufficient_stock?
       @sufficient_stock ||= order.insufficient_stock_lines.blank?
+    end
+
+    def update_line_items
+      return [] if sufficient_stock?
+
+      variants = []
+      order.insufficient_stock_lines.each do |line_item|
+        order.contents.update_item(line_item, { quantity: line_item.variant.on_hand })
+        variants.push line_item.variant
+      end
+
+      variants
     end
   end
 end
