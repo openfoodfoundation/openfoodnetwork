69f3f9f62d9115c52a777fb0f32c974494540953
Hartani Yassir <hartaniyassir@gmail.com>
2025-02-03 18:18:38 +0100

    use format.json in UserRegistrationController#create

--- spec/controllers/user_registrations_controller_spec.rb
+++ spec/controllers/user_registrations_controller_spec.rb
@@ -2,13 +2,12 @@
 
 require 'spec_helper'
 
-# rubocop:disable Rails/ResponseParsedBody
 RSpec.describe UserRegistrationsController, type: :controller do
   before do
     @request.env["devise.mapping"] = Devise.mappings[:spree_user]
   end
 
-  describe "via ajax" do
+  describe "create" do
     render_views
 
     let(:user_params) do
@@ -20,9 +19,9 @@ RSpec.describe UserRegistrationsController, type: :controller do
     end
 
     it "returns validation errors" do
-      post :create, xhr: true, params: { spree_user: {}, use_route: :spree }
+      post :create, params: { spree_user: {}, use_route: :spree }, as: :json
       expect(response.status).to eq(401)
-      json = JSON.parse(response.body)
+      json = response.parsed_body
       expect(json).to eq("email" => ["can't be blank"], "password" => ["can't be blank"])
     end
 
@@ -30,10 +29,10 @@ RSpec.describe UserRegistrationsController, type: :controller do
       allow(Spree::UserMailer).to receive(:confirmation_instructions).and_raise("Some error")
       expect(Alert).to receive(:raise)
 
-      post :create, xhr: true, params: { spree_user: user_params, use_route: :spree }
+      post :create, params: { spree_user: user_params, use_route: :spree }, as: :json
 
       expect(response.status).to eq(401)
-      json = JSON.parse(response.body)
+      json = response.parsed_body
       expect(json).to eq(
         "message" =>
         'Something went wrong while creating your account. Check your email address and try again.'
@@ -41,18 +40,17 @@ RSpec.describe UserRegistrationsController, type: :controller do
     end
 
     it "returns 200 when registration succeeds" do
-      post :create, xhr: true, params: { spree_user: user_params, use_route: :spree }
+      post :create, params: { spree_user: user_params, use_route: :spree }, as: :json
       expect(response.status).to eq(200)
-      json = JSON.parse(response.body)
+      json = response.parsed_body
       expect(json).to eq("email" => "test@test.com")
       expect(controller.spree_current_user).to be_nil
     end
 
     it "sets user.locale from cookie on create" do
       cookies[:locale] = "pt"
-      post :create, xhr: true, params: { spree_user: user_params, use_route: :spree }
+      post :create, params: { spree_user: user_params, use_route: :spree }, as: :json
       expect(assigns[:user].locale).to eq("pt")
     end
   end
 end
-# rubocop:enable Rails/ResponseParsedBody
