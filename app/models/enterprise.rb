98251b306e01e4f9ef882af0e2395ecbb62b4d27
François Turbelin <paco@kokali.net>
2025-02-04 23:10:15 +0100

    Improve the no-white-spaces regexp for enterprises

--- app/models/enterprise.rb
+++ app/models/enterprise.rb
@@ -134,7 +134,7 @@ class Enterprise < ApplicationRecord
             }, allow_blank: true
   validate :validate_white_label_logo_link
   validates :external_billing_id,
-            format: { with: /\A\S+\Z/ },
+            format: { with: /^\S+$/ },
             allow_blank: true
 
   before_validation :initialize_permalink, if: lambda { permalink.nil? }
