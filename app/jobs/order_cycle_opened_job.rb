c1a1166f0c5336abd691108043e4e5027c5bbcd5
David Cook <david@redcliffs.net>
2025-02-12 17:20:37 +1100

    [wip] this is how it might work?
    [skip ci]

--- app/jobs/order_cycle_opened_job.rb
+++ app/jobs/order_cycle_opened_job.rb
@@ -5,6 +5,19 @@ class OrderCycleOpenedJob < ApplicationJob
   def perform
     ActiveRecord::Base.transaction do
       recently_opened_order_cycles.find_each do |order_cycle|
+        # Process the order cycle. this might take a while, so this should be shifted to a separate job to allow concurrent processing.        
+
+        # Sync any remote products
+        updated_products = order_cycle.products.joins(:semantic_links).find_each.map do |product|
+          DfcImporter.import(product) # request DFC product from semantic_id and import it. may take some time
+        end
+        # actually, that might not be possible. 
+        # Instead we might use FdcUrlBuilder and load the whole shop catalogue (which is probably more efficient anyway)
+
+        # then notify shop owners. and/or order cycle co-ordinator.
+        OcSyncedMailer.new(to: contacts, order_cycle, updated_products).deliver_later
+
+        # and notify anyone else
         OrderCycles::WebhookService.create_webhook_job(order_cycle, 'order_cycle.opened')
       end
       mark_as_opened(recently_opened_order_cycles)
