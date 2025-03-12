668d51a20b4f34b5df83a478b52cf13bb0cab72b
David Cook <david@redcliffs.net>
2025-03-12 16:31:34 +1100

    Return the order as a DFC JSON object
    But there's not much in the object yet.

--- engines/dfc_provider/spec/services/order_builder_spec.rb
+++ engines/dfc_provider/spec/services/order_builder_spec.rb
@@ -18,4 +18,17 @@ RSpec.describe OrderBuilder do
       expect(result.orderStatus).to eq "dfc-v:Held"
     end
   end
+
+  describe '.build' do
+    let(:distributor) { create(:distributor_enterprise, id: 10_000) }
+    let(:ofn_order) { create(:completed_order_with_totals, distributor:, id: 1) }
+    subject(:result) { described_class.build(ofn_order) }
+
+    it "builds and stores a DFC order object" do
+      expect(result.semanticId).to  eq "http://test.host/api/dfc/enterprises/10000/orders/1"
+      expect(result.client).to      eq "http://test.host/api/dfc/enterprises/10000"
+      expect(result.orderStatus).to eq "dfc-v:Held"
+      expect(result.lines.count).to eq 0
+    end
+  end
 end
