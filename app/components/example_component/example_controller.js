d6ed536eb794d2dd3ecb7249dbb0325bbbd1e6eb
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2025-03-12 15:11:22 +1100

    Add example of component stimulus controller naming convention

---
+++ app/components/example_component/example_controller.js
@@ -0,0 +1,4 @@
+// This controller will be called "example-component--example", ie "component-subdirectory--js-file-name"
+import { Controller } from "stimulus";
+
+export default class extends Controller {}
