c949c4bdeb88c6eae50033a7243a5a6b3c3f91d6
Ahmed Ejaz <chahmedejaz9050@gmail.com>
2025-03-18 03:30:11 +0500

    fix lint issues

--- db/migrate/20250317221440_change_final_weight_volume_to_scale_3_in_spree_line_items.rb
+++ db/migrate/20250317221440_change_final_weight_volume_to_scale_3_in_spree_line_items.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ChangeFinalWeightVolumeToScale3InSpreeLineItems < ActiveRecord::Migration[7.0]
   def up
     change_column :spree_line_items, :final_weight_volume, :decimal, precision: 10, scale: 3
