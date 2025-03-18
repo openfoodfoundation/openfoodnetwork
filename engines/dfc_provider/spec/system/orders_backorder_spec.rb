d054d689b11b1534463bf5185e0d38de5cd30ec5
David Cook <david@redcliffs.net>
2025-03-18 12:33:16 +1100

    Ah ha, it's waiting for a lock here, possibly at find_ofn_user

--- engines/dfc_provider/spec/system/orders_backorder_spec.rb
+++ engines/dfc_provider/spec/system/orders_backorder_spec.rb
@@ -71,7 +71,8 @@ RSpec.describe "Orders backorder integration" do
       # For debugging you can check log/test.log, eg:
       # echo "" > log/test.log; tail -f log/test.log | egrep "(Started|Completed)"
 
-      BackorderJob.perform_now(order)
+      # BackorderJob.perform_now(order) # Cannot perform whole job as it gets stuck on record lock.
+      BackorderJob.new.place_backorder(order)
 
       # A backorder to the supplier has been created
       expect(supplier.distributed_orders.count).to eq 1
