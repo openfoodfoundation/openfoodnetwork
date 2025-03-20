3a36df7cbfa7276902a63d617edb35f1f3e3fe85
David Cook <david@redcliffs.net>
2025-03-20 17:28:32 +1100

    Build line items

--- engines/dfc_provider/app/services/order_builder.rb
+++ engines/dfc_provider/app/services/order_builder.rb
@@ -24,10 +24,22 @@ class OrderBuilder < DfcBuilder
 
   def self.apply(ofn_order, dfc_order)
     # Set order state if recognised
-    ofn_order.state = "complete" if dfc_order.orderStatus == order_status.HELD
+    ofn_order.state = "complete" if dfc_order.orderStatus == order_states.HELD
+
+    dfc_order.lines.each do |order_line|
+      # We don't need to look at the DFC object, we know the ID is a local variant ID.
+      variant_id = order_line.offer.offeredItem.split('/supplied_products/').last
+
+      ofn_order.line_items.build(
+        variant_id:,
+        quantity: order_line.quantity
+      )
+    end
+
+    ofn_order.save
   end
 
-  def self.order_status
+  def self.order_states
     DfcLoader.vocabulary("vocabulary").STATES.ORDERSTATE
   end
 end
