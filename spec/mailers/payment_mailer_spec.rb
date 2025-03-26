e017b14f31a6f18a59072652172c74a57e24da40
filipefurtad0 <filipefurtado@gmail.com>
2025-03-26 11:56:39 +0000

    Adds spec for payment authorization emails

---
+++ spec/mailers/payment_mailer_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe PaymentMailer do
+  describe '#payment_mailer' do
+    let(:enterprise) { create(:enterprise) }
+    let(:payment_method) {
+      create(:payment_method, distributors: [order.distributor])
+    }
+    let(:payment) {
+      create(:payment, order:, payment_method:)
+    }
+    let(:order) { create(:completed_order_with_totals) }
+
+    context "authorize payment email" do
+      subject(:email) { described_class.authorize_payment(payment) }
+
+      it "includes the distributor's name in the subject" do
+        expect(email.subject).to include("authorize your payment to #{order.distributor.name}")
+      end
+
+      it "sets a reply-to of the customer email" do
+        expect(email.reply_to).to eq([order.distributor.contact.email])
+      end
+    end
+
+    context "authorization required email" do
+      subject(:email) { described_class.authorization_required(payment) }
+
+      it "includes the distributor's name in the subject" do
+        expect(email.subject).to include("A payment requires authorization from the customer")
+      end
+
+      it "sets a reply-to of the customer email" do
+        expect(email.reply_to).to eq([order.email])
+      end
+    end
+  end
+end
