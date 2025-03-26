c95c598f82dc51c7e6b8a2bdd3cde5c11bbf97cf
filipefurtad0 <filipefurtado@gmail.com>
2025-03-26 08:03:36 +0000

    Asserts that the enterprise welcome email has no reply to address
    Asserts that manager invitation email has the inviting enterprise as reply to address

--- spec/mailers/enterprise_mailer_spec.rb
+++ spec/mailers/enterprise_mailer_spec.rb
@@ -14,6 +14,11 @@ RSpec.describe EnterpriseMailer do
       expect(mail.subject)
         .to eq "#{enterprise.name} is now on #{Spree::Config[:site_name]}"
     end
+
+    it "does not set a reply-to email" do
+      EnterpriseMailer.welcome(enterprise).deliver_now
+      expect(ActionMailer::Base.deliveries.first.reply_to).to be nil
+    end
   end
 
   describe "#manager_invitation" do
@@ -22,6 +27,11 @@ RSpec.describe EnterpriseMailer do
       expect(ActionMailer::Base.deliveries.count).to eq 1
       mail = ActionMailer::Base.deliveries.first
       expect(mail.subject).to eq "#{enterprise.name} has invited you to be a manager"
+    end
+
+    it "sets a reply-to of the enterprise email" do
+      EnterpriseMailer.manager_invitation(enterprise, user).deliver_now
+      expect(ActionMailer::Base.deliveries.first.reply_to).to eq([enterprise.contact.email])
     end
   end
 end
