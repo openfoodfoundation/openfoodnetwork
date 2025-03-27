1b8ebc3105194e3caff39d0fceaf7a0ac4e17343
Maikel Linke <maikel@email.org.au>
2025-03-27 11:38:35 +1100

    Test with known text

--- spec/mailers/enterprise_mailer_spec.rb
+++ spec/mailers/enterprise_mailer_spec.rb
@@ -3,14 +3,14 @@
 require 'spec_helper'
 
 RSpec.describe EnterpriseMailer do
-  let(:enterprise) { build(:enterprise) }
+  let(:enterprise) { build(:enterprise, name: "Fred's Farm") }
 
   describe "#welcome" do
     subject(:mail) { EnterpriseMailer.welcome(enterprise) }
 
     it "sends a welcome email when given an enterprise" do
       expect(mail.subject)
-        .to eq "#{enterprise.name} is now on #{Spree::Config[:site_name]}"
+        .to eq "Fred's Farm is now on OFN Demo Site"
     end
 
     it "does not set a reply-to email" do
@@ -23,7 +23,7 @@ RSpec.describe EnterpriseMailer do
     let(:user) { build(:user) }
 
     it "should send a manager invitation email when given an enterprise and user" do
-      expect(mail.subject).to eq "#{enterprise.name} has invited you to be a manager"
+      expect(mail.subject).to eq "Fred's Farm has invited you to be a manager"
     end
 
     it "sets a reply-to of the enterprise email" do
