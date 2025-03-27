e12c51edd1085a0908c80a5a7643547ac3596a9e
Maikel Linke <maikel@email.org.au>
2025-03-27 11:29:15 +1100

    Use RSpec matchers for emails

--- spec/mailers/enterprise_mailer_spec.rb
+++ spec/mailers/enterprise_mailer_spec.rb
@@ -7,31 +7,27 @@ RSpec.describe EnterpriseMailer do
   let!(:user) { create(:user) }
 
   describe "#welcome" do
-    it "sends a welcome email when given an enterprise" do
-      EnterpriseMailer.welcome(enterprise).deliver_now
+    subject(:mail) { EnterpriseMailer.welcome(enterprise) }
 
-      mail = ActionMailer::Base.deliveries.first
+    it "sends a welcome email when given an enterprise" do
       expect(mail.subject)
         .to eq "#{enterprise.name} is now on #{Spree::Config[:site_name]}"
     end
 
     it "does not set a reply-to email" do
-      EnterpriseMailer.welcome(enterprise).deliver_now
-      expect(ActionMailer::Base.deliveries.first.reply_to).to be nil
+      expect(mail.reply_to).to eq nil
     end
   end
 
   describe "#manager_invitation" do
+    subject(:mail) { EnterpriseMailer.manager_invitation(enterprise, user) }
+
     it "should send a manager invitation email when given an enterprise and user" do
-      EnterpriseMailer.manager_invitation(enterprise, user).deliver_now
-      expect(ActionMailer::Base.deliveries.count).to eq 1
-      mail = ActionMailer::Base.deliveries.first
       expect(mail.subject).to eq "#{enterprise.name} has invited you to be a manager"
     end
 
     it "sets a reply-to of the enterprise email" do
-      EnterpriseMailer.manager_invitation(enterprise, user).deliver_now
-      expect(ActionMailer::Base.deliveries.first.reply_to).to eq([enterprise.contact.email])
+      expect(mail.reply_to).to eq([enterprise.contact.email])
     end
   end
 end
