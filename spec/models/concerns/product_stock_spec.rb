d55d4dd8ed9c4bb9b66079bca4bf58e3808e133e
Maikel Linke <maikel@email.org.au>
2025-03-28 14:00:40 +1100

    Remove unused Product#on_demand

--- spec/models/concerns/product_stock_spec.rb
+++ spec/models/concerns/product_stock_spec.rb
@@ -6,12 +6,6 @@ RSpec.describe ProductStock do
   let(:product) { create(:simple_product) }
 
   context "when product has one variant" do
-    describe "product.on_demand" do
-      it "is the products first variant on_demand" do
-        expect(product.on_demand).to eq(product.variants.first.on_demand)
-      end
-    end
-
     describe "product.on_hand" do
       it "is the products first variant on_hand" do
         expect(product.on_hand).to eq(product.variants.first.on_hand)
@@ -22,13 +16,6 @@ RSpec.describe ProductStock do
   context 'when product has more than one variant' do
     before do
       product.variants << create(:variant, product:)
-    end
-
-    describe "product.on_demand" do
-      it "raises error" do
-        expect { product.on_demand }
-          .to raise_error(StandardError, /Cannot determine product on_demand value/)
-      end
     end
 
     describe "product.on_hand" do
