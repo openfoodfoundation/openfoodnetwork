507705a4eb2a22216c4b9b2c5883973924c7955b
Tresor11 <bireketresor@gmail.com>
2025-03-31 22:13:45 +0300

    Update method name to require
    This commit updates the method name to be called required_enterprise
    since we only expect it to raise an error when the enterprise is not found.

---
+++ spec/requests/admin/enterprises_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+RSpec.describe Admin::EnterprisesController, type: :request do
+  let(:admin) { create(:admin_user) }
+  let(:enterprise) { create(:enterprise) }
+
+  before do
+    sign_in admin
+  end
+
+  describe 'GET #show' do
+    it 'returns a successful response' do
+      get edit_admin_enterprise_path(enterprise)
+      expect(response).to have_http_status(:success)
+    end
+
+    it "redirect to the enterprises page for non-existing enterprise" do
+      get edit_admin_enterprise_path(id: 'non-existing')
+      expect(response).to redirect_to(admin_enterprises_path)
+    end
+  end
+end
