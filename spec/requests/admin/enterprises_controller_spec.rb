176e33d6bf4a0432ef64cf7dd88fb91e04c24e75
David Cook <david@redcliffs.net>
2025-04-01 09:29:27 +1100

    Rubocop fixes

--- spec/requests/admin/enterprises_controller_spec.rb
+++ spec/requests/admin/enterprises_controller_spec.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
-RSpec.describe Admin::EnterprisesController, type: :request do
+RSpec.describe Admin::EnterprisesController do
   let(:admin) { create(:admin_user) }
   let(:enterprise) { create(:enterprise) }
 
