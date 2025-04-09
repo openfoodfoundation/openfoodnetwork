559249b6217b8de68fca53f0e2c8eefc9ff29c03
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2025-04-09 13:37:16 +1000

    Improve tagListInputController to integrate with bulkFormController
    tagListInputController is not a standard form controller so we had to
    make a few changes to integrate with the change tracking used int
    bulkFormController. Mainly we make sure to manually trigger event when
    deleting a tags, and we handle the change highlight.

--- app/components/tag_list_input_component/tag_list_input_controller.js
+++ app/components/tag_list_input_component/tag_list_input_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "stimulus";
 
 export default class extends Controller {
   static targets = ["tagList", "newTag", "template", "list"];
+  static values = { highlightClass: String };
 
   addTag() {
     // add to tagList
@@ -12,6 +13,8 @@ export default class extends Controller {
     const spanElement = newTagElement.querySelector("span");
     spanElement.innerText = this.newTagTarget.value;
     this.listTarget.appendChild(newTagElement);
+
+    this.#highlightList();
 
     // Clear new tag value
     this.newTagTarget.value = "";
@@ -27,6 +30,10 @@ export default class extends Controller {
     tags.splice(index, 1);
     this.tagListTarget.value = tags.join(",");
 
+    // manualy dispatch an Input event so the change gets picked up by the bulk form controller
+    this.tagListTarget.dispatchEvent(new InputEvent("input"));
+    this.#highlightList();
+
     // Remove HTML element from the list
     event.srcElement.parentElement.parentElement.remove();
   }
@@ -34,7 +41,17 @@ export default class extends Controller {
   // Strip comma from tag name
   filterInput(event) {
     if (event.key === ",") {
-      event.srcElement.value = event.srcElement.value.replace(",","");
+      event.srcElement.value = event.srcElement.value.replace(",", "");
+    }
+  }
+
+  // private
+
+  #highlightList() {
+    if (this.highlightClassValue !== "") {
+      // div with the tags class
+      const tagsInputDiv = this.tagListTarget.nextElementSibling.firstElementChild;
+      tagsInputDiv.classList.add(this.highlightClassValue);
     }
   }
 }
