3bb017688726504798ff4d2d50dd6601543f6899
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2025-04-16 14:56:01 +1000

    Set up variant tags as searchable

--- config/initializers/acts_as_taggable_on.rb
+++ config/initializers/acts_as_taggable_on.rb
@@ -1,1 +1,19 @@
+# frozen_string_literal: true
+
 ActsAsTaggableOn.force_lowercase = true
+
+# Monkey patch suggested as workaround to the allowlisted issue
+# This may be removed when an official fix is included in Ransack
+module ActsAsTaggableOn
+  class Tag
+    class << self
+      def ransackable_associations(_auth_object = nil)
+        ["taggings"]
+      end
+
+      def ransackable_attributes(_auth_object = nil)
+        ["name"]
+      end
+    end
+  end
+end
