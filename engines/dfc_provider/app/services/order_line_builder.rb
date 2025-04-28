2be158c58a9acf8c976f8ca6f1683140d91e5e8a
David Cook <david@redcliffs.net>
2025-04-29 09:58:02 +1000

    Rename method
    Normally a DfcBuilder 'build's from the equivalent OFN model. I believe the equivalent for DFC OrderLine is OFN Spree::LineItem, so I'll give this existing method a different name. (We'll use 'build' in the next commit).

--- engines/dfc_provider/app/services/order_line_builder.rb
+++ engines/dfc_provider/app/services/order_line_builder.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class OrderLineBuilder < DfcBuilder
-  def self.build(offer, quantity)
+  def self.build_from_offer(offer, quantity)
     DataFoodConsortium::Connector::OrderLine.new(
       nil, offer:, quantity:,
     )
