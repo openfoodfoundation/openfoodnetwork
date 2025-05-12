f199fb182b2edf89dc3a4ae062bcda0629b12bb0
David Cook <david@redcliffs.net>
2025-05-12 17:35:05 +1000

    Add specs

--- spec/controllers/admin/variant_overrides_controller_spec.rb
+++ spec/controllers/admin/variant_overrides_controller_spec.rb
@@ -3,6 +3,29 @@
 require 'spec_helper'
 
 RSpec.describe Admin::VariantOverridesController, type: :controller do
+  describe "index" do
+    context "not logged in" do
+      it "redirects to login" do
+        pending
+        get :index
+        expect(response).to have_http_status :found
+      end
+    end
+
+    context "where I manage the variant override hub" do
+      let(:hub) { create(:distributor_enterprise) }
+
+      before do
+        allow(controller).to receive(:spree_current_user) { hub.owner }
+      end
+
+      it "succeeds" do
+        get :index
+        expect(response).to have_http_status :ok
+      end
+    end
+  end
+
   describe "bulk_update" do
     context "json" do
       let(:format) { :json }
