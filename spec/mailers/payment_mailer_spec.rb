0de4f2f5961f7aefae5b64d06411882301f81736
Konrad <mail@konradsachse.de>
2025-05-26 18:36:39 +0200

    Update payment mailer specs to check that customer facing emails are white labelled, while shop facing emails are not
    Also make use of the newly separated shared_examples

--- spec/mailers/payment_mailer_spec.rb
+++ spec/mailers/payment_mailer_spec.rb
@@ -14,31 +14,41 @@ RSpec.describe PaymentMailer do
     let(:order) { create(:completed_order_with_totals) }
 
     context "authorize payment email" do
-      subject(:email) { described_class.authorize_payment(payment) }
+      subject(:mail) { described_class.authorize_payment(payment) }
 
       it "includes the distributor's name in the subject" do
-        expect(email.subject).to include("authorize your payment to #{order.distributor.name}")
+        expect(mail.subject).to include("authorize your payment to #{order.distributor.name}")
       end
 
       it "sets a reply-to of the customer email" do
-        expect(email.reply_to).to eq([order.distributor.contact.email])
+        expect(mail.reply_to).to eq([order.distributor.contact.email])
+      end
+
+      context "white labelling" do
+        it_behaves_like 'email with inactive white labelling', :mail
+        it_behaves_like 'customer facing email with active white labelling', :mail
       end
 
       it "includes a link to authorize the payment" do
         link = "http://test.host/payments/#{payment.id}/authorize"
-        expect(email.body).to have_link link, href: link
+        expect(mail.body).to have_link link, href: link
       end
     end
 
     context "authorization required email" do
-      subject(:email) { described_class.authorization_required(payment) }
+      subject(:mail) { described_class.authorization_required(payment) }
 
       it "includes the distributor's name in the subject" do
-        expect(email.subject).to include("A payment requires authorization from the customer")
+        expect(mail.subject).to include("A payment requires authorization from the customer")
       end
 
       it "sets a reply-to of the customer email" do
-        expect(email.reply_to).to eq([order.email])
+        expect(mail.reply_to).to eq([order.email])
+      end
+
+      context "white labelling" do
+        it_behaves_like 'email with inactive white labelling', :mail
+        it_behaves_like 'non-customer facing email with active white labelling', :mail
       end
     end
   end
