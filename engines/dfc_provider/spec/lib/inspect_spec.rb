f9764b697d9b28ea18854df8fbefca1ae732a404
David Cook <david@redcliffs.net>
2025-05-28 11:39:42 +1000

    Found a cleaner way to patch inspect
    I tried to move on, I really did, but I already had these things in my head and they suddenly clicked together, so I thought it worth updating now.

--- engines/dfc_provider/spec/lib/inspect_spec.rb
+++ engines/dfc_provider/spec/lib/inspect_spec.rb
@@ -2,24 +2,36 @@
 
 require_relative "../spec_helper"
 
-RSpec.describe "#inspect" do
-  it "provides a clean-enough output for Address" do
+RSpec.describe "#pretty_inspect" do
+  it "provides a clean output for Address" do
     subject = DataFoodConsortium::Connector::Address.new("some/id")
 
-    expect(subject.inspect).to eq <<~HEREDOC.squish
-      #<DataFoodConsortium::Connector::Address
-      {"semanticId"=>"some/id", "semanticType"=>"dfc-b:Address", "street"=>nil, "postalCode"=>nil,
-      "city"=>nil, "country"=>nil, "latitude"=>nil, "longitude"=>nil, "region"=>nil}>
+    expect(subject.pretty_inspect).to match <<~HEREDOC
+      #<DataFoodConsortium::Connector::Address:.*
+       @semanticId="some/id",
+       @semanticType="dfc-b:Address",
+       @street=nil,
+       @postalCode=nil,
+       @city=nil,
+       @country=nil,
+       @latitude=nil,
+       @longitude=nil,
+       @region=nil>
     HEREDOC
   end
 
-  it "provides a clean-enough output for OrderLine" do
+  it "provides a clean output for OrderLine" do
     subject = DataFoodConsortium::Connector::OrderLine.new("some/id")
 
-    expect(subject.inspect).to eq <<~HEREDOC.squish
-      #<DataFoodConsortium::Connector::OrderLine
-      {"semanticId"=>"some/id", "semanticType"=>"dfc-b:OrderLine", "description"=>nil,
-      "quantity"=>nil, "price"=>nil, "offer"=>nil, "order"=>nil}>
+    expect(subject.pretty_inspect).to match <<~HEREDOC
+      #<DataFoodConsortium::Connector::OrderLine:.*
+       @semanticId="some/id",
+       @semanticType="dfc-b:OrderLine",
+       @description=nil,
+       @quantity=nil,
+       @price=nil,
+       @offer=nil,
+       @order=nil>
     HEREDOC
   end
 end
