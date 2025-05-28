7bf6fe93aef01076f75a56c2b55116baadeb23c9
David Cook <david@redcliffs.net>
2025-05-28 11:39:42 +1000

    Found a cleaner way to patch inspect
    I tried to move on, I really did, but I already had these things in my head and they suddenly clicked together, so I thought it worth updating now.

--- engines/dfc_provider/lib/inspect.rb
+++ engines/dfc_provider/lib/inspect.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 module DfcConnectorInspect
-  def inspect
-    # Show all other instance variables, which are a summary of the semanticPropertiesMap
-    "#<#{self.class.name} #{instance_values.except('semanticPropertiesMap')}>"
+  # Override PP method, which is used in pretty_inspect for rails console
+  def pretty_print_instance_variables
+    instance_variables.reject{ |var| var == :@semanticPropertiesMap }
   end
 end
 
-# Include custom inspect on all connector classes
+# Include on all connector classes
 DataFoodConsortium::Connector.constants.each do |klass|
   DataFoodConsortium::Connector.const_get(klass).class_eval do
     include DfcConnectorInspect
