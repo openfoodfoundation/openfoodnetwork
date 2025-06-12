01aa9a041acfedacef8e9cd2906add213b641bea
Maikel Linke <maikel@email.org.au>
2025-06-12 16:33:53 +1000

    Update and list scopes for real

--- engines/dfc_provider/app/controllers/dfc_provider/portals_controller.rb
+++ engines/dfc_provider/app/controllers/dfc_provider/portals_controller.rb
@@ -2,6 +2,21 @@
 
 module DfcProvider
   class PortalsController < DfcProvider::ApplicationController
+    PORTALS = {
+      '682afcc4966dbb3aa7464d56' => {
+        '@id': "https://waterlooregionfood.ca/portal/profile",
+        description: "A super duper portal for the waterloo region",
+        termsandconditions: "https://waterlooregionfood.ca/terms-and-conditions",
+        title: "Waterloo Region Food Portal",
+      },
+      '682b2e2b031c28f69cda1645' => {
+        '@id': "https://anotherplatform.ca/portal/profile",
+        description: "A super duper portal for the waterloo region",
+        termsandconditions: "https://anotherplatform.ca/terms-and-conditions",
+        title: "anotherplatform Portal",
+      },
+    }.freeze
+
     # DANGER!
     # This endpoint is open to CSRF attacks.
     # This is a temporary measure until the DFC Permissions module accesses
@@ -11,15 +26,80 @@ module DfcProvider
     before_action :check_enterprise
 
     def index
-      render json: <<~JSON
-        {"@context":"https://cdn.startinblox.com/owl/context-bis.jsonld","@id":"https://mydataserver.com/enterprises/1/platforms","dfc-t:platforms":{"@list":[{"@id":"https://waterlooregionfood.ca/portal/profile","@type":"dfc-t:Platform","_id":{"$oid":"682afcc4966dbb3aa7464d56"},"description":"A super duper portal for the waterloo region","dfc-t:hasAssignedScopes":{"@list":[{"@id":"https://data-server.cqcm.startinblox.com/enterprises/1/platforms/scopes/ReadEnterprise","@type":"dfc-t:Scope","dfc-t:scope":"ReadEnterprise"},{"@id":"https://data-server.cqcm.startinblox.com/enterprises/1/platforms/scopes/WriteEnterprise","@type":"dfc-t:Scope","dfc-t:scope":"WriteEnterprise"},{"@id":"https://data-server.cqcm.startinblox.com/enterprises/1/platforms/scopes/ReadProducts","@type":"dfc-t:Scope","dfc-t:scope":"ReadProducts"},{"@id":"https://data-server.cqcm.startinblox.com/enterprises/1/platforms/scopes/WriteProducts","@type":"dfc-t:Scope","dfc-t:scope":"WriteProducts"},{"@id":"https://data-server.cqcm.startinblox.com/enterprises/1/platforms/scopes/ReadOrders","@type":"dfc-t:Scope","dfc-t:scope":"ReadOrders"},{"@id":"https://data-server.cqcm.startinblox.com/enterprises/1/platforms/scopes/WriteOrders","@type":"dfc-t:Scope","dfc-t:scope":"WriteOrders"}],"@type":"rdf:List"},"termsandconditions":"https://waterlooregionfood.ca/terms-and-conditions","title":"Waterloo Region Food Portal"},{"@id":"https://anotherplatform.ca/portal/profile","@type":"dfc-t:Platform","_id":{"$oid":"682b2e2b031c28f69cda1645"},"description":"A super duper portal for the waterloo region","dfc-t:hasAssignedScopes":{"@list":[],"@type":"rdf:List"},"termsandconditions":"https://anotherplatform.ca/terms-and-conditions","title":"anotherplatform Portal"}],"@type":"rdf:List"}}
-      JSON
+      render json: portals
     end
 
     def update
-      render json: <<~JSON
-        {"@id":"https://anotherplatform.ca/portal/profile","@type":"dfc-t:Platform","description":"A super duper portal for the waterloo region","dfc-t:hasAssignedScopes":{"@list":[{"@id":"https://data-server.cqcm.startinblox.com/enterprises/1/platforms/scopes/ReadEnterprise","@type":"dfc-t:Scope","dfc-t:scope":"ReadEnterprise"},{"@id":"https://data-server.cqcm.startinblox.com/enterprises/1/platforms/scopes/WriteEnterprise","@type":"dfc-t:Scope","dfc-t:scope":"WriteEnterprise"},{"@id":"https://data-server.cqcm.startinblox.com/enterprises/1/platforms/scopes/ReadProducts","@type":"dfc-t:Scope","dfc-t:scope":"ReadProducts"},{"@id":"https://data-server.cqcm.startinblox.com/enterprises/1/platforms/scopes/WriteProducts","@type":"dfc-t:Scope","dfc-t:scope":"WriteProducts"},{"@id":"https://data-server.cqcm.startinblox.com/enterprises/1/platforms/scopes/ReadOrders","@type":"dfc-t:Scope","dfc-t:scope":"ReadOrders"},{"@id":"https://data-server.cqcm.startinblox.com/enterprises/1/platforms/scopes/WriteOrders","@type":"dfc-t:Scope","dfc-t:scope":"WriteOrders"}],"@type":"rdf:List"},"termsandconditions":"https://anotherplatform.ca/terms-and-conditions","title":"anotherplatform Portal"}
-      JSON
+      key = params[:id]
+      requested_portal = JSON.parse(request.body.read)
+      requested_scopes = requested_portal
+        .dig("dfc-t:hasAssignedScopes", "@list")
+        .pluck("dfc-t:scope")
+      current_scopes = granted_scopes(key)
+      scopes_to_delete = current_scopes - requested_scopes
+      scopes_to_create = requested_scopes - current_scopes
+
+      DfcPermission.where(
+        user: current_user,
+        enterprise: current_enterprise,
+        scope: scopes_to_delete,
+        grantee: key,
+      ).delete_all
+
+      scopes_to_create.each do |scope|
+        DfcPermission.create!(
+          user: current_user,
+          enterprise: current_enterprise,
+          scope:,
+          grantee: key,
+        )
+      end
+      render json: portal(key)
+    end
+
+    private
+
+    def portals
+      id = DfcProvider::Engine.routes.url_helpers.enterprise_portals_url(current_enterprise.id)
+      portals = PORTALS.keys.map(&method(:portal))
+
+      {
+        '@context': "https://cdn.startinblox.com/owl/context-bis.jsonld",
+        '@id': id,
+        'dfc-t:platforms': {
+          '@type': "rdf:List",
+          '@list': portals,
+        }
+      }
+    end
+
+    def portal(key)
+      {
+        '@type': "dfc-t:Platform",
+        _id: { '$oid': key },
+        'dfc-t:hasAssignedScopes': {
+          '@type': "rdf:List",
+          '@list': scopes(key),
+        }
+      }.merge(PORTALS[key])
+    end
+
+    def scopes(portal_id)
+      granted_scopes(portal_id).map do |scope|
+        {
+          '@id': "##{scope}",
+          '@type': "dfc-t:Scope",
+          'dfc-t:scope': scope,
+        }
+      end
+    end
+
+    def granted_scopes(portal_id)
+      DfcPermission.where(
+        user: current_user,
+        enterprise: current_enterprise,
+        grantee: portal_id,
+      ).pluck(:scope)
     end
   end
 end
--- engines/dfc_provider/spec/requests/portals_spec.rb
+++ engines/dfc_provider/spec/requests/portals_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe "Portals", swagger_doc: "dfc.yaml" do
         let(:enterprise_id) { enterprise.id }
 
         run_test! do
-          expect(json_response["@id"]).to eq "https://mydataserver.com/enterprises/1/platforms"
+          expect(json_response["@id"]).to eq "http://test.host/api/dfc/enterprises/10000/portals"
         end
       end
     end
@@ -34,11 +34,57 @@ RSpec.describe "Portals", swagger_doc: "dfc.yaml" do
     parameter name: :portal_id, in: :path, type: :string
 
     put "Update authorized scopes of a portal" do
+      consumes "application/json"
       produces "application/json"
+
+      parameter name: :portal, in: :body, schema: {
+        example: {
+          '@context': "https://cdn.startinblox.com/owl/context-bis.jsonld",
+          '@id': "/api/dfc/enterprises/3/portals/682b2e2b031c28f69cda1645",
+          'dfc-t:hasAssignedScopes': {
+            '@list': [
+              {
+                '@id': "/api/dfc/enterprises/3/portals/scopes/ReadEnterprise",
+                'dfc-t:scope': "ReadEnterprise",
+                '@type': "dfc-t:Scope"
+              },
+              {
+                '@id': "/api/dfc/enterprises/3/portals/scopes/WriteEnterprise",
+                'dfc-t:scope': "WriteEnterprise",
+                '@type': "dfc-t:Scope"
+              },
+              {
+                '@id': "/api/dfc/enterprises/3/portals/scopes/ReadProducts",
+                'dfc-t:scope': "ReadProducts",
+                '@type': "dfc-t:Scope"
+              },
+              {
+                '@id': "/api/dfc/enterprises/3/portals/scopes/WriteProducts",
+                'dfc-t:scope': "WriteProducts",
+                '@type': "dfc-t:Scope"
+              },
+              {
+                '@id': "/api/dfc/enterprises/3/portals/scopes/ReadOrders",
+                'dfc-t:scope': "ReadOrders",
+                '@type': "dfc-t:Scope"
+              },
+              {
+                '@id': "/api/dfc/enterprises/3/portals/scopes/WriteOrders",
+                'dfc-t:scope': "WriteOrders",
+                '@type': "dfc-t:Scope"
+              }
+            ],
+            '@type': "rdf:List"
+          }
+        }
+      }
 
       response "200", "successful" do
         let(:enterprise_id) { enterprise.id }
         let(:portal_id) { "682b2e2b031c28f69cda1645" }
+        let(:portal) do |example|
+          example.metadata[:operation][:parameters].first[:schema][:example]
+        end
 
         run_test! do
           expect(json_response["@id"]).to eq "https://anotherplatform.ca/portal/profile"
