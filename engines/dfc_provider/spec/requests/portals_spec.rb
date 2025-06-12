44f9aea1787595e00c6e98f7ded021956d0619f5
Maikel Linke <maikel@email.org.au>
2025-06-12 14:14:09 +1000

    Add endpoint to update scopes of portal
    Dummy implementation only.

--- engines/dfc_provider/spec/requests/portals_spec.rb
+++ engines/dfc_provider/spec/requests/portals_spec.rb
@@ -28,4 +28,22 @@ RSpec.describe "Portals", swagger_doc: "dfc.yaml" do
       end
     end
   end
+
+  path "/api/dfc/enterprises/{enterprise_id}/portals/{portal_id}" do
+    parameter name: :enterprise_id, in: :path, type: :string
+    parameter name: :portal_id, in: :path, type: :string
+
+    put "Update authorized scopes of a portal" do
+      produces "application/json"
+
+      response "200", "successful" do
+        let(:enterprise_id) { enterprise.id }
+        let(:portal_id) { "682b2e2b031c28f69cda1645" }
+
+        run_test! do
+          expect(json_response["@id"]).to eq "https://anotherplatform.ca/portal/profile"
+        end
+      end
+    end
+  end
 end
