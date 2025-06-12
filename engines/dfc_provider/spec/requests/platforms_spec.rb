818093324b9ff2c3f1841b9ed377fcf7984f3527
Maikel Linke <maikel@email.org.au>
2025-06-12 14:14:09 +1000

    Add endpoint to update scopes of platform
    Dummy implementation only.

--- engines/dfc_provider/spec/requests/platforms_spec.rb
+++ engines/dfc_provider/spec/requests/platforms_spec.rb
@@ -28,4 +28,22 @@ RSpec.describe "Platforms", swagger_doc: "dfc.yaml" do
       end
     end
   end
+
+  path "/api/dfc/enterprises/{enterprise_id}/platforms/{platform_id}" do
+    parameter name: :enterprise_id, in: :path, type: :string
+    parameter name: :platform_id, in: :path, type: :string
+
+    put "Update authorized scopes of a platform" do
+      produces "application/json"
+
+      response "200", "successful" do
+        let(:enterprise_id) { enterprise.id }
+        let(:platform_id) { "682b2e2b031c28f69cda1645" }
+
+        run_test! do
+          expect(json_response["@id"]).to eq "https://anotherplatform.ca/portal/profile"
+        end
+      end
+    end
+  end
 end
