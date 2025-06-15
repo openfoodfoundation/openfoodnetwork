ade35f2fa2a872bfad4dcf5d1965805e5ac907a2
Ahmed Ejaz <chahmedejaz9050@gmail.com>
2025-06-15 18:14:57 +0500

    Fixes specs and update code respectively

--- spec/lib/reports/orders_and_distributors_report_spec.rb
+++ spec/lib/reports/orders_and_distributors_report_spec.rb
@@ -151,7 +151,6 @@ RSpec.describe Reporting::Reports::OrdersAndDistributors::Base do
         subject # build context first
 
         expect { subject.table_rows }.to query_database [
-          "Enterprise Pluck",
           "SQL",
           "Spree::LineItem Load",
           "Spree::PaymentMethod Load",
