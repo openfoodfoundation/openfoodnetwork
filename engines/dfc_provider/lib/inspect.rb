1525501a9dd71c498898ee68f561bd0b169ffb01
David Cook <david@redcliffs.net>
2025-06-18 11:00:11 +1000

    Monkey-patch SemanticObject module directly
    This is the module that defines semanticPropertiesMap.

--- engines/dfc_provider/lib/inspect.rb
+++ engines/dfc_provider/lib/inspect.rb
@@ -1,15 +1,12 @@
 # frozen_string_literal: true
 
-module DfcConnectorInspect
-  # Override PP method, which is used in pretty_inspect for rails console
-  def pretty_print_instance_variables
-    instance_variables.reject{ |var| var == :@semanticPropertiesMap }
-  end
-end
-
-# Include on all connector classes
-DataFoodConsortium::Connector.constants.each do |klass|
-  DataFoodConsortium::Connector.const_get(klass).class_eval do
-    include DfcConnectorInspect
+module VirtualAssembly
+  module Semantizer
+    module SemanticObject
+      # Override PP method, which is used in pretty_inspect for rails console and rspec
+      def pretty_print_instance_variables
+        instance_variables.reject{ |var| var == :@semanticPropertiesMap }
+      end
+    end
   end
 end
