1b6fde463a8491c5896dc2bf4145430d8e7d3425
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2025-07-16 16:54:00 +1000

    TagListInputComponent removed dependency on the form
    This allows us to use the component in a context where there is no form
    object defined.

--- app/components/tag_list_input_component.rb
+++ app/components/tag_list_input_component.rb
@@ -1,16 +1,14 @@
 # frozen_string_literal: true
 
 class TagListInputComponent < ViewComponent::Base
-  # method in a "hidden_field" form helper and is the method used to get a list of tag on the model
-  def initialize(form:, method:, tags:,
+  def initialize(name:, tags:,
                  placeholder: I18n.t("components.tag_list_input.default_placeholder"),
                  aria_label: nil)
-    @f = form
-    @method = method
+    @name = name
     @tags = tags
     @placeholder = placeholder
     @aria_label_option = aria_label ? { 'aria-label': aria_label } : {}
   end
 
-  attr_reader :f, :method, :tags, :placeholder, :aria_label_option
+  attr_reader :name, :tags, :placeholder, :aria_label_option
 end
