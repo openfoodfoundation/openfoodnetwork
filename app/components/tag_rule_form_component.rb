ccdd12bf597d27058f41de27932fe9e9f1249d9c
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2025-08-04 14:24:04 +1000

    Small improvment
    - add value for preferred customer tags
    - allow passing html options to preferred customer tags hidden field

--- app/components/tag_rule_form_component.rb
+++ app/components/tag_rule_form_component.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
 class TagRuleFormComponent < ViewComponent::Base
-  def initialize(rule:, rule_data:, index:)
+  def initialize(rule:, rule_data:, index:, customer_tags: "",
+                 hidden_field_customer_tag_options: {})
     @rule = rule
     @rule_data = rule_data
     @index = index
+    @customer_tags = customer_tags
+    @hidden_field_customer_tag_options = hidden_field_customer_tag_options
   end
 
-  attr_reader :rule, :index, :rule_data
+  attr_reader :rule, :index, :rule_data, :customer_tags, :hidden_field_customer_tag_options
 
   private
 
