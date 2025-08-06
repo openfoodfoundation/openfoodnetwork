c3113f700219546a66ab0ad39a9cc316a2de273e
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2025-08-06 10:44:53 +1000

    Move tag rule group form to a component

--- app/components/tag_rule_group_form_component/tag_rule_group_form_controller.js
+++ app/components/tag_rule_group_form_component/tag_rule_group_form_controller.js
@@ -4,8 +4,8 @@ export default class extends Controller {
   static targets = ["customerTag", "ruleCustomerTag"];
 
   updatePreferredCustomerTag() {
-    const customerTag = this.customerTagTarget.value
-    
-    this.ruleCustomerTagTargets.forEach((element) => element.value = customerTag)
+    const customerTag = this.customerTagTarget.value;
+
+    this.ruleCustomerTagTargets.forEach((element) => (element.value = customerTag));
   }
 }
