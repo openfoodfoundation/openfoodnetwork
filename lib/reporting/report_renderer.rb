1c3c56367b317ea7b5d3bf05171d1f73039cba31
Gareth <garethdavisrogers@gmail.com>
2025-08-12 22:07:23 -0400

    Pretty baffled why it took this long, but now CSVs are printed with header details as was requested at a meeting between me, Amber, and a few farmers

--- lib/reporting/report_renderer.rb
+++ lib/reporting/report_renderer.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-
+require 'csv'
 require 'spreadsheet_architect'
 
 module Reporting
@@ -40,6 +40,19 @@ module Reporting
       @report.table_rows || []
     end
 
+    def report_headers
+      q = @report.respond_to?(:ransack_params) ? (@report.ransack_params || {}) : (@report.params[:q] || {})
+      title = @report.params[:report_type].to_s.tr('_', ' ').titleize
+      from  = q["completed_at_gt"]  || q["completed_at_gteq"]
+      to    = q["completed_at_lt"]  || q["completed_at_lteq"]
+      range = [from, to].compact.join(" → ")
+      rows = []
+      rows << ["Report Title", title]
+      rows << ["Printed At",   Time.zone.now.to_s(:db)]
+      rows << ["Date Range",   range] unless range.empty?
+      rows
+    end
+
     def as_json(_context_controller = nil)
       @report.rows.map(&:to_h).as_json
     end
@@ -61,7 +74,14 @@ module Reporting
     end
 
     def to_csv
-      SpreadsheetArchitect.to_csv(headers: table_headers, data: table_rows)
+      #append headers
+      csv_string = CSV.generate do |csv|
+        report_headers.each do |row|
+          csv << row
+        end
+      end
+      csv_base = SpreadsheetArchitect.to_csv(headers: table_headers, data: table_rows)
+      csv_string + csv_base
     end
 
     def to_xlsx
