1cca328ce988e479d95a54c31ce6f3b5ff6de751
Gareth <garethdavisrogers@gmail.com>
2025-08-27 18:53:53 -0400

    Added guard clause to prevent adding params unnecessarily

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -22,16 +22,14 @@ module Admin
     def show
       @report = report_class.new(spree_current_user, params, render: false)
       @rendering_options = rendering_options
-      @report.params[:rendering_options] = @rendering_options.options
-
+      attach_rendering_options_to_report!
       show_report
     end
 
     def create
       @report = report_class.new(spree_current_user, params, render: true)
       update_rendering_options
-      @report.params[:rendering_options] = @rendering_options.options
-
+      attach_rendering_options_to_report!
       render_in_background
     end
 
@@ -70,6 +68,13 @@ module Admin
         blob: @blob,
         channel: ScopedChannel.for_id(params[:uuid]),
       )
+    end
+
+    def attach_rendering_options_to_report!
+      opts = @rendering_options&.options || {}
+      return unless @report.respond_to?(:params) && @report.params.is_a?(Hash)
+
+      @report.params[:rendering_options] = opts
     end
   end
 end
