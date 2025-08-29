6f7a547e15067687002b1e994f6de58360a5aee2
Cillian O'Ruanaidh <contact@cilliano.com>
2025-08-29 14:21:14 +0100

    Add a :link_to_or_disabled helper method

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -1,6 +1,29 @@
 # frozen_string_literal: true
 
 module LinkHelper
+  def link_to_or_disabled(name = nil, options = nil, html_options = nil, &block)
+    html_options, options, name = options, name, block if block_given?
+    html_options ||= {}
+
+    if !!html_options.delete(:disabled)
+      # https://www.scottohara.me/blog/2021/05/28/disabled-links.html
+      html_options.merge!(
+        'aria-disabled': true,
+        class: (html_options[:class].to_s.split + ["disabled"]).uniq.join(" "),
+        role: "link"
+      )
+      if block_given?
+        content_tag("a", name, **html_options, &block)
+      else
+        content_tag("a", name, **html_options)
+      end
+    elsif block_given?
+      link_to options, html_options, &block
+    else
+      link_to name, options, html_options
+    end
+  end
+
   def link_to_service(baseurl, name, html_options = {}, &)
     return if name.blank?
 
