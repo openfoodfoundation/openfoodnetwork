36784293d8811551c642ab2133d6ed880b23808f
Maikel Linke <maikel@email.org.au>
2025-09-17 11:52:31 +1000

    Add SuppliedProducts index endpoint

--- engines/dfc_provider/config/routes.rb
+++ engines/dfc_provider/config/routes.rb
@@ -13,6 +13,7 @@ DfcProvider::Engine.routes.draw do
     resources :affiliated_by, only: [:create, :destroy], module: 'enterprise_groups'
   end
   resources :persons, only: [:show]
+  resources :supplied_products, only: [:index]
   resources :product_groups, only: [:show]
 
   resource :affiliate_sales_data, only: [:show]
