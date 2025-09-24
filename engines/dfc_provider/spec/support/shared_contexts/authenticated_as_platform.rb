a12839681f7c3ab58dd8229277bae2c7dce4e6dd
Maikel Linke <maikel@email.org.au>
2025-09-24 15:51:19 +1000

    Use test token as source of truth for validity

--- engines/dfc_provider/spec/support/shared_contexts/authenticated_as_platform.rb
+++ engines/dfc_provider/spec/support/shared_contexts/authenticated_as_platform.rb
@@ -3,12 +3,18 @@
 # Authenticate via Authoriztion token
 RSpec.shared_context "authenticated as platform" do
   let(:Authorization) {
-    "Bearer #{file_fixture('startinblox_access_token.jwt').read}"
+    "Bearer #{access_token}"
+  }
+  let(:access_token) {
+    file_fixture("startinblox_access_token.jwt").read
   }
 
   before do
+    payload = JWT.decode(access_token, nil, false, { algorithm: "RS256" }).first
+    issued_at = Time.zone.at(payload["iat"])
+
     # Once upon a time when the access token hadn't expired yet...
-    travel_to(Date.parse("2025-06-13"))
+    travel_to(issued_at)
 
     # Reset any login via session cookie.
     login_as nil
