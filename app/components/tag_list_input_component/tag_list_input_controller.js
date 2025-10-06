749944fc25724c6bd2af43795783fffdf74f518c
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2025-10-06 16:12:33 +1100

    Rework TagListInputComponent to integrate autocomplete
    The component now will try to load a list of existing tag if you give an
    `autocomplete_url`. I tried to keep the tag input and the autocomplete
    functionality decoupled but is wasn't really possible. Instead I opted
    to sub class the Autocomplete stimulus controller, but it only gets
    initialised if we pass an `autocomplete_url`.

--- app/components/tag_list_input_component/tag_list_input_controller.js
+++ app/components/tag_list_input_component/tag_list_input_controller.js
@@ -1,14 +1,23 @@
-import { Controller } from "stimulus";
+import { Autocomplete } from "stimulus-autocomplete";
 
-export default class extends Controller {
-  static targets = ["tagList", "newTag", "template", "list"];
+// Extend the stimulus-autocomplete controller, so we can load tag with existing rules
+// The autocomplete functionality is only loaded if the url value is set
+// For more informatioon on "stimulus-autocomplete", see:
+//    https://github.com/afcapel/stimulus-autocomplete/tree/main
+//
+export default class extends Autocomplete {
+  static targets = ["tagList", "input", "template", "list"];
   static values = { onlyOne: Boolean };
 
-  addTag(event) {
-    // prevent hotkey form submitting the form (default action for "enter" key)
-    event.preventDefault();
+  connect() {
+    // Don't start autocomplete controller if we don't have an url
+    if (this.urlValue.length == 0) return;
 
-    const newTagName = this.newTagTarget.value.trim().replaceAll(" ", "-");
+    super.connect();
+  }
+
+  addTag(event) {
+    const newTagName = this.inputTarget.value.trim().replaceAll(" ", "-");
     if (newTagName.length == 0) {
       return;
     }
@@ -18,7 +27,7 @@ export default class extends Controller {
     const index = tags.indexOf(newTagName);
     if (index != -1) {
       // highlight the value in red
-      this.newTagTarget.classList.add("tag-error");
+      this.inputTarget.classList.add("tag-error");
       return;
     }
 
@@ -38,12 +47,21 @@ export default class extends Controller {
     this.listTarget.appendChild(newTagElement);
 
     // Clear new tag value
-    this.newTagTarget.value = "";
+    this.inputTarget.value = "";
 
     // hide tag input if limited to one tag
     if (this.tagListTarget.value.split(",").length == 1 && this.onlyOneValue == true) {
-      this.newTagTarget.style.display = "none";
+      this.inputTarget.style.display = "none";
     }
+  }
+
+  keyboardAddTag(event) {
+    // prevent hotkey form submitting the form (default action for "enter" key)
+    if (event) {
+      event.preventDefault();
+    }
+
+    this.addTag();
   }
 
   removeTag(event) {
@@ -62,19 +80,68 @@ export default class extends Controller {
 
     // Make sure the tag input is displayed
     if (this.tagListTarget.value.length == 0) {
-      this.newTagTarget.style.display = "block";
+      this.inputTarget.style.display = "block";
     }
   }
 
   filterInput(event) {
     // clear error class if key is not enter
     if (event.key !== "Enter") {
-      this.newTagTarget.classList.remove("tag-error");
+      this.inputTarget.classList.remove("tag-error");
     }
 
     // Strip comma from tag name
     if (event.key === ",") {
       event.srcElement.value = event.srcElement.value.replace(",", "");
     }
+  }
+
+  // Add tag if we don't have an autocomplete list open
+  onBlur() {
+    // check if we have any autocomplete results
+    if (this.resultsTarget.childElementCount == 0) this.addTag();
+  }
+
+  // Override original to add tag filtering
+  replaceResults(html) {
+    const filteredHtml = this.#filterResults(html);
+
+    // Don't show result if we don't have anything to show
+    if (filteredHtml.length == 0) return;
+
+    super.replaceResults(filteredHtml);
+  }
+
+  // Override original to all empty query, which will return all existing tags
+  onInputChange = () => {
+    if (this.urlValue.length == 0) return;
+
+    if (this.hasHiddenTarget) this.hiddenTarget.value = "";
+
+    const query = this.inputTarget.value.trim();
+    if (query.length >= this.minLengthValue) {
+      this.fetchResults(query);
+    } else {
+      this.hideAndRemoveOptions();
+    }
+  };
+
+  //private
+
+  #filterResults(html) {
+    const existingTags = this.tagListTarget.value.split(",");
+    // Parse the HTML
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, "text/html");
+    const lis = doc.getElementsByTagName("li");
+    // Filter
+    let filteredHtml = "";
+    for (let li of lis) {
+      if (!existingTags.includes(li.dataset.autocompleteValue)) {
+        filteredHtml += li.outerHTML;
+      }
+    }
+
+    return filteredHtml;
   }
 }
