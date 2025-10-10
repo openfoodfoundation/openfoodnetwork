d3319cfd6929322d90a984ac27315680a2a64fb5
Maikel Linke <maikel@email.org.au>
2025-10-10 16:39:15 +1100

    Replace global spec helpers with faster `let`

--- spec/support/api_helper.rb
+++ spec/support/api_helper.rb
@@ -6,14 +6,6 @@ module OpenFoodNetwork
       response.parsed_body
     end
 
-    def json_response_ids
-      json_response[:data]&.pluck(:id)
-    end
-
-    def json_error_detail
-      json_response[:errors][0][:detail]
-    end
-
     def assert_unauthorized!
       expect(json_response).to eq("error" => "You are not authorized to perform that action.")
       expect(response).to have_http_status :unauthorized
