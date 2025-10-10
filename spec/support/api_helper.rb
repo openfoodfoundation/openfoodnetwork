378b5e656e64d058cfe1244a08d669c13d13feec
Maikel Linke <maikel@email.org.au>
2025-10-10 16:35:18 +1100

    Use already parsed JSON in specs
    Existing helpers provide indifferent access already.

--- spec/support/api_helper.rb
+++ spec/support/api_helper.rb
@@ -3,15 +3,7 @@
 module OpenFoodNetwork
   module ApiHelper
     def json_response
-      json_response = JSON.parse(response.body)
-      case json_response
-      when Hash
-        json_response.with_indifferent_access
-      when Array
-        json_response.map(&:with_indifferent_access)
-      else
-        json_response
-      end
+      response.parsed_body
     end
 
     def json_response_ids
