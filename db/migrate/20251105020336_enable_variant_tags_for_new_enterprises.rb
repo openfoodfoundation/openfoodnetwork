3e8a34c5f38869438457edf6158ec77cb8c89947
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2025-11-10 11:12:22 +1100

    Per dicussion, remove super admin from migration

--- db/migrate/20251105020336_enable_variant_tags_for_new_enterprises.rb
+++ db/migrate/20251105020336_enable_variant_tags_for_new_enterprises.rb
@@ -4,12 +4,10 @@ class EnableVariantTagsForNewEnterprises < ActiveRecord::Migration[7.1]
   # rubocop:disable Naming/VariableNumber
   def up
     Flipper.enable_group(:variant_tag, :enterprise_created_after_2025_08_11)
-    Flipper.enable_group(:variant_tag, :admins)
   end
 
   def down
     Flipper.disable_group(:variant_tag, :enterprise_created_after_2025_08_11)
-    Flipper.disable_group(:variant_tag, :admins)
   end
   # rubocop:enable Naming/VariableNumber
 end
