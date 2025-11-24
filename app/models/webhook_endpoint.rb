059e36318e69a43bb73db84313c3db87bebc82dc
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2025-11-24 16:13:11 +1100

    Add type to WebhookEnpoints
    Add migration to update existing endpoint to "order_cycle_opened" type

--- app/models/webhook_endpoint.rb
+++ app/models/webhook_endpoint.rb
@@ -2,5 +2,8 @@
 
 # Records a webhook url to send notifications to
 class WebhookEndpoint < ApplicationRecord
+  WEBHOOK_TYPES = %w(order_cycle_opened payment_status_changed).freeze
+
   validates :url, presence: true
+  validates :webhook_type, presence: true, inclusion: { in: WEBHOOK_TYPES }
 end
