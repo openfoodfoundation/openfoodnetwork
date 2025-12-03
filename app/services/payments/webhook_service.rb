b27f12d516eaa13da7207d4347fa2b2dcb660f7a
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2025-12-03 17:06:45 +1100

    Add Payments::WebhookPayload to manage payload data
    It includes test data so any change in the payload should not affect
    the test webhook enpoint functionality

--- app/services/payments/webhook_service.rb
+++ app/services/payments/webhook_service.rb
@@ -7,26 +7,9 @@ module Payments
   class WebhookService
     def self.create_webhook_job(payment:, event:, at:)
       order = payment.order
-      enterprise = order.distributor
+      payload = WebhookPayload.new(payment:, order:, enterprise: order.distributor).to_hash
 
-      line_items = order.line_items.map do |li|
-        li.slice(:quantity, :price)
-          .merge(
-            tax_category_name: li.tax_category&.name,
-            product_name: li.product.name,
-            name_to_display: li.display_name,
-            unit_to_display: li.unit_presentation
-          )
-      end
-
-      payload = {
-        payment: payment.slice(:updated_at, :amount, :state),
-        enterprise: enterprise.slice(:abn, :acn, :name)
-          .merge(address: enterprise.address.slice(:address1, :address2, :city, :zipcode)),
-        order: order.slice(:total, :currency).merge(line_items: line_items)
-      }
-
-      coordinator = order.order_cycle.coordinator
+      coordinator = payment.order.order_cycle.coordinator
       webhook_urls(coordinator).each do |url|
         WebhookDeliveryJob.perform_later(url, event, payload, at:)
       end
