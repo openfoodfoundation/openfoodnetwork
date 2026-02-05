c66b9611b6f98d4dee167bba6e5970c73b7a3cd7
Maikel Linke <maikel@email.org.au>
2026-02-06 09:53:12 +1100

    Move PaymentMailer translations out of Spree namespace
    The mailer is not in the Spree namespace and we don't need to keep the
    clutter.

--- app/mailers/payment_mailer.rb
+++ app/mailers/payment_mailer.rb
@@ -6,7 +6,7 @@ class PaymentMailer < ApplicationMailer
   def authorize_payment(payment)
     @payment = payment
     @order = @payment.order
-    subject = I18n.t('spree.payment_mailer.authorize_payment.subject',
+    subject = I18n.t("payment_mailer.authorize_payment.subject",
                      distributor: @order.distributor.name)
     I18n.with_locale valid_locale(@order.user) do
       mail(to: @order.email,
@@ -18,7 +18,7 @@ class PaymentMailer < ApplicationMailer
   def authorization_required(payment)
     @order = payment.order
     shop_owner = @order.distributor.owner
-    subject = I18n.t('spree.payment_mailer.authorization_required.subject',
+    subject = I18n.t("payment_mailer.authorization_required.subject",
                      order: @order)
     I18n.with_locale valid_locale(shop_owner) do
       mail(to: shop_owner.email,
@@ -31,7 +31,7 @@ class PaymentMailer < ApplicationMailer
     @order = payment.order
     @taler_order_status_url = taler_order_status_url
 
-    subject = I18n.t("spree.payment_mailer.refund_available.subject",
+    subject = I18n.t("payment_mailer.refund_available.subject",
                      order: @order)
     I18n.with_locale valid_locale(@order.user) do
       mail(to: @order.email,
