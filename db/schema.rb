61e6e0ba909a2b2f34ba6c94ed9a9b3e6c6ec810
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2026-02-08 14:09:28 +1100

    Add Customer credit payment method migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_01_30_041706) do
+ActiveRecord::Schema[7.1].define(version: 2026_02_06_054509) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
