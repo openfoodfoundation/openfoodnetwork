8b827bd95ebd0e5bb081e139d01e89de10db6ce2
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2026-02-18 14:41:56 +1100

    Remove unused helper

--- spec/helpers/checkout_helper_spec.rb
+++ spec/helpers/checkout_helper_spec.rb
@@ -1,18 +1,6 @@
 # frozen_string_literal: true
 
 RSpec.describe CheckoutHelper do
-  it "generates html for validated inputs" do
-    expect(helper).to receive(:render).with(
-      "shared/validated_input",
-      name: "test",
-      path: "foo",
-      attributes: { :required => true, :type => :email, :name => "foo", :id => "foo",
-                    "ng-model" => "foo", "ng-class" => "{error: !fieldValid('foo')}" }
-    )
-
-    helper.validated_input("test", "foo", type: :email)
-  end
-
   describe "#display_checkout_tax_total" do
     subject(:display_checkout_tax_total) { helper.display_checkout_tax_total(order) }
 
