f7ef985d5e4272bf70bb9926d58670fb620cf622
yasir azgar <yasirazgar@gmail.com>
2026-02-18 09:02:39 -0400

    Update application_responder.rb

--- lib/application_responder.rb
+++ lib/application_responder.rb
@@ -3,4 +3,9 @@
 class ApplicationResponder < ActionController::Responder
   include Responders::FlashResponder
   include Responders::HttpCacheResponder
+
+  def test
+     binding.pry
+     puts "test"
+  end
 end
