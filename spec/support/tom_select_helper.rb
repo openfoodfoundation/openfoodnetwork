2cff5bc4df0ba23b9e0bc4b71e2cdb97c028bf29
Maikel Linke <maikel@email.org.au>
2026-02-19 14:01:59 +1100

    Style helper code

--- spec/support/tom_select_helper.rb
+++ spec/support/tom_select_helper.rb
@@ -4,7 +4,7 @@ module TomSelectHelper
   def tomselect_multiselect(value, options)
     tomselect_wrapper = page.find_field(options[:from]).sibling(".ts-wrapper")
     tomselect_wrapper.find(".ts-control").click
-    tomselect_wrapper.find(:css, '.ts-dropdown.multi .ts-dropdown-content .option',
+    tomselect_wrapper.find(:css, ".ts-dropdown.multi .ts-dropdown-content .option",
                            text: value).click
     # Close the dropdown
     page.find("body").click
@@ -14,28 +14,28 @@ module TomSelectHelper
     tomselect_wrapper = page.find_field(options[:from]).sibling(".ts-wrapper")
     tomselect_wrapper.find(".ts-control").click
     # Use send_keys as setting the value directly doesn't trigger the search
-    tomselect_wrapper.find(:css, '.ts-dropdown input.dropdown-input').send_keys(value)
-    tomselect_wrapper.find(:css, '.ts-dropdown .ts-dropdown-content .option.active', text: value).click
+    tomselect_wrapper.find(".ts-dropdown input.dropdown-input").send_keys(value)
+    tomselect_wrapper.find(".ts-dropdown .ts-dropdown-content .option.active", text: value).click
   end
 
   def tomselect_select(value, options)
     tomselect_wrapper = page.find_field(options[:from]).sibling(".ts-wrapper")
     tomselect_wrapper.find(".ts-control").click
 
-    tomselect_wrapper.find(:css, '.ts-dropdown .ts-dropdown-content .option', text: value).click
+    tomselect_wrapper.find(".ts-dropdown .ts-dropdown-content .option", text: value).click
   end
 
   def open_tomselect_to_validate!(page, field_name)
     tomselect_wrapper = page.find_field(field_name).sibling(".ts-wrapper")
     tomselect_wrapper.find(".ts-control").click # open the dropdown
 
-    raise 'Please pass the block for expectations' unless block_given?
+    raise "Please pass the block for expectations" unless block_given?
 
     # execute block containing expectations
     yield
 
     tomselect_wrapper.find(
-      '.ts-dropdown .ts-dropdown-content .option.active',
+      ".ts-dropdown .ts-dropdown-content .option.active",
     ).click # close the dropdown by selecting the already selected value
   end
 end
