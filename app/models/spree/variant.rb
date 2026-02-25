8955ffe1261c0604ec44cb22e9611f36afbae3fd
David Cook <david@redcliffs.net>
2026-02-25 13:47:13 +1100

    AddOwnerToSpreeVariants [migration]
    Should existing variants be migrated to have an owner (copied from supplier)? No, because you can change supplier. This concept needs work.

--- app/models/spree/variant.rb
+++ app/models/spree/variant.rb
@@ -40,6 +40,7 @@ module Spree
     belongs_to :shipping_category, class_name: 'Spree::ShippingCategory', optional: false
     belongs_to :primary_taxon, class_name: 'Spree::Taxon', touch: true, optional: false
     belongs_to :supplier, class_name: 'Enterprise', optional: false, touch: true
+    belongs_to :owner, class_name: 'Enterprise', optional: true
 
     delegate :name, :name=, :description, :description=, :meta_keywords, to: :product
 
