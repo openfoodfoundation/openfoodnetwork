2808a41f0d95c01e470deb65d0750e0a40ca23ca
David Thomas <davidthomas@Davids-MacBook-Air.local>
2026-02-26 15:35:55 -0500

    Safely autocorrect Rails/RedirectBackOrTo
    Inspecting 1721 files
    ........................................W...................................................................W................W..W.......W................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................
    
    Offenses:
    
    app/controllers/admin/order_cycles_controller.rb:212:9: W: [Corrected] Rails/RedirectBackOrTo: Use redirect_back_or_to instead of redirect_back with :fallback_location keyword argument.
            redirect_back(fallback_location: root_path)
            ^^^^^^^^^^^^^
    app/controllers/locales_controller.rb:6:5: W: [Corrected] Rails/RedirectBackOrTo: Use redirect_back_or_to instead of redirect_back with :fallback_location keyword argument.
        redirect_back fallback_location: main_app.root_url
        ^^^^^^^^^^^^^
    app/controllers/spree/admin/invoices_controller.rb:31:9: W: [Corrected] Rails/RedirectBackOrTo: Use redirect_back_or_to instead of redirect_back with :fallback_location keyword argument.
            redirect_back(fallback_location: spree.admin_dashboard_path)
            ^^^^^^^^^^^^^
    app/controllers/spree/admin/orders_controller.rb:83:9: W: [Corrected] Rails/RedirectBackOrTo: Use redirect_back_or_to instead of redirect_back with :fallback_location keyword argument.
            redirect_back fallback_location: spree.admin_dashboard_path
            ^^^^^^^^^^^^^
    app/controllers/spree/admin/orders_controller.rb:91:25: W: [Corrected] Rails/RedirectBackOrTo: Use redirect_back_or_to instead of redirect_back with :fallback_location keyword argument.
              format.html { redirect_back(fallback_location: spree.admin_dashboard_path) }
                            ^^^^^^^^^^^^^
    app/controllers/spree/admin/return_authorizations_controller.rb:13:9: W: [Corrected] Rails/RedirectBackOrTo: Use redirect_back_or_to instead of redirect_back with :fallback_location keyword argument.
            redirect_back fallback_location: spree.admin_dashboard_path
            ^^^^^^^^^^^^^
    
    1721 files inspected, 6 offenses detected, 6 offenses corrected

--- app/controllers/locales_controller.rb
+++ app/controllers/locales_controller.rb
@@ -3,6 +3,6 @@
 class LocalesController < BaseController
   def show
     UserLocaleSetter.new(spree_current_user, params[:id], cookies).set_locale
-    redirect_back fallback_location: main_app.root_url
+    redirect_back_or_to(main_app.root_url)
   end
 end
