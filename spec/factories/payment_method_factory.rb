dbf6007977017aa4710000888cacb8272e176f8b
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2026-03-06 14:03:09 +1100

    Remove payment method ApiCustomerCredit

--- spec/factories/payment_method_factory.rb
+++ spec/factories/payment_method_factory.rb
@@ -33,8 +33,4 @@ FactoryBot.define do
   factory :customer_credit_payment_method, class: Spree::PaymentMethod::CustomerCredit do
     environment { 'test' }
   end
-
-  factory :api_customer_credit_payment_method, class: Spree::PaymentMethod::ApiCustomerCredit do
-    environment { 'test' }
-  end
 end
