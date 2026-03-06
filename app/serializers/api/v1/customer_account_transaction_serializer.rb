6a99d2a3c89883bfc270563831bd0cded0b3c5da
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2026-03-06 13:13:54 +1100

    Add transaction origin in descriptiopn

--- app/serializers/api/v1/customer_account_transaction_serializer.rb
+++ app/serializers/api/v1/customer_account_transaction_serializer.rb
@@ -3,7 +3,7 @@
 module Api
   module V1
     class CustomerAccountTransactionSerializer < Api::V1::BaseSerializer
-      attributes :id, :customer_id, :payment_method_id, :amount, :currency, :description, :balance
+      attributes :id, :customer_id, :amount, :currency, :description, :balance
     end
   end
 end
