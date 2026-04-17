ac6d8757e8cefe710642f0814eed16699a4e2441
Konrad <mail@konradsachse.de>
2026-04-17 10:05:13 +0200

    Add new partial to payment_mailers and re-organize used translation keys
    Add spec to payment mailer

--- spec/mailers/payment_mailer_spec.rb
+++ spec/mailers/payment_mailer_spec.rb
@@ -2,13 +2,10 @@
 
 RSpec.describe PaymentMailer do
   describe '#payment_mailer' do
-    let(:payment_method) {
-      create(:payment_method, distributors: [order.distributor])
-    }
-    let(:payment) {
-      create(:payment, order:, payment_method:)
-    }
-    let(:order) { create(:completed_order_with_totals) }
+    let!(:distributor) { create(:distributor_enterprise) }
+    let!(:order) { create(:completed_order_with_totals, distributor: distributor) }
+    let!(:payment_method) { create(:payment_method, distributors: [distributor]) }
+    let!(:payment) { create(:payment, order: order, payment_method: payment_method) }
 
     context "authorize payment email" do
       subject(:mail) { described_class.authorize_payment(payment) }
@@ -25,6 +22,10 @@ RSpec.describe PaymentMailer do
       context "white labelling" do
         it_behaves_like 'email with inactive white labelling', :mail
         it_behaves_like 'customer facing email with active white labelling', :mail
+      end
+
+      context "enterprise logo" do
+        it_behaves_like "enterprise logo rendering", :mail, :distributor
       end
 
       it "includes a link to authorize the payment" do
@@ -47,6 +48,16 @@ RSpec.describe PaymentMailer do
       context "white labelling" do
         it_behaves_like 'email with inactive white labelling', :mail
         it_behaves_like 'non-customer facing email with active white labelling', :mail
+      end
+
+      context "enterprise logo" do
+        it_behaves_like "enterprise logo rendering", :mail, :distributor
+      end
+
+      context "enterprise greeting" do
+        let(:enterprise) { order.distributor }
+        it_behaves_like 'for an enterprise with contact name present', :mail
+        it_behaves_like 'for an enterprise with no contact name present', :mail
       end
     end
   end
