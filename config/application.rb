3129fe217a215a006b6f968224e498754db1442e
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2026-05-13 14:24:14 +1000

    Rails update task `bin/rails app:update`
    Configuration was kept as close as possible to rails default

--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,7 @@ module Openfoodnetwork
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    # config.autoload_lib(ignore: %w(assets tasks))
+    # config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
