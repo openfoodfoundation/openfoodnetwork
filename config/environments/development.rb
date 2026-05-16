4a865b5d0c1541662f31e969eb6c5d051464740b
Konrad <mail@konradsachse.de>
2026-05-16 14:13:43 +0200

    Add rake task to send emails for layout testing
    Add configuration for development environment to enable correct logo image loading in emails
    
    Created with help of AI

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,7 @@
 require "active_support/core_ext/integer/time"
 
+Rails.application.routes.default_url_options[:host] = "localhost:3000"
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
