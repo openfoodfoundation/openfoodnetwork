3bc02352012cd36141f145930f5aa63990083b7f
David Cook <david@redcliffs.net>
2026-05-20 10:35:30 +1000

    Update db/migrate/20260306015300_add_owner_to_spree_variants.rb
    Co-authored-by: Maikel <maikel@email.org.au>

--- db/migrate/20260306015300_add_owner_to_spree_variants.rb
+++ db/migrate/20260306015300_add_owner_to_spree_variants.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddOwnerToSpreeVariants < ActiveRecord::Migration[6.1]
+class AddOwnerToSpreeVariants < ActiveRecord::Migration[7.1]
   def change
     add_reference :spree_variants, :owner, foreign_key: { to_table: :enterprises }
   end
