048370483da23ba957854b2ea85a6f40e785018c
Maikel Linke <maikel@email.org.au>
2026-05-20 13:58:58 +1000

    Show an address in DFC v2 format
    The format of the address is actually exactly the same. So we don't need
    to transform the data in any way. But we to use a different context for
    the document.

--- engines/dfc_provider/spec/requests/addresses_spec.rb
+++ engines/dfc_provider/spec/requests/addresses_spec.rb
@@ -5,23 +5,37 @@ require_relative "../swagger_helper"
 RSpec.describe "Addresses", swagger_doc: "dfc.yaml" do
   let(:user) { create(:oidc_user) }
   let(:address) { create(:address, id: 40_000) }
-  let(:result) { json_response }
+  let(:Accept) { "application/json" }
 
   before { login_as user }
 
   path "/api/dfc/addresses/{id}" do
     get "Show address" do
       parameter name: :id, in: :path, type: :string
-      produces "application/json"
+      parameter name: :Accept, in: :header, type: :string
+      produces "application/json", 'application/ld+json; profile="dfc-v2"'
 
       response "200", "successful" do
         let(:id) { address.id }
 
         before { create(:enterprise, owner: user, address:) }
 
-        run_test! do
-          expect(result["@id"]).to eq "http://test.host/api/dfc/addresses/40000"
-          expect(result["@type"]).to eq "dfc-b:Address"
+        context "in DFC v1 format" do
+          run_test! do
+            expect(subject["@context"]).to eq "https://w3id.org/dfc/ontology/context/context_1.16.0.json"
+            expect(subject["@id"]).to eq "http://test.host/api/dfc/addresses/40000"
+            expect(subject["@type"]).to eq "dfc-b:Address"
+          end
+        end
+
+        context "in DFC v2 format" do
+          let(:Accept) { 'application/ld+json; profile="dfc-v2"' }
+
+          run_test! do
+            expect(subject["@context"]).to eq "https://www.datafoodconsortium.org"
+            expect(subject["@id"]).to eq "http://test.host/api/dfc/addresses/40000"
+            expect(subject["@type"]).to eq "dfc-b:Address"
+          end
         end
       end
 
