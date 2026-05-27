6d2f8a2df81ec259427ca287d5f2d4099c52df7b
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2026-05-27 15:34:15 +1000

    Show loading spinner when changing order cycle

--- app/webpacker/controllers/product_grid_view_controller.js
+++ app/webpacker/controllers/product_grid_view_controller.js
@@ -1,9 +1,16 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
+  static targets = ["loading", "products"];
+  static classes = ["hidden"];
+
   updateProducts(ev) {
     const orderCycleId = ev.detail.orderCycleId;
     // Updating the turbo-frame source will reload the frame
     this.element.src = `/order_cycles/${orderCycleId}/products`;
+
+    // show loading
+    this.loadingTarget.classList.remove(this.hiddenClass);
+    this.productsTarget.classList.add(this.hiddenClass);
   }
 }
