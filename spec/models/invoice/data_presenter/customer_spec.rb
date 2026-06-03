c9057f72ebf1f8bae127c3f1aea6dfce79f9afad
François Turbelin <paco@kokali.net>
2026-06-03 21:27:04 +0200

    Add attributes and specs for customer presenter

---
+++ spec/models/invoice/data_presenter/customer_spec.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+RSpec.describe Invoice::DataPresenter::Customer do
+  subject(:presenter) { described_class.new(data) }
+
+  let(:data) do
+    {
+      code: "C001",
+      email: "customer@example.com",
+      customer_type: "individual",
+      enterprise_name: "Da Box",
+      enterprise_acn: "123456789",
+      enterprise_charges_sales_tax: true
+    }
+  end
+
+  it { is_expected.to be_a(Invoice::DataPresenter::Base) }
+
+  describe "attribute readers" do
+    it "exposes code" do
+      expect(presenter.code).to eq("C001")
+    end
+
+    it "exposes email" do
+      expect(presenter.email).to eq("customer@example.com")
+    end
+
+    it "exposes customer_type" do
+      expect(presenter.customer_type).to eq("individual")
+    end
+
+    it "exposes enterprise_name" do
+      expect(presenter.enterprise_name).to eq("Da Box")
+    end
+
+    it "exposes enterprise_acn" do
+      expect(presenter.enterprise_acn).to eq("123456789")
+    end
+
+    it "exposes enterprise_charges_sales_tax" do
+      expect(presenter.enterprise_charges_sales_tax).to be(true)
+    end
+  end
+
+  context "when data is nil" do
+    let(:data) { nil }
+
+    it "returns nil for all attributes" do
+      expect(presenter.code).to be_nil
+      expect(presenter.email).to be_nil
+      expect(presenter.customer_type).to be_nil
+      expect(presenter.enterprise_name).to be_nil
+      expect(presenter.enterprise_acn).to be_nil
+      expect(presenter.enterprise_charges_sales_tax).to be_nil
+    end
+  end
+
+  context "when attributes are missing from data" do
+    let(:data) { {} }
+
+    it "returns nil for missing attributes" do
+      expect(presenter.code).to be_nil
+      expect(presenter.enterprise_name).to be_nil
+    end
+  end
+end
