dda80efa2c7ad90a6e4cd09235be6ecc0cf37550
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2026-06-03 14:12:38 +1000

    Add ability to not have a link for tooltip

--- app/components/admin_tooltip_component.rb
+++ app/components/admin_tooltip_component.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 class AdminTooltipComponent < ViewComponent::Base
-  def initialize(text:, link_text:, placement: "top", link: "", link_class: "")
+  def initialize(text:, link_text:, placement: "top", link: "", link_class: "",
+                 no_link_element: false )
     @text = text
     @link_text = link_text
     @placement = placement
     @link = link
     @link_class = link_class
+    @no_link_element = no_link_element
   end
 end
