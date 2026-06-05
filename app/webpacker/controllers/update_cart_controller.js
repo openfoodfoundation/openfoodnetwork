a2bd3dd4dbdff6becfaf81c8608bea79216629fd
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2026-06-05 17:16:11 +1000

    Recreate add to cart button functionality
    This is based on the existing AngularJS implementation

--- app/webpacker/controllers/update_cart_controller.js
+++ app/webpacker/controllers/update_cart_controller.js
@@ -1,11 +1,75 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  connect() {}
+  static targets = ["addButton", "quantityButton", "quantity", "minusButton"];
+  static values = { variantId: Number };
 
-  add({ params }) {
+  // TODO display number in cart
+  connect() {
+    let quantity = parseInt(this.quantityTarget.value);
+
+    if (quantity > 0) {
+      this.#showQuantityButtons();
+    }
+  }
+
+  addEmpty() {
+    this.#dispatchCartUpdate(1);
+
+    this.#showQuantityButtons();
+    this.quantityTarget.value = 1;
+  }
+
+  add() {
+    let quantity = parseInt(this.quantityTarget.value);
+    quantity = quantity + 1;
+    this.quantityTarget.value = quantity;
+
+    this.#dispatchCartUpdate(quantity);
+  }
+
+  remove() {
+    let quantity = parseInt(this.quantityTarget.value);
+    quantity = Math.max(0, quantity - 1);
+    this.quantityTarget.value = quantity;
+
+    this.#dispatchCartUpdate(quantity);
+
+    if (quantity === 0) {
+      this.#hideQuantityButtons();
+    }
+  }
+
+  manual(e) {
+    const quantity = parseInt(this.quantityTarget.value);
+
+    // check it's a number or a negative value
+    if (isNaN(quantity) || quantity < 0) {
+      return;
+    }
+
+    this.#dispatchCartUpdate(quantity);
+
+    if (quantity === 0) {
+      this.#hideQuantityButtons();
+    }
+  }
+
+  // private
+
+  #showQuantityButtons() {
+    this.addButtonTarget.style.display = "none";
+    this.quantityButtonTarget.style.display = "block";
+  }
+
+  #hideQuantityButtons() {
+    this.addButtonTarget.style.display = "block";
+    this.quantityButtonTarget.style.display = "none";
+  }
+
+  #dispatchCartUpdate(quantity) {
     const ev = new CustomEvent("updateCart", {
-      detail: { variant: { id: params.variant }, quantity: 1 },
+      detail: { variant: { id: this.variantIdValue }, quantity: quantity },
     });
     window.dispatchEvent(ev);
   }
