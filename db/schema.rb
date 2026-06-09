aec4678e08e37877331b677c8b30f3aa09deab25
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2026-06-09 12:04:34 +1000

    Fix schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_06_01_031443) do
+ActiveRecord::Schema[7.2].define(version: 2026_06_01_031443) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
@@ -30,10 +30,10 @@ ActiveRecord::Schema[7.1].define(version: 2026_06_01_031443) do
     t.string "filename", null: false
     t.string "content_type"
     t.text "metadata"
+    t.string "service_name", null: false
     t.bigint "byte_size", null: false
     t.string "checksum"
     t.datetime "created_at", precision: nil, null: false
-    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
