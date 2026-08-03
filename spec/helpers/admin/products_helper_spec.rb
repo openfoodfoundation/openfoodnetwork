c115543bd1a613d177c9f00a6808c2393e946549
Ahmed Ejaz <chahmedejaz9050@gmail.com>
2026-08-04 04:38:43 +0500

    Display variant images in carousels

--- spec/helpers/admin/products_helper_spec.rb
+++ spec/helpers/admin/products_helper_spec.rb
@@ -44,6 +44,76 @@ RSpec.describe Admin::ProductsHelper do
                            ])
       end
     end
+
+    context 'when product has no images but a variant has images' do
+      let(:product) { create(:product) }
+      let(:variant) { product.variants.first }
+
+      before do
+        Spree::Image.create!(
+          attachment: white_logo_file,
+          viewable: variant
+        )
+      end
+
+      it 'returns the variant image data' do
+        data = helper.product_carousel_images_data(product)
+
+        expect(data.size).to eq 1
+        expect(data.first[:url]).to be_present
+        expect(data.first[:alt]).to eq(product.name)
+        expect(data.first[:caption]).to be_nil
+      end
+    end
+
+    context 'when product has images and a variant also has images' do
+      let(:product) { create(:product) }
+      let(:variant) { product.variants.first }
+
+      before do
+        Spree::Image.create!(
+          attachment: white_logo_file,
+          viewable: product
+        )
+        Spree::Image.create!(
+          attachment: white_logo_file,
+          viewable: variant
+        )
+      end
+
+      it 'returns product images followed by variant images' do
+        data = helper.product_carousel_images_data(product)
+
+        expect(data.size).to eq 2
+        expect(data.first[:caption]).to eq "#{product.name} - 1"
+        expect(data.second[:caption]).to eq "#{product.name} - 2"
+      end
+    end
+
+    context 'when a variant image belongs to a variant with a display_name' do
+      let(:product) { create(:product) }
+      let(:variant) { create(:variant, product:, display_name: 'Red') }
+
+      before do
+        Spree::Image.create!(
+          attachment: white_logo_file,
+          viewable: product
+        )
+        Spree::Image.create!(
+          attachment: white_logo_file,
+          viewable: variant
+        )
+      end
+
+      it 'uses the variant display_name in the caption and alt' do
+        data = helper.product_carousel_images_data(product)
+
+        expect(data.size).to eq 2
+        expect(data.first[:caption]).to eq "#{product.name} - 1"
+        expect(data.second[:caption]).to eq "#{product.name} - 2"
+        expect(data.second[:alt]).to eq product.name
+      end
+    end
   end
 
   describe '#unit_value_with_description' do
