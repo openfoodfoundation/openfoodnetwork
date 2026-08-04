a0b8e7f311d486165e34c84353742626efcc5e2e
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2026-08-04 16:49:00 +1000

    Display variant data
    Unstyled for now

--- app/components/shop_variant_overlay_component.rb
+++ app/components/shop_variant_overlay_component.rb
@@ -1,9 +1,25 @@
 # frozen_string_literal: true
 
 class ShopVariantOverlayComponent < ViewComponent::Base
-  def initialize(variants:)
-    @variants = variants
+  def initialize(variants:, distributor:, order_cycle:, enterprise_fee_calculator:,
+                 variants_in_cart:, low_stock_display:, presenter_class: VariantPresenter)
+    @variants = variants.map { |v|
+      presenter_class.new(variant: v, distributor:, order_cycle:, enterprise_fee_calculator:)
+    }
+
+    @variants_in_cart = variants_in_cart
+    @low_stock_display = low_stock_display
   end
 
-  attr_reader :variants
+  attr_reader :variants, :variants_in_cart, :low_stock_display
+
+  private
+
+  def producers
+    @producers ||= @variants.map(&:enterprise).uniq
+  end
+
+  def same_producers?
+    producers.length == 1
+  end
 end
