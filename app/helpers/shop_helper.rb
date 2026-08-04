64a41311b73739d2b8ece022af5599158c924429
Ahmed Ejaz <chahmedejaz9050@gmail.com>
2026-08-05 04:41:29 +0500

    Deduplicate carousel image helper and fix misleading variant-image spec

--- app/helpers/shop_helper.rb
+++ app/helpers/shop_helper.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 module ShopHelper
+  include SharedHelper
+
   def oc_select_options(order_cycles)
     order_cycles.map { |oc| { time: pickup_time(oc), id: oc.id } }
   end
@@ -68,32 +70,9 @@ module ShopHelper
     "with-darker-background"
   end
 
-  def product_carousel_images_data(product, size: :large)
-    images = product.images.to_a + product.variant_images.to_a
-    show_caption = images.many?
-
-    return [default_carousel_image(size, product)] if images.empty?
-
-    images.map.with_index do |image, index|
-      {
-        url: image.url(size),
-        alt: image.alt.presence || product.name,
-        caption: show_caption ? "#{product.name} - #{index + 1}" : nil
-      }
-    end
-  end
-
   private
 
   def show_groups_tabs?
     !current_distributor.hide_groups_tab? && current_distributor.groups.any?
-  end
-
-  def default_carousel_image(size, product)
-    {
-      url: Spree::Image.default_image_url(size),
-      alt: product.name,
-      caption: nil
-    }
   end
 end
