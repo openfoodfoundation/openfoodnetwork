9ad03936a96e2d6dc56bec45804f00471a4c5d05
Mikel Cordovilla <mikel.cordovilla@tegonal.com>
2026-08-27 15:44:47 +0200

    Add instance-level address display format setting
    In Switzerland addresses are displayed as postal code + commune.
    This adds a configurable instance preference that lets instances
    choose between the default format (city, state, postal code) and
    a postal-code-first format (postal code, city, state omitted).
    
    Changes:
    - New Spree::Config preference :address_display_format
    - Update Spree::Address#full_address and #address_part2
    - Update Invoice::DataPresenter::Address similarly
    - Expose address_part2 through API::AddressSerializer
    - Replace inline city/state/zipcode rendering in views with
      address_part2 / full_address
    - Add setting dropdown on admin/general_settings/edit
    - Add specs and translations

--- app/serializers/api/address_serializer.rb
+++ app/serializers/api/address_serializer.rb
@@ -6,7 +6,7 @@ class Api::AddressSerializer < ActiveModel::Serializer
 
   attributes :id, :zipcode, :city, :state_name, :state_id,
              :phone, :firstname, :lastname, :address1, :address2, :city, :country_id,
-             :zipcode, :country_name
+             :zipcode, :country_name, :address_part2
 
   def country_name
     object.country&.name
