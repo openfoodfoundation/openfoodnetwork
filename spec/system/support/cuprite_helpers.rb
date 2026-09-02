1d5aa1dcc2b0607f324cc8ce55b9fbf0179e281a
Maikel Linke <maikel@email.org.au>
2026-09-02 15:52:54 +1000

    Remove useless helper
    I just add `binding.pry` or `debug` to a spec when I want to pause
    there. It's the same as in any other spec type or code. No need for a
    separate concept here.

--- spec/system/support/cuprite_helpers.rb
+++ spec/system/support/cuprite_helpers.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
 module CupriteHelpers
-  # Drop #pause anywhere in a test to stop the execution.
-  # Useful when you want to checkout the contents of a web page in the middle of a test
-  # running in a headful mode.
-  def pause
-    page.driver.pause
-  end
-
   # Drop #debug anywhere in a test to open a Chrome inspector and pause the execution
   def debug(*)
     page.driver.debug(*)
