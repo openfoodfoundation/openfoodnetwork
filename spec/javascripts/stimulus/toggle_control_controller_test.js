ed9912f5ee3a77acac879e0872b9fc1d425703a9
Manuel Gonçalves <mjgoncalves.trabalho@gmail.com>
2026-09-23 07:46:21 +0100

    Changes to test to reflect the html found in page

--- spec/javascripts/stimulus/toggle_control_controller_test.js
+++ spec/javascripts/stimulus/toggle_control_controller_test.js
@@ -217,30 +217,33 @@ describe("ToggleControlController", () => {
     });
   
     describe("#disableUnlessMatch", () => {
-      describe("with radio buttons", () => {
-        beforeEach(() => {
-          document.body.innerHTML = `
-            <div data-controller="toggle-control" data-toggle-control-match-value="by_category">
-              <input id="radio_category" type="radio" name="sorting" value="by_category" checked
-                    data-action="change->toggle-control#disableUnlessMatch" />
-              <input id="radio_producer" type="radio" name="sorting" value="by_producer"
-                    data-action="change->toggle-control#disableUnlessMatch" />
-              <textarea id="order_box" data-toggle-control-target="control"></textarea>
-            </div>`;
-        });
+      beforeEach(() => {
+        document.body.innerHTML = `
+          <div data-controller="toggle-control">
+            <input id="radio_category" type="radio" name="sorting" value="by_category" checked
+                  data-action="change->toggle-control#disableUnlessMatch" />
+            <input id="radio_producer" type="radio" name="sorting" value="by_producer"
+                  data-action="change->toggle-control#disableUnlessMatch" />
+            <textarea id="taxon_box" data-toggle-control-target="control"
+                      data-toggle-control-match-value="by_category"></textarea>
+            <textarea id="producer_box" data-toggle-control-target="control"
+                      data-toggle-control-match-value="by_producer"></textarea>
+          </div>`;
+      });
 
-        it("disables the control when a non-matching radio is selected", () => {
-          radio_producer.click();
+      it("disables the box whose match value does not correspond to the selected radio", () => {
+        radio_producer.click();
 
-          expect(order_box.readOnly).toBe(true);
-        });
+        expect(taxon_box.readOnly).toBe(true);
+        expect(producer_box.readOnly).toBe(false);
+      });
 
-        it("enables the control when the matching radio is selected", () => {
-          radio_producer.click();
-          radio_category.click();
+      it("updates both boxes when switching back", () => {
+        radio_producer.click();
+        radio_category.click();
 
-          expect(order_box.readOnly).toBe(false);
-        });
+        expect(taxon_box.readOnly).toBe(false);
+        expect(producer_box.readOnly).toBe(true);
       });
     });
   });
