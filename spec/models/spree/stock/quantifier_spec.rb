3e49a52624d7a05163630f7e3db2424cf75fdeed
Gaetan Craig-Riou <gaetan.riou@gmail.com>
2026-09-23 14:45:48 +1000

    Refactor, get rid of module

--- spec/models/spree/stock/quantifier_spec.rb
+++ spec/models/spree/stock/quantifier_spec.rb
@@ -1,21 +1,17 @@
 # frozen_string_literal: true
 
-module Spree
-  module Stock
-    RSpec.describe Quantifier do
-      let(:quantifier) { Spree::Stock::Quantifier.new(variant) }
-      let(:variant) { create(:variant, on_hand: 99) }
+RSpec.describe Spree::Stock::Quantifier do
+  let(:quantifier) { Spree::Stock::Quantifier.new(variant) }
+  let(:variant) { create(:variant, on_hand: 99) }
 
-      describe "#total_on_hand" do
-        context "with a soft-deleted variant" do
-          before do
-            variant.delete
-          end
+  describe "#total_on_hand" do
+    context "with a soft-deleted variant" do
+      before do
+        variant.delete
+      end
 
-          it "returns zero stock for the variant" do
-            expect(quantifier.total_on_hand).to eq 0
-          end
-        end
+      it "returns zero stock for the variant" do
+        expect(quantifier.total_on_hand).to eq 0
       end
     end
   end
